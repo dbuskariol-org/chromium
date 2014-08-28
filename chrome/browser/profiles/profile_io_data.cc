dbuskariol-org/chromium

commit f46f6d55efc7b8411220d920ceb25bb524780dc0

NSS Cros multiprofile: trust roots added by a profile shouldn't apply to other profiles.
BUG=218627

Review URL: https://codereview.chromium.org/137553004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249928 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/profiles/profile_io_data.cc
@@ -94,6 +94,7 @@
 #include "chrome/browser/chromeos/drive/drive_protocol_handler.h"
 #include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chrome/browser/chromeos/net/cert_verify_proc_chromeos.h"
 #include "chrome/browser/chromeos/policy/policy_cert_service.h"
 #include "chrome/browser/chromeos/policy/policy_cert_service_factory.h"
 #include "chrome/browser/chromeos/policy/policy_cert_verifier.h"
@@ -103,6 +104,7 @@
 #include "chromeos/settings/cros_settings_names.h"
 #include "crypto/nss_util.h"
 #include "crypto/nss_util_internal.h"
+#include "net/cert/multi_threaded_cert_verifier.h"
 #include "net/ssl/client_cert_store_chromeos.h"
 #endif  // defined(OS_CHROMEOS)
 
@@ -967,14 +969,20 @@ void ProfileIOData::Init(content::ProtocolHandlerMap* protocol_handlers) const {
 #endif
 
 #if defined(OS_CHROMEOS)
+  username_hash_ = profile_params_->username_hash;
+  crypto::ScopedPK11Slot public_slot =
+      crypto::GetPublicSlotForChromeOSUser(username_hash_);
+  // The private slot won't be ready by this point. It shouldn't be necessary
+  // for cert trust purposes anyway.
+  scoped_refptr<net::CertVerifyProc> verify_proc =
+      new chromeos::CertVerifyProcChromeOS(public_slot.Pass());
   if (cert_verifier_) {
-    cert_verifier_->InitializeOnIOThread();
+    cert_verifier_->InitializeOnIOThread(verify_proc);
     main_request_context_->set_cert_verifier(cert_verifier_.get());
   } else {
     main_request_context_->set_cert_verifier(
-        io_thread_globals->cert_verifier.get());
+        new net::MultiThreadedCertVerifier(verify_proc.get()));
   }
-  username_hash_ = profile_params_->username_hash;
 #else
   main_request_context_->set_cert_verifier(
       io_thread_globals->cert_verifier.get());

chrome/browser/chromeos/net/cert_verify_proc_chromeos.cc
@@ -1,0 +1,103 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/net/cert_verify_proc_chromeos.h"
+
+#include "net/cert/test_root_certs.h"
+#include "net/cert/x509_certificate.h"
+
+// NSS doesn't currently define CERT_LIST_TAIL.
+// See https://bugzilla.mozilla.org/show_bug.cgi?id=962413
+// Can be removed once chrome requires NSS version 3.16 to build.
+#ifndef CERT_LIST_TAIL
+#define CERT_LIST_TAIL(l) ((CERTCertListNode *)PR_LIST_TAIL(&l->list))
+#endif
+
+namespace chromeos {
+
+namespace {
+
+struct ChainVerifyArgs {
+  CertVerifyProcChromeOS* cert_verify_proc;
+  const net::CertificateList& additional_trust_anchors;
+};
+
+}  // namespace
+
+CertVerifyProcChromeOS::CertVerifyProcChromeOS() {}
+
+CertVerifyProcChromeOS::CertVerifyProcChromeOS(
+    crypto::ScopedPK11Slot public_slot) {
+  profile_filter_.Init(public_slot.Pass(), crypto::ScopedPK11Slot());
+}
+
+CertVerifyProcChromeOS::~CertVerifyProcChromeOS() {}
+
+int CertVerifyProcChromeOS::VerifyInternal(
+    net::X509Certificate* cert,
+    const std::string& hostname,
+    int flags,
+    net::CRLSet* crl_set,
+    const net::CertificateList& additional_trust_anchors,
+    net::CertVerifyResult* verify_result) {
+  ChainVerifyArgs chain_verify_args = {this, additional_trust_anchors};
+
+  CERTChainVerifyCallback chain_verify_callback;
+  chain_verify_callback.isChainValid =
+      &CertVerifyProcChromeOS::IsChainValidFunc;
+  chain_verify_callback.isChainValidArg =
+      static_cast<void*>(&chain_verify_args);
+
+  return VerifyInternalImpl(cert,
+                            hostname,
+                            flags,
+                            crl_set,
+                            additional_trust_anchors,
+                            &chain_verify_callback,
+                            verify_result);
+}
+
+// static
+SECStatus CertVerifyProcChromeOS::IsChainValidFunc(
+    void* is_chain_valid_arg,
+    const CERTCertList* current_chain,
+    PRBool* chain_ok) {
+  ChainVerifyArgs* args = static_cast<ChainVerifyArgs*>(is_chain_valid_arg);
+  CERTCertificate* cert = CERT_LIST_TAIL(current_chain)->cert;
+
+  if (net::TestRootCerts::HasInstance()) {
+    if (net::TestRootCerts::GetInstance()->Contains(cert)) {
+      // Certs in the TestRootCerts are not stored in any slot, and thus would
+      // not be allowed by the profile_filter. This should only be hit in tests.
+      DVLOG(3) << cert->subjectName << " is a TestRootCert";
+      *chain_ok = PR_TRUE;
+      return SECSuccess;
+    }
+  }
+
+  for (net::CertificateList::const_iterator i =
+           args->additional_trust_anchors.begin();
+       i != args->additional_trust_anchors.end();
+       ++i) {
+    if (net::X509Certificate::IsSameOSCert(cert, (*i)->os_cert_handle())) {
+      // Certs in the additional_trust_anchors should always be allowed, even if
+      // they aren't stored in a slot that would be allowed by the
+      // profile_filter.
+      DVLOG(3) << cert->subjectName << " is an additional_trust_anchor";
+      *chain_ok = PR_TRUE;
+      return SECSuccess;
+    }
+  }
+
+  // TODO(mattm): If crbug.com/334384 is fixed to allow setting trust
+  // properly when the same cert is in multiple slots, this would also need
+  // updating to check the per-slot trust values.
+  *chain_ok = args->cert_verify_proc->profile_filter_.IsCertAllowed(cert)
+                  ? PR_TRUE
+                  : PR_FALSE;
+  DVLOG(3) << cert->subjectName << " is " << (*chain_ok ? "ok" : "not ok");
+  return SECSuccess;
+}
+
+}  // namespace chromeos

chrome/browser/chromeos/net/cert_verify_proc_chromeos.h
@@ -1,0 +1,59 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_NET_CERT_VERIFY_PROC_CHROMEOS_H_
+#define CHROME_BROWSER_CHROMEOS_NET_CERT_VERIFY_PROC_CHROMEOS_H_
+
+#include "crypto/scoped_nss_types.h"
+#include "net/cert/cert_verify_proc_nss.h"
+#include "net/cert/nss_profile_filter_chromeos.h"
+
+namespace chromeos {
+
+// Wrapper around CertVerifyProcNSS which allows filtering trust decisions on a
+// per-slot basis.
+//
+// Note that only the simple case is currently handled (if a slot contains a new
+// trust root, that root should not be trusted by CertVerifyProcChromeOS
+// instances using other slots). More complicated cases are not handled (like
+// two slots adding the same root cert but with different trust values).
+class CertVerifyProcChromeOS : public net::CertVerifyProcNSS {
+ public:
+  // Creates a CertVerifyProc that doesn't allow any user-provided trust roots.
+  CertVerifyProcChromeOS();
+
+  // Creates a CertVerifyProc that doesn't allow trust roots provided by
+  // users other than the specified slot.
+  explicit CertVerifyProcChromeOS(crypto::ScopedPK11Slot public_slot);
+
+ protected:
+  virtual ~CertVerifyProcChromeOS();
+
+ private:
+  // net::CertVerifyProcNSS implementation:
+  virtual int VerifyInternal(
+      net::X509Certificate* cert,
+      const std::string& hostname,
+      int flags,
+      net::CRLSet* crl_set,
+      const net::CertificateList& additional_trust_anchors,
+      net::CertVerifyResult* verify_result) OVERRIDE;
+
+  // Check if the trust root of |current_chain| is allowed.
+  // |is_chain_valid_arg| is actually a ChainVerifyArgs*, which is used to pass
+  // state through the NSS CERTChainVerifyCallback.isChainValidArg parameter.
+  // If the chain is allowed, |*chain_ok| will be set to PR_TRUE.
+  // If the chain is not allowed, |*chain_ok| is set to PR_FALSE, and this
+  // function may be called again during a single certificate verification if
+  // there are multiple possible valid chains.
+  static SECStatus IsChainValidFunc(void* is_chain_valid_arg,
+                                    const CERTCertList* current_chain,
+                                    PRBool* chain_ok);
+
+  net::NSSProfileFilterChromeOS profile_filter_;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_NET_CERT_VERIFY_PROC_CHROMEOS_H_

chrome/browser/chromeos/net/cert_verify_proc_chromeos_unittest.cc
@@ -1,0 +1,373 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/net/cert_verify_proc_chromeos.h"
+
+#include "crypto/nss_util.h"
+#include "crypto/nss_util_internal.h"
+#include "net/base/net_errors.h"
+#include "net/base/test_data_directory.h"
+#include "net/cert/cert_verify_proc.h"
+#include "net/cert/cert_verify_result.h"
+#include "net/cert/nss_cert_database_chromeos.h"
+#include "net/test/cert_test_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+class CertVerifyProcChromeOSTest : public testing::Test {
+ public:
+  CertVerifyProcChromeOSTest() : user_1_("user1"), user_2_("user2") {}
+
+  virtual void SetUp() OVERRIDE {
+    // Initialize nss_util slots.
+    ASSERT_TRUE(user_1_.constructed_successfully());
+    ASSERT_TRUE(user_2_.constructed_successfully());
+    user_1_.FinishInit();
+    user_2_.FinishInit();
+
+    // Create NSSCertDatabaseChromeOS for each user.
+    db_1_.reset(new net::NSSCertDatabaseChromeOS(
+        crypto::GetPublicSlotForChromeOSUser(user_1_.username_hash()),
+        crypto::GetPrivateSlotForChromeOSUser(
+            user_1_.username_hash(),
+            base::Callback<void(crypto::ScopedPK11Slot)>())));
+    db_2_.reset(new net::NSSCertDatabaseChromeOS(
+        crypto::GetPublicSlotForChromeOSUser(user_2_.username_hash()),
+        crypto::GetPrivateSlotForChromeOSUser(
+            user_2_.username_hash(),
+            base::Callback<void(crypto::ScopedPK11Slot)>())));
+
+    // Create default verifier and for each user.
+    verify_proc_default_ = new CertVerifyProcChromeOS();
+    verify_proc_1_ = new CertVerifyProcChromeOS(db_1_->GetPublicSlot());
+    verify_proc_2_ = new CertVerifyProcChromeOS(db_2_->GetPublicSlot());
+
+    // Load test cert chains from disk.
+    certs_1_ =
+        net::CreateCertificateListFromFile(net::GetTestCertsDirectory(),
+                                           "multi-root-chain1.pem",
+                                           net::X509Certificate::FORMAT_AUTO);
+    ASSERT_EQ(4U, certs_1_.size());
+
+    certs_2_ =
+        net::CreateCertificateListFromFile(net::GetTestCertsDirectory(),
+                                           "multi-root-chain2.pem",
+                                           net::X509Certificate::FORMAT_AUTO);
+    ASSERT_EQ(4U, certs_2_.size());
+
+    // The chains:
+    //   1. A (end-entity) -> B -> C -> D (self-signed root)
+    //   2. A (end-entity) -> B -> C2 -> E (self-signed root)
+    ASSERT_TRUE(certs_1_[0]->Equals(certs_2_[0]));
+    ASSERT_TRUE(certs_1_[1]->Equals(certs_2_[1]));
+    ASSERT_FALSE(certs_1_[2]->Equals(certs_2_[2]));
+    ASSERT_EQ("C CA", certs_1_[2]->subject().common_name);
+    ASSERT_EQ("C CA", certs_2_[2]->subject().common_name);
+
+    root_1_.push_back(certs_1_.back());
+    root_2_.push_back(certs_2_.back());
+
+    ASSERT_EQ("D Root CA", root_1_[0]->subject().common_name);
+    ASSERT_EQ("E Root CA", root_2_[0]->subject().common_name);
+  }
+
+  int VerifyWithAdditionalTrustAnchors(
+      net::CertVerifyProc* verify_proc,
+      const net::CertificateList& additional_trust_anchors,
+      net::X509Certificate* cert,
+      std::string* root_subject_name) {
+    int flags = 0;
+    net::CertVerifyResult verify_result;
+    int error = verify_proc->Verify(cert,
+                                    "127.0.0.1",
+                                    flags,
+                                    NULL,
+                                    additional_trust_anchors,
+                                    &verify_result);
+    if (verify_result.verified_cert.get() &&
+        !verify_result.verified_cert->GetIntermediateCertificates().empty()) {
+      net::X509Certificate::OSCertHandle root =
+          verify_result.verified_cert->GetIntermediateCertificates().back();
+      root_subject_name->assign(root->subjectName);
+    } else {
+      root_subject_name->clear();
+    }
+    return error;
+  }
+
+  int Verify(net::CertVerifyProc* verify_proc,
+             net::X509Certificate* cert,
+             std::string* root_subject_name) {
+    net::CertificateList additional_trust_anchors;
+    return VerifyWithAdditionalTrustAnchors(
+        verify_proc, additional_trust_anchors, cert, root_subject_name);
+  }
+
+ protected:
+  crypto::ScopedTestNSSChromeOSUser user_1_;
+  crypto::ScopedTestNSSChromeOSUser user_2_;
+  scoped_ptr<net::NSSCertDatabaseChromeOS> db_1_;
+  scoped_ptr<net::NSSCertDatabaseChromeOS> db_2_;
+  scoped_refptr<net::CertVerifyProc> verify_proc_default_;
+  scoped_refptr<net::CertVerifyProc> verify_proc_1_;
+  scoped_refptr<net::CertVerifyProc> verify_proc_2_;
+  net::CertificateList certs_1_;
+  net::CertificateList certs_2_;
+  net::CertificateList root_1_;
+  net::CertificateList root_2_;
+};
+
+// Test that the CertVerifyProcChromeOS doesn't trusts roots that are in other
+// user's slots or that have been deleted, and that verifying done by one user
+// doesn't affect verifications done by others.
+TEST_F(CertVerifyProcChromeOSTest, TestChainVerify) {
+  scoped_refptr<net::X509Certificate> server = certs_1_[0];
+  std::string verify_root;
+  // Before either of the root certs have been trusted, all verifications should
+  // fail with CERT_AUTHORITY_INVALID.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_default_.get(), server.get(), &verify_root));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_1_.get(), server.get(), &verify_root));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_2_.get(), server.get(), &verify_root));
+
+  // Import and trust the D root for user 1.
+  net::NSSCertDatabase::ImportCertFailureList failed;
+  EXPECT_TRUE(db_1_->ImportCACerts(
+      root_1_, net::NSSCertDatabase::TRUSTED_SSL, &failed));
+  EXPECT_EQ(0U, failed.size());
+
+  // Imported CA certs are not trusted by default verifier.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_default_.get(), server.get(), &verify_root));
+  // User 1 should now verify successfully through the D root.
+  EXPECT_EQ(net::OK, Verify(verify_proc_1_.get(), server.get(), &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  // User 2 should still fail.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_2_.get(), server.get(), &verify_root));
+
+  // Import and trust the E root for user 2.
+  failed.clear();
+  EXPECT_TRUE(db_2_->ImportCACerts(
+      root_2_, net::NSSCertDatabase::TRUSTED_SSL, &failed));
+  EXPECT_EQ(0U, failed.size());
+
+  // Imported CA certs are not trusted by default verifier.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_default_.get(), server.get(), &verify_root));
+  // User 1 should still verify successfully through the D root.
+  EXPECT_EQ(net::OK, Verify(verify_proc_1_.get(), server.get(), &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  // User 2 should now verify successfully through the E root.
+  EXPECT_EQ(net::OK, Verify(verify_proc_2_.get(), server.get(), &verify_root));
+  EXPECT_EQ("CN=E Root CA", verify_root);
+
+  // Delete D root.
+  EXPECT_TRUE(db_1_->DeleteCertAndKey(root_1_[0]));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_default_.get(), server.get(), &verify_root));
+  // User 1 should now fail to verify.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_1_.get(), server.get(), &verify_root));
+  // User 2 should still verify successfully through the E root.
+  EXPECT_EQ(net::OK, Verify(verify_proc_2_.get(), server.get(), &verify_root));
+  EXPECT_EQ("CN=E Root CA", verify_root);
+
+  // Delete E root.
+  EXPECT_TRUE(db_2_->DeleteCertAndKey(root_2_[0]));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_default_.get(), server.get(), &verify_root));
+  // User 1 should still fail to verify.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_1_.get(), server.get(), &verify_root));
+  // User 2 should now fail to verify.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            Verify(verify_proc_2_.get(), server.get(), &verify_root));
+}
+
+// Test that roots specified through additional_trust_anchors are trusted for
+// that verification, and that there is not any caching that affects later
+// verifications.
+TEST_F(CertVerifyProcChromeOSTest, TestAdditionalTrustAnchors) {
+  EXPECT_TRUE(verify_proc_default_->SupportsAdditionalTrustAnchors());
+  EXPECT_TRUE(verify_proc_1_->SupportsAdditionalTrustAnchors());
+
+  scoped_refptr<net::X509Certificate> server = certs_1_[0];
+  std::string verify_root;
+  net::CertificateList additional_trust_anchors;
+
+  // Before either of the root certs have been trusted, all verifications should
+  // fail with CERT_AUTHORITY_INVALID.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            VerifyWithAdditionalTrustAnchors(verify_proc_default_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            VerifyWithAdditionalTrustAnchors(verify_proc_1_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+
+  // Use D Root CA as additional trust anchor. Verifications should succeed now.
+  additional_trust_anchors.push_back(root_1_[0]);
+  EXPECT_EQ(net::OK,
+            VerifyWithAdditionalTrustAnchors(verify_proc_default_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  EXPECT_EQ(net::OK,
+            VerifyWithAdditionalTrustAnchors(verify_proc_1_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  // User 2 should still fail.
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            VerifyWithAdditionalTrustAnchors(verify_proc_2_.get(),
+                                             net::CertificateList(),
+                                             server.get(),
+                                             &verify_root));
+
+  // Without additional trust anchors, verification should fail again.
+  additional_trust_anchors.clear();
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            VerifyWithAdditionalTrustAnchors(verify_proc_default_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+            VerifyWithAdditionalTrustAnchors(verify_proc_1_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+
+  // Import and trust the D Root CA for user 2.
+  net::CertificateList roots;
+  roots.push_back(root_1_[0]);
+  net::NSSCertDatabase::ImportCertFailureList failed;
+  EXPECT_TRUE(
+      db_2_->ImportCACerts(roots, net::NSSCertDatabase::TRUSTED_SSL, &failed));
+  EXPECT_EQ(0U, failed.size());
+
+  // Use D Root CA as additional trust anchor. Verifications should still
+  // succeed even if the cert is trusted by a different profile.
+  additional_trust_anchors.push_back(root_1_[0]);
+  EXPECT_EQ(net::OK,
+            VerifyWithAdditionalTrustAnchors(verify_proc_default_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  EXPECT_EQ(net::OK,
+            VerifyWithAdditionalTrustAnchors(verify_proc_1_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+  EXPECT_EQ(net::OK,
+            VerifyWithAdditionalTrustAnchors(verify_proc_2_.get(),
+                                             additional_trust_anchors,
+                                             server.get(),
+                                             &verify_root));
+  EXPECT_EQ("CN=D Root CA", verify_root);
+}
+
+class CertVerifyProcChromeOSOrderingTest
+    : public CertVerifyProcChromeOSTest,
+      public ::testing::WithParamInterface<
+          std::tr1::tuple<bool, int, std::string> > {};
+
+// Test a variety of different combinations of (maybe) verifying / (maybe)
+// importing / verifying again, to try to find any cases where caching might
+// affect the results.
+TEST_P(CertVerifyProcChromeOSOrderingTest, TrustThenVerify) {
+  const ParamType& param = GetParam();
+  const bool verify_first = std::tr1::get<0>(param);
+  const int trust_bitmask = std::tr1::get<1>(param);
+  const std::string test_order = std::tr1::get<2>(param);
+  DVLOG(1) << "verify_first: " << verify_first
+           << " trust_bitmask: " << trust_bitmask
+           << " test_order: " << test_order;
+
+  scoped_refptr<net::X509Certificate> server = certs_1_[0];
+  std::string verify_root;
+
+  if (verify_first) {
+    // Before either of the root certs have been trusted, all verifications
+    // should fail with CERT_AUTHORITY_INVALID.
+    EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+              Verify(verify_proc_default_.get(), server.get(), &verify_root));
+    EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+              Verify(verify_proc_1_.get(), server.get(), &verify_root));
+    EXPECT_EQ(net::ERR_CERT_AUTHORITY_INVALID,
+              Verify(verify_proc_2_.get(), server.get(), &verify_root));
+  }
+
+  int expected_user1_result = net::ERR_CERT_AUTHORITY_INVALID;
+  int expected_user2_result = net::ERR_CERT_AUTHORITY_INVALID;
+
+  if (trust_bitmask & 1) {
+    expected_user1_result = net::OK;
+    // Import and trust the D root for user 1.
+    net::NSSCertDatabase::ImportCertFailureList failed;
+    EXPECT_TRUE(db_1_->ImportCACerts(
+        root_1_, net::NSSCertDatabase::TRUSTED_SSL, &failed));
+    EXPECT_EQ(0U, failed.size());
+  }
+
+  if (trust_bitmask & 2) {
+    expected_user2_result = net::OK;
+    // Import and trust the E root for user 2.
+    net::NSSCertDatabase::ImportCertFailureList failed;
+    EXPECT_TRUE(db_2_->ImportCACerts(
+        root_2_, net::NSSCertDatabase::TRUSTED_SSL, &failed));
+    EXPECT_EQ(0U, failed.size());
+  }
+
+  // Repeat the tests twice, they should return the same each time.
+  for (int i = 0; i < 2; ++i) {
+    SCOPED_TRACE(i);
+    for (std::string::const_iterator j = test_order.begin();
+         j != test_order.end();
+         ++j) {
+      switch (*j) {
+        case 'd':
+          // Default verifier should always fail.
+          EXPECT_EQ(
+              net::ERR_CERT_AUTHORITY_INVALID,
+              Verify(verify_proc_default_.get(), server.get(), &verify_root));
+          break;
+        case '1':
+          EXPECT_EQ(expected_user1_result,
+                    Verify(verify_proc_1_.get(), server.get(), &verify_root));
+          if (expected_user1_result == net::OK)
+            EXPECT_EQ("CN=D Root CA", verify_root);
+          break;
+        case '2':
+          EXPECT_EQ(expected_user2_result,
+                    Verify(verify_proc_2_.get(), server.get(), &verify_root));
+          if (expected_user2_result == net::OK)
+            EXPECT_EQ("CN=E Root CA", verify_root);
+          break;
+        default:
+          FAIL();
+      }
+    }
+  }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Variations,
+    CertVerifyProcChromeOSOrderingTest,
+    ::testing::Combine(
+        ::testing::Bool(),
+        ::testing::Range(0, 1 << 2),
+        ::testing::Values("d12", "d21", "1d2", "12d", "2d1", "21d")));
+
+}  // namespace chromeos

chrome/browser/chromeos/policy/policy_cert_verifier.cc
@@ -49,16 +49,15 @@ PolicyCertVerifier::~PolicyCertVerifier() {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
 }
 
-void PolicyCertVerifier::InitializeOnIOThread() {
+void PolicyCertVerifier::InitializeOnIOThread(
+    const scoped_refptr<net::CertVerifyProc>& verify_proc) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
-  scoped_refptr<net::CertVerifyProc> verify_proc =
-      net::CertVerifyProc::CreateDefault();
   if (!verify_proc->SupportsAdditionalTrustAnchors()) {
     LOG(WARNING)
         << "Additional trust anchors not supported on the current platform!";
   }
   net::MultiThreadedCertVerifier* verifier =
-      new net::MultiThreadedCertVerifier(verify_proc.get());
+      new net::MultiThreadedCertVerifier(verify_proc);
   verifier->SetCertTrustAnchorProvider(this);
   delegate_.reset(verifier);
 }

commit cef5ed9ad0affcac039c52760e1774f656711de7

Fix leak of MultiThreadedCertVerifier in ProfileIOData.
Heap allocated MultiThreadedCertVerifier was passed to URLRequestContext::set_cert_verifier, which doesn't take ownership.

Leak was introduced in r249928.

BUG=NONE

Review URL: https://codereview.chromium.org/516453002

chrome/browser/profiles/profile_io_data.cc
@@ -124,6 +124,7 @@
 #include "components/user_manager/user_manager.h"
 #include "crypto/nss_util.h"
 #include "crypto/nss_util_internal.h"
+#include "net/cert/cert_verifier.h"
 #include "net/cert/multi_threaded_cert_verifier.h"
 #include "net/ssl/client_cert_store_chromeos.h"
 #endif  // defined(OS_CHROMEOS)
@@ -475,7 +476,10 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   network_prediction_options_.MoveToThread(io_message_loop_proxy);
 
 #if defined(OS_CHROMEOS)
-  cert_verifier_ = policy::PolicyCertServiceFactory::CreateForProfile(profile);
+  scoped_ptr<policy::PolicyCertVerifier> verifier =
+      policy::PolicyCertServiceFactory::CreateForProfile(profile);
+  policy_cert_verifier_ = verifier.get();
+  cert_verifier_ = verifier.Pass();
 #endif
   // The URLBlacklistManager has to be created on the UI thread to register
   // observers of |pref_service|, and it also has to clean up on
@@ -571,6 +575,7 @@ ProfileIOData::ProfileParams::~ProfileParams() {}
 ProfileIOData::ProfileIOData(Profile::ProfileType profile_type)
     : initialized_(false),
 #if defined(OS_CHROMEOS)
+      policy_cert_verifier_(NULL),
       use_system_key_slot_(false),
 #endif
       resource_context_(new ResourceContext(this)),
@@ -1081,19 +1086,19 @@ void ProfileIOData::Init(
   if (use_system_key_slot_)
     EnableNSSSystemKeySlotForResourceContext(resource_context_.get());
 
-  scoped_refptr<net::CertVerifyProc> verify_proc;
   crypto::ScopedPK11Slot public_slot =
       crypto::GetPublicSlotForChromeOSUser(username_hash_);
   // The private slot won't be ready by this point. It shouldn't be necessary
   // for cert trust purposes anyway.
-  verify_proc = new chromeos::CertVerifyProcChromeOS(public_slot.Pass());
-  if (cert_verifier_) {
-    cert_verifier_->InitializeOnIOThread(verify_proc);
-    main_request_context_->set_cert_verifier(cert_verifier_.get());
+  scoped_refptr<net::CertVerifyProc> verify_proc(
+      new chromeos::CertVerifyProcChromeOS(public_slot.Pass()));
+  if (policy_cert_verifier_) {
+    DCHECK_EQ(policy_cert_verifier_, cert_verifier_.get());
+    policy_cert_verifier_->InitializeOnIOThread(verify_proc);
   } else {
-    main_request_context_->set_cert_verifier(
-        new net::MultiThreadedCertVerifier(verify_proc.get()));
+    cert_verifier_.reset(new net::MultiThreadedCertVerifier(verify_proc.get()));
   }
+  main_request_context_->set_cert_verifier(cert_verifier_.get());
 #else
   main_request_context_->set_cert_verifier(
       io_thread_globals->cert_verifier.get());

chrome/browser/profiles/profile_io_data.h
@@ -47,6 +47,7 @@ class InfoMap;
 }
 
 namespace net {
+class CertVerifier;
 class ChannelIDService;
 class CookieStore;
 class FraudulentCertificateReporter;
@@ -587,7 +588,10 @@ class ProfileIOData {
   mutable scoped_ptr<net::HttpServerProperties>
       http_server_properties_;
 #if defined(OS_CHROMEOS)
-  mutable scoped_ptr<policy::PolicyCertVerifier> cert_verifier_;
+  // Set to |cert_verifier_| if it references a PolicyCertVerifier. In that
+  // case, the verifier is owned by  |cert_verifier_|. Otherwise, set to NULL.
+  mutable policy::PolicyCertVerifier* policy_cert_verifier_;
+  mutable scoped_ptr<net::CertVerifier> cert_verifier_;
   mutable std::string username_hash_;
   mutable bool use_system_key_slot_;
 #endif