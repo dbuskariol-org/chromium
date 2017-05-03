ac61ea38538a827ccaea5ce7db224adb38cb74f4
Josh Karlin <jkarlin@chromium.org>
2017-05-03 14:41:12 -0400

    Disable AdsPageLoadMetricsObserver by default
    1) The feature was enabled, and caused crashes: https://crrev.com/2798953002
    2) The feature was reverted: https://crrev.com/2857963002
    3) The feature was relanded, but disabled: https://crrev.com/2861473003
    4) This merge does the same thing as (2+3), but just has the disabling bits
    
    R=csharrison@chromium.org
    TBR=csharrison
    
    BUG=717892
    
    Review-Url: https://codereview.chromium.org/2861803002 .
    Cr-Commit-Position: refs/branch-heads/3088@{#3}
    Cr-Branched-From: 0ea8510a570b6556ea40679bb03b64a5fdcc7550-refs/heads/master@{#468845}

--- chrome/browser/page_load_metrics/observers/ads_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ads_page_load_metrics_observer.cc
@@ -19,7 +19,8 @@
 
 namespace {
 
-const base::Feature kAdsFeature{"AdsMetrics", base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kAdsFeature{"AdsMetrics",
+                                base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool FrameIsAd(content::NavigationHandle* navigation_handle) {
   content::RenderFrameHost* current_frame_host =
