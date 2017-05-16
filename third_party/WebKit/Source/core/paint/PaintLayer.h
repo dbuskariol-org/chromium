2c693969ab57ca41b67582265c08c1094a71a0bc
Walter Korman <wkorman@chromium.org>
2017-05-16 11:36:52 -0700

    Incorporate ComputedStyle::hasTransform when diffing transform styles.
    BUG=695125
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2767783003
    Cr-Original-Commit-Position: refs/heads/master@{#458907}
    Review-Url: https://codereview.chromium.org/2891453003 .
    Cr-Commit-Position: refs/branch-heads/3029@{#846}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- third_party/WebKit/Source/core/paint/PaintLayer.h
+++ third_party/WebKit/Source/core/paint/PaintLayer.h
@@ -1250,6 +1250,8 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   FRIEND_TEST_ALL_PREFIXES(PaintLayerTest,
                            DescendantDependentFlagsStopsAtThrottledFrames);
+  FRIEND_TEST_ALL_PREFIXES(PaintLayerTest,
+                           PaintLayerTransformUpdatedOnStyleTransformAnimation);
 };
 
 }  // namespace blink
