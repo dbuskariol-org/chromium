nwjs/chromium.src

commit 9c5131ad426872a1c931a6e3a27bbd4c469b7721

Under refresh, use the same menu options for Windows/Linux as under Mac
Use the BubbleBorder for the menus since the drop-shadow under Windows
doesn't track the shape of the menu. We also have much more control over
the shape and depth of the drop-shadow.

Bug: 837782
Change-Id: I91b9340251c2cd0b60e3b3f66d450a4791113007
Reviewed-on: https://chromium-review.googlesource.com/1159532
Commit-Queue: Allen Bauer <kylixrd@chromium.org>
Reviewed-by: Evan Stade <estade@chromium.org>
Reviewed-by: Michael Wasserman <msw@chromium.org>
Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
Cr-Commit-Position: refs/heads/master@{#590731}

ui/views/controls/menu/menu_scroll_view_container.cc
@@ -8,6 +8,7 @@
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/ax_node_data.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/border.h"
@@ -19,6 +20,7 @@
 #include "ui/views/round_rect_painter.h"
 
 using ui::NativeTheme;
+using MD = ui::MaterialDesignController;
 
 namespace views {
 
@@ -186,14 +188,14 @@ MenuScrollViewContainer::MenuScrollViewContainer(SubmenuView* content_view)
   arrow_ = BubbleBorderTypeFromAnchor(
       content_view_->GetMenuItem()->GetMenuController()->GetAnchorPosition());
 
-  if (arrow_ != BubbleBorder::NONE)
+  if (HasBubbleBorder())
     CreateBubbleBorder();
   else
     CreateDefaultBorder();
 }
 
 bool MenuScrollViewContainer::HasBubbleBorder() {
-  return arrow_ != BubbleBorder::NONE;
+  return (arrow_ != BubbleBorder::NONE) || MD::IsRefreshUi();
 }
 
 void MenuScrollViewContainer::SetBubbleArrowOffset(int offset) {
@@ -237,7 +239,7 @@ void MenuScrollViewContainer::Layout() {
 
 void MenuScrollViewContainer::OnNativeThemeChanged(
     const ui::NativeTheme* theme) {
-  if (arrow_ == BubbleBorder::NONE)
+  if (!HasBubbleBorder())
     CreateDefaultBorder();
 }
 
@@ -309,13 +311,14 @@ void MenuScrollViewContainer::CreateDefaultBorder() {
 void MenuScrollViewContainer::CreateBubbleBorder() {
   bubble_border_ =
       new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW, SK_ColorWHITE);
-  if (content_view_->GetMenuItem()
-          ->GetMenuController()
-          ->use_touchable_layout()) {
+  MenuController* controller =
+      content_view_->GetMenuItem()->GetMenuController();
+  if (MD::IsRefreshUi() || controller->use_touchable_layout()) {
     const MenuConfig& menu_config = MenuConfig::instance();
-    bubble_border_->SetCornerRadius(menu_config.touchable_corner_radius);
+    bubble_border_->SetCornerRadius(
+        menu_config.CornerRadiusForMenu(controller));
     bubble_border_->set_md_shadow_elevation(
-        menu_config.touchable_menu_shadow_elevation);
+        menu_config.ShadowElevationForMenu(controller));
     scroll_view_->GetContents()->SetBorder(CreateEmptyBorder(
         gfx::Insets(menu_config.vertical_touchable_menu_item_padding, 0)));
   }

ui/views/controls/menu/menu_scroll_view_container.h
@@ -9,6 +9,7 @@
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/menu/menu_types.h"
 #include "ui/views/view.h"
+#include "ui/views/views_export.h"
 
 namespace views {
 
@@ -17,7 +18,7 @@ class SubmenuView;
 // MenuScrollViewContainer contains the SubmenuView (through a MenuScrollView)
 // and two scroll buttons. The scroll buttons are only visible and enabled if
 // the preferred height of the SubmenuView is bigger than our bounds.
-class MenuScrollViewContainer : public View {
+class VIEWS_EXPORT MenuScrollViewContainer : public View {
  public:
   explicit MenuScrollViewContainer(SubmenuView* content_view);
 
@@ -25,7 +26,7 @@ class MenuScrollViewContainer : public View {
   View* scroll_down_button() const { return scroll_down_button_; }
   View* scroll_up_button() const { return scroll_up_button_; }
 
-  // External function to check if the bubble border is usd.
+  // External function to check if the bubble border is used.
   bool HasBubbleBorder();
 
   // Offsets the Arrow from the default location.

chrome/browser/ui/window_sizer/window_sizer_ash_uitest.cc
@@ -19,6 +19,7 @@
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "ui/events/test/event_generator.h"
+#include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/view.h"
@@ -71,17 +72,25 @@ void OpenBrowserUsingContextMenuOnRootWindow(aura::Window* root) {
   // Chrome replies to Ash with the context menu items to display.
   ChromeLauncherController::instance()->FlushForTesting();
 
+  const views::MenuConfig& menu_config = views::MenuConfig::instance();
   // Move the cursor up to the "New window" menu option - assumes menu content.
-  const int offset =
+  const int y_offset =
       // Top half of the button we just clicked on.
       ash::ShelfConstants::button_size() / 2 +
       // Space between shelf top and menu bottom. Here we get this menu with
       // a right-click but long-pressing yields the same result. All menus
       // here use a touchable layout.
-      views::MenuConfig::instance().touchable_anchor_offset +
+      menu_config.touchable_anchor_offset +
       // 2 menu items we don't want, and go over part of the one we want.
-      2.2 * views::MenuConfig::instance().touchable_menu_height;
-  generator.MoveMouseBy(0, -offset);
+      2.2 * menu_config.touchable_menu_height;
+  const int x_offset =
+      // Skip over the left edge border inset.
+      views::BubbleBorder::GetBorderAndShadowInsets(
+          menu_config.touchable_menu_shadow_elevation)
+          .left() +
+      // Space between the left edge and the item content.
+      menu_config.item_horizontal_padding;
+  generator.MoveMouseBy(x_offset, -y_offset);
   generator.ReleaseRightButton();
 
   // Ash notifies Chrome's ShelfItemDelegate that the menu item was selected.

ui/views/controls/menu/menu_config.cc
@@ -5,9 +5,11 @@
 #include "ui/views/controls/menu/menu_config.h"
 
 #include "base/macros.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/controls/menu/menu_image_util.h"
 #include "ui/views/controls/menu/menu_item_view.h"
+#include "ui/views/layout/layout_provider.h"
 #include "ui/views/round_rect_painter.h"
 
 namespace views {
@@ -83,6 +85,13 @@ int MenuConfig::CornerRadiusForMenu(const MenuController* controller) const {
   return corner_radius;
 }
 
+int MenuConfig::ShadowElevationForMenu(const MenuController* controller) const {
+  if ((controller && controller->use_touchable_layout()) ||
+      ui::MaterialDesignController::IsRefreshUi())
+    return touchable_menu_shadow_elevation;
+  return 0;
+}
+
 bool MenuConfig::ShouldShowAcceleratorText(const MenuItemView* item,
                                            base::string16* text) const {
   if (!show_accelerators || !item->GetDelegate() || !item->GetCommand())
@@ -102,6 +111,39 @@ bool MenuConfig::ShouldShowAcceleratorText(const MenuItemView* item,
 const MenuConfig& MenuConfig::instance() {
   CR_DEFINE_STATIC_LOCAL(MenuConfig, instance, ());
   return instance;
+}
+
+void MenuConfig::InitMaterialMenuConfig() {
+  // These config parameters are from https://crbug.com/829347 and the spec
+  // images linked from that bug.
+  menu_horizontal_border_size = 0;
+  submenu_horizontal_inset = 0;
+  minimum_text_item_height = 28;
+  minimum_container_item_height = 40;
+  minimum_menu_width = 320;
+  label_to_arrow_padding = 0;
+  arrow_to_edge_padding = 16;
+  check_width = 16;
+  check_height = 16;
+  separator_height = 9;
+  separator_lower_height = 4;
+  separator_upper_height = 4;
+  separator_spacing_height = 5;
+  separator_thickness = 1;
+  align_arrow_and_shortcut = true;
+  use_outer_border = false;
+  icons_in_label = true;
+  corner_radius = 8;
+  auxiliary_corner_radius = 4;
+  if (!ui::MaterialDesignController::IsTouchOptimizedUiEnabled()) {
+    touchable_menu_height = minimum_text_item_height;
+    touchable_menu_width = minimum_menu_width;
+    if (LayoutProvider::Get()) {
+      touchable_menu_shadow_elevation =
+          LayoutProvider::Get()->GetShadowElevationMetric(EMPHASIS_MEDIUM);
+    }
+    touchable_anchor_offset = 0;
+  }
 }
 
 }  // namespace views

ui/views/controls/menu/menu_config.h
@@ -22,10 +22,17 @@ struct VIEWS_EXPORT MenuConfig {
 
   static const MenuConfig& instance();
 
+  // Initialize to the specs presented in https://crbug.com/829347
+  void InitMaterialMenuConfig();
+
   // Helper methods to simplify access to MenuConfig:
   // Returns the appropriate corner radius for the menu controlled by
   // |controller|, or the default corner radius if |controller| is nullptr.
   int CornerRadiusForMenu(const MenuController* controller) const;
+
+  // Returns the appropriate shadow elevation for the menu controlled by
+  // |controller|, or the default shadow elevation if |controller| is nullptr.
+  int ShadowElevationForMenu(const MenuController* controller) const;
 
   // Returns whether |item_view| should show accelerator text. If so, returns
   // the text to show.

ui/views/controls/menu/menu_config_chromeos.cc
@@ -4,26 +4,30 @@
 
 #include "ui/views/controls/menu/menu_config.h"
 
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/views/controls/menu/menu_image_util.h"
 
 namespace views {
 
 void MenuConfig::Init() {
-  submenu_horizontal_inset = 1;
-  arrow_to_edge_padding = 21;
-  gfx::ImageSkia check = GetMenuCheckImage(false);
-  check_height = check.height();
-  item_min_height = 29;
-  separator_spacing_height = 7;
-  separator_lower_height = 8;
-  separator_upper_height = 8;
+  if (ui::MaterialDesignController::IsRefreshUi()) {
+    InitMaterialMenuConfig();
+  } else {
+    align_arrow_and_shortcut = true;
+    gfx::ImageSkia check = GetMenuCheckImage(false);
+    check_height = check.height();
+    corner_radius = 2;
+    separator_spacing_height = 7;
+    separator_lower_height = 8;
+    separator_upper_height = 8;
+    submenu_horizontal_inset = 1;
+    // In Ash, the border is provided by the shadow.
+    use_outer_border = false;
+  }
   always_use_icon_to_label_padding = true;
-  align_arrow_and_shortcut = true;
+  arrow_to_edge_padding = 21;
+  item_min_height = 29;
   offset_context_menus = true;
-  corner_radius = 2;
-
-  // In Ash, the border is provided by the shadow.
-  use_outer_border = false;
 }
 
 }  // namespace views

ui/views/controls/menu/menu_config_linux.cc
@@ -4,10 +4,15 @@
 
 #include "ui/views/controls/menu/menu_config.h"
 
+#include "ui/base/material_design/material_design_controller.h"
+
 namespace views {
 
 void MenuConfig::Init() {
-  arrow_to_edge_padding = 6;
+  if (ui::MaterialDesignController::IsRefreshUi())
+    InitMaterialMenuConfig();
+  else
+    arrow_to_edge_padding = 6;
 }
 
 }  // namespace views

ui/views/controls/menu/menu_config_mac.mm
@@ -8,45 +8,16 @@
 
 #include "base/mac/mac_util.h"
 
-namespace {
-
-void InitMaterialMenuConfig(views::MenuConfig* config) {
-  // These config parameters are from https://crbug.com/829347 and the spec
-  // images linked from that bug.
-  config->menu_horizontal_border_size = 0;
-  config->submenu_horizontal_inset = 0;
-  config->minimum_text_item_height = 28;
-  config->minimum_container_item_height = 40;
-  config->minimum_menu_width = 320;
-  config->label_to_arrow_padding = 0;
-  config->arrow_to_edge_padding = 16;
-  config->check_width = 16;
-  config->check_height = 16;
-  config->arrow_width = 8;
-  config->separator_height = 9;
-  config->separator_lower_height = 4;
-  config->separator_upper_height = 4;
-  config->separator_spacing_height = 5;
-  config->separator_thickness = 1;
-  config->align_arrow_and_shortcut = true;
-  config->use_outer_border = false;
-  config->icons_in_label = true;
-  config->corner_radius = 8;
-  config->auxiliary_corner_radius = 4;
-}
-
-}  // namespace
-
 namespace views {
 
 void MenuConfig::Init() {
+  InitMaterialMenuConfig();
   font_list = gfx::FontList(gfx::Font([NSFont menuFontOfSize:0.0]));
   check_selected_combobox_item = true;
   arrow_key_selection_wraps = false;
   use_mnemonics = false;
   show_context_menu_accelerators = false;
   all_menus_use_prefix_selection = true;
-  InitMaterialMenuConfig(this);
 }
 
 }  // namespace views

dbuskariol-org/chromium

commit fc041f1eaef387a886767e99b1c0e4f94f5dc50b

[Merge to 3559] Revert "[GTK] Fix menu background color regression"
> This reverts commit c551571fa598ce656b209364cc4e407f354067c8.
>
> Reason for revert: Need to revert a dependent CL (https://chromium-review.googlesource.com/c/chromium/src/+/1229173), so reverting this to prevent merge conflicts
>
> Original change's description:
> > [GTK] Fix menu background color regression
> >
> > Regressed after 9c5131ad426872a1c931a6e3a27bbd4c469b7721.  This caused the menu
> > background to always render white even though the text colors would still be
> > styled using the system theme.  This caused illegible text when the menu
> > background was supposed to be dark.
> >
> > R=​pkasting
> > TBR=sky
> >
> > Change-Id: I39b836e0941cbfc033515fe3891ecd42d89d73a4
> > Reviewed-on: https://chromium-review.googlesource.com/1237519
> > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
> > Reviewed-by: Peter Kasting <pkasting@chromium.org>
> > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
> > Cr-Commit-Position: refs/heads/master@{#593039}
>
> TBR=sky@chromium.org,pkasting@chromium.org,thomasanderson@chromium.org
> NOTRY=true
> NOPRESUBMIT=true
>
> Change-Id: If1fa983c97f0942cea3fccad14372ed1f5922080
> Reviewed-on: https://chromium-review.googlesource.com/1240713
> Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
> Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
> Cr-Commit-Position: refs/heads/master@{#593649}
> Bug:

TBR=sky@chromium.org,pkasting@chromium.org,thomasanderson@chromium.org
BUG=887424
NOTRY=true
NOPRESUBMIT=true
NOTREECHECKS=true

Change-Id: If24e4ba99e096786e4fc84c793fd3d6d0b31683a
Reviewed-on: https://chromium-review.googlesource.com/1241233
Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
Cr-Commit-Position: refs/branch-heads/3559@{#8}
Cr-Branched-From: b522cfef32adcfdedf00a6e03362101db42bd80a-refs/heads/master@{#593399}

ui/views/controls/menu/menu_scroll_view_container.cc
@@ -188,7 +188,10 @@ MenuScrollViewContainer::MenuScrollViewContainer(SubmenuView* content_view)
   arrow_ = BubbleBorderTypeFromAnchor(
       content_view_->GetMenuItem()->GetMenuController()->GetAnchorPosition());
 
-  CreateBorder();
+  if (HasBubbleBorder())
+    CreateBubbleBorder();
+  else
+    CreateDefaultBorder();
 }
 
 bool MenuScrollViewContainer::HasBubbleBorder() {
@@ -236,7 +239,8 @@ void MenuScrollViewContainer::Layout() {
 
 void MenuScrollViewContainer::OnNativeThemeChanged(
     const ui::NativeTheme* theme) {
-  CreateBorder();
+  if (!HasBubbleBorder())
+    CreateDefaultBorder();
 }
 
 void MenuScrollViewContainer::OnPaintBackground(gfx::Canvas* canvas) {
@@ -266,13 +270,6 @@ void MenuScrollViewContainer::OnBoundsChanged(
   scroll_up_button_->SetVisible(content_pref.height() > height());
   scroll_down_button_->SetVisible(content_pref.height() > height());
   Layout();
-}
-
-void MenuScrollViewContainer::CreateBorder() {
-  if (HasBubbleBorder())
-    CreateBubbleBorder();
-  else
-    CreateDefaultBorder();
 }
 
 void MenuScrollViewContainer::CreateDefaultBorder() {
@@ -313,9 +310,7 @@ void MenuScrollViewContainer::CreateDefaultBorder() {
 
 void MenuScrollViewContainer::CreateBubbleBorder() {
   bubble_border_ =
-      new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW,
-                       GetNativeTheme()->GetSystemColor(
-                           ui::NativeTheme::kColorId_MenuBackgroundColor));
+      new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW, SK_ColorWHITE);
   MenuController* controller =
       content_view_->GetMenuItem()->GetMenuController();
   if (MD::IsRefreshUi() || controller->use_touchable_layout()) {

ui/views/controls/menu/menu_scroll_view_container.h
@@ -44,9 +44,6 @@ class VIEWS_EXPORT MenuScrollViewContainer : public View {
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
  private:
-  // Creates the appropriate default or bubble border.
-  void CreateBorder();
-
   // Create the default border.
   void CreateDefaultBorder();
 