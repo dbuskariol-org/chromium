02cdcb09e972b370ec68860c44f7c83ea8bd33c9
Pedro Amaral <amaralp@chromium.org>
2019-02-22 17:50:05 +0000

    Adding null check to TabThemeColorProvider destruction
    Added a null check before trying to destroy the TabThemeColorProvider
    in ChromeActivity.
    
    Bug: 934183
    Change-Id: If1a9de3664adc24d669f6208eb337e2837d38138
    Reviewed-on: https://chromium-review.googlesource.com/c/1480723
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#634464}(cherry picked from commit 1871149c3562b6a7df099856a716b7df174e4aaf)
    Reviewed-on: https://chromium-review.googlesource.com/c/1483259
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3713@{#4}
    Cr-Branched-From: 186521f9ea29496e00029b6d6cc014116820413f-refs/heads/master@{#634009}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1388,7 +1388,11 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             manager.removeTouchExplorationStateChangeListener(mTouchExplorationStateChangeListener);
         }
 
-        mTabThemeColorProvider.destroy();
+        if (mTabThemeColorProvider != null) {
+            mTabThemeColorProvider.destroy();
+            mTabThemeColorProvider = null;
+        }
+
         mActivityTabProvider.destroy();
 
         mComponent = null;
