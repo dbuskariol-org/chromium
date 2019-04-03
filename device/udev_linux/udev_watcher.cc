dbuskariol-org/chromium

commit 99d9fdb47d4e9f5e57e4122fc1d0c798532d442d

UdevWatcher: Merging UdevLinux into UdevWatcher
UdevLinux and UdevWatcher had similar functionality, this patch
is removing UdevLinux and adding its functionality to UdevWatcher.
It was decided to keep UdevWatcher, because of its ready-made method
to enumerate devices.
From UdevLinux, was kept the filters functionality to be able to select
the event from the subsystem we are interested to monitor.

Major change in UdevWatcher is at creation, by monitoring only devices
matching in the subsystem filter list.

UdevLinux is removed from code base.

All the users of deleted UdevLinux are modified to take into use
UdevWatcher.

Bug: 940514
Change-Id: I1ca191c82da32efc80d3308bb8ad7db4eff6d899
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543252
Reviewed-by: Reilly Grant <reillyg@chromium.org>
Reviewed-by: Dan Sanders <sandersd@chromium.org>
Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Cr-Commit-Position: refs/heads/master@{#645840}

device/udev_linux/udev_watcher.cc
@@ -16,7 +16,9 @@ void UdevWatcher::Observer::OnDeviceAdded(ScopedUdevDevicePtr device) {}
 
 void UdevWatcher::Observer::OnDeviceRemoved(ScopedUdevDevicePtr device) {}
 
-std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
+std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(
+    Observer* observer,
+    const std::vector<Filter>& filters) {
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
   ScopedUdevPtr udev(udev_new());
@@ -32,6 +34,12 @@ std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
     return nullptr;
   }
 
+  for (const Filter& filter : filters) {
+    const int ret = udev_monitor_filter_add_match_subsystem_devtype(
+        udev_monitor.get(), filter.subsystem.c_str(), filter.devtype.c_str());
+    CHECK_EQ(0, ret);
+  }
+
   if (udev_monitor_enable_receiving(udev_monitor.get()) != 0) {
     LOG(ERROR) << "Failed to enable receiving udev events.";
     return nullptr;
@@ -44,7 +52,7 @@ std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
   }
 
   return base::WrapUnique(new UdevWatcher(
-      std::move(udev), std::move(udev_monitor), monitor_fd, observer));
+      std::move(udev), std::move(udev_monitor), monitor_fd, observer, filters));
 }
 
 UdevWatcher::~UdevWatcher() {
@@ -59,6 +67,12 @@ void UdevWatcher::EnumerateExistingDevices() {
   if (!enumerate) {
     LOG(ERROR) << "Failed to initialize a udev enumerator.";
     return;
+  }
+
+  for (const Filter& filter : udev_filters_) {
+    const int ret = udev_enumerate_add_match_subsystem(
+        enumerate.get(), filter.subsystem.c_str());
+    CHECK_EQ(0, ret);
   }
 
   if (udev_enumerate_scan_devices(enumerate.get()) != 0) {
@@ -79,10 +93,12 @@ void UdevWatcher::EnumerateExistingDevices() {
 UdevWatcher::UdevWatcher(ScopedUdevPtr udev,
                          ScopedUdevMonitorPtr udev_monitor,
                          int monitor_fd,
-                         Observer* observer)
+                         Observer* observer,
+                         const std::vector<Filter>& filters)
     : udev_(std::move(udev)),
       udev_monitor_(std::move(udev_monitor)),
-      observer_(observer) {
+      observer_(observer),
+      udev_filters_(filters) {
   file_watcher_ = base::FileDescriptorWatcher::WatchReadable(
       monitor_fd,
       base::Bind(&UdevWatcher::OnMonitorReadable, base::Unretained(this)));

device/udev_linux/udev_watcher.h
@@ -6,10 +6,12 @@
 #define DEVICE_UDEV_LINUX_UDEV_WATCHER_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/files/file_descriptor_watcher_posix.h"
 #include "base/macros.h"
 #include "base/sequence_checker.h"
+#include "base/strings/string_piece.h"
 #include "device/udev_linux/scoped_udev.h"
 
 namespace device {
@@ -25,7 +27,18 @@ class UdevWatcher {
     virtual void OnDeviceRemoved(ScopedUdevDevicePtr device);
   };
 
-  static std::unique_ptr<UdevWatcher> StartWatching(Observer* observer);
+  // subsystem and devtype parameter for
+  // udev_monitor_filter_add_match_subsystem_devtype().
+  struct Filter {
+    Filter(base::StringPiece subsystem_in, base::StringPiece devtype_in)
+        : subsystem(subsystem_in), devtype(devtype_in) {}
+    const std::string subsystem;
+    const std::string devtype;
+  };
+
+  static std::unique_ptr<UdevWatcher> StartWatching(
+      Observer* observer,
+      const std::vector<Filter>& filters = {});
 
   ~UdevWatcher();
 
@@ -37,13 +50,15 @@ class UdevWatcher {
   UdevWatcher(ScopedUdevPtr udev,
               ScopedUdevMonitorPtr udev_monitor,
               int monitor_fd,
-              Observer* observer);
+              Observer* observer,
+              const std::vector<Filter>& filters);
 
   void OnMonitorReadable();
 
   ScopedUdevPtr udev_;
   ScopedUdevMonitorPtr udev_monitor_;
   Observer* observer_;
+  const std::vector<Filter> udev_filters_;
   std::unique_ptr<base::FileDescriptorWatcher::Controller> file_watcher_;
   base::SequenceChecker sequence_checker_;
 

AUTHORS
@@ -89,6 +89,7 @@ Anuj Kumar Sharma <anujk.sharma@samsung.com>
 Arjun Karthik <arjunkar@amazon.com>
 Arman Ghotb <armanghotb@gmail.com>
 Armin Burgmeier <aburgmeier@bloomberg.net>
+Arnaud Mandy <arnaud.mandy@intel.com>
 Arnaud Renevier <a.renevier@samsung.com>
 Arpita Bahuguna <a.bah@samsung.com>
 Arthur Lussos <developer0420@gmail.com>

device/gamepad/gamepad_device_linux.cc
@@ -17,7 +17,7 @@
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "device/gamepad/gamepad_data_fetcher.h"
-#include "device/udev_linux/udev_linux.h"
+#include "device/udev_linux/udev.h"
 
 namespace device {
 

device/gamepad/gamepad_platform_data_fetcher.h
@@ -27,7 +27,7 @@
 #include "device/gamepad/gamepad_platform_data_fetcher_mac.h"
 #include "device/gamepad/nintendo_data_fetcher.h"
 #include "device/gamepad/xbox_data_fetcher_mac.h"
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) && defined(USE_UDEV)
 #include "device/gamepad/gamepad_platform_data_fetcher_linux.h"
 #include "device/gamepad/nintendo_data_fetcher.h"
 #endif

device/gamepad/gamepad_platform_data_fetcher_linux.cc
@@ -21,7 +21,6 @@
 #include "device/gamepad/gamepad_uma.h"
 #include "device/gamepad/nintendo_controller.h"
 #include "device/udev_linux/scoped_udev.h"
-#include "device/udev_linux/udev_linux.h"
 
 namespace device {
 
@@ -39,21 +38,26 @@ GamepadSource GamepadPlatformDataFetcherLinux::source() {
 }
 
 void GamepadPlatformDataFetcherLinux::OnAddedToProvider() {
-  std::vector<UdevLinux::UdevMonitorFilter> filters;
-  filters.push_back(
-      UdevLinux::UdevMonitorFilter(UdevGamepadLinux::kInputSubsystem, nullptr));
-  filters.push_back(UdevLinux::UdevMonitorFilter(
-      UdevGamepadLinux::kHidrawSubsystem, nullptr));
-  udev_.reset(new UdevLinux(
-      filters, base::Bind(&GamepadPlatformDataFetcherLinux::RefreshDevice,
-                          base::Unretained(this))));
+  std::vector<UdevWatcher::Filter> filters;
+  filters.emplace_back(UdevGamepadLinux::kInputSubsystem, nullptr);
+  filters.emplace_back(UdevGamepadLinux::kHidrawSubsystem, nullptr);
+  udev_watcher_ = UdevWatcher::StartWatching(this, filters);
 
   for (auto it = devices_.begin(); it != devices_.end(); ++it)
     it->get()->Shutdown();
   devices_.clear();
 
-  EnumerateSubsystemDevices(UdevGamepadLinux::kInputSubsystem);
-  EnumerateSubsystemDevices(UdevGamepadLinux::kHidrawSubsystem);
+  udev_watcher_->EnumerateExistingDevices();
+}
+
+void GamepadPlatformDataFetcherLinux::OnDeviceAdded(
+    ScopedUdevDevicePtr device) {
+  RefreshDevice(device.get());
+}
+
+void GamepadPlatformDataFetcherLinux::OnDeviceRemoved(
+    ScopedUdevDevicePtr device) {
+  RefreshDevice(device.get());
 }
 
 void GamepadPlatformDataFetcherLinux::GetGamepadData(bool) {
@@ -263,37 +267,6 @@ void GamepadPlatformDataFetcherLinux::RefreshHidrawDevice(
       Gamepad& pad = state->data;
       pad.vibration_actuator.not_null = device->SupportsVibration();
     }
-  }
-}
-
-void GamepadPlatformDataFetcherLinux::EnumerateSubsystemDevices(
-    const std::string& subsystem) {
-  if (!udev_->udev_handle())
-    return;
-  ScopedUdevEnumeratePtr enumerate(udev_enumerate_new(udev_->udev_handle()));
-  if (!enumerate)
-    return;
-  int ret =
-      udev_enumerate_add_match_subsystem(enumerate.get(), subsystem.c_str());
-  if (ret != 0)
-    return;
-  ret = udev_enumerate_scan_devices(enumerate.get());
-  if (ret != 0)
-    return;
-
-  udev_list_entry* devices = udev_enumerate_get_list_entry(enumerate.get());
-  for (udev_list_entry* dev_list_entry = devices; dev_list_entry != nullptr;
-       dev_list_entry = udev_list_entry_get_next(dev_list_entry)) {
-    // Get the filename of the /sys entry for the device and create a
-    // udev_device object (dev) representing it
-    const char* path = udev_list_entry_get_name(dev_list_entry);
-    if (!path)
-      continue;
-    ScopedUdevDevicePtr dev(
-        udev_device_new_from_syspath(udev_->udev_handle(), path));
-    if (!dev)
-      continue;
-    RefreshDevice(dev.get());
   }
 }
 

device/gamepad/gamepad_platform_data_fetcher_linux.h
@@ -16,19 +16,17 @@
 #include "device/gamepad/gamepad_device_linux.h"
 #include "device/gamepad/public/cpp/gamepads.h"
 #include "device/gamepad/udev_gamepad_linux.h"
+#include "device/udev_linux/udev_watcher.h"
 
 extern "C" {
 struct udev_device;
 }
 
 namespace device {
-class UdevLinux;
-}
-
-namespace device {
 
 class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
-    : public GamepadDataFetcher {
+    : public GamepadDataFetcher,
+      public UdevWatcher::Observer {
  public:
   using Factory = GamepadDataFetcherFactoryImpl<GamepadPlatformDataFetcherLinux,
                                                 GAMEPAD_SOURCE_LINUX_UDEV>;
@@ -59,7 +57,6 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   void RefreshJoydevDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
   void RefreshEvdevDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
   void RefreshHidrawDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
-  void EnumerateSubsystemDevices(const std::string& subsystem);
   void ReadDeviceData(size_t index);
 
   GamepadDeviceLinux* GetDeviceWithJoydevIndex(int joydev_index);
@@ -68,9 +65,13 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   void RemoveDevice(GamepadDeviceLinux* device);
   void RemoveDeviceAtIndex(int index);
 
+  // UdevWatcher::Observer overrides
+  void OnDeviceAdded(ScopedUdevDevicePtr device) override;
+  void OnDeviceRemoved(ScopedUdevDevicePtr device) override;
+
   std::unordered_set<std::unique_ptr<GamepadDeviceLinux>> devices_;
 
-  std::unique_ptr<device::UdevLinux> udev_;
+  std::unique_ptr<device::UdevWatcher> udev_watcher_;
 
   DISALLOW_COPY_AND_ASSIGN(GamepadPlatformDataFetcherLinux);
 };

device/udev_linux/BUILD.gn
@@ -14,8 +14,6 @@ if (use_udev) {
       "udev0_loader.h",
       "udev1_loader.cc",
       "udev1_loader.h",
-      "udev_linux.cc",
-      "udev_linux.h",
       "udev_loader.cc",
       "udev_loader.h",
       "udev_watcher.cc",

commit 43ea294683ce67b37dc1680b54493d2af2b4eff4

UdevWatcher: make sure NULL in filters is passed to udev API
When converting a char* to a base::StringPiece to a std::string
and later back to a char*, the std::string to char* conversion
will turn NULL into "", which breaks filters.

This patch is fixing the bug introduced in commit 99d9fdb47
("UdevWatcher: Merging UdevLinux into UdevWatcher").

Bug: 940514
Change-Id: Ibe21452f8960e485ac1dcf29ea0c843ec20d05e6
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548484
Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Reviewed-by: Reilly Grant <reillyg@chromium.org>
Cr-Commit-Position: refs/heads/master@{#647185}

device/udev_linux/udev_watcher.cc
@@ -10,6 +10,25 @@
 
 namespace device {
 
+UdevWatcher::Filter::Filter(base::StringPiece subsystem_in,
+                            base::StringPiece devtype_in) {
+  if (subsystem_in.data())
+    subsystem_ = subsystem_in.as_string();
+  if (devtype_in.data())
+    devtype_ = devtype_in.as_string();
+}
+
+UdevWatcher::Filter::Filter(const Filter&) = default;
+UdevWatcher::Filter::~Filter() = default;
+
+const char* UdevWatcher::Filter::devtype() const {
+  return devtype_ ? devtype_.value().c_str() : nullptr;
+}
+
+const char* UdevWatcher::Filter::subsystem() const {
+  return subsystem_ ? subsystem_.value().c_str() : nullptr;
+}
+
 UdevWatcher::Observer::~Observer() = default;
 
 void UdevWatcher::Observer::OnDeviceAdded(ScopedUdevDevicePtr device) {}
@@ -36,7 +55,7 @@ std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(
 
   for (const Filter& filter : filters) {
     const int ret = udev_monitor_filter_add_match_subsystem_devtype(
-        udev_monitor.get(), filter.subsystem.c_str(), filter.devtype.c_str());
+        udev_monitor.get(), filter.subsystem(), filter.devtype());
     CHECK_EQ(0, ret);
   }
 
@@ -70,8 +89,8 @@ void UdevWatcher::EnumerateExistingDevices() {
   }
 
   for (const Filter& filter : udev_filters_) {
-    const int ret = udev_enumerate_add_match_subsystem(
-        enumerate.get(), filter.subsystem.c_str());
+    const int ret =
+        udev_enumerate_add_match_subsystem(enumerate.get(), filter.subsystem());
     CHECK_EQ(0, ret);
   }
 

device/udev_linux/udev_watcher.h
@@ -10,6 +10,7 @@
 
 #include "base/files/file_descriptor_watcher_posix.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece.h"
 #include "device/udev_linux/scoped_udev.h"
@@ -31,11 +32,18 @@ class UdevWatcher {
 
   // subsystem and devtype parameter for
   // udev_monitor_filter_add_match_subsystem_devtype().
-  struct Filter {
-    Filter(base::StringPiece subsystem_in, base::StringPiece devtype_in)
-        : subsystem(subsystem_in), devtype(devtype_in) {}
-    const std::string subsystem;
-    const std::string devtype;
+  class Filter {
+   public:
+    Filter(base::StringPiece subsystem_in, base::StringPiece devtype_in);
+    Filter(const Filter&);
+    ~Filter();
+
+    const char* devtype() const;
+    const char* subsystem() const;
+
+   private:
+    base::Optional<std::string> subsystem_;
+    base::Optional<std::string> devtype_;
   };
 
   static std::unique_ptr<UdevWatcher> StartWatching(