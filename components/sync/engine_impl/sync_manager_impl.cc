dbuskariol-org/chromium

commit 44074e1450a2ccfc083e7c818b895d57c71bd5f2

Migrate kernel info off Directory
After this patch, fields cache_guid, store_birthday and bag_of_chips
are no longer owned by syncer::Directory.

Instead, we consider the values in prefs as authoritative ones.
According to UMA metric Sync.DirectoryVsPrefsConsistency, the two values
are identical in most cases (desktop is the worst and still values are
identical in ~99.7% of the cases).

For the remaining users (worst-case ~0.3% of desktop users), we expect
that they could throw away all local sync metadata and refetch sync data
from the server. Since we expect this will be less significant in
practice, we will closely monitor UMA metrics
Sync.ModelTypeEntityChange3.* during rollouts to the various channels.
Similarly, we will keep an eye on Sync.PostedClientToServerMessageError
to rule out regressions related to the store birthday.

Implementation-wise, ProfileSyncService is now responsible for:
a) Handing over the initial values to the sync engine.
b) Persisting updates to prefs (mostly via SyncCycleSnapshot).

Within the sync engine, cache_guid is constant (and for now owned by
DirectoryBackingStore, subject to change) and the remaining two fields
(birthday and bag_of_chips) are owned by SyncCycleContext.

In case this patch is reverted, we keep populating (updating) the fields
persisted by syncer::Directory, although they are never read (with the
exception of the UMA metric referred to above).

Bug: 923285
Change-Id: I288752402539bdecce0d12c40be6482d788c9a49
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581682
Commit-Queue: Mikel Astiz <mastiz@chromium.org>
Reviewed-by: Marc Treib <treib@chromium.org>
Cr-Commit-Position: refs/heads/master@{#654363}

components/sync/engine_impl/sync_manager_impl.cc
@@ -118,8 +118,8 @@ void RecordConsistencyBetweenDirectoryAndPrefs(
   DCHECK(open_result == syncable::OPENED_EXISTING ||
          open_result == syncable::OPENED_NEW);
   if (open_result == syncable::OPENED_EXISTING) {
-    directory_cache_guid = directory->cache_guid();
-    directory_birthday = directory->store_birthday();
+    directory_cache_guid = directory->legacy_cache_guid_for_uma();
+    directory_birthday = directory->legacy_store_birthday_for_uma();
   }
 
   const StringConsistency cache_guid_consistency =
@@ -260,6 +260,7 @@ void SyncManagerImpl::ConfigureSyncer(ConfigureReason reason,
 void SyncManagerImpl::Init(InitArgs* args) {
   DCHECK(!initialized_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(!args->cache_guid.empty());
   DCHECK(args->post_factory);
   DCHECK(!args->poll_interval.is_zero());
   if (!args->enable_local_sync_backend) {
@@ -298,7 +299,7 @@ void SyncManagerImpl::Init(InitArgs* args) {
   std::unique_ptr<syncable::DirectoryBackingStore> backing_store =
       args->engine_components_factory->BuildDirectoryBackingStore(
           EngineComponentsFactory::STORAGE_ON_DISK,
-          args->authenticated_account_id, absolute_db_path);
+          args->authenticated_account_id, args->cache_guid, absolute_db_path);
 
   DCHECK(backing_store);
   share_.directory = std::make_unique<syncable::Directory>(
@@ -356,7 +357,8 @@ void SyncManagerImpl::Init(InitArgs* args) {
   cycle_context_ = args->engine_components_factory->BuildContext(
       connection_manager_.get(), directory(), args->extensions_activity,
       listeners, &debug_info_event_listener_, model_type_registry_.get(),
-      args->invalidator_client_id, args->poll_interval);
+      args->invalidator_client_id, args->birthday, args->bag_of_chips,
+      args->poll_interval);
   scheduler_ = args->engine_components_factory->BuildScheduler(
       name_, cycle_context_.get(), args->cancelation_signal,
       args->enable_local_sync_backend);

components/sync/driver/profile_sync_service.cc
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <utility>
 
+#include "base/base64.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
@@ -14,6 +15,7 @@
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/rand_util.h"
 #include "components/invalidation/public/invalidation_service.h"
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/signin_metrics.h"
@@ -38,8 +40,7 @@
 #include "components/sync/engine/sync_encryption_handler.h"
 #include "components/sync/engine/sync_engine_switches.h"
 #include "components/sync/model/sync_error.h"
-#include "components/sync/syncable/base_transaction.h"
-#include "components/sync/syncable/directory.h"
+#include "components/sync/syncable/user_share.h"
 #include "components/version_info/version_info_values.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "services/identity/public/cpp/primary_account_mutator.h"
@@ -120,6 +121,14 @@ DataTypeController::TypeMap BuildDataTypeControllerMap(
     type_map[type] = std::move(controller);
   }
   return type_map;
+}
+
+std::string GenerateCacheGUID() {
+  // Generate a GUID with 128 bits of randomness.
+  const int kGuidBytes = 128 / 8;
+  std::string guid;
+  base::Base64Encode(base::RandBytesAsString(kGuidBytes), &guid);
+  return guid;
 }
 
 }  // namespace
@@ -486,6 +495,10 @@ void ProfileSyncService::StartUpSlowEngineComponents() {
   params.restored_keystore_key_for_bootstrapping =
       sync_prefs_.GetKeystoreEncryptionBootstrapToken();
   params.cache_guid = sync_prefs_.GetCacheGuid();
+  if (params.cache_guid.empty()) {
+    params.cache_guid = GenerateCacheGUID();
+    sync_prefs_.SetCacheGuid(params.cache_guid);
+  }
   params.birthday = sync_prefs_.GetBirthday();
   params.bag_of_chips = sync_prefs_.GetBagOfChips();
   params.engine_components_factory =
@@ -841,15 +854,6 @@ void ProfileSyncService::OnEngineInitialized(
 
   sync_js_controller_.AttachJsBackend(js_backend);
 
-  // Copy some data to preferences to be able to one day migrate away from the
-  // directory.
-  UserShare* user_share = GetUserShare();
-  if (user_share && user_share->directory.get()) {
-    sync_prefs_.SetCacheGuid(user_share->directory->cache_guid());
-    sync_prefs_.SetBirthday(user_share->directory->store_birthday());
-    sync_prefs_.SetBagOfChips(user_share->directory->bag_of_chips());
-  }
-
   if (protocol_event_observers_.might_have_observers()) {
     engine_->RequestBufferedProtocolEventsAndEnableForwarding();
   }
@@ -1191,6 +1195,11 @@ SyncClient* ProfileSyncService::GetSyncClientForTest() {
   return sync_client_.get();
 }
 
+// static
+std::string ProfileSyncService::GenerateCacheGUIDForTest() {
+  return GenerateCacheGUID();
+}
+
 void ProfileSyncService::AddObserver(SyncServiceObserver* observer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   observers_.AddObserver(observer);
@@ -1252,6 +1261,8 @@ void ProfileSyncService::ConfigureDataTypeManager(ConfigureReason reason) {
   configure_context.reason = reason;
   configure_context.configuration_start_time = base::Time::Now();
 
+  DCHECK(!configure_context.cache_guid.empty());
+
   if (!migrator_) {
     // We create the migrator at the same time.
     migrator_ = std::make_unique<BackendMigrator>(
@@ -1269,6 +1280,7 @@ void ProfileSyncService::ConfigureDataTypeManager(ConfigureReason reason) {
 
   DCHECK(!configure_context.authenticated_account_id.empty() ||
          IsLocalSyncEnabled());
+  DCHECK(!configure_context.cache_guid.empty());
   DCHECK_NE(configure_context.reason, CONFIGURE_REASON_UNKNOWN);
 
   // Note: When local Sync is enabled, then we want full-sync mode (not just
@@ -1816,11 +1828,8 @@ void ProfileSyncService::RemoveClientFromServer() const {
     return;
   }
   const std::string cache_guid = sync_prefs_.GetCacheGuid();
-  std::string birthday;
-  UserShare* user_share = GetUserShare();
-  if (user_share && user_share->directory.get()) {
-    birthday = user_share->directory->store_birthday();
-  }
+  const std::string birthday = sync_prefs_.GetBirthday();
+  DCHECK(!cache_guid.empty());
   const std::string& access_token = auth_manager_->access_token();
   if (!access_token.empty() && !birthday.empty()) {
     sync_stopped_reporter_->ReportSyncStopped(access_token, cache_guid,

components/sync/driver/profile_sync_service.h
@@ -254,6 +254,8 @@ class ProfileSyncService : public SyncService,
 
   SyncClient* GetSyncClientForTest();
 
+  static std::string GenerateCacheGUIDForTest();
+
  private:
   friend class TestProfileSyncService;
 

components/sync/driver/profile_sync_service_unittest.cc
@@ -1200,5 +1200,13 @@ TEST_F(ProfileSyncServiceTest, ConfigureDataTypeManagerReason) {
   ShutdownAndDeleteService();
 }
 
+TEST_F(ProfileSyncServiceTest, GenerateCacheGUID) {
+  const std::string guid1 = ProfileSyncService::GenerateCacheGUIDForTest();
+  const std::string guid2 = ProfileSyncService::GenerateCacheGUIDForTest();
+  EXPECT_EQ(24U, guid1.size());
+  EXPECT_EQ(24U, guid2.size());
+  EXPECT_NE(guid1, guid2);
+}
+
 }  // namespace
 }  // namespace syncer

components/sync/engine/engine_components_factory.h
@@ -86,11 +86,14 @@ class EngineComponentsFactory {
       DebugInfoGetter* debug_info_getter,
       ModelTypeRegistry* model_type_registry,
       const std::string& invalidator_client_id,
+      const std::string& store_birthday,
+      const std::string& bag_of_chips,
       base::TimeDelta poll_interval) = 0;
 
   virtual std::unique_ptr<syncable::DirectoryBackingStore>
   BuildDirectoryBackingStore(StorageOption storage,
                              const std::string& dir_name,
+                             const std::string& cache_guid,
                              const base::FilePath& backing_filepath) = 0;
 
   // Returns the Switches struct that this object is using as configuration, if

components/sync/engine/engine_components_factory_impl.cc
@@ -51,22 +51,26 @@ std::unique_ptr<SyncCycleContext> EngineComponentsFactoryImpl::BuildContext(
     DebugInfoGetter* debug_info_getter,
     ModelTypeRegistry* model_type_registry,
     const std::string& invalidation_client_id,
+    const std::string& store_birthday,
+    const std::string& bag_of_chips,
     base::TimeDelta poll_interval) {
   return std::make_unique<SyncCycleContext>(
       connection_manager, directory, extensions_activity, listeners,
       debug_info_getter, model_type_registry,
       switches_.encryption_method == ENCRYPTION_KEYSTORE,
-      invalidation_client_id, poll_interval);
+      invalidation_client_id, store_birthday, bag_of_chips, poll_interval);
 }
 
 std::unique_ptr<syncable::DirectoryBackingStore>
 EngineComponentsFactoryImpl::BuildDirectoryBackingStore(
     StorageOption storage,
     const std::string& dir_name,
+    const std::string& cache_guid,
     const base::FilePath& backing_filepath) {
   if (storage == STORAGE_ON_DISK) {
     return std::unique_ptr<syncable::DirectoryBackingStore>(
-        new syncable::OnDiskDirectoryBackingStore(dir_name, backing_filepath));
+        new syncable::OnDiskDirectoryBackingStore(dir_name, cache_guid,
+                                                  backing_filepath));
   } else {
     NOTREACHED();
     return std::unique_ptr<syncable::DirectoryBackingStore>();

components/sync/engine/engine_components_factory_impl.h
@@ -35,11 +35,14 @@ class EngineComponentsFactoryImpl : public EngineComponentsFactory {
       DebugInfoGetter* debug_info_getter,
       ModelTypeRegistry* model_type_registry,
       const std::string& invalidator_client_id,
+      const std::string& store_birthday,
+      const std::string& bag_of_chips,
       base::TimeDelta poll_interval) override;
 
   std::unique_ptr<syncable::DirectoryBackingStore> BuildDirectoryBackingStore(
       StorageOption storage,
       const std::string& dir_name,
+      const std::string& cache_guid,
       const base::FilePath& backing_filepath) override;
 
   Switches GetSwitches() const override;

components/sync/engine/sync_engine.h
@@ -77,9 +77,7 @@ class SyncEngine : public ModelTypeConfigurer {
     std::unique_ptr<SyncEncryptionHandler::NigoriState> saved_nigori_state;
     std::map<ModelType, int64_t> invalidation_versions;
 
-    // Non-authoritative values from prefs, to be compared with the Directory's
-    // counterparts.
-    // TODO(crbug.com/923285): Consider making these the authoritative data.
+    // Initial authoritative values (usually read from prefs).
     std::string cache_guid;
     std::string birthday;
     std::string bag_of_chips;

commit 20a46541e1d82f19d8474fb541d4a094130ce00b

Fix metric Sync.DirectoryVsPrefsConsistency overcounting diffs
Recent patch 44074e1450a2ccfc083e7c818b895d57c71bd5f2 caused the UMA
metric to overcount differences between preferences (now authoritative)
and Directory (legacy): when the directory is created from scratch, we
need no workaround (as prior to this patch) because prefs are populated
in all cases (in ProfileSyncService).

TBR=treib@chromium.org

Bug: 923285
Change-Id: Ia90fb02ef9849770b83c433c630c7d922e7bd19e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588570
Reviewed-by: Mikel Astiz <mastiz@chromium.org>
Commit-Queue: Mikel Astiz <mastiz@chromium.org>
Cr-Commit-Position: refs/heads/master@{#655355}

components/sync/engine_impl/sync_manager_impl.cc
@@ -104,23 +104,14 @@ constexpr int GetStringConsistencyUmaBucket(
 // information identical to the Directory's value, for the fields that are
 // stored in both. We mostly care about cache GUID and store birthday.
 void RecordConsistencyBetweenDirectoryAndPrefs(
-    syncable::DirOpenResult open_result,
     const syncable::Directory* directory,
     const SyncManager::InitArgs* args) {
   DCHECK(directory);
 
-  std::string directory_cache_guid;
-  std::string directory_birthday;
-
-  // We mimic the directory being empty if it was just opened (OPENED_NEW),
-  // because that means a random cache GUID was just generated and it's not
-  // possible to match empty prefs.
-  DCHECK(open_result == syncable::OPENED_EXISTING ||
-         open_result == syncable::OPENED_NEW);
-  if (open_result == syncable::OPENED_EXISTING) {
-    directory_cache_guid = directory->legacy_cache_guid_for_uma();
-    directory_birthday = directory->legacy_store_birthday_for_uma();
-  }
+  const std::string directory_cache_guid =
+      directory->legacy_cache_guid_for_uma();
+  const std::string directory_birthday =
+      directory->legacy_store_birthday_for_uma();
 
   const StringConsistency cache_guid_consistency =
       CompareStringsForConsistency(args->cache_guid, directory_cache_guid);
@@ -468,7 +459,7 @@ bool SyncManagerImpl::OpenDirectory(const InitArgs* args) {
     return false;
   }
 
-  RecordConsistencyBetweenDirectoryAndPrefs(open_result, directory(), args);
+  RecordConsistencyBetweenDirectoryAndPrefs(directory(), args);
 
   // Unapplied datatypes (those that do not have initial sync ended set) get
   // re-downloaded during any configuration. But, it's possible for a datatype