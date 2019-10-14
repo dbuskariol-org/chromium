dbuskariol-org/chromium

commit d5f8becbd030937f01372a2a6d4af0ec75c7fe9c

Mash cleanup: message center part 5
Move GetArcAppIdByPackageName from AshMessageCenterClient to its own
C++ interface.

Also delete ash::mojom::AshMessageCenterClient since it's now empty.

Bug: 958191
Change-Id: I605ae6ae081e54010348dd3e9c4d01ece8aaaed4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658846
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Tom Sepez <tsepez@chromium.org>
Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
Cr-Commit-Position: refs/heads/master@{#670166}

chrome/browser/notifications/chrome_ash_message_center_client.cc
@@ -6,23 +6,18 @@
 
 #include "ash/public/cpp/notifier_metadata.h"
 #include "ash/public/cpp/notifier_settings_observer.h"
-#include "ash/public/interfaces/constants.mojom.h"
 #include "base/i18n/string_compare.h"
 #include "base/stl_util.h"
 #include "chrome/browser/chrome_notification_types.h"
-#include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/notifications/arc_application_notifier_controller.h"
 #include "chrome/browser/notifications/extension_notifier_controller.h"
 #include "chrome/browser/notifications/web_page_notifier_controller.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/notification_service.h"
-#include "content/public/common/service_manager_connection.h"
-#include "services/service_manager/public/cpp/connector.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
 
@@ -108,21 +103,9 @@ class ForwardingNotificationDelegate
 
 ChromeAshMessageCenterClient::ChromeAshMessageCenterClient(
     NotificationPlatformBridgeDelegate* delegate)
-    : delegate_(delegate), binding_(this) {
+    : delegate_(delegate) {
   DCHECK(!g_chrome_ash_message_center_client);
   g_chrome_ash_message_center_client = this;
-
-  // May be null in unit tests.
-  auto* connection = content::ServiceManagerConnection::GetForProcess();
-  if (connection) {
-    connection->GetConnector()->BindInterface(ash::mojom::kServiceName,
-                                              &controller_);
-
-    // Register this object as the client interface implementation.
-    ash::mojom::AshMessageCenterClientAssociatedPtrInfo ptr_info;
-    binding_.Bind(mojo::MakeRequest(&ptr_info));
-    controller_->SetClient(std::move(ptr_info));
-  }
 
   sources_.insert(
       std::make_pair(message_center::NotifierType::APPLICATION,
@@ -203,14 +186,6 @@ void ChromeAshMessageCenterClient::RemoveNotifierSettingsObserver(
   notifier_observers_.RemoveObserver(observer);
 }
 
-void ChromeAshMessageCenterClient::GetArcAppIdByPackageName(
-    const std::string& package_name,
-    GetArcAppIdByPackageNameCallback callback) {
-  std::move(callback).Run(
-      ArcAppListPrefs::Get(arc::ArcSessionManager::Get()->profile())
-          ->GetAppIdByPackageName(package_name));
-}
-
 void ChromeAshMessageCenterClient::OnIconImageUpdated(
     const NotifierId& notifier_id,
     const gfx::ImageSkia& image) {
@@ -225,25 +200,14 @@ void ChromeAshMessageCenterClient::OnNotifierEnabledChanged(
     MessageCenter::Get()->RemoveNotificationsForNotifierId(notifier_id);
 }
 
-// static
-void ChromeAshMessageCenterClient::FlushForTesting() {
-  g_chrome_ash_message_center_client->binding_.FlushForTesting();
-}
-
 void ChromeAshMessageCenterClient::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  switch (type) {
-    case chrome::NOTIFICATION_PROFILE_ADDED: {
-      Profile* profile = GetProfileForNotifiers();
-      if (profile) {
-        GetNotifiers();
-        registrar_.RemoveAll();
-      }
-      break;
-    }
-    default:
-      NOTREACHED();
+  DCHECK_EQ(type, chrome::NOTIFICATION_PROFILE_ADDED);
+  Profile* profile = GetProfileForNotifiers();
+  if (profile) {
+    GetNotifiers();
+    registrar_.RemoveAll();
   }
 }

chrome/browser/notifications/chrome_ash_message_center_client.h
@@ -6,14 +6,12 @@
 #define CHROME_BROWSER_NOTIFICATIONS_CHROME_ASH_MESSAGE_CENTER_CLIENT_H_
 
 #include "ash/public/cpp/notifier_settings_controller.h"
-#include "ash/public/interfaces/ash_message_center_controller.mojom.h"
+#include "base/observer_list.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
 #include "chrome/browser/notifications/notification_platform_bridge_chromeos.h"
 #include "chrome/browser/notifications/notifier_controller.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "mojo/public/cpp/bindings/associated_binding.h"
-#include "ui/message_center/public/cpp/notifier_id.h"
 
 // This class serves as Chrome's AshMessageCenterClient, as well as the
 // NotificationPlatformBridge for ChromeOS. It dispatches notifications to Ash
@@ -21,7 +19,6 @@
 // NotifierControllers to provide notifier settings information to Ash (visible
 // in NotifierSettingsView).
 class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
-                                     public ash::mojom::AshMessageCenterClient,
                                      public NotifierController::Observer,
                                      public content::NotificationObserver {
  public:
@@ -42,19 +39,11 @@ class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
   void RemoveNotifierSettingsObserver(
       ash::NotifierSettingsObserver* observer) override;
 
-  // ash::mojom::AshMessageCenterClient:
-  void GetArcAppIdByPackageName(
-      const std::string& package_name,
-      GetArcAppIdByPackageNameCallback callback) override;
-
   // NotifierController::Observer:
   void OnIconImageUpdated(const message_center::NotifierId& notifier_id,
                           const gfx::ImageSkia& icon) override;
   void OnNotifierEnabledChanged(const message_center::NotifierId& notifier_id,
                                 bool enabled) override;
-
-  // Flushs |binding_|.
-  static void FlushForTesting();
 
  private:
   // content::NotificationObserver override.
@@ -70,8 +59,6 @@ class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
 
   base::ObserverList<ash::NotifierSettingsObserver> notifier_observers_;
 
-  ash::mojom::AshMessageCenterControllerPtr controller_;
-  mojo::AssociatedBinding<ash::mojom::AshMessageCenterClient> binding_;
   content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeAshMessageCenterClient);

ash/public/cpp/BUILD.gn
@@ -35,6 +35,8 @@ component("cpp") {
     "app_list/tokenized_string_match.h",
     "app_menu_constants.h",
     "app_types.h",
+    "arc_app_id_provider.cc",
+    "arc_app_id_provider.h",
     "ash_constants.h",
     "ash_features.cc",
     "ash_features.h",

ash/public/cpp/arc_app_id_provider.cc
@@ -1,0 +1,30 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/arc_app_id_provider.h"
+
+#include "base/logging.h"
+
+namespace ash {
+
+namespace {
+ArcAppIdProvider* g_instance = nullptr;
+}
+
+// static
+ArcAppIdProvider* ArcAppIdProvider::Get() {
+  return g_instance;
+}
+
+ArcAppIdProvider::ArcAppIdProvider() {
+  DCHECK_EQ(nullptr, g_instance);
+  g_instance = this;
+}
+
+ArcAppIdProvider::~ArcAppIdProvider() {
+  DCHECK_EQ(this, g_instance);
+  g_instance = nullptr;
+}
+
+}  // namespace ash

ash/public/cpp/arc_app_id_provider.h
@@ -1,0 +1,28 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PUBLIC_CPP_ARC_APP_ID_PROVIDER_H_
+#define ASH_PUBLIC_CPP_ARC_APP_ID_PROVIDER_H_
+
+#include <string>
+
+#include "ash/public/cpp/ash_public_export.h"
+
+namespace ash {
+
+class ASH_PUBLIC_EXPORT ArcAppIdProvider {
+ public:
+  static ArcAppIdProvider* Get();
+
+  virtual std::string GetAppIdByPackageName(
+      const std::string& package_name) = 0;
+
+ protected:
+  ArcAppIdProvider();
+  virtual ~ArcAppIdProvider();
+};
+
+}  // namespace ash
+
+#endif  // ASH_PUBLIC_CPP_ARC_APP_ID_PROVIDER_H_

ash/public/interfaces/ash_message_center_controller.mojom
@@ -6,23 +6,10 @@ module ash.mojom;
 
 import "components/arc/common/notifications.mojom";
 
-// The message center controller funnels notification requests from the client
-// to the MessageCenter.
 interface AshMessageCenterController {
-  SetClient(associated AshMessageCenterClient client);
-
   // Sets the ARC notification instance.
   SetArcNotificationsInstance(arc.mojom.NotificationsInstance instance);
 
   // Changes the quiet mode state in the message center.
   SetQuietMode(bool enabled);
-};
-
-// The message center client interface mimics
-// message_center::NotificationDelegate. The ID strings match the id passed in
-// |notification| in ShowClientNotification and the format of the ID is up to
-// the client.
-interface AshMessageCenterClient {
-  // Gets ARC app id from a given package name.
-  GetArcAppIdByPackageName(string package_name) => (string app_id);
 };

ash/system/message_center/arc/arc_notification_manager.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "ash/public/cpp/arc_app_id_provider.h"
 #include "ash/public/cpp/ash_features.h"
 #include "ash/system/message_center/arc/arc_notification_constants.h"
 #include "ash/system/message_center/arc/arc_notification_delegate.h"
@@ -191,10 +192,11 @@ void ArcNotificationManager::OnNotificationPosted(ArcNotificationDataPtr data) {
     it = result.first;
   }
 
-  delegate_->GetAppIdByPackageName(
-      data->package_name.value_or(std::string()),
-      base::BindOnce(&ArcNotificationManager::OnGotAppId,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(data)));
+  std::string app_id =
+      data->package_name
+          ? ArcAppIdProvider::Get()->GetAppIdByPackageName(*data->package_name)
+          : std::string();
+  it->second->OnUpdatedFromAndroid(std::move(data), app_id);
 }
 
 void ArcNotificationManager::OnNotificationUpdated(
@@ -235,10 +237,11 @@ void ArcNotificationManager::OnNotificationUpdated(
     previously_focused_notification_key_.clear();
   }
 
-  delegate_->GetAppIdByPackageName(
-      data->package_name.value_or(std::string()),
-      base::BindOnce(&ArcNotificationManager::OnGotAppId,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(data)));
+  std::string app_id =
+      data->package_name
+          ? ArcAppIdProvider::Get()->GetAppIdByPackageName(*data->package_name)
+          : std::string();
+  it->second->OnUpdatedFromAndroid(std::move(data), app_id);
 }
 
 void ArcNotificationManager::OpenMessageCenter() {
@@ -487,16 +490,6 @@ bool ArcNotificationManager::ShouldIgnoreNotification(
   }
 
   return false;
-}
-
-void ArcNotificationManager::OnGotAppId(ArcNotificationDataPtr data,
-                                        const std::string& app_id) {
-  const std::string& key = data->key;
-  auto it = items_.find(key);
-  if (it == items_.end())
-    return;
-
-  it->second->OnUpdatedFromAndroid(std::move(data), app_id);
 }
 
 void ArcNotificationManager::OnDoNotDisturbStatusUpdated(

ash/system/message_center/arc/arc_notification_manager.h
@@ -81,10 +81,6 @@ class ArcNotificationManager
   void PerformUserAction(uint32_t id, bool open_message_center);
   void CancelUserAction(uint32_t id);
 
-  // Invoked when |get_app_id_callback_| gets back the app id.
-  void OnGotAppId(arc::mojom::ArcNotificationDataPtr data,
-                  const std::string& app_id);
-
   std::unique_ptr<ArcNotificationManagerDelegate> delegate_;
   const AccountId main_profile_id_;
   message_center::MessageCenter* const message_center_;

commit 16d77eb319d7a4691e7a28430a3f5957f5830c21

Remove NotificationService usage from ChromeAshMessageCenterClient
Registration for NOTIFICATION_PROFILE_ADDED is replaced with
User::AddProfileCreatedObserver.

Note that before this patch, the NotificationRegistrar was never
actually used because it was broken by d5f8becbd030937f01372a2a6d4af.
Thus, this patch also fixes the (relatively minor) bug that while a
profile was loading, GetNotifiers would no-op, rather than perform
a delayed op.

Bug: 268984
Change-Id: I3c68b5bb9610e486088e3e0c5cb23255453dab3e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857538
Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#705713}

chrome/browser/notifications/chrome_ash_message_center_client.cc
@@ -8,7 +8,6 @@
 #include "ash/public/cpp/notifier_settings_observer.h"
 #include "base/i18n/string_compare.h"
 #include "base/stl_util.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/notifications/arc_application_notifier_controller.h"
 #include "chrome/browser/notifications/extension_notifier_controller.h"
@@ -16,8 +15,8 @@
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
-#include "content/public/browser/notification_service.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
 
@@ -32,8 +31,8 @@ ChromeAshMessageCenterClient* g_chrome_ash_message_center_client = nullptr;
 // All notifier actions are performed on the notifiers for the currently active
 // profile, so this just returns the active profile.
 Profile* GetProfileForNotifiers() {
-  const auto* user = user_manager::UserManager::Get()->GetActiveUser();
-  return chromeos::ProfileHelper::Get()->GetProfileByUser(user);
+  return chromeos::ProfileHelper::Get()->GetProfileByUser(
+      user_manager::UserManager::Get()->GetActiveUser());
 }
 
 class NotifierComparator {
@@ -145,8 +144,16 @@ void ChromeAshMessageCenterClient::Close(const std::string& notification_id) {
 }
 
 void ChromeAshMessageCenterClient::GetNotifiers() {
+  if (!notifier_observers_.might_have_observers())
+    return;
+
   Profile* profile = GetProfileForNotifiers();
   if (!profile) {
+    user_manager::UserManager::Get()
+        ->GetActiveUser()
+        ->AddProfileCreatedObserver(
+            base::BindOnce(&ChromeAshMessageCenterClient::GetNotifiers,
+                           weak_ptr_.GetWeakPtr()));
     LOG(ERROR) << "GetNotifiers called before profile fully loaded, see "
                   "https://crbug.com/968825";
     return;
@@ -171,9 +178,8 @@ void ChromeAshMessageCenterClient::GetNotifiers() {
 void ChromeAshMessageCenterClient::SetNotifierEnabled(
     const NotifierId& notifier_id,
     bool enabled) {
-  Profile* profile = GetProfileForNotifiers();
-  CHECK(profile);
-  sources_[notifier_id.type]->SetNotifierEnabled(profile, notifier_id, enabled);
+  sources_[notifier_id.type]->SetNotifierEnabled(GetProfileForNotifiers(),
+                                                 notifier_id, enabled);
 }
 
 void ChromeAshMessageCenterClient::AddNotifierSettingsObserver(
@@ -198,16 +204,4 @@ void ChromeAshMessageCenterClient::OnNotifierEnabledChanged(
     bool enabled) {
   if (!enabled)
     MessageCenter::Get()->RemoveNotificationsForNotifierId(notifier_id);
-}
-
-void ChromeAshMessageCenterClient::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  DCHECK_EQ(type, chrome::NOTIFICATION_PROFILE_ADDED);
-  Profile* profile = GetProfileForNotifiers();
-  if (profile) {
-    GetNotifiers();
-    registrar_.RemoveAll();
-  }
 }

chrome/browser/notifications/chrome_ash_message_center_client.h
@@ -6,12 +6,11 @@
 #define CHROME_BROWSER_NOTIFICATIONS_CHROME_ASH_MESSAGE_CENTER_CLIENT_H_
 
 #include "ash/public/cpp/notifier_settings_controller.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
 #include "chrome/browser/notifications/notification_platform_bridge_chromeos.h"
 #include "chrome/browser/notifications/notifier_controller.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 
 // This class serves as Chrome's AshMessageCenterClient, as well as the
 // NotificationPlatformBridge for ChromeOS. It dispatches notifications to Ash
@@ -19,8 +18,7 @@
 // NotifierControllers to provide notifier settings information to Ash (visible
 // in NotifierSettingsView).
 class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
-                                     public NotifierController::Observer,
-                                     public content::NotificationObserver {
+                                     public NotifierController::Observer {
  public:
   explicit ChromeAshMessageCenterClient(
       NotificationPlatformBridgeDelegate* delegate);
@@ -46,11 +44,6 @@ class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
                                 bool enabled) override;
 
  private:
-  // content::NotificationObserver override.
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
-
   NotificationPlatformBridgeDelegate* delegate_;
 
   // Notifier source for each notifier type.
@@ -59,7 +52,7 @@ class ChromeAshMessageCenterClient : public ash::NotifierSettingsController,
 
   base::ObserverList<ash::NotifierSettingsObserver> notifier_observers_;
 
-  content::NotificationRegistrar registrar_;
+  base::WeakPtrFactory<ChromeAshMessageCenterClient> weak_ptr_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ChromeAshMessageCenterClient);
 };