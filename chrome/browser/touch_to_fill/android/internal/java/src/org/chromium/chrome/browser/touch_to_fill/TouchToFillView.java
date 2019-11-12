dbuskariol-org/chromium

commit 640593c06d582451f95a322e85673f7347fdb180

[Android][TouchToFill] Prevent double-dismissing the bottom sheet
If the touch to fill sheet is dismissed, it triggers a second call to
BottomSheetController#hideContent.

That call triggers an observer in the BottomContainer again which then
moves itself out of the visible view port.
(The latter can only happen since a division by zero bypasses some guard
conditions. It might make sense to abort attempts at repeatedly
dismissing the bottomsheet but this exceeds the scope of this CL.)

(cherry picked from commit 56d45f06b41ea68f253a69967b34749bf7e48dcb)

Bug: 1021550
Change-Id: Ida2b6bc9ffd18513419bed40b29ca84528310907
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899882
Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#712703}
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912086
Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
Cr-Commit-Position: refs/branch-heads/3945@{#586}
Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

chrome/browser/touch_to_fill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillView.java
@@ -82,7 +82,7 @@ class TouchToFillView implements BottomSheet.BottomSheetContent {
         if (isVisible) {
             mBottomSheetController.getBottomSheet().addObserver(mBottomSheetObserver);
             mBottomSheetController.requestShowContent(this, false);
-        } else {
+        } else if (mBottomSheetController.isSheetOpen()) {
             mBottomSheetController.hideContent(this, false);
         }
     }

commit b531c3698e0be87361fb892e8011d6b72eec147c

Reland "[Android][TouchToFill] Prevent double-dismissing the bottom sheet"
This is a reland of 640593c06d582451f95a322e85673f7347fdb180
which broke the build due to using the new API.

Original change's description:
> [Android][TouchToFill] Prevent double-dismissing the bottom sheet
>
> If the touch to fill sheet is dismissed, it triggers a second call to
> BottomSheetController#hideContent.
>
> That call triggers an observer in the BottomContainer again which then
> moves itself out of the visible view port.
> (The latter can only happen since a division by zero bypasses some guard
> conditions. It might make sense to abort attempts at repeatedly
> dismissing the bottomsheet but this exceeds the scope of this CL.)
>
> (cherry picked from commit 56d45f06b41ea68f253a69967b34749bf7e48dcb)
>
> Bug: 1021550
> Change-Id: Ida2b6bc9ffd18513419bed40b29ca84528310907
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899882
> Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
> Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
> Cr-Original-Commit-Position: refs/heads/master@{#712703}
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912086
> Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
> Cr-Commit-Position: refs/branch-heads/3945@{#586}
> Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

Bug: 1021550,1023952
Change-Id: I4f3fe78afa7d5da1d023efe0498b80b927bca2ba
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913459
Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
Cr-Commit-Position: refs/branch-heads/3945@{#602}
Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

chrome/browser/touch_to_fill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillView.java
@@ -82,7 +82,7 @@ class TouchToFillView implements BottomSheet.BottomSheetContent {
         if (isVisible) {
             mBottomSheetController.getBottomSheet().addObserver(mBottomSheetObserver);
             mBottomSheetController.requestShowContent(this, false);
-        } else {
+        } else if (mBottomSheetController.getBottomSheet().isSheetOpen()) {
             mBottomSheetController.hideContent(this, false);
         }
     }