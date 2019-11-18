f1eb90c76d0709cd5bc0cbd8de8bf313c19737c1
Yulun Wu <yulunwu@chromium.org>
2019-11-18 22:11:47 +0000

    CrOS: Move Release Notes Notification and Suggestion Chip behind feature flag
    (cherry picked from commit d8b812ebdbde31cb91ebb6121e6b275428974245)
    
    Bug: 1016946,1024509
    Change-Id: Ia4b3a2a5f10bcac12c21f040cde81f7710be9b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879652
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#709296}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921342
    Cr-Commit-Position: refs/branch-heads/3945@{#692}
    Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2139,6 +2139,9 @@ extern const char kReduceDisplayNotificationsDescription[];
 extern const char kReleaseNotesName[];
 extern const char kReleaseNotesDescription[];
 
+extern const char kReleaseNotesNotificationName[];
+extern const char kReleaseNotesNotificationDescription[];
+
 extern const char kSchedulerConfigurationName[];
 extern const char kSchedulerConfigurationDescription[];
 extern const char kSchedulerConfigurationConservative[];
