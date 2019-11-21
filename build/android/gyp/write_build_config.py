dbuskariol-org/chromium

commit e1dc23f16b3f5668fa7a8112c8f808f4ea815809

Android: Make "incremental install" enabled by GN arg
Before:
 * ninja chrome_public_apk_incremental

After:
 * gn gen --args='incremental_install=true ...' .
 * ninja chrome_public_apk

Why make this change?
 * Simpler mental model (e.g. most other build things use GN args)
 * Reduces the load on bots that build all targets
 * "gn gen" on my machine goes from 7200ms -> 6500ms

Bug: 1013688
Change-Id: I62c7c1cea2002a6932867ebf91b31ffbaa8d6a30
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1856438
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Eric Stevenson <estevenson@chromium.org>
Reviewed-by: Dirk Pranke <dpranke@chromium.org>
Cr-Commit-Position: refs/heads/master@{#708209}

build/android/gyp/write_build_config.py
@@ -991,8 +991,7 @@ def main(argv):
 
   jar_path_options = ['jar_path', 'unprocessed_jar_path', 'interface_jar_path']
   required_options_map = {
-      'android_apk': ['build_config', 'dex_path', 'final_dex_path'] + \
-          jar_path_options,
+      'android_apk': ['build_config', 'dex_path'] + jar_path_options,
       'android_app_bundle_module': ['build_config', 'dex_path',
           'final_dex_path', 'res_size_info'] + jar_path_options,
       'android_assets': ['build_config'],

commit 2d7033b4e590929616fe2f9b291fcc5d545b6178

Android: Fix secondary_abi_loadable_modules being ignored
They worked in bundles, but not in apk targets.

As identified in the linked bug, this was broken by:
e1dc23f16b3f5668fa7a8112c8f808f4ea815809

Verified contents of apks are same with & without this change:
* Monochrome.apk (with & without build_apk_secondary_abi)
* TrichromeChrome.aab
* TrichromeLibrary.apk
* SystemWebview.apk (with & without build_apk_secondary_abi)

Bug: 1024862
Change-Id: I540e7b18cbd50eadfe92a36dd6d283e585b20daa
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925784
Auto-Submit: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Richard Coles <torne@chromium.org>
Reviewed-by: Eric Stevenson <estevenson@chromium.org>
Reviewed-by: Christopher Grant <cjgrant@chromium.org>
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#717780}

android_webview/system_webview_apk_tmpl.gni
@@ -51,6 +51,8 @@ template("system_webview_apk_tmpl") {
 
     _use_trichrome_library =
         defined(use_trichrome_library) && use_trichrome_library
+    _is_64_bit_browser = defined(is_64_bit_browser) && is_64_bit_browser
+    assert(_is_64_bit_browser || !_is_64_bit_browser)  # Mark used.
 
     if (!_use_trichrome_library) {
       deps += [ "//android_webview:monochrome_webview_assets" ]
@@ -58,44 +60,25 @@ template("system_webview_apk_tmpl") {
 
     if (!_use_trichrome_library) {
       shared_libraries = [ "//android_webview:libwebviewchromium" ]
-      deps += [
-        "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline",
-      ]
-      loadable_modules = [ "$root_out_dir/libcrashpad_handler_trampoline.so" ]
-
+      shared_resources = true
       if (build_apk_secondary_abi && android_64bit_target_cpu) {
         secondary_abi_shared_libraries = [ "//android_webview:libwebviewchromium($android_secondary_abi_toolchain)" ]
-        _trampoline = "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline($android_secondary_abi_toolchain)"
-        deps += [ _trampoline ]
-        _secondary_out_dir = get_label_info(_trampoline, "root_out_dir")
-        secondary_abi_loadable_modules =
-            [ "$_secondary_out_dir/libcrashpad_handler_trampoline.so" ]
       }
-      shared_resources = true
     } else {
       uncompress_shared_libraries = true
-
       app_as_shared_lib = true
 
       # Include placeholder libraries to ensure we are treated as the desired
       # architecture.
       if (android_64bit_target_cpu) {
-        if (defined(is_64_bit_browser) && is_64_bit_browser) {
+        if (_is_64_bit_browser) {
           native_lib_placeholders = [ "libdummy.so" ]
           if (build_apk_secondary_abi) {
             secondary_abi_shared_libraries = [ "//android_webview:monochrome_64($android_secondary_abi_toolchain)" ]
-            _trampoline = "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline($android_secondary_abi_toolchain)"
-            deps += [ _trampoline ]
-            _secondary_out_dir = get_label_info(_trampoline, "root_out_dir")
-            secondary_abi_loadable_modules =
-                [ "$_secondary_out_dir/libcrashpad_handler_trampoline.so" ]
             deps += [ "//android_webview:v8_snapshot_secondary_abi_assets" ]
           }
         } else {
           shared_libraries = [ "//android_webview:monochrome" ]
-          deps += [ "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline" ]
-          loadable_modules =
-              [ "$root_out_dir/libcrashpad_handler_trampoline.so" ]
           if (build_apk_secondary_abi) {
             secondary_native_lib_placeholders = [ "libdummy.so" ]
             if (use_v8_context_snapshot) {
@@ -106,14 +89,28 @@ template("system_webview_apk_tmpl") {
             }
           }
         }
-      } else if (defined(is_64_bit_browser) && is_64_bit_browser) {
+      } else if (_is_64_bit_browser) {
         shared_libraries = [ "//android_webview:monochrome_64" ]
-        deps += [
-          "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline",
-        ]
-        loadable_modules = [ "$root_out_dir/libcrashpad_handler_trampoline.so" ]
       } else {
         native_lib_placeholders = [ "libdummy.so" ]
+      }
+    }
+
+    if (!_use_trichrome_library ||
+        android_64bit_target_cpu != _is_64_bit_browser) {
+      deps += [
+        "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline",
+      ]
+      loadable_modules = [ "$root_out_dir/libcrashpad_handler_trampoline.so" ]
+    }
+
+    if (build_apk_secondary_abi && android_64bit_target_cpu) {
+      if (!_use_trichrome_library || _is_64_bit_browser) {
+        _trampoline = "//third_party/crashpad/crashpad/handler:crashpad_handler_trampoline($android_secondary_abi_toolchain)"
+        deps += [ _trampoline ]
+        _secondary_out_dir = get_label_info(_trampoline, "root_out_dir")
+        secondary_abi_loadable_modules =
+            [ "$_secondary_out_dir/libcrashpad_handler_trampoline.so" ]
       }
     }
 
@@ -163,7 +160,7 @@ template("system_webview_apk_tmpl") {
 
     if (!defined(version_code)) {
       if (_use_trichrome_library) {
-        if (defined(is_64_bit_browser) && is_64_bit_browser) {
+        if (_is_64_bit_browser) {
           version_code = trichrome_64_32_version_code
         } else {
           version_code = trichrome_version_code

build/android/gyp/write_build_config.py
@@ -370,10 +370,13 @@ Empty if only a single ABI is supported.
 A boolean indicating whether native libraries are stored uncompressed in the
 APK.
 
-* `native['extra_shared_libraries']`
+* `native['loadable_modules']`
 A list of native libraries to store within the APK, in addition to those from
 `native['libraries']`. These correspond to things like the Chromium linker
 or instrumentation libraries.
+
+* `native['secondary_abi_loadable_modules']`
+Secondary ABI version of loadable_modules
 
 * `assets`
 A list of assets stored compressed in the APK. Each entry has the format
@@ -877,19 +880,21 @@ def main(argv):
   parser.add_option('--shared-libraries-runtime-deps',
                     help='Path to file containing runtime deps for shared '
                          'libraries.')
-  parser.add_option('--native-libs',
-                    action='append',
-                    help='GN-list of native libraries for primary '
-                         'android-abi. Can be specified multiple times.',
-                    default=[])
+  parser.add_option(
+      '--loadable-modules',
+      action='append',
+      help='GN-list of native libraries for primary '
+      'android-abi. Can be specified multiple times.',
+      default=[])
   parser.add_option('--secondary-abi-shared-libraries-runtime-deps',
                     help='Path to file containing runtime deps for secondary '
                          'abi shared libraries.')
-  parser.add_option('--secondary-native-libs',
-                    action='append',
-                    help='GN-list of native libraries for secondary '
-                         'android-abi. Can be specified multiple times.',
-                    default=[])
+  parser.add_option(
+      '--secondary-abi-loadable-modules',
+      action='append',
+      help='GN-list of native libraries for secondary '
+      'android-abi. Can be specified multiple times.',
+      default=[])
   parser.add_option(
       '--native-lib-placeholders',
       action='append',
@@ -1661,16 +1666,15 @@ def main(argv):
           options.secondary_abi_shared_libraries_runtime_deps)
       all_inputs.append(options.secondary_abi_shared_libraries_runtime_deps)
 
-    secondary_abi_library_paths.extend(
-        build_utils.ParseGnList(options.secondary_native_libs))
-
     native_library_placeholder_paths = build_utils.ParseGnList(
         options.native_lib_placeholders)
 
     secondary_native_library_placeholder_paths = build_utils.ParseGnList(
         options.secondary_native_lib_placeholders)
 
-    extra_shared_libraries = build_utils.ParseGnList(options.native_libs)
+    loadable_modules = build_utils.ParseGnList(options.loadable_modules)
+    secondary_abi_loadable_modules = build_utils.ParseGnList(
+        options.secondary_abi_loadable_modules)
 
     config['native'] = {
         'libraries':
@@ -1685,8 +1689,10 @@ def main(argv):
         java_libraries_list,
         'uncompress_shared_libraries':
         options.uncompress_shared_libraries,
-        'extra_shared_libraries':
-        extra_shared_libraries,
+        'loadable_modules':
+        loadable_modules,
+        'secondary_abi_loadable_modules':
+        secondary_abi_loadable_modules,
     }
     config['assets'], config['uncompressed_assets'], locale_paks = (
         _MergeAssets(deps.All('android_assets')))

build/config/android/internal_rules.gni
@@ -362,15 +362,10 @@ template("write_build_config") {
       args += [ "--is-base-module" ]
     }
 
-    if (defined(invoker.loadable_modules) && invoker.loadable_modules != []) {
-      _rebased_modules = rebase_path(invoker.loadable_modules, root_build_dir)
-      args += [ "--native-libs=$_rebased_modules" ]
-    }
-
-    if (defined(invoker.extra_shared_libraries)) {
-      _rebased_extra_shared_libraries =
-          rebase_path(invoker.extra_shared_libraries, root_build_dir)
-      args += [ "--native-libs=$_rebased_extra_shared_libraries" ]
+    if (defined(invoker.loadable_modules)) {
+      _rebased_loadable_modules =
+          rebase_path(invoker.loadable_modules, root_build_dir)
+      args += [ "--loadable-modules=$_rebased_loadable_modules" ]
     }
 
     if (defined(invoker.secondary_abi_shared_libraries_runtime_deps_file)) {
@@ -386,9 +381,9 @@ template("write_build_config") {
 
     if (defined(invoker.secondary_abi_loadable_modules) &&
         invoker.secondary_abi_loadable_modules != []) {
-      _rebased_secondary_abi_modules =
+      _rebased_secondary_abi_loadable_modules =
           rebase_path(invoker.secondary_abi_loadable_modules, root_build_dir)
-      args += [ "--secondary-native-libs=$_rebased_secondary_abi_modules" ]
+      args += [ "--secondary-abi-loadable-modules=$_rebased_secondary_abi_loadable_modules" ]
     }
 
     if (defined(invoker.native_lib_placeholders) &&
@@ -2568,9 +2563,9 @@ if (enable_java_templates) {
   #     (optional).
   #   secondary_native_lib_placeholders: List of placeholder filenames to add to
   #     the apk for the secondary ABI (optional).
-  #   native_libs: List of native libraries.
+  #   loadable_modules: List of native libraries.
   #   native_libs_filearg: @FileArg() of additionally native libraries.
-  #   secondary_abi_native_libs: (optional) List of native libraries for
+  #   secondary_abi_loadable_modules: (optional) List of native libraries for
   #     secondary ABI.
   #   secondary_abi_native_libs_filearg: (optional). @FileArg() of additional
   #     secondary ABI native libs.
@@ -2605,7 +2600,7 @@ if (enable_java_templates) {
         "//tools/android/md5sum",
       ]  # Used when deploying APKs
 
-      inputs = invoker.native_libs + [
+      inputs = invoker.loadable_modules + [
                  invoker.keystore_path,
                  invoker.packaged_resources_path,
                  _apksigner,
@@ -2659,16 +2654,18 @@ if (enable_java_templates) {
         _rebased_dex_path = rebase_path(invoker.dex_path, root_build_dir)
         args += [ "--dex-file=$_rebased_dex_path" ]
       }
-      if (invoker.native_libs != [] || defined(invoker.native_libs_filearg) ||
+      if (invoker.loadable_modules != [] ||
+          defined(invoker.native_libs_filearg) ||
           _native_lib_placeholders != []) {
         args += [ "--android-abi=$android_app_abi" ]
       }
       if (defined(android_app_secondary_abi)) {
         args += [ "--secondary-android-abi=$android_app_secondary_abi" ]
       }
-      if (invoker.native_libs != []) {
-        _rebased_native_libs = rebase_path(invoker.native_libs, root_build_dir)
-        args += [ "--native-libs=$_rebased_native_libs" ]
+      if (invoker.loadable_modules != []) {
+        _rebased_loadable_modules =
+            rebase_path(invoker.loadable_modules, root_build_dir)
+        args += [ "--native-libs=$_rebased_loadable_modules" ]
       }
       if (defined(invoker.native_libs_filearg)) {
         args += [ "--native-libs=${invoker.native_libs_filearg}" ]
@@ -2676,12 +2673,19 @@ if (enable_java_templates) {
       if (_native_lib_placeholders != []) {
         args += [ "--native-lib-placeholders=$_native_lib_placeholders" ]
       }
-      if (_secondary_native_lib_placeholders != []) {
-        args += [ "--secondary-native-lib-placeholders=$_secondary_native_lib_placeholders" ]
-      }
 
       if (defined(invoker.secondary_abi_native_libs_filearg)) {
         args += [ "--secondary-native-libs=${invoker.secondary_abi_native_libs_filearg}" ]
+      }
+      if (defined(invoker.secondary_abi_loadable_modules)) {
+        _rebased_secondary_abi_loadable_modules =
+            rebase_path(invoker.secondary_abi_loadable_modules, root_build_dir)
+        args += [
+          "--secondary-native-libs=$_rebased_secondary_abi_loadable_modules",
+        ]
+      }
+      if (_secondary_native_lib_placeholders != []) {
+        args += [ "--secondary-native-lib-placeholders=$_secondary_native_lib_placeholders" ]
       }
 
       if (defined(invoker.uncompress_shared_libraries) &&
@@ -3018,8 +3022,10 @@ if (enable_java_templates) {
   #  secondary_abi_shared_libraries_runtime_deps_file:
   #  secondary_native_lib_placeholders: Optional. List of placeholder filenames
   #    to add to the APK for the secondary ABI.
-  #  extra_shared_libraries: Optional list of extra native libraries to
+  #  loadable_modules: Optional list of extra native libraries to
   #    be stored in the APK.
+  #  secondary_abi_loadable_modules: Optional list of native libraries for
+  #    secondary ABI.
   #  uncompress_shared_libraries: Optional. True to store native shared
   #    libraries uncompressed and page-aligned.
   #  proto_resources_path: The path of an zip archive containing the APK's
@@ -3225,13 +3231,11 @@ if (enable_java_templates) {
                                "base_whitelist_rtxt_path",
                                "gradle_treat_as_prebuilt",
                                "input_jars_paths",
-                               "loadable_modules",
                                "main_class",
                                "proguard_configs",
                                "proguard_enabled",
                                "proguard_mapping_path",
                                "r_text_path",
-                               "secondary_abi_loadable_modules",
                                "type",
                              ])
       if (type == "android_apk" || type == "android_app_bundle_module") {
@@ -3240,10 +3244,11 @@ if (enable_java_templates) {
             [
               "android_manifest",
               "android_manifest_dep",
-              "extra_shared_libraries",
               "final_dex_path",
+              "loadable_modules",
               "native_lib_placeholders",
               "res_size_info_path",
+              "secondary_abi_loadable_modules",
               "secondary_abi_shared_libraries_runtime_deps_file",
               "secondary_native_lib_placeholders",
               "shared_libraries_runtime_deps_file",

build/config/android/rules.gni
@@ -2283,11 +2283,6 @@ if (enable_java_templates) {
     _native_libs_deps = []
     _shared_libraries_is_valid =
         defined(invoker.shared_libraries) && invoker.shared_libraries != []
-    _secondary_abi_native_libs_deps = []
-    assert(_secondary_abi_native_libs_deps == [])  # mark as used.
-    _secondary_abi_shared_libraries_is_valid =
-        defined(invoker.secondary_abi_shared_libraries) &&
-        invoker.secondary_abi_shared_libraries != []
 
     if (_shared_libraries_is_valid) {
       _native_libs_deps += invoker.shared_libraries
@@ -2311,8 +2306,12 @@ if (enable_java_templates) {
       }
     }
 
-    if (_secondary_abi_shared_libraries_is_valid) {
-      _secondary_abi_native_libs_deps += invoker.secondary_abi_shared_libraries
+    _secondary_abi_native_libs_deps = []
+    assert(_secondary_abi_native_libs_deps == [])  # mark as used.
+
+    if (defined(invoker.secondary_abi_shared_libraries) &&
+        invoker.secondary_abi_shared_libraries != []) {
+      _secondary_abi_native_libs_deps = invoker.secondary_abi_shared_libraries
 
       # Write shared library output files of all dependencies to a file. Those
       # will be the shared libraries packaged into the APK.
@@ -2691,15 +2690,18 @@ if (enable_java_templates) {
       }
     }
 
-    _extra_native_libs = []
-    _extra_native_libs_deps = []
-    assert(_extra_native_libs_deps == [])  # Mark as used.
+    _loadable_modules = []
+    if (defined(invoker.loadable_modules)) {
+      _loadable_modules = invoker.loadable_modules
+    }
+
+    _loadable_modules_deps = []
     if (_native_libs_deps != []) {
-      _extra_native_libs += _sanitizer_runtimes
+      _loadable_modules += _sanitizer_runtimes
       if (_use_chromium_linker) {
-        _extra_native_libs +=
+        _loadable_modules +=
             [ "$root_out_dir/libchromium_android_linker$shlib_extension" ]
-        _extra_native_libs_deps +=
+        _loadable_modules_deps +=
             [ "//base/android/linker:chromium_android_linker" ]
       }
     }
@@ -2790,7 +2792,6 @@ if (enable_java_templates) {
                                "jacoco_never_instrument",
                                "java_files",
                                "javac_args",
-                               "loadable_modules",
                                "manifest_package",
                                "native_lib_placeholders",
                                "no_build_hooks",
@@ -2867,12 +2868,12 @@ if (enable_java_templates) {
       if (_native_libs_deps != []) {
         shared_libraries_runtime_deps_file = _shared_library_list_file
       }
-      if (_secondary_abi_native_libs_deps != []) {
+      if (defined(_secondary_abi_shared_library_list_file)) {
         secondary_abi_shared_libraries_runtime_deps_file =
             _secondary_abi_shared_library_list_file
       }
 
-      extra_shared_libraries = _extra_native_libs
+      loadable_modules = _loadable_modules
 
       uncompress_shared_libraries = _uncompress_shared_libraries
 
@@ -3069,8 +3070,6 @@ if (enable_java_templates) {
       }
     }
 
-    _loadable_modules = []
-    assert(_loadable_modules == [])  # Mark as used.
     if (_native_libs_deps != []) {
       _create_stack_script_rule_name = "${_template_name}__stack_script"
       _final_deps += [ ":${_create_stack_script_rule_name}" ]
@@ -3080,11 +3079,7 @@ if (enable_java_templates) {
       }
     }
 
-    if (defined(invoker.loadable_modules) && invoker.loadable_modules != []) {
-      _loadable_modules += invoker.loadable_modules
-    }
-
-    _all_native_libs_deps = _native_libs_deps + _extra_native_libs_deps +
+    _all_native_libs_deps = _native_libs_deps + _loadable_modules_deps +
                             _secondary_abi_native_libs_deps
     if (_all_native_libs_deps != []) {
       _native_libs_file_arg_dep = ":$_build_config_target"
@@ -3181,6 +3176,7 @@ if (enable_java_templates) {
         forward_variables_from(invoker,
                                [
                                  "native_lib_placeholders",
+                                 "secondary_abi_loadable_modules",
                                  "secondary_native_lib_placeholders",
                                  "uncompress_dex",
                                  "uncompress_shared_libraries",
@@ -3192,7 +3188,7 @@ if (enable_java_templates) {
         keystore_password = _keystore_password
         uncompress_shared_libraries = _uncompress_shared_libraries
 
-        native_libs = _loadable_modules
+        loadable_modules = _loadable_modules
         deps = _deps + [ ":$_build_config_target" ]
 
         if (_incremental_apk) {
@@ -3209,7 +3205,7 @@ if (enable_java_templates) {
 
           # http://crbug.com/384638
           _has_native_libs =
-              defined(invoker.native_libs_filearg) || _extra_native_libs != []
+              defined(invoker.native_libs_filearg) || _loadable_modules != []
           if (_has_native_libs && _loadable_modules == []) {
             native_lib_placeholders = [ "libfix.crbug.384638.so" ]
           }
@@ -3227,7 +3223,6 @@ if (enable_java_templates) {
             deps += [ _final_dex_target_dep ]
           }
 
-          native_libs += _extra_native_libs
           if (_optimize_resources) {
             packaged_resources_path = _optimized_arsc_resources_path
           } else {
@@ -3275,10 +3270,10 @@ if (enable_java_templates) {
           args += [ "--native-libs=$_native_libs_file_arg" ]
           deps += [ _native_libs_file_arg_dep ]
         }
-        if (_extra_native_libs != []) {
-          _rebased_extra_native_libs =
-              rebase_path(_extra_native_libs, root_build_dir)
-          args += [ "--native-libs=$_rebased_extra_native_libs" ]
+        if (_loadable_modules != []) {
+          _rebased_loadable_modules =
+              rebase_path(_loadable_modules, root_build_dir)
+          args += [ "--native-libs=$_rebased_loadable_modules" ]
         }
         if (_load_library_from_apk) {
           args += [ "--dont-even-try=Incremental builds do not work with load_library_from_apk. Try setting is_component_build=true in your GN args." ]
@@ -3359,7 +3354,7 @@ if (enable_java_templates) {
 
         # device/commands is used by the installer script to push files via .zip.
         data_deps += [ "//build/android/pylib/device/commands" ] +
-                     _native_libs_deps + _extra_native_libs_deps
+                     _native_libs_deps + _loadable_modules_deps
       }
     }
   }
@@ -4536,7 +4531,7 @@ if (enable_java_templates) {
       modules = _modules
       native_libraries_filearg_keys = [
         "native:libraries",
-        "native:extra_shared_libraries",
+        "native:loadable_modules",
       ]
       output = _native_libraries_config
       if (_uses_static_library) {
@@ -4549,7 +4544,10 @@ if (enable_java_templates) {
       allot_native_libraries(
           "${_target_name}__allot_secondary_abi_native_libraries") {
         modules = _modules
-        native_libraries_filearg_keys = [ "native:secondary_abi_libraries" ]
+        native_libraries_filearg_keys = [
+          "native:secondary_abi_libraries",
+          "native:secondary_abi_loadable_modules",
+        ]
         output = _secondary_abi_native_libraries_config
         if (_uses_static_library) {
           modules += [ _lib_proxy_module ]