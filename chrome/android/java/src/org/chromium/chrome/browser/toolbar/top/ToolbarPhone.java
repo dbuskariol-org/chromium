f52d783980cac79f67119c9f3d26524a38daf66c
Brandon Wylie <wylieb@chromium.org>
2019-11-28 16:18:42 +0000

    Prevent the LocationBar being captured incorrectly
    Currently, the tab switcher captures an overlay to use when entering
    and exiting the TabSwitcher mode. This captures the LocationBar even
    when the fakebox would be drawn instead, which causes the LocationBar
    to flash on the screen when transitioning between the TabSwitcher to
    the NTP.
    
    (cherry picked from commit 5c5d75efae370145bc13076995d3aca731b7de96)
    
    Bug: 1011481
    Change-Id: I082fdfd26a7b50d77cd38dfc4e5ef8068a126018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913471
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#717293}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942938
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3945@{#814}
    Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -1253,7 +1253,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         previousAlpha = mLocationBar.getAlpha();
         mLocationBar.setAlpha(previousAlpha * floatAlpha);
         // If the location bar is now fully transparent, do not bother drawing it.
-        if (mLocationBar.getAlpha() != 0) {
+        if (mLocationBar.getAlpha() != 0 && isLocationBarCurrentlyShown()) {
             drawChild(canvas, mLocationBar, SystemClock.uptimeMillis());
         }
         mLocationBar.setAlpha(previousAlpha);
@@ -2382,6 +2382,11 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     private boolean isLocationBarShownInNTP() {
         NewTabPage ntp = getToolbarDataProvider().getNewTabPageForCurrentTab();
         return ntp != null && ntp.isLocationBarShownInNTP();
+    }
+
+    private boolean isLocationBarCurrentlyShown() {
+        NewTabPage ntp = getToolbarDataProvider().getNewTabPageForCurrentTab();
+        return ntp == null || !isLocationBarShownInNTP() || mUrlExpansionPercent > 0;
     }
 
     /**
