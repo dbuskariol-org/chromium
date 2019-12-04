ac0796ce326630bd6974153bfa4c35989413fea2
Yue Zhang <yuezhanggg@chromium.org>
2019-12-04 00:03:08 +0000

    Add flag DuetTabStripIntegrationAndroid
    Add Duet-TabStrip integration flag. Originated from
    http://crrev.com/c/1915462.
    
    (cherry picked from commit e9e90c5ac4ba0cfe292a5addb2d9b0e6788bcec4)
    
    Bug: 1022827
    Change-Id: I3a47b151573357dbdad45184ae72a83e706f7dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935439
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#719344}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949231
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3945@{#872}
    Cr-Branched-From: e4635fff7defbae0f9c29e798349f6fc0cce4b1b-refs/heads/master@{#706915}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
@@ -242,6 +242,7 @@ public abstract class ChromeFeatureList {
     public static final String DOWNLOADS_LOCATION_CHANGE = "DownloadsLocationChange";
     public static final String DOWNLOAD_TAB_MANAGEMENT_MODULE = "DownloadTabManagementModule";
     public static final String DRAW_VERTICALLY_EDGE_TO_EDGE = "DrawVerticallyEdgeToEdge";
+    public static final String DUET_TABSTRIP_INTEGRATION_ANDROID = "DuetTabStripIntegrationAndroid";
     public static final String EPHEMERAL_TAB = "EphemeralTab";
     public static final String EPHEMERAL_TAB_USING_BOTTOM_SHEET = "EphemeralTabUsingBottomSheet";
     public static final String EXPLICIT_LANGUAGE_ASK = "ExplicitLanguageAsk";
