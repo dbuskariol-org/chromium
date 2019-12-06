3df81bcb05946a22785805d4f0ec0c447ce31a35
Nate Chapin <japhet@chromium.org>
2019-12-06 17:31:38 +0000

    Make ScriptRunner a ContextLifecycleStateObserver
    ScriptRunner has special cases in Document to allow it to
    pause/unpause, but ContextLifecycleStateObserver is the standard way
    to pause/unpause.
    
    Bug: 1029822
    Change-Id: Ie8b5e427e6a217ba6a1aad9af7a286ce22444918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949225
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722506}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7293,15 +7293,11 @@ void Document::AddConsoleMessageImpl(ConsoleMessage* console_message,
 }
 
 void Document::TasksWerePaused() {
-  GetScriptRunner()->Suspend();
-
   if (scripted_animation_controller_)
     scripted_animation_controller_->Pause();
 }
 
 void Document::TasksWereUnpaused() {
-  GetScriptRunner()->Resume();
-
   if (scripted_animation_controller_)
     scripted_animation_controller_->Unpause();
 }
