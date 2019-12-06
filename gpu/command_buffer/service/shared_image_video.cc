36a639fc2d93173bd03d74a5ca4faf99eb2f64f8
Vikas Soni <vikassoni@chromium.org>
2019-12-06 03:58:20 +0000

    Vulkan: Update chromaFilter to use correct VkFilter.
    Vulkan uses VkSamplerYcbcrConversion struct which needs the
    chromaFilter and formatFeatures information. Update the chromaFilter
    based on the bits set in formatFeatures as per the vulkan spec.
    
    Bug: 991291
    Change-Id: I598219ec3fb851957447da00fb1f1fd2934fb1f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954708
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722374}

--- gpu/command_buffer/service/shared_image_video.cc
+++ gpu/command_buffer/service/shared_image_video.cc
@@ -57,16 +57,18 @@ sk_sp<SkPromiseImageTexture> CreatePromiseTextureVideo(
     return nullptr;
   }
 
-  GrVkYcbcrConversionInfo fYcbcrConversionInfo(
-      static_cast<VkSamplerYcbcrModelConversion>(
-          ycbcr_info.suggested_ycbcr_model),
-      static_cast<VkSamplerYcbcrRange>(ycbcr_info.suggested_ycbcr_range),
-      static_cast<VkChromaLocation>(ycbcr_info.suggested_xchroma_offset),
-      static_cast<VkChromaLocation>(ycbcr_info.suggested_ychroma_offset),
-      VK_FILTER_LINEAR,  // VkFilter
-      0,                 // VkBool32 forceExplicitReconstruction
-      ycbcr_info.external_format,
-      static_cast<VkFormatFeatureFlags>(ycbcr_info.format_features));
+  // We always use VK_IMAGE_TILING_OPTIMAL while creating the vk image in
+  // VulkanImplementationAndroid::CreateVkImageAndImportAHB. Hence pass the
+  // tiling parameter as VK_IMAGE_TILING_OPTIMAL to below call rather than
+  // passing |vk_image_info.tiling|. This is also to ensure that the promise
+  // image created here at [1] as well the fullfil image created via the current
+  // function call are consistent and both are using VK_IMAGE_TILING_OPTIMAL.
+  // [1] -
+  // https://cs.chromium.org/chromium/src/components/viz/service/display_embedder/skia_output_surface_impl.cc?rcl=db5ffd448ba5d66d9d3c5c099754e5067c752465&l=789.
+  DCHECK_EQ(static_cast<int32_t>(vk_image_info.tiling),
+            static_cast<int32_t>(VK_IMAGE_TILING_OPTIMAL));
+  GrVkYcbcrConversionInfo gr_ycbcr_info = CreateGrVkYcbcrConversionInfo(
+      vk_physical_device, VK_IMAGE_TILING_OPTIMAL, ycbcr_info);
 
   // Create backend texture from the VkImage.
   GrVkAlloc alloc = {vk_device_memory, 0, mem_allocation_size, 0};
@@ -78,7 +80,7 @@ sk_sp<SkPromiseImageTexture> CreatePromiseTextureVideo(
                            vk_image_info.mipLevels,
                            VK_QUEUE_FAMILY_EXTERNAL,
                            GrProtected::kNo,
-                           fYcbcrConversionInfo};
+                           gr_ycbcr_info};
 
   // TODO(bsalomon): Determine whether it makes sense to attempt to reuse this
   // if the vk_info stays the same on subsequent calls.
