970a09b281ded2e230d83cd99b6a067160376daf
Kevin McNee <mcnee@chromium.org>
2019-12-06 16:07:27 +0000

    Remove embeddedExtensionOptions extension permission
    <extensionoptions> is used to display extension options pages embedded
    in chrome://extensions. There currently exists an extension permission
    "embeddedExtensionOptions" which would allow an extension that requested
    it to also use <extensionoptions>. This permission is restricted to
    trunk. This was last changed in 2014. According to issue 420926, there
    is no longer a need for this permission as it is enough to whitelist
    usage for WebUI.
    
    We remove this permission and the related tests that were specific to
    the use of <extensionoptions> by other extensions. Note that we still
    keep the <extensionoptions> tests related to the extension management
    WebUI as that is separate from the permission. The tests that were
    implemented in terms of extensions and are still relevant to the
    extensions WebUI have been reimplemented in terms of WebUI.
    
    Also note that there is a switch with the same name
    (--embedded-extension-options), but this does not relate to the
    permission. It controls the default value of |open_in_tab| if not
    specified in the extension manifest.
    
    Bug: 420926, 415949
    Change-Id: I91081ca3056fdd5bb1e14e5852ec067ed26f455a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864519
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722489}

--- chrome/browser/extensions/extension_webui_apitest.cc
+++ chrome/browser/extensions/extension_webui_apitest.cc
@@ -13,10 +13,14 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/guest_view/browser/guest_view_manager_delegate.h"
+#include "components/guest_view/browser/test_guest_view_manager.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
+#include "extensions/browser/api/extensions_api_client.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/common/api/test.h"
 #include "extensions/common/extension.h"
@@ -73,6 +77,52 @@ class ExtensionWebUITest : public ExtensionApiTest {
     // Tests should fail.
     return RunTest(name, GURL("chrome://about"), false);
   }
+};
+
+// Tests running within an <extensionoptions> WebContents.
+class ExtensionWebUIEmbeddedOptionsTest : public ExtensionWebUITest {
+ public:
+  void SetUpOnMainThread() override {
+    ExtensionWebUITest::SetUpOnMainThread();
+    guest_view::GuestViewManager::set_factory_for_testing(
+        &test_guest_view_manager_factory_);
+    test_guest_view_manager_ = static_cast<guest_view::TestGuestViewManager*>(
+        guest_view::GuestViewManager::CreateWithDelegate(
+            browser()->profile(),
+            ExtensionsAPIClient::Get()->CreateGuestViewManagerDelegate(
+                browser()->profile())));
+  }
+
+ protected:
+  // Loads |extension|'s options page in an <extensionoptions> and returns the
+  // <extensionoptions>'s WebContents.
+  content::WebContents* OpenExtensionOptions(const Extension* extension) {
+    ui_test_utils::NavigateToURL(browser(),
+                                 GURL(chrome::kChromeUIExtensionsURL));
+    content::WebContents* webui =
+        browser()->tab_strip_model()->GetActiveWebContents();
+
+    EXPECT_EQ(0U, test_guest_view_manager_->num_guests_created());
+
+    EXPECT_TRUE(content::ExecuteScript(
+        webui,
+        content::JsReplace(
+            "let extensionoptions = document.createElement('extensionoptions');"
+            "extensionoptions.extension = $1;"
+            "document.body.appendChild(extensionoptions);",
+            extension->id())));
+
+    content::WebContents* guest_web_contents =
+        test_guest_view_manager_->WaitForSingleGuestCreated();
+    EXPECT_TRUE(guest_web_contents);
+    EXPECT_TRUE(content::WaitForLoadStop(guest_web_contents));
+
+    return guest_web_contents;
+  }
+
+ private:
+  guest_view::TestGuestViewManagerFactory test_guest_view_manager_factory_;
+  guest_view::TestGuestViewManager* test_guest_view_manager_ = nullptr;
 };
 
 #if !defined(OS_WIN)  // flaky http://crbug.com/530722
@@ -141,7 +191,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, CanEmbedExtensionOptions) {
 
   const Extension* extension =
       LoadExtension(test_data_dir_.AppendASCII("extension_options")
-                        .AppendASCII("embed_self"));
+                        .AppendASCII("extension_with_options_page"));
   ASSERT_TRUE(extension);
 
   ASSERT_TRUE(RunTestOnExtensionsPage("can_embed_extension_options.js"));
@@ -150,6 +200,93 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, CanEmbedExtensionOptions) {
   listener->Reply(extension->id());
   listener.reset(new ExtensionTestMessageListener("load", false));
   ASSERT_TRUE(listener->WaitUntilSatisfied());
+}
+
+// Tests that an <extensionoptions> guest view can access the chrome.storage
+// API, a privileged extension API.
+IN_PROC_BROWSER_TEST_F(ExtensionWebUIEmbeddedOptionsTest,
+                       ExtensionOptionsCanAccessStorage) {
+  const Extension* extension =
+      LoadExtension(test_data_dir_.AppendASCII("extension_options")
+                        .AppendASCII("extension_with_options_page"));
+  ASSERT_TRUE(extension);
+
+  content::WebContents* guest_web_contents = OpenExtensionOptions(extension);
+
+  const std::string storage_key = "test";
+  const int storage_value = 42;
+
+  EXPECT_TRUE(content::ExecuteScript(
+      guest_web_contents,
+      content::JsReplace("var onChangedPromise = new Promise((resolve) => {"
+                         "  chrome.storage.onChanged.addListener((change) => {"
+                         "    resolve(change[$1].newValue);"
+                         "  });"
+                         "});",
+                         storage_key)));
+
+  std::string set_result;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractString(
+      guest_web_contents,
+      content::JsReplace(
+          "try {"
+          "  chrome.storage.local.set({$1: $2}, () => {"
+          "    domAutomationController.send("
+          "        chrome.runtime.lastError ?"
+          "            chrome.runtime.lastError.message : 'success');"
+          "  });"
+          "} catch (e) {"
+          "  domAutomationController.send(e.name + ': ' + e.message);"
+          "}",
+          storage_key, storage_value),
+      &set_result));
+  ASSERT_EQ("success", set_result);
+
+  int actual_value = 0;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractInt(
+      guest_web_contents,
+      content::JsReplace("chrome.storage.local.get((storage) => {"
+                         "  domAutomationController.send(storage[$1]);"
+                         "});",
+                         storage_key),
+      &actual_value));
+  EXPECT_EQ(storage_value, actual_value);
+
+  EXPECT_TRUE(content::ExecuteScriptAndExtractInt(
+      guest_web_contents,
+      "onChangedPromise.then((newValue) => {"
+      "  domAutomationController.send(newValue);"
+      "});",
+      &actual_value));
+  EXPECT_EQ(storage_value, actual_value);
+}
+
+// TODO(https://crbug.com/415949): This test was converted from a test that
+// was marked as flaky on Mac. It may be that this new test is not flaky, so
+// we can try to re-enable once the CL that introduces this converted test
+// lands.
+#if defined(OS_MACOSX)
+#define MAYBE_ExtensionOptionsExternalLinksOpenInNewTab \
+  DISABLED_ExtensionOptionsExternalLinksOpenInNewTab
+#else
+#define MAYBE_ExtensionOptionsExternalLinksOpenInNewTab \
+  ExtensionOptionsExternalLinksOpenInNewTab
+#endif
+IN_PROC_BROWSER_TEST_F(ExtensionWebUIEmbeddedOptionsTest,
+                       MAYBE_ExtensionOptionsExternalLinksOpenInNewTab) {
+  const Extension* extension =
+      LoadExtension(test_data_dir_.AppendASCII("extension_options")
+                        .AppendASCII("extension_with_options_page"));
+  ASSERT_TRUE(extension);
+
+  content::WebContents* guest_web_contents = OpenExtensionOptions(extension);
+
+  content::WebContentsAddedObserver new_contents_observer;
+  EXPECT_TRUE(content::ExecuteScript(
+      guest_web_contents, "document.getElementById('link').click();"));
+  content::WebContents* new_contents = new_contents_observer.GetWebContents();
+  EXPECT_NE(TabStripModel::kNoTab,
+            browser()->tab_strip_model()->GetIndexOfWebContents(new_contents));
 }
 
 IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, ReceivesExtensionOptionsOnClose) {
@@ -182,7 +319,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, EmbedDisabledExtension) {
   {
     const Extension* extension =
         LoadExtension(test_data_dir_.AppendASCII("extension_options")
-                          .AppendASCII("embed_self"));
+                          .AppendASCII("extension_with_options_page"));
     ASSERT_TRUE(extension);
     extension_id = extension->id();
     DisableExtension(extension_id);
@@ -194,6 +331,35 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, EmbedDisabledExtension) {
   listener->Reply(extension_id);
   listener.reset(new ExtensionTestMessageListener("createfailed", false));
   ASSERT_TRUE(listener->WaitUntilSatisfied());
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, EmbedInvalidExtension) {
+  ExtensionTestMessageListener ready_listener("ready", true);
+
+  const std::string extension_id = "thisisprobablynotrealextensionid";
+
+  ASSERT_TRUE(RunTestOnExtensionsPage("can_embed_extension_options.js"));
+  ASSERT_TRUE(ready_listener.WaitUntilSatisfied());
+
+  ExtensionTestMessageListener create_failed_listener("createfailed", false);
+  ready_listener.Reply(extension_id);
+  ASSERT_TRUE(create_failed_listener.WaitUntilSatisfied());
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionWebUITest, EmbedExtensionWithoutOptionsPage) {
+  ExtensionTestMessageListener ready_listener("ready", true);
+
+  const Extension* extension =
+      LoadExtension(test_data_dir_.AppendASCII("extension_options")
+                        .AppendASCII("extension_without_options_page"));
+  ASSERT_TRUE(extension);
+
+  ASSERT_TRUE(RunTestOnExtensionsPage("can_embed_extension_options.js"));
+  ASSERT_TRUE(ready_listener.WaitUntilSatisfied());
+
+  ExtensionTestMessageListener create_failed_listener("createfailed", false);
+  ready_listener.Reply(extension->id());
+  ASSERT_TRUE(create_failed_listener.WaitUntilSatisfied());
 }
 
 #endif
