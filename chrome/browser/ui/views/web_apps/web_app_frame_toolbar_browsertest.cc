ecf50882249a7fa410d75cff66a0d65968a2ecb5
Eric Willigers <ericwilligers@chromium.org>
2019-12-09 07:11:30 +0000

    Desktop PWAs: Test hiding of minimal-ui controls and window title
    We test that when the window width reduces, the minimal-ui controls
    and the window title are hidden.
    
    Bug: 1007151
    Change-Id: Ia6c6a9d48cb684d7f4af238b8bca56467dcb2ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952558
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722851}

--- chrome/browser/ui/views/web_apps/web_app_frame_toolbar_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_frame_toolbar_browsertest.cc
@@ -6,9 +6,12 @@
 #include "chrome/browser/ui/views/web_apps/web_app_frame_toolbar_view.h"
 
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/frame/app_menu_button.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
@@ -46,6 +49,7 @@ class WebAppFrameToolbarBrowserTest : public InProcessBrowserTest {
     auto web_app_info = std::make_unique<WebApplicationInfo>();
     web_app_info->app_url = GetAppURL();
     web_app_info->scope = GetAppURL().GetWithoutFilename();
+    web_app_info->title = base::ASCIIToUTF16("A minimal-ui app");
     web_app_info->display_mode = web_app::DisplayMode::kMinimalUi;
     web_app_info->open_as_window = true;
 
@@ -77,20 +81,43 @@ class WebAppFrameToolbarBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(WebAppFrameToolbarBrowserTest, SpaceConstrained) {
   InstallAndLaunchWebApp();
 
-  views::View* toolbar_right_container =
+  views::View* const toolbar_left_container =
+      web_app_frame_toolbar_->GetLeftContainerForTesting();
+  EXPECT_EQ(toolbar_left_container->parent(), web_app_frame_toolbar_);
+
+  views::View* const window_title =
+      frame_view_->GetViewByID(VIEW_ID_WINDOW_TITLE);
+#if defined(OS_CHROMEOS)
+  EXPECT_FALSE(window_title);
+#else
+  EXPECT_EQ(window_title->parent(), frame_view_);
+#endif
+
+  views::View* const toolbar_right_container =
       web_app_frame_toolbar_->GetRightContainerForTesting();
   EXPECT_EQ(toolbar_right_container->parent(), web_app_frame_toolbar_);
 
-  views::View* page_action_icon_container =
+  views::View* const page_action_icon_container =
       web_app_frame_toolbar_->GetPageActionIconContainerForTesting();
   EXPECT_EQ(page_action_icon_container->parent(), toolbar_right_container);
 
-  views::View* menu_button =
+  views::View* const menu_button =
       browser_view_->toolbar_button_provider()->GetAppMenuButton();
   EXPECT_EQ(menu_button->parent(), toolbar_right_container);
 
-  // Initially the page action icons are not visible, just the menu button has
-  // width.
+  // Ensure we initially have abundant space.
+  frame_view_->SetSize(gfx::Size(1000, 1000));
+
+  EXPECT_TRUE(toolbar_left_container->GetVisible());
+  const int original_left_container_width = toolbar_left_container->width();
+  EXPECT_GT(original_left_container_width, 0);
+
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  const int original_window_title_width = window_title->width();
+  EXPECT_GT(original_window_title_width, 0);
+#endif
+
+  // Initially the page action icons are not visible.
   EXPECT_EQ(page_action_icon_container->width(), 0);
   const int original_menu_button_width = menu_button->width();
   EXPECT_GT(original_menu_button_width, 0);
@@ -100,19 +127,41 @@ IN_PROC_BROWSER_TEST_F(WebAppFrameToolbarBrowserTest, SpaceConstrained) {
 
   // The layout should be invalidated, but since we don't have the benefit of
   // the compositor to immediately kick a layout off, we have to do it manually.
-  web_app_frame_toolbar_->Layout();
+  frame_view_->Layout();
 
-  // The page action icons should now take up width.
+  // The page action icons should now take up width, leaving less space on
+  // Windows and Linux for the window title. (On Mac, the window title remains
+  // centered - not tested here.)
+
+  EXPECT_TRUE(toolbar_left_container->GetVisible());
+  EXPECT_EQ(toolbar_left_container->width(), original_left_container_width);
+
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  EXPECT_GT(window_title->width(), 0);
+  EXPECT_LT(window_title->width(), original_window_title_width);
+#endif
+
   EXPECT_GT(page_action_icon_container->width(), 0);
   EXPECT_EQ(menu_button->width(), original_menu_button_width);
 
   // Resize the WebAppFrameToolbarView just enough to clip out the page action
-  // icons.
+  // icons (and toolbar contents left of them).
+  const int original_toolbar_width = web_app_frame_toolbar_->width();
   web_app_frame_toolbar_->SetSize(
       gfx::Size(toolbar_right_container->width() -
                     page_action_icon_container->bounds().right(),
                 web_app_frame_toolbar_->height()));
-  web_app_frame_toolbar_->Layout();
+  frame_view_->SetSize(gfx::Size(frame_view_->width() - original_toolbar_width +
+                                     web_app_frame_toolbar_->width(),
+                                 frame_view_->height()));
+
+  // The left container (containing Back and Reload) should be hidden.
+  EXPECT_FALSE(toolbar_left_container->GetVisible());
+
+  // The window title should be clipped to 0 width.
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  EXPECT_EQ(window_title->width(), 0);
+#endif
 
   // The page action icons should be clipped to 0 width while the app menu
   // button retains its full width.
