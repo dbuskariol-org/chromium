dace0deac4eae4ca7a9e3ad10140bce401855c95
Esmael El-Moslimany <aee@chromium.org>
2019-12-09 20:07:21 +0000

    WebUI NTP: most-visited/shortcuts, initial support
    This implementation of most-visited is functionally the same of the
    most-visited on the current NTP. The differences in UI are going to be
    discussed with @bklmn after this CL lands.
    
     - Show up to 10 most-visited tiles.
     - Toast is shown with undo and restore to defaults buttons when
       certain tile actions are performed.
     - Action menu displays options to edit and remove tile.
     - Show add shortcut tile if space allows.
     - Supports light and dark mode.
    
    Bug: 997229
    Change-Id: Ie22220e2f866ba1ba485daf504265e9d868f9050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924702
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723050}

---
+++ chrome/test/data/webui/new_tab_page/app_test.js
@@ -0,0 +1,52 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://new-tab-page/app.js';
+
+import {BrowserProxy} from 'chrome://new-tab-page/browser_proxy.js';
+import {TestProxy} from 'chrome://test/new_tab_page/test_support.js';
+import {eventToPromise, flushTasks} from 'chrome://test/test_util.m.js';
+
+suite('NewTabPageAppTest', () => {
+  /** @type {!NewTabPageApp} */
+  let app;
+
+  /** @type {TestProxy} */
+  let testProxy;
+
+  /** @type {CrToastManagerElement} */
+  let toastManager;
+
+  setup(() => {
+    PolymerTest.clearBody();
+
+    testProxy = new TestProxy();
+    BrowserProxy.instance_ = testProxy;
+
+    app = document.createElement('new-tab-page-app');
+    document.body.appendChild(app);
+
+    toastManager = app.shadowRoot.querySelector('cr-toast-manager');
+  });
+
+  test('toast restore defaults button', async () => {
+    const wait = testProxy.handler.whenCalled('restoreMostVisitedDefaults');
+    assertFalse(toastManager.isToastOpen);
+    toastManager.show('');
+    assertTrue(toastManager.isToastOpen);
+    app.$.restore.click();
+    await wait;
+    assertFalse(toastManager.isToastOpen);
+  });
+
+  test('toast undo button', async () => {
+    const wait = testProxy.handler.whenCalled('undoMostVisitedTileAction');
+    assertFalse(toastManager.isToastOpen);
+    toastManager.show('');
+    assertTrue(toastManager.isToastOpen);
+    app.$.undo.click();
+    await wait;
+    assertFalse(toastManager.isToastOpen);
+  });
+});
