ffe6b3734a81612f6eee0e3abade09c692be6092
Kuo Jen Wei <inker@google.com>
2019-12-10 04:41:24 +0000

    [CCA] Migrate cca.models.Filenamer to ES6 class.
    Bug: b/141518780
    Test: Pass closure compiler check, tast run <DUT> 'camera.CCAUI*' and
    validate all function of CCA on HALv1/v3 device works correctly.
    
    Change-Id: Ifd4e601cd6f8ba25d32edcae7a1e68d4d4082163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954994
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723244}

--- chrome/browser/resources/chromeos/camera/src/js/models/filenamer.js
+++ chrome/browser/resources/chromeos/camera/src/js/models/filenamer.js
@@ -16,26 +16,84 @@ cca.models = cca.models || {};
 
 /**
  * Filenamer for single camera session.
- * @param {number=} timestamp Timestamp of camera session.
- * @constructor
  */
-cca.models.Filenamer = function(timestamp) {
+cca.models.Filenamer = class {
   /**
-   * Timestamp of camera session.
-   * @type {number}
-   * @private
+   * @param {number=} timestamp Timestamp of camera session.
    */
-  this.timestamp_ = timestamp === undefined ? Date.now() : timestamp;
+  constructor(timestamp) {
+    /**
+     * Timestamp of camera session.
+     * @type {number}
+     * @private
+     */
+    this.timestamp_ = timestamp === undefined ? Date.now() : timestamp;
+
+    /**
+     * Number of already saved burst images.
+     * @type {number}
+     * @private
+     */
+    this.burstCount_ = 0;
+  }
 
   /**
-   * Number of already saved burst images.
-   * @type {number}
+   * Creates new filename for burst image.
+   * @param {boolean} isCover If the image is set as cover of the burst.
+   * @return {string} New filename.
+   */
+  newBurstName(isCover) {
+    const prependZeros = (n, width) => {
+      n = n + '';
+      return new Array(Math.max(0, width - n.length) + 1).join('0') + n;
+    };
+    return cca.models.Filenamer.IMAGE_PREFIX +
+        this.constructor.timestampToDatetimeName_(this.timestamp_) +
+        cca.models.Filenamer.BURST_SUFFIX +
+        prependZeros(++this.burstCount_, 5) +
+        (isCover ? cca.models.Filenamer.BURST_COVER_SUFFIX : '') + '.jpg';
+  }
+
+  /**
+   * Creates new filename for video.
+   * @return {string} New filename.
+   */
+  newVideoName() {
+    return cca.models.Filenamer.VIDEO_PREFIX +
+        this.constructor.timestampToDatetimeName_(this.timestamp_) + '.mkv';
+  }
+
+  /**
+   * Creates new filename for image.
+   * @return {string} New filename.
+   */
+  newImageName() {
+    return cca.models.Filenamer.IMAGE_PREFIX +
+        this.constructor.timestampToDatetimeName_(this.timestamp_) + '.jpg';
+  }
+
+  /**
+   * Transforms from capture timestamp to datetime name.
+   * @param {number} timestamp Timestamp to be transformed.
+   * @return {string} Transformed datetime name.
    * @private
    */
-  this.burstCount_ = 0;
+  static timestampToDatetimeName_(timestamp) {
+    const pad = (n) => (n < 10 ? '0' : '') + n;
+    var date = new Date(timestamp);
+    return date.getFullYear() + pad(date.getMonth() + 1) + pad(date.getDate()) +
+        '_' + pad(date.getHours()) + pad(date.getMinutes()) +
+        pad(date.getSeconds());
+  }
 
-  // End of properties, seal the object.
-  Object.seal(this);
+  /**
+   * Get the metadata name from image name.
+   * @param {string} imageName Name of image to derive the metadata name.
+   * @return {string} Metadata name of the image.
+   */
+  static getMetadataName(imageName) {
+    return imageName.replace(/\.[^/.]+$/, '.json');
+  }
 };
 
 /**
@@ -65,60 +123,3 @@ cca.models.Filenamer.BURST_SUFFIX = '_BURST';
  * @const
  */
 cca.models.Filenamer.BURST_COVER_SUFFIX = '_COVER';
-
-/**
- * Creates new filename for burst image.
- * @param {boolean} isCover If the image is set as cover of the burst.
- * @return {string} New filename.
- */
-cca.models.Filenamer.prototype.newBurstName = function(isCover) {
-  const prependZeros = (n, width) => {
-    n = n + '';
-    return new Array(Math.max(0, width - n.length) + 1).join('0') + n;
-  };
-  return cca.models.Filenamer.IMAGE_PREFIX +
-      cca.models.Filenamer.timestampToDatetimeName_(this.timestamp_) +
-      cca.models.Filenamer.BURST_SUFFIX + prependZeros(++this.burstCount_, 5) +
-      (isCover ? cca.models.Filenamer.BURST_COVER_SUFFIX : '') + '.jpg';
-};
-
-/**
- * Creates new filename for video.
- * @return {string} New filename.
- */
-cca.models.Filenamer.prototype.newVideoName = function() {
-  return cca.models.Filenamer.VIDEO_PREFIX +
-      cca.models.Filenamer.timestampToDatetimeName_(this.timestamp_) + '.mkv';
-};
-
-/**
- * Creates new filename for image.
- * @return {string} New filename.
- */
-cca.models.Filenamer.prototype.newImageName = function() {
-  return cca.models.Filenamer.IMAGE_PREFIX +
-      cca.models.Filenamer.timestampToDatetimeName_(this.timestamp_) + '.jpg';
-};
-
-/**
- * Transforms from capture timestamp to datetime name.
- * @param {number} timestamp Timestamp to be transformed.
- * @return {string} Transformed datetime name.
- * @private
- */
-cca.models.Filenamer.timestampToDatetimeName_ = function(timestamp) {
-  const pad = (n) => (n < 10 ? '0' : '') + n;
-  var date = new Date(timestamp);
-  return date.getFullYear() + pad(date.getMonth() + 1) + pad(date.getDate()) +
-      '_' + pad(date.getHours()) + pad(date.getMinutes()) +
-      pad(date.getSeconds());
-};
-
-/**
- * Get the metadata name from image name.
- * @param {string} imageName Name of image to derive the metadata name.
- * @return {string} Metadata name of the image.
- */
-cca.models.Filenamer.getMetadataName = function(imageName) {
-  return imageName.replace(/\.[^/.]+$/, '.json');
-};
