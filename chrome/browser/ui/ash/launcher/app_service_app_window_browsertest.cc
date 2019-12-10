c6cd7d61b4714d8c27a702aaa2adf24d4373ce7a
nancy <nancylingwang@chromium.org>
2019-12-10 01:02:29 +0000

    Add AppService instance browser test for Web apps.
    BUG=1011235
    
    Change-Id: I3b801416ea1c9ee629651b7cef954875f8597409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956969
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723179}

--- chrome/browser/ui/ash/launcher/app_service_app_window_browsertest.cc
+++ chrome/browser/ui/ash/launcher/app_service_app_window_browsertest.cc
@@ -13,9 +13,11 @@
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller_test_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/services/app_service/public/cpp/instance.h"
 #include "chrome/services/app_service/public/cpp/instance_registry.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/common/extension.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -190,5 +192,94 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowBrowserTest, MultipleWindows) {
   // Close the first window; the shelf item should be removed.
   CloseAppWindow(app_window1);
   windows = app_service_proxy_->InstanceRegistry().GetWindows(app->id());
+  EXPECT_EQ(0u, windows.size());
+}
+
+class AppServiceAppWindowWebAppBrowserTest
+    : public AppServiceAppWindowBrowserTest {
+ protected:
+  // AppServiceAppWindowBrowserTest:
+  void SetUpOnMainThread() override {
+    AppServiceAppWindowBrowserTest::SetUpOnMainThread();
+
+    https_server_.AddDefaultHandlers(GetChromeTestDataDir());
+    ASSERT_TRUE(https_server_.Start());
+  }
+
+  // |SetUpWebApp()| must be called after |SetUpOnMainThread()| to make sure
+  // the Network Service process has been setup properly.
+  std::string CreateWebApp() const {
+    auto web_app_info = std::make_unique<WebApplicationInfo>();
+    web_app_info->app_url = GetAppURL();
+    web_app_info->scope = GetAppURL().GetWithoutFilename();
+
+    std::string app_id =
+        web_app::InstallWebApp(browser()->profile(), std::move(web_app_info));
+    content::TestNavigationObserver navigation_observer(GetAppURL());
+    navigation_observer.StartWatchingNewWebContents();
+    // Browser* app_browser_ = nullptr;
+    web_app::LaunchWebAppBrowser(browser()->profile(), app_id);
+    navigation_observer.WaitForNavigationFinished();
+
+    return app_id;
+  }
+
+  GURL GetAppURL() const {
+    return https_server_.GetURL("app.com", "/ssl/google.html");
+  }
+
+  // For mocking a secure site.
+  net::EmbeddedTestServer https_server_;
+};
+
+// Test that we have the correct instance for Web apps.
+IN_PROC_BROWSER_TEST_F(AppServiceAppWindowWebAppBrowserTest, WebAppsWindow) {
+  std::string app_id = CreateWebApp();
+
+  auto windows = app_service_proxy_->InstanceRegistry().GetWindows(app_id);
+  EXPECT_EQ(1u, windows.size());
+  aura::Window* window = *windows.begin();
+  apps::InstanceState latest_state = apps::InstanceState::kUnknown;
+  app_service_proxy_->InstanceRegistry().ForOneInstance(
+      window, [&app_id, &latest_state](const apps::InstanceUpdate& inner) {
+        if (inner.AppId() == app_id) {
+          latest_state = inner.State();
+        }
+      });
+
+  EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning |
+                apps::InstanceState::kActive | apps::InstanceState::kVisible,
+            latest_state);
+
+  const ash::ShelfItem& item = GetLastLauncherItem();
+  // Since it is already active, clicking it should minimize.
+  SelectItem(item.id);
+  latest_state = apps::InstanceState::kUnknown;
+  app_service_proxy_->InstanceRegistry().ForOneInstance(
+      window, [&app_id, &latest_state](const apps::InstanceUpdate& inner) {
+        if (inner.AppId() == app_id) {
+          latest_state = inner.State();
+        }
+      });
+  EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning,
+            latest_state);
+
+  // Click the item again to activate the app.
+  SelectItem(item.id);
+  latest_state = apps::InstanceState::kUnknown;
+  app_service_proxy_->InstanceRegistry().ForOneInstance(
+      window, [&app_id, &latest_state](const apps::InstanceUpdate& inner) {
+        if (inner.AppId() == app_id) {
+          latest_state = inner.State();
+        }
+      });
+  EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning |
+                apps::InstanceState::kActive | apps::InstanceState::kVisible,
+            latest_state);
+
+  controller_->Close(item.id);
+  // Make sure that the window is closed.
+  base::RunLoop().RunUntilIdle();
+  windows = app_service_proxy_->InstanceRegistry().GetWindows(app_id);
   EXPECT_EQ(0u, windows.size());
 }
