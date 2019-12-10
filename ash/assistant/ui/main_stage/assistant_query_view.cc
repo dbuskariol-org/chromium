5c298ed87b1419ea707b767034a1314b1c739ff8
Jeroen Dhollander <jeroendh@google.com>
2019-12-10 01:10:11 +0000

    Fix aliasing issues in Assistant UI on veyron-speedy board.
    The text in the Assistant UI is drawn with anti-aliasing. Since the
    background info isn't available when drawing text on a layer, it can
    cause aliasing issues.
    
    To fix this we set the label's background to solid white.
    
    Bug: chromium:1016006
    Test: manual
    Change-Id: Ib9fb3db78d30964dd45cc9d665f379044751d177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959345
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723188}

--- ash/assistant/ui/main_stage/assistant_query_view.cc
+++ ash/assistant/ui/main_stage/assistant_query_view.cc
@@ -14,6 +14,7 @@
 #include "net/base/escape.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/background.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace ash {
@@ -83,6 +84,7 @@ void AssistantQueryView::InitLayout() {
   label_->SetAutoColorReadabilityEnabled(false);
   label_->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_CENTER);
   label_->SetLineHeight(kLineHeightDip);
+  label_->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
   AddChildView(label_);
 }
 
