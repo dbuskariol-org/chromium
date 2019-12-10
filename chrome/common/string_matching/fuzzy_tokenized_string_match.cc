dbuskariol-org/chromium

commit 2b2ea3c09b286fab66bb920d481f74bb32136bd6

[cros search service] Move shared string matching functions to //chrome
We are implementing a local search service using approximate string
matching for query search. Currently the approximate string matching
algorithm is implemented as FuzzyTokenizedStringMatch under

chrome/browser/ui/app_list/search/search_utils/

and we will need to migrate this function to a location that can be used
by the local search service (under chrome/services/).

This cl moves FuzzyTokenizedStringMatch, TokenizedString and all other
related string matching functions to //chrome/common/string_matching so
that they can be shared by current clients and also future local search
service.

Design doc for local search service: go/cros-search-service

Bug: 1018613
Change-Id: If5648bf3046d52dd92ab5479dd94916552a99786
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939128
Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
Reviewed-by: Colin Blundell <blundell@chromium.org>
Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
Commit-Queue: Jia Meng <jiameng@chromium.org>
Cr-Commit-Position: refs/heads/master@{#721300}

chrome/common/string_matching/fuzzy_tokenized_string_match.cc
@@ -2,33 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/app_list/search/search_utils/fuzzy_tokenized_string_match.h"
-#include <pthread.h>
+#include "chrome/common/string_matching/fuzzy_tokenized_string_match.h"
 
 #include <algorithm>
 #include <iterator>
 
-#include "ash/public/cpp/app_list/app_list_features.h"
 #include "base/i18n/case_conversion.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/ui/app_list/search/search_utils/sequence_matcher.h"
+#include "chrome/common/string_matching/sequence_matcher.h"
 
-namespace app_list {
+namespace string_matching {
 
 namespace {
-constexpr bool kDefaultUseWeightedRatio = true;
-constexpr double kDefaultRelevanceThreshold = 0.3;
 constexpr double kMinScore = 0.0;
 constexpr double kMaxScore = 1.0;
 constexpr double kFirstCharacterMatchPenalty = 0.2;
 constexpr double kPrefixMatchPenalty = 0.1;
-constexpr double kPartialMatchPenaltyRate = 0.9;
 
 // Returns sorted tokens from a TokenizedString.
-std::vector<base::string16> ProcessAndSort(const ash::TokenizedString& text) {
+std::vector<base::string16> ProcessAndSort(const TokenizedString& text) {
   std::vector<base::string16> result;
   for (const auto& token : text.tokens()) {
     result.emplace_back(token);
@@ -41,8 +36,8 @@ std::vector<base::string16> ProcessAndSort(const ash::TokenizedString& text) {
 namespace internal {
 // Check if the query only contains first characters of the text,
 // e.g. "coc" is a match of "Clash of Clan". Range of the score is [0, 1].
-double FirstCharacterMatch(const ash::TokenizedString& query,
-                           const ash::TokenizedString& text) {
+double FirstCharacterMatch(const TokenizedString& query,
+                           const TokenizedString& text) {
   const base::string16 query_lower = base::i18n::ToLower(query.text());
   size_t query_index = 0;
   for (size_t text_index = 0; text_index < text.tokens().size(); text_index++) {
@@ -63,8 +58,7 @@ double FirstCharacterMatch(const ash::TokenizedString& query,
 
 // Check if tokens of query are prefixes of text's tokens. Range of score is
 // [0, 1].
-double PrefixMatch(const ash::TokenizedString& query,
-                   const ash::TokenizedString& text) {
+double PrefixMatch(const TokenizedString& query, const TokenizedString& text) {
   const std::vector<base::string16> query_tokens(query.tokens());
   const std::vector<base::string16> text_tokens(text.tokens());
   double match_score = kMaxScore;
@@ -98,9 +92,11 @@ FuzzyTokenizedStringMatch::~FuzzyTokenizedStringMatch() {}
 FuzzyTokenizedStringMatch::FuzzyTokenizedStringMatch() {}
 
 double FuzzyTokenizedStringMatch::TokenSetRatio(
-    const ash::TokenizedString& query,
-    const ash::TokenizedString& text,
-    bool partial) {
+    const TokenizedString& query,
+    const TokenizedString& text,
+    bool partial,
+    double partial_match_penalty_rate,
+    bool use_edit_distance) {
   std::set<base::string16> query_token(query.tokens().begin(),
                                        query.tokens().end());
   std::set<base::string16> text_token(text.tokens().begin(),
@@ -136,34 +132,47 @@ double FuzzyTokenizedStringMatch::TokenSetRatio(
                  base::JoinString(text_diff_query, base::UTF8ToUTF16(" "))});
 
   if (partial) {
-    return std::max({PartialRatio(intersection_string, query_rewritten),
-                     PartialRatio(intersection_string, text_rewritten),
-                     PartialRatio(query_rewritten, text_rewritten)});
+    return std::max(
+        {PartialRatio(intersection_string, query_rewritten,
+                      partial_match_penalty_rate, use_edit_distance),
+         PartialRatio(intersection_string, text_rewritten,
+                      partial_match_penalty_rate, use_edit_distance),
+         PartialRatio(query_rewritten, text_rewritten,
+                      partial_match_penalty_rate, use_edit_distance)});
   }
 
   return std::max(
-      {SequenceMatcher(intersection_string, query_rewritten).Ratio(),
-       SequenceMatcher(intersection_string, text_rewritten).Ratio(),
-       SequenceMatcher(query_rewritten, text_rewritten).Ratio()});
+      {SequenceMatcher(intersection_string, query_rewritten, use_edit_distance)
+           .Ratio(),
+       SequenceMatcher(intersection_string, text_rewritten, use_edit_distance)
+           .Ratio(),
+       SequenceMatcher(query_rewritten, text_rewritten, use_edit_distance)
+           .Ratio()});
 }
 
 double FuzzyTokenizedStringMatch::TokenSortRatio(
-    const ash::TokenizedString& query,
-    const ash::TokenizedString& text,
-    bool partial) {
+    const TokenizedString& query,
+    const TokenizedString& text,
+    bool partial,
+    double partial_match_penalty_rate,
+    bool use_edit_distance) {
   const base::string16 query_sorted =
       base::JoinString(ProcessAndSort(query), base::UTF8ToUTF16(" "));
   const base::string16 text_sorted =
       base::JoinString(ProcessAndSort(text), base::UTF8ToUTF16(" "));
 
   if (partial) {
-    return PartialRatio(query_sorted, text_sorted);
+    return PartialRatio(query_sorted, text_sorted, partial_match_penalty_rate,
+                        use_edit_distance);
   }
-  return SequenceMatcher(query_sorted, text_sorted).Ratio();
+  return SequenceMatcher(query_sorted, text_sorted, use_edit_distance).Ratio();
 }
 
-double FuzzyTokenizedStringMatch::PartialRatio(const base::string16& query,
-                                               const base::string16& text) {
+double FuzzyTokenizedStringMatch::PartialRatio(
+    const base::string16& query,
+    const base::string16& text,
+    double partial_match_penalty_rate,
+    bool use_edit_distance) {
   if (query.empty() || text.empty()) {
     return kMinScore;
   }
@@ -176,7 +185,7 @@ double FuzzyTokenizedStringMatch::PartialRatio(const base::string16& query,
   }
 
   const auto matching_blocks =
-      SequenceMatcher(shorter, longer).GetMatchingBlocks();
+      SequenceMatcher(shorter, longer, use_edit_distance).GetMatchingBlocks();
   double partial_ratio = 0;
 
   for (const auto& block : matching_blocks) {
@@ -186,10 +195,6 @@ double FuzzyTokenizedStringMatch::PartialRatio(const base::string16& query,
             : 0;
 
     // Penalizes the match if it is not close to the beginning of a token.
-    const double partial_match_penalty_rate =
-        base::GetFieldTrialParamByFeatureAsDouble(
-            app_list_features::kEnableFuzzyAppSearch,
-            "paritial_match_penalty_rate", kPartialMatchPenaltyRate);
     int current = long_start - 1;
     while (current >= 0 &&
            !base::EqualsCaseInsensitiveASCII(longer.substr(current, 1),
@@ -202,7 +207,8 @@ double FuzzyTokenizedStringMatch::PartialRatio(const base::string16& query,
     // pair. Improve this to reduce latency.
     partial_ratio = std::max(
         partial_ratio,
-        SequenceMatcher(shorter, longer.substr(long_start, shorter.size()))
+        SequenceMatcher(shorter, longer.substr(long_start, shorter.size()),
+                        use_edit_distance)
                 .Ratio() *
             penalty);
 
@@ -214,8 +220,10 @@ double FuzzyTokenizedStringMatch::PartialRatio(const base::string16& query,
 }
 
 double FuzzyTokenizedStringMatch::WeightedRatio(
-    const ash::TokenizedString& query,
-    const ash::TokenizedString& text) {
+    const TokenizedString& query,
+    const TokenizedString& text,
+    double partial_match_penalty_rate,
+    bool use_edit_distance) {
   const double unbase_scale = 0.95;
   // Since query.text() and text.text() is not normalized, we use query.tokens()
   // and text.tokens() instead.
@@ -224,7 +232,8 @@ double FuzzyTokenizedStringMatch::WeightedRatio(
   const base::string16 text_normalized(
       base::JoinString(text.tokens(), base::UTF8ToUTF16(" ")));
   double weighted_ratio =
-      SequenceMatcher(query_normalized, text_normalized).Ratio();
+      SequenceMatcher(query_normalized, text_normalized, use_edit_distance)
+          .Ratio();
   const double length_ratio =
       static_cast<double>(
           std::max(query_normalized.size(), text_normalized.size())) /
@@ -238,31 +247,42 @@ double FuzzyTokenizedStringMatch::WeightedRatio(
     // If one string is much much shorter than the other, set |partial_scale| to
     // be 0.6, otherwise set it to be 0.9.
     partial_scale = length_ratio > 8 ? 0.6 : 0.9;
-    weighted_ratio = std::max(
-        weighted_ratio,
-        PartialRatio(query_normalized, text_normalized) * partial_scale);
+    weighted_ratio =
+        std::max(weighted_ratio,
+                 PartialRatio(query_normalized, text_normalized,
+                              partial_match_penalty_rate, use_edit_distance) *
+                     partial_scale);
   }
-  weighted_ratio = std::max(
-      weighted_ratio, TokenSortRatio(query, text, use_partial /*partial*/) *
-                          unbase_scale * partial_scale);
-  weighted_ratio = std::max(
-      weighted_ratio, TokenSetRatio(query, text, use_partial /*partial*/) *
-                          unbase_scale * partial_scale);
+  weighted_ratio =
+      std::max(weighted_ratio,
+               TokenSortRatio(query, text, use_partial /*partial*/,
+                              partial_match_penalty_rate, use_edit_distance) *
+                   unbase_scale * partial_scale);
+  weighted_ratio =
+      std::max(weighted_ratio,
+               TokenSetRatio(query, text, use_partial /*partial*/,
+                             partial_match_penalty_rate, use_edit_distance) *
+                   unbase_scale * partial_scale);
   return weighted_ratio;
 }
 
-double FuzzyTokenizedStringMatch::PrefixMatcher(
-    const ash::TokenizedString& query,
-    const ash::TokenizedString& text) {
+double FuzzyTokenizedStringMatch::PrefixMatcher(const TokenizedString& query,
+                                                const TokenizedString& text) {
   return std::max(internal::PrefixMatch(query, text),
                   internal::FirstCharacterMatch(query, text));
 }
 
-bool FuzzyTokenizedStringMatch::IsRelevant(const ash::TokenizedString& query,
-                                           const ash::TokenizedString& text) {
+bool FuzzyTokenizedStringMatch::IsRelevant(const TokenizedString& query,
+                                           const TokenizedString& text,
+                                           double relevance_threshold,
+                                           bool use_prefix_only,
+                                           bool use_weighted_ratio,
+                                           bool use_edit_distance,
+                                           double partial_match_penalty_rate) {
   // Find |hits_| using SequenceMatcher on original query and text.
   for (const auto& match :
-       SequenceMatcher(query.text(), text.text()).GetMatchingBlocks()) {
+       SequenceMatcher(query.text(), text.text(), use_edit_distance)
+           .GetMatchingBlocks()) {
     if (match.length > 0) {
       hits_.push_back(gfx::Range(match.pos_second_string,
                                  match.pos_second_string + match.length));
@@ -274,45 +294,43 @@ bool FuzzyTokenizedStringMatch::IsRelevant(const ash::TokenizedString& query,
     return false;
   }
 
-  const double relevance_threshold = base::GetFieldTrialParamByFeatureAsDouble(
-      app_list_features::kEnableFuzzyAppSearch, "relevance_threshold",
-      kDefaultRelevanceThreshold);
   const double prefix_score = PrefixMatcher(query, text);
 
-  if (base::GetFieldTrialParamByFeatureAsBool(
-          app_list_features::kEnableFuzzyAppSearch, "use_prefix_only", false) &&
-      prefix_score >= relevance_threshold) {
+  if (use_prefix_only && prefix_score >= relevance_threshold) {
     // If the prefix score is already higher than |relevance_threshold|, use
     // prefix score as final score.
     relevance_ = prefix_score;
     return true;
   }
 
-  const bool use_weighted_ratio = base::GetFieldTrialParamByFeatureAsBool(
-      app_list_features::kEnableFuzzyAppSearch, "use_weighted_ratio",
-      kDefaultUseWeightedRatio);
-
   if (use_weighted_ratio) {
     // If WeightedRatio is used, |relevance_| is the average of WeightedRatio
     // and PrefixMatcher scores.
-    relevance_ = (WeightedRatio(query, text) + prefix_score) / 2;
+    relevance_ = (WeightedRatio(query, text, partial_match_penalty_rate,
+                                use_edit_distance) +
+                  prefix_score) /
+                 2;
   } else {
     // Use simple algorithm to calculate match ratio.
     double partial_match = 0.0;
     for (const auto& query_token : query.tokens()) {
       for (const auto& text_token : text.tokens()) {
-        partial_match = std::max(
-            partial_match, SequenceMatcher(query_token, text_token).Ratio());
+        partial_match =
+            std::max(partial_match,
+                     SequenceMatcher(query_token, text_token, use_edit_distance)
+                         .Ratio());
       }
     }
     const double partial_scale = 0.9;
-    relevance_ = (std::max(SequenceMatcher(query.text(), text.text()).Ratio(),
-                           partial_match * partial_scale) +
-                  prefix_score) /
-                 2;
+    relevance_ =
+        (std::max(SequenceMatcher(query.text(), text.text(), use_edit_distance)
+                      .Ratio(),
+                  partial_match * partial_scale) +
+         prefix_score) /
+        2;
   }
 
   return relevance_ >= relevance_threshold;
 }
 
-}  // namespace app_list
+}  // namespace string_matching

ash/app_list/model/search/search_result.cc
@@ -7,8 +7,6 @@
 #include <map>
 
 #include "ash/app_list/model/search/search_result_observer.h"
-#include "ash/public/cpp/app_list/tokenized_string.h"
-#include "ash/public/cpp/app_list/tokenized_string_match.h"
 #include "ui/base/models/menu_model.h"
 
 namespace ash {

ash/public/cpp/BUILD.gn
@@ -38,14 +38,6 @@ component("cpp") {
     "app_list/app_list_types.cc",
     "app_list/app_list_types.h",
     "app_list/internal_app_id_constants.h",
-    "app_list/term_break_iterator.cc",
-    "app_list/term_break_iterator.h",
-    "app_list/tokenized_string.cc",
-    "app_list/tokenized_string.h",
-    "app_list/tokenized_string_char_iterator.cc",
-    "app_list/tokenized_string_char_iterator.h",
-    "app_list/tokenized_string_match.cc",
-    "app_list/tokenized_string_match.h",
     "app_menu_constants.h",
     "app_types.h",
     "arc_app_id_provider.cc",

chrome/browser/chromeos/BUILD.gn
@@ -82,6 +82,7 @@ source_set("chromeos") {
     "//chrome/browser/web_applications/extensions:extensions",
     "//chrome/common",
     "//chrome/common/extensions/api",
+    "//chrome/common/string_matching",
     "//chrome/services/app_service:lib",
     "//chrome/services/app_service/public/cpp:app_update",
     "//chrome/services/file_util/public/cpp",

chrome/browser/chromeos/launcher_search_provider/launcher_search_provider_service.cc
@@ -8,13 +8,13 @@
 
 #include <utility>
 
-#include "ash/public/cpp/app_list/tokenized_string.h"
-#include "ash/public/cpp/app_list/tokenized_string_match.h"
 #include "base/numerics/ranges.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/launcher_search_provider/launcher_search_provider_service_factory.h"
 #include "chrome/browser/ui/app_list/search/launcher_search/launcher_search_provider.h"
 #include "chrome/browser/ui/app_list/search/launcher_search/launcher_search_result.h"
+#include "chrome/common/string_matching/tokenized_string.h"
+#include "chrome/common/string_matching/tokenized_string_match.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -135,9 +135,9 @@ void Service::SetSearchResults(
     // set the title tags (highlighting which parts of the title matched the
     // search query).
     const base::string16 title = base::UTF8ToUTF16(result.title);
-    ash::TokenizedString tokenized_title(title);
-    ash::TokenizedStringMatch match;
-    ash::TokenizedString tokenized_query(base::UTF8ToUTF16(query_));
+    string_matching::TokenizedString tokenized_title(title);
+    string_matching::TokenizedStringMatch match;
+    string_matching::TokenizedString tokenized_query(base::UTF8ToUTF16(query_));
     if (!match.Calculate(tokenized_query, tokenized_title))
       continue;
 

chrome/browser/ui/BUILD.gn
@@ -361,6 +361,7 @@ jumbo_static_library("ui") {
   # have the same dependencies. Once browser_ui is untangled from
   # browser, then we can clean up these dependencies.
   public_deps = [
+    "//chrome/common/string_matching",
     "//components/dom_distiller/core",
     "//components/safe_browsing:buildflags",
     "//components/sync",
@@ -3693,10 +3694,6 @@ jumbo_static_library("ui") {
       "app_list/search/search_result_ranker/search_ranking_event_logger.h",
       "app_list/search/search_result_ranker/search_result_ranker.cc",
       "app_list/search/search_result_ranker/search_result_ranker.h",
-      "app_list/search/search_utils/fuzzy_tokenized_string_match.cc",
-      "app_list/search/search_utils/fuzzy_tokenized_string_match.h",
-      "app_list/search/search_utils/sequence_matcher.cc",
-      "app_list/search/search_utils/sequence_matcher.h",
       "app_list/search/settings_shortcut/settings_shortcut_metadata.cc",
       "app_list/search/settings_shortcut/settings_shortcut_metadata.h",
       "app_list/search/settings_shortcut/settings_shortcut_provider.cc",

chrome/browser/ui/app_list/search/app_search_provider.cc
@@ -16,8 +16,6 @@
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/app_list/internal_app_id_constants.h"
-#include "ash/public/cpp/app_list/tokenized_string.h"
-#include "ash/public/cpp/app_list/tokenized_string_match.h"
 #include "base/bind.h"
 #include "base/callback_list.h"
 #include "base/macros.h"
@@ -41,7 +39,9 @@
 #include "chrome/browser/ui/app_list/search/app_service_app_result.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/app_search_result_ranker.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.h"
-#include "chrome/browser/ui/app_list/search/search_utils/fuzzy_tokenized_string_match.h"
+#include "chrome/common/string_matching/fuzzy_tokenized_string_match.h"
+#include "chrome/common/string_matching/tokenized_string.h"
+#include "chrome/common/string_matching/tokenized_string_match.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync_sessions/session_sync_service.h"
 
@@ -130,12 +130,13 @@ class AppSearchProvider::App {
     }
   };
 
-  ash::TokenizedString* GetTokenizedIndexedName() {
+  string_matching::TokenizedString* GetTokenizedIndexedName() {
     // Tokenizing a string is expensive. Don't pay the price for it at
     // construction of every App, but rather, only when needed (i.e. when the
     // query is not empty and cache the result.
     if (!tokenized_indexed_name_)
-      tokenized_indexed_name_ = std::make_unique<ash::TokenizedString>(name_);
+      tokenized_indexed_name_ =
+          std::make_unique<string_matching::TokenizedString>(name_);
     return tokenized_indexed_name_.get();
   }
 
@@ -147,16 +148,16 @@ class AppSearchProvider::App {
     return base::Time();
   }
 
-  bool MatchSearchableText(const ash::TokenizedString& query) {
+  bool MatchSearchableText(const string_matching::TokenizedString& query) {
     if (searchable_text_.empty())
       return false;
     if (tokenized_indexed_searchable_text_.empty()) {
       for (const base::string16& curr_text : searchable_text_) {
         tokenized_indexed_searchable_text_.push_back(
-            std::make_unique<ash::TokenizedString>(curr_text));
+            std::make_unique<string_matching::TokenizedString>(curr_text));
       }
     }
-    ash::TokenizedStringMatch match;
+    string_matching::TokenizedStringMatch match;
     for (auto& curr_text : tokenized_indexed_searchable_text_) {
       match.Calculate(query, *curr_text);
       if (match.relevance() > relevance_threshold())
@@ -196,8 +197,8 @@ class AppSearchProvider::App {
 
  private:
   AppSearchProvider::DataSource* data_source_;
-  std::unique_ptr<ash::TokenizedString> tokenized_indexed_name_;
-  std::vector<std::unique_ptr<ash::TokenizedString>>
+  std::unique_ptr<string_matching::TokenizedString> tokenized_indexed_name_;
+  std::vector<std::unique_ptr<string_matching::TokenizedString>>
       tokenized_indexed_searchable_text_;
   const std::string id_;
   const base::string16 name_;
@@ -497,14 +498,15 @@ void AppSearchProvider::UpdateQueriedResults() {
   const size_t apps_size = apps_.size();
   new_results.reserve(apps_size);
 
-  const ash::TokenizedString query_terms(query_);
+  const string_matching::TokenizedString query_terms(query_);
   for (auto& app : apps_) {
     if (!app->searchable())
       continue;
 
-    ash::TokenizedString* indexed_name = app->GetTokenizedIndexedName();
+    string_matching::TokenizedString* indexed_name =
+        app->GetTokenizedIndexedName();
     if (!app_list_features::IsFuzzyAppSearchEnabled()) {
-      ash::TokenizedStringMatch match;
+      string_matching::TokenizedStringMatch match;
       if (match.Calculate(query_terms, *indexed_name)) {
         // Exact matches should be shown even if the threshold isn't reached,
         // e.g. due to a localized name being particularly short.
@@ -521,8 +523,28 @@ void AppSearchProvider::UpdateQueriedResults() {
       result->UpdateFromMatch(*indexed_name, match);
       MaybeAddResult(&new_results, std::move(result), &seen_or_filtered_apps);
     } else {
-      FuzzyTokenizedStringMatch match;
-      if (match.IsRelevant(query_terms, *indexed_name) ||
+      string_matching::FuzzyTokenizedStringMatch match;
+
+      // TODO(crbug.com/1018613): consolidate finch parameters.
+      const bool use_prefix_only = base::GetFieldTrialParamByFeatureAsBool(
+          app_list_features::kEnableFuzzyAppSearch, "use_prefix_only", false);
+      const bool use_weighted_ratio = base::GetFieldTrialParamByFeatureAsBool(
+          app_list_features::kEnableFuzzyAppSearch, "use_weighted_ratio", true);
+      const bool use_edit_distance = base::GetFieldTrialParamByFeatureAsBool(
+          app_list_features::kEnableFuzzyAppSearch, "use_edit_distance", false);
+
+      const double relevance_threshold =
+          base::GetFieldTrialParamByFeatureAsDouble(
+              app_list_features::kEnableFuzzyAppSearch, "relevance_threshold",
+              0.3);
+      const double partial_match_penalty_rate =
+          base::GetFieldTrialParamByFeatureAsDouble(
+              app_list_features::kEnableFuzzyAppSearch,
+              "partial_match_penalty_rate", 0.9);
+
+      if (match.IsRelevant(query_terms, *indexed_name, relevance_threshold,
+                           use_prefix_only, use_weighted_ratio,
+                           use_edit_distance, partial_match_penalty_rate) ||
           app->MatchSearchableText(query_terms) ||
           base::EqualsCaseInsensitiveASCII(query_, app->name())) {
         std::unique_ptr<AppResult> result = app->data_source()->CreateResult(

chrome/browser/ui/app_list/search/chrome_search_result.cc
@@ -6,10 +6,10 @@
 
 #include <map>
 
-#include "ash/public/cpp/app_list/tokenized_string.h"
-#include "ash/public/cpp/app_list/tokenized_string_match.h"
 #include "base/containers/adapters.h"
 #include "chrome/browser/ui/app_list/app_context_menu.h"
+#include "chrome/common/string_matching/tokenized_string.h"
+#include "chrome/common/string_matching/tokenized_string_match.h"
 
 ChromeSearchResult::ChromeSearchResult()
     : metadata_(std::make_unique<ash::SearchResultMetadata>()) {}
@@ -160,9 +160,9 @@ void ChromeSearchResult::OnVisibilityChanged(bool visibility) {
 }
 
 void ChromeSearchResult::UpdateFromMatch(
-    const ash::TokenizedString& title,
-    const ash::TokenizedStringMatch& match) {
-  const ash::TokenizedStringMatch::Hits& hits = match.hits();
+    const string_matching::TokenizedString& title,
+    const string_matching::TokenizedStringMatch& match) {
+  const string_matching::TokenizedStringMatch::Hits& hits = match.hits();
 
   Tags tags;
   tags.reserve(hits.size());

commit 8273f4d3130e06fd8b6bef87b07c936304b971d9

[cros search service]: Include <cmath> for std::pow()
IWYU. Follow up to commit 2b2ea3c09b ("[cros search service] Move shared
string matching functions to //chrome"), which broke the libstdc++ build:

    ../../chrome/common/string_matching/fuzzy_tokenized_string_match.cc:199:14: error: no member named 'pow' in namespace 'std'
            std::pow(partial_match_penalty_rate, long_start - current - 1);
            ~~~~~^

Bug: 957519
Change-Id: I66f61cb4f93cfa0bfa3d1b00ba391ddd8f31a7fb
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960310
Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Reviewed-by: Jia Meng <jiameng@chromium.org>
Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Cr-Commit-Position: refs/heads/master@{#723499}

chrome/common/string_matching/fuzzy_tokenized_string_match.cc
@@ -5,6 +5,7 @@
 #include "chrome/common/string_matching/fuzzy_tokenized_string_match.h"
 
 #include <algorithm>
+#include <cmath>
 #include <iterator>
 
 #include "base/i18n/case_conversion.h"