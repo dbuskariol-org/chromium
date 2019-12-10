a128fb40ddb2e4346e8d99b4fe98d740626a12c2
Yunchao He <yunchao.he@intel.com>
2019-12-10 19:13:45 +0000

    [WebGPU] Enable the support for readonly storage buffer enum
    Readonly storage buffer is fully implemented in Dawn. We should
    enable the support for related enum.
    
    BUG=dawn:180
    
    Change-Id: I13ba9b61757b8c1e7f612c93f34947c510663cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957744
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Yunchao He <yunchao.he@intel.com>
    Cr-Commit-Position: refs/heads/master@{#723432}

--- third_party/blink/renderer/modules/webgpu/dawn_conversions.cc
+++ third_party/blink/renderer/modules/webgpu/dawn_conversions.cc
@@ -23,6 +23,9 @@ WGPUBindingType AsDawnEnum<WGPUBindingType>(const WTF::String& webgpu_enum) {
   if (webgpu_enum == "storage-buffer") {
     return WGPUBindingType_StorageBuffer;
   }
+  if (webgpu_enum == "readonly-storage-buffer") {
+    return WGPUBindingType_ReadonlyStorageBuffer;
+  }
   if (webgpu_enum == "sampler") {
     return WGPUBindingType_Sampler;
   }
