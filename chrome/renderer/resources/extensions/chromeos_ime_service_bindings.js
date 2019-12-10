3b0537d208b5cd1b9a9210f290be6abca20ad7b6
Maggie Cai <mxcai@chromium.org>
2019-12-10 06:45:00 +0000

    Revert "Convert InputEngineManager client to use BrowserInterfaceBroker"
    This reverts commit 328e5c367d1b2f11a8082a58201b64bd22fa23ab.
    
    Reason for revert: compile failed on https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-google-rel/10375?blamelist=1#blamelist-tab, https://ci.chromium.org/p/chrome/builders/ci/chromeos-eve-google-rel/3538 and https://ci.chromium.org/p/chrome/builders/ci/chromeos-betty-google-rel/16026?blamelist=1#blamelist-tab
    
    Original change's description:
    > Convert InputEngineManager client to use BrowserInterfaceBroker
    > 
    > This CL converts InputEngineManager mojom interface clients for
    > extensions in chrome to use BrowserInterfaceBroker.
    > 
    > Bug: 936482
    > Change-Id: Iecc57f2a8c15a9523f08e4ab49156273a2ba4677
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952623
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#723261}
    
    TBR=rockot@google.com,sammc@chromium.org,oksamyt@chromium.org,myid.shin@igalia.com
    
    Change-Id: I28cc7789bbedb8cb7dc9c46b60d6620105379ba2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 936482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959240
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723266}

--- chrome/renderer/resources/extensions/chromeos_ime_service_bindings.js
+++ chrome/renderer/resources/extensions/chromeos_ime_service_bindings.js
@@ -281,6 +281,6 @@ class ImeService {
 (function() {
   let ptr = new chromeos.ime.mojom.InputEngineManagerPtr;
   Mojo.bindInterface(chromeos.ime.mojom.InputEngineManager.name,
-                     mojo.makeRequest(ptr).handle, "context", true);
+                     mojo.makeRequest(ptr).handle);
   exports.$set('returnValue', new ImeService(ptr));
 })();
