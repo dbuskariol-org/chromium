4364277ec58f59248fc97ebf1239988eb0dd8c79
Tim Song <tengs@chromium.org>
2019-12-11 21:36:28 +0000

    Address missed CL comments for chrome.enterprisePrivate.getDeviceInfo() CL.
    Original CL was at:
    https://chromium-review.googlesource.com/c/chromium/src/+/1951561
    
    Change-Id: I8784ec61cec5834b17afe04ea8b1d474178af15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961211
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723985}

--- chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher_mac.mm
+++ chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher_mac.mm
@@ -47,13 +47,14 @@ std::string GetSerialNumber() {
 enterprise_reporting_private::SettingValue GetScreenlockSecured() {
   CFStringRef screen_saver_application = CFSTR("com.apple.screensaver");
   CFStringRef ask_for_password_key = CFSTR("askForPassword");
-  base::ScopedCFTypeRef<CFNumberRef> ask_for_password(
-      base::mac::CFCastStrict<CFNumberRef>(CFPreferencesCopyAppValue(
-          ask_for_password_key, screen_saver_application)));
+
+  base::ScopedCFTypeRef<CFTypeRef> ask_for_password(CFPreferencesCopyAppValue(
+      ask_for_password_key, screen_saver_application));
 
   int screen_lock_enabled = 0;
-  if (!ask_for_password || !CFNumberGetValue(ask_for_password, kCFNumberIntType,
-                                             &screen_lock_enabled)) {
+  if (!ask_for_password ||
+      !CFNumberGetValue(base::mac::CFCastStrict<CFNumberRef>(ask_for_password),
+                        kCFNumberIntType, &screen_lock_enabled)) {
     return enterprise_reporting_private::SETTING_VALUE_UNKNOWN;
   }
 
