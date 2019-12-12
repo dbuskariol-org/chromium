53528a068330d6af3c17c5a53c356dd3dd834c2d
Sammie Quon <sammiequon@google.com>
2019-12-12 18:51:07 +0000

    overview: Fix ARC apps sometimes not visible.
    This happens when ARC apps are minimized (via the launcher or otherwise),
    and then overview is started before the async minimized state gets sent
    out. So, ARC window enters overview as unminimized, which normally
    waits for overview animation before fading in |item_widget_|, but changes
    to minimized state before animation is done, so |item_widget_| is not
    faded in.
    
    This patch address by forcing |item_widget_| to visible when minimized
    state change.
    
    Test: manual
    Change-Id: Ic677d56c5f28bcb491242c441389995212f82e29
    Bug: 1030921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962989
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724277}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -1070,6 +1070,12 @@ void OverviewItem::OnPostWindowStateTypeChange(WindowState* window_state,
   if (!minimized)
     EnsureVisible();
 
+  // Ensures the item widget is visible. |item_widget_| opacity is set to 0.f
+  // and shown at either |SetBounds| or |OnStartingAnimationComplete| based on
+  // the minimized state. It's possible the minimized state changes in between
+  // for ARC apps, so just force show it here.
+  item_widget_->GetLayer()->SetOpacity(1.f);
+
   overview_grid_->PositionWindows(/*animate=*/false);
   // If |activate_on_unminimized_| is true, then this code path is from an
   // unminimizing an ARC app async from |OverviewSession::SelectWindow|.
