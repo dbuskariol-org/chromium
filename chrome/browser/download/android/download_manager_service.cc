dbuskariol-org/chromium

commit b530a38682cb9d41735ce2bb67c4cb75f2169044

Remove NotificationService usage from DownloadManagerService
Bug: 268984
Change-Id: I36fc3bff7b2805ac6d950b2b6a56179169c05d06
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954533
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: David Trainor <dtrainor@chromium.org>
Cr-Commit-Position: refs/heads/master@{#723231}

chrome/browser/download/android/download_manager_service.cc
@@ -21,7 +21,6 @@
 #include "chrome/browser/android/chrome_feature_list.h"
 #include "chrome/browser/android/feature_utilities.h"
 #include "chrome/browser/android/profile_key_startup_accessor.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/download/android/download_controller.h"
 #include "chrome/browser/download/android/download_startup_utils.h"
 #include "chrome/browser/download/android/download_utils.h"
@@ -43,7 +42,6 @@
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/download_item_utils.h"
 #include "content/public/browser/download_request_utils.h"
-#include "content/public/browser/notification_service.h"
 #include "third_party/blink/public/common/mime_util/mime_util.h"
 #include "url/origin.h"
 
@@ -203,36 +201,17 @@ void DownloadManagerService::Init(JNIEnv* env,
 }
 
 void DownloadManagerService::OnProfileAdded(JNIEnv* env, jobject obj) {
-  registrar_.Add(this, chrome::NOTIFICATION_PROFILE_CREATED,
-                 content::NotificationService::AllSources());
-  // Register coordinator for each available profile.
-  DownloadStartupUtils::EnsureDownloadSystemInitialized(
-      true /* is_full_browser_started */, false /* is_incognito */);
   Profile* profile =
       ProfileManager::GetActiveUserProfile()->GetOriginalProfile();
-  ResetCoordinatorIfNeeded(profile->GetProfileKey());
-  if (profile->HasOffTheRecordProfile()) {
-    DownloadStartupUtils::EnsureDownloadSystemInitialized(
-        true /* is_full_browser_started */, true /* is_incognito */);
-    ResetCoordinatorIfNeeded(
-        profile->GetOffTheRecordProfile()->GetProfileKey());
-  }
+  InitializeForProfile(profile);
+  observed_profiles_.Add(profile);
+  if (profile->HasOffTheRecordProfile())
+    InitializeForProfile(profile->GetOffTheRecordProfile());
 }
 
-void DownloadManagerService::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  switch (type) {
-    case chrome::NOTIFICATION_PROFILE_CREATED: {
-      Profile* profile = content::Source<Profile>(source).ptr();
-      DownloadStartupUtils::EnsureDownloadSystemInitialized(
-          true /* is_full_browser_started */, profile->IsOffTheRecord());
-      ResetCoordinatorIfNeeded(profile->GetProfileKey());
-    } break;
-    default:
-      NOTREACHED();
-  }
+void DownloadManagerService::OnOffTheRecordProfileCreated(
+    Profile* off_the_record) {
+  InitializeForProfile(off_the_record);
 }
 
 void DownloadManagerService::OpenDownload(download::DownloadItem* download,
@@ -781,6 +760,12 @@ void DownloadManagerService::CreateInterruptedDownloadForTest(
           download::DOWNLOAD_INTERRUPT_REASON_CRASH, false, false, false,
           base::Time(), false,
           std::vector<download::DownloadItem::ReceivedSlice>(), nullptr));
+}
+
+void DownloadManagerService::InitializeForProfile(Profile* profile) {
+  DownloadStartupUtils::EnsureDownloadSystemInitialized(
+      true /* is_full_browser_started */, profile->IsOffTheRecord());
+  ResetCoordinatorIfNeeded(profile->GetProfileKey());
 }
 
 // static

chrome/browser/download/android/download_manager_service.h
@@ -13,12 +13,13 @@
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/singleton.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/download/download_manager_utils.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_observer.h"
 #include "components/download/public/common/all_download_event_notifier.h"
 #include "components/download/public/common/in_progress_download_manager.h"
 #include "content/public/browser/download_manager.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 
 using base::android::JavaParamRef;
 
@@ -33,7 +34,7 @@ class SimpleDownloadManagerCoordinator;
 // Java object.
 class DownloadManagerService
     : public download::AllDownloadEventNotifier::Observer,
-      public content::NotificationObserver {
+      public ProfileObserver {
  public:
   static void CreateAutoResumptionHandler();
 
@@ -156,10 +157,8 @@ class DownloadManagerService
       download::SimpleDownloadManagerCoordinator* coordinator,
       download::DownloadItem* item) override;
 
-  // content::NotificationObserver methods.
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
+  // ProfileObserver:
+  void OnOffTheRecordProfileCreated(Profile* off_the_record) override;
 
   // Called by the java code to create and insert an interrupted download to
   // |in_progress_manager_| for testing purpose.
@@ -245,6 +244,8 @@ class DownloadManagerService
   download::SimpleDownloadManagerCoordinator* GetCoordinator(
       bool is_off_the_record);
 
+  void InitializeForProfile(Profile* profile);
+
   // Reference to the Java object.
   base::android::ScopedJavaGlobalRef<jobject> java_ref_;
 
@@ -280,8 +281,7 @@ class DownloadManagerService
 
   ResumeCallback resume_callback_for_testing_;
 
-  // The Registrar used to register for notifications.
-  content::NotificationRegistrar registrar_;
+  ScopedObserver<Profile, ProfileObserver> observed_profiles_{this};
 
   download::SimpleDownloadManagerCoordinator* original_coordinator_;
   download::SimpleDownloadManagerCoordinator* off_the_record_coordinator_;

commit 476e25ece65456c28dfcbcabc9175a7cf52b0a05

[Clank] Fix download manager initialization for incognito profiles.
Broken by b530a38682cb9d41735ce2bb67c4cb75f2169044 because
OnOffTheRecordProfileCreated is called just before the otr pointer is
set on the original profile.

The fix is to pass the actual profile to
EnsureDownloadSystemInitialized, rather than a pair of booleans.

Bug: 1033430
Change-Id: Id260291248261038b7b815b5acc54898d79ffdff
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965471
Reviewed-by: David Trainor <dtrainor@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#724663}

chrome/browser/download/android/download_manager_service.cc
@@ -193,10 +193,8 @@ void DownloadManagerService::Init(JNIEnv* env,
     OnProfileAdded(env, obj);
   } else {
     // In reduced mode, only non-incognito downloads should be loaded.
-    DownloadStartupUtils::EnsureDownloadSystemInitialized(
-        false /* is_full_browser_started */, false /* is_incognito */);
     ResetCoordinatorIfNeeded(
-        ProfileKeyStartupAccessor::GetInstance()->profile_key());
+        DownloadStartupUtils::EnsureDownloadSystemInitialized(nullptr));
   }
 }
 
@@ -763,9 +761,8 @@ void DownloadManagerService::CreateInterruptedDownloadForTest(
 }
 
 void DownloadManagerService::InitializeForProfile(Profile* profile) {
-  DownloadStartupUtils::EnsureDownloadSystemInitialized(
-      true /* is_full_browser_started */, profile->IsOffTheRecord());
-  ResetCoordinatorIfNeeded(profile->GetProfileKey());
+  ResetCoordinatorIfNeeded(
+      DownloadStartupUtils::EnsureDownloadSystemInitialized(profile));
 }
 
 // static

chrome/browser/download/android/download_startup_utils.cc
@@ -17,29 +17,27 @@ static void JNI_DownloadStartupUtils_EnsureDownloadSystemInitialized(
     JNIEnv* env,
     jboolean is_full_browser_started,
     jboolean is_incognito) {
-  DownloadStartupUtils::EnsureDownloadSystemInitialized(is_full_browser_started,
-                                                        is_incognito);
-}
-
-// static
-void DownloadStartupUtils::EnsureDownloadSystemInitialized(
-    bool is_full_browser_started,
-    bool is_incognito) {
   DCHECK(is_full_browser_started || !is_incognito)
       << "Incognito mode must load full browser.";
-  ProfileKey* profile_key = nullptr;
+  Profile* profile = nullptr;
   if (is_full_browser_started) {
-    Profile* profile =
-        ProfileManager::GetActiveUserProfile()->GetOriginalProfile();
+    profile = ProfileManager::GetActiveUserProfile();
     if (is_incognito) {
       if (profile->HasOffTheRecordProfile())
         profile = profile->GetOffTheRecordProfile();
       else
         return;
     }
-    profile_key = profile->GetProfileKey();
-  } else {
-    profile_key = ProfileKeyStartupAccessor::GetInstance()->profile_key();
   }
+  DownloadStartupUtils::EnsureDownloadSystemInitialized(profile);
+}
+
+// static
+ProfileKey* DownloadStartupUtils::EnsureDownloadSystemInitialized(
+    Profile* profile) {
+  ProfileKey* profile_key =
+      profile ? profile->GetProfileKey()
+              : ProfileKeyStartupAccessor::GetInstance()->profile_key();
   DownloadManagerUtils::GetInProgressDownloadManager(profile_key);
+  return profile_key;
 }

chrome/browser/download/android/download_startup_utils.h
@@ -5,13 +5,21 @@
 #ifndef CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_STARTUP_UTILS_H_
 #define CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_STARTUP_UTILS_H_
 
+#include "base/macros.h"
+
+class Profile;
+class ProfileKey;
+
 // Native side of DownloadStartupUtils.java.
 class DownloadStartupUtils {
  public:
   // Ensures that the download system is initialized for the targeted profile.
-  // If the corresponding profile is not created, this method will do nothing.
-  static void EnsureDownloadSystemInitialized(bool is_full_browser_started,
-                                              bool is_incognito);
+  // If |profile| is null, reduced mode will be assumed. The returned value is
+  // the ProfileKey that was used.
+  static ProfileKey* EnsureDownloadSystemInitialized(Profile* profile);
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(DownloadStartupUtils);
 };
 
 #endif  // CHROME_BROWSER_DOWNLOAD_ANDROID_DOWNLOAD_STARTUP_UTILS_H_