e618869173165d488f390753de04414bbb223d40
Sahel Sharify <sahel@chromium.org>
2019-12-13 16:20:19 +0000

    Disable OmniboxApiTest.OnInputEntered flaky test.
    TBR=blundell@chromium.org
    
    Bug: 1029568
    Change-Id: I188666ab353b1df6854b8efa4902133ec70894cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967551
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724657}

--- chrome/browser/extensions/api/omnibox/omnibox_api_interactive_test.cc
+++ chrome/browser/extensions/api/omnibox/omnibox_api_interactive_test.cc
@@ -206,8 +206,8 @@ IN_PROC_BROWSER_TEST_F(OmniboxApiTest, DISABLED_Basic) {
   }
   */
 }
-
-IN_PROC_BROWSER_TEST_F(OmniboxApiTest, OnInputEntered) {
+// Test is flaky: https://crbug.com/1029568
+IN_PROC_BROWSER_TEST_F(OmniboxApiTest, DISABLED_OnInputEntered) {
   ASSERT_TRUE(RunExtensionTest("omnibox")) << message_;
   Profile* profile = browser()->profile();
   search_test_utils::WaitForTemplateURLServiceToLoad(
