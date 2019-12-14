e150538c9d2ac5103cc6447b4d5d7866623061c5
Mikel Astiz <mastiz@chromium.org>
2019-12-14 08:41:49 +0000

    Revert "Temporarily disable GC of local sync cache GUIDs"
    This reverts commit 6a8855ea54353112dcd09c28867150e75deed131.
    
    Reason for revert: temporary measure rolling with M80 but
    should be avoided in future releases.
    
    Original change's description:
    > Temporarily disable GC of local sync cache GUIDs
    > 
    > Crashes have been reported on Beta and and the implementation has been
    > refactored since (base::Value refactoring) so it's safest to just
    > disable the garbage-collection for one milestone.
    > 
    > Bug: 1029673,989340
    > Change-Id: Id2e062ba302a7ffea970964170c492b513a52b73
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945812
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#720435}
    
    TBR=treib@chromium.org,mastiz@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1029673, 989340
    Change-Id: Ib688785be5d42f22bf9ff967a9d27c4f449df77e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967086
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724934}

--- components/sync_device_info/device_info_sync_bridge.cc
+++ components/sync_device_info/device_info_sync_bridge.cc
@@ -210,9 +210,8 @@ void DeviceInfoSyncBridge::OnSyncStarting(
     const DataTypeActivationRequest& request) {
   // Store the cache GUID, mainly in case MergeSyncData() is executed later.
   local_cache_guid_ = request.cache_guid;
-
-  // TODO(crbug.com/989340): Call GarbageCollectExpiredCacheGuids() here.
-
+  // Garbage-collect old local cache GUIDs, for privacy reasons.
+  device_info_prefs_->GarbageCollectExpiredCacheGuids();
   // Add the cache guid to the local prefs.
   device_info_prefs_->AddLocalCacheGuid(local_cache_guid_);
   // SyncMode determines the client name in GetLocalClientName().
