0302fbe3de8d0cf576bd060dda8ba62545ef3d72
Gauthier Ambard <gambard@chromium.org>
2019-12-16 08:50:33 +0000

    [iOS][web] Fix UserAgentType inheritance
    This CL fixes the copy constructor of the NavigationItem to take
    into account the UserAgent for inheritance.
    It also re-enable LoadURLWithParams which was failing because of it.
    
    Fixed: 1032651
    Change-Id: I0f36a4ff6e28dcb430dcbcc6010e92fbdd313695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965587
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725039}

--- ios/web/navigation/wk_based_navigation_manager_impl_unittest.mm
+++ ios/web/navigation/wk_based_navigation_manager_impl_unittest.mm
@@ -1006,7 +1006,7 @@ TEST_F(WKBasedNavigationManagerDetachedModeTest, LoadIfNecessary) {
 
 // Tests that LoadURLWithParams from detached mode restores backward history and
 // adds the new item at the end.
-TEST_F(WKBasedNavigationManagerDetachedModeTest, DISABLED_LoadURLWithParams) {
+TEST_F(WKBasedNavigationManagerDetachedModeTest, LoadURLWithParams) {
   manager_->DetachFromWebView();
   delegate_.RemoveWebView();
 
