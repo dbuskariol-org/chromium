f44ef663aa1a7f338fe811b82970fea5696c303c
Markus Handell <handellm@google.com>
2019-12-16 23:19:53 +0000

    MediaRecorder: Store ColorSpace information too.
    This change adds present ColorSpace information into the WebM files
    created by the MediaRecorder.
    
    Change-Id: I4d875a1bf9d581f221f29fe4140808750dfb73d3
    Bug: 1013590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893993
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725316}

--- third_party/blink/renderer/modules/mediarecorder/video_track_recorder.cc
+++ third_party/blink/renderer/modules/mediarecorder/video_track_recorder.cc
@@ -697,12 +697,16 @@ void VideoTrackRecorderPassthrough::HandleEncodedVideoFrame(
   }
   state_ = KeyFrameState::kKeyFrameReceivedOK;
 
+  base::Optional<gfx::ColorSpace> color_space;
+  if (encoded_frame->ColorSpace())
+    color_space = encoded_frame->ColorSpace()->ToGfxColorSpace();
   auto span = encoded_frame->Data();
   const char* span_begin = reinterpret_cast<const char*>(span.data());
   std::string data(span_begin, span_begin + span.size());
   media::WebmMuxer::VideoParameters params(encoded_frame->Resolution(),
                                            /*framerate=*/0.0f,
-                                           /*codec=*/encoded_frame->Codec());
+                                           /*codec=*/encoded_frame->Codec(),
+                                           color_space);
   callback_.Run(params, std::move(data), {}, estimated_capture_time,
                 encoded_frame->IsKeyFrame());
 }
