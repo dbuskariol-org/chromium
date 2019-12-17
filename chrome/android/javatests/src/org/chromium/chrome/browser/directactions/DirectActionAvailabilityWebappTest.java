3bc60643048f30b122b68480eda13d58580afb87
Mathias Carlen <mcarlen@chromium.org>
2019-12-17 11:29:57 +0000

    [Direct Actions] Disable test.
    Disable flaky test DirectActionAvailabilityWebappTest#testCoreDirectActionInWebappActivity.
    
    R=arbesser@google.com
    
    Bug: 1033006
    Change-Id: Ice0a39c7bcaa3ed56a56d53ae06be016a13b4529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970557
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725488}

--- chrome/android/javatests/src/org/chromium/chrome/browser/directactions/DirectActionAvailabilityWebappTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/directactions/DirectActionAvailabilityWebappTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -45,6 +46,7 @@ public class DirectActionAvailabilityWebappTest {
     @Test
     @MediumTest
     @Feature({"DirectActions"})
+    @DisabledTest(message = "crbug.com/1033006")
     public void testCoreDirectActionInWebappActivity() throws Exception {
         mWebAppActivityTestRule.startWebappActivity();
 
