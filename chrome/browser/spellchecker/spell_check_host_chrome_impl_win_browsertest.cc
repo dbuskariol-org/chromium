f5a23de30ff66c0c0f18a2d55e7aaf446c544ee5
Guillaume Jenkins <gujen@google.com>
2019-12-18 23:30:12 +0000

    [Spellcheck] Add tests for Windows hybrid spell checking
    Adds tests for the Hunspell fallback logic (Windows hybrid spell checking)
    submitted here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1918277
    
    - Adds browser tests for SpellCheckHostChromeImpl
    - Adds component tests for the new SpellcheckHostMetrics
    - Adds component test coverage for the new SpellcheckPlatform methods in the
      Windows platform test
        - Created a bug to track splitting the single test into multiple ones
    - Adds component tests for SpellcheckProvider
        - Creates a new FakeSpellCheck class to fake the count of enabled spell
          check locales, which is needed to control when to call into the
          browser spell checker
    
    Bug: 463364, 1035450
    Change-Id: Ie3e692c0d5d5970ed2ab7612018198bf42049139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972573
    Auto-Submit: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726150}

--- chrome/browser/spellchecker/spell_check_host_chrome_impl_win_browsertest.cc
+++ chrome/browser/spellchecker/spell_check_host_chrome_impl_win_browsertest.cc
@@ -21,7 +21,14 @@
 class SpellCheckHostChromeImplWinBrowserTest : public InProcessBrowserTest {
  public:
   SpellCheckHostChromeImplWinBrowserTest() {
+#if BUILDFLAG(USE_WIN_HYBRID_SPELLCHECKER)
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{spellcheck::kWinUseBrowserSpellChecker,
+                              spellcheck::kWinUseHybridSpellChecker},
+        /*disabled_features=*/{});
+#else
     feature_list_.InitAndEnableFeature(spellcheck::kWinUseBrowserSpellChecker);
+#endif  // BUILDFLAG(USE_WIN_HYBRID_SPELLCHECKER)
   }
 
   void SetUpOnMainThread() override {
@@ -34,9 +41,17 @@ class SpellCheckHostChromeImplWinBrowserTest : public InProcessBrowserTest {
 
   void TearDownOnMainThread() override { renderer_.reset(); }
 
-  void LogResult(const std::vector<SpellCheckResult>& result) {
+  void OnSpellcheckResult(const std::vector<SpellCheckResult>& result) {
     received_result_ = true;
     result_ = result;
+    if (quit_)
+      std::move(quit_).Run();
+  }
+
+  void OnSuggestionResult(
+      const std::vector<std::vector<::base::string16>>& suggestions) {
+    received_result_ = true;
+    suggestion_result_ = suggestions;
     if (quit_)
       std::move(quit_).Run();
   }
@@ -65,26 +80,29 @@ class SpellCheckHostChromeImplWinBrowserTest : public InProcessBrowserTest {
 
   bool received_result_ = false;
   std::vector<SpellCheckResult> result_;
+  std::vector<std::vector<::base::string16>> suggestion_result_;
   base::OnceClosure quit_;
 };
 
 // Uses browsertest to setup chrome threads.
 IN_PROC_BROWSER_TEST_F(SpellCheckHostChromeImplWinBrowserTest,
                        SpellCheckReturnMessage) {
-  if (base::win::GetVersion() < base::win::Version::WIN8)
+  if (!spellcheck::WindowsVersionSupportsSpellchecker()) {
     return;
+  }
 
   spellcheck_platform::SetLanguage(
       "en-US", base::BindOnce(&SpellCheckHostChromeImplWinBrowserTest::
                                   SetLanguageCompletionCallback,
                               base::Unretained(this)));
-
   RunUntilResultReceived();
 
   spell_check_host_->RequestTextCheck(
-      base::UTF8ToUTF16("zz."), 123,
-      base::BindOnce(&SpellCheckHostChromeImplWinBrowserTest::LogResult,
-                     base::Unretained(this)));
+      base::UTF8ToUTF16("zz."),
+      /*route_id=*/123,
+      base::BindOnce(
+          &SpellCheckHostChromeImplWinBrowserTest::OnSpellcheckResult,
+          base::Unretained(this)));
   RunUntilResultReceived();
 
   ASSERT_EQ(1U, result_.size());
@@ -92,3 +110,100 @@ IN_PROC_BROWSER_TEST_F(SpellCheckHostChromeImplWinBrowserTest,
   EXPECT_EQ(result_[0].length, 2);
   EXPECT_EQ(result_[0].decoration, SpellCheckResult::SPELLING);
 }
+
+#if BUILDFLAG(USE_WIN_HYBRID_SPELLCHECKER)
+IN_PROC_BROWSER_TEST_F(SpellCheckHostChromeImplWinBrowserTest,
+                       WithPartialResults) {
+  if (!spellcheck::WindowsVersionSupportsSpellchecker()) {
+    return;
+  }
+
+  spellcheck_platform::SetLanguage(
+      "en-US", base::BindOnce(&SpellCheckHostChromeImplWinBrowserTest::
+                                  SetLanguageCompletionCallback,
+                              base::Unretained(this)));
+  RunUntilResultReceived();
+
+  // Fake renderer results: "tihs" is misspelled but pretend "wrod" isn't
+  std::vector<SpellCheckResult> renderer_results;
+  renderer_results.push_back(
+      SpellCheckResult(SpellCheckResult::SPELLING, 0, 4));
+  spell_check_host_->RequestPartialTextCheck(
+      base::UTF8ToUTF16("tihs is a word wrod."),
+      /*route_id=*/123, renderer_results,
+      /*fill_suggestions=*/false,
+      base::BindOnce(
+          &SpellCheckHostChromeImplWinBrowserTest::OnSpellcheckResult,
+          base::Unretained(this)));
+  RunUntilResultReceived();
+
+  // Only "tihs" should be found, since "wrod" was deemed correct by the
+  // renderer
+  ASSERT_EQ(1U, result_.size());
+  EXPECT_EQ(result_[0].location, 0);
+  EXPECT_EQ(result_[0].length, 4);
+  EXPECT_EQ(result_[0].decoration, SpellCheckResult::SPELLING);
+}
+
+IN_PROC_BROWSER_TEST_F(SpellCheckHostChromeImplWinBrowserTest,
+                       WithoutPartialResults) {
+  if (!spellcheck::WindowsVersionSupportsSpellchecker()) {
+    return;
+  }
+
+  spellcheck_platform::SetLanguage(
+      "en-US", base::BindOnce(&SpellCheckHostChromeImplWinBrowserTest::
+                                  SetLanguageCompletionCallback,
+                              base::Unretained(this)));
+  RunUntilResultReceived();
+
+  // Empty renderer results
+  std::vector<SpellCheckResult> renderer_results;
+  spell_check_host_->RequestPartialTextCheck(
+      base::UTF8ToUTF16("tihs is a wrod."),
+      /*route_id=*/123, renderer_results,
+      /*fill_suggestions=*/true,
+      base::BindOnce(
+          &SpellCheckHostChromeImplWinBrowserTest::OnSpellcheckResult,
+          base::Unretained(this)));
+  RunUntilResultReceived();
+
+  // Both "tihs" and "wrod" should be detected, and should have replacement
+  // suggestions
+  ASSERT_EQ(2U, result_.size());
+
+  EXPECT_EQ(result_[0].location, 0);
+  EXPECT_EQ(result_[0].length, 4);
+  EXPECT_EQ(result_[0].decoration, SpellCheckResult::SPELLING);
+  EXPECT_GT(result_[0].replacements.size(), 0U);
+
+  EXPECT_EQ(result_[1].location, 10);
+  EXPECT_EQ(result_[1].length, 4);
+  EXPECT_EQ(result_[1].decoration, SpellCheckResult::SPELLING);
+  EXPECT_GT(result_[1].replacements.size(), 0U);
+}
+
+IN_PROC_BROWSER_TEST_F(SpellCheckHostChromeImplWinBrowserTest,
+                       PerLanguageSuggestions) {
+  if (!spellcheck::WindowsVersionSupportsSpellchecker()) {
+    return;
+  }
+
+  spellcheck_platform::SetLanguage(
+      "en-US", base::BindOnce(&SpellCheckHostChromeImplWinBrowserTest::
+                                  SetLanguageCompletionCallback,
+                              base::Unretained(this)));
+  RunUntilResultReceived();
+
+  spell_check_host_->GetPerLanguageSuggestions(
+      base::UTF8ToUTF16("tihs"),
+      base::BindOnce(
+          &SpellCheckHostChromeImplWinBrowserTest::OnSuggestionResult,
+          base::Unretained(this)));
+  RunUntilResultReceived();
+
+  // Should have 1 vector of results, which contains at least 1 suggestion
+  ASSERT_EQ(1U, suggestion_result_.size());
+  EXPECT_GT(suggestion_result_[0].size(), 0U);
+}
+#endif  // BUILDFLAG(USE_WIN_HYBRID_SPELLCHECKER)
