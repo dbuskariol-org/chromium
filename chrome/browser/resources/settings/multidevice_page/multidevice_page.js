bebad7f180b915c83a07d41a22eeb1b554879862
Azeem Arshad <azeemarshad@chromium.org>
2019-12-18 05:50:22 +0000

    [a11y] Reduce tab stops for multidevice settings row
    This CL changes multidevicepage so that the text label and sublabel are
    conditionally aria-hidden when possible. This simplifies the navigation.
    
    Fixed: 1025414
    Change-Id: Ic31596fe3c72e630f221002a717e5723e44b0070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1973213
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725847}

--- chrome/browser/resources/settings/multidevice_page/multidevice_page.js
+++ chrome/browser/resources/settings/multidevice_page/multidevice_page.js
@@ -159,6 +159,21 @@ Polymer({
   },
 
   /**
+   * @return {string} "true" or "false" indicating whether the text box
+   *                  should be aria-hidden or not.
+   * @private
+   */
+  getTextAriaHidden_: function() {
+    // When host is set and verified, we only show subpage arrow button and
+    // toggle. In this case, we avoid the navigation stops on the text to make
+    // navigating easier. The arrow button is labeled and described by the text,
+    // so the text is still available to assistive tools.
+    return String(
+        this.pageContentData.mode ===
+        settings.MultiDeviceSettingsMode.HOST_SET_VERIFIED);
+  },
+
+  /**
    * @return {boolean}
    * @private
    */
