d8fa0cc3b7b42a4a6e29609822c455116e8e89cd
Antonio Gomes <tonikitoo@igalia.com>
2019-12-18 23:49:49 +0000

    Switch WebRtcAudioDeviceImpl away from std::vector
    ... to use WTF::Vector instead.
    
    BUG=923394
    R=guidou@chromium.org
    
    Change-Id: I08ddf7cbaac201f8b6e93aa1f4ba1addbc55f3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970133
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#726155}

--- third_party/blink/renderer/modules/webrtc/webrtc_audio_device_impl.h
+++ third_party/blink/renderer/modules/webrtc/webrtc_audio_device_impl.h
@@ -10,7 +10,6 @@
 #include <list>
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
@@ -19,6 +18,7 @@
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/webrtc/webrtc_audio_device_not_impl.h"
 #include "third_party/blink/renderer/platform/webrtc/webrtc_source.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 // A WebRtcAudioDeviceImpl instance implements the abstract interface
 // webrtc::AudioDeviceModule which makes it possible for a user (e.g. webrtc::
@@ -187,9 +187,7 @@ class MODULES_EXPORT WebRtcAudioDeviceImpl
 
   // Buffer used for temporary storage during render callback.
   // It is only accessed by the audio render thread.
-  //
-  // TODO(crbug.com/923394): Replace std::vector by WTF::Vector.
-  std::vector<int16_t> render_buffer_;
+  Vector<int16_t> render_buffer_;
 
   // The output device used for echo cancellation
   //
