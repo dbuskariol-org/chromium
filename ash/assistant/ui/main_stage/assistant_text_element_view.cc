8e595643bbe72f2bd63897bc0dea3e4ae71d90aa
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-12-20 02:41:21 +0000

    Revert "Create base class for AssistantUiElement views."
    This reverts commit f5232c5d1a501625b239a196d35c89da56a161eb.
    
    Reason for revert: Broke linux-chromeos-chrome build.
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/531
    
    Original change's description:
    > Create base class for AssistantUiElement views.
    > 
    > This is part of a multi-CL effort to clean up the AssistantUiElement
    > modeling/rendering pipeline. Once complete, UiElementContainerView will
    > only know about AssistantUiElementViews, not inheriting subclasses.
    > 
    > More details in the bug.
    > 
    > Bug: b:145003512
    > Change-Id: I6b9b96af2f7f7e73a19785d58104049f287616df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953700
    > Commit-Queue: David Black <dmblack@google.com>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#726629}
    
    TBR=xiaohuic@chromium.org,dmblack@google.com
    
    Change-Id: I20b002539e39ca441bda91a3e4ffa4169b49dd38
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:145003512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977881
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726660}

--- ash/assistant/ui/main_stage/assistant_text_element_view.cc
+++ ash/assistant/ui/main_stage/assistant_text_element_view.cc
@@ -4,49 +4,32 @@
 
 #include "ash/assistant/ui/main_stage/assistant_text_element_view.h"
 
-#include <memory>
-
 #include "ash/assistant/model/ui/assistant_text_element.h"
 #include "ash/assistant/ui/assistant_ui_constants.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/views/background.h"
-#include "ui/views/controls/label.h"
-#include "ui/views/layout/fill_layout.h"
 
 namespace ash {
 
 // AssistantTextElementView ----------------------------------------------------
 
 AssistantTextElementView::AssistantTextElementView(
-    const AssistantTextElement* text_element) {
-  InitLayout(text_element);
+    const AssistantTextElement* text_element)
+    : views::Label(base::UTF8ToUTF16(text_element->text())) {
+  SetAutoColorReadabilityEnabled(false);
+  SetEnabledColor(kTextColorPrimary);
+  SetFontList(assistant::ui::GetDefaultFontList()
+                  .DeriveWithSizeDelta(2)
+                  .DeriveWithWeight(gfx::Font::Weight::MEDIUM));
+  SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
+  SetMultiLine(true);
+  SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
 }
 
 AssistantTextElementView::~AssistantTextElementView() = default;
 
 const char* AssistantTextElementView::GetClassName() const {
   return "AssistantTextElementView";
-}
-
-void AssistantTextElementView::ChildPreferredSizeChanged(views::View* child) {
-  PreferredSizeChanged();
-}
-
-void AssistantTextElementView::InitLayout(
-    const AssistantTextElement* text_element) {
-  SetLayoutManager(std::make_unique<views::FillLayout>());
-
-  // Label.
-  views::Label* label = AddChildView(
-      std::make_unique<views::Label>(base::UTF8ToUTF16(text_element->text())));
-  label->SetAutoColorReadabilityEnabled(false);
-  label->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
-  label->SetEnabledColor(kTextColorPrimary);
-  label->SetFontList(assistant::ui::GetDefaultFontList()
-                         .DeriveWithSizeDelta(2)
-                         .DeriveWithWeight(gfx::Font::Weight::MEDIUM));
-  label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
-  label->SetMultiLine(true);
 }
 
 }  // namespace ash
