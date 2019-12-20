9f631fddb69f329be5f2ac42912ec47eca238abb
Eugene But <eugenebut@google.com>
2019-12-20 18:48:51 +0000

    [ios][eg2] Make settings_egtest.mm EG2 compatible.
    Convert the following test:
     - testMetricsReporting
     - testBreakpadReporting
     - testBreakpadReportingFirstLaunch
     - testSettingsKeyboardCommands
    
    testClearCookies does not work in EG2 and will be converted
    in the follow up CL.
    
    Also create SettingsAppInterface to allow using metrics and
    breakpad code.
    
    Bug: 987646
    Change-Id: Ieb86ba8fcf5f0cfe933ead130c14ea0c7f721b40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977655
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726869}

--- ios/chrome/browser/ui/settings/settings_egtest.mm
+++ ios/chrome/browser/ui/settings/settings_egtest.mm
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #include <map>
@@ -10,38 +9,34 @@
 
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/task/post_task.h"
 #include "build/branding_buildflags.h"
-#include "components/browsing_data/core/pref_names.h"
-#include "components/metrics/metrics_pref_names.h"
-#include "components/prefs/pref_member.h"
-#include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
-#import "ios/chrome/app/main_controller.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#include "ios/chrome/browser/content_settings/host_content_settings_map_factory.h"
-#include "ios/chrome/browser/pref_names.h"
-#import "ios/chrome/browser/ui/authentication/cells/signin_promo_view.h"
+#import "ios/chrome/browser/ui/settings/settings_app_interface.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
-#include "ios/chrome/grit/ios_theme_resources.h"
-#import "ios/chrome/test/app/chrome_test_util.h"
+#import "ios/chrome/test/earl_grey/chrome_actions_app_interface.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
-#include "ios/web/public/thread/web_task_traits.h"
-#include "ios/web/public/thread/web_thread.h"
-#include "net/url_request/url_request_context.h"
-#include "net/url_request/url_request_context_getter.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+#if defined(CHROME_EARL_GREY_2)
+// TODO(crbug.com/1015113): The EG2 macro is breaking indexing for some reason
+// without the trailing semicolon.  For now, disable the extra semi warning
+// so Xcode indexing works for the egtest.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++98-compat-extra-semi"
+GREY_STUB_CLASS_IN_APP_MAIN_QUEUE(SettingsAppInterface);
+#endif  // defined(CHROME_EARL_GREY_2)
 
 using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::ClearBrowsingDataButton;
@@ -126,15 +121,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
   // gesture to return to the top of the Clear Browsing Data screen, so scroll
   // programatically instead. Remove this custom action if we switch back to a
   // fullscreen presentation.
-  GREYPerformBlock scrollToTopBlock =
-      ^BOOL(id element, __strong NSError** error) {
-        UIScrollView* view = base::mac::ObjCCastStrict<UIScrollView>(element);
-        view.contentOffset = CGPointZero;
-        return YES;
-      };
   [[EarlGrey selectElementWithMatcher:ClearBrowsingDataView()]
-      performAction:[GREYActionBlock actionWithName:@"Scroll to top"
-                                       performBlock:scrollToTopBlock]];
+      performAction:[ChromeActionsAppInterface scrollToTop]];
 }
 
 // From the NTP, clears the cookies and site data via the UI.
@@ -192,61 +180,29 @@ id<GREYMatcher> ClearBrowsingDataCell() {
       performAction:grey_tap()];
 }
 
-// Restore the Clear Browsing Data checkmarks prefs to their default state.
-- (void)restoreClearBrowsingDataCheckmarksToDefault {
-  ios::ChromeBrowserState* browserState =
-      chrome_test_util::GetOriginalBrowserState();
-  PrefService* preferences = browserState->GetPrefs();
-  preferences->SetBoolean(browsing_data::prefs::kDeleteBrowsingHistory, true);
-  preferences->SetBoolean(browsing_data::prefs::kDeleteCache, true);
-  preferences->SetBoolean(browsing_data::prefs::kDeleteCookies, true);
-  preferences->SetBoolean(browsing_data::prefs::kDeletePasswords, false);
-  preferences->SetBoolean(browsing_data::prefs::kDeleteFormData, false);
-}
-
-- (void)setMetricsReportingEnabled:(BOOL)reportingEnabled {
-  chrome_test_util::SetBooleanLocalStatePref(
-      metrics::prefs::kMetricsReportingEnabled, reportingEnabled);
-  // Breakpad uses dispatch_async to update its state. Wait to get to a
-  // consistent state.
-  chrome_test_util::WaitForBreakpadQueue();
-}
-
-// TODO(crbug.com/953862): Remove as part of feature flag cleanup.
-- (void)setMetricsReportingEnabled:(BOOL)reportingEnabled
-                          wifiOnly:(BOOL)wifiOnly {
-  chrome_test_util::SetBooleanLocalStatePref(
-      metrics::prefs::kMetricsReportingEnabled, reportingEnabled);
-  chrome_test_util::SetBooleanLocalStatePref(prefs::kMetricsReportingWifiOnly,
-                                             wifiOnly);
-  // Breakpad uses dispatch_async to update its state. Wait to get to a
-  // consistent state.
-  chrome_test_util::WaitForBreakpadQueue();
-}
-
 // Checks for a given service that it is both recording and uploading, where
 // appropriate.
 - (void)assertMetricsServiceEnabled:(MetricsServiceType)serviceType {
   switch (serviceType) {
     case kMetrics:
-      GREYAssertTrue(chrome_test_util::IsMetricsRecordingEnabled(),
+      GREYAssertTrue([SettingsAppInterface isMetricsRecordingEnabled],
                      @"Metrics recording should be enabled.");
-      GREYAssertTrue(chrome_test_util::IsMetricsReportingEnabled(),
+      GREYAssertTrue([SettingsAppInterface isMetricsReportingEnabled],
                      @"Metrics reporting should be enabled.");
       break;
     case kBreakpad:
-      GREYAssertTrue(chrome_test_util::IsBreakpadEnabled(),
+      GREYAssertTrue([SettingsAppInterface isBreakpadEnabled],
                      @"Breakpad should be enabled.");
-      GREYAssertTrue(chrome_test_util::IsBreakpadReportingEnabled(),
+      GREYAssertTrue([SettingsAppInterface isBreakpadReportingEnabled],
                      @"Breakpad reporting should be enabled.");
       break;
     case kBreakpadFirstLaunch:
       // For first launch after upgrade, or after install, uploading of crash
       // reports is always disabled.  Check that the first launch flag is being
       // honored.
-      GREYAssertTrue(chrome_test_util::IsBreakpadEnabled(),
+      GREYAssertTrue([SettingsAppInterface isBreakpadEnabled],
                      @"Breakpad should be enabled.");
-      GREYAssertFalse(chrome_test_util::IsBreakpadReportingEnabled(),
+      GREYAssertFalse([SettingsAppInterface isBreakpadReportingEnabled],
                       @"Breakpad reporting should be disabled.");
       break;
   }
@@ -256,9 +212,9 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 - (void)assertMetricsServiceDisabled:(MetricsServiceType)serviceType {
   switch (serviceType) {
     case kMetrics: {
-      GREYAssertFalse(chrome_test_util::IsMetricsRecordingEnabled(),
+      GREYAssertFalse([SettingsAppInterface isMetricsRecordingEnabled],
                       @"Metrics recording should be disabled.");
-      GREYAssertFalse(chrome_test_util::IsMetricsReportingEnabled(),
+      GREYAssertFalse([SettingsAppInterface isMetricsReportingEnabled],
                       @"Metrics reporting should be disabled.");
       break;
     }
@@ -267,7 +223,7 @@ id<GREYMatcher> ClearBrowsingDataCell() {
       // Check only whether or not breakpad is enabled.  Disabling
       // breakpad does stop uploading, and does not change the flag
       // used to check whether or not it's uploading.
-      GREYAssertFalse(chrome_test_util::IsBreakpadEnabled(),
+      GREYAssertFalse([SettingsAppInterface isBreakpadEnabled],
                       @"Breakpad should be disabled.");
       break;
     }
@@ -281,17 +237,17 @@ id<GREYMatcher> ClearBrowsingDataCell() {
     (MetricsServiceType)serviceType {
   switch (serviceType) {
     case kMetrics: {
-      GREYAssertTrue(chrome_test_util::IsMetricsRecordingEnabled(),
+      GREYAssertTrue([SettingsAppInterface isMetricsRecordingEnabled],
                      @"Metrics recording should be enabled.");
-      GREYAssertFalse(chrome_test_util::IsMetricsReportingEnabled(),
+      GREYAssertFalse([SettingsAppInterface isMetricsReportingEnabled],
                       @"Metrics reporting should be disabled.");
       break;
     }
     case kBreakpad:
     case kBreakpadFirstLaunch: {
-      GREYAssertTrue(chrome_test_util::IsBreakpadEnabled(),
+      GREYAssertTrue([SettingsAppInterface isBreakpadEnabled],
                      @"Breakpad should be enabled.");
-      GREYAssertFalse(chrome_test_util::IsBreakpadReportingEnabled(),
+      GREYAssertFalse([SettingsAppInterface isBreakpadReportingEnabled],
                       @"Breakpad reporting should be disabled.");
       break;
     }
@@ -320,10 +276,10 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
   if ([ChromeEarlGrey isUMACellularEnabled]) {
     // kMetricsReportingEnabled OFF
-    [self setMetricsReportingEnabled:NO];
+    [SettingsAppInterface setMetricsReportingEnabled:NO];
   } else {
     // kMetricsReportingEnabled OFF and kMetricsReportingWifiOnly OFF
-    [self setMetricsReportingEnabled:NO wifiOnly:NO];
+    [SettingsAppInterface setMetricsReportingEnabled:NO wifiOnly:NO];
   }
   // Service should be completely disabled.
   // I.e. no recording of data, and no uploading of what's been recorded.
@@ -331,10 +287,10 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
   if ([ChromeEarlGrey isUMACellularEnabled]) {
     // kMetricsReportingEnabled OFF
-    [self setMetricsReportingEnabled:NO];
+    [SettingsAppInterface setMetricsReportingEnabled:NO];
   } else {
     // kMetricsReportingEnabled OFF and kMetricsReportingWifiOnly ON
-    [self setMetricsReportingEnabled:NO wifiOnly:YES];
+    [SettingsAppInterface setMetricsReportingEnabled:NO wifiOnly:YES];
   }
   // If kMetricsReportingEnabled is OFF, any service should remain completely
   // disabled, i.e. no uploading even if kMetricsReportingWifiOnly is ON.
@@ -350,7 +306,7 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
   if (![ChromeEarlGrey isUMACellularEnabled]) {
     // kMetricsReportingEnabled ON and kMetricsReportingWifiOnly ON.
-    [self setMetricsReportingEnabled:YES wifiOnly:YES];
+    [SettingsAppInterface setMetricsReportingEnabled:YES wifiOnly:YES];
     // Service should be enabled.
     [self assertMetricsServiceEnabled:serviceType];
 
@@ -366,7 +322,7 @@ id<GREYMatcher> ClearBrowsingDataCell() {
     [self assertMetricsServiceEnabled:serviceType];
 
     // kMetricsReportingEnabled ON and kMetricsReportingWifiOnly OFF
-    [self setMetricsReportingEnabled:YES wifiOnly:NO];
+    [SettingsAppInterface setMetricsReportingEnabled:YES wifiOnly:NO];
     [self assertMetricsServiceEnabled:serviceType];
   }
 
@@ -380,12 +336,12 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
   if (![ChromeEarlGrey isUMACellularEnabled]) {
     // kMetricsReportingEnabled ON and kMetricsReportingWifiOnly ON
-    [self setMetricsReportingEnabled:YES wifiOnly:YES];
+    [SettingsAppInterface setMetricsReportingEnabled:YES wifiOnly:YES];
     // Service should remain disabled.
     [self assertMetricsServiceDisabled:serviceType];
 
     // kMetricsReportingEnabled ON and kMetricsReportingWifiOnly OFF
-    [self setMetricsReportingEnabled:YES wifiOnly:NO];
+    [SettingsAppInterface setMetricsReportingEnabled:YES wifiOnly:NO];
     // Service should remain disabled.
     [self assertMetricsServiceDisabled:serviceType];
   }
@@ -397,7 +353,14 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 // Tests that clearing the cookies through the UI does clear all of them. Use a
 // local server to navigate to a page that sets then tests a cookie, and then
 // clears the cookie and tests it is not set.
-- (void)testClearCookies {
+#if defined(CHROME_EARL_GREY_1)
+#define MAYBE_testClearCookies testClearCookies
+#elif defined(CHROME_EARL_GREY_2)
+#define MAYBE_testClearCookies DISABLED_testClearCookies
+#endif
+// TODO(crbug.com/1036133): [ChromeEarlGrey cookies] does not work correctly in
+// this test.
+- (void)MAYBE_testClearCookies {
   // Creates a map of canned responses and set up the test HTML server.
   std::map<GURL, std::pair<std::string, std::string>> response;
 
@@ -433,9 +396,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
   // Restore the Clear Browsing Data checkmarks prefs to their default state
   // in Teardown.
-  __weak SettingsTestCase* weakSelf = self;
   [self setTearDownHandler:^{
-    [weakSelf restoreClearBrowsingDataCheckmarksToDefault];
+    [SettingsAppInterface restoreClearBrowsingDataCheckmarksToDefault];
   }];
 
   // Clear all cookies.
@@ -464,11 +426,10 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 - (void)testBreakpadReporting {
   [self setTearDownHandler:^{
     // Restore the first launch state to previous state.
-    chrome_test_util::SetFirstLaunchStateTo(
-        chrome_test_util::IsFirstLaunchAfterUpgrade());
+    [SettingsAppInterface resetFirstLaunchState];
   }];
 
-  chrome_test_util::SetFirstLaunchStateTo(NO);
+  [SettingsAppInterface setFirstLunchState:NO];
   [self assertsMetricsPrefsForService:kBreakpad];
 }
 
@@ -479,11 +440,10 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 - (void)testBreakpadReportingFirstLaunch {
   [self setTearDownHandler:^{
     // Restore the first launch state to previous state.
-    chrome_test_util::SetFirstLaunchStateTo(
-        chrome_test_util::IsFirstLaunchAfterUpgrade());
+    [SettingsAppInterface resetFirstLaunchState];
   }];
 
-  chrome_test_util::SetFirstLaunchStateTo(YES);
+  [SettingsAppInterface setFirstLunchState:YES];
   [self assertsMetricsPrefsForService:kBreakpadFirstLaunch];
 }
 
@@ -495,12 +455,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
       assertWithMatcher:grey_notNil()];
 
   // Verify that the Settings register keyboard commands.
-  UIViewController* viewController =
-      chrome_test_util::GetMainController()
-          .interfaceProvider.mainInterface.viewController;
-  UIViewController* settings = viewController.presentedViewController;
-  GREYAssertNotNil(settings.keyCommands,
-                   @"Settings should register key commands when presented.");
+  GREYAssertTrue([SettingsAppInterface settingsRegisteredKeyboardCommands],
+                 @"Settings should register key commands when presented.");
 
   // Present the Sign-in UI.
   id<GREYMatcher> matcher = grey_allOf(chrome_test_util::PrimarySignInButton(),
@@ -510,8 +466,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
 
   // Verify that the Settings register keyboard commands.
-  GREYAssertNil(settings.keyCommands,
-                @"Settings should not register key commands when presented.");
+  GREYAssertFalse([SettingsAppInterface settingsRegisteredKeyboardCommands],
+                  @"Settings should not register key commands when presented.");
 
   // Cancel the sign-in operation.
   [[EarlGrey selectElementWithMatcher:
@@ -523,8 +479,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
 
   // Verify that the Settings register keyboard commands.
-  GREYAssertNotNil(settings.keyCommands,
-                   @"Settings should register key commands when presented.");
+  GREYAssertTrue([SettingsAppInterface settingsRegisteredKeyboardCommands],
+                 @"Settings should register key commands when presented.");
 }
 
 @end
