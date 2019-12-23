26a1bd0c1c3c9185de7a70ec05925d442f0d6611
Austin Tankiang <austinct@chromium.org>
2019-12-23 07:41:28 +0000

    Fix transferToUsbHasDestinationText
    Replace selectInDirectoryTree with navigateWithDirectoryTree which
    handles waiting for the navigation to complete as well.
    
    Bug: 1036743
    Change-Id: Ia35f88e91e8f92c5f9e9e377973e85c99b6b6334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980335
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727126}

--- ui/file_manager/integration_tests/file_manager/transfer.js
+++ ui/file_manager/integration_tests/file_manager/transfer.js
@@ -657,7 +657,6 @@ testcase.transferInfoIsRemembered = async () => {
  * Tests that destination text line shows name for USB targets.
  */
 testcase.transferToUsbHasDestinationText = async () => {
-  const USB_VOLUME_QUERY = '#directory-tree [volume-type-icon="removable"]';
   const entry = ENTRIES.hello;
 
   // Open files app.
@@ -667,6 +666,7 @@ testcase.transferToUsbHasDestinationText = async () => {
   await sendTestMessage({name: 'mountFakeUsbEmpty'});
 
   // Wait for the USB volume to mount.
+  const USB_VOLUME_QUERY = '#directory-tree [volume-type-icon="removable"]';
   await remoteCall.waitForElement(appId, USB_VOLUME_QUERY);
 
   // Select the file.
@@ -678,8 +678,7 @@ testcase.transferToUsbHasDestinationText = async () => {
       await remoteCall.callRemoteTestUtil('execCommand', appId, ['copy']));
 
   // Select USB volume.
-  chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
-      'selectInDirectoryTree', appId, [USB_VOLUME_QUERY]));
+  await navigateWithDirectoryTree(appId, '/fake-usb');
 
   // Tell the background page to never finish the file copy.
   await remoteCall.callRemoteTestUtil(
