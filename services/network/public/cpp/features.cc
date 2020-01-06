eac9d32f4b1abbb568daebb27ab427d72877d8d1
Takashi Toyoshima <toyoshim@chromium.org>
2020-01-06 07:12:47 +0000

    OOR-CORS: Enabled by default
    Now the OOR-CORS m79 rollout is started. To follow the best practices
    for incremental feature rollouts, this patch enables the OOR-CORS
    by default on TOT.
    
    NOAUTOREVERT=true
    
    Bug: 905971, 1001450, 1035763, 1039165
    Change-Id: I2ca742eab6a14891968892f7f1ef40adbe8bad67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988127
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728462}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -31,12 +31,13 @@ const base::Feature kNetworkService {
       base::FEATURE_ENABLED_BY_DEFAULT
 };
 
-// Out of Blink CORS will be launched at m79. The flag will be enabled by
-// default around m81 after the feature rolled out over the finch successfully
-// at m79. Both mode will be maintained at least until m81, or around m83+ for
-// enterprise supports.
+// Out of Blink CORS for browsers is launched at m79 (http://crbug.com/1001450),
+// and one for WebView will be at m81 (http://crbug.com/1035763).
+// The legacy CORS will be also maintained at least until m81 for enterprise
+// users. See https://sites.google.com/a/chromium.org/dev/Home/loading/oor-cors
+// for FYI Builders information.
 const base::Feature kOutOfBlinkCors{"OutOfBlinkCors",
-                                    base::FEATURE_DISABLED_BY_DEFAULT};
+                                    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kReporting{"Reporting", base::FEATURE_ENABLED_BY_DEFAULT};
 
