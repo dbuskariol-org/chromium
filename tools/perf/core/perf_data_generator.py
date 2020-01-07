d0f4dea2ef20c53a3ffca5519a158b3226f9a46a
Haiyang Pan <hypan@google.com>
2020-01-07 18:13:12 +0000

    [perf] Switch pixel2 webview to Android App Bundles
    Bug: 986990
    Change-Id: Ie86239ed57a6cbdc77aed65924c256fd2af21dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959661
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#728993}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -352,7 +352,7 @@ BUILDERS = {
         'isolate': 'performance_webview_test_suite',
       }
     ],
-    'platform': 'android-webview-google',
+    'platform': 'android-webview-google-bundle',
     'dimension': {
       'pool': 'chrome.tests.perf-webview',
       'os': 'Android',
@@ -464,7 +464,7 @@ BUILDERS = {
         'isolate': 'performance_webview_test_suite',
       }
     ],
-    'platform': 'android-webview-google',
+    'platform': 'android-webview-google-bundle',
     'dimension': {
       'pool': 'chrome.tests.perf-webview',
       'os': 'Android',
