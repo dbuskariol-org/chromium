28864fc446d353150e6728d9f76d62cbaee21e0a
Caleb Raitto <caraitto@chromium.org>
2020-01-07 00:18:19 +0000

    Produce a list of all HighEntropy entities as a build step.
    Example usage:
    
    $ autoninja -C out/Default generate_high_entropy_list
    $ cat
    out/Default/gen/third_party/blink/renderer/bindings/high_entropy_list.txt
    
    Example output:
    
    Keyboard.getLayoutMap
    VisualViewport.offsetLeft
    VisualViewport.offsetTop
    VisualViewport.pageLeft
    VisualViewport.pageTop
    VisualViewport.width
    ...
    
    Bug: 915620
    Change-Id: I88543f614ea3082b0ff187846a338d6a45d63254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974895
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728714}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1338,6 +1338,8 @@ _GENERIC_PYDEPS_FILES = [
     'third_party/blink/renderer/bindings/scripts/build_web_idl_database.pydeps',
     'third_party/blink/renderer/bindings/scripts/collect_idl_files.pydeps',
     'third_party/blink/renderer/bindings/scripts/generate_bindings.pydeps',
+    ('third_party/blink/renderer/bindings/scripts/'
+     'generate_high_entropy_list.pydeps'),
     'tools/binary_size/sizes.pydeps',
     'tools/binary_size/supersize.pydeps',
 ]
