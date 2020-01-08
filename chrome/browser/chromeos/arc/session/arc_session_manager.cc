0caf9a192a380e57d2258594b99a10de8632918d
Junichi Uekawa <uekawa@google.com>
2020-01-08 05:54:20 +0000

    arcvm: Extend timeout for Android boot on ARCVM on VM.
    ARCVM does not start within 10 minutes for L2 VM guest.
    
    BUG=b:146843805
    TEST=None
    
    
    Change-Id: I3b27ece8a1a28a16bad072f0b5bfd7ca14f675a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981519
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729241}

--- chrome/browser/chromeos/arc/session/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/session/arc_session_manager.cc
@@ -43,6 +43,7 @@
 #include "chromeos/cryptohome/cryptohome_parameters.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
+#include "chromeos/system/statistics_provider.h"
 #include "components/account_id/account_id.h"
 #include "components/arc/arc_features.h"
 #include "components/arc/arc_prefs.h"
@@ -80,7 +81,18 @@ base::Optional<bool> g_enable_check_android_management_in_tests;
 // Maximum amount of time we'll wait for ARC to finish booting up. Once this
 // timeout expires, keep ARC running in case the user wants to file feedback,
 // but present the UI to try again.
-constexpr base::TimeDelta kArcSignInTimeout = base::TimeDelta::FromMinutes(5);
+base::TimeDelta GetArcSignInTimeout() {
+  constexpr base::TimeDelta kArcSignInTimeout = base::TimeDelta::FromMinutes(5);
+  constexpr base::TimeDelta kArcVmSignInTimeoutForVM =
+      base::TimeDelta::FromMinutes(20);
+
+  if (chromeos::system::StatisticsProvider::GetInstance()->IsRunningOnVm() &&
+      arc::IsArcVmEnabled()) {
+    return kArcVmSignInTimeoutForVM;
+  } else {
+    return kArcSignInTimeout;
+  }
+}
 
 // Updates UMA with user cancel only if error is not currently shown.
 void MaybeUpdateOptInCancelUMA(const ArcSupportHost* support_host) {
@@ -968,7 +980,7 @@ void ArcSessionManager::OnAndroidManagementChecked(
       VLOG(1) << "Starting ARC for first sign in.";
       sign_in_start_time_ = base::TimeTicks::Now();
       arc_sign_in_timer_.Start(
-          FROM_HERE, kArcSignInTimeout,
+          FROM_HERE, GetArcSignInTimeout(),
           base::Bind(&ArcSessionManager::OnArcSignInTimeout,
                      weak_ptr_factory_.GetWeakPtr()));
       StartArc();
