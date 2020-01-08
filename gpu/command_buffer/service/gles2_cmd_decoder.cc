0a94c4c7ae768e8271ad87ca22d06a5a46758bb9
Lin Sun <lin.sun@intel.com>
2020-01-08 17:19:38 +0000

    Revert "Fixed WebGL flush bug for old Intel driver"
    This reverts commit 1cdb246fcf8cd94d35d81e93f71e90cbae0f6476.
    
    Reason for revert: Potential performance regression in http://crbug.com/1032210
    
    Original change's description:
    > Fixed WebGL flush bug for old Intel driver
    >
    > Add a workaround to do extra flush() before and after BindFrameBuffer
    > call if Intel graphics driver is older then 24.20.100.6286(August 30,
    > 2018).
    >
    > Verified this patch on Intel UHD Graphics 620 (23.20.16.4973) with the
    > attachment flush-bug.html from issue 1018028, which is provided by the
    > reporter to reproduce this bug.
    >
    > Bug: 1018028
    > Change-Id: Iea220f5e5f2bbeeac87c92f761a44a36c5efe72b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912790
    > Commit-Queue: Lin Sun <lin.sun@intel.com>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#722827}
    
    Bug: 1018028, 1032210
    Change-Id: I8a6759b08d5b3609e8e03ceeed02c180748a57a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987315
    Commit-Queue: Lin Sun <lin.sun@intel.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729376}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -6501,11 +6501,7 @@ void GLES2DecoderImpl::DoBindFramebuffer(GLenum target, GLuint client_id) {
     service_id = GetBackbufferServiceId();
   }
 
-  if (workarounds().do_extra_flush_around_bindframebuffer)
-    api()->glFlushFn();
   api()->glBindFramebufferEXTFn(target, service_id);
-  if (workarounds().do_extra_flush_around_bindframebuffer)
-    api()->glFlushFn();
   OnFboChanged();
 }
 
