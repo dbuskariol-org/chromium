348bd2d4c29c680d1d3a1d1bbd0cc6e56c1cc4f1
John Lee <johntlee@chromium.org>
2020-01-09 21:33:45 +0000

    WebUI Tab Strip: Add TabGroupElement
    This CL simply sets up a TabGroupElement that takes in slotted content
    of TabElements. This is just the first CL to setup the base of
    the front-end work for tab groups.
    
    Bug: 1027373
    Change-Id: Ic7248505c6ee7cfeaac92810e9e054f83c840c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992803
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729909}

---
+++ chrome/browser/resources/tab_strip/tab_group.js
@@ -0,0 +1,13 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CustomElement} from './custom_element.js';
+
+class TabGroupElement extends CustomElement {
+  static get template() {
+    return `{__html_template__}`;
+  }
+}
+
+customElements.define('tabstrip-tab-group', TabGroupElement);
