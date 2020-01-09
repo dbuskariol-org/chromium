a3782800c80329a9aa7c604286510c26b791a762
Luke Zielinski <lpz@chromium.org>
2020-01-09 18:37:13 +0000

    Roll new version of WPT tools.
    The biggest change here is the increment of the manifest version, caused
    by https://github.com/web-platform-tests/wpt/pull/20459, which changes
    reftest computation logic.
    
    This roll also introduces crashtests to the manifest.
    
    The rest of the changes are relatively small (new browser support, bug
    fixes etc).
    
    Change-Id: I72dc0901e42ae81c4a5db0e8435574578afe53c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987915
    Commit-Queue: Luke Z <lpz@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729824}

--- testing/scripts/run_wpt_tests.py
+++ testing/scripts/run_wpt_tests.py
@@ -68,7 +68,7 @@ class WPTTestAdapter(common.BaseIsolatedScriptArgsAdapter):
             # update the manifest in cast it's stale.
             #"--no-manifest-update",
             "--manifest=../../third_party/blink/web_tests/external/"
-                "WPT_BASE_MANIFEST_6.json",
+                "WPT_BASE_MANIFEST_7.json",
             # (crbug.com/1023835) The flags below are temporary to aid debugging
             "--log-mach=-",
             "--log-mach-verbose",
