b80ab5527f7b239312aae801985a47cae06cf4b8
Antonio Gomes <tonikitoo@igalia.com>
2020-01-09 01:17:57 +0000

    Move web_audio_media_stream_source.h out of the Blink APi
    ... down to renderer/platform/mediastream, and also rename it to
    webaudio_destination_consumer.h.
    
    BUG=919392, 704136
    R=haraken@chromium.org
    
    Change-Id: I1592af19495c729ab7911036c1a6858e610822f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991403
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#729584}

--- third_party/blink/renderer/platform/exported/web_media_stream_source.cc
+++ third_party/blink/renderer/platform/exported/web_media_stream_source.cc
@@ -34,12 +34,12 @@
 #include <utility>
 
 #include "base/memory/ptr_util.h"
-#include "third_party/blink/public/platform/web_audio_destination_consumer.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/platform/audio/audio_bus.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/mediastream/media_constraints.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
+#include "third_party/blink/renderer/platform/mediastream/webaudio_destination_consumer.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
