d63380b813e3791f18a93bc8021aa48b39588f21
Christopher Grant <cjgrant@chromium.org>
2020-01-09 19:56:46 +0000

    Android: Clean up feature module JNI registration
    DFMs use explicit JNI registration for both Chrome and Monochrome, but
    the feature shouldn't care which it's in. Traditionally, JNI registration has
    been done based on APK targets, but we can also point generation at an Android
    library.
    
    Do that here, and also, tweak the JNI generation template to allow
    exclusion of transitive deps. For a DFM, this means we can generate JNI
    only for a module's entrypoints, without pulling in methods from the
    base module or other unneeded deps. This seems cleaner, and also allows
    this change to work in component builds.
    
    Bug: None
    Change-Id: I3f3f457815a7e2f06f575f72476cc4c877741c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992694
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729869}

--- chrome/android/modules/stack_unwinder/internal/entrypoints.cc
+++ chrome/android/modules/stack_unwinder/internal/entrypoints.cc
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/android/jni_generator/jni_generator_helper.h"
 #include "base/android/jni_utils.h"
 #include "chrome/android/modules/stack_unwinder/internal/jni_registration.h"
 
 extern "C" {
 // This JNI registration method is found and called by module framework code.
-__attribute__((visibility("default"))) bool JNI_OnLoad_stack_unwinder(
-    JNIEnv* env) {
+JNI_GENERATOR_EXPORT bool JNI_OnLoad_stack_unwinder(JNIEnv* env) {
   if (!base::android::IsSelectiveJniRegistrationEnabled(env) &&
       !stack_unwinder::RegisterNonMainDexNatives(env)) {
     return false;
