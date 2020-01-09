4844ce3c16669939ae0da71148166319562d761d
Allen Bauer <kylixrd@chromium.org>
2020-01-09 21:41:46 +0000

    Reland "Removed BrowserView::ViewHierarchChanged, moved most code to constructor and widget-specfic code to AddedToWidget."
    This is a reland of f3616c26047a891b122dd12875cb97eacf715e3e
    
    Original change's description:
    > Removed BrowserView::ViewHierarchChanged, moved most code to constructor and widget-specfic code to AddedToWidget.
    > 
    > Bug: 245409
    > Change-Id: I1e606548df4361199862c561ad86f4d40cbaa7df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979098
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#728998}
    
    Bug: 245409
    Change-Id: Id5317849e130ec8715200f0947bc01f2bc932842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991945
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729914}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view.h
@@ -177,8 +177,6 @@ class BrowserNonClientFrameView : public views::NonClientFrameView,
   int GetSystemMenuY() const override;
 #endif
 
-  void MaybeObserveTabstrip();
-
   // Gets a theme provider that should be non-null even before we're added to a
   // view hierarchy.
   const ui::ThemeProvider* GetThemeProviderForProfile() const;
