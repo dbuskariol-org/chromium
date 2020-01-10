2662d64f0e000fac3b134e96d45a1e73798c30b5
Piotr Bialecki <bialpio@chromium.org>
2020-01-10 21:43:00 +0000

    Revert "Move away from using DFM to distribute AR-related code"
    This reverts commit cd8428970f38f0d0c17ec4317d7ffa53b0d9c8f2.
    
    Reason for revert: https://crbug.com/1040950
    
    Original change's description:
    > Move away from using DFM to distribute AR-related code
    > 
    > This change will make Chrome for Android stop using DFM to ship the
    > ARCore SDK. This is done to enable us to provide AR experiences for
    > apps using WebView. The changes are introduced in a way to enable us to
    > switch back to using AR DFM if need be.
    > 
    > Changes summary:
    > - chrome_java target (defined in chrome/android/BUILD.gn) now also
    >   compiles ArCoreShimImpl.java (see java_sources.gni) - this means that
    >   java parts from ARCore SDK have to be added to its deps
    > - libarcore_sdk_c.so needs to be listed in loadable modules - this is
    >   done in monochrome_public_common_apk_or_module_tmpl (only if target
    >   is a base module when ARCore is enabled)
    > - AndroidManifest.xml no longer needs to contain the min_apk_version
    >   and InstallActivity entries explicitly - they will be brought in
    >   auto-magically by the build system and were needed only because
    >   manifest merging did not work for DFMs
    > - ArCoreInstallUtils (java) should never report that we can request
    >   installation of AR DFM
    > - ArCoreConsentPrompt (native) should never think it needs to request
    >   installation of AR DFM
    > - Added a DCHECK and graceful handling of null jstring for java_path
    >   (path to ARCore SDK .so) - it can be null if loadable_modules do not
    >   contain correct entry - with the DCHECK, debug builds will still
    >   crash but the release builds should just report that the WebXR
    >   specified session configuration (session mode == "immersive-ar") is
    >   not supported
    > 
    > Binary-Size: Tracked in crbug/1040289
    > Bug: 1031623, 1040289
    > Change-Id: Ia2f305d1685f1c7207cc187d253f59a6331c36fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988834
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    > Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#729818}
    
    TBR=agrieve@chromium.org,bialpio@chromium.org,alcooper@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1031623, 1040289
    Change-Id: Ie71baf8604ae81db447a3d8b4d110ee7e4f17c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995470
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730303}

--- chrome/browser/android/vr/arcore_device/arcore_consent_prompt.cc
+++ chrome/browser/android/vr/arcore_device/arcore_consent_prompt.cc
@@ -66,8 +66,9 @@ ArCoreConsentPrompt::ArCoreConsentPrompt() : weak_ptr_factory_(this) {}
 
 ArCoreConsentPrompt::~ArCoreConsentPrompt() = default;
 
-void ArCoreConsentPrompt::OnUserConsentResult(JNIEnv* env,
-                                              jboolean is_granted) {
+void ArCoreConsentPrompt::OnUserConsentResult(
+    JNIEnv* env,
+    jboolean is_granted) {
   jdelegate_.Reset();
 
   if (!on_user_consent_callback_)
@@ -138,8 +139,9 @@ void ArCoreConsentPrompt::RequestInstallSupportedArCore() {
       GetTabFromRenderer(render_process_id_, render_frame_id_));
 }
 
-void ArCoreConsentPrompt::OnRequestInstallArModuleResult(JNIEnv* env,
-                                                         bool success) {
+void ArCoreConsentPrompt::OnRequestInstallArModuleResult(
+    JNIEnv* env,
+    bool success) {
   DVLOG(1) << __func__;
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
@@ -148,8 +150,9 @@ void ArCoreConsentPrompt::OnRequestInstallArModuleResult(JNIEnv* env,
   }
 }
 
-void ArCoreConsentPrompt::OnRequestInstallSupportedArCoreResult(JNIEnv* env,
-                                                                bool success) {
+void ArCoreConsentPrompt::OnRequestInstallSupportedArCoreResult(
+    JNIEnv* env,
+    bool success) {
   DVLOG(1) << __func__;
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(on_request_arcore_install_or_update_result_callback_);
@@ -159,8 +162,17 @@ void ArCoreConsentPrompt::OnRequestInstallSupportedArCoreResult(JNIEnv* env,
 
 void ArCoreConsentPrompt::RequestArModule() {
   DVLOG(1) << __func__;
+  if (ShouldRequestInstallArModule()) {
+    if (!CanRequestInstallArModule()) {
+      OnRequestArModuleResult(false);
+      return;
+    }
 
-  DCHECK(!ShouldRequestInstallArModule()) << "AR DFM should not be needed!";
+    on_request_ar_module_result_callback_ = base::BindOnce(
+        &ArCoreConsentPrompt::OnRequestArModuleResult, GetWeakPtr());
+    RequestInstallArModule();
+    return;
+  }
 
   OnRequestArModuleResult(true);
 }
