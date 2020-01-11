6fa239d60df076ecca13e0a2f147ddba159d8ca5
rbpotter <rbpotter@chromium.org>
2020-01-11 00:32:09 +0000

    SplitSettings: Start splitting up route.js
    This CL:
    - Moves the Route and Router class definitions to a separate file, so
    that they can more easily be shared by browser and OS settings
    - Moves SettingsRoutes to a separate file as this is a shared
    dependency of Router and the remaining functions in route.js
    - Makes Route class's createChild and createSection methods public
    so that they can be called from route.js.
    
    This is in anticipation of forking the remainder of route.js, which
    contains different logic for adding OS vs browser settings routes.
    
    Bug: 967861
    Change-Id: I6565792c808633f3eb00e1286f66cabb225002d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991811
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730356}

--- chrome/browser/resources/settings/route.js
+++ chrome/browser/resources/settings/route.js
@@ -2,236 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Specifies all possible routes in settings.
- *
- * @typedef {{
- *   ABOUT: (undefined|!settings.Route),
- *   ABOUT_ABOUT: (undefined|!settings.Route),
- *   ACCESSIBILITY: (undefined|!settings.Route),
- *   ACCOUNTS: (undefined|!settings.Route),
- *   ACCOUNT_MANAGER: (undefined|!settings.Route),
- *   ADVANCED: (undefined|!settings.Route),
- *   ADDRESSES: (undefined|!settings.Route),
- *   APP_MANAGEMENT: (undefined|!settings.Route),
- *   APP_MANAGEMENT_DETAIL: (undefined|!settings.Route),
- *   APPS: (undefined|!settings.Route),
- *   ANDROID_APPS: (undefined|!settings.Route),
- *   ANDROID_APPS_DETAILS: (undefined|!settings.Route),
- *   CROSTINI: (undefined|!settings.Route),
- *   CROSTINI_ANDROID_ADB: (undefined|!settings.Route),
- *   CROSTINI_DETAILS: (undefined|!settings.Route),
- *   CROSTINI_EXPORT_IMPORT: (undefined|!settings.Route),
- *   CROSTINI_SHARED_PATHS: (undefined|!settings.Route),
- *   CROSTINI_SHARED_USB_DEVICES: (undefined|!settings.Route),
- *   APPEARANCE: (undefined|!settings.Route),
- *   AUTOFILL: (undefined|!settings.Route),
- *   BASIC: (undefined|!settings.Route),
- *   BLUETOOTH: (undefined|!settings.Route),
- *   BLUETOOTH_DEVICES: (undefined|!settings.Route),
- *   CAPTIONS: (undefined|!settings.Route),
- *   CERTIFICATES: (undefined|!settings.Route),
- *   CHANGE_PICTURE: (undefined|!settings.Route),
- *   CHROME_CLEANUP: (undefined|!settings.Route),
- *   CLEAR_BROWSER_DATA: (undefined|!settings.Route),
- *   CLOUD_PRINTERS: (undefined|!settings.Route),
- *   CUPS_PRINTERS: (undefined|!settings.Route),
- *   DATETIME: (undefined|!settings.Route),
- *   DATETIME_TIMEZONE_SUBPAGE: (undefined|!settings.Route),
- *   DEFAULT_BROWSER: (undefined|!settings.Route),
- *   DETAILED_BUILD_INFO: (undefined|!settings.Route),
- *   DEVICE: (undefined|!settings.Route),
- *   DISPLAY: (undefined|!settings.Route),
- *   DOWNLOADS: (undefined|!settings.Route),
- *   EDIT_DICTIONARY: (undefined|!settings.Route),
- *   EXTERNAL_STORAGE_PREFERENCES: (undefined|!settings.Route),
- *   FINGERPRINT: (undefined|!settings.Route),
- *   FILES: (undefined|!settings.Route),
- *   FONTS: (undefined|!settings.Route),
- *   GOOGLE_ASSISTANT: (undefined|!settings.Route),
- *   IMPORT_DATA: (undefined|!settings.Route),
- *   INCOMPATIBLE_APPLICATIONS: (undefined|!settings.Route),
- *   INPUT_METHODS: (undefined|!settings.Route),
- *   INTERNET: (undefined|!settings.Route),
- *   INTERNET_NETWORKS: (undefined|!settings.Route),
- *   KERBEROS_ACCOUNTS: (undefined|!settings.Route),
- *   KEYBOARD: (undefined|!settings.Route),
- *   KNOWN_NETWORKS: (undefined|!settings.Route),
- *   LANGUAGES: (undefined|!settings.Route),
- *   LANGUAGES_DETAILS: (undefined|!settings.Route),
- *   LOCK_SCREEN: (undefined|!settings.Route),
- *   MANAGE_ACCESSIBILITY: (undefined|!settings.Route),
- *   MANAGE_CAPTION_SETTINGS: (undefined|!settings.Route),
- *   MANAGE_PROFILE: (undefined|!settings.Route),
- *   MANAGE_SWITCH_ACCESS_SETTINGS: (undefined|!settings.Route),
- *   MANAGE_TTS_SETTINGS: (undefined|!settings.Route),
- *   MULTIDEVICE: (undefined|!settings.Route),
- *   MULTIDEVICE_FEATURES: (undefined|!settings.Route),
- *   NETWORK_DETAIL: (undefined|!settings.Route),
- *   ON_STARTUP: (undefined|!settings.Route),
- *   OS_SYNC: (undefined|!settings.Route),
- *   PASSWORDS: (undefined|!settings.Route),
- *   PAYMENTS: (undefined|!settings.Route),
- *   PEOPLE: (undefined|!settings.Route),
- *   PERSONALIZATION: (undefined|!settings.Route),
- *   PLUGIN_VM: (undefined|!settings.Route),
- *   PLUGIN_VM_DETAILS: (undefined|!settings.Route),
- *   PLUGIN_VM_SHARED_PATHS: (undefined|!settings.Route),
- *   POINTERS: (undefined|!settings.Route),
- *   POWER: (undefined|!settings.Route),
- *   PRINTING: (undefined|!settings.Route),
- *   PRIVACY: (undefined|!settings.Route),
- *   RESET: (undefined|!settings.Route),
- *   RESET_DIALOG: (undefined|!settings.Route),
- *   SEARCH: (undefined|!settings.Route),
- *   SEARCH_ENGINES: (undefined|!settings.Route),
- *   SECURITY: (undefined|!settings.Route),
- *   SECURITY_KEYS: (undefined|!settings.Route),
- *   SIGN_OUT: (undefined|!settings.Route),
- *   SITE_SETTINGS: (undefined|!settings.Route),
- *   SITE_SETTINGS_ADS: (undefined|!settings.Route),
- *   SITE_SETTINGS_ALL: (undefined|!settings.Route),
- *   SITE_SETTINGS_AUTOMATIC_DOWNLOADS: (undefined|!settings.Route),
- *   SITE_SETTINGS_BACKGROUND_SYNC: (undefined|!settings.Route),
- *   SITE_SETTINGS_BLUETOOTH_SCANNING: (undefined|!settings.Route),
- *   SITE_SETTINGS_CAMERA: (undefined|!settings.Route),
- *   SITE_SETTINGS_CLIPBOARD: (undefined|!settings.Route),
- *   SITE_SETTINGS_COOKIES: (undefined|!settings.Route),
- *   SITE_SETTINGS_DATA_DETAILS: (undefined|!settings.Route),
- *   SITE_SETTINGS_FLASH: (undefined|!settings.Route),
- *   SITE_SETTINGS_HANDLERS: (undefined|!settings.Route),
- *   SITE_SETTINGS_HID_DEVICES: (undefined|!settings.Route),
- *   SITE_SETTINGS_IMAGES: (undefined|!settings.Route),
- *   SITE_SETTINGS_MIXEDSCRIPT: (undefined|!settings.Route),
- *   SITE_SETTINGS_JAVASCRIPT: (undefined|!settings.Route),
- *   SITE_SETTINGS_SENSORS: (undefined|!settings.Route),
- *   SITE_SETTINGS_SOUND: (undefined|!settings.Route),
- *   SITE_SETTINGS_LOCATION: (undefined|!settings.Route),
- *   SITE_SETTINGS_MICROPHONE: (undefined|!settings.Route),
- *   SITE_SETTINGS_MIDI_DEVICES: (undefined|!settings.Route),
- *   SITE_SETTINGS_NATIVE_FILE_SYSTEM_WRITE: (undefined|!settings.Route),
- *   SITE_SETTINGS_NOTIFICATIONS: (undefined|!settings.Route),
- *   SITE_SETTINGS_PAYMENT_HANDLER: (undefined|!settings.Route),
- *   SITE_SETTINGS_PDF_DOCUMENTS: (undefined|!settings.Route),
- *   SITE_SETTINGS_POPUPS: (undefined|!settings.Route),
- *   SITE_SETTINGS_PROTECTED_CONTENT: (undefined|!settings.Route),
- *   SITE_SETTINGS_SITE_DATA: (undefined|!settings.Route),
- *   SITE_SETTINGS_SITE_DETAILS: (undefined|!settings.Route),
- *   SITE_SETTINGS_UNSANDBOXED_PLUGINS: (undefined|!settings.Route),
- *   SITE_SETTINGS_USB_DEVICES: (undefined|!settings.Route),
- *   SITE_SETTINGS_SERIAL_PORTS: (undefined|!settings.Route),
- *   SITE_SETTINGS_ZOOM_LEVELS: (undefined|!settings.Route),
- *   SMART_LOCK: (undefined|!settings.Route),
- *   SMB_SHARES: (undefined|!settings.Route),
- *   STORAGE: (undefined|!settings.Route),
- *   STYLUS: (undefined|!settings.Route),
- *   SYNC: (undefined|!settings.Route),
- *   SYNC_ADVANCED: (undefined|!settings.Route),
- *   SYSTEM: (undefined|!settings.Route),
- *   TRIGGERED_RESET_DIALOG: (undefined|!settings.Route),
- * }}
- */
-let SettingsRoutes;
-
 cr.define('settings', function() {
-
-  /**
-   * Class for navigable routes. May only be instantiated within this file.
-   */
-  class Route {
-    /** @param {string} path */
-    constructor(path) {
-      /** @type {string} */
-      this.path = path;
-
-      /** @type {?settings.Route} */
-      this.parent = null;
-
-      /** @type {number} */
-      this.depth = 0;
-
-      /**
-       * @type {boolean} Whether this route corresponds to a navigable
-       *     dialog. Those routes don't belong to a "section".
-       */
-      this.isNavigableDialog = false;
-
-      // Below are all legacy properties to provide compatibility with the old
-      // routing system.
-
-      /** @type {string} */
-      this.section = '';
-    }
-
-    /**
-     * Returns a new Route instance that's a child of this route.
-     * @param {string} path Extends this route's path if it doesn't contain a
-     *     leading slash.
-     * @return {!settings.Route}
-     * @private
-     */
-    createChild(path) {
-      assert(path);
-
-      // |path| extends this route's path if it doesn't have a leading slash.
-      // If it does have a leading slash, it's just set as the new route's URL.
-      const newUrl = path[0] == '/' ? path : `${this.path}/${path}`;
-
-      const route = new Route(newUrl);
-      route.parent = this;
-      route.section = this.section;
-      route.depth = this.depth + 1;
-
-      return route;
-    }
-
-    /**
-     * Returns a new Route instance that's a child section of this route.
-     * TODO(tommycli): Remove once we've obsoleted the concept of sections.
-     * @param {string} path
-     * @param {string} section
-     * @return {!settings.Route}
-     * @private
-     */
-    createSection(path, section) {
-      const route = this.createChild(path);
-      route.section = section;
-      return route;
-    }
-
-    /**
-     * Returns the absolute path string for this Route, assuming this function
-     * has been called from within chrome://settings.
-     * @return {string}
-     */
-    getAbsolutePath() {
-      return window.location.origin + this.path;
-    }
-
-    /**
-     * Returns true if this route matches or is an ancestor of the parameter.
-     * @param {!settings.Route} route
-     * @return {boolean}
-     */
-    contains(route) {
-      for (let r = route; r != null; r = r.parent) {
-        if (this == r) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    /**
-     * Returns true if this route is a subpage of a section.
-     * @return {boolean}
-     */
-    isSubpage() {
-      return !!this.parent && !!this.section &&
-          this.parent.section == this.section;
-    }
-  }
-
   /**
    * @return {!SettingsRoutes} Routes that are shared between browser and OS
    *     settings under the same conditions (e.g. in guest mode).
@@ -241,8 +12,8 @@ cr.define('settings', function() {
     const r = {};
 
     // Root pages.
-    r.BASIC = new Route('/');
-    r.ABOUT = new Route('/help');
+    r.BASIC = new settings.Route('/');
+    r.ABOUT = new settings.Route('/help');
 
     r.SIGN_OUT = r.BASIC.createChild('/signOut');
     r.SIGN_OUT.isNavigableDialog = true;
@@ -380,7 +151,7 @@ cr.define('settings', function() {
 
     // Advanced Routes
     if (pageVisibility.advancedSettings !== false) {
-      r.ADVANCED = new Route('/advanced');
+      r.ADVANCED = new settings.Route('/advanced');
 
       r.LANGUAGES = r.ADVANCED.createSection('/languages', 'languages');
       // <if expr="not is_macosx">
@@ -499,7 +270,7 @@ cr.define('settings', function() {
 
     r.GOOGLE_ASSISTANT = r.SEARCH.createChild('/googleAssistant');
 
-    r.ADVANCED = new Route('/advanced');
+    r.ADVANCED = new settings.Route('/advanced');
 
     r.PRIVACY = r.ADVANCED.createSection('/privacy', 'privacy');
 
@@ -570,198 +341,6 @@ cr.define('settings', function() {
   }
   // </if>
 
-  class Router {
-    /** @param {!SettingsRoutes} availableRoutes */
-    constructor(availableRoutes) {
-      /**
-       * List of available routes. This is populated taking into account current
-       * state (like guest mode).
-       * @private {!SettingsRoutes}
-       */
-      this.routes_ = availableRoutes;
-
-      /**
-       * The current active route. This updated is only by settings.navigateTo
-       * or settings.initializeRouteFromUrl.
-       * @type {!settings.Route}
-       */
-      this.currentRoute = /** @type {!settings.Route} */ (this.routes_.BASIC);
-
-      /**
-       * The current query parameters. This is updated only by
-       * settings.navigateTo or settings.initializeRouteFromUrl.
-       * @private {!URLSearchParams}
-       */
-      this.currentQueryParameters_ = new URLSearchParams();
-
-      /** @private {boolean} */
-      this.wasLastRouteChangePopstate_ = false;
-
-      /** @private {boolean}*/
-      this.initializeRouteFromUrlCalled_ = false;
-    }
-
-    /** @return {settings.Route} */
-    getRoute(routeName) {
-      return this.routes_[routeName];
-    }
-
-    /** @return {!SettingsRoutes} */
-    getRoutes() {
-      return this.routes_;
-    }
-
-    /**
-     * Helper function to set the current route and notify all observers.
-     * @param {!settings.Route} route
-     * @param {!URLSearchParams} queryParameters
-     * @param {boolean} isPopstate
-     */
-    setCurrentRoute(route, queryParameters, isPopstate) {
-      this.recordMetrics(route.path);
-
-      const oldRoute = this.currentRoute;
-      this.currentRoute = route;
-      this.currentQueryParameters_ = queryParameters;
-      this.wasLastRouteChangePopstate_ = isPopstate;
-      new Set(routeObservers).forEach((observer) => {
-        observer.currentRouteChanged(this.currentRoute, oldRoute);
-      });
-    }
-
-    /** @return {!settings.Route} */
-    getCurrentRoute() {
-      return this.currentRoute;
-    }
-
-    /** @return {!URLSearchParams} */
-    getQueryParameters() {
-      return new URLSearchParams(
-          this.currentQueryParameters_);  // Defensive copy.
-    }
-
-    /** @return {boolean} */
-    lastRouteChangeWasPopstate() {
-      return this.wasLastRouteChangePopstate_;
-    }
-
-    /**
-     * @param {string} path
-     * @return {?settings.Route} The matching canonical route, or null if none
-     *     matches.
-     */
-    getRouteForPath(path) {
-      // Allow trailing slash in paths.
-      const canonicalPath = path.replace(CANONICAL_PATH_REGEX, '$1$2');
-
-      // TODO(tommycli): Use Object.values once Closure compilation supports it.
-      const matchingKey =
-          Object.keys(this.routes_)
-              .find((key) => this.routes_[key].path == canonicalPath);
-
-      return matchingKey ? this.routes_[matchingKey] : null;
-    }
-
-    /**
-     * Navigates to a canonical route and pushes a new history entry.
-     * @param {!settings.Route} route
-     * @param {URLSearchParams=} opt_dynamicParameters Navigations to the same
-     *     URL parameters in a different order will still push to history.
-     * @param {boolean=} opt_removeSearch Whether to strip the 'search' URL
-     *     parameter during navigation. Defaults to false.
-     */
-    navigateTo(route, opt_dynamicParameters, opt_removeSearch) {
-      // The ADVANCED route only serves as a parent of subpages, and should not
-      // be possible to navigate to it directly.
-      if (route == this.routes_.ADVANCED) {
-        route = /** @type {!settings.Route} */ (this.routes_.BASIC);
-      }
-
-      const params = opt_dynamicParameters || new URLSearchParams();
-      const removeSearch = !!opt_removeSearch;
-
-      const oldSearchParam = this.getQueryParameters().get('search') || '';
-      const newSearchParam = params.get('search') || '';
-
-      if (!removeSearch && oldSearchParam && !newSearchParam) {
-        params.append('search', oldSearchParam);
-      }
-
-      let url = route.path;
-      const queryString = params.toString();
-      if (queryString) {
-        url += '?' + queryString;
-      }
-
-      // History serializes the state, so we don't push the actual route object.
-      window.history.pushState(this.currentRoute.path, '', url);
-      this.setCurrentRoute(route, params, false);
-    }
-
-    /**
-     * Navigates to the previous route if it has an equal or lesser depth.
-     * If there is no previous route in history meeting those requirements,
-     * this navigates to the immediate parent. This will never exit Settings.
-     */
-    navigateToPreviousRoute() {
-      const previousRoute = window.history.state &&
-          assert(this.getRouteForPath(
-              /** @type {string} */ (window.history.state)));
-
-      if (previousRoute && previousRoute.depth <= this.currentRoute.depth) {
-        window.history.back();
-      } else {
-        this.navigateTo(
-            this.currentRoute.parent ||
-            /** @type {!settings.Route} */ (this.routes_.BASIC));
-      }
-    }
-
-    /**
-     * Initialize the route and query params from the URL.
-     */
-    initializeRouteFromUrl() {
-      assert(!this.initializeRouteFromUrlCalled_);
-      this.initializeRouteFromUrlCalled_ = true;
-
-      const route = this.getRouteForPath(window.location.pathname);
-
-      // Record all correct paths entered on the settings page, and
-      // as all incorrect paths are routed to the main settings page,
-      // record all incorrect paths as hitting the main settings page.
-      this.recordMetrics(route ? route.path : this.routes_.BASIC.path);
-
-      // Never allow direct navigation to ADVANCED.
-      if (route && route != this.routes_.ADVANCED) {
-        this.currentRoute = route;
-        this.currentQueryParameters_ =
-            new URLSearchParams(window.location.search);
-      } else {
-        window.history.replaceState(undefined, '', this.routes_.BASIC.path);
-      }
-    }
-
-    /**
-     * Make a UMA note about visiting this URL path.
-     * @param {string} urlPath The url path (only).
-     */
-    recordMetrics(urlPath) {
-      assert(!urlPath.startsWith('chrome://'));
-      assert(!urlPath.startsWith('settings'));
-      assert(urlPath.startsWith('/'));
-      assert(!urlPath.match(/\?/g));
-      chrome.metricsPrivate.recordSparseHashable(
-          'WebUI.Settings.PathVisited', urlPath);
-    }
-
-    resetRouteForTesting() {
-      this.initializeRouteFromUrlCalled_ = false;
-      this.wasLastRouteChangePopstate_ = false;
-      this.currentRoute = /** @type {!settings.Route} */ (this.routes_.BASIC);
-      this.currentQueryParameters_ = new URLSearchParams();
-    }
-  }
-
   /**
    * @return {!settings.Router} A router with at least those routes common to OS
    *     and browser settings. If the window is not in OS settings (based on
@@ -782,18 +361,15 @@ cr.define('settings', function() {
       addOSSettingsRoutes(availableRoutes);
     }
     // </if>
-    return new Router(availableRoutes);
+    return new settings.Router(availableRoutes);
   }
   const routerInstance = buildRouter();
-
-  const routeObservers = new Set();
 
   /** @polymerBehavior */
   const RouteObserverBehavior = {
     /** @override */
     attached: function() {
-      assert(!routeObservers.has(this));
-      routeObservers.add(this);
+      routerInstance.addObserver(this);
 
       // Emulating Polymer data bindings, the observer is called when the
       // element starts observing the route.
@@ -802,7 +378,7 @@ cr.define('settings', function() {
 
     /** @override */
     detached: function() {
-      assert(routeObservers.delete(this));
+      routerInstance.removeObserver(this);
     },
 
     /**
@@ -813,13 +389,6 @@ cr.define('settings', function() {
       assertNotReached();
     },
   };
-
-  /**
-   * Regular expression that captures the leading slash, the content and the
-   * trailing slash in three different groups.
-   * @type {!RegExp}
-   */
-  const CANONICAL_PATH_REGEX = /(^\/)([\/-\w]+)(\/$)/;
 
   window.addEventListener('popstate', function(event) {
     // On pop state, do not push the state onto the window.history again.
@@ -851,8 +420,6 @@ cr.define('settings', function() {
       routerInstance.navigateToPreviousRoute.bind(routerInstance);
 
   return {
-    Route: Route,            // The Route class definition.
-    Router: Router,          // The Router class definition.
     router: routerInstance,  // the singleton.
     buildRouterForTesting: buildRouter,
     routes: routes,
