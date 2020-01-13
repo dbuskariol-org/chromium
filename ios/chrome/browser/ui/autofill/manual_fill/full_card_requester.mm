e39a16c38c69e5d507887d80827fb8e8146d543d
Gauthier Ambard <gambard@chromium.org>
2020-01-13 11:32:24 +0000

    [iOS] Move FullCard requester to autofill
    To have a less dependencies on ui/payments.
    
    Bug: none
    Change-Id: Ife6942e0c52a4eb2d2e80efc746e76da1616589c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995336
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730506}

--- ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/ui/payments/full_card_requester.h"
+#include "ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.h"
 
 #include "components/autofill/core/browser/autofill_manager.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_full_card_requester.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_full_card_requester.mm
@@ -9,7 +9,7 @@
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/ios/browser/autofill_driver_ios.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/full_card_request_result_delegate_bridge.h"
-#include "ios/chrome/browser/ui/payments/full_card_requester.h"
+#include "ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #include "ios/web/public/js_messaging/web_frame.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
