a81727c89f5c3a62489b526c6b5c4eca8b2fffa0
Lindsay Pasricha <lindsayw@google.com>
2020-01-15 22:39:05 +0000

    Disable TestAdjustFontSizeForNestedElements on iphone device
    Bug: 1041873
    Change-Id: I3d8a2a6a9eed43fe5d12680fc061eda880037da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2000958
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732167}

--- ios/chrome/browser/web/font_size_js_unittest.mm
+++ ios/chrome/browser/web/font_size_js_unittest.mm
@@ -255,6 +255,13 @@ TEST_F(FontSizeJsTest, TestAdjustFontSizeForUnit) {
 
 // Tests that __gCrWeb.accessibility.adjustFontSize works for nested elements.
 TEST_F(FontSizeJsTest, TestAdjustFontSizeForNestedElements) {
+// TODO(crbug.com/1041873): Re-enable this test on device.
+#if !TARGET_IPHONE_SIMULATOR
+  if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_TABLET) {
+    return;
+  }
+#endif  // TARGET_IPHONE_SIMULATOR
+
   // TODO(crbug.com/983776): This test fails on ipad since beta5 due to a
   // simulator bug. Re-enable this once the bug is fixed.
   if (base::ios::IsRunningOnIOS13OrLater() &&
