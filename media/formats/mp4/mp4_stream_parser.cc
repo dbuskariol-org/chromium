0f9265df3b36cd39b7fc540f24154b25688cb558
Dale Curtis <dalecurtis@chromium.org>
2020-01-15 01:46:44 +0000

    Port color parsing metadata from Cobalt to Chrome.
    Adds support for specifying the VideoColorSpace and HDRMetadata fields
    in the VideoDecoderConfig. This shouldn't change anything at present
    unless someone sets the the HDRMetadata field for H264 or VP9 in MP4
    on Windows. Otherwise we generally prefer the frame color space
    information.
    
    Signed-off-by: Dale Curtis <dalecurtis@chromium.org>
    Bug: b/143382177
    Change-Id: I39bfb06228f5f5f67b858551e9fe67a74c79d20e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2001248
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731777}

--- media/formats/mp4/mp4_stream_parser.cc
+++ media/formats/mp4/mp4_stream_parser.cc
@@ -59,6 +59,44 @@ EncryptionScheme GetEncryptionScheme(const ProtectionSchemeInfo& sinf) {
   }
   return EncryptionScheme::kUnencrypted;
 }
+
+VideoColorSpace ConvertColorParameterInformationToColorSpace(
+    const ColorParameterInformation& info) {
+  auto primary_id =
+      static_cast<VideoColorSpace::PrimaryID>(info.colour_primaries);
+  auto transfer_id =
+      static_cast<VideoColorSpace::TransferID>(info.transfer_characteristics);
+  auto matrix_id =
+      static_cast<VideoColorSpace::MatrixID>(info.matrix_coefficients);
+
+  // Note that we don't check whether the embedded ids are valid.  We rely on
+  // the underlying video decoder to reject any ids that it doesn't support.
+  return VideoColorSpace(primary_id, transfer_id, matrix_id,
+                         info.full_range ? gfx::ColorSpace::RangeID::FULL
+                                         : gfx::ColorSpace::RangeID::LIMITED);
+}
+
+MasteringMetadata ConvertMdcvToMasteringMetadata(
+    const MasteringDisplayColorVolume& mdcv) {
+  MasteringMetadata mastering_metadata;
+
+  mastering_metadata.primary_r = MasteringMetadata::Chromaticity(
+      mdcv.display_primaries_rx, mdcv.display_primaries_ry);
+  mastering_metadata.primary_g = MasteringMetadata::Chromaticity(
+      mdcv.display_primaries_gx, mdcv.display_primaries_gy);
+  mastering_metadata.primary_b = MasteringMetadata::Chromaticity(
+      mdcv.display_primaries_bx, mdcv.display_primaries_by);
+  mastering_metadata.white_point =
+      MasteringMetadata::Chromaticity(mdcv.white_point_x, mdcv.white_point_y);
+
+  mastering_metadata.luminance_max =
+      static_cast<float>(mdcv.max_display_mastering_luminance);
+  mastering_metadata.luminance_min =
+      static_cast<float>(mdcv.min_display_mastering_luminance);
+
+  return mastering_metadata;
+}
+
 }  // namespace
 
 MP4StreamParser::MP4StreamParser(const std::set<int>& audio_object_types,
@@ -515,6 +553,30 @@ bool MP4StreamParser::ParseMoov(BoxReader* reader) {
                               // SPS/PPS are embedded in the video stream
                               EmptyExtraData(), scheme);
       video_config.set_level(entry.video_codec_level);
+
+      if (entry.color_parameter_information) {
+        video_config.set_color_space_info(
+            ConvertColorParameterInformationToColorSpace(
+                *entry.color_parameter_information));
+
+        if (entry.mastering_display_color_volume ||
+            entry.content_light_level_information) {
+          HDRMetadata hdr_metadata;
+          if (entry.mastering_display_color_volume) {
+            hdr_metadata.mastering_metadata = ConvertMdcvToMasteringMetadata(
+                *entry.mastering_display_color_volume);
+          }
+
+          if (entry.content_light_level_information) {
+            hdr_metadata.max_content_light_level =
+                entry.content_light_level_information->max_content_light_level;
+            hdr_metadata.max_frame_average_light_level =
+                entry.content_light_level_information
+                    ->max_pic_average_light_level;
+          }
+          video_config.set_hdr_metadata(hdr_metadata);
+        }
+      }
 
       DVLOG(1) << "video_track_id=" << video_track_id
                << " config=" << video_config.AsHumanReadableString();
