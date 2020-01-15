b103ae55611bbaa35857e0d9539700eacc39b0dd
Mohamed Amir Yosef <mamir@chromium.org>
2020-01-15 13:09:20 +0000

    [Passwords] Fix DCHECK crash in PasswordPendingView::UpdateDialogButtons
    After recent refactoring in http://crrev.com/c/1990126, the code
    now compute the set label for the OK button even if when no OK button
    is shown.
    
    This caused DCHECK failure. Details are in the linked bug.
    
    This CL fixes this problem.
    
    Bug: 1041494
    Change-Id: If3f7be64e2957511b2d7e2c3e18d113094eb9cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998178
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731949}

--- chrome/browser/ui/views/passwords/password_pending_view.cc
+++ chrome/browser/ui/views/passwords/password_pending_view.cc
@@ -472,9 +472,12 @@ void PasswordPendingView::ReplaceWithPromo() {
 }
 
 void PasswordPendingView::UpdateDialogButtons() {
+  if (sign_in_promo_) {
+    DialogDelegate::set_buttons(ui::DIALOG_BUTTON_NONE);
+    return;
+  }
   DialogDelegate::set_buttons(
-      sign_in_promo_ ? ui::DIALOG_BUTTON_NONE
-                     : (ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL));
+      (ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL));
   DialogDelegate::set_button_label(
       ui::DIALOG_BUTTON_OK,
       l10n_util::GetStringUTF16(model()->IsCurrentStateUpdate()
