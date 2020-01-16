fd086b25130895f60dc0fa1010ce7e3edab98c84
Deepti Gandluri <gdeepti@chromium.org>
2020-01-16 23:50:15 +0000

    Remove V8 flag"--wasm-disable-structured-cloning"
    Change-Id: I4ef5f38e116520a501b8a32a2b5fe6ce85d87237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999105
    Reviewed-by: Ben Smith <binji@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Deepti Gandluri <gdeepti@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732611}

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -141,13 +141,10 @@ RenderProcessImpl::RenderProcessImpl()
   if (base::FeatureList::IsEnabled(features::kWebAssemblyThreads)) {
     constexpr char kFlags[] =
         "--harmony-sharedarraybuffer "
-        "--no-wasm-disable-structured-cloning "
         "--experimental-wasm-threads";
 
     v8::V8::SetFlagsFromString(kFlags, sizeof(kFlags));
   } else {
-    SetV8FlagIfNotFeature(features::kWebAssembly,
-                          "--wasm-disable-structured-cloning");
     SetV8FlagIfFeature(features::kSharedArrayBuffer,
                        "--harmony-sharedarraybuffer");
     SetV8FlagIfNotFeature(features::kSharedArrayBuffer,
