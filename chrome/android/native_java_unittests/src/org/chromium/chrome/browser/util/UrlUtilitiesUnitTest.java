5e462030e4b6c96c073cc6faa7de3bb997b978b9
Michael Thiessen <mthiesse@chromium.org>
2020-01-16 21:31:35 +0000

    Move UrlUtilities unit tests to unit_tests
    In order to migrate java.net.URI to GURL the junit tests that rely on
    java.net.URI need to be moved to a test apk that supports the native
    library.
    
    This change moves the UrlUtilitiesUnitTest unittests and the
    UrlUtilitiesTest from junit and ChromePublicTestApk, respectively, to
    unit_tests (Android only). These tests should be just as fast as they
    were as junit tests (and much faster than chrome_test_apk unit tests).
    
    Bug: 783819
    Change-Id: Ic8d27df818563d8caa8e9e7dbac02b9e4557e811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998001
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732557}

--- chrome/android/native_java_unittests/src/org/chromium/chrome/browser/util/UrlUtilitiesUnitTest.java
+++ chrome/android/native_java_unittests/src/org/chromium/chrome/browser/util/UrlUtilitiesUnitTest.java
@@ -5,19 +5,19 @@
 package org.chromium.chrome.browser.util;
 
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
 
-import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.CalledByNativeJavaTest;
 
 /**
- * Unit tests for non-native part of {@link UrlUtilities}.
+ * Unit tests for {@link UrlUtilities}.
  */
-@RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@SuppressWarnings(value = "AuthLeak")
 public class UrlUtilitiesUnitTest {
-    @Test
+    @CalledByNative
+    private UrlUtilitiesUnitTest() {}
+
+    @CalledByNativeJavaTest
     public void testIsHttpOrHttps() {
         Assert.assertTrue(
                 UrlUtilities.isHttpOrHttps("https://user:pass@awesome.com:9000/bad-scheme/#fake"));
@@ -47,7 +47,7 @@ public class UrlUtilitiesUnitTest {
         Assert.assertFalse(UrlUtilities.isHttpOrHttps(""));
     }
 
-    @Test
+    @CalledByNativeJavaTest
     public void testValidateIntentUrl() {
         // Valid action, hostname, and (empty) path.
         Assert.assertTrue(UrlUtilities.validateIntentUrl(
@@ -115,7 +115,7 @@ public class UrlUtilitiesUnitTest {
                 + "S.goat=&leg;end"));
     }
 
-    @Test
+    @CalledByNativeJavaTest
     public void testStripPath() {
         Assert.assertEquals("https://example.com:9000",
                 UrlUtilities.stripPath("https://user:pass@example.com:9000/path/#extra"));
@@ -125,7 +125,7 @@ public class UrlUtilitiesUnitTest {
         Assert.assertEquals("http://", UrlUtilities.stripPath("http:"));
     }
 
-    @Test
+    @CalledByNativeJavaTest
     public void testStripScheme() {
         // Only scheme gets stripped.
         Assert.assertEquals("cs.chromium.org", UrlUtilities.stripScheme("https://cs.chromium.org"));
@@ -139,5 +139,103 @@ public class UrlUtilitiesUnitTest {
         // Beginning and ending spaces get trimmed.
         Assert.assertEquals(
                 "cs.chromium.org", UrlUtilities.stripScheme("  https://cs.chromium.org  "));
+    }
+
+    @CalledByNativeJavaTest
+    public void testIsAcceptedScheme() {
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("about:awesome"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("data:data"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme(
+                "https://user:pass@awesome.com:9000/bad-scheme/#fake"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("http://awesome.example.com/"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("file://hostname/path/to/file"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("inline:skates.co.uk"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("javascript:alert(1)"));
+        Assert.assertTrue(UrlUtilities.isAcceptedScheme("http://foo.bar/has[square].html"));
+
+        Assert.assertFalse(UrlUtilities.isAcceptedScheme("super:awesome"));
+        Assert.assertFalse(UrlUtilities.isAcceptedScheme("ftp://https:password@example.com/"));
+        Assert.assertFalse(
+                UrlUtilities.isAcceptedScheme("ftp://https:password@example.com/?http:#http:"));
+        Assert.assertFalse(UrlUtilities.isAcceptedScheme(
+                "google-search://https:password@example.com/?http:#http:"));
+        Assert.assertFalse(UrlUtilities.isAcceptedScheme("chrome://http://version"));
+        Assert.assertFalse(UrlUtilities.isAcceptedScheme(""));
+    }
+
+    @CalledByNativeJavaTest
+    public void testIsDownloadableScheme() {
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme("data:data"));
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme(
+                "https://user:pass@awesome.com:9000/bad-scheme:#fake:"));
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme("http://awesome.example.com/"));
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme(
+                "filesystem:https://user:pass@google.com:99/t/foo;bar?q=a#ref"));
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme("blob:https://awesome.example.com/"));
+        Assert.assertTrue(UrlUtilities.isDownloadableScheme("file://hostname/path/to/file"));
+
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("inline:skates.co.uk"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("javascript:alert(1)"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("about:awesome"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("super:awesome"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("ftp://https:password@example.com/"));
+        Assert.assertFalse(
+                UrlUtilities.isDownloadableScheme("ftp://https:password@example.com/?http:#http:"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme(
+                "google-search://https:password@example.com/?http:#http:"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme("chrome://http://version"));
+        Assert.assertFalse(UrlUtilities.isDownloadableScheme(""));
+    }
+
+    @CalledByNativeJavaTest
+    public void testIsValidForIntentFallbackUrl() {
+        Assert.assertTrue(UrlUtilities.isValidForIntentFallbackNavigation(
+                "https://user:pass@awesome.com:9000/bad-scheme:#fake:"));
+        Assert.assertTrue(
+                UrlUtilities.isValidForIntentFallbackNavigation("http://awesome.example.com/"));
+        Assert.assertFalse(UrlUtilities.isValidForIntentFallbackNavigation("inline:skates.co.uk"));
+        Assert.assertFalse(UrlUtilities.isValidForIntentFallbackNavigation("javascript:alert(1)"));
+        Assert.assertFalse(
+                UrlUtilities.isValidForIntentFallbackNavigation("file://hostname/path/to/file"));
+        Assert.assertFalse(UrlUtilities.isValidForIntentFallbackNavigation("data:data"));
+        Assert.assertFalse(UrlUtilities.isValidForIntentFallbackNavigation("about:awesome"));
+        Assert.assertFalse(UrlUtilities.isValidForIntentFallbackNavigation(""));
+    }
+
+    @CalledByNativeJavaTest
+    public void testIsUrlWithinScope() {
+        String scope = "http://www.example.com/sub";
+        Assert.assertTrue(UrlUtilities.isUrlWithinScope(scope, scope));
+        Assert.assertTrue(UrlUtilities.isUrlWithinScope(scope + "/path", scope));
+        Assert.assertTrue(UrlUtilities.isUrlWithinScope(scope + "/a b/path", scope + "/a%20b"));
+
+        Assert.assertFalse(UrlUtilities.isUrlWithinScope("https://www.example.com/sub", scope));
+        Assert.assertFalse(UrlUtilities.isUrlWithinScope(scope, scope + "/inner"));
+        Assert.assertFalse(UrlUtilities.isUrlWithinScope(scope + "/this", scope + "/different"));
+        Assert.assertFalse(
+                UrlUtilities.isUrlWithinScope("http://awesome.example.com", "http://example.com"));
+        Assert.assertFalse(UrlUtilities.isUrlWithinScope(
+                "https://www.google.com.evil.com", "https://www.google.com"));
+    }
+
+    @CalledByNativeJavaTest
+    public void testUrlsMatchIgnoringFragments() {
+        String url = "http://www.example.com/path";
+        Assert.assertTrue(UrlUtilities.urlsMatchIgnoringFragments(url, url));
+        Assert.assertTrue(UrlUtilities.urlsMatchIgnoringFragments(url + "#fragment", url));
+        Assert.assertTrue(
+                UrlUtilities.urlsMatchIgnoringFragments(url + "#fragment", url + "#fragment2"));
+        Assert.assertTrue(UrlUtilities.urlsMatchIgnoringFragments("HTTP://www.example.com/path"
+                        + "#fragment",
+                url + "#fragment2"));
+        Assert.assertFalse(UrlUtilities.urlsMatchIgnoringFragments(
+                url + "#fragment", "http://example.com:443/path#fragment"));
+    }
+
+    @CalledByNativeJavaTest
+    public void testUrlsFragmentsDiffer() {
+        String url = "http://www.example.com/path";
+        Assert.assertFalse(UrlUtilities.urlsFragmentsDiffer(url, url));
+        Assert.assertTrue(UrlUtilities.urlsFragmentsDiffer(url + "#fragment", url));
     }
 }
