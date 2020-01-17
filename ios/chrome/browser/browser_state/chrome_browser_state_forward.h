503a0b71ca5139a9bb63b52f507d9126c41df644
Sylvain Defresne <sdefresne@chromium.org>
2020-01-17 19:02:58 +0000

    [ios] Add header to forward declare ChromeBrowserState
    In preparation of moving ios::ChromeBrowserState to the global
    namespace, add a header to forward declare ChromeBrowserState.
    This can be used to change the forward declaration atomically
    when moving the class without having to change all the clients.
    
    Bug: 1042208
    Change-Id: I9273f83989390b569b74db771d3b1fc00fe9525a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007415
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732939}

---
+++ ios/chrome/browser/browser_state/chrome_browser_state_forward.h
@@ -0,0 +1,16 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BROWSER_STATE_CHROME_BROWSER_STATE_FORWARD_H_
+#define IOS_CHROME_BROWSER_BROWSER_STATE_CHROME_BROWSER_STATE_FORWARD_H_
+
+// This file is there to help move ios::ChromeBrowserState to the global
+// namespace (i.e. rename it to ChromeBrowserState).
+
+namespace ios {
+class ChromeBrowserState;
+enum class ChromeBrowserStateType;
+}  // namespace ios
+
+#endif  // IOS_CHROME_BROWSER_BROWSER_STATE_CHROME_BROWSER_STATE_FORWARD_H_
