4373636f5a9ba8bbd10e2bea03c8628b7228bcc1
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-01-17 11:29:34 +0000

    [Sheriff] Disable v8.runtime_stats.top_25 on Windows
    TBR=mythria
    
    Bug: 1043048
    Change-Id: I84098257798335abaec43a54b59e5f746219eef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006353
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732806}

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -101,11 +101,12 @@ _BLACK_LIST_TEST_MODULES = {
 
 # The list of benchmark names to be excluded from our smoke tests.
 _BLACK_LIST_TEST_NAMES = [
-   'memory.long_running_idle_gmail_background_tbmv2',
-   'tab_switching.typical_25',
-   'UNSCHEDULED_oortonline_tbmv2',
-   'webrtc',  # crbug.com/932036
-   'speedometer2-future' # Flaky on mac, crbug.com/1003776
+    'memory.long_running_idle_gmail_background_tbmv2',
+    'tab_switching.typical_25',
+    'UNSCHEDULED_oortonline_tbmv2',
+    'webrtc',  # crbug.com/932036
+    'speedometer2-future'  # Flaky on mac, crbug.com/1003776
+    'v8.runtime_stats.top_25'  # Fails in Windows, crbug.com/1043048
 ]
 
 
