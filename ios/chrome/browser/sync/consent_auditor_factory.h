83d0316d140da96706ef43cf33b8b15e01c869d5
Sylvain Defresne <sdefresne@chromium.org>
2020-01-20 19:04:26 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/sync/consent_auditor_factory.h
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=msarda@chromium.org
    
    Bug: 1042208
    Change-Id: I5e7941be37469d17ae0610fb475ae10533cd79f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010788
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733425}

--- ios/chrome/browser/sync/consent_auditor_factory.h
+++ ios/chrome/browser/sync/consent_auditor_factory.h
@@ -13,14 +13,11 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 namespace consent_auditor {
 class ConsentAuditor;
 }  // namespace consent_auditor
-
-namespace ios {
-class ChromeBrowserState;
-}  // namespace ios
 
 // Singleton that owns all ConsentAuditors and associates them with
 // ios::ChromeBrowserState.
