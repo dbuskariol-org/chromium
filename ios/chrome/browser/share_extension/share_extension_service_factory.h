5a834bdc2278f07c5773a8617f5048d5ed49c3f4
Sylvain Defresne <sdefresne@chromium.org>
2020-01-20 13:49:16 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/share_extension
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: I42f47878447b96d7cc58d1d754b6f265248e913f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007425
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733341}

--- ios/chrome/browser/share_extension/share_extension_service_factory.h
+++ ios/chrome/browser/share_extension/share_extension_service_factory.h
@@ -10,12 +10,9 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 class ShareExtensionService;
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 // Singleton that creates the ShareExtensionService and associates that service
 // with ios::ChromeBrowserState.
