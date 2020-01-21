d3f98cb964b7b1814bc1da66732836c6e8ab4fe7
Henrique Nakashima <hnakashima@chromium.org>
2020-01-21 16:08:09 +0000

    Remove LegacyChromeFeatureList.
    It is now unused. This was a transitory class to move downstream
    references to ChromeFeatureList without causing breakages.
    
    This is CL 5/5 for moving ChromeFeatureList to the .flags package.
    
    Bug: 1041468
    Change-Id: Iab7b13219bbd9b808c53f23e40d5e5b064ba20f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003450
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733617}

--- chrome/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -23,7 +23,7 @@ public abstract class ChromeFeatureList {
     private static Map<String, Boolean> sTestFeatures;
 
     // Prevent instantiation.
-    protected ChromeFeatureList() {}
+    private ChromeFeatureList() {}
 
     /**
      * Sets the feature flags to use in JUnit tests, since native calls are not available there.
