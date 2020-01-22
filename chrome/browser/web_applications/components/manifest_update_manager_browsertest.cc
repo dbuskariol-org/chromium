1f96120156671715392e186f39caaadd7745c403
Olga Sharonova <olka@chromium.org>
2020-01-22 16:22:47 +0000

    Revert "Move ManifestUpdateManager and ManifestUpdateTask into web_application:common"
    This reverts commit 68ea97aaa0ac2b0557482ab0be23ef846683dc53.
    
    Reason for revert: Speculative; suspected root cause of Deterministic Fuchsia failures.
    
    BUG=1044617
    
    Original change's description:
    > Move ManifestUpdateManager and ManifestUpdateTask into web_application:common
    > 
    > ManifestUpdateManager and ManifestUpdateTask are not used outside of
    > web_applications and are not isolated components, they should live one
    > level up while maintaining independence from the BMO/Extension backend.
    > 
    > This CL is in preparation to add a dependency from
    > ManifestUpdateManager to SystemWebAppManager.
    > 
    > Bug: 1035242
    > Change-Id: I3a5e8e691079619e5b063ac90653dcae6ee3f76f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1973441
    > Auto-Submit: Alan Cutter <alancutter@chromium.org>
    > Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    > Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#733898}
    
    TBR=alancutter@chromium.org,loyso@chromium.org
    
    Change-Id: Ie1a4f075a6515b85effd5c853b82d7daeb2aa7b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1035242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014922
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734033}

--- chrome/browser/web_applications/components/manifest_update_manager_browsertest.cc
+++ chrome/browser/web_applications/components/manifest_update_manager_browsertest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/web_applications/manifest_update_manager.h"
+#include "chrome/browser/web_applications/components/manifest_update_manager.h"
 
 #include <string>
 #include <vector>
