f94bd87cf3ae212fc54022dd156b45f8752ecc20
Rohit Rao <rohitrao@chromium.org>
2020-01-22 15:53:53 +0000

    [ios] Fixes the name of the kTestFeature feature flag.
    BUG=None
    
    Change-Id: I5973080c15ffaa50b01a08cc271950e935ba8fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014102
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734018}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -31,7 +31,8 @@ const base::Feature kForceUnstackedTabstrip{"ForceUnstackedTabstrip",
 const base::Feature kContainedBVC{"ContainedBVC",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kTestFeature{"ContainedBVC",
+const base::Feature kTestFeature{"TestFeature",
                                  base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kClearSyncedData{"ClearSyncedData",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
