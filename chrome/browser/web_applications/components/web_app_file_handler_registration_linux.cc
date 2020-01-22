32a1f8bfdb40f7fe12b7b167fc3d1c59bced0e21
Jay Harris <harrisjay@chromium.org>
2020-01-22 03:02:03 +0000

    WebApps: Implement unregistering file handlers on Linux.
    This will allow us to disable the file handlers for an app on Linux
    without uninstalling it. This is necessary preparation for the origin
    trial, as we need to be able to disable/enable the file handlers as
    the validity of the app's origin trial changes.
    
    Bug: 1028448
    Change-Id: I63eb6582852ff75d3d88181ede07f0cbd010df2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010234
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733859}

--- chrome/browser/web_applications/components/web_app_file_handler_registration_linux.cc
+++ chrome/browser/web_applications/components/web_app_file_handler_registration_linux.cc
@@ -6,6 +6,7 @@
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/app_shortcut_manager.h"
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
 #include "chrome/browser/web_applications/components/web_app_shortcut.h"
@@ -26,6 +27,16 @@ void OnShortcutInfoReceived(std::unique_ptr<ShortcutInfo> info) {
       base::DoNothing());
 }
 
+void UpdateFileHandlerRegistrationInOs(const AppId& app_id, Profile* profile) {
+  // On Linux, file associations are managed through shortcuts in the app menu,
+  // so after enabling or disabling file handling for an app its shortcuts
+  // need to be recreated.
+  AppShortcutManager& shortcut_manager =
+      WebAppProviderBase::GetProviderBase(profile)->shortcut_manager();
+  shortcut_manager.GetShortcutInfoForApp(
+      app_id, base::BindOnce(&OnShortcutInfoReceived));
+}
+
 }  // namespace
 
 bool ShouldRegisterFileHandlersWithOs() {
@@ -37,14 +48,18 @@ void RegisterFileHandlersWithOs(const AppId& app_id,
                                 Profile* profile,
                                 const std::set<std::string>& file_extensions,
                                 const std::set<std::string>& mime_types) {
-  AppShortcutManager& shortcut_manager =
-      WebAppProviderBase::GetProviderBase(profile)->shortcut_manager();
-  shortcut_manager.GetShortcutInfoForApp(
-      app_id, base::BindOnce(OnShortcutInfoReceived));
+  UpdateFileHandlerRegistrationInOs(app_id, profile);
 }
 
 void UnregisterFileHandlersWithOs(const AppId& app_id, Profile* profile) {
-  // TODO(harrisjay): Add support for unregistering file handlers.
+  // If this was triggered as part of the uninstallation process, nothing more
+  // is needed. Uninstalling already cleans up shortcuts (and thus, file
+  // handlers).
+  auto* provider = WebAppProviderBase::GetProviderBase(profile);
+  if (!provider->registrar().IsInstalled(app_id))
+    return;
+
+  UpdateFileHandlerRegistrationInOs(app_id, profile);
 }
 
 }  // namespace web_app
