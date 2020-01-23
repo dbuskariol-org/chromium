5a1b43cd9bd038eeaa55a4615dbbc428b7e9fa05
Mike Wittman <wittman@chromium.org>
2020-01-23 22:35:27 +0000

    [Sampling profiler] Eliminate unneeded Java interfaces for stack_unwinder
    Strips the stack_unwinder module interfaces and contents down to the
    minimal Java required to get the dynamic feature module to work from
    native code. The module will be providing native code interfaces[1],
    which will be consumed by native code, so it doesn't make sense to
    interpose more Java than necessary in between. It's also the case that
    the parts of the DFM Java API that deal with the Play Store can't be
    used within the sandboxed renderer process.
    
    This change exposes via JNI the StackUnwinderModuleProvider's functions
    to query and request installation. This is the only Java-provided
    functionality that will be required for manipulating the module from
    native code.
    
    1. Design doc: https://docs.google.com/document/d/1QB5IzWuVKfoPdPuiyRok2MUEta1FmRLIQkq1wDATA4k
    
    Bug: 1027654
    Change-Id: Ie69e687335a753c17206fc6c81ae981118c698b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003298
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734653}

--- chrome/android/modules/stack_unwinder/internal/entrypoints.cc
+++ chrome/android/modules/stack_unwinder/internal/entrypoints.cc
@@ -4,18 +4,12 @@
 
 #include "base/android/jni_generator/jni_generator_helper.h"
 #include "base/android/jni_utils.h"
-#include "chrome/android/modules/stack_unwinder/internal/jni_registration.h"
+#include "base/profiler/unwinder.h"
 
 extern "C" {
-// This JNI registration method is found and called by module framework code.
+// This JNI registration method is found and called by module framework
+// code. Empty because we have no JNI items to register within the module code.
 JNI_GENERATOR_EXPORT bool JNI_OnLoad_stack_unwinder(JNIEnv* env) {
-  if (!base::android::IsSelectiveJniRegistrationEnabled(env) &&
-      !stack_unwinder::RegisterNonMainDexNatives(env)) {
-    return false;
-  }
-  if (!stack_unwinder::RegisterMainDexNatives(env)) {
-    return false;
-  }
   return true;
 }
 
