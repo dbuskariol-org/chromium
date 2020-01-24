d32b542e3dc9c0cb649bec24494112488743951c
Karan Bhatia <karandeepb@chromium.org>
2020-01-24 22:36:37 +0000

    DNR: Add support for allowAllRequests rules to filter list converter.
    $document exception rules can be implemented using allowAllRequests action type.
    
    BUG=1038831
    
    Change-Id: Iab90eff5923ad76b3091f54479c1cafe33e92a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005113
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735127}

--- extensions/browser/api/declarative_net_request/filter_list_converter/converter_unittest.cc
+++ extensions/browser/api/declarative_net_request/filter_list_converter/converter_unittest.cc
@@ -39,7 +39,7 @@ void TestConversion(std::vector<std::string> filter_list_rules,
   if (write_type == WriteType::kJSONRuleset)
     output_path = output_path.AppendASCII("rules.json");
 
-  ConvertRuleset({input_path}, output_path, write_type, false /* noisy */);
+  ConvertRuleset({input_path}, output_path, write_type, true /* noisy */);
 
   base::FilePath output_json_path =
       temp_dir.GetPath().AppendASCII("rules.json");
@@ -63,7 +63,15 @@ TEST_P(FilterListConverterTest, Convert) {
       "||example.com^|$script,image,font",
       "@@allowed.com$domain=example.com|~sub.example.com",
       "|https://*.abc.com|$match-case,~image,third-party",
-      "abc.com$~third-party"};
+      "abc.com$~third-party",
+      "@@||yahoo.com^$document,subdocument",
+      "@@||yahoo.com^$document",
+
+      // This rule is invalid and should be ignored.
+      "@@||yahoo.com^$document,script",
+
+      "@@||yahoo.com^$subdocument",
+  };
 
   std::string expected_result = R"(
     [ {
@@ -112,6 +120,39 @@ TEST_P(FilterListConverterTest, Convert) {
           "domainType": "firstParty"
        },
        "id": 4,
+       "priority": 1
+    }, {
+       "action": {
+          "type": "allowAllRequests"
+       },
+       "condition": {
+          "isUrlFilterCaseSensitive": false,
+          "resourceTypes": [ "sub_frame", "main_frame" ],
+          "urlFilter": "||yahoo.com^"
+       },
+       "id": 5,
+       "priority": 1
+    }, {
+       "action": {
+          "type": "allowAllRequests"
+       },
+       "condition": {
+          "isUrlFilterCaseSensitive": false,
+          "resourceTypes": [ "main_frame" ],
+          "urlFilter": "||yahoo.com^"
+       },
+       "id": 6,
+       "priority": 1
+    }, {
+       "action": {
+          "type": "allow"
+       },
+       "condition": {
+          "isUrlFilterCaseSensitive": false,
+          "resourceTypes": [ "sub_frame" ],
+          "urlFilter": "||yahoo.com^"
+       },
+       "id": 7,
        "priority": 1
     } ]
 
