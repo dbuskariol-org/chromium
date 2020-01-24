602f2148c8ad7046abeef0d6e3229a861c9c3fbf
Rachel Wong <wrong@chromium.org>
2020-01-24 03:21:57 +0000

    [settings logging] Implement feature logging for quick settings events.
    This CL implements logging functions for user-initiated quick settings changes. The CL that calls these logging functions is at
    https://chromium-review.googlesource.com/c/chromium/src/+/1999973.
    
    Bug: 1014839
    Change-Id: I25ad746f5cfc6b02d784898c72f90e964c1aa1c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015915
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734796}

--- ash/system/machine_learning/user_settings_event_logger.cc
+++ ash/system/machine_learning/user_settings_event_logger.cc
@@ -5,6 +5,7 @@
 #include "ash/system/machine_learning/user_settings_event_logger.h"
 
 #include "ash/shell.h"
+#include "ash/system/night_light/night_light_controller_impl.h"
 #include "base/logging.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
 
@@ -36,9 +37,19 @@ UserSettingsEventLogger* UserSettingsEventLogger::Get() {
 }
 
 UserSettingsEventLogger::UserSettingsEventLogger()
-    : used_cellular_in_session_(false) {}
+    : presenting_session_count_(0),
+      is_recently_presenting_(false),
+      is_recently_fullscreen_(false),
+      used_cellular_in_session_(false),
+      is_playing_audio_(false) {
+  Shell::Get()->AddShellObserver(this);
+  chromeos::CrasAudioHandler::Get()->AddAudioObserver(this);
+}
 
-UserSettingsEventLogger::~UserSettingsEventLogger() = default;
+UserSettingsEventLogger::~UserSettingsEventLogger() {
+  Shell::Get()->RemoveShellObserver(this);
+  chromeos::CrasAudioHandler::Get()->RemoveAudioObserver(this);
+}
 
 void UserSettingsEventLogger::LogNetworkUkmEvent(
     const NetworkStateProperties& network) {
@@ -88,6 +99,127 @@ void UserSettingsEventLogger::LogBluetoothUkmEvent(
 
   PopulateSharedFeatures(&settings_event);
   SendToUkm(settings_event);
+}
+
+void UserSettingsEventLogger::LogNightLightUkmEvent(const bool enabled) {
+  UserSettingsEvent settings_event;
+  auto* const event = settings_event.mutable_event();
+  auto* const features = settings_event.mutable_features();
+
+  event->set_setting_id(UserSettingsEvent::Event::NIGHT_LIGHT);
+  event->set_setting_type(UserSettingsEvent::Event::QUICK_SETTINGS);
+  // Convert the setting state to an int. Some settings have multiple states, so
+  // all setting states are stored as ints.
+  event->set_previous_value(!enabled ? 1 : 0);
+  event->set_current_value(enabled ? 1 : 0);
+
+  const auto& schedule_type =
+      Shell::Get()->night_light_controller()->GetScheduleType();
+  features->set_has_night_light_schedule(
+      schedule_type != NightLightController::ScheduleType::kNone);
+  // TODO(crbug/1014839): Set the |is_after_sunset| feature field.
+
+  PopulateSharedFeatures(&settings_event);
+  SendToUkm(settings_event);
+}
+
+void UserSettingsEventLogger::LogQuietModeUkmEvent(const bool enabled) {
+  UserSettingsEvent settings_event;
+  auto* const event = settings_event.mutable_event();
+
+  event->set_setting_id(UserSettingsEvent::Event::DO_NOT_DISTURB);
+  event->set_setting_type(UserSettingsEvent::Event::QUICK_SETTINGS);
+  // Convert the setting state to an int. Some settings have multiple states, so
+  // all setting states are stored as ints.
+  event->set_previous_value(!enabled ? 1 : 0);
+  event->set_current_value(enabled ? 1 : 0);
+
+  settings_event.mutable_features()->set_is_recently_presenting(
+      is_recently_presenting_);
+
+  PopulateSharedFeatures(&settings_event);
+  SendToUkm(settings_event);
+}
+
+void UserSettingsEventLogger::LogVolumeUkmEvent(const int previous_level,
+                                                const int current_level) {
+  UserSettingsEvent settings_event;
+  auto* const event = settings_event.mutable_event();
+
+  event->set_setting_id(UserSettingsEvent::Event::VOLUME);
+  event->set_setting_type(UserSettingsEvent::Event::QUICK_SETTINGS);
+  event->set_previous_value(previous_level);
+  event->set_current_value(current_level);
+
+  settings_event.mutable_features()->set_is_playing_audio(is_playing_audio_);
+
+  PopulateSharedFeatures(&settings_event);
+  SendToUkm(settings_event);
+}
+
+void UserSettingsEventLogger::LogBrightnessUkmEvent(const int previous_level,
+                                                    const int current_level) {
+  UserSettingsEvent settings_event;
+  auto* const event = settings_event.mutable_event();
+
+  event->set_setting_id(UserSettingsEvent::Event::BRIGHTNESS);
+  event->set_setting_type(UserSettingsEvent::Event::QUICK_SETTINGS);
+  event->set_previous_value(previous_level);
+  event->set_current_value(current_level);
+
+  settings_event.mutable_features()->set_is_recently_fullscreen(
+      is_recently_fullscreen_);
+
+  PopulateSharedFeatures(&settings_event);
+  SendToUkm(settings_event);
+}
+
+void UserSettingsEventLogger::OnCastingSessionStartedOrStopped(
+    const bool started) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (started) {
+    ++presenting_session_count_;
+    is_recently_presenting_ = true;
+    presenting_timer_.Stop();
+  } else {
+    --presenting_session_count_;
+    DCHECK_GE(presenting_session_count_, 0);
+    if (presenting_session_count_ == 0) {
+      presenting_timer_.Start(FROM_HERE, base::TimeDelta::FromMinutes(5), this,
+                              &UserSettingsEventLogger::OnPresentingTimerEnded);
+    }
+  }
+}
+
+void UserSettingsEventLogger::OnPresentingTimerEnded() {
+  is_recently_presenting_ = false;
+}
+
+void UserSettingsEventLogger::OnFullscreenStateChanged(
+    const bool is_fullscreen,
+    aura::Window* /*container*/) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (is_fullscreen) {
+    is_recently_fullscreen_ = true;
+    fullscreen_timer_.Stop();
+  } else {
+    fullscreen_timer_.Start(FROM_HERE, base::TimeDelta::FromMinutes(5), this,
+                            &UserSettingsEventLogger::OnFullscreenTimerEnded);
+  }
+}
+
+void UserSettingsEventLogger::OnFullscreenTimerEnded() {
+  is_recently_fullscreen_ = false;
+}
+
+void UserSettingsEventLogger::OnOutputStarted() {
+  is_playing_audio_ = true;
+}
+
+void UserSettingsEventLogger::OnOutputStopped() {
+  is_playing_audio_ = false;
 }
 
 void UserSettingsEventLogger::PopulateSharedFeatures(UserSettingsEvent* event) {
