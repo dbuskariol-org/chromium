147807796e2bbd28853c41166a5611c2fd294d28
Xing Liu <xingliu@chromium.org>
2020-01-25 03:08:30 +0000

    Announcement notification for M80.
    This CL finishes the implementation of announcement notification. This
    feature targets M80.
    
    We show a notification on browser start up on Android and desktops for
    an important informational update.
    
    TBR=dtrainor@chromium.org, peter@chromium.org, jwd@chromium.org
    
    Bug: 1042124
    Change-Id: Ib422c789f06382adac4f25efe0b8c0275e128174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018718
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735208}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunStatus.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunStatus.java
@@ -13,6 +13,8 @@ import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
  * Gets and sets preferences related to the status of the first run experience.
  */
 public class FirstRunStatus {
+    // Whether the first run flow is triggered in the current browser session.
+    private static boolean sFirstRunTriggered;
 
     /**
      * Sets the "main First Run Experience flow complete" preference.
@@ -35,6 +37,22 @@ public class FirstRunStatus {
         }
         return CommandLine.getInstance().hasSwitch(
                 ChromeSwitches.FORCE_FIRST_RUN_FLOW_COMPLETE_FOR_TESTING);
+    }
+
+    /**
+     * Sets whether the first run flow is triggered in the current browser session.
+     * @param triggered
+     * @return
+     */
+    public static void setFirstRunTriggered(boolean triggered) {
+        sFirstRunTriggered = triggered;
+    }
+
+    /**
+     * Returns whether first run flow is triggered in the current browser session.
+     */
+    public static boolean isFirstRunTriggered() {
+        return sFirstRunTriggered;
     }
 
     /**
