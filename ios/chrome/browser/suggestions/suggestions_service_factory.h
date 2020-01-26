0c5f6cb968f62e5743e12ddc8eb79ffc3d2e1f68
Sylvain Defresne <sdefresne@chromium.org>
2020-01-26 14:23:40 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/suggestions
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1042208
    Change-Id: I570f09c12bf770f8b1f30ec527b26d0604c463df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017514
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735275}

--- ios/chrome/browser/suggestions/suggestions_service_factory.h
+++ ios/chrome/browser/suggestions/suggestions_service_factory.h
@@ -10,7 +10,8 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
+class ChromeBrowserState;
 
 namespace suggestions {
 
@@ -24,7 +25,7 @@ class SuggestionsServiceFactory : public BrowserStateKeyedServiceFactory {
 
   // Returns the SuggestionsService for |browser_state|.
   static SuggestionsService* GetForBrowserState(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
 
  private:
   friend class base::NoDestructor<SuggestionsServiceFactory>;
