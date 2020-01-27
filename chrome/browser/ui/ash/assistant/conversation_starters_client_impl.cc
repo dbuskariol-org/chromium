cb418174fefe638c34888eba83a117d1ac21a397
David Black <dmblack@google.com>
2020-01-27 22:25:07 +0000

    Implements loader for conversation starters.
    This CL adds the loader and makes the server request but does not yet
    parse the response. That will be done in the next CL.
    
    Bug: b:148239201
    Change-Id: I7f8582f3f0335ce5b3f193ff21791ca2a6ee7348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018387
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735594}

--- chrome/browser/ui/ash/assistant/conversation_starters_client_impl.cc
+++ chrome/browser/ui/ash/assistant/conversation_starters_client_impl.cc
@@ -4,10 +4,100 @@
 
 #include "chrome/browser/ui/ash/assistant/conversation_starters_client_impl.h"
 
+#include <memory>
+#include <string>
 #include <vector>
 
 #include "ash/public/cpp/assistant/conversation_starter.h"
+#include "base/i18n/rtl.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile.h"
+#include "net/base/url_util.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+
+namespace {
+
+constexpr char kBaseUrl[] =
+    "https://assistant.google.com/proactivesuggestions/conversationstarters";
+
+constexpr char kLocaleParamKey[] = "hl";
+
+constexpr int kMaxBodySizeBytes = 10 * 1024;
+
+constexpr net::NetworkTrafficAnnotationTag kNetworkTrafficAnnotationTag =
+    net::DefineNetworkTrafficAnnotation("conversation_starters_loader", R"(
+          semantics: {
+            sender: "Google Assistant Conversation Starters"
+            description:
+              "The Google Assistant requests conversation starters to suggest "
+              "potential actions for the user at the start of a new UI session."
+            trigger:
+              "Start of a new Assistant UI session."
+            destination: GOOGLE_OWNED_SERVICE
+          }
+          policy: {
+            cookies_allowed: YES
+          })");
+
+// Loader ----------------------------------------------------------------------
+
+class Loader {
+ public:
+  explicit Loader(scoped_refptr<network::SharedURLLoaderFactory> factory)
+      : factory_(factory) {}
+
+  Loader(const Loader& copy) = delete;
+  Loader& operator=(const Loader& assign) = delete;
+
+  ~Loader() {
+    is_destroying_ = true;
+
+    if (callback_)
+      std::move(callback_).Run(std::vector<ash::ConversationStarter>());
+  }
+
+  void Start(ConversationStartersClientImpl::Callback callback) {
+    callback_ = std::move(callback);
+
+    // Setup request.
+    auto resource_request = std::make_unique<network::ResourceRequest>();
+    resource_request->url = net::AppendOrReplaceQueryParameter(
+        GURL(kBaseUrl), kLocaleParamKey, base::i18n::GetConfiguredLocale());
+
+    // Setup loader.
+    loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
+                                               kNetworkTrafficAnnotationTag);
+
+    // Start loading.
+    loader_->DownloadToString(
+        factory_.get(),
+        base::BindOnce(&Loader::OnComplete, weak_factory_.GetWeakPtr()),
+        kMaxBodySizeBytes);
+  }
+
+  bool is_destroying() const { return is_destroying_; }
+
+ private:
+  // TODO(dmblack): Parse conversation starters from |body|.
+  void OnComplete(std::unique_ptr<std::string> body) {
+    if (callback_)
+      std::move(callback_).Run(std::vector<ash::ConversationStarter>());
+  }
+
+  scoped_refptr<network::SharedURLLoaderFactory> const factory_;
+  ConversationStartersClientImpl::Callback callback_;
+  std::unique_ptr<network::SimpleURLLoader> loader_;
+
+  bool is_destroying_ = false;
+
+  base::WeakPtrFactory<Loader> weak_factory_{this};
+};
+
+}  // namespace
+
+// ConversationStartersClientImpl ----------------------------------------------
 
 ConversationStartersClientImpl::ConversationStartersClientImpl(Profile* profile)
     : profile_(profile) {
@@ -16,8 +106,21 @@ ConversationStartersClientImpl::ConversationStartersClientImpl(Profile* profile)
 
 ConversationStartersClientImpl::~ConversationStartersClientImpl() = default;
 
-// TODO(dmblack): Fetch conversation starters from the server.
 void ConversationStartersClientImpl::FetchConversationStarters(
     Callback callback) {
-  std::move(callback).Run(std::vector<ash::ConversationStarter>());
+  // Initialize and start a loader to fetch conversation starters from the
+  // server. The created pointer will be deleted on load completion.
+  auto* loader = new Loader(profile_->GetURLLoaderFactory());
+  loader->Start(base::BindOnce(
+      [](Loader* loader, Callback callback,
+         std::vector<ash::ConversationStarter>&& conversation_starters) {
+        // Pass on |conversation_starters| to the original |callback|.
+        std::move(callback).Run(std::move(conversation_starters));
+
+        // We only delete |loader| if this code is *not* being run as part
+        // of its destruction sequence.
+        if (!loader->is_destroying())
+          delete loader;
+      },
+      base::Unretained(loader), std::move(callback)));
 }
