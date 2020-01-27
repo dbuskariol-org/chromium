43e9c379cdc914467b18c541c7b26f471169cef3
Sylvain Defresne <sdefresne@chromium.org>
2020-01-27 18:31:57 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/invalidation
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 1042208
    Change-Id: I3461ae666e4338e945492df669a39f0fbeb445bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017526
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735478}

--- ios/chrome/browser/invalidation/ios_chrome_deprecated_profile_invalidation_provider_factory.h
+++ ios/chrome/browser/invalidation/ios_chrome_deprecated_profile_invalidation_provider_factory.h
@@ -10,7 +10,8 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
+class ChromeBrowserState;
 
 namespace invalidation {
 class ProfileInvalidationProvider;
@@ -28,7 +29,7 @@ class IOSChromeDeprecatedProfileInvalidationProviderFactory
   // Returns the ProfileInvalidationProvider for the given |browser_state|,
   // lazily creating one first if required.
   static invalidation::ProfileInvalidationProvider* GetForBrowserState(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
 
   static IOSChromeDeprecatedProfileInvalidationProviderFactory* GetInstance();
 
