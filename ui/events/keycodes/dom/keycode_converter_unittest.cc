2f74d0edc619b063c26b647a50fbed5b9051084e
Wez <wez@chromium.org>
2020-01-27 16:47:38 +0000

    Revert "Disable tests for notouch builds"
    This reverts commit 312152d09848f4069ce6bae6b644c5eb73a7fe6c.
    
    Reason for revert: The underlying issue can be resolved in a separate
    layer, so it's no longer necessary to disable these tests.
    
    Original change's description:
    > Disable tests for notouch builds
    >
    > These tests are currently expected to fail in notouch.
    > In this CL I've disabled and left a comment pointing to
    > the associated bug.
    >
    > Bug: 956756
    > Change-Id: I070da0ef5ae5a0fa1b03b3090d40aa3adf7c19f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673918
    > Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#672398}
    
    TBR=garykac@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 956756, 1045960
    Change-Id: I88537e3f233695ca228efdd7668aac2bbe2b96f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020930
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735431}

--- ui/events/keycodes/dom/keycode_converter_unittest.cc
+++ ui/events/keycodes/dom/keycode_converter_unittest.cc
@@ -53,8 +53,6 @@ const uint32_t kUsbNonExistentKeycode = 0xffffff;
 const uint32_t kUsbUsBackslash =        0x070031;
 const uint32_t kUsbNonUsHash =          0x070032;
 
-// TODO(crbug.com/956756)
-#if !defined(NOTOUCH_BUILD)
 TEST(UsbKeycodeMap, KeycodeConverterData) {
   // This test looks at all kinds of supported native codes.
   // Verify that there are no duplicate entries in the mapping.
@@ -110,7 +108,6 @@ TEST(UsbKeycodeMap, EvdevXkb) {
     }
   }
 }
-#endif
 
 TEST(UsbKeycodeMap, Basic) {
   // Verify that the first element in the table is the "invalid" code.
