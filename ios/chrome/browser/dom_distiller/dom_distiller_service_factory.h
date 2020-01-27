49838d1f70ff7c3a801a784dae4d3d7a8bde6902
Sylvain Defresne <sdefresne@chromium.org>
2020-01-27 11:28:59 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/dom_distiller
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: I1e39b9c33f4b1a99c713f560e10c5d759dd25b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017329
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735364}

--- ios/chrome/browser/dom_distiller/dom_distiller_service_factory.h
+++ ios/chrome/browser/dom_distiller/dom_distiller_service_factory.h
@@ -10,7 +10,8 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
+class ChromeBrowserState;
 
 namespace dom_distiller {
 class DomDistillerService;
@@ -22,7 +23,7 @@ class DomDistillerServiceFactory : public BrowserStateKeyedServiceFactory {
  public:
   static DomDistillerServiceFactory* GetInstance();
   static DomDistillerService* GetForBrowserState(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
 
  private:
   friend class base::NoDestructor<DomDistillerServiceFactory>;
