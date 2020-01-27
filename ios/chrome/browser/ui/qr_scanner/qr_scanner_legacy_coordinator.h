c3acc5c50675aa17a50a82377179de1ab1199a64
Sylvain Defresne <sdefresne@chromium.org>
2020-01-27 11:12:16 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/qr_scanner
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: I2379c1b78e2088aa448ab50b2156c99f545d8139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017363
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735362}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.h
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.h
@@ -17,8 +17,7 @@
 
 // Unavailable, use -initWithBaseViewController:browser:.
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
+                              browserState:(ChromeBrowserState*)browserState
     NS_UNAVAILABLE;
 
 @end
