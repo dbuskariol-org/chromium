1acdfe3cf6ab59f45bcac290c684d6cd058181b7
Sylvain Defresne <sdefresne@chromium.org>
2020-01-27 11:00:54 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/share_extension
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: I8681d8f472f14a1dbc22e680b236581e02093c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017503
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735358}

--- ios/chrome/browser/share_extension/share_extension_service_factory.h
+++ ios/chrome/browser/share_extension/share_extension_service_factory.h
@@ -10,18 +10,18 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
+class ChromeBrowserState;
 class ShareExtensionService;
 
 // Singleton that creates the ShareExtensionService and associates that service
-// with ios::ChromeBrowserState.
+// with ChromeBrowserState.
 class ShareExtensionServiceFactory : public BrowserStateKeyedServiceFactory {
  public:
   static ShareExtensionService* GetForBrowserState(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
   static ShareExtensionService* GetForBrowserStateIfExists(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
   static ShareExtensionServiceFactory* GetInstance();
 
  private:
