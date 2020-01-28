31f93aa292d22e01cf6df87026eb80bc05414dca
Omer Katz <omerkatz@chromium.org>
2020-01-28 12:09:49 +0000

    heap: Check empty or deleted keys on local copy
    Tracing an HashTable involves checking whether keys are empty or
    deleted. This can result in data races with the mutator if the
    HashTable is modified while it is traced.
    To avoid such data races, we create a local copy of the key on the
    concurrent thread and perform the check on the copy.
    
    Using the local copy on the mutator thread would result in significant
    regressions. Measured locally on the facebook_infinite_scroll:2018 story
    from v8.browsind_desktop benchmark, showed an increase ranging from 70%
    to 110% in the time it takes to process ephemerons (this measurement
    doesn't cover all HashTables, just the ephemeron HashTable).
    To avoid these regressions, the local copy is used only for concurrent
    visitors.
    
    For further details see
    https://drive.google.com/open?id=13DTRcF3xfVjJ2QaKzAOYFzqvTfGRNiX6WWL3CCACSoM
    
    Bug: 986235
    Change-Id: Ie135c1076bd1834ae7f8b97ca6446890fc30a02c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012961
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735833}

--- third_party/blink/renderer/platform/heap/trace_traits.h
+++ third_party/blink/renderer/platform/heap/trace_traits.h
@@ -483,7 +483,19 @@ struct TraceHashTableBackingInCollectionTrait {
     // Use the payload size as recorded by the heap to determine how many
     // elements to trace.
     size_t length = header->PayloadSize() / sizeof(Value);
+    const bool is_concurrent = visitor->IsConcurrent();
     for (size_t i = 0; i < length; ++i) {
+      // If tracing concurrently, use a concurrent-safe version of
+      // IsEmptyOrDeletedBucket (check performed on a local copy instead
+      // of directly on the bucket).
+      if (is_concurrent &&
+          !HashTableHelper<Value, typename Table::ExtractorType,
+                           typename Table::KeyTraitsType>::
+              IsEmptyOrDeletedBucketSafe(array[i])) {
+        blink::TraceCollectionIfEnabled<WeakHandling, Value, Traits>::Trace(
+            visitor, &array[i]);
+        continue;
+      }
       if (!HashTableHelper<Value, typename Table::ExtractorType,
                            typename Table::KeyTraitsType>::
               IsEmptyOrDeletedBucket(array[i])) {
@@ -494,7 +506,6 @@ struct TraceHashTableBackingInCollectionTrait {
     return false;
   }
 };
-
 template <typename Table>
 struct TraceInCollectionTrait<kNoWeakHandling,
                               blink::HeapHashTableBacking<Table>,
@@ -504,7 +515,6 @@ struct TraceInCollectionTrait<kNoWeakHandling,
                                                   Table>::Trace(visitor, self);
   }
 };
-
 template <typename Table>
 struct TraceInCollectionTrait<kWeakHandling,
                               blink::HeapHashTableBacking<Table>,
@@ -550,11 +560,21 @@ struct TraceInCollectionTrait<
     blink::HeapObjectHeader* header =
         blink::HeapObjectHeader::FromPayload(self);
     size_t length = header->PayloadSize() / sizeof(Node*);
+    const bool is_concurrent = visitor->IsConcurrent();
     for (size_t i = 0; i < length; ++i) {
-      if (!HashTableHelper<Node*, typename Table::ExtractorType,
-                           typename Table::KeyTraitsType>::
-              IsEmptyOrDeletedBucket(array[i])) {
-        visitor->Trace(array[i]);
+      Node* node;
+      if (is_concurrent) {
+        // If tracing concurrently, IsEmptyOrDeletedBucket can cause data
+        // races. Loading array[i] atomically prevents possible data races.
+        // array[i] is of type Node* so can directly loaded atomically.
+        node = AsAtomicPtr(&array[i])->load(std::memory_order_relaxed);
+      } else {
+        node = array[i];
+      }
+      if (!HashTableHelper<
+              Node*, typename Table::ExtractorType,
+              typename Table::KeyTraitsType>::IsEmptyOrDeletedBucket(node)) {
+        visitor->Trace(node);
       }
     }
     return false;
