4525e6a9becb55ac34597ca1693bbddda1af58cf
Evan Stade <estade@chromium.org>
2020-01-30 17:37:14 +0000

    Split embedder-specific behavior off from JavaScriptDialogTabHelper
    This will allow us to componentize JavaScriptDialogTabHelper and reuse
    it for WebLayer.
    
    Bug: 1045499,1025256
    Change-Id: I07d6b2123a00898a8aa03400b4b60c203b694252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023397
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736922}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -115,12 +115,14 @@
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/ui/android/context_menu_helper.h"
 #include "chrome/browser/ui/android/view_android_helper.h"
+#include "chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper_delegate_android.h"
 #else
 #include "chrome/browser/banners/app_banner_manager_desktop.h"
 #include "chrome/browser/safe_browsing/safe_browsing_tab_observer.h"
 #include "chrome/browser/tab_contents/form_interaction_tab_helper.h"
 #include "chrome/browser/ui/bookmarks/bookmark_tab_helper.h"
 #include "chrome/browser/ui/intent_picker_tab_helper.h"
+#include "chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper_delegate_desktop.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/search/search_tab_helper.h"
 #include "chrome/browser/ui/sync/browser_synced_tab_delegate.h"
@@ -305,7 +307,9 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
     banners::AppBannerManagerAndroid::CreateForWebContents(web_contents);
   }
   ContextMenuHelper::CreateForWebContents(web_contents);
-  JavaScriptDialogTabHelper::CreateForWebContents(web_contents);
+  JavaScriptDialogTabHelper::CreateForWebContents(
+      web_contents,
+      std::make_unique<JavaScriptDialogTabHelperDelegateAndroid>(web_contents));
   if (OomInterventionTabHelper::IsEnabled()) {
     OomInterventionTabHelper::CreateForWebContents(web_contents);
   }
@@ -322,7 +326,9 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   FormInteractionTabHelper::CreateForWebContents(web_contents);
   FramebustBlockTabHelper::CreateForWebContents(web_contents);
   IntentPickerTabHelper::CreateForWebContents(web_contents);
-  JavaScriptDialogTabHelper::CreateForWebContents(web_contents);
+  JavaScriptDialogTabHelper::CreateForWebContents(
+      web_contents,
+      std::make_unique<JavaScriptDialogTabHelperDelegateDesktop>(web_contents));
   ManagePasswordsUIController::CreateForWebContents(web_contents);
   pdf::PDFWebContentsHelper::CreateForWebContentsWithClient(
       web_contents, std::make_unique<ChromePDFWebContentsHelperClient>());
