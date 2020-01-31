09c7229020364b56739fc20ddc39fcbfb63a0ef3
Peter Boström <pbos@chromium.org>
2020-01-31 03:07:39 +0000

    Migrate ImeWarningBubbleView to use container
    This makes sure the ImeWarningBubbleView can show (and not crash :))
    when ExtensionsToolbarMenu is enabled.
    
    Also fixes ImeWarningBubbleTest.* when ExtensionsToolbarMenu is enabled.
    
    Bug: chromium:943702, chromium:984654
    Change-Id: Idf8bdd14792201eba421547b8ccd0543817e8ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028186
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737188}

--- chrome/browser/ui/views/ime/ime_warning_bubble_view.h
+++ chrome/browser/ui/views/ime/ime_warning_bubble_view.h
@@ -11,7 +11,6 @@
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar_observer.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
-class BrowserActionsContainer;
 class BrowserView;
 enum class ImeWarningBubblePermissionStatus;
 
@@ -69,7 +68,7 @@ class ImeWarningBubbleView : public views::BubbleDialogDelegateView,
   bool IsToolbarAnimating();
 
   const extensions::Extension* extension_;
-  BrowserView* browser_view_;
+  BrowserView* const browser_view_;
 
   // Saves the Browser instance of the browser view, which will be used in
   // OnBrowserRemoved(), as browser_view_->browser() may be null when
@@ -86,10 +85,6 @@ class ImeWarningBubbleView : public views::BubbleDialogDelegateView,
 
   // True if the warning bubble has been shown.
   bool bubble_has_shown_ = false;
-
-  BrowserActionsContainer* container_;
-
-  ToolbarActionsBar* toolbar_actions_bar_;
 
   ScopedObserver<ToolbarActionsBar, ToolbarActionsBarObserver>
       toolbar_actions_bar_observer_{this};
