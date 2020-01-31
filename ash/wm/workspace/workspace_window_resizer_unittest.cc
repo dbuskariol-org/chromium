05689693a1d559a3d9871d97f30a617db86d83de
Ioana Pandele <ioanap@chromium.org>
2020-01-31 13:16:40 +0000

    [Sheriff] Disable WorkspaceWindowResizerTest.ResizeHistogram
    This test has been flaky for a few months.
    
    TBR=oshima@chromium.org
    
    Bug: 1047657
    Change-Id: I25ae1733a3407c9f40f907f05505158409437739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2030961
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737303}

--- ash/wm/workspace/workspace_window_resizer_unittest.cc
+++ ash/wm/workspace/workspace_window_resizer_unittest.cc
@@ -1905,7 +1905,7 @@ TEST_F(WorkspaceWindowResizerTest, TouchResizeToEdge_BOTTOM) {
             touch_resize_window_->bounds().ToString());
 }
 
-TEST_F(WorkspaceWindowResizerTest, ResizeHistogram) {
+TEST_F(WorkspaceWindowResizerTest, DISABLED_ResizeHistogram) {
   base::HistogramTester histograms;
   window_->SetBounds(gfx::Rect(20, 30, 400, 60));
   std::unique_ptr<WindowResizer> resizer(
