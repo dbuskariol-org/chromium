ea0fc3962ab5e9e91ccc69ee0a2a4694ae99a2ce
Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
2020-01-31 21:58:10 +0000

    Revert "[3P blocking] Fix tests accessing the ModuleDatabase on the UI thread"
    This reverts commit 57213418516d189f02902e91ef75dbf488c7fbcc.
    
    Reason for revert: Causing compile error in win-chrome builder: https://ci.chromium.org/p/chrome/builders/ci/win-chrome
    
    Original change's description:
    > [3P blocking] Fix tests accessing the ModuleDatabase on the UI thread
    > 
    > The ModuleDatabase was moved to its own sequence but both the
    > IncompatibleApplicationsBrowserTest and ThirdPartyBlockingBrowserTest
    > test fixtures were not updated.
    > 
    > This CL moves the accesses to the ModuleDatabase instance to the right
    > sequence.
    > 
    > Also added an override for the OutOfProcessQuarantine feature to allow
    > local testing. This was not an issue on the bots because they respect
    > the fieldtrial_testing_config.json file.
    > 
    > Bug: 850517
    > Change-Id: I2e8955b33cd7ffc75f74dd56b01df9dec96fc604
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2031742
    > Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    > Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#737448}
    
    TBR=chrisha@chromium.org,pmonette@chromium.org
    
    Change-Id: I6067a3b50bb835025807ff1b81e7787cda35625d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 850517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033634
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737480}

--- chrome/browser/win/conflicts/incompatible_applications_browsertest.cc
+++ chrome/browser/win/conflicts/incompatible_applications_browsertest.cc
@@ -12,7 +12,6 @@
 #include "base/scoped_native_library.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_reg_util_win.h"
 #include "base/threading/thread_restrictions.h"
@@ -28,7 +27,6 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
-#include "components/services/quarantine/public/cpp/quarantine_features_win.h"
 
 // This class allows to wait until the kIncompatibleApplications preference is
 // modified. This can only happen if a new incompatible application is found,
@@ -94,10 +92,8 @@ class IncompatibleApplicationsBrowserTest : public InProcessBrowserTest {
     ASSERT_NO_FATAL_FAILURE(
         registry_override_manager_.OverrideRegistry(HKEY_CURRENT_USER));
 
-    scoped_feature_list_.InitWithFeatures(
-        {features::kIncompatibleApplicationsWarning,
-         quarantine::kOutOfProcessQuarantine},
-        {});
+    scoped_feature_list_.InitAndEnableFeature(
+        features::kIncompatibleApplicationsWarning);
 
     ASSERT_NO_FATAL_FAILURE(CreateModuleList());
     ASSERT_NO_FATAL_FAILURE(InstallThirdPartyApplication());
@@ -202,27 +198,18 @@ IN_PROC_BROWSER_TEST_F(IncompatibleApplicationsBrowserTest,
   if (base::win::GetVersion() < base::win::Version::WIN10)
     return;
 
+  ModuleDatabase* module_database = ModuleDatabase::GetInstance();
+
+  // Speed up the test.
+  module_database->IncreaseInspectionPriority();
+
   // Create the observer early so the change is guaranteed to be observed.
   auto incompatible_applications_observer =
       std::make_unique<IncompatibleApplicationsObserver>();
 
-  base::RunLoop run_loop;
-  ModuleDatabase::GetTaskRunner()->PostTask(
-      FROM_HERE,
-      base::BindLambdaForTesting([module_list_path = GetModuleListPath(),
-                                  quit_closure = run_loop.QuitClosure()]() {
-        ModuleDatabase* module_database = ModuleDatabase::GetInstance();
-
-        // Speed up the test.
-        module_database->IncreaseInspectionPriority();
-
-        // Simulate the download of the module list component.
-        module_database->third_party_conflicts_manager()->LoadModuleList(
-            module_list_path);
-
-        quit_closure.Run();
-      }));
-  run_loop.Run();
+  // Simulate the download of the module list component.
+  module_database->third_party_conflicts_manager()->LoadModuleList(
+      GetModuleListPath());
 
   // Injects the DLL into the process.
   base::ScopedAllowBlockingForTesting scoped_allow_blocking;
--- chrome/browser/win/conflicts/module_database.cc
+++ chrome/browser/win/conflicts/module_database.cc
@@ -118,7 +118,6 @@ scoped_refptr<base::SequencedTaskRunner> ModuleDatabase::GetTaskRunner() {
 
 // static
 ModuleDatabase* ModuleDatabase::GetInstance() {
-  DCHECK(GetTaskRunner()->RunsTasksInCurrentSequence());
   return g_module_database;
 }
 
