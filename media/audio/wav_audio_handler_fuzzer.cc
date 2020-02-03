eca3d7636b249de2e6ed0219c2330b5ca85f6547
Daniel McArdle <dmcardle@chromium.org>
2020-02-03 15:26:28 +0000

    Add media/audio/wav_audio_handler_fuzzer
    This adds fuzzer coverage for wav_audio_handler.cc.
    
    Fuzzer coverage report: https://chromium-coverage.appspot.com/reports/737219_fuzzers_only/linux/chromium/src/media/audio/wav_audio_handler.cc.html
    
    Change-Id: I60176aba90301bcab648029511a27c16c93ef736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033443
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737778}

---
+++ media/audio/wav_audio_handler_fuzzer.cc
@@ -0,0 +1,32 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+
+#include "base/logging.h"
+#include "media/audio/wav_audio_handler.h"
+#include "media/base/audio_bus.h"
+
+struct Environment {
+  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+  base::StringPiece wav_data(reinterpret_cast<const char*>(data), size);
+  std::unique_ptr<media::WavAudioHandler> handler =
+      media::WavAudioHandler::Create(wav_data);
+  if (!handler) {
+    return 0;
+  }
+
+  std::unique_ptr<media::AudioBus> audio_bus =
+      media::AudioBus::Create(handler->num_channels(), handler->total_frames());
+  size_t bytes_written;
+  handler->CopyTo(audio_bus.get(), 0, &bytes_written);
+  return 0;
+}
