90b8dec0602ef0fbebae5bcecce0b2a47d0546c8
Hirokazu Honda <hiroh@chromium.org>
2020-02-03 04:18:12 +0000

    media/gpu/test: Move CompareFramesWithErrorDiff out of VideoFrameHelpers
    Bug: 1044452
    Test: compile image_processor_test
    Change-Id: I474c94707f16ef44a495868a8ef8e380fbbb0a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024391
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737666}

--- media/gpu/test/video_frame_validator.cc
+++ media/gpu/test/video_frame_validator.cc
@@ -13,6 +13,7 @@
 #include "build/build_config.h"
 #include "media/base/video_frame.h"
 #include "media/gpu/buildflags.h"
+#include "media/gpu/test/image_quality_metrics.h"
 #include "media/gpu/test/video_decode_accelerator_unittest_helpers.h"
 #include "media/gpu/video_frame_mapper.h"
 #include "media/gpu/video_frame_mapper_factory.h"
