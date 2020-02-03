bed1e2c3b88d55a84b88bf37f3a7a773c3738906
Marc Treib <treib@chromium.org>
2020-02-03 12:22:38 +0000

    Disable SavePageBrowserTest.SavePageAsMHTML on Windows
    TBR=dtrainor@chromium.org
    
    Bug: 1048100
    Change-Id: I747ea99ddcd9d41b598818784c872460b0591e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033449
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737736}

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -734,7 +734,13 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SecurityLevelHistogram) {
 }
 
 // Tests that a page can be saved as MHTML.
-IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SavePageAsMHTML) {
+// Flaky on Windows, crbug.com/1048100
+#if defined(OS_WIN)
+#define MAYBE_SavePageAsMHTML DISABLED_SavePageAsMHTML
+#else
+#define MAYBE_SavePageAsMHTML SavePageAsMHTML
+#endif
+IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, MAYBE_SavePageAsMHTML) {
   static const int64_t kFileSizeMin = 2758;
   GURL url = NavigateToMockURL("b");
   base::FilePath download_dir = DownloadPrefs::FromDownloadManager(
