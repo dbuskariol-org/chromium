5019665f14ede5e4fc686bc31843a5fb14ced7a6
Ted Meyer <tmathmeyer@chromium.org>
2020-02-04 21:36:19 +0000

    Reland "Call MFShutdown in places where MFStartup is called"
    This reverts commit 5be12bf85db3af2d41398d6c379bfa4d725bb4c9.
    
    Reason for revert: Fixing the issue, and running on more trybots
    
    Original change's description:
    > Revert "Call MFShutdown in places where MFStartup is called"
    > 
    > This reverts commit abf9a1f555cb8fe668d1c5f389811f13c728c9e2.
    > 
    > Reason for revert: Broke tests on Win7 dbg, see linked bug
    > 
    > Original change's description:
    > > Call MFShutdown in places where MFStartup is called
    > > 
    > > Bug: 1012527
    > > Fixed: 1012527
    > > Change-Id: Id9ef248aadb0aa24704dec973b60ea23c9e1f1ed
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032332
    > > Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    > > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#738063}
    > 
    > TBR=xhwang@chromium.org,tmathmeyer@chromium.org
    > 
    > Change-Id: I267213cc06b42769736c7eb337432ade7953de20
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1012527
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036057
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738130}
    
    TBR=xhwang@chromium.org,treib@chromium.org,tmathmeyer@chromium.org
    
    Change-Id: I8f87e933e133d0dcbf9904c002af7738af450f41
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1012527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037811
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738344}

--- media/capture/video/win/video_capture_device_factory_win.cc
+++ media/capture/video/win/video_capture_device_factory_win.cc
@@ -146,10 +146,8 @@ bool PrepareVideoCaptureAttributesMediaFoundation(
   // Once https://bugs.chromium.org/p/chromium/issues/detail?id=791615 is fixed,
   // we must make sure that this method succeeds in capture_unittests context
   // when MediaFoundation is enabled.
-  if (!VideoCaptureDeviceFactoryWin::PlatformSupportsMediaFoundation() ||
-      !InitializeMediaFoundation()) {
+  if (!VideoCaptureDeviceFactoryWin::PlatformSupportsMediaFoundation())
     return false;
-  }
 
   if (FAILED(MFCreateAttributes(attributes, count)))
     return false;
@@ -382,6 +380,7 @@ VideoCaptureDeviceFactoryWin::VideoCaptureDeviceFactoryWin()
     LogVideoCaptureWinBackendUsed(
         VideoCaptureWinBackendUsed::kUsingDirectShowAsFallback);
   } else if (use_media_foundation_) {
+    session_ = InitializeMediaFoundation();
     LogVideoCaptureWinBackendUsed(
         VideoCaptureWinBackendUsed::kUsingMediaFoundationAsDefault);
   } else {
@@ -436,7 +435,8 @@ void VideoCaptureDeviceFactoryWin::GetDeviceDescriptors(
     VideoCaptureDeviceDescriptors* device_descriptors) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  if (use_media_foundation_) {
+  if (use_media_foundation_ && session_) {
+    DCHECK(PlatformSupportsMediaFoundation());
     GetDeviceDescriptorsMediaFoundation(device_descriptors);
     AugmentDescriptorListWithDirectShowOnlyDevices(device_descriptors);
   } else {
