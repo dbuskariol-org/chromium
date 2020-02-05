dbuskariol-org/chromium

commit fec354dcd9f174488e5b4f89968155193fa6506e

Automated Commit: LKGM 12880.0.0 for chromeos.
CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-chrome
CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-pi-arc-chrome
CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-eve-compile-chrome
CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-kevin-compile-chrome

BUG=762641
TBR=chrome-os-gardeners@google.com

Change-Id: I0e3f952babd58e1b019c4e42d6c5099a1faaa145
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037501
Reviewed-by: ChromeOS bot <3su6n15k.default@developer.gserviceaccount.com>
Commit-Queue: ChromeOS bot <3su6n15k.default@developer.gserviceaccount.com>
Cr-Commit-Position: refs/heads/master@{#738320}

chromeos/CHROMEOS_LKGM
@@ -1,1 +1,1 @@
-12878.0.0
+12880.0.0

commit 2c8f8ff6e5dd5bfc38b8f53b2b4fac97faf012fb

Disable all failing capture_unittests after latest Chrome OS LKGM update.
These tests have started failing after fec354dcd9f174488e5b4f89968155193fa6506e.

Tbr: guidou@chromium.org
Bug: 1048934
Change-Id: Id54e609d286cea6c4b2506edc11c3bc1433d46c4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038253
Commit-Queue: Ben Pastene <bpastene@chromium.org>
Reviewed-by: Ben Pastene <bpastene@chromium.org>
Cr-Commit-Position: refs/heads/master@{#738428}

media/capture/video/video_capture_device_unittest.cc
@@ -94,9 +94,11 @@
 #define MAYBE_UsingRealWebcam_AllocateBadSize \
   DISABLED_UsingRealWebcam_AllocateBadSize
 #define MAYBE_UsingRealWebcam_CaptureMjpeg UsingRealWebcam_CaptureMjpeg
-#define MAYBE_UsingRealWebcam_TakePhoto UsingRealWebcam_TakePhoto
-#define MAYBE_UsingRealWebcam_GetPhotoState UsingRealWebcam_GetPhotoState
-#define MAYBE_UsingRealWebcam_CaptureWithSize UsingRealWebcam_CaptureWithSize
+#define MAYBE_UsingRealWebcam_TakePhoto DISABLED_UsingRealWebcam_TakePhoto
+#define MAYBE_UsingRealWebcam_GetPhotoState \
+  DISABLED_UsingRealWebcam_GetPhotoState
+#define MAYBE_UsingRealWebcam_CaptureWithSize \
+  DISABLED_UsingRealWebcam_CaptureWithSize
 #define MAYBE_UsingRealWebcam_CheckPhotoCallbackRelease \
   UsingRealWebcam_CheckPhotoCallbackRelease
 #elif defined(OS_LINUX)
@@ -479,6 +481,8 @@ class VideoCaptureDeviceTest
 // Cause hangs on Windows Debug. http://crbug.com/417824
 #if defined(OS_WIN) && !defined(NDEBUG)
 #define MAYBE_OpenInvalidDevice DISABLED_OpenInvalidDevice
+#elif defined(OS_CHROMEOS)
+#define MAYBE_OpenInvalidDevice DISABLED_OpenInvalidDevice
 #else
 #define MAYBE_OpenInvalidDevice OpenInvalidDevice
 #endif
@@ -707,7 +711,13 @@ void VideoCaptureDeviceTest::RunCaptureMjpegTestCase() {
   device->StopAndDeAllocate();
 }
 
-WRAPPED_TEST_P(VideoCaptureDeviceTest, NoCameraSupportsPixelFormatMax) {
+#if defined(OS_CHROMEOS)
+#define MAYBE_NoCameraSupportsPixelFormatMax \
+  DISABLED_NoCameraSupportsPixelFormatMax
+#else
+#define MAYBE_NoCameraSupportsPixelFormatMax NoCameraSupportsPixelFormatMax
+#endif
+WRAPPED_TEST_P(VideoCaptureDeviceTest, MAYBE_NoCameraSupportsPixelFormatMax) {
   RunTestCase(base::BindOnce(
       &VideoCaptureDeviceTest::RunNoCameraSupportsPixelFormatMaxTestCase,
       base::Unretained(this)));