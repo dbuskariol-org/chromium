d0217fcfd7e3d0d3f4e197e7d851e257a129c28b
Robbie Gibson <rkgibson@google.com>
2020-02-05 20:05:22 +0000

    [iOS] Add UKM metrics to track when users zoom
    Using these metrics, we can track if any particular websites are broken
    when users change their iOS default text size.
    
    Bug: 1046356
    Change-Id: Ia324cce81bf2926affc93c1622fcc6f6cde53dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029515
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#738647}

--- ios/chrome/browser/web/font_size_tab_helper.mm
+++ ios/chrome/browser/web/font_size_tab_helper.mm
@@ -12,13 +12,88 @@
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
+#include "components/ukm/ios/ukm_url_recorder.h"
+#include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/ui/util/dynamic_type_util.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+// Content size category to report UMA metrics.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class IOSContentSizeCategory {
+  kUnspecified = 0,
+  kExtraSmall = 1,
+  kSmall = 2,
+  kMedium = 3,
+  kLarge = 4,
+  kExtraLarge = 5,
+  kExtraExtraLarge = 6,
+  kExtraExtraExtraLarge = 7,
+  kAccessibilityMedium = 8,
+  kAccessibilityLarge = 9,
+  kAccessibilityExtraLarge = 10,
+  kAccessibilityExtraExtraLarge = 11,
+  kAccessibilityExtraExtraExtraLarge = 12,
+  kMaxValue = kAccessibilityExtraExtraExtraLarge,
+};
+
+// Converts a UIKit content size category to a content size category for
+// reporting.
+IOSContentSizeCategory IOSContentSizeCategoryForCurrentUIContentSizeCategory() {
+  UIContentSizeCategory size =
+      UIApplication.sharedApplication.preferredContentSizeCategory;
+  if ([size isEqual:UIContentSizeCategoryUnspecified]) {
+    return IOSContentSizeCategory::kUnspecified;
+  }
+  if ([size isEqual:UIContentSizeCategoryExtraSmall]) {
+    return IOSContentSizeCategory::kExtraSmall;
+  }
+  if ([size isEqual:UIContentSizeCategorySmall]) {
+    return IOSContentSizeCategory::kSmall;
+  }
+  if ([size isEqual:UIContentSizeCategoryMedium]) {
+    return IOSContentSizeCategory::kMedium;
+  }
+  if ([size isEqual:UIContentSizeCategoryLarge]) {
+    return IOSContentSizeCategory::kLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryExtraLarge]) {
+    return IOSContentSizeCategory::kExtraLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryExtraExtraLarge]) {
+    return IOSContentSizeCategory::kExtraExtraLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryExtraExtraExtraLarge]) {
+    return IOSContentSizeCategory::kExtraExtraExtraLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryAccessibilityMedium]) {
+    return IOSContentSizeCategory::kAccessibilityMedium;
+  }
+  if ([size isEqual:UIContentSizeCategoryAccessibilityLarge]) {
+    return IOSContentSizeCategory::kAccessibilityLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryAccessibilityExtraLarge]) {
+    return IOSContentSizeCategory::kAccessibilityExtraLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryAccessibilityExtraExtraLarge]) {
+    return IOSContentSizeCategory::kAccessibilityExtraExtraLarge;
+  }
+  if ([size isEqual:UIContentSizeCategoryAccessibilityExtraExtraExtraLarge]) {
+    return IOSContentSizeCategory::kAccessibilityExtraExtraExtraLarge;
+  }
+
+  return IOSContentSizeCategory::kUnspecified;
+}
+
+}  // namespace
 
 FontSizeTabHelper::~FontSizeTabHelper() {
   // Remove observer in destructor because |this| is captured by the usingBlock
@@ -62,6 +137,19 @@ void FontSizeTabHelper::SetPageFontSize(int size) {
 void FontSizeTabHelper::UserZoom(Zoom zoom) {
   double new_zoom_multiplier = NewMultiplierAfterZoom(zoom).value_or(1);
   StoreCurrentUserZoomMultiplier(new_zoom_multiplier);
+
+  // Track when the user zooms to see if there are certain websites that are
+  // broken when zooming.
+  IOSContentSizeCategory content_size_category =
+      IOSContentSizeCategoryForCurrentUIContentSizeCategory();
+  ukm::UkmRecorder* ukm_recorder = GetApplicationContext()->GetUkmRecorder();
+  ukm::SourceId source_id = ukm::GetSourceIdForWebStateDocument(web_state_);
+  ukm::builders::IOS_PageZoomChanged(source_id)
+      .SetContentSizeCategory(static_cast<int>(content_size_category))
+      .SetUserZoomLevel(GetCurrentUserZoomMultiplier() * 100)
+      .SetOverallZoomLevel(GetFontSize())
+      .Record(ukm_recorder);
+
   SetPageFontSize(GetFontSize());
 }
 
