08a6c7b8df4cb02fde3f2686c8898991c9b60627
Sam Bowen <sgbowen@google.com>
2020-02-05 19:58:08 +0000

    Generate property configurations for schema.org metadata.
    This will be used to validate that property data is the right type as
    described by the schema. This amends an existing script that will use a
    json+ld file checked into third_party.
    
    Bug: 1044250
    Change-Id: I781f4f50a0331655e36a03b7fc26d9c54c0ae87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036892
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Sam Bowen <sgbowen@google.com>
    Cr-Commit-Position: refs/heads/master@{#738644}

--- components/schema_org/generate_schema_org_code.py
+++ components/schema_org/generate_schema_org_code.py
@@ -7,10 +7,10 @@ This script generates C++ objects based on a JSON+LD schema file. Blink uses the
 generated code to scrape schema.org data from web pages.
 """
 
-import argparse
-import json
-import sys
 import os
+import sys
+import json
+import argparse
 
 _current_dir = os.path.dirname(os.path.realpath(__file__))
 # jinja2 is in chromium's third_party directory
@@ -20,11 +20,75 @@ sys.path.insert(
 import jinja2
 from jinja2 import Environment, PackageLoader, select_autoescape
 env = Environment(loader=PackageLoader('generate_schema_org_code', ''))
+env.trim_blocks = True
+env.lstrip_blocks = True
+
+SCHEMA_ORG_PREFIX = 'http://schema.org/'
 
 
-def object_name_from_id(schema_org_id):
+def schema_org_id(object_name):
+    return SCHEMA_ORG_PREFIX + object_name
+
+
+def object_name_from_id(the_id):
     """Get the object name from a schema.org ID."""
-    return schema_org_id[len('http://schema.org/'):]
+    return the_id[len(SCHEMA_ORG_PREFIX):]
+
+
+def get_schema_obj(obj_id, schema):
+    """Search the schema graph for an object with the given ID."""
+    matches = [obj for obj in schema['@graph'] if obj['@id'] == obj_id]
+    return matches[0] if len(matches) == 1 else None
+
+
+def get_root_type(the_class, schema):
+    """Get the base type the class is descended from."""
+    class_obj = get_schema_obj(the_class['@id'], schema)
+    if class_obj is None:
+        return the_class
+
+    if class_obj['@id'] == schema_org_id('Thing'):
+        return class_obj
+    if (class_obj.has_key('@type')
+            and schema_org_id('DataType') in class_obj['@type']):
+        return class_obj
+    if class_obj.has_key('rdfs:subClassOf'):
+        subclass = class_obj['rdfs:subClassOf']
+        # All classes that use multiple inheritance are Thing type.
+        if isinstance(subclass, list):
+            return get_schema_obj(schema_org_id('Thing'), schema)
+        return get_root_type(subclass, schema)
+    return class_obj
+
+
+def parse_property(prop, schema):
+    """Parse out details about the property, including what type it can be."""
+    parsed_prop = {'name': object_name_from_id(prop['@id']), 'thing_types': []}
+
+    if not prop.has_key(schema_org_id('rangeIncludes')):
+        return parsed_prop
+
+    rangeIncludes = prop[schema_org_id('rangeIncludes')]
+    if not isinstance(rangeIncludes, list):
+        rangeIncludes = [rangeIncludes]
+
+    for possible_type in rangeIncludes:
+        root_type = get_root_type(possible_type, schema)
+        if root_type['@id'] == schema_org_id('Thing'):
+            parsed_prop['thing_types'].append(possible_type['@id'])
+        elif root_type['@id'] == schema_org_id('Text'):
+            parsed_prop['has_text'] = True
+        elif root_type['@id'] == schema_org_id('Date'):
+            parsed_prop['has_date'] = True
+        elif root_type['@id'] == schema_org_id('Time'):
+            parsed_prop['has_time'] = True
+        elif root_type['@id'] == schema_org_id('Boolean'):
+            parsed_prop['has_boolean'] = True
+        elif root_type['@id'] == schema_org_id('Number'):
+            parsed_prop['has_number'] = True
+        elif root_type['@id'] == schema_org_id('DateTime'):
+            parsed_prop['has_date_time'] = True
+    return parsed_prop
 
 
 def get_template_vars(schema_file_path):
@@ -38,11 +102,10 @@ def get_template_vars(schema_file_path):
         if thing['@type'] == 'rdfs:Class':
             template_vars['entities'].append(object_name_from_id(thing['@id']))
         elif thing['@type'] == 'rdf:Property':
-            template_vars['properties'].append(
-                object_name_from_id(thing['@id']))
+            template_vars['properties'].append(parse_property(thing, schema))
 
     template_vars['entities'].sort()
-    template_vars['properties'].sort()
+    template_vars['properties'].sort(key=lambda p: p['name'])
 
     return template_vars
 
@@ -50,7 +113,7 @@ def get_template_vars(schema_file_path):
 def generate_file(file_name, template_file, template_vars):
     """Generate and write file given a template and variables to render."""
     template_vars['header_file'] = os.path.basename(
-        template_file[template_file.index('.')])
+        template_file[:template_file.index('.')])
     template_vars['header_guard'] = template_vars['header_file'].upper() + '_H'
     with open(file_name, 'w') as f:
         f.write(env.get_template(template_file).render(template_vars))
--- components/schema_org/generate_schema_org_code_unittest.py
+++ components/schema_org/generate_schema_org_code_unittest.py
@@ -6,11 +6,11 @@
 import sys
 import unittest
 import generate_schema_org_code
+from generate_schema_org_code import schema_org_id
 import os
 
 SRC = os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)
 sys.path.append(os.path.join(SRC, 'third_party', 'pymock'))
-
 import mock
 
 _current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -42,8 +42,75 @@ class GenerateSchemaOrgCodeTest(unittest.TestCase):
             self.assertEqual(
                 generate_schema_org_code.get_template_vars(m_open), {
                     'entities': ['MediaObject'],
-                    'properties': ['propertyName']
+                    'properties': [{
+                        'name': 'propertyName',
+                        'thing_types': []
+                    }]
                 })
+
+    def test_get_root_type_thing(self):
+        thing = {'@id': schema_org_id('Thing')}
+        intangible = {
+            '@id': schema_org_id('Intangible'),
+            'rdfs:subClassOf': thing
+        }
+        structured_value = {
+            '@id': schema_org_id('StructuredValue'),
+            'rdfs:subClassOf': intangible
+        }
+        schema = {'@graph': [thing, intangible, structured_value]}
+
+        self.assertEqual(
+            generate_schema_org_code.get_root_type(structured_value, schema),
+            thing)
+
+    def test_get_root_type_datatype(self):
+        text = {
+            '@id': schema_org_id('Text'),
+            '@type': [schema_org_id('DataType'), 'rdfs:Class']
+        }
+        url = {'@id': schema_org_id('URL'), 'rdfs:subClassOf': text}
+        schema = {'@graph': [url, text]}
+
+        self.assertEqual(
+            generate_schema_org_code.get_root_type(url, schema), text)
+
+    def test_parse_property_identifier(self):
+        thing = {'@id': schema_org_id('Thing')}
+        intangible = {
+            '@id': schema_org_id('Intangible'),
+            'rdfs:subClassOf': thing
+        }
+        structured_value = {
+            '@id': schema_org_id('StructuredValue'),
+            'rdfs:subClassOf': intangible
+        }
+        property_value = {
+            '@id': schema_org_id('PropertyValue'),
+            'rdfs:subClassOf': structured_value
+        }
+        text = {
+            '@id': schema_org_id('Text'),
+            '@type': [schema_org_id('DataType'), 'rdfs:Class']
+        }
+        url = {'@id': schema_org_id('URL'), 'rdfs:subClassOf': text}
+        identifier = {
+            '@id': schema_org_id('Identifier'),
+            schema_org_id('rangeIncludes'): [property_value, url, text]
+        }
+        schema = {
+            '@graph': [
+                thing, intangible, structured_value, property_value, text, url,
+                identifier
+            ]
+        }
+
+        self.assertEqual(
+            generate_schema_org_code.parse_property(identifier, schema), {
+                'name': 'Identifier',
+                'has_text': True,
+                'thing_types': [property_value['@id']]
+            })
 
 
 if __name__ == '__main__':
