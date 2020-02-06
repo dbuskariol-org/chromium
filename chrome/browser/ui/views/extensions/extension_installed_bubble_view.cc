b1eeea08595d53e1344fa8bab2fab09ea651d68a
Elly Fong-Jones <ellyjones@chromium.org>
2020-02-06 18:17:31 +0000

    ExtensionInstalledBubble: exile Model
    This change moves the ExtensionInstalledBubble's model, which was
    previously a collection of anonymous functions in the Views implementation,
    into //cbui/extensions as a separate class with unit tests. It also changes
    the Views implementation to use this new class.
    
    Change-Id: I3e379db88fcd4d40014c74f14c67b155dde3ac63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007508
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739021}

--- chrome/browser/ui/views/extensions/extension_installed_bubble_view.cc
+++ chrome/browser/ui/views/extensions/extension_installed_bubble_view.cc
@@ -23,6 +23,7 @@
 #include "chrome/browser/ui/bubble_anchor_util.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/extensions/extension_installed_bubble.h"
+#include "chrome/browser/ui/extensions/extension_installed_bubble_model.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/sync/bubble_sync_promo_delegate.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
@@ -64,6 +65,7 @@ using extensions::Extension;
 namespace {
 
 const int kRightColumnWidth = 285;
+constexpr gfx::Size kMaxIconSize{43, 43};
 
 views::Label* CreateLabel(const base::string16& text) {
   views::Label* label = new views::Label(text);
@@ -73,51 +75,18 @@ views::Label* CreateLabel(const base::string16& text) {
   return label;
 }
 
-const extensions::ActionInfo* GetActionInfoForExtension(
-    const extensions::Extension* extension) {
-  const extensions::ActionInfo* action_info =
-      extensions::ActionInfo::GetBrowserActionInfo(extension);
-
-  if (!action_info)
-    action_info = extensions::ActionInfo::GetPageActionInfo(extension);
-
-  return action_info;
-}
-
-bool ShouldAnchorToAction(const extensions::Extension* extension) {
-  const auto* info = GetActionInfoForExtension(extension);
-  if (!info)
-    return false;
-
-  switch (info->type) {
-    case extensions::ActionInfo::TYPE_BROWSER:
-    case extensions::ActionInfo::TYPE_PAGE:
-      return true;
-    case extensions::ActionInfo::TYPE_ACTION:
-      return false;
-  }
-}
-
-bool HasOmniboxKeyword(const Extension* extension) {
-  return !extensions::OmniboxInfo::GetKeyword(extension).empty();
-}
-
-bool ShouldAnchorToOmnibox(const extensions::Extension* extension) {
-  return !ShouldAnchorToAction(extension) && HasOmniboxKeyword(extension);
-}
-
-views::View* AnchorViewForBrowser(const extensions::Extension* extension,
+views::View* AnchorViewForBrowser(const ExtensionInstalledBubbleModel* model,
                                   Browser* browser) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
   views::View* reference_view = nullptr;
 
-  if (ShouldAnchorToAction(extension)) {
+  if (model->anchor_to_action()) {
     if (base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu)) {
       ExtensionsToolbarContainer* const container =
           browser_view->toolbar_button_provider()
               ->GetExtensionsToolbarContainer();
       if (container)
-        reference_view = container->GetViewForId(extension->id());
+        reference_view = container->GetViewForId(model->extension_id());
     } else {
       BrowserActionsContainer* container =
           browser_view->toolbar()->browser_actions();
@@ -125,9 +94,9 @@ views::View* AnchorViewForBrowser(const extensions::Extension* extension,
       DCHECK(container);
       DCHECK(!container->animating());
 
-      reference_view = container->GetViewForId(extension->id());
+      reference_view = container->GetViewForId(model->extension_id());
     }
-  } else if (ShouldAnchorToOmnibox(extension)) {
+  } else if (model->anchor_to_omnibox()) {
     reference_view = browser_view->GetLocationBarView()->location_icon_view();
   }
 
@@ -154,129 +123,6 @@ std::unique_ptr<views::View> CreateSigninPromoView(
 #endif
 }
 
-gfx::ImageSkia MakeIconFromBitmap(const SkBitmap& bitmap) {
-  constexpr int kMaxIconSize = 43;
-
-  // Scale down to 43x43, but allow smaller icons (don't scale up).
-  gfx::Size size(bitmap.width(), bitmap.height());
-  if (size.width() > kMaxIconSize || size.height() > kMaxIconSize)
-    size.SetSize(kMaxIconSize, kMaxIconSize);
-
-  return gfx::ImageSkiaOperations::CreateResizedImage(
-      gfx::ImageSkia::CreateFrom1xBitmap(bitmap),
-      skia::ImageOperations::RESIZE_BEST, size);
-}
-
-bool ShouldShowHowToUse(const extensions::Extension* extension) {
-  const auto* info = GetActionInfoForExtension(extension);
-
-  if (HasOmniboxKeyword(extension))
-    return true;
-
-  if (!info)
-    return false;
-
-  if (info->type == extensions::ActionInfo::TYPE_BROWSER ||
-      info->type == extensions::ActionInfo::TYPE_PAGE) {
-    return !info->synthesized;
-  }
-
-  return false;
-}
-
-bool HasCommandKeybinding(const extensions::Extension* extension,
-                          const Browser* browser,
-                          extensions::Command* command = nullptr) {
-  const auto* info = GetActionInfoForExtension(extension);
-  extensions::CommandService* command_service =
-      extensions::CommandService::Get(browser->profile());
-  extensions::Command ignored_command;
-  if (!command)
-    command = &ignored_command;
-
-  if (info->type == extensions::ActionInfo::TYPE_BROWSER) {
-    return command_service->GetBrowserActionCommand(
-        extension->id(), extensions::CommandService::ACTIVE, command, nullptr);
-  } else if (info->type == extensions::ActionInfo::TYPE_PAGE) {
-    return command_service->GetPageActionCommand(
-        extension->id(), extensions::CommandService::ACTIVE, command, nullptr);
-  }
-
-  return false;
-}
-
-bool ShouldShowHowToManage(const extensions::Extension* extension,
-                           const Browser* browser) {
-  const auto* info = GetActionInfoForExtension(extension);
-
-  if (!info)
-    return false;
-
-  switch (info->type) {
-    case extensions::ActionInfo::TYPE_BROWSER:
-    case extensions::ActionInfo::TYPE_PAGE:
-      return !HasCommandKeybinding(extension, browser);
-    case extensions::ActionInfo::TYPE_ACTION:
-      return HasOmniboxKeyword(extension);
-  }
-}
-
-bool ShouldShowKeybinding(const Extension* extension, const Browser* browser) {
-  const auto* info = GetActionInfoForExtension(extension);
-
-  if (!info)
-    return false;
-
-  switch (info->type) {
-    case extensions::ActionInfo::TYPE_BROWSER:
-    case extensions::ActionInfo::TYPE_PAGE:
-      return HasCommandKeybinding(extension, browser);
-    case extensions::ActionInfo::TYPE_ACTION:
-      return false;
-  }
-}
-
-bool ShouldShowSignInPromo(const Extension* extension, const Browser* browser) {
-  return extensions::sync_helper::IsSyncable(extension) &&
-         SyncPromoUI::ShouldShowSyncPromo(browser->profile());
-}
-
-base::string16 GetHowToUseDescription(const Extension* extension,
-                                      const Browser* browser) {
-  int message_id = 0;
-  base::string16 extra;
-  const auto* action_info = GetActionInfoForExtension(extension);
-  extensions::Command command;
-  if (HasCommandKeybinding(extension, browser, &command))
-    extra = command.accelerator().GetShortcutText();
-
-  switch (action_info->type) {
-    case extensions::ActionInfo::TYPE_BROWSER:
-      message_id =
-          extra.empty()
-              ? IDS_EXTENSION_INSTALLED_BROWSER_ACTION_INFO
-              : IDS_EXTENSION_INSTALLED_BROWSER_ACTION_INFO_WITH_SHORTCUT;
-      break;
-    case extensions::ActionInfo::TYPE_PAGE:
-      message_id = extra.empty()
-                       ? IDS_EXTENSION_INSTALLED_PAGE_ACTION_INFO
-                       : IDS_EXTENSION_INSTALLED_PAGE_ACTION_INFO_WITH_SHORTCUT;
-      break;
-    case extensions::ActionInfo::TYPE_ACTION:
-      break;
-  }
-
-  if (message_id == 0 && HasOmniboxKeyword(extension)) {
-    extra = base::UTF8ToUTF16(extensions::OmniboxInfo::GetKeyword(extension));
-    message_id = IDS_EXTENSION_INSTALLED_OMNIBOX_KEYWORD_INFO;
-  }
-
-  if (message_id == 0)
-    return base::string16();
-  return extra.empty() ? l10n_util::GetStringUTF16(message_id)
-                       : l10n_util::GetStringFUTF16(message_id, extra);
-}
-
 }  // namespace
 
 // Provides feedback to the user upon successful installation of an
@@ -294,14 +140,15 @@ class ExtensionInstalledBubbleView : public BubbleSyncPromoDelegate,
   ExtensionInstalledBubbleView(
       BubbleReference reference,
       Browser* browser,
-      scoped_refptr<const extensions::Extension> extension,
-      const SkBitmap& icon);
+      std::unique_ptr<ExtensionInstalledBubbleModel> model);
   ~ExtensionInstalledBubbleView() override;
 
   // Recalculate the anchor position for this bubble.
   void UpdateAnchorView();
 
   void CloseBubble(BubbleCloseReason reason);
+
+  const ExtensionInstalledBubbleModel* model() const { return model_.get(); }
 
  private:
   // views::BubbleDialogDelegateView:
@@ -320,8 +167,8 @@ class ExtensionInstalledBubbleView : public BubbleSyncPromoDelegate,
   BubbleReference bubble_reference_;
 
   Browser* const browser_;
-  const scoped_refptr<const extensions::Extension> extension_;
-  const gfx::ImageSkia icon_;
+  const std::unique_ptr<ExtensionInstalledBubbleModel> model_;
+  gfx::ImageSkia icon_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionInstalledBubbleView);
 };
@@ -329,29 +176,27 @@ class ExtensionInstalledBubbleView : public BubbleSyncPromoDelegate,
 ExtensionInstalledBubbleView::ExtensionInstalledBubbleView(
     BubbleReference bubble_reference,
     Browser* browser,
-    scoped_refptr<const extensions::Extension> extension,
-    const SkBitmap& icon)
+    std::unique_ptr<ExtensionInstalledBubbleModel> model)
     : BubbleDialogDelegateView(nullptr,
-                               ShouldAnchorToOmnibox(extension.get())
+                               model->anchor_to_omnibox()
                                    ? views::BubbleBorder::TOP_LEFT
                                    : views::BubbleBorder::TOP_RIGHT),
       bubble_reference_(bubble_reference),
       browser_(browser),
-      extension_(extension),
-      icon_(MakeIconFromBitmap(icon)) {
+      model_(std::move(model)),
+      icon_(model_->MakeIconOfSize(kMaxIconSize)) {
   chrome::RecordDialogCreation(chrome::DialogIdentifier::EXTENSION_INSTALLED);
   DialogDelegate::set_buttons(ui::DIALOG_BUTTON_NONE);
-  if (ShouldShowSignInPromo(extension_.get(), browser_)) {
+  if (model_->show_sign_in_promo()) {
     DialogDelegate::SetFootnoteView(
         CreateSigninPromoView(browser->profile(), this));
   }
 }
 
-ExtensionInstalledBubbleView::~ExtensionInstalledBubbleView() {}
+ExtensionInstalledBubbleView::~ExtensionInstalledBubbleView() = default;
 
 void ExtensionInstalledBubbleView::UpdateAnchorView() {
-  views::View* reference_view =
-      AnchorViewForBrowser(extension_.get(), browser_);
+  views::View* reference_view = AnchorViewForBrowser(model_.get(), browser_);
   DCHECK(reference_view);
   SetAnchorView(reference_view);
 }
@@ -368,7 +213,7 @@ void ExtensionInstalledBubbleView::CloseBubble(BubbleCloseReason reason) {
 
 base::string16 ExtensionInstalledBubbleView::GetWindowTitle() const {
   // Add the heading (for all options).
-  base::string16 extension_name = base::UTF8ToUTF16(extension_->name());
+  base::string16 extension_name = base::UTF8ToUTF16(model_->extension_name());
   base::i18n::AdjustStringForLocaleDirection(&extension_name);
   return l10n_util::GetStringFUTF16(IDS_EXTENSION_INSTALLED_HEADING,
                                     extension_name);
@@ -421,12 +266,10 @@ void ExtensionInstalledBubbleView::Init() {
       views::BoxLayout::CrossAxisAlignment::kStart);
   SetLayoutManager(std::move(layout));
 
-  if (ShouldShowHowToUse(extension_.get())) {
-    AddChildView(
-        CreateLabel(GetHowToUseDescription(extension_.get(), browser_)));
-  }
+  if (model_->show_how_to_use())
+    AddChildView(CreateLabel(model_->GetHowToUseText()));
 
-  if (ShouldShowKeybinding(extension_.get(), browser_)) {
+  if (model_->show_key_binding()) {
     auto* manage_shortcut = AddChildView(std::make_unique<views::Link>(
         l10n_util::GetStringUTF16(IDS_EXTENSION_INSTALLED_MANAGE_SHORTCUTS)));
     manage_shortcut->set_callback(base::BindRepeating(
@@ -434,7 +277,7 @@ void ExtensionInstalledBubbleView::Init() {
     manage_shortcut->SetUnderline(false);
   }
 
-  if (ShouldShowHowToManage(extension_.get(), browser_)) {
+  if (model_->show_how_to_manage()) {
     AddChildView(CreateLabel(
         l10n_util::GetStringUTF16(IDS_EXTENSION_INSTALLED_MANAGE_INFO)));
   }
@@ -469,9 +312,11 @@ ExtensionInstalledBubbleUi::~ExtensionInstalledBubbleUi() {
 }
 
 void ExtensionInstalledBubbleUi::Show(BubbleReference bubble_reference) {
-  bubble_view_ =
-      new ExtensionInstalledBubbleView(bubble_reference, bubble_->browser(),
-                                       bubble_->extension(), bubble_->icon());
+  bubble_view_ = new ExtensionInstalledBubbleView(
+      bubble_reference, bubble_->browser(),
+      std::make_unique<ExtensionInstalledBubbleModel>(
+          bubble_->browser()->profile(), bubble_->extension(),
+          bubble_->icon()));
   bubble_reference_ = bubble_reference;
 
   views::Widget* const widget =
@@ -479,7 +324,7 @@ void ExtensionInstalledBubbleUi::Show(BubbleReference bubble_reference) {
   // When the extension is installed to the ExtensionsToolbarContainer, use the
   // container to pop out the extension icon and show the widget. Otherwise show
   // the widget directly.
-  if (ShouldAnchorToAction(bubble_->extension()) &&
+  if (bubble_view_->model()->anchor_to_action() &&
       base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu)) {
     ExtensionsToolbarContainer* const container =
         BrowserView::GetBrowserViewForBrowser(bubble_->browser())
