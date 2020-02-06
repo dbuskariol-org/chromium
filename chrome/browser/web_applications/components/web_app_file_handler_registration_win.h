972010e4a16716eee592c19366531a03fe03d920
Keishi Hattori <keishi@chromium.org>
2020-02-06 05:41:33 +0000

    Revert "desktop-pwas: Multi-profile support for pwa file registration."
    This reverts commit e4b16bb5fcaca4e4cf2fe4b1b97775fb7116ba1e.
    
    Reason for revert: Suspecting cause for ShellUtilRegistryTest.GetFileAssociationsAndAppName and WebAppFileHandlerRegistrationWinTest.* test failures
    
    Original change's description:
    > desktop-pwas: Multi-profile support for pwa file registration.
    > 
    > When the same app is installed in multiple profiles, the open with
    > context menu needs to distinguish between the different profiles.
    > We do this by adding the profile name in parentheses after the app name,
    > e.g., "foo app (Profile1)". This CL detects when registering and
    > unregistering an app changes the app from being installed in
    > multiple profiles or just a single profile, and updates the app name.
    > 
    > This is similar to the way we badge/unbadge pinned taskbar icons when
    > the user switches between having multiple profiles and
    > having a single-profile.
    > 
    > Bug: 960245
    > Change-Id: I4c2a50c55cd8ab45be05eb023b162afc4f634cd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023283
    > Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    > Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738802}
    
    TBR=loyso@chromium.org,grt@chromium.org,davidbienvenu@chromium.org
    
    Change-Id: Ia7c5c0146c9dd481605c7205cb2bf3aff13bff74
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 960245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041050
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738852}

--- chrome/browser/web_applications/components/web_app_file_handler_registration_win.h
+++ chrome/browser/web_applications/components/web_app_file_handler_registration_win.h
@@ -14,9 +14,8 @@
 namespace web_app {
 
 // Returns the Windows ProgId for the web app with the passed |app_id| in
-// |profile_path|.
-base::string16 GetProgIdForApp(const base::FilePath& profile_path,
-                               const AppId& app_id);
+// |profile|.
+base::string16 GetProgIdForApp(Profile* profile, const AppId& app_id);
 
 // The name of "Last Browser" file, where UpdateChromeExePath() stores the path
 // of the last Chrome executable to use the containing user-data directory.
