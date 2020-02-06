d7682c6ef9c2f26c81751cfb45a0e6319539b893
Mohamed Amir Yosef <mamir@chromium.org>
2020-02-06 11:54:28 +0000

    [Passwords] Introduce SaveUpdateWithAccountStore bubble
    This is a mechnical fork of the existing SaveUpdate bubble without
    any change in functionality.
    Follow up CLs will introduce extra funcationlaity and change in the UI
    elements.
    
    Bug: 1044038
    Change-Id: I1ab6b08a9097b4f7adc06b53317676b42b3a2bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033072
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738913}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller.h
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller.h
@@ -104,9 +104,8 @@ class ManagePasswordsUIController
     return bubble_status_ == BubbleStatus::SHOULD_POP_UP;
   }
 
-  // TODO(crbug.com/1044034): Rename to GetControllerDelegateProxy after the
-  // refactoring of ManagePasswordsBubbleModel is done.
-  base::WeakPtr<PasswordsModelDelegate> GetModelDelegateProxy();
+  // virtual to be overridden in tests.
+  virtual base::WeakPtr<PasswordsModelDelegate> GetModelDelegateProxy();
 
   // PasswordsModelDelegate:
   content::WebContents* GetWebContents() const override;
