dc0f90dd20a70604332b151d8f30d2775dacffcc
Michael Spang <spang@chromium.org>
2020-02-07 19:35:40 +0000

    chromecast: Clean up #includes in external_mojo
    This executable does not compile, add missing #include
    and remove some unused ones.
    
    Change-Id: I759b49291b605d15e8c8e8deeabd9b8b24e0712e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044634
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739512}

--- chromecast/external_mojo/external_service_support/external_connector.h
+++ chromecast/external_mojo/external_service_support/external_connector.h
@@ -12,8 +12,6 @@
 
 #include "base/callback.h"
 #include "chromecast/external_mojo/public/mojom/connector.mojom.h"
-#include "mojo/public/cpp/bindings/interface_ptr.h"
-#include "mojo/public/cpp/bindings/interface_request.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/system/message_pipe.h"
