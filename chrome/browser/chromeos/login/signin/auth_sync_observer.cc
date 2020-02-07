0e2fa9d7b98ab40b683fe72cb16484e2631890d6
James Cook <jamescook@chromium.org>
2020-02-07 09:08:18 +0000

    Migrate SigninErrorNotifier to unconsented primary account
    SplitSettingsSync will allow Chrome OS users to opt-out of browser
    sync.  However, IdentityAccessor::GetPrimaryAccountInfo() assumes
    the user has consented to browser sync. We still need to detect cases
    where the refresh token is invalid, even if the user has not consented
    to browser sync.
    
    Switch to using the "unconsented" primary account. On Chrome OS this
    account always exists for the logged-in user account, whether or not
    the user consented to browser sync.
    
    See go/cros-sync-mock and go/cros-primary-account for details.
    
    Bug: 1042400, 760610
    Test: updated existing components_unittests
    Test: Bug 760610 still does not repro with SplitSettingsSync enabled
    Change-Id: I030e45eca6bcf82381bf1be9a4e6f829b7a4a3a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040531
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739314}

--- chrome/browser/chromeos/login/signin/auth_sync_observer.cc
+++ chrome/browser/chromeos/login/signin/auth_sync_observer.cc
@@ -13,6 +13,7 @@
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
+#include "components/signin/public/identity_manager/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/user_manager/user_manager.h"
@@ -70,7 +71,8 @@ void AuthSyncObserver::OnErrorChanged() {
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile_);
   HandleAuthError(identity_manager->GetErrorStateOfRefreshTokenForAccount(
-      identity_manager->GetPrimaryAccountId()));
+      identity_manager->GetPrimaryAccountId(
+          signin::ConsentLevel::kNotRequired)));
 }
 
 void AuthSyncObserver::HandleAuthError(
