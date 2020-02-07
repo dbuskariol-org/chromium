dc836775b42d7547937104e97bb904b30af97a93
Finnur Thorarinsson <finnur@chromium.org>
2020-02-07 13:32:48 +0000

    Revert "Add EDU login flow: EduAccountLoginHandler"
    This reverts commit 6ed337def51ced670d414fa7cee3a0e5e7b02877.
    
    Reason for revert: The new unit test fails on linux-chromeos-rel.
    See build where it was introduced: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/34701
    
    Original change's description:
    > Add EDU login flow: EduAccountLoginHandler
    > 
    > Add EDU account login handler to fetch user's family members and
    > getting ReAuthProof token for parent.
    > 
    > Bug: 1043108
    > Change-Id: Icf4b2782a4e6f114f452468baf09d2478db8d58c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033156
    > Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739305}
    
    TBR=khorimoto@chromium.org,dpapad@chromium.org,sinhak@chromium.org,anastasiian@chromium.org
    
    Change-Id: I469bd5e3ed648880df75e49093610a8031cc313e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1043108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043838
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739369}

--- chrome/browser/ui/webui/signin/inline_login_ui.cc
+++ chrome/browser/ui/webui/signin/inline_login_ui.cc
@@ -26,7 +26,6 @@
 #include "content/public/common/content_switches.h"
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/ui/webui/chromeos/edu_account_login_handler_chromeos.h"
 #include "chrome/browser/ui/webui/signin/inline_login_handler_chromeos.h"
 #else
 #include "chrome/browser/ui/webui/signin/inline_login_handler_impl.h"
@@ -116,9 +115,6 @@ InlineLoginUI::InlineLoginUI(content::WebUI* web_ui) : WebDialogUI(web_ui) {
       std::make_unique<chromeos::InlineLoginHandlerChromeOS>(
           base::BindRepeating(&WebDialogUIBase::CloseDialog,
                               weak_factory_.GetWeakPtr(), nullptr /* args */)));
-  web_ui->AddMessageHandler(std::make_unique<chromeos::EduAccountLoginHandler>(
-      base::BindRepeating(&WebDialogUIBase::CloseDialog,
-                          weak_factory_.GetWeakPtr(), nullptr /* args */)));
 #else
   web_ui->AddMessageHandler(std::make_unique<InlineLoginHandlerImpl>());
 #endif  // defined(OS_CHROMEOS)
