f794339a7e6bbc14ff7feda9c61e56b8a54fd447
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-02-10 19:54:52 +0000

    [iOS] Typo
    Bug: n/a
    Change-Id: I42b0c540c4c7bcaf89822d9645fca9c44da100d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047452
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739979}

--- ios/chrome/browser/ui/passwords/password_breach_constants.h
+++ ios/chrome/browser/ui/passwords/password_breach_constants.h
@@ -7,7 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-// A11y ID for the pasword breach view controller.
+// A11y ID for the password breach view controller.
 extern NSString* const kPasswordBreachViewAccessibilityIdentifier;
 
 #endif  // IOS_CHROME_BROWSER_UI_PASSWORDS_PASSWORD_BREACH_CONSTANTS_H_
