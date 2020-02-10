26295ae55f0bd20ec65b20a5d7e6acf2ca7e852c
Shimi Zhang <ctzsm@chromium.org>
2020-02-10 23:53:31 +0000

    Removes AddRuleForHostname() from ProxyBypassRules
    This API is allowing embedders of ProxyBypassRules to add rules directly
    without ProxyBypassRules' parsing function, which isn't ideal. We should
    have a central function to parse the input from outside of
    ProxyBypassRules.
    
    This CL removes AddRuleForHostname() and replace it with
    AddRuleFromString(). In issue 1049876, we passed an IP address like
    hostname to the internal class SchemeHostPortHostnamePatternRule, which
    is now invalid.
    
    Bug: 1049876
    Change-Id: I2136d6b03e5cf53a246b7ffd582c158a25e5a8b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045311
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740039}

--- net/proxy_resolution/proxy_bypass_rules.cc
+++ net/proxy_resolution/proxy_bypass_rules.cc
@@ -185,18 +185,6 @@ void ProxyBypassRules::ParseFromString(const std::string& raw) {
   }
 }
 
-bool ProxyBypassRules::AddRuleForHostname(const std::string& optional_scheme,
-                                          const std::string& hostname_pattern,
-                                          int optional_port) {
-  if (hostname_pattern.empty())
-    return false;
-
-  matcher_.AddAsLastRule(
-      std::make_unique<SchemeHostPortMatcherHostnamePatternRule>(
-          optional_scheme, hostname_pattern, optional_port));
-  return true;
-}
-
 void ProxyBypassRules::PrependRuleToBypassSimpleHostnames() {
   matcher_.AddAsFirstRule(std::make_unique<BypassSimpleHostnamesRule>());
 }
