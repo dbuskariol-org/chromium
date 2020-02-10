5b96a1185ff5fd8f8032cebc84389e187d4e04e0
Daniel Cheng <dcheng@chromium.org>
2020-02-10 16:27:21 +0000

    Directly include histogram_macros.h in //third_party/blink/renderer/modules/cache_storage
    cache_storage already used the //base histogram helpers, so just include
    the appropriate header directly.
    
    Bug: 1047547
    Change-Id: I6b215843a35496af4d29a933584ba2d9a6f87e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2046538
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739886}

--- third_party/blink/renderer/modules/cache_storage/cache.cc
+++ third_party/blink/renderer/modules/cache_storage/cache.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink.h"
 #include "third_party/blink/public/common/cache_storage/cache_storage_utils.h"
@@ -40,7 +41,6 @@
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 #include "third_party/blink/renderer/platform/loader/fetch/cached_metadata.h"
