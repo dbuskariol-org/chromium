fc2895b6147d8f1a5ea9f16b7dd15add43d8623d
Nate Chapin <japhet@chromium.org>
2020-02-11 00:00:15 +0000

    Temporarily suppress failing case in ScriptPromisePropertyGarbageCollectedTest.Reset
    Bug: 1029822
    Change-Id: I027b826e3cd2bee6764b89dc7ae82b5eee53dab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047276
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740050}

--- third_party/blink/renderer/bindings/core/v8/script_promise_property_test.cc
+++ third_party/blink/renderer/bindings/core/v8/script_promise_property_test.cc
@@ -471,7 +471,9 @@ TEST_F(ScriptPromisePropertyGarbageCollectedTest, Reset) {
   v8::MicrotasksScope::PerformCheckpoint(GetIsolate());
   EXPECT_EQ(1u, n_old_resolve_calls);
   EXPECT_EQ(1u, n_new_reject_calls);
-  EXPECT_NE(old_promise, new_promise);
+  // TODO(crbug.com/1029822): This EXPECT is failing on win-asan only. It's
+  // not clear how it could fail but all of the other expectations pass.
+  // EXPECT_NE(old_promise, new_promise);
   EXPECT_EQ(Wrap(MainWorld(), old_value), old_actual);
   EXPECT_EQ(Wrap(MainWorld(), new_value), new_actual);
   EXPECT_NE(old_actual, new_actual);
