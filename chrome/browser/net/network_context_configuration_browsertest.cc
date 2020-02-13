66f040fe2d06f723c7cdd86e456b82e4a809ee63
Alan Cutter <alancutter@chromium.org>
2020-02-13 13:17:32 +0000

    Remove empty NetworkContextConfigurationHttpsStrippingPacBrowserTest instantiation
    The NetworkContextConfigurationHttpsStrippingPacBrowserTest tests were
    removed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1567589
    This CL removes the residue fixture and instantiation.
    
    This fixes the warning thrown by browser_tests:
    [test_launcher.cc(1498)] The following parameterized test case is not instantiated: UninstantiatedParamaterizedTestSuite<NetworkContextConfigurationHttpsStrippingPacBrowserTest>
    
    Change-Id: I167dc60ebbfe788f0e3e2cc1202dfd744d1daabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053268
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741036}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -1888,54 +1888,6 @@ IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationFtpPacBrowserTest, FtpPac) {
   EXPECT_EQ(net::ERR_PROXY_CONNECTION_FAILED, simple_loader->NetError());
 }
 
-// Used to test that PAC HTTPS URL stripping works. A different test server is
-// used as the "proxy" based on whether the PAC script sees the full path or
-// not. The servers aren't correctly set up to mimic HTTP proxies that tunnel
-// to an HTTPS test server, so the test fixture just watches for any incoming
-// connection.
-class NetworkContextConfigurationHttpsStrippingPacBrowserTest
-    : public NetworkContextConfigurationBrowserTest {
- public:
-  NetworkContextConfigurationHttpsStrippingPacBrowserTest() {}
-
-  ~NetworkContextConfigurationHttpsStrippingPacBrowserTest() override {}
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    // Test server HostPortPair, as a string.
-    std::string test_server_host_port_pair =
-        net::HostPortPair::FromURL(embedded_test_server()->base_url())
-            .ToString();
-    // Set up a PAC file that directs to different servers based on the URL it
-    // sees.
-    std::string pac_script = base::StringPrintf(
-        "function FindProxyForURL(url, host) {"
-        // With the test URL stripped of the path, try to use the embedded test
-        // server to establish a an SSL tunnel over an HTTP proxy. The request
-        // will fail with ERR_TUNNEL_CONNECTION_FAILED.
-        "  if (url == 'https://does.not.resolve.test:1872/')"
-        "    return 'PROXY %s';"
-        // With the full test URL, try to use a domain that does not resolve as
-        // a proxy. Errors connecting to the proxy result in
-        // ERR_PROXY_CONNECTION_FAILED.
-        "  if (url == 'https://does.not.resolve.test:1872/foo')"
-        "    return 'PROXY does.not.resolve.test';"
-        // Otherwise, use direct. If a connection to "does.not.resolve.test"
-        // tries to use a direction connection, it will fail with
-        // ERR_NAME_NOT_RESOLVED. This path will also
-        // be used by the initial request in NetworkServiceState::kRestarted
-        // tests to make sure the network service process is fully started
-        // before it's crashed and restarted. Using direct in this case avoids
-        // that request failing with an unexpeced error when being directed to a
-        // bogus proxy.
-        "  return 'DIRECT';"
-        "}",
-        test_server_host_port_pair.c_str());
-
-    command_line->AppendSwitchASCII(switches::kProxyPacUrl,
-                                    "data:," + pac_script);
-  }
-};
-
 class NetworkContextConfigurationProxySettingsBrowserTest
     : public NetworkContextConfigurationHttpPacBrowserTest {
  public:
@@ -2340,8 +2292,6 @@ INSTANTIATE_TEST_CASES_FOR_TEST_FIXTURE(
     NetworkContextConfigurationDataPacBrowserTest);
 INSTANTIATE_TEST_CASES_FOR_TEST_FIXTURE(
     NetworkContextConfigurationFtpPacBrowserTest);
-INSTANTIATE_TEST_CASES_FOR_TEST_FIXTURE(
-    NetworkContextConfigurationHttpsStrippingPacBrowserTest);
 INSTANTIATE_TEST_CASES_FOR_TEST_FIXTURE(
     NetworkContextConfigurationProxySettingsBrowserTest);
 INSTANTIATE_TEST_CASES_FOR_TEST_FIXTURE(
