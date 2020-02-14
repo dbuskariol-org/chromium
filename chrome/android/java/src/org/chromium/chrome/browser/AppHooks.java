ffe0b51dc77114b779d9f316466459d425113b94
Sky Malice <skym@chromium.org>
2020-02-14 23:09:41 +0000

    Expose SurfaceAdapterFactory in AppHooks.
    Bug: 1050094
    Change-Id: I247ac07b80be87a94623d2bcf4b6dea7f8accb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055356
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741641}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -56,6 +56,7 @@ import org.chromium.chrome.browser.ui.ImmersiveModeManager;
 import org.chromium.chrome.browser.usage_stats.DigitalWellbeingClient;
 import org.chromium.chrome.browser.webapps.GooglePlayWebApkInstallDelegate;
 import org.chromium.chrome.browser.webauth.Fido2ApiHandler;
+import org.chromium.chrome.browser.xsurface.SurfaceAdapterFactory;
 import org.chromium.components.browser_ui.widget.FeatureHighlightProvider;
 import org.chromium.components.download.DownloadCollectionBridge;
 import org.chromium.components.signin.AccountManagerDelegate;
@@ -395,5 +396,13 @@ public abstract class AppHooks {
      */
     public TrustedVaultClient.Backend createSyncTrustedVaultClientBackend() {
         return new TrustedVaultClient.EmptyBackend();
+    }
+
+    /**
+     * Returns a new {@link SurfaceAdapterFactory} if the xsurface implementation is included in the
+     * apk. Otherwise null is returned.
+     */
+    public @Nullable SurfaceAdapterFactory createExternalSurfaceAdapterFactory() {
+        return null;
     }
 }
