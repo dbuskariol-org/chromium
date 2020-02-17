2169939e02501cfadeef6a7d7bbce2680196331c
Corentin Wallez <cwallez@chromium.org>
2020-02-17 11:08:28 +0000

    Revert "SkiaOutputdeviceBufferQueue: kick off next frame earlier"
    This reverts commit 75b5db21e0b6d9702c832dfb999c447ef1df7f20.
    
    Reason for revert: tentative revert for crbug.com/1052790
    
    Original change's description:
    > SkiaOutputdeviceBufferQueue: kick off next frame earlier
    > 
    > PageFlipComplete() is not trivial, so call FinishSwapBuffers()
    > before it to kick off next frame in viz thread earlier, and compositor
    > will have more time budget to render the next frame.
    > 
    > Bug: None
    > Change-Id: Ie6e0f87d75d81b405447757585ec291887ca0063
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057043
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Auto-Submit: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#741430}
    
    TBR=penghuang@chromium.org,vasilyt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: None
    Change-Id: Ia7df67ad5f4b3975b27b5d19a42d4711a648a4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058971
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741877}

--- components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
+++ components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
@@ -529,8 +529,8 @@ void SkiaOutputDeviceBufferQueue::DoFinishSwapBuffers(
     std::unique_ptr<gfx::GpuFence> gpu_fence) {
   DCHECK(!gpu_fence);
 
-  FinishSwapBuffers(result, size, latency_info);
   PageFlipComplete(image.get());
+  FinishSwapBuffers(result, size, latency_info);
 }
 
 bool SkiaOutputDeviceBufferQueue::Reshape(const gfx::Size& size,
