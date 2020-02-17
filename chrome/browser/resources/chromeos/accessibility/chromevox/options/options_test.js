af8dbb2317dd34f61874625286391d4e093bef45
David Tseng <dtseng@chromium.org>
2020-02-17 15:40:35 +0000

    Disable ChromeVoxOptionsTest.NumberReadingStyleSelect
    TBR=dtseng@chromium.org
    
    Fixed: 1052175
    Change-Id: I2f29c90e71761616ca453bcc3811183dfd061d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059428
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741930}

--- chrome/browser/resources/chromeos/accessibility/chromevox/options/options_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/options/options_test.js
@@ -52,7 +52,7 @@ ChromeVoxOptionsTest = class extends ChromeVoxNextE2ETest {
   }
 };
 
-TEST_F('ChromeVoxOptionsTest', 'NumberReadingStyleSelect', function() {
+TEST_F('ChromeVoxOptionsTest', 'DISABLED_NumberReadingStyleSelect', function() {
   this.runOnOptionsPage((mockFeedback, evt) => {
     const numberStyleSelect = evt.target.find({
       role: chrome.automation.RoleType.POP_UP_BUTTON,
