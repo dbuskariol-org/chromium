ef50ee64d786702706efa23663105039af883a5c
Tim Volodine <timvolodine@chromium.org>
2020-02-18 21:03:15 +0000

    [Merge M81][WebLayer] Make error_page_helper handle commands for safebrowsing committed interstitials
    MERGE to M81
    
    Some safebrowsing specific interstitial commands do not work in the
    current weblayer implementation when committed interstitials are
    enabled (kCommittedSBInterstitials).
    
    This patch makes sure to handle safebrowsing specific interstitial
    commands such as CMD_OPEN_DIAGNOSTIC and CMD_REPORT_PHISHING_ERROR.
    The CMD_OPEN_DIAGNOSTIC command loads "google transparency report"
    site with "safe browsing site status". The CMD_REPORT_PHISHING_ERROR
    command proceeds to the "report incorrect phishing warning" site.
    
    BUG=1044596,1042662
    
    (cherry picked from commit 35c4015cbe6e8c8622008f53b1dad8d3aa2cfbd9)
    
    Change-Id: I092c3574222f51e30b8c4159eb2fa6e1a4bf0d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044192
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#741076}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062272
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4044@{#324}
    Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}

--- weblayer/renderer/error_page_helper.cc
+++ weblayer/renderer/error_page_helper.cc
@@ -63,20 +63,27 @@ void ErrorPageHelper::SendCommand(
     case security_interstitials::CMD_OPEN_HELP_CENTER:
       interface->OpenHelpCenter();
       break;
+    case security_interstitials::CMD_OPEN_DIAGNOSTIC:
+      // Used by safebrowsing interstitials.
+      interface->OpenDiagnostic();
+      break;
     case security_interstitials::CMD_RELOAD:
       interface->Reload();
       break;
     case security_interstitials::CMD_OPEN_LOGIN:
       interface->OpenLogin();
       break;
-    case security_interstitials::CMD_OPEN_DIAGNOSTIC:
+    case security_interstitials::CMD_REPORT_PHISHING_ERROR:
+      // Used by safebrowsing phishing interstitial.
+      interface->ReportPhishingError();
+      break;
     case security_interstitials::CMD_OPEN_DATE_SETTINGS:
     case security_interstitials::CMD_DO_REPORT:
     case security_interstitials::CMD_DONT_REPORT:
     case security_interstitials::CMD_OPEN_REPORTING_PRIVACY:
     case security_interstitials::CMD_OPEN_WHITEPAPER:
-    case security_interstitials::CMD_REPORT_PHISHING_ERROR:
-      // Commands not used by the generic SSL error page.
+      // Commands not used by the generic SSL error pages.
+      // Also not currently used by the safebrowsing error pages.
       NOTREACHED();
       break;
     case security_interstitials::CMD_ERROR:
