11357e7b380f7eb0fb77f3c44f1878e02c6b1270
Mila Green <milagreen@chromium.org>
2020-02-18 18:07:42 +0000

    Proof of concept for XPC service in the updater.
    Continues https://chromium-review.googlesource.com/c/chromium/src/+/2045058
    
    Rebased. Moved code to the new chrome/updater/server/mac/server.mm::LaunchServer, addressed more feedback.
    
    Need to create a plist for the xpc service. It is not clear yet what executatble will be used.
    
    Bug: 1048653
    Change-Id: Iadbbd7004afa39fcc902f2ec68297845c2f94571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055284
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742204}

--- chrome/updater/server/mac/server.mm
+++ chrome/updater/server/mac/server.mm
@@ -4,14 +4,35 @@
 
 #import "chrome/updater/server/mac/server.h"
 
-#import "base/logging.h"
+#import <Foundation/Foundation.h>
+#include <xpc/xpc.h>
+
+#include "base/mac/scoped_nsobject.h"
+#include "base/strings/sys_string_conversions.h"
+#import "chrome/updater/server/mac/service_delegate.h"
 #import "chrome/updater/update_service.h"
+#include "chrome/updater/updater_version.h"
 
 namespace updater {
 
 int RunServer(std::unique_ptr<UpdateService> update_service) {
-  NOTREACHED();
-  return -1;
+  @autoreleasepool {
+    std::string service_name = MAC_BUNDLE_IDENTIFIER_STRING;
+    service_name.append(".UpdaterXPCService");
+    base::scoped_nsobject<NSString> updaterMachServiceName(
+        base::SysUTF8ToNSString(service_name), base::scoped_policy::RETAIN);
+    base::scoped_nsobject<UpdateCheckXPCServiceDelegate> delegate(
+        [[UpdateCheckXPCServiceDelegate alloc]
+            initWithUpdateService:std::move(update_service)]);
+
+    NSXPCListener* listener =
+        [[NSXPCListener alloc] initWithMachServiceName:updaterMachServiceName];
+    listener.delegate = delegate.get();
+
+    [listener resume];
+
+    return 0;
+  }
 }
 
 }  // namespace updater
