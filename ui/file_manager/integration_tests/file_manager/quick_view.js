0a72cc7e55216b3b1210c3df63396cf55563b090
Isabella Scalzi <isabellascalzi@google.com>
2020-02-18 00:48:00 +0000

    [quickview] Fix flaky test |openQuickViewDeleteEntireCheckSelection|
    On the bots, |openQuickViewDeleteEntireCheckSelection| is still failing
    occasionally. Check that the first file in the test has loaded
    completely before attempting to delete it.
    
    Bug: 803259
    Change-Id: I8cdc38352356cbe6d6dfee29efe14a81021269d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2060396
    Commit-Queue: Isabella Scalzi <isabellascalzi@google.com>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742005}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -2210,6 +2210,29 @@
     // Open Quick View on the check-selected files.
     await openQuickViewMultipleSelection(appId, ['Beautiful', 'Desktop']);
 
+    /**
+     * The <webview> resides in the <files-safe-media type="audio"> shadow DOM,
+     * which is a child of the #quick-view shadow DOM.
+     */
+    const audioWebView =
+        ['#quick-view', 'files-safe-media[type="audio"]', 'webview'];
+
+    // Wait for the Quick View <webview> to load and display its content.
+    function checkWebViewAudioLoaded(elements) {
+      let haveElements = Array.isArray(elements) && elements.length === 1;
+      if (haveElements) {
+        haveElements = elements[0].styles.display.includes('block');
+      }
+      if (!haveElements || elements[0].attributes.loaded !== '') {
+        return pending(caller, 'Waiting for <webview> to load.');
+      }
+      return;
+    }
+    await repeatUntil(async () => {
+      return checkWebViewAudioLoaded(await remoteCall.callRemoteTestUtil(
+          'deepQueryAllElements', appId, [audioWebView, ['display']]));
+    });
+
     // Open the Quick View delete confirm dialog.
     const deleteKey = ['#quick-view', 'Delete', false, false, false];
     chrome.test.assertTrue(
@@ -2228,7 +2251,7 @@
      * The <webview> resides in the <files-safe-media type="image"> shadow DOM,
      * which is a child of the #quick-view shadow DOM.
      */
-    const webView =
+    const imageWebView =
         ['#quick-view', 'files-safe-media[type="image"]', 'webview'];
 
     // Wait for the Quick View <webview> to load and display its content.
@@ -2244,7 +2267,7 @@
     }
     await repeatUntil(async () => {
       return checkWebViewImageLoaded(await remoteCall.callRemoteTestUtil(
-          'deepQueryAllElements', appId, [webView, ['display']]));
+          'deepQueryAllElements', appId, [imageWebView, ['display']]));
     });
 
     // Open the Quick View delete confirm dialog.
