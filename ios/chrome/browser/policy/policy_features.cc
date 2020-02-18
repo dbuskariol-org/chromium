8eefc0a126a10305d58cf7acdbae6af4313eb3b2
Rohit Rao <rohitrao@chromium.org>
2020-02-18 18:49:34 +0000

    [ios] Adds a feature flag for policy support.
    BUG=1024115
    
    Change-Id: Ib9f9491d7650dba83d6cbc7ae822eaf04f907b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057116
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742218}

---
+++ ios/chrome/browser/policy/policy_features.cc
@@ -0,0 +1,8 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/policy/policy_features.h"
+
+const base::Feature kEnterprisePolicyIOS{"EnterprisePolicyIOS",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
