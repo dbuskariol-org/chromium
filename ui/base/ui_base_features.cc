8b4654b15ad052bfb7614488f1acd50971876b71
Saman Sami <samans@chromium.org>
2020-02-19 18:13:27 +0000

    Ozone/DRM: Enable OzoneDrmMojo feature by default
    This feature is on at 50% of stable with no known issues.
    
    TBR=sadrul@chromium.org
    
    Bug: 620927
    Change-Id: I1d2ef64003a56714f648662f1571c8d99fe7a44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055184
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742700}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -157,7 +157,7 @@ bool IsAutomaticUiAdjustmentsForTouchEnabled() {
 }
 
 const base::Feature kEnableOzoneDrmMojo = {"OzoneDrmMojo",
-                                           base::FEATURE_DISABLED_BY_DEFAULT};
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool IsOzoneDrmMojo() {
   return base::FeatureList::IsEnabled(kEnableOzoneDrmMojo);
