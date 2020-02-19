63b5afc0ca8d8f1ad996b50d037aeb75b5854be0
Donn Denman <donnd@chromium.org>
2020-02-19 18:05:19 +0000

    [TTS][Template URL] Add support for translations.
    Adds support for new Contextual Search Resolve-request parameters
    to provide translation hints from the client.  The server uses these
    params as additional hints from the Chrome Language Model for the
    user's preferred language and the language of the selection and
    page content.
    
    This means the returned Search Term may be translated or be accompanied
    by a Translation Card.
    
    The parameters we send in the Resolve request are the same parameters
    we currently send to force a translation one-box in the Search request:
    "&tlitesl=" for source language and "&tlitetl=" for the target language.
    
    BUG=952401
    
    (cherry picked from commit 7a24101af42a5054263db06e523e1eb67a7e222a)
    
    Change-Id: Ia79d8c0cf3706009c2b6213895ad8f6df543c24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032058
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#737387}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2064448
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4044@{#352}
    Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}

--- chrome/browser/android/contextualsearch/contextual_search_delegate.cc
+++ chrome/browser/android/contextualsearch/contextual_search_delegate.cc
@@ -293,7 +293,8 @@ std::string ContextualSearchDelegate::BuildRequestUrl(
   TemplateURLRef::SearchTermsArgs::ContextualSearchParams params(
       kContextualSearchRequestVersion, contextual_cards_version,
       context->GetHomeCountry(), context->GetPreviousEventId(),
-      context->GetPreviousEventResults(), context->GetExactResolve());
+      context->GetPreviousEventResults(), context->GetExactResolve(),
+      std::string(), std::string());
 
   search_terms_args.contextual_search_params = params;
 
