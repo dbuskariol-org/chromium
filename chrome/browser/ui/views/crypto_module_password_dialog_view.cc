fcfdd3fcd11633c431a49b22b3f6f4c7ff482e8a
Elly Fong-Jones <ellyjones@chromium.org>
2020-02-20 17:38:41 +0000

    cbuiv: migrate CryptoModulePasswordDialogView to new callbacks
    This change removes CryptoModulePasswordDialogView::{Accept,Cancel} and
    replaces them with explicit callbacks. It also changes the behavior
    (hence the separate CL): the old code used to explicitly clear the
    password entry field's text on the closure path. I can't find any good
    reason why this would be done, and views::TextField has never guaranteed
    that setting the text will cause the old text to be securely erased from
    memory. In any case, if we want that behavior, it should happen in
    //ui/views, not here.
    
    Bug: 1011446
    Change-Id: I3b81438511cf8eb3e52241543024ec081c467636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062887
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743149}

--- chrome/browser/ui/views/crypto_module_password_dialog_view.cc
+++ chrome/browser/ui/views/crypto_module_password_dialog_view.cc
@@ -28,6 +28,13 @@ CryptoModulePasswordDialogView::CryptoModulePasswordDialogView(
   DialogDelegate::set_button_label(
       ui::DIALOG_BUTTON_OK,
       l10n_util::GetStringUTF16(IDS_CRYPTO_MODULE_AUTH_DIALOG_OK_BUTTON_LABEL));
+  DialogDelegate::set_accept_callback(base::BindOnce(
+      [](CryptoModulePasswordDialogView* dialog) {
+        dialog->callback_.Run(
+            base::UTF16ToUTF8(dialog->password_entry_->GetText()));
+      },
+      base::Unretained(this)));
+  DialogDelegate::set_cancel_callback(base::BindOnce(callback_, std::string()));
   set_margins(ChromeLayoutProvider::Get()->GetDialogInsetsForContentType(
       views::TEXT, views::CONTROL));
   Init(hostname, slot_name, reason);
@@ -50,20 +57,6 @@ ui::ModalType CryptoModulePasswordDialogView::GetModalType() const {
 
 base::string16 CryptoModulePasswordDialogView::GetWindowTitle() const {
   return l10n_util::GetStringUTF16(IDS_CRYPTO_MODULE_AUTH_DIALOG_TITLE);
-}
-
-bool CryptoModulePasswordDialogView::Cancel() {
-  callback_.Run(std::string());
-  const base::string16 empty;
-  password_entry_->SetText(empty);
-  return true;
-}
-
-bool CryptoModulePasswordDialogView::Accept() {
-  callback_.Run(base::UTF16ToUTF8(password_entry_->GetText()));
-  const base::string16 empty;
-  password_entry_->SetText(empty);
-  return true;
 }
 
 void CryptoModulePasswordDialogView::ContentsChanged(
