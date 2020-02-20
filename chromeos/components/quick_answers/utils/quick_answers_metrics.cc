d17d7810586a1192e920b27c5878e1ca241117aa
Li Lin <llin@chromium.org>
2020-02-20 21:07:56 +0000

    Add quick answers logging for the length of the selected text.
    QuickAnswers.SelectedContent.Length is recorded to learn about users
    usage pattern.
    
    (cherry picked from commit 7dbe21f47498ed7a0d4c32b024fa0e029b4ac421)
    
    Bug: 1051751
    Test: Manual
    Change-Id: Iba7c9ecf605cf39cc7d65864eb54d9ff6ef12be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053288
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#741583}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057493
    Cr-Commit-Position: refs/branch-heads/4044@{#385}
    Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}

--- chromeos/components/quick_answers/utils/quick_answers_metrics.cc
+++ chromeos/components/quick_answers/utils/quick_answers_metrics.cc
@@ -16,6 +16,8 @@ const char kQuickAnswerClick[] = "QuickAnswers.Click";
 const char kQuickAnswerResult[] = "QuickAnswers.Result";
 const char kQuickAnswerLoadingStatus[] = "QuickAnswers.Loading.Status";
 const char kQuickAnswerLoadingDuration[] = "QuickAnswers.Loading.Duration";
+const char kQuickAnswerSelectedContentLength[] =
+    "QuickAnswers.SelectedContent.Length";
 const char kDurationSuffix[] = ".Duration";
 
 std::string ResultTypeToString(ResultType result_type) {
@@ -71,6 +73,10 @@ void RecordLoadingStatus(LoadStatus status, const base::TimeDelta duration) {
 void RecordClick(ResultType result_type, const base::TimeDelta duration) {
   RecordTypeAndDuration(kQuickAnswerClick, result_type, duration,
                         /*is_medium_bucketization=*/true);
+}
+
+void RecordSelectedTextLength(int length) {
+  base::UmaHistogramCounts1000(kQuickAnswerSelectedContentLength, length);
 }
 
 }  // namespace quick_answers
