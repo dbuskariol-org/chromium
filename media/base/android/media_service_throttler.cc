0ee8c82184bf1ad630c539317f1c8e67caed0a70
Jose Lopes <jabolopes@google.com>
2020-02-21 10:29:17 +0000

    media: Migrate media server crash callback.
    According to the documentation, this is a repeating callback, called every time the media server crashes:
    * https://cs.chromium.org/chromium/src/media/base/android/media_server_crash_listener.h?rcl=06d998f53ecbf18ef4512d8a22f1261ee4ab8c71&l=18
    * https://cs.chromium.org/chromium/src/media/base/android/media_server_crash_listener.h?rcl=06d998f53ecbf18ef4512d8a22f1261ee4ab8c71&l=27
    * https://cs.chromium.org/chromium/src/media/base/android/media_server_crash_listener.h?rcl=06d998f53ecbf18ef4512d8a22f1261ee4ab8c71&l=38
    
    This is part of the base::Callback migration.
    
    Context: https://cs.chromium.org/chromium/src/docs/callback.md?rcl=9fcc3764aea8f97e9f6de4a9ee61d554e67edcda&l=40
    
    Bug: 714018
    Change-Id: I871e908c72a6fc016faa2317bea51ece2e0da947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047247
    Commit-Queue: Jose Lopes <jabolopes@google.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743493}

--- media/base/android/media_service_throttler.cc
+++ media/base/android/media_service_throttler.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/default_tick_clock.h"
 #include "media/base/android/media_server_crash_listener.h"
@@ -197,8 +198,8 @@ void MediaServiceThrottler::EnsureCrashListenerStarted() {
     // base::Unretained is safe here because the MediaServiceThrottler will live
     // until the process is terminated.
     crash_listener_ = std::make_unique<MediaServerCrashListener>(
-        base::Bind(&MediaServiceThrottler::OnMediaServerCrash,
-                   base::Unretained(this)),
+        base::BindRepeating(&MediaServiceThrottler::OnMediaServerCrash,
+                            base::Unretained(this)),
         crash_listener_task_runner_);
   } else {
     crash_listener_->EnsureListening();
@@ -225,8 +226,7 @@ void MediaServiceThrottler::SetCrashListenerTaskRunnerForTesting(
 
   // Re-create the crash listener.
   crash_listener_ = std::make_unique<MediaServerCrashListener>(
-      MediaServerCrashListener::OnMediaServerCrashCB(),
-      crash_listener_task_runner_);
+      base::NullCallback(), crash_listener_task_runner_);
 }
 
 }  // namespace media
