1d585b9019434c71d2d46f229f92aece02756cb9
Kenneth Russell <kbr@chromium.org>
2020-02-21 21:58:22 +0000

    Reland: Allow OffscreenCanvas to access the high-performance GPU.
    (Relanding with new tests skipped on Mac NVIDIA Retina, which forces
    the integrated GPU due to graphics driver bugs.)
    
    Instead of only checking whether the containing frame is same-origin
    with respect to the top-level document during
    transferControlToOffscreen, check in the OffscreenCanvas constructor,
    both for the main thread as well as dedicated workers. Dedicated workers
    are same-origin per specification, so always grant access to the
    discrete GPU in this scenario.
    
    Add new pixel tests that verify WebGL-rendered OffscreenCanvas into an
    ImageBitmapRenderingContext (i.e., where the OffscreenCanvas was not
    originally tied to a canvas element). On dual-GPU macOS devices, also
    verify the active state of the low-power and high-performance GPUs.
    
    Simplify some of the tests most recently written for low/high-power GPU
    switching.
    
    Bug: 1048892
    Change-Id: I20b7150029fa83d7fe066b8347bf150a0ed45d21
    Tbr: kainino@chromium.org
    Tbr: fserb@chromium.org
    Tbr: jdarpinian@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068645
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: James Darpinian <jdarpinian@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743686}

--- third_party/blink/renderer/modules/canvas/htmlcanvas/html_canvas_element_module.cc
+++ third_party/blink/renderer/modules/canvas/htmlcanvas/html_canvas_element_module.cc
@@ -7,7 +7,6 @@
 #include "base/metrics/histogram_functions.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_canvas_context_creation_attributes_module.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
-#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
 #include "third_party/blink/renderer/modules/canvas/htmlcanvas/canvas_context_creation_attributes_helpers.h"
@@ -69,12 +68,6 @@ OffscreenCanvas* HTMLCanvasElementModule::TransferControlToOffscreenInternal(
   OffscreenCanvas* offscreen_canvas = OffscreenCanvas::Create(
       execution_context, canvas.width(), canvas.height());
   offscreen_canvas->SetFilterQuality(canvas.FilterQuality());
-
-  // If this canvas is cross-origin, then the associated offscreen canvas
-  // should prefer using the low-power GPU.
-  LocalFrame* frame = canvas.GetDocument().GetFrame();
-  if (!(frame && frame->IsCrossOriginToMainFrame()))
-    offscreen_canvas->AllowHighPerformancePowerPreference();
 
   DOMNodeId canvas_id = DOMNodeIds::IdForNode(&canvas);
   canvas.RegisterPlaceholderCanvas(static_cast<int>(canvas_id));
