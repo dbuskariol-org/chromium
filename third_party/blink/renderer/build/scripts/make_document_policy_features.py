2581d06eeedd896fb6cc9201c2cad25f9188f258
Charlie Hu <chenleihu@google.com>
2020-02-21 20:22:31 +0000

    Add short-hand syntax for max/min policy value
    This CL adds a short-hand syntax for specifying max/min default_value
    in document_policy_features.json5.
    
    Bug: 993790
    Change-Id: I970a407a738f7afd5c6a081caefddadef47f6a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067658
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743652}

--- third_party/blink/renderer/build/scripts/make_document_policy_features.py
+++ third_party/blink/renderer/build/scripts/make_document_policy_features.py
@@ -12,12 +12,31 @@ class DocumentPolicyFeatureWriter(json5_generator.Writer):
     def __init__(self, json5_file_path, output_dir):
         super(DocumentPolicyFeatureWriter, self).__init__(json5_file_path, output_dir)
 
+        def parse_default_value(default_value, value_type):
+            """ Parses default_value string to actual usable C++ expression.
+            @param default_value_str: default_value field specified in document_policy_features.json5
+            @param value_type: value_type field specified in document_policy_features.json5
+            """
+            policy_value_type = "mojom::PolicyValueType::k{}".format(value_type)
+
+            if default_value == 'max':
+                return "PolicyValue::CreateMaxPolicyValue({})".format(policy_value_type)
+            if default_value == 'min':
+                return "PolicyValue::CreateMinPolicyValue({})".format(policy_value_type)
+
+            if value_type in {'bool'}:  # types that have only one corresponding PolicyValueType
+                return "PolicyValue({})".format(default_value)
+            else:
+                return "PolicyValue({}, {})".format(default_value, policy_value_type)
+
+
         @template_expander.use_jinja('templates/' + self.file_basename + '.cc.tmpl')
         def generate_implementation():
             return {
                 'header_guard': self.make_header_guard(self._relative_output_dir + self.file_basename + '.h'),
                 'input_files': self._input_files,
-                'features': self.json5_file.name_dictionaries
+                'features': self.json5_file.name_dictionaries,
+                'parse_default_value': parse_default_value
             }
 
         self._outputs = {
