e31bbb5bfde11418f4fb1dff883cb46d881811f5
Adrienne Walker <enne@chromium.org>
2020-02-24 23:02:39 +0000

    indexeddb: fix presubmit violations in indexeddb code
    This is just preparation for making moving this code easier.
    
    Bug: 1015214
    
    Change-Id: I379b6d1c8be4f61f9e13329c6ddae590fd8c80a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068929
    Auto-Submit: enne <enne@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744107}

--- content/browser/indexed_db/indexed_db_backing_store.cc
+++ content/browser/indexed_db/indexed_db_backing_store.cc
@@ -2746,13 +2746,13 @@ IndexedDBBackingStore::OpenObjectStoreCursor(
   if (!ObjectStoreCursorOptions(leveldb_transaction, database_id,
                                 object_store_id, range, direction,
                                 &cursor_options, s)) {
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
   }
   std::unique_ptr<ObjectStoreCursorImpl> cursor(
       std::make_unique<ObjectStoreCursorImpl>(transaction->AsWeakPtr(),
                                               database_id, cursor_options));
   if (!cursor->FirstSeek(s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
 
   return std::move(cursor);
 }
@@ -2775,13 +2775,13 @@ IndexedDBBackingStore::OpenObjectStoreKeyCursor(
   if (!ObjectStoreCursorOptions(leveldb_transaction, database_id,
                                 object_store_id, range, direction,
                                 &cursor_options, s)) {
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
   }
   std::unique_ptr<ObjectStoreKeyCursorImpl> cursor(
       std::make_unique<ObjectStoreKeyCursorImpl>(transaction->AsWeakPtr(),
                                                  database_id, cursor_options));
   if (!cursor->FirstSeek(s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
 
   return std::move(cursor);
 }
@@ -2804,12 +2804,12 @@ IndexedDBBackingStore::OpenIndexKeyCursor(
   cursor_options.mode = transaction->mode();
   if (!IndexCursorOptions(leveldb_transaction, database_id, object_store_id,
                           index_id, range, direction, &cursor_options, s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
   std::unique_ptr<IndexKeyCursorImpl> cursor(
       std::make_unique<IndexKeyCursorImpl>(transaction->AsWeakPtr(),
                                            database_id, cursor_options));
   if (!cursor->FirstSeek(s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
 
   return std::move(cursor);
 }
@@ -2831,11 +2831,11 @@ IndexedDBBackingStore::OpenIndexCursor(
   cursor_options.mode = transaction->mode();
   if (!IndexCursorOptions(leveldb_transaction, database_id, object_store_id,
                           index_id, range, direction, &cursor_options, s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
   std::unique_ptr<IndexCursorImpl> cursor(new IndexCursorImpl(
       transaction->AsWeakPtr(), database_id, cursor_options));
   if (!cursor->FirstSeek(s))
-    return std::unique_ptr<IndexedDBBackingStore::Cursor>();
+    return nullptr;
 
   return std::move(cursor);
 }
