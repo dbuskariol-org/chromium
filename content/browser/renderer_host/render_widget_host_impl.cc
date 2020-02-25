76916a0a657df8c0312d9b60af462f855302f4d4
Hui Wang <wanghui210@huawei.com>
2020-02-25 23:08:54 +0000

    Shouldn't restart the timer when renderer was blocked.
    This happens when a JavaScriptDialog is showing with the count of
    | in_flight_event_count | is greater than zero then we hide and
    re-show the view which will trigger the
    RestartInputEventAckTimeoutIfNecessary.
    
    Change-Id: I728d4ea26809c2f6ffcf7a46e0e3b124bb63b781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071191
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744418}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1050,7 +1050,8 @@ void RenderWidgetHostImpl::StartInputEventAckTimeout(TimeDelta delay) {
 }
 
 void RenderWidgetHostImpl::RestartInputEventAckTimeoutIfNecessary() {
-  if (input_event_ack_timeout_ && in_flight_event_count_ > 0 && !is_hidden_)
+  if (!GetProcess()->IsBlocked() && input_event_ack_timeout_ &&
+      in_flight_event_count_ > 0 && !is_hidden_)
     input_event_ack_timeout_->Restart(hung_renderer_delay_);
 }
 
