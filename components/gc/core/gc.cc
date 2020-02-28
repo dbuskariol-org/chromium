eb2545300e1d60b5db22f6b135f15de92f3c6a8b
Michael Lippautz <mlippautz@chromium.org>
2020-02-28 20:25:41 +0000

    gc: Add library and test targets
    Adds a target for the library and test targets.
    
    - Integrate into compontents_unittests as all other components do as
      well. This ensures that unittests are automatically run on CI.
    - Provide a separate gc_unittests binary as the components binary
      requires ~35k files to build.
    
    Bug: 1056170
    Change-Id: I8fa235c239f2dc5217145c12abb8c1f27af84fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080422
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745597}

---
+++ components/gc/core/gc.cc
@@ -0,0 +1,16 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/gc/core/gc_export.h"
+
+namespace gc {
+namespace internal {
+
+GC_EXPORT void Dummy() {
+  // TODO(mlippautz): Placeholder to force building a library. Remove as soon as
+  // actual code is available.
+}
+
+}  // namespace internal
+}  // namespace gc
