7d6c5197efa0564d3941c41f6350be8edc196208
Jeff Yoon <jeffyoon@chromium.org>
2020-03-02 21:24:32 +0000

    Enable iOS test runner unit tests as part of presubmit
    Run iOS test runner unit tests as part of presubmit. Keeping scope to
    functional tests, and will have separate CLs for the remaining files
    that require further fixes.
    
    * PRESUBMIT.py - include unit test execution for ios/build/bots/scripts
    * run.py - unit tests caught logic incorrectness. Running the json
       parsing first, before validation.
    * wpr_runner_test.py - remove install xcode mock, fixed broken tests
    * test_apps_test.py and xcode_log_parser_test.py - include main call
       to unittest.
    
    Bug: 1056457
    Change-Id: Ifc233695ba8f6120f862586a497df61e96d4954f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076765
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jeff Yoon <jeffyoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746102}

--- ios/build/bots/scripts/test_apps_test.py
+++ ios/build/bots/scripts/test_apps_test.py
@@ -3,6 +3,8 @@
 # found in the LICENSE file.
 """Unittests for test_apps.py."""
 
+import unittest
+
 import test_apps
 import test_runner_test
 
@@ -54,3 +56,7 @@ class GetGTestFilterTest(test_runner_test.TestCase):
     expected = '-test.1:test.2'
 
     self.assertEqual(test_apps.get_gtest_filter(tests, invert=True), expected)
+
+
+if __name__ == '__main__':
+  unittest.main()
