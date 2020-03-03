3e95729789638406076d053e47d91460804634bc
Sammie Quon <sammiequon@chromium.org>
2020-03-03 01:11:03 +0000

    Revert "media/gpu/vaapi: Create VAImage with va surface size on UploadVideoFrameToSurface()"
    This reverts commit 95ad9e5a05acab4b62cc1d273aec114a7312640c.
    
    Reason for revert: crbug.com/1057857
    
    Original change's description:
    > media/gpu/vaapi: Create VAImage with va surface size on UploadVideoFrameToSurface()
    >
    > This is a workaround of the failure at 1080p video encoding
    > test on grunt.
    >
    > Originally vaCreateImage is created with a coded frame size.
    > However, it causes SEGV_MAPERR on vaPutImage, which copies
    > vaImage to vaSurface.
    >
    > As a result of experiments, the vaImage size needs to be the
    > same as va surface size. Since this is not stated in VA-API
    > document, so this might be a bug of AMD gallium driver.
    >
    > Besides of the workaround, this CL copies visible size area
    > of VideoFrame to the VAImage.
    >
    > Bug: 1048908
    > Bug: 1050377
    > Bug: b:148744040
    > Test: video.EncodeAcel.* on grunt, eve and soraka
    > Test: camera.* on grunt, eve and soraka
    > Change-Id: I1ee853964387846d2452e72cfbd4c939f0a18f62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041382
    > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#744953}
    
    TBR=hiroh@chromium.org,andrescj@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1048908, 1050377, b:148744040, 1057857
    Change-Id: I21fa0d014fdf2bd18fa2629772fb4b822913c82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083867
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746186}

--- media/gpu/vaapi/vaapi_wrapper.h
+++ media/gpu/vaapi/vaapi_wrapper.h
@@ -344,8 +344,7 @@ class MEDIA_GPU_EXPORT VaapiWrapper
 
   // Upload contents of |frame| into |va_surface_id| for encode.
   bool UploadVideoFrameToSurface(const VideoFrame& frame,
-                                 VASurfaceID va_surface_id,
-                                 const gfx::Size& va_surface_size);
+                                 VASurfaceID va_surface_id);
 
   // Create a buffer of |size| bytes to be used as encode output.
   bool CreateVABuffer(size_t size, VABufferID* buffer_id);
