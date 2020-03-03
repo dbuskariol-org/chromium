5aeb5fb111680d01b4b1d774492d995e34e37865
Andrew Grieve <agrieve@chromium.org>
2020-03-03 16:50:30 +0000

    diagnose_bloat.py: Add symbol_level=1 GN arg
    The default was changed to 2, but size builds don't need the extra info.
    
    Change-Id: I8e28af952d3452334c020f8fe4d975a395dd7d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083346
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746408}

--- tools/binary_size/diagnose_bloat.py
+++ tools/binary_size/diagnose_bloat.py
@@ -334,6 +334,9 @@ class _BuildHelper(object):
     gn_args += ' treat_warnings_as_errors=false'
     # Speed things up a bit by skipping lint & errorprone.
     gn_args += ' disable_android_lint=true'
+    # Down from default of 2 to speed up compile and use less disk.
+    # Compiles need at least symbol_level=1 for pak whitelist to work.
+    gn_args += ' symbol_level=1'
     gn_args += ' use_errorprone_java_compiler=false'
     gn_args += ' use_goma=%s' % str(self.use_goma).lower()
     gn_args += ' target_os="%s"' % self.target_os
