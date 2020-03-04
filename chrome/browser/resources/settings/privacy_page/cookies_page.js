f57c8e06e20602db0c128dc8dcdc7ed83008a64f
Sean Harrison <harrisonsean@chromium.org>
2020-03-04 03:46:02 +0000

    Add site specific exceptions to cookies page
    Bug: 1032584
    Change-Id: Iede0e4630357ae5e4ccb653e7925a8beb808f20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002616
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746654}

--- chrome/browser/resources/settings/privacy_page/cookies_page.js
+++ chrome/browser/resources/settings/privacy_page/cookies_page.js
@@ -1,4 +1,4 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,9 +7,14 @@
  * 'settings-cookies-page' is the settings page containing cookies
  * settings.
  */
-(function() {
 
 Polymer({
   is: 'settings-cookies-page',
+
+  properties: {
+    /**
+     * @private {!settings.ContentSettingsTypes}
+     */
+    ContentSettingsTypes: {type: Object, value: settings.ContentSettingsTypes},
+  },
 });
-})();
