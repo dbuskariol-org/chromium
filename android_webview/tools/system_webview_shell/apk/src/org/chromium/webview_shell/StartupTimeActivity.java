a1a4ece1d1509a1d3ba2b61a466a69aed445cf68
Changwan Ryu <changwan@google.com>
2020-03-05 19:51:31 +0000

    Ensure that startup time baseline is zero
    DO_NOTHING is supposed to output an empty result,
    but it currently reports 100-500 ms.
    
    It seems that setContentView() and Activity seem to cause additional
    delays. Posting the measurement task with a delay seems to fix this.
    
    Also, this adds useful comments such as example run and sample results.
    
    Bug: 1055970
    Change-Id: I0e458746b1d4051adef9662bddaff2d368493d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080586
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747379}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/StartupTimeActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/StartupTimeActivity.java
@@ -20,13 +20,38 @@ import org.chromium.base.Log;
 import java.util.LinkedList;
 
 /**
- * This activity is designed for startup time testing of the WebView.
+ * An activity to measure the startup time of WebView in various scenarios.
+ *
+ * Example run:
+ *   # Compile dex file for optimized run.
+ *   adb shell cmd package compile -m speed -f <webview_package_name>
+ *   adb shell killall org.chromium.webview_shell
+ *   # Run scenario: CREATE (1). There are other scenarios you can try.
+ *   adb shell am start -n org.chromium.webview_shell/.StartupTimeActivity \
+ *     -a android.intent.action.VIEW --ei "target" 1
+ *   adb logcat | grep WebViewShell
+ *
+ * Then you will see a tuple of durations in ms that the scenario blocked UI thread for each loop in
+ * the logcat. Here are empirical sample results for WebView 80 on an internal Go device:
+ * +-------------+-------+-----------------+
+ * | target name | index | results (ms)    |
+ * +-------------+-------+-----------------+
+ * | DO_NOTHING  | 0     | (empty)         |
+ * | CREATE      | 1     | (620, 50)       |
+ * | ADD_VIEW    | 2     | (620, 150)      |
+ * | LOAD        | 3     | (620, 150, 140) |
+ * | WORKAROUND  | 4     | (220, 155)      |
+ * +-------------+-------+-----------------+
+ *
+ * Note that you need to run this multiple times and ignore the first few runs
+ * to get an accurate measurement.
  */
 public class StartupTimeActivity extends Activity {
     private static final String TAG = "WebViewShell";
     // Only records the tasks that affect the rendering performance of 30 FPS.
     private static final long MIN_TIME_TO_RECORD_MS = 33;
     private static final long TIME_TO_FINISH_APP_MS = 5000;
+    private static final long TIME_TO_WAIT_BEFORE_START_MS = 2000;
 
     private static final String TARGET_KEY = "target";
 
@@ -40,6 +65,15 @@ public class StartupTimeActivity extends Activity {
     private WebView mWebView;
 
     private Handler mHandler;
+
+    @IntDef({Target.DO_NOTHING, Target.CREATE, Target.ADD_VIEW, Target.LOAD, Target.WORKAROUND})
+    @interface Target {
+        int DO_NOTHING = 0;
+        int CREATE = 1;
+        int ADD_VIEW = 2;
+        int LOAD = 3;
+        int WORKAROUND = 4;
+    }
 
     private Runnable mUiBlockingTaskTracker = new Runnable() {
         @Override
@@ -76,36 +110,35 @@ public class StartupTimeActivity extends Activity {
         }
     };
 
-    @IntDef({Target.DO_NOTHING, Target.CREATE, Target.ADD_VIEW, Target.LOAD, Target.WORKAROUND})
-    @interface Target {
-        int DO_NOTHING = 0;
-        int CREATE = 1;
-        int ADD_VIEW = 2;
-        int LOAD = 3;
-        int WORKAROUND = 4;
-    }
-
     private @Target int getTarget() {
+        final int defaultTarget = Target.CREATE;
         Intent intent = getIntent();
-        if (intent == null) return Target.CREATE;
+        if (intent == null) return defaultTarget;
         Bundle extras = intent.getExtras();
-        if (extras == null) return Target.CREATE;
-        return extras.getInt(TARGET_KEY, Target.CREATE);
+        if (extras == null) return defaultTarget;
+        return extras.getInt(TARGET_KEY, defaultTarget);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        @Target
-        int target = getTarget();
-        getWindow().setTitle(
-                getResources().getString(R.string.title_activity_startup_time));
+        getWindow().setTitle(getResources().getString(R.string.title_activity_startup_time));
         mLayout = new LinearLayout(this);
         setContentView(mLayout);
         mHandler = new Handler();
+        @Target
+        int target = getTarget();
+        Log.i(TAG, "Target: " + target);
+        // There are other posted tasks caused by the activity. Give it some delay to avoid
+        // recording them.
+        mHandler.postDelayed(() -> { runScenario(target); }, TIME_TO_WAIT_BEFORE_START_MS);
+    }
+
+    private void runScenario(@Target int target) {
         mUiBlockingTaskTracker.run();
         switch (target) {
             case Target.DO_NOTHING: {
+                // This is the baseline. It should output an empty result.
                 break;
             }
             case Target.CREATE: {
