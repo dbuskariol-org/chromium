d412aa217748eac5912ed0eb1f8d528f1aed4df1
Kurt Horimoto <kkhorimoto@chromium.org>
2020-03-05 21:43:36 +0000

    [iOS] Disable voice search while VoiceOver is enabled.
    When VoiceOver is enabled, triggering the voice search UI begins reading
    the accessibility elements on the screen.  There is not a good way to
    check when VoiceOver has stopped speaking, so voice search cannot simply
    wait for VoiceOver to stop before recording.  Moreover, removing the
    accessibility features for the other views in the voice search UI is
    also not a solution, as VoiceOver users would be stuck on the voice
    search screen.  The long-term solution to this issue is to update the
    voice search feature to ignore audio that is emitted from the device,
    but this fix is not yet built.
    
    Instead, this CL disables voice search while VoiceOver is enabled.
    Visually-impaired users can still use other iOS accessibility features
    to dictate queries into the omnibox, so this does not negatively
    affect the ability of these users to search.
    
    VoiceSearchAvailability was created to implement this feature.  It is
    an observable helper object that uses VoiceOver notifications and
    VoiceSearchProvider::IsVoiceSearchEnabled() to determine whether voice
    search should be enabled.  The NTP and keyboard accessory view were
    updated to use VoiceSearchAvailability to optionally disable the
    microphone button.
    
    TBR=kkhorimoto@chromium.org
    
    (cherry picked from commit fd40aa3d0cf1879ad6e683e116361336c9541599)
    
    Bug: 998524
    Change-Id: I8789a9a05542bdc659f8d98d0f1c90de3dc3ecb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005903
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#742372}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090301
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4044@{#651}
    Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}

--- ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_input_assistant_items.mm
+++ ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_input_assistant_items.mm
@@ -9,6 +9,7 @@
 #import "ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_assistive_keyboard_views_utils.h"
 #import "ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_ui_bar_button_item.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/browser/voice/voice_search_availability.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
@@ -19,18 +20,23 @@
 
 NSArray<UIBarButtonItemGroup*>* ToolbarAssistiveKeyboardLeadingBarButtonGroups(
     id<ToolbarAssistiveKeyboardDelegate> delegate) {
-  UIImage* voiceSearchIcon =
-      [[UIImage imageNamed:@"keyboard_accessory_voice_search"]
-          imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-  UIBarButtonItem* voiceSearchItem = [[UIBarButtonItem alloc]
-      initWithImage:voiceSearchIcon
-              style:UIBarButtonItemStylePlain
-             target:delegate
-             action:@selector(keyboardAccessoryVoiceSearchTouchUpInside:)];
-  NSString* accessibilityLabel =
-      l10n_util::GetNSString(IDS_IOS_KEYBOARD_ACCESSORY_VIEW_VOICE_SEARCH);
-  voiceSearchItem.accessibilityLabel = accessibilityLabel;
-  voiceSearchItem.accessibilityIdentifier = kVoiceSearchInputAccessoryViewID;
+  NSMutableArray<UIBarButtonItem*>* items = [NSMutableArray array];
+  VoiceSearchAvailability voice_search_availability;
+  if (voice_search_availability.IsVoiceSearchAvailable()) {
+    UIImage* voiceSearchIcon =
+        [[UIImage imageNamed:@"keyboard_accessory_voice_search"]
+            imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIBarButtonItem* voiceSearchItem = [[UIBarButtonItem alloc]
+        initWithImage:voiceSearchIcon
+                style:UIBarButtonItemStylePlain
+               target:delegate
+               action:@selector(keyboardAccessoryVoiceSearchTouchUpInside:)];
+    NSString* accessibilityLabel =
+        l10n_util::GetNSString(IDS_IOS_KEYBOARD_ACCESSORY_VIEW_VOICE_SEARCH);
+    voiceSearchItem.accessibilityLabel = accessibilityLabel;
+    voiceSearchItem.accessibilityIdentifier = kVoiceSearchInputAccessoryViewID;
+    [items addObject:voiceSearchItem];
+  }
 
   UIImage* cameraIcon = [[UIImage imageNamed:@"keyboard_accessory_qr_scanner"]
       imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
@@ -42,8 +48,7 @@ NSArray<UIBarButtonItemGroup*>* ToolbarAssistiveKeyboardLeadingBarButtonGroups(
   SetA11yLabelAndUiAutomationName(
       cameraItem, IDS_IOS_KEYBOARD_ACCESSORY_VIEW_QR_CODE_SEARCH,
       @"QR code Search");
-
-  NSArray* items = @[ voiceSearchItem, cameraItem ];
+  [items addObject:cameraItem];
 
   UIBarButtonItemGroup* group =
       [[UIBarButtonItemGroup alloc] initWithBarButtonItems:items
