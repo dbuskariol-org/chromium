2a3516fe9604bfac69ab2008a76c1364793a6e5f
Steve Kobes <skobes@chromium.org>
2020-03-06 21:31:52 +0000

    Include top 5 shifted nodes in LayoutShift trace event.
    When the layout_shift.debug trace category is enabled, the LayoutShift
    trace event includes an impacted_nodes property containing the node ID
    and the old and new visual rects for up to 5 shifted elements, selected
    on the basis of their contribution to the impact region.
    
    Bug: 1046212
    Change-Id: I5d1833fbf831f0894b7036b73fa3c242b695ed64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042674
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747871}

--- chrome/browser/page_load_metrics/integration_tests/layout_instability_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/layout_instability_browsertest.cc
@@ -39,6 +39,14 @@ std::vector<double> LayoutShiftScores(TraceAnalyzer& analyzer) {
   return scores;
 }
 
+void CheckRect(const Value& list_value, int x, int y, int width, int height) {
+  auto list = list_value.GetList();
+  EXPECT_EQ(list[0].GetInt(), x);
+  EXPECT_EQ(list[1].GetInt(), y);
+  EXPECT_EQ(list[2].GetInt(), width);
+  EXPECT_EQ(list[3].GetInt(), height);
+}
+
 }  // namespace
 
 IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, LayoutInstability) {
@@ -65,4 +73,129 @@ IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, LayoutInstability) {
       "PageLoad.LayoutInstability.CumulativeShiftScore");
   EXPECT_EQ(1ul, samples.size());
   EXPECT_EQ(samples[0], Bucket(LayoutShiftUmaValue(expected_score), 1));
+}
+
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, CLSAttribution_Enclosure) {
+  LoadHTML(R"HTML(
+    <script src="/layout-instability/resources/util.js"></script>
+    <style>
+    body { margin: 0; }
+    #shifter {
+      position: relative; background: #def;
+      width: 300px; height: 200px;
+    }
+    #inner {
+      position: relative; background: #f97;
+      width: 100px; height: 100px;
+    }
+    #absfollow {
+      position: absolute; background: #ffd; opacity: 50%;
+      width: 350px; height: 200px; left: 0; top: 160px;
+    }
+    .stateB { top: 160px; }
+    .stateB #inner { left: 100px; }
+    .stateC ~ #absfollow { top: 0; }
+    </style>
+    <div id="shifter" class="stateA">
+      <div id="inner"></div>
+    </div>
+    <div id="absfollow"></div>
+    <script>
+    runTest = async () => {
+      await waitForAnimationFrames(2);
+      document.querySelector("#shifter").className = "stateB";
+      await waitForAnimationFrames(2);
+      document.querySelector("#shifter").className = "stateC";
+      await waitForAnimationFrames(2);
+    };
+    </script>
+  )HTML");
+
+  StartTracing({"loading", TRACE_DISABLED_BY_DEFAULT("layout_shift.debug")});
+  ASSERT_TRUE(EvalJs(web_contents(), "runTest()").error.empty());
+  std::unique_ptr<TraceAnalyzer> analyzer = StopTracingAndAnalyze();
+
+  TraceEventVector events;
+  analyzer->FindEvents(Query::EventNameIs("LayoutShift"), &events);
+  EXPECT_EQ(2ul, events.size());
+
+  // Shift of #inner ignored as redundant, fully enclosed by #shifter.
+
+  std::unique_ptr<Value> shift_data1;
+  events[0]->GetArgAsValue("data", &shift_data1);
+  auto impacted_nodes1 = shift_data1->FindListKey("impacted_nodes")->GetList();
+  EXPECT_EQ(1ul, impacted_nodes1.size());
+
+  const Value& node_data1 = impacted_nodes1[0];
+  EXPECT_NE(*node_data1.FindIntKey("node_id"), 0);
+  CheckRect(*node_data1.FindListKey("old_rect"), 0, 0, 300, 200);
+  CheckRect(*node_data1.FindListKey("new_rect"), 0, 160, 300, 200);
+
+  // Shift of #shifter ignored as redundant, fully enclosed by #follow.
+
+  std::unique_ptr<Value> shift_data2;
+  events[1]->GetArgAsValue("data", &shift_data2);
+  auto impacted_nodes2 = shift_data2->FindListKey("impacted_nodes")->GetList();
+  EXPECT_EQ(1ul, impacted_nodes2.size());
+
+  const Value& node_data2 = impacted_nodes2[0];
+  EXPECT_NE(*node_data2.FindIntKey("node_id"), 0);
+  CheckRect(*node_data2.FindListKey("old_rect"), 0, 160, 350, 200);
+  CheckRect(*node_data2.FindListKey("new_rect"), 0, 0, 350, 200);
+}
+
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, CLSAttribution_MaxImpact) {
+  LoadHTML(R"HTML(
+    <script src="/layout-instability/resources/util.js"></script>
+    <style>
+    body { margin: 0; }
+    #a, #b, #c, #d, #e, #f {
+      display: inline-block;
+      background: gray;
+      min-width: 10px;
+      min-height: 10px;
+      vertical-align: top;
+    }
+    #a { width: 30px; height: 30px; }
+    #b { width: 20px; height: 20px; }
+    #c { height: 50px; }
+    #d { width: 50px; }
+    #e { width: 40px; height: 30px; }
+    #f { width: 30px; height: 40px; }
+    </style>
+    <div id="grow"></div>
+    <div id="a"></div
+    ><div id="b"></div
+    ><div id="c"></div
+    ><div id="d"></div
+    ><div id="e"></div
+    ><div id="f"></div>
+    <script>
+    runTest = async () => {
+      await waitForAnimationFrames(2);
+      document.querySelector("#grow").style.height = "50px";
+      await waitForAnimationFrames(2);
+    };
+    </script>
+  )HTML");
+
+  StartTracing({"loading", TRACE_DISABLED_BY_DEFAULT("layout_shift.debug")});
+  ASSERT_TRUE(EvalJs(web_contents(), "runTest()").error.empty());
+  std::unique_ptr<TraceAnalyzer> analyzer = StopTracingAndAnalyze();
+
+  TraceEventVector events;
+  analyzer->FindEvents(Query::EventNameIs("LayoutShift"), &events);
+  EXPECT_EQ(1ul, events.size());
+
+  std::unique_ptr<Value> shift_data;
+  events[0]->GetArgAsValue("data", &shift_data);
+  auto impacted = shift_data->FindListKey("impacted_nodes")->GetList();
+  EXPECT_EQ(5ul, impacted.size());
+
+  // #f should replace #b, the smallest div.
+  CheckRect(*impacted[0].FindListKey("new_rect"), 0, 50, 30, 30);    // #a
+  CheckRect(*impacted[1].FindListKey("new_rect"), 150, 50, 30, 40);  // #f
+  CheckRect(*impacted[2].FindListKey("new_rect"), 50, 50, 10, 50);   // #c
+  CheckRect(*impacted[3].FindListKey("new_rect"), 60, 50, 50, 10);   // #d
+  CheckRect(*impacted[4].FindListKey("new_rect"), 110, 50, 40, 30);  // #e
 }
