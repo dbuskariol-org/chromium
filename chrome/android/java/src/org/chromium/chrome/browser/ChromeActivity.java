1f68a7295be0a18bdbd2321e665f66313d1adbde
Andrew Grieve <agrieve@chromium.org>
2020-03-06 19:27:09 +0000

    Reland "Android: Speculative crash fix for DelayedScreenLockIntentHandler"
    This is a reland of b41b7175edcb87cbf6ba0e614a4021d601abc1c8
    
    Original change's description:
    > Android: Speculative crash fix for DelayedScreenLockIntentHandler
    > 
    > Stack is:
    > Caused by: android.util.AndroidRuntimeException: Calling startActivity() from outside of an Activity  context requires the FLAG_ACTIVITY_NEW_TASK flag. Is this really what you want?
    >   at android.app.ContextImpl.startActivity	(ContextImpl.java: 952)
    >   at android.content.ContextWrapper.startActivity	(ContextWrapper.java: 405)
    >   at org.chromium.chrome.browser.ChromeApplication.startActivity	(ChromeApplication.java: 206)
    >   at org.chromium.chrome.browser.ChromeApplication.startActivity	(ChromeApplication.java: 199)
    >   at org.chromium.chrome.browser.DelayedScreenLockIntentHandler.onReceive	(DelayedScreenLockIntentHandler.java: 40)
    >   at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0$LoadedApk$ReceiverDispatcher$Args	(LoadedApk.java: 1550)
    >   ...
    > 
    > From history on DelayedScreenLockIntentHandler, repro steps involve
    > launching a page via NFC while locked.
    > 
    > This restriction is listed as a new one in Android P:
    > https://developer.android.com/about/versions/pie/android-9.0-changes-all#fant-required
    > 
    > (cherry picked from commit 91ac1e4597667f679f99a9c58ada806b0c5c19b1)
    > 
    > Bug: 1034440
    > Change-Id: Ibe13fb99a34d36a15e266d9369393dd6060a1e93
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972135
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#746442}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091585
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/4044@{#686}
    > Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}
    
    Bug: 1034440
    Change-Id: I87cf316577d3b2b1e4c40444b9e2118bd0a327bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090202
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4044@{#691}
    Cr-Branched-From: a6d9daf149a473ceea37f629c41d4527bf2055bd-refs/heads/master@{#737173}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -234,7 +234,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
     private boolean mPartnerBrowserRefreshNeeded;
 
-    protected IntentHandler mIntentHandler;
+    protected final IntentHandler mIntentHandler;
 
     /** Set if {@link #postDeferredStartupIfNeeded()} is called before native has loaded. */
     private boolean mDeferredStartupQueued;
@@ -325,6 +325,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
     /** Controls tab reparenting for night mode. */
     NightModeReparentingController mNightModeReparentingController;
+
+    protected ChromeActivity() {
+        mIntentHandler = new IntentHandler(this, createIntentHandlerDelegate());
+    }
 
     @Override
     protected ActivityWindowAndroid createWindowAndroid() {
@@ -656,8 +660,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
      */
     protected StartupTabPreloader getStartupTabPreloader() {
         if (mStartupTabPreloader == null) {
-            mStartupTabPreloader = new StartupTabPreloader(
-                    this::getIntent, getLifecycleDispatcher(), getWindowAndroid(), this);
+            mStartupTabPreloader = new StartupTabPreloader(this::getIntent,
+                    getLifecycleDispatcher(), getWindowAndroid(), this, mIntentHandler);
         }
         return mStartupTabPreloader;
     }
@@ -754,7 +758,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
         IntentHandler.setTestIntentsEnabled(
                 CommandLine.getInstance().hasSwitch(ContentSwitches.ENABLE_TEST_INTENTS));
-        mIntentHandler = new IntentHandler(createIntentHandlerDelegate(), getPackageName());
 
         // This also ensures that subsequent native library and resource loading takes place
         // immediately, needed by restored tabs that use DFM.
@@ -990,7 +993,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         }
 
         super.onNewIntentWithNative(intent);
-        if (IntentHandler.shouldIgnoreIntent(intent)) return;
+        if (mIntentHandler.shouldIgnoreIntent(intent)) return;
 
         // We send this intent so that we can enter WebVr presentation mode if needed. This
         // call doesn't consume the intent because it also has the url that we need to load.
