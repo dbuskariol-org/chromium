a3d85be24bb9d1312b3ac1dd824df3bc6287ebaf
Aaron Leventhal <aleventhal@chromium.org>
2020-03-07 00:05:36 +0000

    Skip checking children of reparented node
    This is an optimization -- once reparenting has been found, we generally
    keep checking for more, as it could affect the least common ancestor.
    However, there is no need to check the descendants of an ancestor node
    that has already affected the least common ancestor.
    
    Bug: None
    Change-Id: I60967d4fe22ee8835f706a59f3e5d5816dae453b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092176
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747938}

--- ui/accessibility/ax_tree_serializer.h
+++ ui/accessibility/ax_tree_serializer.h
@@ -336,6 +336,7 @@ bool AXTreeSerializer<AXSourceNode, AXNodeData, AXTreeData>::
         // and return true (reparenting was found).
         *out_lca = LeastCommonAncestor(*out_lca, client_child);
         result = true;
+        continue;
       } else if (!client_child->invalid) {
         // This child is already in the client tree and valid, we won't
         // recursively serialize it so we don't need to check this
