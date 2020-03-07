2108b0429f34a017b9c8eb35ad6715e9dd773036
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-03-07 06:12:33 +0000

    viz: Fix leak in GLOutputSurfaceBufferQueue.
    This CL fixes a GPU resource leak in the GLOutputSurfaceBufferQueue.
    Whenever we tell the BufferQueue to free all its buffers,
    GLOutputSurfaceBufferQueue should delete the corresponding textures and
    the stencil buffer. This was being done in the case of Reshape(), but
    not in the case of DidReceiveSwapBuffersAck() with a response of
    SWAP_NAK_RECREATE_BUFFERS. As a consequence, the underlying buffer was
    not being freed since there is reference counting in the SharedImage
    system.
    
    An example of this path happens in kukui when the screen is turned off
    and then on. If this is done many times, eventually too many buffers are
    allocated and leaked and we run out of memory.
    
    /sys/kernel/debug/dri/1/framebuffer
    
    Bug: b:149972616, 1054166
    Test: turn screen on/off in kukui repeatedly and monitor
    Change-Id: Idfbd97a470951103e115cc3ecd9e3d2c4c05f6b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093021
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748009}

--- components/viz/service/display_embedder/gl_output_surface_buffer_queue.cc
+++ components/viz/service/display_embedder/gl_output_surface_buffer_queue.cc
@@ -148,9 +148,9 @@ void GLOutputSurfaceBufferQueue::Reshape(const gfx::Size& size,
   if (may_have_freed_buffers || (stencil_buffer_ && !use_stencil)) {
     auto* gl = context_provider_->ContextGL();
     gl->BindFramebuffer(GL_FRAMEBUFFER, fbo_);
-    gl->FramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT,
-                                GL_RENDERBUFFER, 0);
     if (stencil_buffer_) {
+      gl->FramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT,
+                                  GL_RENDERBUFFER, 0);
       gl->DeleteRenderbuffers(1, &stencil_buffer_);
       stencil_buffer_ = 0u;
     }
@@ -160,7 +160,6 @@ void GLOutputSurfaceBufferQueue::Reshape(const gfx::Size& size,
     if (texture_target_ && may_have_freed_buffers) {
       gl->FramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                                texture_target_, 0, 0);
-      gl->BindFramebuffer(GL_FRAMEBUFFER, 0u);
       for (const auto& buffer_texture : buffer_queue_textures_)
         gl->DeleteTextures(1u, &buffer_texture.second);
       buffer_queue_textures_.clear();
@@ -229,6 +228,30 @@ void GLOutputSurfaceBufferQueue::DidReceiveSwapBuffersAck(
     // Even through the swap failed, this is a fixable error so we can pretend
     // it succeeded to the rest of the system.
     buffer_queue_->FreeAllSurfaces();
+
+    // TODO(andrescj): centralize the logic that deletes the stencil buffer and
+    // the textures since we do this in multiple places.
+    auto* gl = context_provider_->ContextGL();
+    gl->BindFramebuffer(GL_FRAMEBUFFER, fbo_);
+    if (stencil_buffer_) {
+      gl->FramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT,
+                                  GL_RENDERBUFFER, 0);
+      gl->DeleteRenderbuffers(1, &stencil_buffer_);
+      stencil_buffer_ = 0u;
+    }
+
+    // Reshape() must have been called before we got here, so |texture_target_|
+    // should contain a valid value.
+    DCHECK(texture_target_);
+    gl->FramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                             texture_target_, 0, 0);
+    for (const auto& buffer_texture : buffer_queue_textures_)
+      gl->DeleteTextures(1u, &buffer_texture.second);
+    buffer_queue_textures_.clear();
+    current_texture_ = 0u;
+    last_bound_texture_ = 0u;
+    last_bound_mailbox_.SetZero();
+
     force_swap = true;
   }
 
