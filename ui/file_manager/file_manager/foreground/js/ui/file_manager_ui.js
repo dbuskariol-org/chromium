f8228a6ba7097261fe4be9bd267496e5b284ba21
Alex Danilo <adanilo@chromium.org>
2020-03-10 00:57:54 +0000

    Make click anywhere collapse search box
    Clicking anywhere on the action bar causes the search box to collapse
    unless it contains text input.
    
    Change the search box collapse logic to respond to clicks anywhere in
    the Files App rather than just the action bar area.
    
    Bug: 1058547, 1002413
    Change-Id: I5e09ad4eb7ec3e5bf0b732d45665dbbc6af7bb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094377
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748450}

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
@@ -170,8 +170,8 @@ class FileManagerUI {
         queryRequiredElement('#search-box', this.element),
         queryRequiredElement('#search-wrapper', this.element),
         queryRequiredElement('#search-button', this.element));
-    // Add a listener to the containing action bar for hiding the search box.
-    this.actionbar.addEventListener('click', (event) => {
+    // Add a listener to the dialog container for hiding the search box.
+    this.dialogContainer.addEventListener('click', (event) => {
       this.searchBox.removeHidePending(event);
     });
 
