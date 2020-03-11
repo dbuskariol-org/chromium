8675efcce38caa1c393bbad1692a332429d315c7
Sam Bowen <sgbowen@google.com>
2020-03-11 18:13:04 +0000

    Reland "Reland: "Add validator for schema org entities and types in doc metadata mojo.""
    This is a reland of d828e2eb27d505f22bd3a094803b57d0d7dea5fb
    
    * I added the BUILD dependency to the wrong target in the
      previous reland. It needed to be on generate_schema_org_code.
      https://crrev.com/c/2097359
    
    Original change's description:
    > Reland: "Add validator for schema org entities and types in doc metadata mojo."
    >
    > "
    > * Add a validator that removes properties that are not the right type.
    > * Add double, time, and timedelta types in the document metadata mojo
    >   structure.
    > * Use the new mojo types in document metadata extractor.
    > "
    >
    > This is a reland of https://crrev.com/c/2062907 which was reverted in
    > https://crrev.com/c/2095670.
    >
    > The substantial changes are two-fold:
    > (1) add build dep on base in schema_org_properties. This should fix the
    >     try bot issue which was somehow skipped.
    > (2) Rebase on top of https://crrev.com/c/2073337 which was submitted and
    >     contains some changes to the mojo structure.
    >
    > Change-Id: I7f47d74e8833f76d4077028eac0dcb43790a1d26
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097059
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Commit-Queue: Sam Bowen <sgbowen@google.com>
    > Cr-Commit-Position: refs/heads/master@{#748945}
    
    Change-Id: Ibc7124d5a779d663048315a2697c9c050c1c00c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097452
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Sam Bowen <sgbowen@google.com>
    Cr-Commit-Position: refs/heads/master@{#749248}

---
+++ components/schema_org/validator.cc
@@ -0,0 +1,75 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/schema_org/validator.h"
+
+#include <vector>
+
+#include "components/schema_org/common/improved_metadata.mojom.h"
+#include "components/schema_org/schema_org_entity_names.h"
+#include "components/schema_org/schema_org_property_configurations.h"
+#include "components/schema_org/schema_org_property_names.h"
+
+namespace schema_org {
+
+using improved::mojom::Entity;
+using improved::mojom::EntityPtr;
+
+// static
+bool ValidateEntity(Entity* entity) {
+  if (!entity::IsValidEntityName(entity->type)) {
+    return false;
+  }
+
+  // Cycle through properties and remove any that have the wrong type.
+  auto it = entity->properties.begin();
+  while (it != entity->properties.end()) {
+    property::PropertyConfiguration config =
+        property::GetPropertyConfiguration((*it)->name);
+
+    if (!(*it)->values->string_values.empty() && !config.text) {
+      it = entity->properties.erase(it);
+    } else if (!(*it)->values->double_values.empty() && !config.number) {
+      it = entity->properties.erase(it);
+    } else if (!(*it)->values->time_values.empty() && !config.time) {
+      it = entity->properties.erase(it);
+    } else if (!(*it)->values->date_time_values.empty() && !config.date_time &&
+               !config.date) {
+      it = entity->properties.erase(it);
+    } else if (!(*it)->values->entity_values.empty()) {
+      if (config.thing_types.empty()) {
+        // Property is not supposed to have an entity type.
+        it = entity->properties.erase(it);
+      } else {
+        // Check all the entities nested in this property. Remove any invalid
+        // ones.
+        bool has_valid_entities = false;
+        auto nested_it = (*it)->values->entity_values.begin();
+        while (nested_it != (*it)->values->entity_values.end()) {
+          auto& nested_entity = *nested_it;
+          if (!ValidateEntity(nested_entity.get())) {
+            nested_it = (*it)->values->entity_values.erase(nested_it);
+          } else {
+            has_valid_entities = true;
+            ++nested_it;
+          }
+        }
+
+        // If there were no valid entity values for this property, remove the
+        // whole property.
+        if (!has_valid_entities) {
+          it = entity->properties.erase(it);
+        } else {
+          ++it;
+        }
+      }
+    } else {
+      ++it;
+    }
+  }
+
+  return true;
+}
+
+}  // namespace schema_org
