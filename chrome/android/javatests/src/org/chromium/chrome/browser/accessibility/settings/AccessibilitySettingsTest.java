e64d53eeb6130c5eac8c755b6ced4a60a30cbc20
Natalie Chouinard <chouinard@google.com>
2020-03-12 01:48:28 +0000

    [Settings Refactor] Move Accessibility settings
    Move Accessibility settings to
    org.chromium.chrome.browser.accessibility.settings and update OWNERS.
    
    Bug: 1047357
    Change-Id: Ia7bbc3682577cdfc1033c40266d130055b0ec08b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095153
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749467}

--- chrome/android/javatests/src/org/chromium/chrome/browser/accessibility/settings/AccessibilitySettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/accessibility/settings/AccessibilitySettingsTest.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.settings.accessibility;
+package org.chromium.chrome.browser.accessibility.settings;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
