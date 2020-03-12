9d7e5a15ad5f4799e597ffc5b13993c8985749e1
Mikhail Khokhlov <khokhlov@google.com>
2020-03-12 13:32:25 +0000

    sqlite: Create an sqlite_dev target for use in developer tools
    We'd like to use some SQLite features currently disabled in Chrome
    in a developer trace-analyzing tool called trace_processor_shell.
    As discussed on the thread [1], it's undesirable to enable them
    Chrome-wide.
    
    This CL adds an "sqlite_dev" build target and third_party/sqlite/dev
    include directory with a separate sqlite library that has most of the
    features enabled.
    
    [1] https://groups.google.com/a/chromium.org/g/storage-dev/c/OkwAfpYWAa0
    
    Bug: b/150127983, b/150125501
    Change-Id: If328bb737679b6d352904bcb543bc6ed1c522b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082389
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#749640}

--- third_party/sqlite/scripts/generate_amalgamation.sh
+++ third_party/sqlite/scripts/generate_amalgamation.sh
@@ -4,16 +4,19 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This script generates two amalgamated sources for SQLite.
+# One is for Chromium, it has some features disabled. The other one is for
+# developer tools, with a fuller set of features.
+# The build flag sets should be in sync with the defines in BUILD.gn,
+# "chromium_sqlite3_compile_options" and "common_sqlite3_compile_options"
+# configs.
+
 set -o errexit  # Stop the script on the first error.
 set -o nounset  # Catch un-initialized variables.
 
 cd patched
 
-mkdir build
-cd build
-
-# The compile flags here should match the defines in BUILD.gn.
-BUILD_GN_FLAGS="\
+BUILD_GN_FLAGS_COMMON="\
     -DSQLITE_DISABLE_FTS3_UNICODE \
     -DSQLITE_DISABLE_FTS4_DEFERRED \
     -DSQLITE_ENABLE_FTS3 \
@@ -27,47 +30,66 @@ BUILD_GN_FLAGS="\
     -DSQLITE_HAVE_ISNAN \
     -DSQLITE_MAX_WORKER_THREADS=0 \
     -DSQLITE_MAX_MMAP_SIZE=268435456 \
-    -DSQLITE_OMIT_ANALYZE \
-    -DSQLITE_OMIT_AUTOINIT \
-    -DSQLITE_OMIT_AUTORESET \
-    -DSQLITE_OMIT_COMPILEOPTION_DIAGS \
-    -DSQLITE_OMIT_COMPLETE \
     -DSQLITE_OMIT_DECLTYPE \
     -DSQLITE_OMIT_DEPRECATED \
-    -DSQLITE_OMIT_EXPLAIN \
-    -DSQLITE_OMIT_GET_TABLE \
     -DSQLITE_OMIT_LOAD_EXTENSION \
-    -DSQLITE_OMIT_LOOKASIDE \
-    -DSQLITE_OMIT_TCL_VARIABLE \
     -DSQLITE_OMIT_PROGRESS_CALLBACK \
-    -DSQLITE_OMIT_REINDEX \
     -DSQLITE_OMIT_SHARED_CACHE \
-    -DSQLITE_OMIT_TRACE \
-    -DSQLITE_OMIT_UPSERT \
-    -DSQLITE_OMIT_WINDOWFUNC \
     -DSQLITE_SECURE_DELETE \
     -DSQLITE_THREADSAFE=1 \
     -DSQLITE_USE_ALLOCA \
 "
 
-../configure \
-    CFLAGS="-Os $BUILD_GN_FLAGS $(icu-config --cppflags)" \
-    LDFLAGS="$(icu-config --ldflags)" \
-    --disable-load-extension \
-    --enable-amalgamation \
-    --enable-threadsafe
+BUILD_GN_FLAGS_CHROMIUM="\
+    -DSQLITE_OMIT_ANALYZE \
+    -DSQLITE_OMIT_AUTOINIT \
+    -DSQLITE_OMIT_AUTORESET \
+    -DSQLITE_OMIT_COMPILEOPTION_DIAGS \
+    -DSQLITE_OMIT_COMPLETE \
+    -DSQLITE_OMIT_EXPLAIN \
+    -DSQLITE_OMIT_GET_TABLE \
+    -DSQLITE_OMIT_LOOKASIDE \
+    -DSQLITE_OMIT_TCL_VARIABLE \
+    -DSQLITE_OMIT_REINDEX \
+    -DSQLITE_OMIT_TRACE \
+    -DSQLITE_OMIT_UPSERT \
+    -DSQLITE_OMIT_WINDOWFUNC \
+"
 
-make shell.c sqlite3.h sqlite3.c
-cp -f sqlite3.h sqlite3.c ../../amalgamation
+BUILD_GN_FLAGS_DEV="\
+    -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
+"
 
-# shell.c must be placed in a different directory from sqlite3.h, because it
-# contains an '#include "sqlite3.h"' that we want to resolve to our custom
-# //third_party/sqlite/sqlite3.h, not to the sqlite3.h produced here.
-mkdir -p ../../amalgamation/shell/
-cp -f shell.c ../../amalgamation/shell/
+generate_amalgamation() {
+  local build_flags=$1
+  local amalgamation_path=$2
 
-cd ..
-rm -rf build
+  mkdir build
+  cd build
 
-../scripts/extract_sqlite_api.py ../amalgamation/sqlite3.h \
-                                 ../amalgamation/rename_exports.h
+  ../configure \
+      CFLAGS="-Os $build_flags $(icu-config --cppflags)" \
+      LDFLAGS="$(icu-config --ldflags)" \
+      --disable-load-extension \
+      --enable-amalgamation \
+      --enable-threadsafe
+
+  make shell.c sqlite3.h sqlite3.c
+  cp -f sqlite3.h sqlite3.c ../$amalgamation_path/
+
+  # shell.c must be placed in a different directory from sqlite3.h, because it
+  # contains an '#include "sqlite3.h"' that we want to resolve to our custom
+  # //third_party/sqlite/sqlite3.h, not to the sqlite3.h produced here.
+  mkdir -p ../$amalgamation_path/shell/
+  cp -f shell.c ../$amalgamation_path/shell/
+
+  cd ..
+  rm -rf build
+
+  ../scripts/extract_sqlite_api.py $amalgamation_path/sqlite3.h \
+                                   $amalgamation_path/rename_exports.h
+}
+
+generate_amalgamation "$BUILD_GN_FLAGS_COMMON $BUILD_GN_FLAGS_CHROMIUM" ../amalgamation
+generate_amalgamation "$BUILD_GN_FLAGS_COMMON $BUILD_GN_FLAGS_DEV" ../amalgamation_dev
+
