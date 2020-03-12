1a3e96282adc1adce33caa1e68ff4f584715f8ee
Johann <johannkoenig@google.com>
2020-03-12 03:22:24 +0000

    heap mojo: use Proxy to redirect Remote
    ImplPointerType is only defined for mojo::Receiver.
    
    Change-Id: I06983fc3e987b34a8c5e00c9e6b14743d5cb1f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096604
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749503}

--- third_party/blink/renderer/platform/mojo/heap_mojo_remote.h
+++ third_party/blink/renderer/platform/mojo/heap_mojo_remote.h
@@ -11,7 +11,7 @@
 
 namespace blink {
 
-// HeapMojoRemote is a wrapper for mojo::Receiver to be owned by a
+// HeapMojoRemote is a wrapper for mojo::Remote to be owned by a
 // garbage-collected object. Blink is expected to use HeapMojoRemote by
 // default. HeapMojoRemote must be associated with context.
 // HeapMojoRemote's constructor takes context as a mandatory parameter.
@@ -22,14 +22,13 @@ class HeapMojoRemote {
   DISALLOW_NEW();
 
  public:
-  using ImplPointerType = typename mojo::Receiver<Interface>::ImplPointerType;
-
   HeapMojoRemote(ContextLifecycleNotifier* notifier)
       : wrapper_(MakeGarbageCollected<Wrapper>(notifier)) {}
 
-  // Methods to redirect to mojo::Receiver:
-  ImplPointerType operator->() const { return get(); }
-  ImplPointerType get() const { return wrapper_->remote().get(); }
+  // Methods to redirect to mojo::Remote.
+  using Proxy = typename Interface::Proxy_;
+  Proxy* operator->() const { return get(); }
+  Proxy* get() const { return wrapper_->remote().get(); }
   bool is_bound() const { return wrapper_->remote().is_bound(); }
   bool is_connected() const { return wrapper_->remote().is_connected(); }
   void reset() { wrapper_->remote().reset(); }
