4293825bee10cda2eba550cff7365688f4050ab0
kylechar <kylechar@chromium.org>
2020-03-17 16:58:26 +0000

    Cleanup Compositing.SurfaceManager.* histograms
    1. Remove expired histograms that aren't useful anymore.
    2. Extend expiry for the AliveSurfaces histogram.
    3. Update histogram owners with current team members.
    
    Bug: 1036992, 1036993
    Change-Id: I76356542cc1450ee6dbada9bea15ba2f10c2109f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105759
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750998}

--- components/viz/service/surfaces/surface_manager.cc
+++ components/viz/service/surfaces/surface_manager.cc
@@ -32,13 +32,7 @@ namespace {
 
 const char kUmaAliveSurfaces[] = "Compositing.SurfaceManager.AliveSurfaces";
 
-const char kUmaTemporaryReferences[] =
-    "Compositing.SurfaceManager.TemporaryReferences";
-
 constexpr base::TimeDelta kExpireInterval = base::TimeDelta::FromSeconds(10);
-
-const char kUmaRemovedTemporaryReference[] =
-    "Compositing.SurfaceManager.RemovedTemporaryReference";
 
 }  // namespace
 
@@ -195,9 +189,6 @@ void SurfaceManager::GarbageCollectSurfaces() {
   // Log the number of reachable surfaces after a garbage collection.
   UMA_HISTOGRAM_CUSTOM_COUNTS(kUmaAliveSurfaces, reachable_surfaces.size(), 1,
                               200, 50);
-  // Log the number of temporary references after a garbage collection.
-  UMA_HISTOGRAM_CUSTOM_COUNTS(kUmaTemporaryReferences,
-                              temporary_references_.size(), 1, 200, 50);
 
   std::vector<SurfaceId> surfaces_to_delete;
 
@@ -372,16 +363,8 @@ void SurfaceManager::RemoveTemporaryReferenceImpl(const SurfaceId& surface_id,
   auto begin_iter = frame_sink_temp_refs.begin();
 
   // Remove temporary references and range tracking information.
-  for (auto iter = begin_iter; iter != end_iter; ++iter) {
+  for (auto iter = begin_iter; iter != end_iter; ++iter)
     temporary_references_.erase(SurfaceId(frame_sink_id, *iter));
-
-    // If removing more than the temporary reference to |surface_id| then the
-    // reason for removing others is because they are being skipped.
-    const bool was_skipped = (*iter != surface_id.local_surface_id());
-    UMA_HISTOGRAM_ENUMERATION(kUmaRemovedTemporaryReference,
-                              was_skipped ? RemovedReason::SKIPPED : reason,
-                              RemovedReason::COUNT);
-  }
   frame_sink_temp_refs.erase(begin_iter, end_iter);
 
   // If last temporary reference is removed for |frame_sink_id| then cleanup
