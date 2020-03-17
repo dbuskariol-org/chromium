72b33dd8fa6b053600568eb8c1dd4e1a06e4cd2e
Alexander Timin <altimin@chromium.org>
2020-03-17 17:41:24 +0000

    [blink] Remove unused blink::FileSystemManager::TouchFile.
    R=nasko@chromium.org,jsbell@chromium.org
    
    Change-Id: I30a490342c039cdab09e4a37454043a2d3ff84ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106157
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751024}

--- content/browser/file_system/file_system_manager_impl.cc
+++ content/browser/file_system/file_system_manager_impl.cc
@@ -559,28 +559,6 @@ void FileSystemManagerImpl::TruncateSync(const GURL& file_path,
                           base::Passed(&callback)));
 }
 
-void FileSystemManagerImpl::TouchFile(const GURL& path,
-                                      base::Time last_access_time,
-                                      base::Time last_modified_time,
-                                      TouchFileCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  FileSystemURL url(context_->CrackURL(path));
-  base::Optional<base::File::Error> opt_error = ValidateFileSystemURL(url);
-  if (opt_error) {
-    std::move(callback).Run(opt_error.value());
-    return;
-  }
-  if (!security_policy_->CanCreateFileSystemFile(process_id_, url)) {
-    std::move(callback).Run(base::File::FILE_ERROR_SECURITY);
-    return;
-  }
-
-  operation_runner()->TouchFile(
-      url, last_access_time, last_modified_time,
-      base::BindRepeating(&FileSystemManagerImpl::DidFinish, GetWeakPtr(),
-                          base::Passed(&callback)));
-}
-
 void FileSystemManagerImpl::CreateSnapshotFile(
     const GURL& file_path,
     CreateSnapshotFileCallback callback) {
