307266a2a76aac09c68f9c11cfcc4bc0cc225162
Thomas Lukaszewicz <tluk@chromium.org>
2020-03-17 21:21:08 +0000

    Added missing color id definitions to gtk
    Bug: 1062343
    Change-Id: I87307c13437f573a0f984034073691ea4d82eb26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107810
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751116}

--- chrome/browser/ui/gtk/gtk_ui.cc
+++ chrome/browser/ui/gtk/gtk_ui.cc
@@ -969,6 +969,10 @@ void GtkUi::UpdateColors() {
     color_map[ThemeProperties::COLOR_DOWNLOAD_SHELF] = tab_color;
     color_map[ThemeProperties::COLOR_INFOBAR] = tab_color;
     color_map[ThemeProperties::COLOR_STATUS_BUBBLE] = tab_color;
+    color_map[ThemeProperties::COLOR_TAB_BACKGROUND_ACTIVE_FRAME_ACTIVE] =
+        tab_color;
+    color_map[ThemeProperties::COLOR_TAB_BACKGROUND_ACTIVE_FRAME_INACTIVE] =
+        tab_color;
 
     const SkColor background_tab_text_color =
         GetFgColor(header_selector + " GtkLabel.title");
