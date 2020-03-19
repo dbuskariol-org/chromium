f771d8d95029ff42508b8cbdaf1f11572a422154
Marijn Kruisselbrink <mek@chromium.org>
2020-03-19 23:26:50 +0000

    [NativeFS] Possibly show usage indicators on about:blank.
    Rather than figuring out the origin to show a indicator for from
    WebContents::GetLastCommittedURL use RFH::GetLastCommittedOrigin.
    This way about:blank pages will also show a usage indicator if their
    origin has access to files/directories via the native file system API.
    
    Bug: 106439
    Change-Id: I8f25461adaec612852323045be0904ec75b575b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106888
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751841}

--- chrome/browser/ui/views/native_file_system/native_file_system_access_icon_view.cc
+++ chrome/browser/ui/views/native_file_system/native_file_system_access_icon_view.cc
@@ -43,7 +43,7 @@ void NativeFileSystemAccessIconView::UpdateImpl() {
       has_write_access_ = false;
     } else {
       url::Origin origin =
-          url::Origin::Create(GetWebContents()->GetLastCommittedURL());
+          GetWebContents()->GetMainFrame()->GetLastCommittedOrigin();
       auto* context =
           NativeFileSystemPermissionContextFactory::GetForProfileIfExists(
               GetWebContents()->GetBrowserContext());
@@ -80,10 +80,9 @@ NativeFileSystemAccessIconView::GetTextForTooltipAndAccessibleName() const {
 }
 
 void NativeFileSystemAccessIconView::OnExecuting(ExecuteSource execute_source) {
-  url::Origin origin =
-      url::Origin::Create(GetWebContents()->GetLastCommittedURL());
-
   auto* web_contents = GetWebContents();
+  url::Origin origin = web_contents->GetMainFrame()->GetLastCommittedOrigin();
+
   auto* context =
       NativeFileSystemPermissionContextFactory::GetForProfileIfExists(
           web_contents->GetBrowserContext());
