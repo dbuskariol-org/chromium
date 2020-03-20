439fe1c48ce154c9092234f0afc311a38e1932be
Tina Wang <tinazwang@chromium.org>
2020-03-20 20:40:11 +0000

    [Sheriff] Disabled flaky test TestScrollingMaximizedPageBeforeGoingToTabletMode
    Bug: 1063364
    Change-Id: Ib2d3e5688d5a7ac488149b93427903b54b405222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113070
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752163}

--- chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos_browsertest.cc
@@ -1076,8 +1076,10 @@ IN_PROC_BROWSER_TEST_F(TopControlsSlideControllerTest, TestDropDowns) {
   EXPECT_EQ("4", content::EvalJs(contents, "getSelectedValue();"));
 }
 
-IN_PROC_BROWSER_TEST_F(TopControlsSlideControllerTest,
-                       TestScrollingMaximizedPageBeforeGoingToTabletMode) {
+// Disabled due to flakiness. crbug/1063364
+IN_PROC_BROWSER_TEST_F(
+    TopControlsSlideControllerTest,
+    DISABLED_TestScrollingMaximizedPageBeforeGoingToTabletMode) {
   // If the page exists in a maximized browser window before going to tablet
   // mode, the layout that results from going to tablet mode does not change
   // the size of the page viewport. Hence, the visual properties of the renderer
