5236f2b229728b0160a7de60ce119dabf9afd6d0
Elly Fong-Jones <ellyjones@google.com>
2020-03-20 19:54:09 +0000

    mac: hide some system UI in kiosk mode
    This change adds these behaviors to --kiosk mode:
    * The dock is always hidden
    * The menu bar is always hidden & disabled
    * Process switching via cmd-` is disabled
    * Force quit is disabled
    * Session termination UI is disabled
    
    Bug: 531573
    Change-Id: Id5b602ec66e2cef66d641c41b8a109542065125d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112243
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752144}

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -203,6 +203,15 @@ bool IsProfileSignedOut(Profile* profile) {
   return has_entry && entry->IsSigninRequired();
 }
 
+void ConfigureNSAppForKioskMode() {
+  NSApp.presentationOptions =
+      NSApplicationPresentationHideDock | NSApplicationPresentationHideMenuBar |
+      NSApplicationPresentationDisableProcessSwitching |
+      NSApplicationPresentationDisableSessionTermination |
+      NSApplicationPresentationDisableForceQuit |
+      NSApplicationPresentationFullScreen;
+}
+
 }  // namespace
 
 @interface AppController () <HandoffActiveURLObserverBridgeDelegate>
@@ -775,6 +784,15 @@ static base::mac::ScopedObjCClassSwizzler* g_swizzle_imk_input_session;
   [self registerServicesMenuTypesTo:[notify object]];
 
   _startupComplete = YES;
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kKioskMode)) {
+    ConfigureNSAppForKioskMode();
+
+    // Leak a fullscreen request to hide all UI surfaces. This ensures that any
+    // other attempts to manipulate the fullscreen state via web APIs, etc will
+    // not unhide these UI elements.
+    base::mac::RequestFullScreen(base::mac::kFullScreenModeHideAll);
+  }
 
   Browser* browser = chrome::FindLastActive();
   content::WebContents* activeWebContents = nullptr;
