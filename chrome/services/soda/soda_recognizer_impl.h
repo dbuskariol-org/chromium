7873e726c1fbd05ecca345113765e697c009ef03
evliu <evliu@google.com>
2020-03-20 19:21:46 +0000

    Integrate the SODA repo with the recognition pipeline
    This CL integrates the internal SODA repository with the speech
    recognition pipeline. The design document for this feature is located
    here: go/chrome-live-caption. The SodaClient and the C-style SODA
    interface are located in the internal chrome/soda repo.
    
    Bug: 1061174
    Change-Id: Ifdafe1d232196bfbfea22fa12d8855cf808346dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101243
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#752131}

--- chrome/services/soda/soda_recognizer_impl.h
+++ chrome/services/soda/soda_recognizer_impl.h
@@ -6,6 +6,8 @@
 #define CHROME_SERVICES_SODA_SODA_RECOGNIZER_IMPL_H_
 
 #include "base/memory/weak_ptr.h"
+#include "build/branding_buildflags.h"
+#include "chrome/services/soda/buildflags.h"
 #include "media/mojo/mojom/soda_service.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -33,6 +35,10 @@ class SodaRecognizerImpl : public media::mojom::SodaRecognizer {
   explicit SodaRecognizerImpl(
       mojo::PendingRemote<media::mojom::SodaRecognizerClient> remote);
 
+  // Convert the audio buffer into the appropriate format and feed the raw audio
+  // into the SODA instance.
+  void SendAudioToSoda(media::mojom::AudioDataS16Ptr buffer) final;
+
   // Return the transcribed audio from the recognition event back to the caller
   // via the recognition event client.
   void OnRecognitionEvent(const std::string& result);
@@ -40,6 +46,10 @@ class SodaRecognizerImpl : public media::mojom::SodaRecognizer {
   // The remote endpoint for the mojo pipe used to return transcribed audio from
   // the SODA service back to the renderer.
   mojo::Remote<media::mojom::SodaRecognizerClient> client_remote_;
+
+#if BUILDFLAG(ENABLE_SODA)
+  std::unique_ptr<SodaClient> soda_client_;
+#endif  // BUILDFLAG(ENABLE_SODA)
 
   // The callback that is eventually executed on a speech recognition event
   // which passes the transcribed audio back to the caller via the SODA
