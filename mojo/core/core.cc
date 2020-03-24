07cdf115b689faa93c2cf6fc9382b9622d9f874a
Lukasz Anforowicz <lukasza@chromium.org>
2020-03-24 20:52:50 +0000

    Call the default bad message handler if the peer doesn't have a handler.
    To ensure that bad message reports are not lost, make
    mojo::Core::NotifyBadMessage call the default bad message handler when
    the peer doesn't have its own handler.
    
    Bug: 987986
    Change-Id: I563a35260f404f58ea09444ac1851b0d08ecb495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117455
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#752983}

--- mojo/core/core.cc
+++ mojo/core/core.cc
@@ -602,15 +602,19 @@ MojoResult Core::NotifyBadMessage(MojoMessageHandle message_handle,
   auto* message_event =
       reinterpret_cast<ports::UserMessageEvent*>(message_handle);
   auto* message = message_event->GetMessage<UserMessageImpl>();
-  if (message->source_node() == ports::kInvalidNodeName) {
-    DVLOG(1) << "Received invalid message from unknown node.";
+  NodeController* node_controller = GetNodeController();
+
+  if (!node_controller->HasBadMessageHandler(message->source_node())) {
+    if (message->source_node() == ports::kInvalidNodeName)
+      DVLOG(1) << "Received invalid message from unknown node.";
     if (!default_process_error_callback_.is_null())
       default_process_error_callback_.Run(std::string(error, error_num_bytes));
     return MOJO_RESULT_OK;
   }
 
-  GetNodeController()->NotifyBadMessageFrom(
-      message->source_node(), std::string(error, error_num_bytes));
+  node_controller->NotifyBadMessageFrom(message->source_node(),
+                                        std::string(error, error_num_bytes));
+
   return MOJO_RESULT_OK;
 }
 
