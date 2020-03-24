562753cceda1855df7afe6059939e5a6ddc3e3e8
Mark Cogan <marq@google.com>
2020-03-24 16:45:51 +0000

    [iOS] Remove unused includes from TabModel unittest
    Removes many includes (and associated deps in BUILD.gn) in the tab model
    unit test that weren't actually needed. Also removed the functionless
    web client ivar from the test class.
    
    Bug: 620480
    Change-Id: Ib87cf2ff9c425831369446d9e346fb7881acb3d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116207
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752881}

--- ios/chrome/browser/tabs/tab_model_unittest.mm
+++ ios/chrome/browser/tabs/tab_model_unittest.mm
@@ -2,49 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <objc/runtime.h>
+#import "ios/chrome/browser/tabs/tab_model.h"
 
-#include "base/files/file_path.h"
-#include "base/run_loop.h"
-#include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state_manager.h"
-#include "ios/chrome/browser/chrome_url_constants.h"
-#include "ios/chrome/browser/infobars/infobar_manager_impl.h"
 #import "ios/chrome/browser/main/browser_web_state_list_delegate.h"
 #import "ios/chrome/browser/main/test_browser.h"
-#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
-#import "ios/chrome/browser/ntp/new_tab_page_tab_helper_delegate.h"
-#include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
-#import "ios/chrome/browser/sessions/session_ios.h"
 #include "ios/chrome/browser/sessions/session_restoration_browser_agent.h"
 #import "ios/chrome/browser/sessions/session_window_ios.h"
 #import "ios/chrome/browser/sessions/test_session_service.h"
-#import "ios/chrome/browser/tabs/tab_helper_util.h"
-#import "ios/chrome/browser/tabs/tab_model.h"
-#import "ios/chrome/browser/web/chrome_web_client.h"
 #import "ios/chrome/browser/web_state_list/tab_insertion_browser_agent.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
-#import "ios/chrome/browser/web_state_list/web_state_list_delegate.h"
-#import "ios/chrome/browser/web_state_list/web_state_opener.h"
 #import "ios/chrome/browser/web_state_list/web_usage_enabler/web_usage_enabler_browser_agent.h"
-#import "ios/chrome/test/fakes/fake_web_state_list_observing_delegate.h"
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
-#include "ios/web/common/features.h"
-#import "ios/web/navigation/navigation_manager_impl.h"
-#import "ios/web/public/navigation/navigation_manager.h"
-#include "ios/web/public/navigation/referrer.h"
-#import "ios/web/public/session/crw_session_storage.h"
-#import "ios/web/public/session/serializable_user_data_manager.h"
-#include "ios/web/public/test/scoped_testing_web_client.h"
 #include "ios/web/public/test/web_task_environment.h"
 #include "ios/web/public/thread/web_thread.h"
-#import "ios/web/web_state/web_state_impl.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
-#import "third_party/ocmock/OCMock/OCMock.h"
-#include "third_party/ocmock/gtest_support.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -60,7 +33,6 @@ class TabModelTest : public PlatformTest {
   TabModelTest()
       : scoped_browser_state_manager_(
             std::make_unique<TestChromeBrowserStateManager>(base::FilePath())),
-        web_client_(std::make_unique<ChromeWebClient>()),
         web_state_list_delegate_(
             std::make_unique<BrowserWebStateListDelegate>()),
         web_state_list_(
@@ -132,7 +104,6 @@ class TabModelTest : public PlatformTest {
  protected:
   web::WebTaskEnvironment task_environment_;
   IOSChromeScopedTestingChromeBrowserStateManager scoped_browser_state_manager_;
-  web::ScopedTestingWebClient web_client_;
   std::unique_ptr<WebStateListDelegate> web_state_list_delegate_;
   std::unique_ptr<WebStateList> web_state_list_;
   std::unique_ptr<Browser> browser_;
