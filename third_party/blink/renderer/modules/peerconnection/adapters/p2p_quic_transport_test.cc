69b5912380863a78ccabd4fb0e25a589c2226515
Harald Alvestrand <hta@chromium.org>
2020-03-24 08:28:00 +0000

    Use a better API for RTC certificates
    Bug: webrtc:11450
    Change-Id: I3d907c53e9c8cf132f08926f1c39afcddee9c2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116073
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752763}

--- third_party/blink/renderer/modules/peerconnection/adapters/p2p_quic_transport_test.cc
+++ third_party/blink/renderer/modules/peerconnection/adapters/p2p_quic_transport_test.cc
@@ -335,10 +335,9 @@ class QuicPeerForTest {
 
 rtc::scoped_refptr<rtc::RTCCertificate> CreateTestCertificate() {
   rtc::KeyParams params;
-  rtc::SSLIdentity* ssl_identity =
-      rtc::SSLIdentity::Generate("dummy_certificate", params);
+
   return rtc::RTCCertificate::Create(
-      std::unique_ptr<rtc::SSLIdentity>(ssl_identity));
+      rtc::SSLIdentity::Create("dummy_certificate", params));
 }
 
 // Allows faking a failing handshake.
