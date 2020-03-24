1a861efd10d02a5c9a6ec46dc9934c83edb6dbd7
Yuki Shiino <yukishiino@chromium.org>
2020-03-24 12:34:29 +0000

    bind-gen: Update the argument for [StringContext]
    It turned out that we need to respect the receiver object's
    active document, thus we cannot use ${execution_context}.
    
    This patch makes changes on the new bindings generator, which
    is not yet shipped.  No prod change.
    
    Bug: 839389
    Change-Id: Ia8ce3c68a8dbdfc1aff791b89922782788b212ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117559
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752810}

--- third_party/blink/renderer/bindings/scripts/bind_gen/blink_v8_bridge.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/blink_v8_bridge.py
@@ -423,7 +423,8 @@ def make_v8_to_blink_value(blink_var_name,
                 "${exception_state}",
             ]
         if "StringContext" in idl_type.effective_annotations:
-            arguments.append("${execution_context}")
+            arguments.append(
+                "bindings::ExecutionContextFromV8Wrappable(${blink_receiver})")
         blink_value_expr = _format(
             "NativeValueTraits<{_1}>::{_2}({_3})",
             _1=native_value_tag(idl_type),
@@ -490,7 +491,8 @@ def make_v8_to_blink_value_variadic(blink_var_name, v8_array,
         str(v8_array_start_index), "${exception_state}"
     ]
     if "StringContext" in idl_type.element_type.effective_annotations:
-        arguments.append("${execution_context}")
+        arguments.append(
+            "bindings::ExecutionContextFromV8Wrappable(${blink_receiver})")
     text = _format(
         pattern,
         _1=blink_var_name,
