e9924c76ce9a1e0b67d966122da34064ca8d2480
Ahmed Mehfooz <amehfooz@chromium.org>
2020-03-24 00:04:19 +0000

    Fix message center focus traversal with Arc Notifications
    This CL makes sure the control buttons view in arc notifications
    is not set to visible when the message center is collapsed.
    Also fixes GetLastFocusableChild to account for arc notifications.
    
    Bug: 1057988
    Change-Id: If4e9988e7362783f4e0331b7bf8cb3c2fe2ce447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114166
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752648}

--- ash/system/message_center/unified_message_center_view.cc
+++ ash/system/message_center/unified_message_center_view.cc
@@ -302,7 +302,7 @@ void UnifiedMessageCenterView::OnDidChangeFocus(views::View* before,
 
   OnMessageCenterScrolled();
 
-  if (features::IsUnifiedMessageCenterRefactorEnabled()) {
+  if (features::IsUnifiedMessageCenterRefactorEnabled() && !collapsed()) {
     views::View* first_view = GetFirstFocusableChild();
     views::View* last_view = GetLastFocusableChild();
 
@@ -519,8 +519,10 @@ void UnifiedMessageCenterView::NotifyRectBelowScroll() {
 }
 
 void UnifiedMessageCenterView::FocusOut(bool reverse) {
-  if (message_center_bubble_)
-    message_center_bubble_->FocusOut(reverse);
+  if (message_center_bubble_ && message_center_bubble_->FocusOut(reverse)) {
+    GetFocusManager()->ClearFocus();
+    GetFocusManager()->SetStoredFocusView(nullptr);
+  }
 }
 
 void UnifiedMessageCenterView::FocusEntered(bool reverse) {
@@ -542,14 +544,24 @@ views::View* UnifiedMessageCenterView::GetFirstFocusableChild() {
 }
 
 views::View* UnifiedMessageCenterView::GetLastFocusableChild() {
-  views::FocusTraversable* dummy_focus_traversable;
-  views::View* dummy_focus_traversable_view;
-  return focus_search_->FindNextFocusableView(
+  views::FocusTraversable* focus_traversable = nullptr;
+  views::View* dummy_focus_traversable_view = nullptr;
+  views::View* last_view = focus_search_->FindNextFocusableView(
       nullptr, views::FocusSearch::SearchDirection::kBackwards,
       views::FocusSearch::TraversalDirection::kDown,
       views::FocusSearch::StartingViewPolicy::kSkipStartingView,
       views::FocusSearch::AnchoredDialogPolicy::kCanGoIntoAnchoredDialog,
-      &dummy_focus_traversable, &dummy_focus_traversable_view);
+      &focus_traversable, &dummy_focus_traversable_view);
+
+  if (last_view || !focus_traversable)
+    return last_view;
+
+  return focus_traversable->GetFocusSearch()->FindNextFocusableView(
+      nullptr, views::FocusSearch::SearchDirection::kBackwards,
+      views::FocusSearch::TraversalDirection::kDown,
+      views::FocusSearch::StartingViewPolicy::kSkipStartingView,
+      views::FocusSearch::AnchoredDialogPolicy::kCanGoIntoAnchoredDialog,
+      &focus_traversable, &dummy_focus_traversable_view);
 }
 
 }  // namespace ash
