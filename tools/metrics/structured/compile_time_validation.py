b3046604294c9c4d5ac709d0d8437f55cd924c7c
tby <tby@chromium.org>
2020-03-25 06:30:03 +0000

    [Structured metrics] Add more compile-time validation of the XML file.
    Currently we have presubmits that check the structured metrics XML file
    is well-formatted (doesn't have duplicated metric names etc), but these
    only run when you `git cl upload`. It would be better if an invalid
    structured.xml file caused a compilation failure, because it will lead
    to confusing bugs when testing locally.
    
    This CL expands the compile-time validations to cover the cases
    currently checked only via presubmit.
    
    Bug: 1016655
    Change-Id: Iec71eb7db629e566c10c7c997e509a5824373fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120070
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753141}

--- tools/metrics/structured/compile_time_validation.py
+++ tools/metrics/structured/compile_time_validation.py
@@ -3,6 +3,8 @@
 # found in the LICENSE file.
 """Verifies that structured.xml is well-structured."""
 
+from collections import Counter
+from model import _METRIC_TYPE
 from model import _EVENT_TYPE, _EVENTS_TYPE
 from model import _PROJECT_TYPE, _PROJECTS_TYPE
 
@@ -34,6 +36,40 @@ def projectAndEventNamesDontCollide(data):
                        "name: '{}'.").format(event['name']))
 
 
+def eventNamesUnique(data):
+  """Check that no two events have the same name."""
+  name_counts = Counter(
+      event['name'] for event in data[_EVENTS_TYPE.tag][_EVENT_TYPE.tag])
+  for name, count in name_counts.items():
+    if count != 1:
+      raise Exception(
+          "Structured metrics events have duplicate name '{}'.".format(name))
+
+
+def projectNamesUnique(data):
+  """Check that no two projects have the same name."""
+  name_counts = Counter(
+      project['name']
+      for project in data[_PROJECTS_TYPE.tag][_PROJECT_TYPE.tag])
+  for name, count in name_counts.items():
+    if count != 1:
+      raise Exception(
+          "Structured metrics projects have duplicate name '{}'.".format(name))
+
+
+def metricNamesUniqueWithinEvent(data):
+  """Check that no two metrics within an event have the same name."""
+  for event in data[_EVENTS_TYPE.tag][_EVENT_TYPE.tag]:
+    name_counts = Counter(metric['name'] for metric in event[_METRIC_TYPE.tag])
+    for name, count in name_counts.items():
+      if count != 1:
+        raise Exception(("Structured metrics event '{}' has duplicated metric "
+                         "name '{}'.").format(event['name'], name))
+
+
 def validate(data):
   eventsReferenceValidProjects(data)
   projectAndEventNamesDontCollide(data)
+  eventNamesUnique(data)
+  projectNamesUnique(data)
+  metricNamesUniqueWithinEvent(data)
