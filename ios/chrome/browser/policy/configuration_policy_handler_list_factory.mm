d319f402a5b2a3e3ac387d1ff80cebfd878c782c
Guillaume Jenkins <gujen@google.com>
2020-03-25 13:12:06 +0000

    [iOS Enterprise] Add policy map + handler loop for simple policies
    Creates the policy map for simple policies (those that simply set a
    Chrome flag). Also adds an entry as an example (I arbitrarily picked
    kSearchSuggestEnabled), though unit tests will still need to be added
    for that policy.
    
    Also creates the simple loop that creates SimplePolicyHandlers for each
    policy described in the map.
    
    Change-Id: If35def703d79df367aa69a55cdd13fa27d240337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117468
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#753201}

--- ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
@@ -11,12 +11,20 @@
 #include "components/policy/core/browser/configuration_policy_handler_parameters.h"
 #include "components/policy/policy_constants.h"
 #include "ios/chrome/browser/policy/policy_features.h"
+#include "ios/chrome/browser/pref_names.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+using policy::PolicyToPreferenceMapEntry;
+using policy::SimplePolicyHandler;
+
 namespace {
+
+const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
+    {policy::key::kSearchSuggestEnabled, prefs::kSearchSuggestEnabled,
+     base::Value::Type::BOOLEAN}};
 
 void PopulatePolicyHandlerParameters(
     policy::PolicyHandlerParameters* parameters) {}
@@ -30,5 +38,12 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildPolicyHandlerList(
       std::make_unique<policy::ConfigurationPolicyHandlerList>(
           base::Bind(&PopulatePolicyHandlerParameters),
           base::Bind(&policy::GetChromePolicyDetails));
+
+  for (size_t i = 0; i < base::size(kSimplePolicyMap); ++i) {
+    handlers->AddHandler(std::make_unique<SimplePolicyHandler>(
+        kSimplePolicyMap[i].policy_name, kSimplePolicyMap[i].preference_path,
+        kSimplePolicyMap[i].value_type));
+  }
+
   return handlers;
 }
