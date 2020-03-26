c4ac33faf7cd9ec40dcc55422ea62af25590d8b8
Peng Huang <penghuang@chromium.org>
2020-03-26 14:20:02 +0000

    Reland "Reland "VulkanImage: support create from GpuMemoryBufferHandle on Android""
    This is a reland of 105f51975f0b8e04e57fd417b1b97e668f8f9876
    
    Original change's description:
    > Reland "VulkanImage: support create from GpuMemoryBufferHandle on Android"
    >
    > This is a reland of 838eb0340c389a587575c67efbc0ce0a7e30fe15
    >
    > Original change's description:
    > > VulkanImage: support create from GpuMemoryBufferHandle on Android
    > >
    > > VulkanImage::CreateFromGpuMemoryBufferHandle() is added to replace
    > > VulkanImplementation::CreateVkImageAndImportAHB(), and will replace
    > > VulkanImplementation::CreateImageFromGpuMemoryHandle() as well.
    > >
    > > Bug: None
    > > Change-Id: I7c6e3cea685f1ec305790f4a6d7bb66164f26e55
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114587
    > > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#753394}
    >
    > TBR=rjkroege@chromium.org,vasilyt@chromium.org
    >
    > Cq-Include-Trybots: luci.chromium.try:android_archive_rel_ng
    > Bug: None
    > Change-Id: Ie6132474ef4a386ed661116aa8f580fd8460901d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2121442
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#753501}
    
    TBR=rjkroege@chromium.org,vasilyt@chromium.org
    
    Bug: None
    Change-Id: Ie9c83df2711431373c6eed4794be19640de9702f
    Cq-Include-Trybots: luci.chromium.try:android_archive_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119888
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753608}

--- gpu/vulkan/vulkan_image.h
+++ gpu/vulkan/vulkan_image.h
@@ -14,6 +14,7 @@
 #include "gpu/ipc/common/vulkan_ycbcr_info.h"
 #include "gpu/vulkan/vulkan_export.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 
 #if defined(OS_FUCHSIA)
 #include <lib/zx/vmo.h>
@@ -48,8 +49,17 @@ class VULKAN_EXPORT VulkanImage {
       const gfx::Size& size,
       VkFormat format,
       VkImageUsageFlags usage,
-      VkImageCreateFlags flags,
-      VkImageTiling image_tiling);
+      VkImageCreateFlags flags = 0,
+      VkImageTiling image_tiling = VK_IMAGE_TILING_OPTIMAL);
+
+  static std::unique_ptr<VulkanImage> CreateFromGpuMemoryBufferHandle(
+      VulkanDeviceQueue* device_queue,
+      gfx::GpuMemoryBufferHandle gmb_handle,
+      const gfx::Size& size,
+      VkFormat format,
+      VkImageUsageFlags usage,
+      VkImageCreateFlags flags = 0,
+      VkImageTiling image_tiling = VK_IMAGE_TILING_OPTIMAL);
 
   static std::unique_ptr<VulkanImage> Create(
       VulkanDeviceQueue* device_queue,
@@ -93,13 +103,22 @@ class VULKAN_EXPORT VulkanImage {
                   VkImageCreateFlags flags,
                   VkImageTiling image_tiling,
                   void* image_create_info_next,
-                  void* memory_allocation_info_next);
+                  void* memory_allocation_info_next,
+                  const VkMemoryRequirements* requirements);
   bool InitializeWithExternalMemory(VulkanDeviceQueue* device_queue,
                                     const gfx::Size& size,
                                     VkFormat format,
                                     VkImageUsageFlags usage,
                                     VkImageCreateFlags flags,
                                     VkImageTiling image_tiling);
+  bool InitializeFromGpuMemoryBufferHandle(
+      VulkanDeviceQueue* device_queue,
+      gfx::GpuMemoryBufferHandle gmb_handle,
+      const gfx::Size& size,
+      VkFormat format,
+      VkImageUsageFlags usage,
+      VkImageCreateFlags flags,
+      VkImageTiling image_tiling);
 
   VulkanDeviceQueue* device_queue_ = nullptr;
   gfx::Size size_;
