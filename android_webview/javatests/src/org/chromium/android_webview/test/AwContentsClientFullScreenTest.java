b1f027d7e670062c55269fd5e47534136d955c6c
Hayato Ito <hayato@chromium.org>
2020-03-27 02:44:34 +0000

    Sheriff: Disable a flaky test
    TBR: ntfschr@chromium.org
    Bug: 1065115
    Change-Id: I25b9c70329dc842e39bff4708ce9627ea1b2eb8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123489
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753888}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenTest.java
@@ -230,6 +230,7 @@ public class AwContentsClientFullScreenTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "crbug.com/1065115")
     public void testPowerSaveBlockerIsEnabledDuringFullscreenPlayback_video() throws Throwable {
         doTestPowerSaveBlockerIsEnabledDuringFullscreenPlayback(VIDEO_TEST_URL);
     }
