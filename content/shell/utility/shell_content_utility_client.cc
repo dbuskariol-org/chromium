8a69ae5f3d3a2c072bdc837f840b5f9b8f5b0bc9
Robert Sesek <rsesek@chromium.org>
2020-03-27 23:04:34 +0000

    Add missing null-check to ShellContentUtilityClient::RegisterNetworkBinders()
    The network_service_test_helper_ is created conditionally in the
    constructor, and it's undefined behavior to dereference a null pointer.
    
    Bug: 1064609
    Change-Id: Ic9c79ef049225d1172e568c39d14c9528e3e78d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124538
    Auto-Submit: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754204}

--- content/shell/utility/shell_content_utility_client.cc
+++ content/shell/utility/shell_content_utility_client.cc
@@ -177,7 +177,8 @@ mojo::ServiceFactory* ShellContentUtilityClient::GetIOThreadServiceFactory() {
 
 void ShellContentUtilityClient::RegisterNetworkBinders(
     service_manager::BinderRegistry* registry) {
-  network_service_test_helper_->RegisterNetworkBinders(registry);
+  if (network_service_test_helper_)
+    network_service_test_helper_->RegisterNetworkBinders(registry);
 }
 
 }  // namespace content
