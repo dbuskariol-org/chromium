9dc21ef54e9c9e737608a844bf40a366b362dc0f
Reilly Grant <reillyg@chromium.org>
2020-03-27 20:23:49 +0000

    Add logging when receiving an invalid frame token
    To investigate issue 1045372 some crash keys were added to log the old
    and new frame tokens however this doesn't work for the flaky browser
    tests I am investigating in issue 1022321. This change adds log messages
    which should show up when the test flakes.
    
    Bug: 1022321
    Change-Id: If1c88d1cea3dc771c96a4d636b6797a8686b6315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123030
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754145}

--- content/browser/renderer_host/frame_token_message_queue.cc
+++ content/browser/renderer_host/frame_token_message_queue.cc
@@ -35,6 +35,11 @@ void FrameTokenMessageQueue::DidProcessFrame(uint32_t frame_token) {
                                    base::NumberToString(frame_token));
     base::debug::DumpWithoutCrashing();
 
+    // TODO(reillyg): Remove this once investigation into
+    // https://crbug.com/1022321 is over.
+    LOG(ERROR) << "Got invalid frame token: " << frame_token
+               << " <= " << last_received_frame_token_;
+
     client_->OnInvalidFrameToken(frame_token);
     return;
   }
@@ -59,6 +64,10 @@ void FrameTokenMessageQueue::EnqueueOrRunFrameTokenCallback(
     base::OnceClosure callback) {
   // Zero token is invalid.
   if (!frame_token) {
+    // TODO(reillyg): Remove this once investigation into
+    // https://crbug.com/1022321 is over.
+    LOG(ERROR) << "Got invalid frame token: " << frame_token;
+
     client_->OnInvalidFrameToken(frame_token);
     return;
   }
