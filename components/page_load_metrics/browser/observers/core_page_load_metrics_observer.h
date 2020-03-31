4a7fe5295046b465852b4ca79090dc08731de9ae
Xiaocheng Hu <xiaochengh@chromium.org>
2020-03-31 01:46:40 +0000

    Record split UMA histograms when rendering has been delayed for font preloading
    This patch adds split histograms of the following metrics when
    rendering delay for web font preloading has been triggered:
    - PaintTiming.NavigationToFirstPaint
    - PaintTiming.NavigationToFirstContentfulPaint
    - PaintTiming.NavigationToLargestContentfulPaint
    - LayoutInstability.CumulativeShiftScore
    
    This will help us understand the effectiveness of this optimization, and
    adjust the length of the delay to the best value.
    
    Bug: 1040632
    Change-Id: I6f8aa151c576c9d8d583647a3a7ef6fcb8fb833d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2109251
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754771}

--- components/page_load_metrics/browser/observers/core_page_load_metrics_observer.h
+++ components/page_load_metrics/browser/observers/core_page_load_metrics_observer.h
@@ -98,6 +98,13 @@ extern const char kBackgroundHistogramInputToFirstPaint[];
 extern const char kHistogramInputToFirstContentfulPaint[];
 extern const char kBackgroundHistogramInputToFirstContentfulPaint[];
 
+// Split histograms recorded only when the first rendering cycle has been
+// delayed for web font preloading.
+// See design doc https://bit.ly/36E8UKB for details.
+extern const char kHistogramFontPreloadFirstPaint[];
+extern const char kHistogramFontPreloadFirstContentfulPaint[];
+extern const char kHistogramFontPreloadLargestContentfulPaint[];
+
 enum FirstMeaningfulPaintStatus {
   FIRST_MEANINGFUL_PAINT_RECORDED,
   FIRST_MEANINGFUL_PAINT_BACKGROUNDED,
@@ -163,6 +170,8 @@ class CorePageLoadMetricsObserver
       const page_load_metrics::mojom::CpuTiming& timing) override;
   void OnDidFinishSubFrameNavigation(
       content::NavigationHandle* navigation_handle) override;
+  void OnLoadingBehaviorObserved(content::RenderFrameHost* rfh,
+                                 int behavior_flags) override;
 
  private:
   void TrackPossibleClickBurst(const blink::WebInputEvent& event);
@@ -204,6 +213,10 @@ class CorePageLoadMetricsObserver
 
   // True if we've received a scroll input after first paint has happened.
   bool received_scroll_input_after_first_paint_ = false;
+
+  // True if the first rendering cycle has been delayed due to web font
+  // preloading.
+  bool render_delayed_for_web_font_preloading_observed_ = false;
 
   base::TimeTicks first_paint_;
 
