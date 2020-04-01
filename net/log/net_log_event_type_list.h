52788b20cefb14f79865323a62a67152ccd43380
Renjie Tang <renjietang@chromium.org>
2020-04-01 18:37:13 +0000

    Log coalesced stream frames.
    Change-Id: I99d8d77190b66eda62a5116a224ae23d94845d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128470
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755461}

--- net/log/net_log_event_type_list.h
+++ net/log/net_log_event_type_list.h
@@ -1812,6 +1812,15 @@ EVENT_TYPE(QUIC_SESSION_STREAM_FRAME_RECEIVED)
 //   }
 EVENT_TYPE(QUIC_SESSION_STREAM_FRAME_SENT)
 
+// A stream frame is coalesced with an existing stream frame.
+//   {
+//     "stream_id": <The id of the stream which this data is for>,
+//     "fin": <True if this is the final data set by the peer on this stream>,
+//     "offset": <Offset in the byte stream where this data starts>,
+//     "length": <Length of the data in this frame>,
+//   }
+EVENT_TYPE(QUIC_SESSION_STREAM_FRAME_COALESCED)
+
 // Session received an ACK frame.
 //   {
 //     "sent_info": <Details of packet sent by the peer>
