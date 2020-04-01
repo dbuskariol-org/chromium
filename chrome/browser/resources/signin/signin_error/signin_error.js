dd7519288490529a824df96bd8bc598e326bca3f
dpapad <dpapad@chromium.org>
2020-04-01 20:40:57 +0000

    Fix a few UI bugs in chrome://signin-error.
    This is in preparation of migrating to Polymer3.
    
    Specifically:
     - Adjust icon color in dark mode, previously same color was used
       in both light and dark mode.
     - Correctly hide "Learn more" link when it should be hidden.
       Previously was not correctly hidden because :empty CSS selector
       was always false.
     - Add missing icon on 3rd row of profile-blocking-error-message.
     - Remove unnecessary white space between #normal-error-message and
       #action-container.
    
    In the process of fixing these bugs and paving the way for Polymer3:
     - Stop using "flattenhtml" to embed icons, instead use same icons
       from shared cr_elements/icons.html along with iron-icon.html
     - Extract most UI and logic in a <signin-error-app> element.
     - Leverage Polymer bindings to determine what to show/hide instead of
       :empty CSS selectors.
    
    Bug: 1012533
    Change-Id: Iaed8fdabbeb821f5c4d5b52f76510d477c55fd1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130809
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755510}

--- chrome/browser/resources/signin/signin_error/signin_error.js
+++ chrome/browser/resources/signin/signin_error/signin_error.js
@@ -4,17 +4,7 @@
 
 (function() {
 function initialize() {
-  document.addEventListener('keydown', onKeyDown);
-  $('confirmButton').addEventListener('click', onConfirm);
-  $('closeButton').addEventListener('click', onConfirm);
-  $('switchButton').addEventListener('click', onSwitchToExistingProfile);
-  $('learnMoreLink').addEventListener('click', onLearnMore);
-  if (loadTimeData.getBoolean('isSystemProfile')) {
-    $('learnMoreLink').hidden = true;
-  }
-
   cr.addWebUIListener('clear-focus', clearFocus);
-  cr.addWebUIListener('switch-button-unavailable', removeSwitchButton);
 
   // Prefer using |document.body.offsetHeight| instead of
   // |document.body.scrollHeight| as it returns the correct height of the
@@ -22,37 +12,8 @@ function initialize() {
   chrome.send('initializedWithSize', [document.body.offsetHeight]);
 }
 
-function onKeyDown(e) {
-  // If the currently focused element isn't something that performs an action
-  // on "enter" being pressed and the user hits "enter", perform the default
-  // action of the dialog, which is "OK".
-  if (e.key == 'Enter' &&
-      !/^(A|CR-BUTTON)$/.test(document.activeElement.tagName)) {
-    $('confirmButton').click();
-    e.preventDefault();
-  }
-}
-
-function onConfirm(e) {
-  chrome.send('confirm');
-}
-
-function onSwitchToExistingProfile(e) {
-  chrome.send('switchToExistingProfile');
-}
-
-function onLearnMore(e) {
-  chrome.send('learnMore');
-}
-
 function clearFocus() {
   document.activeElement.blur();
-}
-
-function removeSwitchButton() {
-  $('switchButton').hidden = true;
-  $('closeButton').hidden = true;
-  $('confirmButton').hidden = false;
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
