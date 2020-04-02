a7615b1173b4420e36f64e103f3762bdd6e3da19
Lei Zhang <thestig@chromium.org>
2020-04-02 22:55:25 +0000

    Remove unused IDS_CREATE_SHORTCUTS_DIALOG_WIDTH_CHARS.
    Change-Id: I2aca9cbdd3df2efd0e2a5c9316cc121f345ce06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133230
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756040}

--- chrome/browser/ui/views/create_application_shortcut_view.cc
+++ chrome/browser/ui/views/create_application_shortcut_view.cc
@@ -147,7 +147,6 @@ void CreateChromeApplicationShortcutView::InitControls() {
 }
 
 gfx::Size CreateChromeApplicationShortcutView::CalculatePreferredSize() const {
-  // TODO(evanm): should this use IDS_CREATE_SHORTCUTS_DIALOG_WIDTH_CHARS?
   static const int kDialogWidth = 360;
   int height = GetLayoutManager()->GetPreferredHeightForWidth(this,
       kDialogWidth);
