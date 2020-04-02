4e6c1d9ee2bd2b454121f9a4b5b6389b7f6f524a
Natalie Chouinard <chouinard@chromium.org>
2020-04-02 20:44:39 +0000

    Re-enable testPaymentsIntegrationCheckboxClearsServerAutofillCreditCards
    Test succeeded 10/10 times locally, and no log data available on the bug
    or flakiness dashboard (disabled >6 months ago). Re-enabling to monitor
    whether flakiness still exists.
    
    Pre-emptively increasing timeout to MediumTest either way since this
    test is more than a simple unit test.
    
    Bug: 994726
    Change-Id: Iaa645356906b17ff17c44cc49c4c76c7b9dac4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134204
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755973}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.sync;
 
 import android.app.Dialog;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +24,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
@@ -203,9 +203,8 @@ public class ManageSyncSettingsTest {
         assertPaymentsIntegrationEnabled(true);
     }
 
-    @DisabledTest(message = "crbug.com/994726")
     @Test
-    @SmallTest
+    @MediumTest
     @Feature({"Sync"})
     public void testPaymentsIntegrationCheckboxClearsServerAutofillCreditCards() {
         mSyncTestRule.setUpTestAccountAndSignIn();
