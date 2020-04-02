02ba63de1444facb507685a396b23202e1b565be
Alexandre Courbot <acourbot@chromium.org>
2020-04-02 06:16:08 +0000

    media/gpu/v4l2: move frame splitter to its own helper class
    The frame splitting functionality will be required by the V4L2VD, so
    move it into a helper class to avoid code duplication. Doing so also
    lets us remove a few members from V4L2VDA and simplify the class
    overall.
    
    BUG=b:149663704
    TEST=vdatest passes on Hana.
    
    Change-Id: I6fd7fdcb20e6fd695f6028c0be92256bf97e6454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130455
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755709}

--- media/gpu/v4l2/v4l2_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_video_decode_accelerator.cc
@@ -37,7 +37,6 @@
 #include "media/gpu/v4l2/v4l2_image_processor_backend.h"
 #include "media/gpu/v4l2/v4l2_stateful_workaround.h"
 #include "media/gpu/v4l2/v4l2_vda_helpers.h"
-#include "media/video/h264_parser.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_pixmap_handle.h"
 #include "ui/gl/gl_context.h"
@@ -144,14 +143,12 @@ V4L2VideoDecodeAccelerator::V4L2VideoDecodeAccelerator(
       decoder_cmd_supported_(false),
       flush_awaiting_last_output_buffer_(false),
       reset_pending_(false),
-      decoder_partial_frame_pending_(false),
       output_dpb_size_(0),
       picture_clearing_count_(0),
       device_poll_thread_("V4L2DevicePollThread"),
       egl_display_(egl_display),
       get_gl_context_cb_(get_gl_context_cb),
       make_context_current_cb_(make_context_current_cb),
-      video_profile_(VIDEO_CODEC_PROFILE_UNKNOWN),
       input_format_fourcc_(0),
       weak_this_factory_(this) {
   weak_this_ = weak_this_factory_.GetWeakPtr();
@@ -195,8 +192,6 @@ bool V4L2VideoDecodeAccelerator::Initialize(const Config& config,
     DCHECK(!decode_client_);
     decode_client_ = client_;
   }
-
-  video_profile_ = config.profile;
 
   // We need the context to be initialized to query extensions.
   if (make_context_current_cb_) {
@@ -260,8 +255,12 @@ void V4L2VideoDecodeAccelerator::InitializeTask(const Config& config,
   if (!config_result)
     return;
 
-  if (config.profile >= H264PROFILE_MIN && config.profile <= H264PROFILE_MAX) {
-    decoder_h264_parser_.reset(new H264Parser());
+  frame_splitter_ =
+      v4l2_vda_helpers::InputBufferFragmentSplitter::CreateFromProfile(
+          config.profile);
+  if (!frame_splitter_) {
+    NOTIFY_ERROR(INVALID_ARGUMENT);
+    return;
   }
 
   base::trace_event::MemoryDumpManager::GetInstance()->RegisterDumpProvider(
@@ -920,7 +919,6 @@ void V4L2VideoDecodeAccelerator::DecodeBufferTask() {
 
     if (schedule_task && AppendToInputFrame(NULL, 0) && FlushInputFrame()) {
       VLOGF(2) << "enqueued flush buffer";
-      decoder_partial_frame_pending_ = false;
       schedule_task = true;
     } else {
       // If we failed to enqueue the empty buffer (due to pipeline
@@ -950,7 +948,8 @@ void V4L2VideoDecodeAccelerator::DecodeBufferTask() {
       }
     }
 
-    if (!AdvanceFrameFragment(data, data_size, &decoded_size)) {
+    if (!frame_splitter_->AdvanceFrameFragment(data, data_size,
+                                               &decoded_size)) {
       LOG(ERROR) << "Invalid Stream";
       NOTIFY_ERROR(UNREADABLE_INPUT);
       return;
@@ -991,94 +990,6 @@ void V4L2VideoDecodeAccelerator::DecodeBufferTask() {
   }
 }
 
-bool V4L2VideoDecodeAccelerator::AdvanceFrameFragment(const uint8_t* data,
-                                                      size_t size,
-                                                      size_t* endpos) {
-  DCHECK(decoder_thread_.task_runner()->BelongsToCurrentThread());
-
-  if (video_profile_ >= H264PROFILE_MIN && video_profile_ <= H264PROFILE_MAX) {
-    // For H264, we need to feed HW one frame at a time.  This is going to take
-    // some parsing of our input stream.
-    decoder_h264_parser_->SetStream(data, size);
-    H264NALU nalu;
-    H264Parser::Result result;
-    *endpos = 0;
-
-    // Keep on peeking the next NALs while they don't indicate a frame
-    // boundary.
-    for (;;) {
-      bool end_of_frame = false;
-      result = decoder_h264_parser_->AdvanceToNextNALU(&nalu);
-      if (result == H264Parser::kInvalidStream ||
-          result == H264Parser::kUnsupportedStream)
-        return false;
-      if (result == H264Parser::kEOStream) {
-        // We've reached the end of the buffer before finding a frame boundary.
-        decoder_partial_frame_pending_ = true;
-        *endpos = size;
-        return true;
-      }
-      switch (nalu.nal_unit_type) {
-        case H264NALU::kNonIDRSlice:
-        case H264NALU::kIDRSlice:
-          if (nalu.size < 1)
-            return false;
-          // For these two, if the "first_mb_in_slice" field is zero, start a
-          // new frame and return.  This field is Exp-Golomb coded starting on
-          // the eighth data bit of the NAL; a zero value is encoded with a
-          // leading '1' bit in the byte, which we can detect as the byte being
-          // (unsigned) greater than or equal to 0x80.
-          if (nalu.data[1] >= 0x80) {
-            end_of_frame = true;
-            break;
-          }
-          break;
-        case H264NALU::kSEIMessage:
-        case H264NALU::kSPS:
-        case H264NALU::kPPS:
-        case H264NALU::kAUD:
-        case H264NALU::kEOSeq:
-        case H264NALU::kEOStream:
-        case H264NALU::kReserved14:
-        case H264NALU::kReserved15:
-        case H264NALU::kReserved16:
-        case H264NALU::kReserved17:
-        case H264NALU::kReserved18:
-          // These unconditionally signal a frame boundary.
-          end_of_frame = true;
-          break;
-        default:
-          // For all others, keep going.
-          break;
-      }
-      if (end_of_frame) {
-        if (!decoder_partial_frame_pending_ && *endpos == 0) {
-          // The frame was previously restarted, and we haven't filled the
-          // current frame with any contents yet.  Start the new frame here and
-          // continue parsing NALs.
-        } else {
-          // The frame wasn't previously restarted and/or we have contents for
-          // the current frame; signal the start of a new frame here: we don't
-          // have a partial frame anymore.
-          decoder_partial_frame_pending_ = false;
-          return true;
-        }
-      }
-      *endpos = (nalu.data + nalu.size) - data;
-    }
-    NOTREACHED();
-    return false;
-  } else {
-    DCHECK_GE(video_profile_, VP8PROFILE_MIN);
-    DCHECK_LE(video_profile_, VP9PROFILE_MAX);
-    // For VP8/9, we can just dump the entire buffer.  No fragmentation needed,
-    // and we never return a partial frame.
-    *endpos = size;
-    decoder_partial_frame_pending_ = false;
-    return true;
-  }
-}
-
 void V4L2VideoDecodeAccelerator::ScheduleDecodeBufferTaskIfNeeded() {
   DCHECK(decoder_thread_.task_runner()->BelongsToCurrentThread());
 
@@ -1108,7 +1019,7 @@ bool V4L2VideoDecodeAccelerator::DecodeBufferInitial(const void* data,
     return false;
 
   // If we only have a partial frame, don't flush and process yet.
-  if (decoder_partial_frame_pending_)
+  if (frame_splitter_->IsPartialFramePending())
     return true;
 
   if (!FlushInputFrame())
@@ -1143,7 +1054,7 @@ bool V4L2VideoDecodeAccelerator::DecodeBufferContinue(const void* data,
   // Both of these calls will set kError state if they fail.
   // Only flush the frame if it's complete.
   return (AppendToInputFrame(data, size) &&
-          (decoder_partial_frame_pending_ || FlushInputFrame()));
+          (frame_splitter_->IsPartialFramePending() || FlushInputFrame()));
 }
 
 bool V4L2VideoDecodeAccelerator::AppendToInputFrame(const void* data,
@@ -1897,16 +1808,12 @@ void V4L2VideoDecodeAccelerator::ResetDoneTask() {
       return;
   }
 
-  // Reset format-specific bits.
-  if (video_profile_ >= H264PROFILE_MIN && video_profile_ <= H264PROFILE_MAX) {
-    decoder_h264_parser_.reset(new H264Parser());
-  }
+  frame_splitter_->Reset();
 
   // Jobs drained, we're finished resetting.
   DCHECK_EQ(decoder_state_, kResetting);
   decoder_state_ = kInitialized;
 
-  decoder_partial_frame_pending_ = false;
   decoder_delay_bitstream_buffer_id_ = -1;
   child_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&Client::NotifyResetDone, client_));
@@ -1951,7 +1858,7 @@ void V4L2VideoDecodeAccelerator::DestroyTask() {
   input_queue_ = nullptr;
   output_queue_ = nullptr;
 
-  decoder_h264_parser_ = nullptr;
+  frame_splitter_ = nullptr;
   workarounds_.clear();
 
   // Clear the V4L2 devices in the decoder thread so the V4L2Device's
