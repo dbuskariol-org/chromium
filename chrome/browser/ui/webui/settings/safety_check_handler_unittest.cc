42f9cdd402961b4ed56304f9b5725ca7b6d68fb0
Rainhard Findling <rainhard@chromium.org>
2020-04-03 16:23:59 +0000

    Safety check UI: reuse password check strings
    * Safety check password check reuses the newly introducted strings
      for password check, instead of introducing separate strings.
    
    Bug: 1015841
    Change-Id: I7ad111f1ef7b57b8e5df1073e8c84221e60f5be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132455
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756293}

--- chrome/browser/ui/webui/settings/safety_check_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/safety_check_handler_unittest.cc
@@ -615,9 +615,9 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_InterruptedAndRefreshed) {
           kPasswords,
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kSignedOut));
   ASSERT_TRUE(event3);
-  VerifyDisplayString(
-      event3,
-      "Browser can't check your passwords because you're not signed in");
+  VerifyDisplayString(event3,
+                      "Browser can't check your passwords because you're not "
+                      "signed in");
   histogram_tester_.ExpectBucketCount(
       "Settings.SafetyCheck.PasswordsResult",
       SafetyCheckHandler::PasswordsStatus::kSignedOut, 1);
@@ -730,8 +730,7 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_Error) {
           kPasswords,
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kError));
   ASSERT_TRUE(event);
-  VerifyDisplayString(event,
-                      "Browser can't check your passwords. Try again later.");
+  VerifyDisplayString(event, "Browser can't check your passwords.");
   histogram_tester_.ExpectBucketCount(
       "Settings.SafetyCheck.PasswordsResult",
       SafetyCheckHandler::PasswordsStatus::kError, 1);
@@ -767,7 +766,9 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_NoPasswords) {
           kPasswords,
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kNoPasswords));
   EXPECT_TRUE(event);
-  VerifyDisplayString(event, "No saved passwords");
+  VerifyDisplayString(event,
+                      "No saved passwords. Chrome can check your passwords "
+                      "when you save them.");
   histogram_tester_.ExpectBucketCount(
       "Settings.SafetyCheck.PasswordsResult",
       SafetyCheckHandler::PasswordsStatus::kNoPasswords, 1);
@@ -789,7 +790,7 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_Progress) {
           kPasswords,
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kChecking));
   EXPECT_TRUE(event);
-  VerifyDisplayString(event, base::UTF8ToUTF16("1/3 passwords checked…"));
+  VerifyDisplayString(event, base::UTF8ToUTF16("Checking passwords (1 of 3)…"));
 
   test_passwords_delegate_.SetProgress(2, 3);
   static_cast<password_manager::BulkLeakCheckService::Observer*>(
@@ -800,7 +801,8 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_Progress) {
           kPasswords,
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kChecking));
   EXPECT_TRUE(event2);
-  VerifyDisplayString(event2, base::UTF8ToUTF16("2/3 passwords checked…"));
+  VerifyDisplayString(event2,
+                      base::UTF8ToUTF16("Checking passwords (2 of 3)…"));
 
   // Final update comes after status change, so no new progress message should
   // be present.
@@ -816,7 +818,8 @@ TEST_F(SafetyCheckHandlerTest, CheckPasswords_Progress) {
           static_cast<int>(SafetyCheckHandler::PasswordsStatus::kChecking));
   EXPECT_TRUE(event3);
   // Still 2/3 event.
-  VerifyDisplayString(event3, base::UTF8ToUTF16("2/3 passwords checked…"));
+  VerifyDisplayString(event3,
+                      base::UTF8ToUTF16("Checking passwords (2 of 3)…"));
 }
 
 TEST_F(SafetyCheckHandlerTest, CheckExtensions_NoExtensions) {
