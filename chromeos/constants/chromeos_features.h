074aa44bff9255d51eec53c7907265f0e60b9465
Joseph Kim <josephkimsh@google.com>
2020-04-03 12:26:01 +0000

    Demo Mode: Display descriptive progress during Demo setup
    This work builds upon 2086358: Demo Mode: Display progress bar rather
    than spinner during Demo setup
    https://chromium-review.googlesource.com/c/chromium/src/+/2086358
    
    Now Demo Mode setup progress is displayed to the user as a list of
    steps with individual progress indicators (not begun, in progress,
    complete). This is hidden behind the `ShowStepsInDemoModeSetup`
    flag, disabled by default.
    
    Bug: b:148753805
    Change-Id: I5bdfe643e80f291755df64e28d671f5fc7069e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114014
    Commit-Queue: Joseph Kim <josephkimsh@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756229}

--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -136,7 +136,7 @@ extern const base::Feature kShowBluetoothDeviceBattery;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kShowPlayInDemoMode;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
-extern const base::Feature kShowProgressBarInDemoModeSetup;
+extern const base::Feature kShowStepsInDemoModeSetup;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kSmartDimExperimentalComponent;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
