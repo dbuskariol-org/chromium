415c6850d08299a07b0b472e81d296857f4cc6cb
rbpotter <rbpotter@chromium.org>
2020-04-07 18:42:44 +0000

    Settings: clang-format settings.js and lazy_load.js
    Bug: 1026426
    Change-Id: I8bfa01f305890554249cf444ceda3e7e8b30de77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2136281
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757137}

--- chrome/browser/resources/settings/lazy_load.js
+++ chrome/browser/resources/settings/lazy_load.js
@@ -24,7 +24,6 @@ import './site_settings/protocol_handlers.m.js';
 import './site_settings/site_data.m.js';
 import './site_settings/site_details.m.js';
 import './site_settings/zoom_levels.m.js';
-
 // <if expr="not chromeos">
 import './people_page/import_data_dialog.m.js';
 import './people_page/manage_profile.m.js';
@@ -32,7 +31,6 @@ import './people_page/manage_profile.m.js';
 import './people_page/signout_dialog.m.js';
 import './people_page/sync_controls.m.js';
 import './people_page/sync_page.m.js';
-
 // <if expr="use_nss_certs">
 import 'chrome://resources/cr_components/certificate_manager/certificate_manager.m.js';
 // </if>
@@ -43,7 +41,6 @@ import './downloads_page/downloads_page.m.js';
 import './languages_page/languages_page.m.js';
 import './printing_page/printing_page.m.js';
 import './reset_page/reset_page.m.js';
-
 // <if expr="not chromeos">
 import './system_page/system_page.m.js';
 // </if>
@@ -52,44 +49,46 @@ import './system_page/system_page.m.js';
 import './languages_page/edit_dictionary_page.m.js';
 // </if>
 
-// <if expr="_google_chrome and is_win">
-export {CHROME_CLEANUP_DEFAULT_ITEMS_TO_SHOW} from './chrome_cleanup_page/items_to_remove_list.m.js';
-export {ChromeCleanupIdleReason} from './chrome_cleanup_page/chrome_cleanup_page.m.js';
-export {ChromeCleanupProxyImpl} from './chrome_cleanup_page/chrome_cleanup_proxy.m.js';
-export {IncompatibleApplication, IncompatibleApplicationsBrowserProxyImpl} from './incompatible_applications_page/incompatible_applications_browser_proxy.m.js';
-// </if>
-
 export {getToastManager} from 'chrome://resources/cr_elements/cr_toast/cr_toast_manager.m.js';
-
 export {FontsBrowserProxy, FontsBrowserProxyImpl} from './appearance_page/fonts_browser_proxy.m.js';
 export {CountryDetailManagerImpl} from './autofill_page/address_edit_dialog.m.js';
 export {AutofillManagerImpl} from './autofill_page/autofill_section.m.js';
+// <if expr="chromeos">
+export {BlockingRequestManager} from './autofill_page/blocking_request_manager.m.js';
+// </if>
 export {PaymentsManagerImpl} from './autofill_page/payments_section.m.js';
+// <if expr="_google_chrome and is_win">
+export {ChromeCleanupIdleReason} from './chrome_cleanup_page/chrome_cleanup_page.m.js';
+export {ChromeCleanupProxyImpl} from './chrome_cleanup_page/chrome_cleanup_proxy.m.js';
+export {CHROME_CLEANUP_DEFAULT_ITEMS_TO_SHOW} from './chrome_cleanup_page/items_to_remove_list.m.js';
+// </if>
 export {ClearBrowsingDataBrowserProxyImpl} from './clear_browsing_data_dialog/clear_browsing_data_browser_proxy.m.js';
 export {DownloadsBrowserProxyImpl} from './downloads_page/downloads_browser_proxy.m.js';
-export {kMenuCloseDelay} from './languages_page/languages_page.m.js';
+// <if expr="_google_chrome and is_win">
+export {IncompatibleApplication, IncompatibleApplicationsBrowserProxyImpl} from './incompatible_applications_page/incompatible_applications_browser_proxy.m.js';
+// </if>
 export {LanguagesBrowserProxyImpl} from './languages_page/languages_browser_proxy.m.js';
-export {ChooserType,ContentSetting,ContentSettingsTypes,CookieControlsMode,SiteSettingSource,SITE_EXCEPTION_WILDCARD,SortMethod} from './site_settings/constants.m.js';
-export {cookieInfo} from './site_settings/cookie_info.m.js';
-export {kControlledByLookup} from './site_settings/site_settings_behavior.m.js';
-export {LocalDataBrowserProxyImpl} from './site_settings/local_data_browser_proxy.m.js';
-export {ContentSettingProvider,SiteSettingsPrefsBrowserProxyImpl} from './site_settings/site_settings_prefs_browser_proxy.m.js';
-export {defaultSettingLabel} from './site_settings_page/site_settings_list.m.js';
+export {kMenuCloseDelay} from './languages_page/languages_page.m.js';
+// <if expr="not chromeos">
+export {ImportDataBrowserProxyImpl, ImportDataStatus} from './people_page/import_data_browser_proxy.m.js';
+export {ManageProfileBrowserProxyImpl, ProfileShortcutStatus} from './people_page/manage_profile_browser_proxy.m.js';
+// </if>
 export {SafeBrowsingBrowserProxyImpl} from './privacy_page/safe_browsing_browser_proxy.m.js';
-export {SecurityKeysResetBrowserProxyImpl, SecurityKeysPINBrowserProxyImpl, SecurityKeysCredentialBrowserProxyImpl, SecurityKeysBioEnrollProxyImpl, SampleStatus, Ctap2Status} from './privacy_page/security_keys_browser_proxy.m.js';
+export {BioEnrollDialogPage} from './privacy_page/security_keys_bio_enroll_dialog.m.js';
+export {Ctap2Status, SampleStatus, SecurityKeysBioEnrollProxyImpl, SecurityKeysCredentialBrowserProxyImpl, SecurityKeysPINBrowserProxyImpl, SecurityKeysResetBrowserProxyImpl} from './privacy_page/security_keys_browser_proxy.m.js';
+export {CredentialManagementDialogPage} from './privacy_page/security_keys_credential_management_dialog.m.js';
 export {ResetDialogPage} from './privacy_page/security_keys_reset_dialog.m.js';
 export {SetPINDialogPage} from './privacy_page/security_keys_set_pin_dialog.m.js';
-export {CredentialManagementDialogPage} from './privacy_page/security_keys_credential_management_dialog.m.js';
-export {BioEnrollDialogPage} from './privacy_page/security_keys_bio_enroll_dialog.m.js';
-export {WebsiteUsageBrowserProxyImpl} from './site_settings/website_usage_browser_proxy.m.js';
-
-// <if expr="not chromeos">
-export {SystemPageBrowserProxyImpl} from './system_page/system_page_browser_proxy.m.js';
-export {ImportDataBrowserProxyImpl, ImportDataStatus} from './people_page/import_data_browser_proxy.m.js';
-export {ProfileShortcutStatus, ManageProfileBrowserProxyImpl} from './people_page/manage_profile_browser_proxy.m.js';
-// </if>
-
 // <if expr="chromeos">
 export {AndroidInfoBrowserProxyImpl} from './site_settings/android_info_browser_proxy.m.js';
-export {BlockingRequestManager} from './autofill_page/blocking_request_manager.m.js';
+// </if>
+export {ChooserType, ContentSetting, ContentSettingsTypes, CookieControlsMode, SITE_EXCEPTION_WILDCARD, SiteSettingSource, SortMethod} from './site_settings/constants.m.js';
+export {cookieInfo} from './site_settings/cookie_info.m.js';
+export {LocalDataBrowserProxyImpl} from './site_settings/local_data_browser_proxy.m.js';
+export {kControlledByLookup} from './site_settings/site_settings_behavior.m.js';
+export {ContentSettingProvider, SiteSettingsPrefsBrowserProxyImpl} from './site_settings/site_settings_prefs_browser_proxy.m.js';
+export {WebsiteUsageBrowserProxyImpl} from './site_settings/website_usage_browser_proxy.m.js';
+export {defaultSettingLabel} from './site_settings_page/site_settings_list.m.js';
+// <if expr="not chromeos">
+export {SystemPageBrowserProxyImpl} from './system_page/system_page_browser_proxy.m.js';
 // </if>
--- chrome/browser/resources/settings/settings.js
+++ chrome/browser/resources/settings/settings.js
@@ -4,20 +4,13 @@
 
 import './settings_ui/settings_ui.m.js';
 
-// <if expr="not chromeos">
-export {DefaultBrowserBrowserProxyImpl} from './default_browser_page/default_browser_browser_proxy.m.js';
-export {ImportDataBrowserProxyImpl, ImportDataStatus} from './people_page/import_data_browser_proxy.m.js';
-// </if>
-
-// <if expr="chromeos">
-export {AccountManagerBrowserProxyImpl} from './people_page/account_manager_browser_proxy.m.js';
-// </if>
-
 export {AboutPageBrowserProxy, AboutPageBrowserProxyImpl, UpdateStatus} from './about_page/about_page_browser_proxy.m.js';
 export {AppearanceBrowserProxy, AppearanceBrowserProxyImpl} from './appearance_page/appearance_browser_proxy.m.js';
-export {CrSettingsPrefs} from './prefs/prefs_types.m.js';
+export {PasswordManagerImpl, PasswordManagerProxy} from './autofill_page/password_manager_proxy.m.js';
+// <if expr="not chromeos">
+export {DefaultBrowserBrowserProxyImpl} from './default_browser_page/default_browser_browser_proxy.m.js';
+// </if>
 export {ExtensionControlBrowserProxyImpl} from './extension_control_browser_proxy.m.js';
-export {getSearchManager, SearchRequest, setSearchManagerForTesting} from './search_settings.m.js';
 export {HatsBrowserProxyImpl} from './hats_browser_proxy.m.js';
 export {LifetimeBrowserProxyImpl} from './lifetime_browser_proxy.m.js';
 export {MetricsBrowserProxyImpl, PrivacyElementInteractions, SafetyCheckInteractions} from './metrics_browser_proxy.m.js';
@@ -25,16 +18,23 @@ export {OnStartupBrowserProxy, OnStartupBrowserProxyImpl} from './on_startup_pag
 export {EDIT_STARTUP_URL_EVENT} from './on_startup_page/startup_url_entry.m.js';
 export {StartupUrlsPageBrowserProxy, StartupUrlsPageBrowserProxyImpl} from './on_startup_page/startup_urls_page_browser_proxy.m.js';
 export {OpenWindowProxyImpl} from './open_window_proxy.m.js';
+export {pageVisibility, setPageVisibilityForTesting} from './page_visibility.m.js';
+// <if expr="chromeos">
+export {AccountManagerBrowserProxyImpl} from './people_page/account_manager_browser_proxy.m.js';
+// </if>
+// <if expr="not chromeos">
+export {ImportDataBrowserProxyImpl, ImportDataStatus} from './people_page/import_data_browser_proxy.m.js';
+// </if>
+export {ProfileInfoBrowserProxyImpl} from './people_page/profile_info_browser_proxy.m.js';
 export {MAX_SIGNIN_PROMO_IMPRESSION} from './people_page/sync_account_control.m.js';
 export {PageStatus, StatusAction, SyncBrowserProxyImpl} from './people_page/sync_browser_proxy.m.js';
-export {ProfileInfoBrowserProxyImpl} from './people_page/profile_info_browser_proxy.m.js';
-export {pageVisibility, setPageVisibilityForTesting} from './page_visibility.m.js';
-export {PasswordManagerProxy, PasswordManagerImpl} from './autofill_page/password_manager_proxy.m.js';
 export {PluralStringProxyImpl} from './plural_string_proxy.m.js';
 export {prefToString, stringToPrefValue} from './prefs/pref_util.m.js';
+export {CrSettingsPrefs} from './prefs/prefs_types.m.js';
+export {PrivacyPageBrowserProxyImpl, SecureDnsMode, SecureDnsUiManagementMode} from './privacy_page/privacy_page_browser_proxy.m.js';
 export {ResetBrowserProxyImpl} from './reset_page/reset_browser_proxy.m.js';
 export {buildRouter, routes} from './route.m.js';
 export {Route, Router} from './router.m.js';
+export {SafetyCheckBrowserProxy, SafetyCheckBrowserProxyImpl, SafetyCheckCallbackConstants, SafetyCheckExtensionsStatus, SafetyCheckPasswordsStatus, SafetyCheckSafeBrowsingStatus, SafetyCheckUpdatesStatus} from './safety_check_page/safety_check_browser_proxy.m.js';
 export {SearchEnginesBrowserProxyImpl} from './search_engines_page/search_engines_browser_proxy.m.js';
-export {PrivacyPageBrowserProxyImpl, SecureDnsMode, SecureDnsUiManagementMode} from './privacy_page/privacy_page_browser_proxy.m.js';
-export {SafetyCheckBrowserProxy, SafetyCheckBrowserProxyImpl, SafetyCheckCallbackConstants, SafetyCheckExtensionsStatus, SafetyCheckSafeBrowsingStatus, SafetyCheckPasswordsStatus, SafetyCheckUpdatesStatus} from './safety_check_page/safety_check_browser_proxy.m.js';
+export {getSearchManager, SearchRequest, setSearchManagerForTesting} from './search_settings.m.js';
