5980069b13bf00812578aa2cf3f34015b90b2ceb
Darwin Huang <huangdarwin@chromium.org>
2020-04-07 21:36:51 +0000

    Revert "Ensure cookie_controls_mode is set with block_third_party_cookies"
    This reverts commit 21274196ecebc62c81de590d4d5925a88cd4f9fa.
    
    Reason for revert: Mac10.13 Tests trybot failures on CrSettingsPrivacyPageV3Test.PrivacyPageTests for all of the last 3 runs. (all runs since the test was updated in this CL)
    
    Trybot runs: https://ci.chromium.org/p/chromium/builders/ci/Mac10.13%20Tests%20%28dbg%29
    First failed run: https://ci.chromium.org/p/chromium/builders/ci/Mac10.13%20Tests%20%28dbg%29/17846
    
    Original change's description:
    > Ensure cookie_controls_mode is set with block_third_party_cookies
    > 
    > To ensure that the two related preferences, cookie_controls_mode and
    > block_third_party_cookies remain in a consistent state, this CL updates
    > the existing block_third_party_cookies toggle located in settings
    > to update both preferences.
    > 
    > Bug: 1068144
    > Change-Id: Ia64cb37c766a3143c6ac986efc67e87283852a8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134261
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    > Cr-Commit-Position: refs/heads/master@{#757019}
    
    TBR=dpapad@chromium.org,dullweber@chromium.org,sauski@google.com
    
    Change-Id: I207144aed2d417f7531676db12ad9c6fc7e8e992
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1068144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140771
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757213}

--- chrome/test/data/webui/settings/privacy_page_test.js
+++ chrome/test/data/webui/settings/privacy_page_test.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 // clang-format off
-// #import {CookieControlsMode, SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
+// #import {SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {MetricsBrowserProxyImpl, PrivacyElementInteractions, PrivacyPageBrowserProxyImpl, SyncBrowserProxyImpl, HatsBrowserProxyImpl, Router, routes} from 'chrome://settings/settings.js';
 // #import {TestMetricsBrowserProxy} from 'chrome://test/settings/test_metrics_browser_proxy.m.js';
@@ -225,30 +225,6 @@ suite('PrivacyPage', function() {
       assertTrue(
           test_util.isChildVisible(page, '#security-keys-subpage-trigger'));
     }
-  });
-
-  test('BlockThirdPartyCookiesToggle', async function() {
-    page.prefs.profile.block_third_party_cookies = {value: false};
-    page.prefs.profile.cookie_controls_mode = {
-      value: settings.CookieControlsMode.DISABLED
-    };
-    settings.Router.getInstance().navigateTo(
-        settings.routes.SITE_SETTINGS_COOKIES);
-    Polymer.dom.flush();
-
-    page.$$('#blockThirdPartyCookies').click();
-    Polymer.dom.flush();
-    assertTrue(page.prefs.profile.block_third_party_cookies.value);
-    assertEquals(
-        page.prefs.profile.cookie_controls_mode.value,
-        settings.CookieControlsMode.ENABLED);
-
-    page.$$('#blockThirdPartyCookies').click();
-    Polymer.dom.flush();
-    assertFalse(page.prefs.profile.block_third_party_cookies.value);
-    assertEquals(
-        page.prefs.profile.cookie_controls_mode.value,
-        settings.CookieControlsMode.DISABLED);
   });
 });
 
