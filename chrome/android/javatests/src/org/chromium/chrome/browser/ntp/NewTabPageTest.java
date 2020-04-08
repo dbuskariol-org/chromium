309e1e97b0b412a04d4ac8c7133adaa1a9aaed05
Natalie Chouinard <chouinard@chromium.org>
2020-04-08 23:04:24 +0000

    Re-enable NewTabPageTest#testRender_FocusFakeBox
    Enable flaky test to gather failure images. Unable to repro locally, and
    insufficient information on bug for speculative fixes yet.
    
    Bug: 888129
    Change-Id: I438daec8bbb9faa4d098fc83016e96f3db1792d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2136272
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757630}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -214,7 +214,6 @@ public class NewTabPageTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/888129")
     @SmallTest
     @Feature({"NewTabPage", "FeedNewTabPage", "RenderTest"})
     @ParameterAnnotations.UseMethodParameter(InterestFeedParams.class)
