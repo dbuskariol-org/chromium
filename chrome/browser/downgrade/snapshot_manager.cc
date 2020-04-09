0c6bc2e3ed1c9513ac6de4827353cf96d9c73d44
Yann Dago <ydago@chromium.org>
2020-04-09 14:38:42 +0000

    Take user data snapshots on canary builds for each minor upgrade.
    Bug: 958893
    Change-Id: I987a99d3db2b659babe46d2f15cfac6627bc573c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134396
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757835}

--- chrome/browser/downgrade/snapshot_manager.cc
+++ chrome/browser/downgrade/snapshot_manager.cc
@@ -313,7 +313,8 @@ void SnapshotManager::RestoreSnapshot(const base::Version& version) {
 }
 
 void SnapshotManager::PurgeInvalidAndOldSnapshots(
-    int max_number_of_snapshots) const {
+    int max_number_of_snapshots,
+    base::Optional<uint32_t> milestone) const {
   const auto snapshot_dir = user_data_dir_.Append(kSnapshotsDir);
 
   // Move the invalid snapshots within from Snapshots/NN to Snapshots.DELETE/NN.
@@ -329,7 +330,17 @@ void SnapshotManager::PurgeInvalidAndOldSnapshots(
                                "Downgrade.InvalidSnapshotMove.FailureCount");
   }
 
-  auto available_snapshots = GetAvailableSnapshots(snapshot_dir);
+  base::flat_set<base::Version> available_snapshots =
+      GetAvailableSnapshots(snapshot_dir);
+  if (milestone.has_value()) {
+    // Only consider versions for the specified milestone.
+    available_snapshots.erase(available_snapshots.upper_bound(
+                                  base::Version({*milestone + 1, 0, 0, 0})),
+                              available_snapshots.end());
+    available_snapshots.erase(
+        available_snapshots.begin(),
+        available_snapshots.lower_bound(base::Version({*milestone, 0, 0, 0})));
+  }
 
   if (available_snapshots.size() <=
       base::checked_cast<size_t>(max_number_of_snapshots)) {
