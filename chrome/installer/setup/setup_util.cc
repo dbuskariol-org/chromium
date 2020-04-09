244fcc4dc4e35b9f544c8ea5d5a9341a46cbf36e
Greg Thompson <grt@chromium.org>
2020-04-09 09:30:16 +0000

    Reland: Remove multi-install remnants
    The multi-install support was deactivated in 2017 but there were still
    many remnants remaining in the Chromium code base.  These remnants have
    been a source of friction for changes being made in the install and
    update infrastructure.
    
    Some additional house-keeping is included by reviewer request.
    
    TBR=rsesek@chromium.org,jxiang@google.com
    NOTRY=true
    
    Bug: 1067489
    Change-Id: I132f3146db2f465226d1b793c8fd2f8c4e66d9f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143811
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757778}

--- chrome/installer/setup/setup_util.cc
+++ chrome/installer/setup/setup_util.cc
@@ -139,7 +139,13 @@ void RemoveProfileStatistics(const InstallerState& installer_state) {
 // for this mode of install was dropped from ToT in December 2016. Remove any
 // stray bits in the registry leftover from such installs.
 void RemoveBinariesVersionKey(const InstallerState& installer_state) {
-  base::string16 path(install_static::GetBinariesClientsKeyPath());
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  base::string16 path(install_static::GetClientsKeyPath(
+      L"{4DC8B4CA-1BDA-483e-B5FA-D3C12E15B62D}"));
+#else
+  // Assume that non-Google is Chromium branding.
+  base::string16 path(L"Software\\Chromium Binaries");
+#endif
   if (base::win::RegKey(installer_state.root_key(), path.c_str(),
                         KEY_QUERY_VALUE | KEY_WOW64_32KEY)
           .Valid()) {
@@ -677,21 +683,6 @@ void DeRegisterEventLogProvider() {
   // but leaves files behind.
   InstallUtil::DeleteRegistryKey(HKEY_LOCAL_MACHINE, reg_path,
                                  WorkItem::kWow64Default);
-}
-
-bool AreBinariesInstalled(const InstallerState& installer_state) {
-  if (!install_static::InstallDetails::Get().supported_multi_install())
-    return false;
-
-  base::win::RegKey key;
-  base::string16 pv;
-
-  // True if the "pv" value exists and isn't empty.
-  return key.Open(installer_state.root_key(),
-                  install_static::GetBinariesClientsKeyPath().c_str(),
-                  KEY_QUERY_VALUE | KEY_WOW64_32KEY) == ERROR_SUCCESS &&
-         key.ReadValue(google_update::kRegVersionField, &pv) == ERROR_SUCCESS &&
-         !pv.empty();
 }
 
 void DoLegacyCleanups(const InstallerState& installer_state,
