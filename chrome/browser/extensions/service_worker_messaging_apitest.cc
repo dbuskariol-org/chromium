f4a731aae382727e3bb50195e09df34ae8944648
Reilly Grant <reillyg@chromium.org>
2020-04-09 19:39:35 +0000

    Disable ServiceWorkerMessagingTest.TabToWorker_StopWorkerDisconnects
    This test is flaky.
    
    Bug: 1069550
    Change-Id: I45b2260a996ee7611b3d5fe250dc20d1e7170328
    Tbr: lazyboy@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143428
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757965}

--- chrome/browser/extensions/service_worker_messaging_apitest.cc
+++ chrome/browser/extensions/service_worker_messaging_apitest.cc
@@ -103,8 +103,9 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerMessagingTest, TabToWorker) {
 // Tests that a message port disconnects if the extension SW is forcefully
 // stopped.
 // Regression test for https://crbug.com/1033783.
+// Disabled due to flake: https://crbug.com/1069550
 IN_PROC_BROWSER_TEST_F(ServiceWorkerMessagingTest,
-                       TabToWorker_StopWorkerDisconnects) {
+                       DISABLED_TabToWorker_StopWorkerDisconnects) {
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   TestExtensionDir test_dir;
