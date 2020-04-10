ab7bfda3b2c17e605c12b84c1c2d06270e97f822
Esmael El-Moslimany <aee@chromium.org>
2020-04-10 21:28:26 +0000

    WebUI NTP: make ntp-mini-page reusable
    Bug: 1032328
    Change-Id: Id2c69183598d01f08f8fd20f4134b0c3cba863ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145599
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758297}

--- chrome/browser/resources/new_tab_page/customize_backgrounds.js
+++ chrome/browser/resources/new_tab_page/customize_backgrounds.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import './grid.js';
+import './mini_page.js';
 import './untrusted_iframe.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
