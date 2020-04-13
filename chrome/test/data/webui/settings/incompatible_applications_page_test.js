dff42f85ff4d9b0117847fd47af151569b7ffbe0
dpapad <dpapad@chromium.org>
2020-04-13 21:16:57 +0000

    Settings: Fix CrSettingsIncompatibleApplicationsPageV3Test.All failure.
    Fixed: 1064234
    Change-Id: I608d1b2a0b824bde5c430bc85df3323dd42ece49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147848
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758607}

--- chrome/test/data/webui/settings/incompatible_applications_page_test.js
+++ chrome/test/data/webui/settings/incompatible_applications_page_test.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 // clang-format off
-// #import {IncompatibleApplicationsBrowserProxyImpl, IncompatibleApplication} from 'chrome://settings/settings.js';
+// #import {IncompatibleApplicationsBrowserProxyImpl, IncompatibleApplication} from 'chrome://settings/lazy_load.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {TestBrowserProxy} from 'chrome://test/test_browser_proxy.m.js';
 // #import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
