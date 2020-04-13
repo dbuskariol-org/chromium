588d178119f417cd15b38d3206eb12cdc3388a42
Ayu Ishii <ayui@chromium.org>
2020-04-13 20:20:38 +0000

    WebOTP: Use WeakPtr to hold onto RenderFrameHost
    (cherry picked from commit 4105618e07984c3a138ffea8b64a53b5f0228f81)
    
    Change-Id: Id9ab078652ae626170b51fb5e6697808de67f34a
    Bug: 1068395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141114
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#757875}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147863
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4103@{#106}
    Cr-Branched-From: 8ad47e8d21f6866e4a37f47d83a860d41debf514-refs/heads/master@{#756066}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -6938,7 +6938,8 @@ void RenderFrameHostImpl::BindSmsReceiverReceiver(
     mojo::ReportBadMessage("Must have the same origin as the top-level frame.");
     return;
   }
-  auto* fetcher = SmsFetcher::Get(GetProcess()->GetBrowserContext(), this);
+  auto* fetcher = SmsFetcher::Get(GetProcess()->GetBrowserContext(),
+                                  weak_ptr_factory_.GetWeakPtr());
   SmsService::Create(fetcher, this, std::move(receiver));
 }
 
