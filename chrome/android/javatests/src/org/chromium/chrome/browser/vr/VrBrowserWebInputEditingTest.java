ec77774ac52df9f1a0c47c7b9ed07899ead605f7
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-04-15 21:17:31 +0000

    Remove -webkit-center from mediaControls.css for immersive-mode.
    "text-align: -webkit-center" is a non-standard css property/value pair
    which has undefined behaviour.
    
    This was being used for centering the video controls within the video
    player when in the "immersive-mode".
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1977196
    ...changed how the blink's layout codebase handles margins, removing a
    bug for when "-webkit-center" is set on a child.
    
    This patch changes the immersive-mode stylesheet to use:
    "align-items: center" for centering the controls in their container.
    
    It was also necessary to specify the width of the "timeline" to ensure
    it filled the available space.
    
    This also re-enables the disabled test.
    
    Bug: 1041904
    Change-Id: I6ca530a14185fa0bba8900824cef3b369728e90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144777
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759418}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserWebInputEditingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserWebInputEditingTest.java
@@ -24,7 +24,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BundleTestRule;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -438,7 +437,6 @@ public class VrBrowserWebInputEditingTest {
     @Test
     @MediumTest
     @Feature({"Browser", "RenderTest"})
-    @DisabledTest(message = "https://crbug.com/1041904")
     public void testFullscreenVideoControls()
             throws InterruptedException, TimeoutException, IOException {
         // There's occasionally slight AA differences along the play button, so tolerate a small
