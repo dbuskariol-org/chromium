f5f18cd49c3a5c5aeac43b518db9b2386434e63f
Akihiro Ota <akihiroota@chromium.org>
2020-04-16 23:20:00 +0000

    ChromeVox: load all available phonetic dictionaries at build time.
    This change adds a build action that decompresses all localized message
    files and extracts their phonetic dictionaries. These are then written
    to a generated .js file and loaded into ChromeVox.
    
    The getPhoneticDisambiguation API checks the generated contents
    when doing lookups and a TtsBackground test has been added to confirm
    this behavior.
    
    Tangible effect: phonetic hints are given when moving by character
    when language switching is active.
    
    Bug: 923068
    
    Relnotes: N/A
    Change-Id: Ia10d2f2344fe543f65279bbbfb54f4c72f5ad8da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144405
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759849}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/phonetic_data.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/phonetic_data.js
@@ -13,57 +13,42 @@ goog.provide('PhoneticData');
 goog.require('JaPhoneticData');
 
 /**
- * Maps languages to their phonetic maps.
- * @type {Object<string,Object<string,string>>}
- * @private
- */
-PhoneticData.phoneticMap_ = {};
-
-/**
  * Initialization function for PhoneticData.
  */
 PhoneticData.init = function() {
   JaPhoneticData.init();
-  try {
-    // The UI language of the browser. This corresponds to the system language
-    // set by the user. Behind the scenes, the getUIlanguage() API retrieves the
-    // locale that was passed from the browser to the renderer via the --lang
-    // command line flag.
-    const browserUILanguage = chrome.i18n.getUILanguage().toLowerCase();
-    // Phonetic disambiguation data for the browserUI language.
-    // This is loaded from a chromevox_strings_*.xtb file, where * is a variable
-    // language code that corresponds to the system language.
-    const browserUILanguagePhoneticMap = /** @type {Object<string,string>} */
-        (JSON.parse(Msgs.getMsg('phonetic_map')));
-    PhoneticData.phoneticMap_[browserUILanguage] = browserUILanguagePhoneticMap;
-  } catch (e) {
-    console.log('Error: unable to parse phonetic map message.');
-  }
-  PhoneticData.phoneticMap_['ja'] = JaPhoneticData.phoneticMap_;
 };
 
 /**
- * Returns the phonetic disambiguation for the provided character in the
- * provided language. Returns empty string if disambiguation can't be found.
- * @param {string} language
+ * Returns the phonetic disambiguation for |character| in |locale|.
+ * Returns empty string if disambiguation can't be found.
+ * @param {string} locale
  * @param {string} character
  * @return {string}
  */
-PhoneticData.getPhoneticDisambiguation = function(language, character) {
-  if (!language || !character) {
+PhoneticData.getPhoneticDisambiguation = function(locale, character) {
+  const phoneticDictionaries =
+      chrome.extension.getBackgroundPage().PhoneticDictionaries;
+  if (!locale || !character || !phoneticDictionaries ||
+      !phoneticDictionaries.phoneticMap_) {
     return '';
   }
-  language = language.toLowerCase();
+
+  locale = locale.toLowerCase();
   character = character.toLowerCase();
-  // If language isn't in the map, try stripping extra information, such as the
-  // country and/or script codes (e.g. "en-us" or "zh-hant-hk") and use only the
-  // language code to do a lookup.
-  if (!PhoneticData.phoneticMap_[language]) {
-    language = language.split('-')[0];
+  let map = null;
+  if (locale === 'ja') {
+    map = JaPhoneticData.phoneticMap_;
+  } else {
+    // Try a lookup using |locale|, but use only the language component if the
+    // lookup fails, e.g. "en-us" -> "en" or "zh-hant-hk" -> "zh".
+    map = phoneticDictionaries.phoneticMap_[locale] ||
+        phoneticDictionaries.phoneticMap_[locale.split('-')[0]];
   }
-  // If language still isn't in the map, return empty string.
-  if (!PhoneticData.phoneticMap_[language]) {
+
+  if (!map) {
     return '';
   }
-  return PhoneticData.phoneticMap_[language][character] || '';
+
+  return map[character] || '';
 };
