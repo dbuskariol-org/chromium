92956837050c30efa53acef4b63b341bd108ba27
Allen Bauer <kylixrd@chromium.org>
2020-04-17 17:35:39 +0000

    Move examples creations to separate module.
    Change-Id: I778aded413a596176c3a2b164856db6b873caf1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149952
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760123}

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -18,35 +18,9 @@
 #include "ui/base/ui_base_paths.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/combobox/combobox.h"
+#include "ui/views/controls/combobox/combobox_listener.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/examples/ax_example.h"
-#include "ui/views/examples/box_layout_example.h"
-#include "ui/views/examples/bubble_example.h"
-#include "ui/views/examples/button_example.h"
-#include "ui/views/examples/button_sticker_sheet.h"
-#include "ui/views/examples/checkbox_example.h"
-#include "ui/views/examples/combobox_example.h"
-#include "ui/views/examples/dialog_example.h"
-#include "ui/views/examples/flex_layout_example.h"
-#include "ui/views/examples/label_example.h"
-#include "ui/views/examples/link_example.h"
-#include "ui/views/examples/menu_example.h"
-#include "ui/views/examples/message_box_example.h"
-#include "ui/views/examples/multiline_example.h"
-#include "ui/views/examples/native_theme_example.h"
-#include "ui/views/examples/progress_bar_example.h"
-#include "ui/views/examples/radio_button_example.h"
-#include "ui/views/examples/scroll_view_example.h"
-#include "ui/views/examples/slider_example.h"
-#include "ui/views/examples/tabbed_pane_example.h"
-#include "ui/views/examples/table_example.h"
-#include "ui/views/examples/text_example.h"
-#include "ui/views/examples/textfield_example.h"
-#include "ui/views/examples/throbber_example.h"
-#include "ui/views/examples/toggle_button_example.h"
-#include "ui/views/examples/tree_view_example.h"
-#include "ui/views/examples/vector_example.h"
-#include "ui/views/examples/widget_example.h"
+#include "ui/views/examples/create_examples.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
@@ -58,40 +32,6 @@ namespace examples {
 using ExampleVector = std::vector<std::unique_ptr<ExampleBase>>;
 
 namespace {
-
-// Creates the default set of examples.
-ExampleVector CreateExamples() {
-  ExampleVector examples;
-  examples.push_back(std::make_unique<AxExample>());
-  examples.push_back(std::make_unique<BoxLayoutExample>());
-  examples.push_back(std::make_unique<BubbleExample>());
-  examples.push_back(std::make_unique<ButtonExample>());
-  examples.push_back(std::make_unique<ButtonStickerSheet>());
-  examples.push_back(std::make_unique<CheckboxExample>());
-  examples.push_back(std::make_unique<ComboboxExample>());
-  examples.push_back(std::make_unique<DialogExample>());
-  examples.push_back(std::make_unique<FlexLayoutExample>());
-  examples.push_back(std::make_unique<LabelExample>());
-  examples.push_back(std::make_unique<LinkExample>());
-  examples.push_back(std::make_unique<MenuExample>());
-  examples.push_back(std::make_unique<MessageBoxExample>());
-  examples.push_back(std::make_unique<MultilineExample>());
-  examples.push_back(std::make_unique<NativeThemeExample>());
-  examples.push_back(std::make_unique<ProgressBarExample>());
-  examples.push_back(std::make_unique<RadioButtonExample>());
-  examples.push_back(std::make_unique<ScrollViewExample>());
-  examples.push_back(std::make_unique<SliderExample>());
-  examples.push_back(std::make_unique<TabbedPaneExample>());
-  examples.push_back(std::make_unique<TableExample>());
-  examples.push_back(std::make_unique<TextExample>());
-  examples.push_back(std::make_unique<TextfieldExample>());
-  examples.push_back(std::make_unique<ToggleButtonExample>());
-  examples.push_back(std::make_unique<ThrobberExample>());
-  examples.push_back(std::make_unique<TreeViewExample>());
-  examples.push_back(std::make_unique<VectorExample>());
-  examples.push_back(std::make_unique<WidgetExample>());
-  return examples;
-}
 
 struct ExampleTitleCompare {
   bool operator()(const std::unique_ptr<ExampleBase>& a,
