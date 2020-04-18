56fe148ea317c2b78bc1e000b4a7ecd2981af8a8
Jesse McKenna <jessemckenna@google.com>
2020-04-18 00:00:13 +0000

    desktop-pwas: WebApp.Launcher.UpdateResult metric
    This change adds the WebApp.Launcher.UpdateResult metric, which will
    report failures updating Progressive Web App (PWA) launcher
    executables.
    
    On Windows, each PWA has a separate launcher executable in its web-app
    data directory (located at
    <User Data dir>/<profile>/Web Applications/_crx_<app ID>/).
    
    When Chrome is launched by an out-of-date PWA launcher, it updates all
    PWA launchers for the current profile. Because these updates involve
    file operations and take place in the user-data directory, they may
    fail due to permissions errors, etc.
    
    This metric will allow us to monitor how often PWA-launcher update is
    failing, and handle common failure causes if necessary.
    
    Bug: 1067241
    Change-Id: Ie2ea99ea2f1c284a2388e3eb52b664c6ba5af8ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146084
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#760261}

--- chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.cc
+++ chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.cc
@@ -11,6 +11,7 @@
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/web_applications/chrome_pwa_launcher/chrome_pwa_launcher_util.h"
 #include "chrome/installer/util/callback_work_item.h"
 #include "chrome/installer/util/delete_tree_work_item.h"
@@ -20,6 +21,16 @@ namespace {
 
 constexpr base::FilePath::StringPieceType kOldLauncherSuffix =
     FILE_PATH_LITERAL("_old");
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class WebAppLauncherUpdateResult {
+  kSuccess = 0,
+  kFailedToCreateTempDir = 1,
+  kFailedToReplaceLauncher = 2,
+  kLatestVersionPathDoesNotExist = 3,
+  kMaxValue = kLatestVersionPathDoesNotExist,
+};
 
 // A callback invoked by |work_item| that tries to create a hardlink to
 // |latest_version_path| at |launcher_path|. If it fails, tries to create a copy
@@ -38,6 +49,11 @@ void DeleteHardLinkOrCopyCallback(const base::FilePath& launcher_path,
   base::DeleteFile(launcher_path, /*recursive=*/false);
 }
 
+void RecordWebAppLauncherUpdateResult(WebAppLauncherUpdateResult result) {
+  base::UmaHistogramEnumeration("WebApp.Launcher.WebAppLauncherUpdateResult",
+                                result);
+}
+
 // Replaces |launcher_path| with the one at |latest_version_path|. This is done
 // by atomically renaming |launcher_path| to |old_path| and creating a hardlink
 // to or copy of |latest_version_path| at |launcher_path|. Makes a best-effort
@@ -47,14 +63,16 @@ void ReplaceLauncherWithLatestVersion(const base::FilePath& launcher_path,
                                       const base::FilePath& latest_version_path,
                                       const base::FilePath& old_path) {
   if (!base::PathExists(latest_version_path)) {
-    // TODO(jessemckenna): emit failure metric to UMA.
+    RecordWebAppLauncherUpdateResult(
+        WebAppLauncherUpdateResult::kLatestVersionPathDoesNotExist);
     return;
   }
 
   // Create a temporary backup directory for use while moving in-use files.
   base::ScopedTempDir temp_dir;
   if (!temp_dir.CreateUniqueTempDirUnderPath(launcher_path.DirName())) {
-    // TODO(jessemckenna): emit failure metric to UMA.
+    RecordWebAppLauncherUpdateResult(
+        WebAppLauncherUpdateResult::kFailedToCreateTempDir);
     return;
   }
 
@@ -78,9 +96,12 @@ void ReplaceLauncherWithLatestVersion(const base::FilePath& launcher_path,
   delete_old_version_work_item->set_rollback_enabled(false);
   change_list->AddWorkItem(delete_old_version_work_item.release());
 
-  if (!change_list->Do()) {
+  if (change_list->Do()) {
+    RecordWebAppLauncherUpdateResult(WebAppLauncherUpdateResult::kSuccess);
+  } else {
     change_list->Rollback();
-    // TODO(jessemckenna): emit failure metric to UMA.
+    RecordWebAppLauncherUpdateResult(
+        WebAppLauncherUpdateResult::kFailedToReplaceLauncher);
   }
 }
 
@@ -115,7 +136,6 @@ namespace web_app {
 
 void UpdatePwaLaunchers(std::vector<base::FilePath> launcher_paths) {
   const base::FilePath latest_version_path = GetChromePwaLauncherPath();
-  DCHECK(!latest_version_path.empty());
 
   for (const auto& path : launcher_paths) {
     DCHECK(!path.empty());
