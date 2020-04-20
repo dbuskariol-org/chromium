6137a19b6bc58c8618475a5b3b72a16394f6c3aa
Hans Wennborg <hans@chromium.org>
2020-04-20 16:54:49 +0000

    logging.h related include-what-you-use (google_apis/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: If0ae65768b8b03077e06c70d37a221b7a4392971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157085
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760538}

--- google_apis/gcm/engine/account_mapping.cc
+++ google_apis/gcm/engine/account_mapping.cc
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
