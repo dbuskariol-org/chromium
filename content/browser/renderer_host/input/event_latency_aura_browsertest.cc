cc2f99e2ae7bf83fa042f12bc9cd3263df161ba7
Mohsen Izadi <mohsen@chromium.org>
2020-04-22 22:03:05 +0000

    Revert "Sheriff: disable flaky test EventLatencyBrowserTest.KeyPressInInputWithAnimation"
    This reverts commit e9180a2b1be725271fbcac9237b9d08445f4e6d0.
    
    Reason for revert: The CL disables the wrong test. The correct test was disabled later.
    
    Original change's description:
    > Sheriff: disable flaky test EventLatencyBrowserTest.KeyPressInInputWithAnimation
    > 
    > See bug
    > 
    > TBR=dtapuska@chromium.org,mohsen@chromium.org
    > 
    > Bug: 1072340
    > Change-Id: I0f08bf747e79184a3886d4cb6b0e4191822e9b65
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160372
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#761341}
    
    TBR=rakina@chromium.org
    
    Change-Id: Iecb8cab4a6683fe6739d596f757828b09450336c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1072340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161888
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761661}

--- content/browser/renderer_host/input/event_latency_aura_browsertest.cc
+++ content/browser/renderer_host/input/event_latency_aura_browsertest.cc
@@ -114,8 +114,7 @@ IN_PROC_BROWSER_TEST_F(EventLatencyBrowserTest, KeyPressOnButton) {
 
 // Tests that if a key-press on a page with an animation causes a visual update,
 // appropriate event latency metrics are reported.
-IN_PROC_BROWSER_TEST_F(EventLatencyBrowserTest,
-                       DISABLED_KeyPressOnButtonWithAnimation) {
+IN_PROC_BROWSER_TEST_F(EventLatencyBrowserTest, KeyPressOnButtonWithAnimation) {
   base::HistogramTester histogram_tester;
 
   ASSERT_NO_FATAL_FAILURE(LoadTestPage());
