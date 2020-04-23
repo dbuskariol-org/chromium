f5c212bb7ec7a4ab5b31092b67838a45b3f28d7b
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-04-23 18:30:15 +0000

    [CrOS settings] Fix polymer_modulizer targets in os_printing_page/.
    This CL is in preparation for enabling Polymer3 in ChromeOS settings
    pages. It fixed issues in os_printing_page/ folder that break
    semi-automatic Polymer3 migration. These changes include splitting up
    files that contain multiple <dom-module> elements, as this is required
    by Polymer3 conversion tools.
    
    Bug: 1045266
    Change-Id: Idf9b18aac87b74d34a431ffe5383fd2f845554fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151090
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: jimmy gong <jimmyxgong@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#762024}

--- chrome/browser/resources/settings/chromeos/os_printing_page/cups_printer_dialog_error.js
+++ chrome/browser/resources/settings/chromeos/os_printing_page/cups_printer_dialog_error.js
@@ -2,20 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** 'add-printer-dialog' is the template of the Add Printer dialog. */
-Polymer({
-  is: 'add-printer-dialog',
-
-  /** @private */
-  attached() {
-    this.$.dialog.showModal();
-  },
-
-  close() {
-    this.$.dialog.close();
-  },
-});
-
 /**
  * 'printer-dialog-error' is the error container for dialogs.
  */
