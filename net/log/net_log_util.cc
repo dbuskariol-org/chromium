53a26f4a00ba1a5c9572a3bbbab6e10b0e18fc21
Matt Menke <mmenke@chromium.org>
2020-04-23 17:50:18 +0000

    Add DNS cache entries with transient NIKs to net-export logs.
    Bug: 997049
    Change-Id: I2b6bd94a72110094914e0962c34f8bb893a19ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154726
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762002}

--- net/log/net_log_util.cc
+++ net/log/net_log_util.cc
@@ -381,7 +381,8 @@ NET_EXPORT std::unique_ptr<base::DictionaryValue> GetNetInfo(
       cache_info_dict->SetInteger("network_changes", cache->network_changes());
 
       cache->GetAsListValue(cache_contents_list.get(),
-                            /*include_staleness=*/true);
+                            true /* include_staleness */,
+                            HostCache::SerializationType::kDebug);
       cache_info_dict->Set("entries", std::move(cache_contents_list));
 
       dict->Set("cache", std::move(cache_info_dict));
