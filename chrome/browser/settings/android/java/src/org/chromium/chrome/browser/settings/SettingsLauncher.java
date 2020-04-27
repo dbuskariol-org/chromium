799414f1fb28ab6644123e85018794da0d588512
Natalie Chouinard <chouinard@chromium.org>
2020-04-27 18:45:07 +0000

    Modularize SettingsLauncher interface
    Build SettingsLauncher interface to //chrome/browser/settings module.
    
    Bug: 1054193
    Change-Id: Iad22e160f016eab2a1fbc43c7be26f5358a5f6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157773
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762960}

--- chrome/browser/settings/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncher.java
+++ chrome/browser/settings/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncher.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.settings;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
