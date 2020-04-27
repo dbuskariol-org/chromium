60dd6ce92c8b1563400fa6eba7a5e0b63758b40a
Daniel Hosseinian <dhoss@chromium.org>
2020-04-27 19:30:12 +0000

    Add tests for GetColorModelForMode()
    Make sure every color model returns a color setting name and color
    value, including |UNKNOWN_COLOR_MODEL|.
    
    Bug: 1069537
    Change-Id: Ic62e57ab1f0a768479b7d6bfe25ee5cd4be8707b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163663
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762981}

--- printing/print_settings_unittest.cc
+++ printing/print_settings_unittest.cc
@@ -21,4 +21,18 @@ TEST(PrintSettingsDeathTest, IsColorModelSelectedUnknown) {
   EXPECT_DCHECK_DEATH(IsColorModelSelected(COLOR_MODEL_LAST + 1));
 }
 
+#if defined(USE_CUPS)
+TEST(PrintSettingsTest, GetColorModelForMode) {
+  std::string color_setting_name;
+  std::string color_value;
+  for (int model = UNKNOWN_COLOR_MODEL; model <= COLOR_MODEL_LAST; ++model) {
+    GetColorModelForMode(model, &color_setting_name, &color_value);
+    EXPECT_FALSE(color_setting_name.empty());
+    EXPECT_FALSE(color_value.empty());
+    color_setting_name.clear();
+    color_value.clear();
+  }
+}
+#endif  // defined(USE_CUPS)
+
 }  // namespace printing
