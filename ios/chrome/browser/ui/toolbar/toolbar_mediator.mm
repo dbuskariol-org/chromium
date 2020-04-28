de2e1fa0f3b3d281aee81656f863ec58abe168e1
Gauthier Ambard <gambard@chromium.org>
2020-04-28 10:46:13 +0000

    [iOS] Fix NTP double omnibox
    This CL makes sure to update the consumer of the toolbar when the a page
    has finished loading.
    This is the only callback we have when a navigation is cancelled, which
    can happen on the NTP when loading a AppStore URL.
    
    Fixed: 1074291
    Change-Id: Ic71b28ef8cf4a102e9f28c9206e4fab8267fd6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2167445
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#763279}

--- ios/chrome/browser/ui/toolbar/toolbar_mediator.mm
+++ ios/chrome/browser/ui/toolbar/toolbar_mediator.mm
@@ -119,7 +119,7 @@
 
 - (void)webStateDidStopLoading:(web::WebState*)webState {
   DCHECK_EQ(_webState, webState);
-  [self.consumer setLoadingState:self.webState->IsLoading()];
+  [self updateConsumer];
 }
 
 - (void)webState:(web::WebState*)webState
