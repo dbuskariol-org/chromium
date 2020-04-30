ca023e1ef4865c393c6cbfcff9bfc880586b5eeb
Bence Béky <bnc@chromium.org>
2020-04-30 18:15:10 +0000

    Remove unused spdy::spdy_enable_granular_decompress_errors flag.
    All uses of the flag have been removed at
    https://quiche.googlesource.com/quiche/+/260f2e4e5763e56d0b824119af655cfddc655f78.
    This flag has been defined twice in Chromium: once in quic namespace,
    once in spdy namespace.  The former definition has already been removed
    at https://crrev.com/c/2170589
    
    Change-Id: I9e3b5d8caffddbc7178739910b1f07fc5450aea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173561
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Auto-Submit: Bence Béky <bnc@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764336}

--- net/spdy/platform/impl/spdy_flags_impl.cc
+++ net/spdy/platform/impl/spdy_flags_impl.cc
@@ -4,8 +4,6 @@
 
 #include "net/spdy/platform/impl/spdy_flags_impl.h"
 
-bool spdy_enable_granular_decompress_errors = true;
-
 // If true, use indexed name if possible when sending
 // Literal Header Field without Indexing instruction.
 bool spdy_hpack_use_indexed_name = true;
