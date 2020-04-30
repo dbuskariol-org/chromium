654f1eb1c2a17120139cfafce3a5d4b962112084
Caleb Raitto <caraitto@chromium.org>
2020-04-30 22:20:52 +0000

    Add stub to determine if user in identifiability study.
    This will be helpful for things like canvas context operation digest
    calculation, where we want to avoid doing work if the user isn't in the
    identifiability study.
    
    Bug: 973801
    Change-Id: Ia7b87a1da199b11b28ac07fb23af4942364dc01d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174529
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764449}

---
+++ third_party/blink/public/common/privacy_budget/identifiability_study_participation.h
@@ -0,0 +1,20 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_PRIVACY_BUDGET_IDENTIFIABILITY_STUDY_PARTICIPATION_H_
+#define THIRD_PARTY_BLINK_PUBLIC_COMMON_PRIVACY_BUDGET_IDENTIFIABILITY_STUDY_PARTICIPATION_H_
+
+namespace blink {
+
+// Returns true if the user is participating in the identifiability study, and
+// false otherwise.
+//
+// This method can be used to avoid computation that is only needed for the
+// study, such as complex digest calculation on canvas operations; for UKM
+// reporting, filtering should happen automatically.
+bool IsUserInStudy();
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_PUBLIC_COMMON_PRIVACY_BUDGET_IDENTIFIABILITY_STUDY_PARTICIPATION_H_
