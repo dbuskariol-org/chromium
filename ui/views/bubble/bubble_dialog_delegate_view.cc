3c2ea4d520a3a8b1225a0aef53ede9fe730c2363
Richard Knoll <knollr@chromium.org>
2020-05-01 19:43:31 +0000

    Fix various Widget observers
    This is a combination of 3 CLs to be merged to M83:
    1: crrev.com/c/2137319
    2: crrev.com/c/2164608
    3: crrev.com/c/2165105
    
    All of them fix various usages of Widget observers and make sure they
    get deregistered before they are destroyed.
    
    Bug: 1055483
    Change-Id: I0e74540acf262cbed81bc4c2af20348f4c5408f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172305
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4103@{#409}
    Cr-Branched-From: 8ad47e8d21f6866e4a37f47d83a860d41debf514-refs/heads/master@{#756066}

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -178,7 +178,7 @@ Widget* BubbleDialogDelegateView::CreateBubble(
 #endif
 
   bubble_delegate->SizeToContents();
-  bubble_widget->AddObserver(bubble_delegate);
+  bubble_delegate->widget_observer_.Add(bubble_widget);
   return bubble_widget;
 }
 
@@ -202,8 +202,6 @@ BubbleDialogDelegateView::BubbleDialogDelegateView(View* anchor_view,
 }
 
 BubbleDialogDelegateView::~BubbleDialogDelegateView() {
-  if (GetWidget())
-    GetWidget()->RemoveObserver(this);
   SetLayoutManager(nullptr);
   SetAnchorView(nullptr);
 }
@@ -262,6 +260,9 @@ void BubbleDialogDelegateView::OnWidgetClosing(Widget* widget) {
 void BubbleDialogDelegateView::OnWidgetDestroying(Widget* widget) {
   if (anchor_widget() == widget)
     SetAnchorView(nullptr);
+
+  if (widget_observer_.IsObserving(widget))
+    widget_observer_.Remove(widget);
 }
 
 void BubbleDialogDelegateView::OnWidgetVisibilityChanging(Widget* widget,
