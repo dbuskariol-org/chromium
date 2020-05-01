43de4f31341695a9520eab1009c8e9eb6046eb11
Johann <johannkoenig@google.com>
2020-05-01 04:48:26 +0000

    heap mojo: add missing copyrights
    Change-Id: I78b9d34b89098bf41aa5cff43d4de02392fbc39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174331
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764568}

--- third_party/blink/renderer/platform/mojo/heap_mojo_remote_test.cc
+++ third_party/blink/renderer/platform/mojo/heap_mojo_remote_test.cc
@@ -1,3 +1,6 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "base/test/null_task_runner.h"
