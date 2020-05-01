856dcc8c9aee22a12732323ee9eb668a715b914d
Eugene But <eugenebut@chromium.org>
2020-05-01 08:45:08 +0000

    Reland: Componetize HeapProfilerController to use on iOS
    This CL only moves files and HeapProfilerController compilation
    is still turned off on iOS, because fixing compilation requires
    changes in base. There will be a few follow up CLs to:
     - separate iOS and non-iOS feature flags
     - compile HeapProfilerController on iOS
     - instantiate and start HeapProfilerController on iOS
    
    Patchset 1: Original CL (crrev.com/c/2168923)
    Patchset 3: Fix
    
    TBR=jwd@chromium.org,erikchen@chromium.org,blundell@chromium.org
    
    Bug: 1075702
    Change-Id: I84c46bd9fb442fd0f844d5d46dbff9ce5f21b208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174902
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764606}

--- components/heap_profiling/in_process/heap_profiler_controller.cc
+++ components/heap_profiling/in_process/heap_profiler_controller.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/common/heap_profiler_controller.h"
+#include "components/heap_profiling/in_process/heap_profiler_controller.h"
 
 #include <cmath>
 
@@ -67,8 +67,7 @@ void HeapProfilerController::ScheduleNextSnapshot(
 }
 
 // static
-void HeapProfilerController::TakeSnapshot(
-    scoped_refptr<StoppedFlag> stopped) {
+void HeapProfilerController::TakeSnapshot(scoped_refptr<StoppedFlag> stopped) {
   if (stopped->data.IsSet())
     return;
   RetrieveAndSendSnapshot();
