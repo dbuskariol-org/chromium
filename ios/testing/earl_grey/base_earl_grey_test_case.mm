e65863dcf3e63fcbfccde1f7d03dedadcc24a324
zhaoyangli <zhaoyangli@chromium.org>
2020-05-02 19:41:41 +0000

    [iOS][EG2] Handle more system alerts correctly at -setUp.
    - For "Software Update" alert, add handling of the second dialog after
      choosing "Later" in first one.
    - Add handling for another format of iOS upgrade alert and carrier
      settings alert.
    - Fail the test when an unknown alert other than iOS upgrade alerts and
      carrier settings alert exists, instead of denying them in EG, since
      these cannot be handled in EG framework.
    
    Bug: 1072000
    Change-Id: I16aaa419187dc5cde336e47d4e53de974c93f77f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173822
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764941}

--- ios/testing/earl_grey/base_earl_grey_test_case.mm
+++ ios/testing/earl_grey/base_earl_grey_test_case.mm
@@ -113,27 +113,49 @@ bool g_needs_set_up_for_test_case = true;
                NSNotFound),
           @"Unknown error caught when handling unknown system alert: %@",
           exception.reason);
-      // If the unsupported alert is iOS upgrade alert, choose "Later".
-      // Otherwise, reject it, as unknown types include alerts which are not
-      // desirable to accept.
+      // If the unsupported alert is iOS upgrade or carrier settings alert,
+      // handle it. Otherwise, fail the test.
       if ([alertText isEqualToString:@"Software Update"]) {
         DLOG(WARNING) << "Denying iOS system alert of Software Update!";
-
-        NSError* dismissingUpgradeAlertError = nil;
-        [self grey_tapSystemDialogButtonWithText:@"Later"
-                                           error:&dismissingUpgradeAlertError];
-        GREYAssertNil(dismissingUpgradeAlertError,
-                      @"Error denying Software Update alert.\n%@",
-                      dismissingUpgradeAlertError);
-
+        // Software Update alert usually has two consecutive alerts, handle them
+        // one by one.
+        NSError* error = nil;
+        // Choose "Later" for the first alert.
+        [self tapAlertButtonWithText:@"Later" error:&error];
+        // If an error with code |GREYSystemAlertCustomButtonNotFound| happens,
+        // probably the second alert is already there. Try to handle it in
+        // following steps.
+        GREYAssert(
+            error == nil || error.code == GREYSystemAlertCustomButtonNotFound,
+            @"Error denying first Software Update alert.\n%@", error);
+        // A second alert promoting to update tonight will appear. Wait for it.
+        [self grey_waitForAlertVisibility:YES
+                              withTimeout:kSystemAlertVisibilityTimeout];
+        error = nil;
+        // Choose "Remind Me Later" for the second alert.
+        [self tapAlertButtonWithText:@"Remind Me Later" error:&error];
+        GREYAssertNil(error, @"Error denying second Software Update alert.\n%@",
+                      error);
+      } else if ([alertText
+                     containsString:@"A new iOS update is now available."]) {
+        DLOG(WARNING)
+            << "Denying iOS system alert of new iOS update is now available!";
+        // This is another format of Software Update dialog. Need to choose
+        // "Close".
+        NSError* error = nil;
+        [self tapAlertButtonWithText:@"Close" error:&error];
+        GREYAssertNil(error, @"Error closing Software Update alert.\n%@",
+                      error);
+      } else if ([alertText isEqualToString:@"Carrier Settings Update"]) {
+        DLOG(WARNING) << "Denying iOS system alert of Carrier Settings Update!";
+        NSError* error = nil;
+        [self tapAlertButtonWithText:@"Not Now" error:&error];
+        GREYAssertNil(
+            error, @"Error closing Carrier Settings Update alert.\n%@", error);
       } else {
-        DLOG(WARNING) << "Denying iOS system alert of unknown type: "
-                      << base::SysNSStringToUTF8(alertText);
-
-        NSError* denyAlertError = nil;
-        [self grey_denySystemDialogWithError:&denyAlertError];
-        GREYAssertNil(denyAlertError, @"Error denying system alert.\n%@",
-                      denyAlertError);
+        XCTFail("An unsupported system alert is present on device. Failing "
+                "this test. Alert label: %@",
+                alertText);
       }
     }
   }
@@ -162,6 +184,33 @@ bool g_needs_set_up_for_test_case = true;
             @"+setUp method to +setUpForTestCase.",
             NSStringFromClass([self class]));
   }
+}
+
+// Taps button with |text| in the system alert on screen. If an alert or the
+// button doesn't exist, note it in |error| accordingly. In EG1, this method is
+// no-op.
+- (void)tapAlertButtonWithText:(NSString*)text error:(NSError**)error {
+#if defined(CHROME_EARL_GREY_2)
+  XCUIApplication* springboardApp = [[XCUIApplication alloc]
+      initWithBundleIdentifier:@"com.apple.springboard"];
+  XCUIElement* alert = [[springboardApp
+      descendantsMatchingType:XCUIElementTypeAlert] firstMatch];
+  if (![alert waitForExistenceWithTimeout:kSystemAlertVisibilityTimeout]) {
+    *error = [NSError errorWithDomain:kGREYSystemAlertDismissalErrorDomain
+                                 code:GREYSystemAlertNotPresent
+                             userInfo:nil];
+    return;
+  }
+  XCUIElement* button = alert.buttons[text];
+  if (![alert.buttons[text] exists]) {
+    *error = [NSError errorWithDomain:kGREYSystemAlertDismissalErrorDomain
+                                 code:GREYSystemAlertCustomButtonNotFound
+                             userInfo:nil];
+    return;
+  }
+
+  [button tap];
+#endif  // CHROME_EARL_GREY_2
 }
 
 @end
