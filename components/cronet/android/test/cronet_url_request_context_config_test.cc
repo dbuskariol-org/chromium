652185a088229532f81f9f30e41a0498694fd8e6
Renjie Tang <renjietang@chromium.org>
2020-05-04 23:44:30 +0000

    Default enable QUIC in Cronet Android and IOS.
    This change introduces no API changes. But API documents need to be updated.
    
    Change-Id: Ibc347df79066de704c5be0683c5b658ba6e1896a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168553
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765334}

--- components/cronet/android/test/cronet_url_request_context_config_test.cc
+++ components/cronet/android/test/cronet_url_request_context_config_test.cc
@@ -21,6 +21,8 @@ namespace cronet {
 // Verifies that all the configuration options set by
 // CronetUrlRequestContextTest.testCronetEngineBuilderConfig
 // made it from the CronetEngine.Builder to the URLRequestContextConfig.
+// TODO(b/1078039): Make this function take an expected config instead of
+// comparing against a hardcoded one.
 static void JNI_CronetUrlRequestContextTest_VerifyUrlRequestContextConfig(
     JNIEnv* env,
     jlong jurl_request_context_config,
@@ -36,6 +38,29 @@ static void JNI_CronetUrlRequestContextTest_VerifyUrlRequestContextConfig(
   CHECK_EQ((*config->quic_hints.begin())->alternate_port, 34);
   CHECK_NE(config->quic_user_agent_id.find("Cronet/" CRONET_VERSION),
            std::string::npos);
+  CHECK_EQ(config->load_disable_cache, false);
+  CHECK_EQ(config->http_cache, URLRequestContextConfig::HttpCacheType::MEMORY);
+  CHECK_EQ(config->http_cache_max_size, 54321);
+  CHECK_EQ(config->user_agent, "efgh");
+  CHECK(!config->effective_experimental_options);
+  CHECK_EQ(config->storage_path,
+           base::android::ConvertJavaStringToUTF8(env, jstorage_path));
+}
+
+// Verify that QUIC can be turned off in CronetEngine.Builder.
+// TODO(http://crbug.com/1078039): Make VerifyUrlRequestContextConfig() take an
+// expected config instead of hardcoded values so that this method can be
+// removed.
+static void
+JNI_CronetUrlRequestContextTest_VerifyUrlRequestContextQuicOffConfig(
+    JNIEnv* env,
+    jlong jurl_request_context_config,
+    const JavaParamRef<jstring>& jstorage_path) {
+  URLRequestContextConfig* config =
+      reinterpret_cast<URLRequestContextConfig*>(jurl_request_context_config);
+  CHECK_EQ(config->enable_spdy, false);
+  CHECK_EQ(config->enable_quic, false);
+  CHECK_EQ(config->bypass_public_key_pinning_for_local_trust_anchors, false);
   CHECK_EQ(config->load_disable_cache, false);
   CHECK_EQ(config->http_cache, URLRequestContextConfig::HttpCacheType::MEMORY);
   CHECK_EQ(config->http_cache_max_size, 54321);
