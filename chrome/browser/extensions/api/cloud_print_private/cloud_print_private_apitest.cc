8a395f363284e946702ef2f7a9cea0e3aac68cf3
David Bertoni <dbertoni@chromium.org>
2020-05-04 20:11:19 +0000

    [Extensions] Re-enable a cloudPrintPrivate API test.
    This test was disabled as flaky long ago, but passed on
    the bots with flying color for 3000 iterations.
    
    Bug: 177163
    Change-Id: I0a20abd01c89d98ef000da701157e790d2b304b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176698
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765233}

--- chrome/browser/extensions/api/cloud_print_private/cloud_print_private_apitest.cc
+++ chrome/browser/extensions/api/cloud_print_private/cloud_print_private_apitest.cc
@@ -85,14 +85,8 @@ MATCHER(IsExpectedUserSettings, "") {
          settings.printers[1].connect;
 }
 
-// http://crbug.com/177163
-#if defined(OS_WIN) && !defined(NDEBUG)
-#define MAYBE_CloudPrintHostedWithMock DISABLED_CloudPrintHostedWithMock
-#else
-#define MAYBE_CloudPrintHostedWithMock CloudPrintHostedWithMock
-#endif
 IN_PROC_BROWSER_TEST_F(ExtensionCloudPrintPrivateApiTest,
-                       MAYBE_CloudPrintHostedWithMock) {
+                       CloudPrintHostedWithMock) {
   CloudPrintTestsDelegateMock cloud_print_mock;
 
   EXPECT_CALL(cloud_print_mock,
