2aa56a9fa7b1dd72a5b96534c2d18115ee8a37be
deepak1556 <hop2deep@gmail.com>
2020-05-04 23:43:19 +0000

    fix: disable kPlugin helper when enable_plugins=false
    Bug: none
    Change-Id: Iace766c61a64987fa4c25a12b0a74b3ec09d2c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173416
    Auto-Submit: Deepak Mohan <hop2deep@gmail.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765333}

--- content/common/child_process_host_impl.cc
+++ content/common/child_process_host_impl.cc
@@ -32,6 +32,7 @@
 #include "ipc/ipc_logging.h"
 #include "ipc/message_filter.h"
 #include "mojo/public/cpp/bindings/lib/message_quota_checker.h"
+#include "ppapi/buildflags/buildflags.h"
 #include "services/resource_coordinator/public/mojom/memory_instrumentation/constants.mojom.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 
@@ -95,8 +96,10 @@ base::FilePath ChildProcessHost::GetChildPath(int flags) {
       child_base_name += kMacHelperSuffix_renderer;
     } else if (flags == CHILD_GPU) {
       child_base_name += kMacHelperSuffix_gpu;
+#if BUILDFLAG(ENABLE_PLUGINS)
     } else if (flags == CHILD_PLUGIN) {
       child_base_name += kMacHelperSuffix_plugin;
+#endif
     } else {
       NOTREACHED();
     }
