86b7fcbfdbc8d40966eebf0e20917cbf1af2114b
Hesen Zhang <hesen@google.com>
2020-05-05 04:00:19 +0000

    [Upboarding]: Use iterator to verify response conversion.
    Bug: 1077852
    Change-Id: If793d508092c4f339d2c416dcad0fe56d9fa5678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180366
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765398}

--- components/query_tiles/internal/proto_conversion_unittest.cc
+++ components/query_tiles/internal/proto_conversion_unittest.cc
@@ -5,6 +5,7 @@
 #include "components/query_tiles/internal/proto_conversion.h"
 
 #include "base/strings/string_number_conversions.h"
+#include "components/query_tiles/internal/tile_iterator.h"
 #include "components/query_tiles/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -14,20 +15,26 @@ namespace {
 constexpr char kTestLocale[] = "en";
 
 std::string BuildIdForSampleTile(size_t level, size_t pos) {
-  return "test_tile_id_" + base::NumberToString(level) + "_" +
-         base::NumberToString(pos);
+  return base::NumberToString(level) + "-" + base::NumberToString(pos);
 }
 
+void VerifySampleTileId(int level, int pos, const std::string& id) {
+  auto expect_id = BuildIdForSampleTile(level, pos);
+  EXPECT_EQ(expect_id, id);
+}
+
+// Build a fake two level response proto.
 void InitResponseProto(ResponseGroupProto* response,
-                       size_t num_level,
-                       size_t num_tiles_each_level) {
+                       size_t num_tiles_each_tile) {
   response->set_locale(kTestLocale);
-  for (size_t i = 0; i < num_level; i++) {
+  // Add top level tiles.
+  for (size_t i = 0; i < num_tiles_each_tile; i++) {
     auto* new_top_level_tile = response->add_tiles();
-    new_top_level_tile->set_tile_id(BuildIdForSampleTile(i, 0));
+    new_top_level_tile->set_tile_id(BuildIdForSampleTile(0, i));
     new_top_level_tile->set_is_top_level(true);
-    for (size_t j = 0; j < num_tiles_each_level; j++) {
-      auto new_tile_id = BuildIdForSampleTile(i, j);
+    // Add sub-tiles.
+    for (size_t j = 0; j < num_tiles_each_tile; j++) {
+      auto new_tile_id = BuildIdForSampleTile(1, i * num_tiles_each_tile + j);
       new_top_level_tile->add_sub_tile_ids(new_tile_id);
       auto* new_tile = response->add_tiles();
       new_tile->set_tile_id(new_tile_id);
@@ -72,7 +79,8 @@ TEST(TileProtoConversionTest, TileGroupConversions) {
 
 TEST(TileProtoConversionTest, TileGroupFromResponseConversions) {
   ResponseGroupProto server_response;
-  InitResponseProto(&server_response, 2, 3);
+  const int num_tiles_each_tile = 3;
+  InitResponseProto(&server_response, num_tiles_each_tile);
   TileGroup tile_group;
   TileGroupFromResponse(server_response, &tile_group);
   std::string server_response_str;
@@ -80,7 +88,16 @@ TEST(TileProtoConversionTest, TileGroupFromResponseConversions) {
   EXPECT_EQ(tile_group.locale, "en") << std::endl
                                      << server_response_str << std::endl
                                      << test::DebugString(&tile_group);
-  // TODO(crbug.com/1077852): Verify group data by using iterator.
+  TileIterator iter(tile_group, TileIterator::kAllTiles);
+  size_t count = 0;
+  while (iter.HasNext()) {
+    const auto* next = iter.Next();
+    int level = count >= num_tiles_each_tile ? 1 : 0;
+    int pos = count - (level == 0 ? 0 : num_tiles_each_tile);
+    VerifySampleTileId(level, pos, next->id);
+    count++;
+  }
+  EXPECT_EQ(count, 12u) << test::DebugString(&tile_group);
 }
 
 }  // namespace
