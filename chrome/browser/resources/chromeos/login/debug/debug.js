6933250508ffd8dff79e60b333778d966c751933
Denis Kuznetsov <antrim@google.com>
2020-05-05 19:23:08 +0000

    OOBE dev overaly: Add buttons for swtiching between screens
    Bug: 1073095
    Change-Id: Iba05e2c5fcc014bfdbca31b231a60933d4415a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181204
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765699}

--- chrome/browser/resources/chromeos/login/debug/debug.js
+++ chrome/browser/resources/chromeos/login/debug/debug.js
@@ -6,7 +6,6 @@
  * @fileoverview Root element of the OOBE UI Debugger.
  */
 
-
 cr.define('cr.ui.login.debug', function() {
   const DEBUG_BUTTON_STYLE = `
       height:20px;
@@ -23,9 +22,9 @@ cr.define('cr.ui.login.debug', function() {
       position: absolute;
       top: 0;
       left: 0;
-      height: 100%;
-      width: 100%;
-      background-color: rgba(0, 0, 255, 0.75);
+      right: 0;
+      bottom : 0;
+      background-color: rgba(0, 0, 255, 0.90);
       color: white;
       z-index: 10000;
       padding: 20px;
@@ -35,23 +34,269 @@ cr.define('cr.ui.login.debug', function() {
   const TOOL_PANEL_STYLE = `
       direction: ltr;
       display: flex;
+      flex-wrap: wrap;
       flex-direction: row;`;
 
   const TOOL_BUTTON_STYLE = `
       border: 2px solid white;
       padding: 3px 10px;
-      margin: 0 4px;`;
+      margin: 3px 4px;
+      text-align: center;
+      vertical-align: middle;
+      font-weight: bold;`;
+
+  const TOOL_BUTTON_SELECTED_STYLE = `
+      border-width: 4px !important;
+      padding: 2px 9px !important;
+      margin: 2px 3px !important;`;
+
+  // White.
+  const SCREEN_BUTTON_STYLE_NORMAL = `
+      border-color: #fff !important;
+      color: #fff;`;
+  // Orange-tinted.
+  const SCREEN_BUTTON_STYLE_ERROR = `
+      border-color: #f80 !important;
+      color: #f80`;
+  // Green-tinted.
+  const SCREEN_BUTTON_STYLE_OTHER = `
+      border-color: #afa !important;
+      color: #afa`;
+  // Pink-tinted.
+  const SCREEN_BUTTON_STYLE_UNKNOWN = `
+      border-color: #faa !important;
+      color: #faa`;
+
+  /**
+   * Indicates if screen is present in usual user flow, represents some error
+   * state or is shown in some other cases. See KNOWN_SCREENS for more details.
+   * @enum {string}
+   */
+  const ScreenKind = {
+    NORMAL: 'normal',
+    ERROR: 'error',
+    OTHER: 'other',
+    UNKNOWN: 'unknown',
+  };
+
+  /**
+   * List of possible screens.
+   * Attributes:
+   *    id - ID of the screen passed to login.createScreen
+   *    kind - Used for button coloring, indicates if screen is present in
+   *           normal flow (via debug-button-<kind> classes).
+   *    suffix - extra text to display on button, gives some insight
+   *             on context in which screen is shown (e.g. Enterprise-specific
+   *             screens).
+   *    data - extra data passed to DisplayManager.showScreen.
+   */
+  const KNOWN_SCREENS = [
+    {
+      // Device without mouse/keyboard.
+      id: 'hid-detection',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      // Welcome screen.
+      id: 'connect',
+      kind: ScreenKind.NORMAL,
+      data: {
+        isDeveloperMode: true,
+      }
+    },
+    {
+      id: 'debugging',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      id: 'demo-preferences',
+      kind: ScreenKind.OTHER,
+      suffix: 'demo',
+    },
+    {
+      id: 'network-selection',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'eula',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'demo-setup',
+      kind: ScreenKind.OTHER,
+      suffix: 'demo',
+    },
+    {
+      id: 'update',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'auto-enrollment-check',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'device-disabled',
+      kind: ScreenKind.ERROR,
+      suffix: 'E',
+    },
+    {
+      id: 'oauth-enrollment',
+      kind: ScreenKind.NORMAL,
+      suffix: 'E',
+    },
+    {
+      id: 'packaged-license',
+      kind: ScreenKind.NORMAL,
+      suffix: 'E',
+    },
+    // Login screen starts here.
+    {
+      id: 'error-message',
+      kind: ScreenKind.ERROR,
+    },
+    {
+      id: 'update-required',
+      kind: ScreenKind.OTHER,
+      suffix: 'E',
+    },
+    {
+      id: 'autolaunch',
+      kind: ScreenKind.OTHER,
+      suffix: 'kiosk',
+    },
+    {
+      id: 'app-launch-splash',
+      kind: ScreenKind.OTHER,
+      suffix: 'kiosk',
+      data: {
+        appInfo: {
+          name: 'Application Name',
+          url: 'http://example.com/someApplication/VeryLongURL',
+          iconURL: 'chrome://theme/IDR_LOGO_GOOGLE_COLOR_90',
+        },
+        shortcutEnabled: true,
+      }
+    },
+    {
+      id: 'wrong-hwid',
+      kind: ScreenKind.ERROR,
+    },
+    {
+      id: 'reset',
+      kind: ScreenKind.OTHER,
+    },
+
+    {
+      // Shown instead of sign-in screen, triggered from Crostini.
+      id: 'adb-sideloading',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      // Customer kiosk feature.
+      id: 'kiosk-enable',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      id: 'account-picker',
+      kind: ScreenKind.OTHER,
+      suffix: 'multiuser',
+    },
+    {
+      id: 'gaia-signin',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'tpm-error-message',
+      kind: ScreenKind.ERROR,
+    },
+    {
+      // Failure during SAML flow.
+      id: 'fatal-error',
+      kind: ScreenKind.ERROR,
+      suffix: 'SAML',
+    },
+    {
+      // GAIA password changed.
+      id: 'password-changed',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      id: 'ad-password-change',
+      kind: ScreenKind.OTHER,
+      data: {
+        username: 'username',
+        error: 1,
+      }
+    },
+    {
+      id: 'encryption-migration',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      id: 'confirm-password',
+      kind: ScreenKind.OTHER,
+      suffix: 'SAML',
+    },
+    {
+      id: 'supervision-transition',
+      kind: ScreenKind.OTHER,
+    },
+    {
+      id: 'terms-of-service',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'sync-consent',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'fingerprint-setup',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'discover',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'arc-tos',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'recommend-apps',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'app-downloading',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'assistant-optin-flow',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'multidevice-setup',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'gesture-navigation',
+      kind: ScreenKind.NORMAL,
+    },
+    {
+      id: 'marketing-opt-in',
+      kind: ScreenKind.NORMAL,
+    },
+  ];
 
   class DebugButton {
     constructor(parent, title, callback) {
-      let div =
+      this.element =
           /** @type {!HTMLElement} */ (document.createElement('div'));
-      div.textContent = title;
+      this.element.textContent = title;
 
-      div.setAttribute('style', TOOL_BUTTON_STYLE);
-      div.addEventListener('click', this.onClick.bind(this));
+      this.element.className = 'debug-tool-button';
+      this.element.addEventListener('click', this.onClick.bind(this));
 
-      parent.appendChild(div);
+      parent.appendChild(this.element);
 
       this.callback_ = callback;
       // In most cases we want to hide debugger UI right after making some
@@ -75,11 +320,11 @@ cr.define('cr.ui.login.debug', function() {
   class ToolPanel {
     constructor(parent, title) {
       let titleDiv =
-          /** @type {!HTMLElement} */ (document.createElement('h1'));
+          /** @type {!HTMLElement} */ (document.createElement('h2'));
       titleDiv.textContent = title;
 
       let panel = /** @type {!HTMLElement} */ (document.createElement('div'));
-      panel.setAttribute('style', TOOL_PANEL_STYLE);
+      panel.className = 'debug-tool-panel';
 
       parent.appendChild(titleDiv);
       parent.appendChild(panel);
@@ -93,6 +338,9 @@ cr.define('cr.ui.login.debug', function() {
     }
 
     showDebugUI() {
+      if (this.debuggerVisible_)
+        return;
+      this.refreshScreensPanel();
       this.debuggerVisible_ = true;
       this.debuggerOverlay_.removeAttribute('hidden');
     }
@@ -128,12 +376,105 @@ cr.define('cr.ui.login.debug', function() {
       });
     }
 
+    createScreensPanel(parent) {
+      let panel = new ToolPanel(this.debuggerOverlay_, 'Screens');
+      // List of screens will be created later, as not all screens
+      // might be registered at this point.
+      this.screensPanel = panel;
+    }
+
+    switchToScreen(screen) {
+      var data = {};
+      if (screen.data) {
+        data = screen.data;
+      }
+      /** @suppress {visibility} */
+      cr.ui.Oobe.instance_.showScreen({id: screen.id, data: data});
+    }
+
+    createScreensList() {
+      var screenMap = {};
+      // Ordering of the screens.
+      KNOWN_SCREENS.forEach(function(screen, index) {
+        screen.index = index;
+        screenMap[screen.id] = screen;
+      });
+      this.knownScreens = [];
+      this.screenButtons = {};
+      /** @suppress {visibility} */
+      for (var id of cr.ui.Oobe.instance_.screens_) {
+        if (id in screenMap) {
+          this.knownScreens.push(screenMap[id]);
+        } else {
+          console.error('### Screen not registered in debug overlay ' + id);
+          let unknownScreen = {
+            id: id,
+            kind: ScreenKind.UNKNOWN,
+            suffix: '???',
+            index: this.knownScreens.length + 1000,
+          };
+          this.knownScreens.push(unknownScreen);
+        }
+      }
+      this.knownScreens = this.knownScreens.sort((a, b) => a.index - b.index);
+      let content = this.screensPanel.content;
+      let debugThis = this;
+      this.knownScreens.forEach(function(screen) {
+        var name = screen.id;
+        if (screen.suffix) {
+          name = name + ' (' + screen.suffix + ')';
+        }
+        var button = new DebugButton(
+            content, name, debugThis.switchToScreen.bind(debugThis, screen));
+        button.element.classList.add('debug-button-' + screen.kind);
+        debugThis.screenButtons[screen.id] = button;
+      });
+    }
+
+    refreshScreensPanel() {
+      if (this.knownScreens === undefined) {
+        this.createScreensList();
+      }
+      /** @suppress {visibility} */
+      let displayManager = cr.ui.Oobe.instance_;
+      if (this.lastScreenId) {
+        this.screenButtons[this.lastScreenId].element.classList.remove(
+            'debug-button-selected');
+      }
+      if (displayManager.currentScreen) {
+        this.lastScreenId = displayManager.currentScreen.id;
+        this.screenButtons[this.lastScreenId].element.classList.add(
+            'debug-button-selected');
+      }
+    }
+
+    createCssStyle(name, styleSpec) {
+      var style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = '.' + name + ' {' + styleSpec + '}';
+      document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
     register(element) {
+      // Create CSS styles
+      {
+        this.createCssStyle('debugger-button', DEBUG_BUTTON_STYLE);
+        this.createCssStyle('debugger-overlay', DEBUG_OVERLAY_STYLE);
+        this.createCssStyle('debug-tool-panel', TOOL_PANEL_STYLE);
+        this.createCssStyle('debug-tool-button', TOOL_BUTTON_STYLE);
+        this.createCssStyle(
+            'debug-button-selected', TOOL_BUTTON_SELECTED_STYLE);
+        this.createCssStyle('debug-button-normal', SCREEN_BUTTON_STYLE_NORMAL);
+        this.createCssStyle('debug-button-error', SCREEN_BUTTON_STYLE_ERROR);
+        this.createCssStyle('debug-button-other', SCREEN_BUTTON_STYLE_OTHER);
+        this.createCssStyle(
+            'debug-button-unknown', SCREEN_BUTTON_STYLE_UNKNOWN);
+      }
       {
         // Create UI Debugger button
         let button =
             /** @type {!HTMLElement} */ (document.createElement('div'));
-        button.setAttribute('style', DEBUG_BUTTON_STYLE);
+        button.className = 'debugger-button';
         button.textContent = 'Debug';
         button.addEventListener('click', this.toggleDebugUI.bind(this));
 
@@ -143,11 +484,12 @@ cr.define('cr.ui.login.debug', function() {
         // Create base debugger panel.
         let overlay =
             /** @type {!HTMLElement} */ (document.createElement('div'));
-        overlay.setAttribute('style', DEBUG_OVERLAY_STYLE);
+        overlay.className = 'debugger-overlay';
         overlay.setAttribute('hidden', true);
         this.debuggerOverlay_ = overlay;
       }
       this.createLanguagePanel(this.debuggerOverlay_);
+      this.createScreensPanel(this.debuggerOverlay_);
 
       element.appendChild(this.debuggerButton_);
       element.appendChild(this.debuggerOverlay_);
