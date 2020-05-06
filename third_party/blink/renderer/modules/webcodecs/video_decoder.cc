5331f113bf92ba46cc5a6345c516aa8bb65913bf
Dan Sanders <sandersd@chromium.org>
2020-05-06 23:11:08 +0000

    [media] Implement dispatching of requests in blink::VideoDecoder
    This implements the sequencing between requests and all state
    transitions. It does not yet create an actual media::VideoDecoder or
    handle errors.
    
    Change-Id: I372a47bf9c5ad430ba8b5d6bcc594aec818d2a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152206
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766197}

--- third_party/blink/renderer/modules/webcodecs/video_decoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder.cc
@@ -4,18 +4,24 @@
 
 #include "third_party/blink/renderer/modules/webcodecs/video_decoder.h"
 
+#include <utility>
+
 #include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/time/time.h"
+#include "media/base/decoder_buffer.h"
 #include "media/base/media_util.h"
 #include "media/base/video_decoder.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_encoded_video_chunk.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_encoded_video_config.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/webcodecs/encoded_video_chunk.h"
+#include "third_party/blink/renderer/modules/webcodecs/video_decoder_init.h"
 #include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
 #include "third_party/blink/renderer/platform/bindings/exception_code.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
@@ -35,7 +41,9 @@ VideoDecoder::VideoDecoder(ScriptState* script_state,
                            ExceptionState& exception_state)
     : script_state_(script_state) {
   DVLOG(1) << __func__;
-  // TODO(sandersd): Extract callbacks from |init|.
+  // TODO(sandersd): Is it an error to not provide all callbacks?
+  output_cb_ = init->output();
+  error_cb_ = init->error();
 }
 
 VideoDecoder::~VideoDecoder() {
@@ -89,34 +97,184 @@ ScriptPromise VideoDecoder::EnqueueRequest(Request* request) {
       MakeGarbageCollected<ScriptPromiseResolver>(script_state_);
   request->resolver = resolver;
   requests_.push_back(request);
-
-  // If there were no requests before, trigger request processing.
-  if (requests_.size() == 1)
-    ProcessRequests();
-
+  ProcessRequests();
   return resolver->Promise();
 }
 
-void VideoDecoder::ProcessRequests() {}
+void VideoDecoder::ProcessRequests() {
+  DVLOG(3) << __func__;
+  // TODO(sandersd): Re-entrancy checker.
+  // TODO(sandersd): Requests should probably be using virtual dispatch.
+  while (!pending_request_ && !requests_.IsEmpty()) {
+    Request* request = requests_.front();
+    switch (request->type) {
+      case Request::Type::kConfigure:
+        if (!ProcessConfigureRequest(request))
+          return;
+        break;
+      case Request::Type::kDecode:
+        if (!ProcessDecodeRequest(request))
+          return;
+        break;
+      case Request::Type::kFlush:
+        if (!ProcessFlushRequest(request))
+          return;
+        break;
+      case Request::Type::kReset:
+        if (!ProcessResetRequest(request))
+          return;
+        break;
+    }
+    requests_.pop_front();
+  }
+}
 
-void VideoDecoder::HandleError() {}
+bool VideoDecoder::ProcessConfigureRequest(Request* request) {
+  DVLOG(3) << __func__;
+  DCHECK(!pending_request_);
+  DCHECK_EQ(request->type, Request::Type::kConfigure);
+  DCHECK(request->config);
+
+  // TODO(sandersd): If we require configure() after reset() and there is a
+  // pending reset, then we could drop this request.
+  // TODO(sandersd): If the next request is also a configure(), they can be
+  // merged. It's not trivial to detect that situation.
+  // TODO(sandersd): Elide this request if the configuration is unchanged.
+
+  if (!decoder_) {
+    // TODO(sandersd): Create and initialize a decoder.
+
+    // Processing continues in OnInitializeDone().
+    pending_request_ = request;
+    return true;
+  }
+
+  // Note: This flush must not be elided when there is a pending reset. An
+  // alternative would be to process Reset() requests immediately, then process
+  // already queued requests in a special mode. It seems easier to drop all of
+  // this and require configure() after reset() instead.
+  if (pending_decodes_.size() + 1 > size_t{decoder_->GetMaxDecodeRequests()}) {
+    // Try again after OnDecodeDone().
+    return false;
+  }
+
+  // Processing continues in OnConfigureFlushDone().
+  pending_request_ = request;
+  decoder_->Decode(
+      media::DecoderBuffer::CreateEOSBuffer(),
+      WTF::Bind(&VideoDecoder::OnConfigureFlushDone, WrapWeakPersistent(this)));
+  return true;
+}
+
+bool VideoDecoder::ProcessDecodeRequest(Request* request) {
+  DVLOG(3) << __func__;
+  DCHECK(!pending_request_);
+  DCHECK_EQ(request->type, Request::Type::kDecode);
+  DCHECK(request->chunk);
+  DCHECK_GT(requested_decodes_, 0);
+
+  // TODO(sandersd): If a reset has been requested, resolve immediately.
+
+  if (!decoder_) {
+    // TODO(sandersd): Add explanation (no valid configuration).
+    request->resolver.Release()->Reject();
+    return true;
+  }
+
+  if (pending_decodes_.size() + 1 > size_t{decoder_->GetMaxDecodeRequests()}) {
+    // Try again after OnDecodeDone().
+    return false;
+  }
+
+  // Convert |chunk| to a DecoderBuffer.
+  auto decoder_buffer = media::DecoderBuffer::CopyFrom(
+      static_cast<uint8_t*>(request->chunk->data()->Data()),
+      request->chunk->data()->ByteLengthAsSizeT());
+  decoder_buffer->set_timestamp(
+      base::TimeDelta::FromMicroseconds(request->chunk->timestamp()));
+  // TODO(sandersd): Use kUnknownTimestamp instead of 0?
+  decoder_buffer->set_duration(base::TimeDelta::FromMicroseconds(
+      request->chunk->duration().value_or(0)));
+  decoder_buffer->set_is_key_frame(request->chunk->type() == "key");
+
+  // Submit for decoding.
+  // |pending_decode_id_| must not be zero because it is used as a key in a
+  // HeapHashMap (pending_decodes_).
+  while (++pending_decode_id_ == 0 ||
+         pending_decodes_.Contains(pending_decode_id_))
+    ;
+  pending_decodes_.Set(pending_decode_id_, request);
+  --requested_decodes_;
+  decoder_->Decode(std::move(decoder_buffer),
+                   WTF::Bind(&VideoDecoder::OnDecodeDone,
+                             WrapWeakPersistent(this), pending_decode_id_));
+  return true;
+}
+
+bool VideoDecoder::ProcessFlushRequest(Request* request) {
+  DVLOG(3) << __func__;
+  DCHECK(!pending_request_);
+  DCHECK_EQ(request->type, Request::Type::kFlush);
+
+  // TODO(sandersd): If a reset has been requested, resolve immediately.
+
+  if (!decoder_) {
+    // TODO(sandersd): Maybe it is valid to flush no decoder? If not, it may be
+    // necessary to enter a full error state here.
+    request->resolver.Release()->Reject();
+    return true;
+  }
+
+  if (pending_decodes_.size() + 1 > size_t{decoder_->GetMaxDecodeRequests()}) {
+    // Try again after OnDecodeDone().
+    return false;
+  }
+
+  // Processing continues in OnFlushDone().
+  pending_request_ = request;
+  decoder_->Decode(
+      media::DecoderBuffer::CreateEOSBuffer(),
+      WTF::Bind(&VideoDecoder::OnFlushDone, WrapWeakPersistent(this)));
+  return true;
+}
+
+bool VideoDecoder::ProcessResetRequest(Request* request) {
+  DVLOG(3) << __func__;
+  DCHECK(!pending_request_);
+  DCHECK_EQ(request->type, Request::Type::kReset);
+  DCHECK_GT(requested_resets_, 0);
+
+  // Processing continues in OnResetDone().
+  pending_request_ = request;
+  --requested_resets_;
+  decoder_->Reset(
+      WTF::Bind(&VideoDecoder::OnResetDone, WrapWeakPersistent(this)));
+  return true;
+}
+
+void VideoDecoder::HandleError() {
+  // TODO(sandersd): Reject outstanding requests. We can stop rejeting at a
+  // decode(keyframe), reset(), or configure(), but maybe we should reject
+  // everything already queued (an implicit reset).
+  NOTIMPLEMENTED();
+}
 
 void VideoDecoder::OnInitializeDone(media::Status status) {
   DVLOG(3) << __func__;
-
-  Request* request = requests_.TakeFirst();
-  DCHECK_EQ(request->type, Request::Type::kConfigure);
+  DCHECK(pending_request_);
+  DCHECK_EQ(pending_request_->type, Request::Type::kConfigure);
 
   if (!status.is_ok()) {
     // TODO(tmathmeyer) this drops the media error - should we consider logging
     // it or converting it to the DOMException type somehow?
-    request->resolver.Release()->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kNotSupportedError, "Codec initialization failed."));
+    pending_request_.Release()->resolver.Release()->Reject(
+        MakeGarbageCollected<DOMException>(DOMExceptionCode::kNotSupportedError,
+                                           "Codec initialization failed."));
     HandleError();
     return;
   }
 
-  request->resolver.Release()->Resolve();
+  pending_request_.Release()->resolver.Release()->Resolve();
   ProcessRequests();
 }
 
@@ -125,7 +283,7 @@ void VideoDecoder::OnDecodeDone(uint32_t id, media::DecodeStatus status) {
   DCHECK(pending_decodes_.Contains(id));
 
   if (status != media::DecodeStatus::OK) {
-    // TODO(sandersd): Handle ABORTED during Reset.
+    // TODO(sandersd): Handle ABORTED.
     HandleError();
     return;
   }
@@ -136,15 +294,58 @@ void VideoDecoder::OnDecodeDone(uint32_t id, media::DecodeStatus status) {
   ProcessRequests();
 }
 
+void VideoDecoder::OnFlushDone(media::DecodeStatus status) {
+  DVLOG(3) << __func__;
+  DCHECK(pending_request_);
+  DCHECK_EQ(pending_request_->type, Request::Type::kFlush);
+
+  if (status != media::DecodeStatus::OK) {
+    HandleError();
+    return;
+  }
+
+  pending_request_.Release()->resolver.Release()->Resolve();
+  ProcessRequests();
+}
+
+void VideoDecoder::OnConfigureFlushDone(media::DecodeStatus status) {
+  DVLOG(3) << __func__;
+  DCHECK(pending_request_);
+  DCHECK_EQ(pending_request_->type, Request::Type::kConfigure);
+
+  if (status != media::DecodeStatus::OK) {
+    HandleError();
+    return;
+  }
+
+  // TODO(sandersd): Actually reconfigure the decoder.
+  // Processing continues in OnInitializeDone().
+  NOTIMPLEMENTED();
+
+  ProcessRequests();
+}
+
+void VideoDecoder::OnResetDone() {
+  DVLOG(3) << __func__;
+  DCHECK(pending_request_);
+  DCHECK_EQ(pending_request_->type, Request::Type::kReset);
+
+  pending_request_.Release()->resolver.Release()->Resolve();
+  ProcessRequests();
+}
+
 void VideoDecoder::OnOutput(scoped_refptr<media::VideoFrame> frame) {
   DVLOG(3) << __func__;
-  // TODO(sandersd): Call output callback.
-  // MakeGarbageCollected<VideoFrame>(frame)
+  output_cb_->InvokeAndReportException(nullptr,
+                                       MakeGarbageCollected<VideoFrame>(frame));
 }
 
 void VideoDecoder::Trace(Visitor* visitor) {
   visitor->Trace(script_state_);
+  visitor->Trace(output_cb_);
+  visitor->Trace(error_cb_);
   visitor->Trace(requests_);
+  visitor->Trace(pending_request_);
   visitor->Trace(pending_decodes_);
   ScriptWrappable::Trace(visitor);
 }
