93a541d989151003ff9ec4a451159b584da114af
Mitsuru Oshima <oshima@chromium.org>
2020-05-06 09:05:48 +0000

    RenderPass reason in layer dump
    Bug: None
    Change-Id: Ibeb4b55dd04fc11a7ba8ed4564da3f49a6d0f0af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173665
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765918}

--- ui/compositor/debug_utils.cc
+++ ui/compositor/debug_utils.cc
@@ -67,6 +67,21 @@ void PrintLayerHierarchyImp(const Layer* layer,
   if (!layer->GetSubpixelOffset().IsZero())
     *out << " " << layer->GetSubpixelOffset().ToString();
 
+  const cc::Layer* cc_layer = layer->cc_layer_for_testing();
+  if (cc_layer) {
+    // Property trees must be updated in order to get valid render surface
+    // reasons.
+    if (cc_layer->layer_tree_host() &&
+        !cc_layer->layer_tree_host()->property_trees()->needs_rebuild) {
+      cc::RenderSurfaceReason render_surface =
+          cc_layer->GetRenderSurfaceReason();
+      if (render_surface != cc::RenderSurfaceReason::kNone) {
+        *out << " render-surface-reason: "
+             << cc::RenderSurfaceReasonToString(render_surface);
+      }
+    }
+  }
+
   const ui::Layer* mask = const_cast<ui::Layer*>(layer)->layer_mask_layer();
 
   if (mask) {
