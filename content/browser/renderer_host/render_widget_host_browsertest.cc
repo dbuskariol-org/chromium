ed1447725ad712a9305a934c88743bd8e0329364
Ilya Nikolaevskiy <ilnik@chromium.org>
2020-05-06 13:18:18 +0000

    Cleanup RenderWidgetHostBrowserTest
    Previous CL introduced unneeded include.
    
    Bug: 1060795
    Change-Id: Id939a9b3ce8a21eea149dd59045283eb86d3e161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184250
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765968}

--- content/browser/renderer_host/render_widget_host_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_browsertest.cc
@@ -6,7 +6,6 @@
 
 #include "base/bind.h"
 #include "base/run_loop.h"
-#include "base/strings/stringprintf.h"
 #include "base/test/bind_test_util.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
