5b86c41c7368c8b060cc94b36b228cc8f5e77bb8
Rachel Carpenter <carpenterr@chromium.org>
2020-05-07 03:17:05 +0000

    Add category_tag to our feedback reports.
    This ensures it shows up in the right place.
    String from http://screen/NuJk06Lthei.
    
    Bug: b/155923758,1078651
    Change-Id: Ie40b65caee930eb40a68c98998aa36b451ca8ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186609
    Commit-Queue: Rachel Carpenter <carpenterr@google.com>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766281}

--- chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
+++ chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
@@ -29,15 +29,15 @@ ChromeHelpAppUIDelegate::ChromeHelpAppUIDelegate(content::WebUI* web_ui)
 
 base::Optional<std::string> ChromeHelpAppUIDelegate::OpenFeedbackDialog() {
   Profile* profile = Profile::FromWebUI(web_ui_);
-  // TODO(crbug/1045222): Additional strings are blank right now while we decide
-  // on the language and relevant information we want feedback to include.
-  // Note that category_tag is the name of the listnr bucket we want our
-  // reports to end up in. I.e DESKTOP_TAB_GROUPS.
-  chrome::ShowFeedbackPage(
-      GURL(chromeos::kChromeUIHelpAppURL), profile,
-      chrome::kFeedbackSourceHelpApp, std::string() /* description_template */,
-      std::string() /* description_placeholder_text */,
-      std::string() /* category_tag */, std::string() /* extra_diagnostics */);
+  constexpr char kHelpAppFeedbackCategoryTag[] = "FromHelpApp";
+  // We don't change the default description, or add extra diagnostics so those
+  // are empty strings.
+  chrome::ShowFeedbackPage(GURL(chromeos::kChromeUIHelpAppURL), profile,
+                           chrome::kFeedbackSourceHelpApp,
+                           std::string() /* description_template */,
+                           std::string() /* description_placeholder_text */,
+                           kHelpAppFeedbackCategoryTag /* category_tag */,
+                           std::string() /* extra_diagnostics */);
   return base::nullopt;
 }
 
