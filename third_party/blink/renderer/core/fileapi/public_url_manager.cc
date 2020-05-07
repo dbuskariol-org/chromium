f37090eb10054599b4990c1b52cd976feb75f7d7
Johann <johannkoenig@google.com>
2020-05-07 00:31:56 +0000

    Add AssociatedRemote wrapper and migrate PublicURLManager
    Copy the HeapMojoRemote wrapper class and make some small
    changes to support AssociatedRemote.
    
    Migrate PublicURLManager to the HeapMojo wrapper so that
    Oilpan will manage the lifetime of the objects and garbage
    collect them.
    
    BUG=chromium:1049056
    
    Change-Id: I2d27a6b3b3f331f55d1c7da7eba5a39f70b4c00e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162520
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#766218}

--- third_party/blink/renderer/core/fileapi/public_url_manager.cc
+++ third_party/blink/renderer/core/fileapi/public_url_manager.cc
@@ -36,6 +36,7 @@
 #include "third_party/blink/renderer/platform/blob/blob_url.h"
 #include "third_party/blink/renderer/platform/blob/blob_url_null_origin_map.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/task_type_names.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -53,7 +54,9 @@ static void RemoveFromNullOriginMapIfNecessary(const KURL& blob_url) {
 }  // namespace
 
 PublicURLManager::PublicURLManager(ExecutionContext* context)
-    : ExecutionContextLifecycleObserver(context), is_stopped_(false) {}
+    : ExecutionContextLifecycleObserver(context),
+      is_stopped_(false),
+      url_store_(context) {}
 
 String PublicURLManager::RegisterURL(URLRegistrable* registrable) {
   if (is_stopped_)
@@ -67,9 +70,11 @@ String PublicURLManager::RegisterURL(URLRegistrable* registrable) {
   if (registrable->IsMojoBlob()) {
     // Measure how much jank the following synchronous IPC introduces.
     SCOPED_UMA_HISTOGRAM_TIMER("Storage.Blob.RegisterPublicURLTime");
-    if (!url_store_) {
+    if (!url_store_.is_bound()) {
       BlobDataHandle::GetBlobRegistry()->URLStoreForOrigin(
-          origin, url_store_.BindNewEndpointAndPassReceiver());
+          origin,
+          url_store_.BindNewEndpointAndPassReceiver(
+              GetExecutionContext()->GetTaskRunner(TaskType::kFileReading)));
     }
     mojo::PendingRemote<mojom::blink::Blob> blob_remote;
     mojo::PendingReceiver<mojom::blink::Blob> blob_receiver =
@@ -100,10 +105,11 @@ void PublicURLManager::Revoke(const KURL& url) {
           GetExecutionContext()->GetSecurityOrigin()))
     return;
 
-  if (!url_store_) {
+  if (!url_store_.is_bound()) {
     BlobDataHandle::GetBlobRegistry()->URLStoreForOrigin(
         GetExecutionContext()->GetSecurityOrigin(),
-        url_store_.BindNewEndpointAndPassReceiver());
+        url_store_.BindNewEndpointAndPassReceiver(
+            GetExecutionContext()->GetTaskRunner(TaskType::kFileReading)));
   }
   url_store_->Revoke(url);
   mojo_urls_.erase(url.GetString());
@@ -124,10 +130,11 @@ void PublicURLManager::Resolve(
     return;
 
   DCHECK(url.ProtocolIs("blob"));
-  if (!url_store_) {
+  if (!url_store_.is_bound()) {
     BlobDataHandle::GetBlobRegistry()->URLStoreForOrigin(
         GetExecutionContext()->GetSecurityOrigin(),
-        url_store_.BindNewEndpointAndPassReceiver());
+        url_store_.BindNewEndpointAndPassReceiver(
+            GetExecutionContext()->GetTaskRunner(TaskType::kFileReading)));
   }
   url_store_->ResolveAsURLLoaderFactory(url, std::move(factory_receiver));
 }
@@ -139,10 +146,11 @@ void PublicURLManager::Resolve(
     return;
 
   DCHECK(url.ProtocolIs("blob"));
-  if (!url_store_) {
+  if (!url_store_.is_bound()) {
     BlobDataHandle::GetBlobRegistry()->URLStoreForOrigin(
         GetExecutionContext()->GetSecurityOrigin(),
-        url_store_.BindNewEndpointAndPassReceiver());
+        url_store_.BindNewEndpointAndPassReceiver(
+            GetExecutionContext()->GetTaskRunner(TaskType::kFileReading)));
   }
   url_store_->ResolveForNavigation(url, std::move(token_receiver));
 }
@@ -161,11 +169,10 @@ void PublicURLManager::ContextDestroyed() {
 
   url_to_registry_.clear();
   mojo_urls_.clear();
-
-  url_store_.reset();
 }
 
 void PublicURLManager::Trace(Visitor* visitor) {
+  visitor->Trace(url_store_);
   ExecutionContextLifecycleObserver::Trace(visitor);
 }
 
