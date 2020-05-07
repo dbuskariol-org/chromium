98bc8544b4694d065915a06e9102992461cbaa1d
Jazz Xu <jazzhsu@chromium.org>
2020-05-07 18:40:38 +0000

    GMC: Relocate enter_pip icon and exit_pip icon.
    Relocate enter_pip and exit_pip icon to
    components/media_message_center/vector_icons/
    since these two icons are not generic icons from
    material.io/resources/icons.
    
    Bug: 1047479
    Change-Id: I1d93e117720701854fe4bbbe822596e49f8213da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183092
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766509}

--- components/media_message_center/media_notification_view_impl.cc
+++ components/media_message_center/media_notification_view_impl.cc
@@ -11,6 +11,7 @@
 #include "components/media_message_center/media_notification_container.h"
 #include "components/media_message_center/media_notification_item.h"
 #include "components/media_message_center/media_notification_util.h"
+#include "components/media_message_center/vector_icons/vector_icons.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "services/media_session/public/mojom/media_session.mojom.h"
@@ -74,9 +75,9 @@ const gfx::VectorIcon* GetVectorIconForMediaAction(MediaSessionAction action) {
     case MediaSessionAction::kNextTrack:
       return &vector_icons::kMediaNextTrackIcon;
     case MediaSessionAction::kEnterPictureInPicture:
-      return &vector_icons::kMediaEnterPipIcon;
+      return &kMediaEnterPipIcon;
     case MediaSessionAction::kExitPictureInPicture:
-      return &vector_icons::kMediaExitPipIcon;
+      return &kMediaExitPipIcon;
     case MediaSessionAction::kStop:
     case MediaSessionAction::kSkipAd:
     case MediaSessionAction::kSeekTo:
