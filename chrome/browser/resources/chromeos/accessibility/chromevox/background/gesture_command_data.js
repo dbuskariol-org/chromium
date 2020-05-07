c7f5aa2c6a2f672f48be36648362edc964a6d7e4
Marijn Kruisselbrink <mek@chromium.org>
2020-05-07 22:32:32 +0000

    Revert "Fixes regressions with ChromeVox Panel + gestures"
    This reverts commit c92ed45796d1efedd8ca81ab09923b9940fb2a17.
    
    Reason for revert: ChromeVoxPanelTest.Gestures is flaky, failing about 1 in every 5 builds on https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    
    For example in https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/5439
    
    Original change's description:
    > Fixes regressions with ChromeVox Panel + gestures
    > 
    > - message strings for most gestures were removed mistakenly
    > - PanelMenuItem early returned before setting |this.enabled_| when using gestures, so the menus don't actually work
    > 
    > R=​akihiroota@chromium.org
    > 
    > Test: added ChromeVoxPanelTest.Gestures, which should prevent further regressions.
    > Change-Id: I7dd5c3a30d796ad39bb6a58b1e04b2d11ef78395
    > AX-Relnotes: fixes the ChromeVox Menus when using touch. Enter the menus with a four finger tap, swipe left/right to move among the search box, and the top level menus. Swipe up or down to move within each menu.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182739
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#766104}
    
    TBR=dtseng@chromium.org,akihiroota@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I3e945597690ac319f28ef3f282a738fd45864342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188771
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766613}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/gesture_command_data.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/gesture_command_data.js
@@ -21,37 +21,37 @@ goog.provide('GestureGranularity');
 GestureCommandData.GESTURE_COMMAND_MAP = {
   'click': {command: 'forceClickOnCurrentItem', msgId: 'click_gesture'},
   'swipeUp1': {
-    msgId: 'swipeup1_gesture',
+    msgId: 'swipeUp1_gesture',
     command: 'previousAtGranularity',
     menuKeyOverride: {keyCode: 38 /* up */},
     shouldRecoverRange: true
   },
   'swipeDown1': {
-    msgId: 'swipedown1_gesture',
+    msgId: 'swipeDown1_gesture',
     command: 'nextAtGranularity',
     menuKeyOverride: {keyCode: 40 /* Down */},
     shouldRecoverRange: true
   },
   'swipeLeft1': {
-    msgId: 'swipeleft1_gesture',
+    msgId: 'swipeLeft1_gesture',
     command: 'previousObject',
     menuKeyOverride: {keyCode: 37 /* left */},
     shouldRecoverRange: true
   },
   'swipeRight1': {
-    msgId: 'swiperight1_gesture',
+    msgId: 'swipeRight1_gesture',
     command: 'nextObject',
     menuKeyOverride: {keyCode: 39 /* right */},
     shouldRecoverRange: true
   },
-  'swipeUp2': {msgId: 'swipeup2_gesture', command: 'jumpToTop'},
-  'swipeDown2': {msgId: 'swipedown2_gesture', command: 'readFromHere'},
-  'swipeLeft2': {msgId: 'swipeleft2_gesture', command: 'previousWord'},
-  'swipeRight2': {msgId: 'swiperight2_gesture', command: 'nextWord'},
-  'swipeUp3': {msgId: 'swipeup3_gesture', command: 'scrollForward'},
-  'swipeDown3': {msgId: 'swipedown3_gesture', command: 'scrollBackward'},
-  'swipeLeft3': {msgId: 'swipeleft3_gesture', command: 'previousGranularity'},
-  'swipeRight3': {msgId: 'swiperight3_gesture', command: 'nextGranularity'},
+  'swipeUp2': {msgId: 'swipeUp2_gesture', command: 'jumpToTop'},
+  'swipeDown2': {msgId: 'swipeDown2_gesture', command: 'readFromHere'},
+  'swipeLeft2': {msgId: 'swipeLeft2_gesture', command: 'previousWord'},
+  'swipeRight2': {msgId: 'swipeRight2_gesture', command: 'nextWord'},
+  'swipeUp3': {msgId: 'swipeUp3_gesture', command: 'scrollForward'},
+  'swipeDown3': {msgId: 'swipeDown3_gesture', command: 'scrollBackward'},
+  'swipeLeft3': {msgId: 'swipeLeft3_gesture', command: 'previousGranularity'},
+  'swipeRight3': {msgId: 'swipeRight3_gesture', command: 'nextGranularity'},
 
   'tap2': {msgId: 'tap2_gesture', command: 'stopSpeech'},
   'tap4': {msgId: 'tap4_gesture', command: 'showPanelMenuMostRecent'},
