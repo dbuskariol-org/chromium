ecefc59fa42232ecb8a2aa941f2f3041f8cb1f69
Jared Saul <jsaul@google.com>
2020-05-12 03:18:01 +0000

    [AF] Enable AutofillUpstreamEditableExpirationDate by default
    Change-Id: I1115f0c5c6145d171d330b0352b07665b0d18244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195095
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#767653}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -105,14 +105,8 @@ const base::Feature kAutofillUpstream{"AutofillUpstream",
 const base::Feature kAutofillUpstreamAllowAllEmailDomains{
     "AutofillUpstreamAllowAllEmailDomains", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kAutofillUpstreamEditableExpirationDate {
-  "AutofillUpstreamEditableExpirationDate",
-#if defined(OS_ANDROID)
-      base::FEATURE_ENABLED_BY_DEFAULT
-#else
-      base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kAutofillUpstreamEditableExpirationDate{
+    "AutofillUpstreamEditableExpirationDate", base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool ShouldShowImprovedUserConsentForCreditCardSave() {
 #if defined(OS_WIN) || defined(OS_MACOSX) || \
