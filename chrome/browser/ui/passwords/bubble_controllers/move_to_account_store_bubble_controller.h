0b6b83ece1ede7533e8117c0cb14a287367547b2
Friedrich Horschig <fhorschig@chromium.org>
2020-05-12 16:31:17 +0000

    [Passwords] Retrieve profile icon in move password controller
    To indicate which account the password is moved to, the controller now
    provides a resized, round account image for the current primary account.
    
    Bug: 1060128
    Change-Id: Ifaf2b38f75ab5e87f66f086db4a0d46c44303ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196392
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767828}

--- chrome/browser/ui/passwords/bubble_controllers/move_to_account_store_bubble_controller.h
+++ chrome/browser/ui/passwords/bubble_controllers/move_to_account_store_bubble_controller.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_UI_PASSWORDS_BUBBLE_CONTROLLERS_MOVE_TO_ACCOUNT_STORE_BUBBLE_CONTROLLER_H_
 
 #include "chrome/browser/ui/passwords/bubble_controllers/password_bubble_controller_base.h"
+#include "ui/gfx/image/image.h"
 
 class PasswordsModelDelegate;
 
@@ -19,6 +20,9 @@ class MoveToAccountStoreBubbleController : public PasswordBubbleControllerBase {
 
   // Called by the view when the user clicks the confirmation button.
   void AcceptMove();
+
+  // Returns either a large site icon or a fallback icon.
+  gfx::Image GetProfileIcon();
 
  private:
   // PasswordBubbleControllerBase:
