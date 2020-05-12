c13e52bd71918250b9e306970e11b93dbef27945
Connie Wan <connily@chromium.org>
2020-05-12 00:15:35 +0000

    Revert "DumpAccTree formatter prefiltering: prototype."
    This reverts commit 82a2dcc1ad2ae354bf3f606649f8d22e1a397cf5.
    
    Reason for revert: Reverting CLs in this chain to address test failure, see crbug.com/1081512
    
    Original change's description:
    > DumpAccTree formatter prefiltering: prototype.
    > 
    > AX-Relnotes: n/a
    > Bug: 1076955
    > Change-Id: I5298bdc60f68ae834998ba489ac9115806eaab69
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2177151
    > Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#767505}
    
    TBR=dmazzoni@chromium.org,asurkov@igalia.com
    
    Change-Id: I59edad3f794ad8b6ee4727a33fd47c4eb373538a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1076955,1081512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195328
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767585}

--- content/browser/accessibility/accessibility_tree_formatter_mac.mm
+++ content/browser/accessibility/accessibility_tree_formatter_mac.mm
@@ -210,8 +210,8 @@ AccessibilityTreeFormatterMac::~AccessibilityTreeFormatterMac() {}
 void AccessibilityTreeFormatterMac::AddDefaultFilters(
     std::vector<PropertyFilter>* property_filters) {
   static NSArray* default_attributes = [@[
-    @"AXAutocompleteValue=*", @"AXDescription=*", @"AXRole=*", @"AXTitle=*",
-    @"AXTitleUIElement=*", @"AXHelp=*", @"AXValue=*"
+    @"AXAutocomplete*", @"AXDescription=*", @"AXTitle=*", @"AXTitleUIElement=*",
+    @"AXHelp=*", @"AXValue=*"
   ] retain];
 
   for (NSString* attribute : default_attributes) {
@@ -278,11 +278,9 @@ void AccessibilityTreeFormatterMac::AddProperties(
 
   for (NSString* supportedAttribute in
        [cocoa_node accessibilityAttributeNames]) {
-    if (FilterPropertyName(SysNSStringToUTF16(supportedAttribute))) {
-      id value = [cocoa_node accessibilityAttributeValue:supportedAttribute];
-      if (value != nil) {
-        dict->Set(SysNSStringToUTF8(supportedAttribute), PopulateObject(value));
-      }
+    id value = [cocoa_node accessibilityAttributeValue:supportedAttribute];
+    if (value != nil) {
+      dict->Set(SysNSStringToUTF8(supportedAttribute), PopulateObject(value));
     }
   }
   dict->Set(kPositionDictAttr, PopulatePosition(*node));
