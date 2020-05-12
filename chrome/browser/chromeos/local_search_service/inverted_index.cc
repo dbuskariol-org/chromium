75e91aa1618cdba6ae3e6a9f3496fd97bbfddcc6
Thanh Nguyen <thanhdng@chromium.org>
2020-05-12 02:54:34 +0000

    [local-search-service] Add inverted index for local search service
    This CL adds (some functions of) a inverted index for local search
    service. More functions will be added in other CLs.
    
    Bug: 1080427
    Change-Id: I03aa202cc0b36704091e1b2e11aefdcedc66fe93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189830
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767648}

---
+++ chrome/browser/chromeos/local_search_service/inverted_index.cc
@@ -0,0 +1,62 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/local_search_service/inverted_index.h"
+
+#include <string>
+#include <vector>
+
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
+
+namespace local_search_service {
+TokenPosition::TokenPosition(const std::string& id,
+                             uint32_t start_value,
+                             uint32_t length_value)
+    : content_id(id), start(start_value), length(length_value) {}
+
+Token::Token() = default;
+Token::Token(const base::string16& text, const std::vector<TokenPosition>& pos)
+    : content(text), positions(pos) {}
+Token::Token(const Token& token)
+    : content(token.content), positions(token.positions) {}
+Token::~Token() = default;
+
+PostingList InvertedIndex::FindTerm(const base::string16& term) const {
+  if (dictionary_.find(term) != dictionary_.end())
+    return dictionary_.at(term);
+
+  PostingList empty_list;
+  return empty_list;
+}
+
+InvertedIndex::InvertedIndex() = default;
+InvertedIndex::~InvertedIndex() = default;
+
+void InvertedIndex::AddDocument(const std::string& document_id,
+                                const std::vector<Token>& tokens) {
+  // Removes document if it is already in the inverted index.
+  if (doc_id_list_.find(document_id) != doc_id_list_.end())
+    RemoveDocument(document_id);
+
+  for (const auto& token : tokens) {
+    dictionary_[token.content][document_id] = token.positions;
+  }
+}
+
+void InvertedIndex::RemoveDocument(const std::string& document_id) {
+  doc_id_list_.erase(document_id);
+
+  for (auto it = dictionary_.begin(); it != dictionary_.end();) {
+    it->second.erase(document_id);
+
+    // Removes term from the dictionary if its posting list is empty.
+    if (it->second.empty()) {
+      it = dictionary_.erase(it);
+    } else {
+      it++;
+    }
+  }
+}
+}  // namespace local_search_service
---
+++ chrome/browser/chromeos/local_search_service/inverted_index_unittest.cc
@@ -0,0 +1,164 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/local_search_service/inverted_index.h"
+
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace local_search_service {
+
+namespace {
+using testing::ElementsAre;
+}  // namespace
+
+class InvertedIndexTest : public ::testing::Test {
+ public:
+  InvertedIndexTest() = default;
+  void SetUp() override {
+    index_.doc_id_list_ = std::unordered_set<std::string>({"doc1", "doc2"});
+
+    index_.dictionary_[base::UTF8ToUTF16("A")] = PostingList(
+        {{"doc1",
+          Posting({TokenPosition("header", 1, 1), TokenPosition("header", 3, 1),
+                   TokenPosition("body", 5, 1), TokenPosition("body", 7, 1)})},
+         {"doc2",
+          Posting({TokenPosition("header", 2, 1), TokenPosition("header", 4, 1),
+                   TokenPosition("body", 6, 1),
+                   TokenPosition("body", 8, 1)})}});
+
+    index_.dictionary_[base::UTF8ToUTF16("B")] =
+        PostingList({{"doc1", Posting({TokenPosition("header", 2, 1),
+                                       TokenPosition("body", 4, 1),
+                                       TokenPosition("header", 6, 1),
+                                       TokenPosition("body", 8, 1)})}});
+
+    index_.dictionary_[base::UTF8ToUTF16("C")] =
+        PostingList({{"doc2", Posting({TokenPosition("header", 1, 1),
+                                       TokenPosition("body", 3, 1),
+                                       TokenPosition("header", 5, 1),
+                                       TokenPosition("body", 7, 1)})}});
+  }
+
+  PostingList FindTerm(const base::string16& term) {
+    return index_.FindTerm(term);
+  }
+
+  void AddDocument(const std::string& doc_id,
+                   const std::vector<Token>& tokens) {
+    index_.AddDocument(doc_id, tokens);
+  }
+
+  void RemoveDocument(const std::string& doc_id) {
+    index_.RemoveDocument(doc_id);
+  }
+
+  std::unordered_map<base::string16, PostingList> GetDictionary() {
+    return index_.dictionary_;
+  }
+
+  std::unordered_set<std::string> GetDocIdList() { return index_.doc_id_list_; }
+
+ private:
+  InvertedIndex index_;
+};
+
+TEST_F(InvertedIndexTest, FindTermTest) {
+  PostingList result = FindTerm(base::UTF8ToUTF16("A"));
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(2));
+  EXPECT_EQ(result["doc1"][0].start, static_cast<uint32_t>(1));
+  EXPECT_EQ(result["doc1"][1].start, static_cast<uint32_t>(3));
+  EXPECT_EQ(result["doc1"][2].start, static_cast<uint32_t>(5));
+  EXPECT_EQ(result["doc1"][3].start, static_cast<uint32_t>(7));
+
+  EXPECT_EQ(result["doc2"][0].start, static_cast<uint32_t>(2));
+  EXPECT_EQ(result["doc2"][1].start, static_cast<uint32_t>(4));
+  EXPECT_EQ(result["doc2"][2].start, static_cast<uint32_t>(6));
+  EXPECT_EQ(result["doc2"][3].start, static_cast<uint32_t>(8));
+}
+
+TEST_F(InvertedIndexTest, AddNewDocumentTest) {
+  const base::string16 a_utf16(base::UTF8ToUTF16("A"));
+  const base::string16 d_utf16(base::UTF8ToUTF16("D"));
+
+  AddDocument("doc3",
+              {{a_utf16, {{"header", 1, 1}, {"body", 2, 1}, {"header", 4, 1}}},
+               {d_utf16, {{"header", 3, 1}, {"body", 5, 1}}}});
+
+  // Find "A"
+  PostingList result = FindTerm(a_utf16);
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(3));
+  EXPECT_EQ(result["doc3"][0].start, static_cast<uint32_t>(1));
+  EXPECT_EQ(result["doc3"][1].start, static_cast<uint32_t>(2));
+  EXPECT_EQ(result["doc3"][2].start, static_cast<uint32_t>(4));
+
+  // Find "D"
+  result = FindTerm(d_utf16);
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(1));
+  EXPECT_EQ(result["doc3"][0].start, static_cast<uint32_t>(3));
+  EXPECT_EQ(result["doc3"][1].start, static_cast<uint32_t>(5));
+}
+
+TEST_F(InvertedIndexTest, ReplaceDocumentTest) {
+  const base::string16 a_utf16(base::UTF8ToUTF16("A"));
+  const base::string16 d_utf16(base::UTF8ToUTF16("D"));
+
+  AddDocument("doc1",
+              {{a_utf16, {{"header", 1, 1}, {"body", 2, 1}, {"header", 4, 1}}},
+               {d_utf16, {{"header", 3, 1}, {"body", 5, 1}}}});
+
+  // Find "A"
+  PostingList result = FindTerm(a_utf16);
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(2));
+  EXPECT_EQ(result["doc1"][0].start, static_cast<uint32_t>(1));
+  EXPECT_EQ(result["doc1"][1].start, static_cast<uint32_t>(2));
+  EXPECT_EQ(result["doc1"][2].start, static_cast<uint32_t>(4));
+
+  // Find "B"
+  result = FindTerm(base::UTF8ToUTF16("B"));
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(0));
+
+  // Find "D"
+  result = FindTerm(d_utf16);
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(1));
+  EXPECT_EQ(result["doc1"][0].start, static_cast<uint32_t>(3));
+  EXPECT_EQ(result["doc1"][1].start, static_cast<uint32_t>(5));
+}
+
+TEST_F(InvertedIndexTest, RemoveDocumentTest) {
+  EXPECT_EQ(GetDictionary().size(), static_cast<unsigned long>(3));
+  EXPECT_EQ(GetDocIdList().size(), static_cast<unsigned long>(2));
+
+  RemoveDocument("doc1");
+  EXPECT_EQ(GetDictionary().size(), static_cast<unsigned long>(2));
+  EXPECT_EQ(GetDocIdList().size(), static_cast<unsigned long>(1));
+
+  // Find "A"
+  PostingList result = FindTerm(base::UTF8ToUTF16("A"));
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(1));
+  EXPECT_EQ(result["doc2"][0].start, static_cast<uint32_t>(2));
+  EXPECT_EQ(result["doc2"][1].start, static_cast<uint32_t>(4));
+  EXPECT_EQ(result["doc2"][2].start, static_cast<uint32_t>(6));
+  EXPECT_EQ(result["doc2"][3].start, static_cast<uint32_t>(8));
+
+  // Find "B"
+  result = FindTerm(base::UTF8ToUTF16("B"));
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(0));
+
+  // Find "C"
+  result = FindTerm(base::UTF8ToUTF16("C"));
+  ASSERT_EQ(result.size(), static_cast<unsigned long>(1));
+  EXPECT_EQ(result["doc2"][0].start, static_cast<uint32_t>(1));
+  EXPECT_EQ(result["doc2"][1].start, static_cast<uint32_t>(3));
+  EXPECT_EQ(result["doc2"][2].start, static_cast<uint32_t>(5));
+  EXPECT_EQ(result["doc2"][3].start, static_cast<uint32_t>(7));
+}
+}  // namespace local_search_service
