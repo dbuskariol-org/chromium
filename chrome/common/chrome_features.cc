caad93c58bd0b1f07d9be0683d98390506a2c18d
Takashi Toyoshima <toyoshim@chromium.org>
2020-05-12 01:54:32 +0000

    OOR-CORS: Hide OOR-CORS related enterprise policies by default
    This patch start hiding OOR-CORS related enterprise policies by default.
    These Feature flags and policies will be removed in m85 branch soon.
    
    Bug: 1053866
    Change-Id: I32314c5eb6bd8f307072a2022195c3a5b0668cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2193291
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767626}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -398,15 +398,14 @@ const base::Feature kHappinessTrackingSurveysForDesktopMigration{
 #endif  // !defined(OS_ANDROID)
 
 // Hide CorsLegacyModeEnabled enterprise policy support. This feature and
-// the enterprise policy supporting code will be removed soon around m83.
+// the enterprise policy supporting code will be removed at m84.
 const base::Feature kHideCorsLegacyModeEnabledPolicySupport{
-    "HideCorsLegacyModeEnabledPolicySupport",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    "HideCorsLegacyModeEnabledPolicySupport", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Hide CorsMitigationList enterprise policy support. This feature and
-// the enterprise policy supporting code will be removed soon around m83.
+// the enterprise policy supporting code will be removed at m84.
 const base::Feature kHideCorsMitigationListPolicySupport{
-    "HideCorsMitigationListPolicySupport", base::FEATURE_DISABLED_BY_DEFAULT};
+    "HideCorsMitigationListPolicySupport", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enables navigation suggestions UI for lookalike URLs (e.g. internationalized
 // domain names that are visually similar to popular domains or to domains with
