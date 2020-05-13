9f23a7db803910f8651fac39466900d023ff2322
James Vecore <vecore@google.com>
2020-05-13 01:01:22 +0000

    [Nearby] Create the Nearby Sharing keyed service and prefs.
    This keyed service will implement Nearby Sharing on top of the Nearby
    Connections mojo interface which runs in a utility process similar to
    the way the Android implementation is layered. This commit just adds a
    stubbed out service with a factory that uses the feature flag and user
    prefs to determine if the service should run. See go/nearby-chrome for
    the full design doc.
    
    Bug: b:156423106
    Change-Id: Ifb3b663a2f716ba2403576339a2618fd2d353c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154814
    Commit-Queue: James Vecore <vecore@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768078}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -44,6 +44,7 @@
 #include "chrome/browser/media/router/media_router_factory.h"
 #include "chrome/browser/media/webrtc/webrtc_event_log_manager_keyed_service_factory.h"
 #include "chrome/browser/media_galleries/media_galleries_preferences_factory.h"
+#include "chrome/browser/nearby_sharing/nearby_sharing_service_factory.h"
 #include "chrome/browser/notifications/notifier_state_tracker_factory.h"
 #include "chrome/browser/ntp_snippets/content_suggestions_service_factory.h"
 #include "chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_service_factory.h"
@@ -304,6 +305,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   metrics::DesktopProfileSessionDurationsServiceFactory::GetInstance();
 #endif
   ModelTypeStoreServiceFactory::GetInstance();
+#if !defined(OS_ANDROID)
+  NearbySharingServiceFactory::GetInstance();
+#endif
   NotifierStateTrackerFactory::GetInstance();
 #if !defined(OS_ANDROID)
   NTPResourceCacheFactory::GetInstance();
