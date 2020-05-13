e334ec41436b42ec62e31f85ad41a3343c6fad33
Matt Menke <mmenke@chromium.org>
2020-05-13 03:53:10 +0000

    Add histogram for usage of each WebRequestConditionAttribute::Type.
    We want to remove WebRequestConditionAttributeThirdParty, since it
    is implemented using a field we want to remove, for security reasons.
    We want to make sure its usage is low enough that we can remove it
    instead of reworking how it's implemented.
    
    Bug: 1076043
    Change-Id: I42b07226f21d2849763f43b773d4ef3931c37ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196843
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768129}

--- extensions/browser/api/declarative_webrequest/webrequest_condition_attribute.cc
+++ extensions/browser/api/declarative_webrequest/webrequest_condition_attribute.cc
@@ -13,6 +13,7 @@
 #include "base/lazy_instance.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
@@ -121,8 +122,15 @@ WebRequestConditionAttribute::Create(
     std::string* error) {
   CHECK(value != nullptr && error != nullptr);
   bool bad_message = false;
-  return g_web_request_condition_attribute_factory.Get().factory.Instantiate(
-      name, value, error, &bad_message);
+  auto condition_attribute =
+      g_web_request_condition_attribute_factory.Get().factory.Instantiate(
+          name, value, error, &bad_message);
+  if (condition_attribute) {
+    base::UmaHistogramEnumeration(
+        "Extensions.DeclarativeWebRequest.ConditionAttributeType",
+        condition_attribute->GetType(), static_cast<Type>(CONDITION_MAX + 1));
+  }
+  return condition_attribute;
 }
 
 //
