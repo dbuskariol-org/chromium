90339c619cfe2effedaa78c67a7932d4d9e8bd01
Dave Tapuska <dtapuska@chromium.org>
2020-05-13 14:27:31 +0000

    Move cc::OverscrollBehavior IPC struct traits over to CC.
    This will allow us to eventually remove the dependency of input on
    the content/renderer layer.
    
    BUG=1073560
    
    Change-Id: I25dff884da0d9bce2a27e0ede72ec2f0d0cd3ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196921
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768268}

--- content/common/input_messages.h
+++ content/common/input_messages.h
@@ -54,21 +54,12 @@ IPC_ENUM_TRAITS_MAX_VALUE(
     content::SyntheticPointerActionParams::Button::BUTTON_MAX)
 IPC_ENUM_TRAITS_MAX_VALUE(content::InputEventDispatchType,
                           content::InputEventDispatchType::DISPATCH_TYPE_MAX)
-IPC_ENUM_TRAITS_MAX_VALUE(
-    cc::OverscrollBehavior::OverscrollBehaviorType,
-    cc::OverscrollBehavior::OverscrollBehaviorType::kOverscrollBehaviorTypeMax)
-
 IPC_STRUCT_TRAITS_BEGIN(ui::DidOverscrollParams)
   IPC_STRUCT_TRAITS_MEMBER(accumulated_overscroll)
   IPC_STRUCT_TRAITS_MEMBER(latest_overscroll_delta)
   IPC_STRUCT_TRAITS_MEMBER(current_fling_velocity)
   IPC_STRUCT_TRAITS_MEMBER(causal_event_viewport_point)
   IPC_STRUCT_TRAITS_MEMBER(overscroll_behavior)
-IPC_STRUCT_TRAITS_END()
-
-IPC_STRUCT_TRAITS_BEGIN(cc::OverscrollBehavior)
-  IPC_STRUCT_TRAITS_MEMBER(x)
-  IPC_STRUCT_TRAITS_MEMBER(y)
 IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(content::SyntheticGestureParams)
