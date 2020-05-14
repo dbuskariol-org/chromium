bb2fd1e2a536a500c914ad4ce38204ea09da7157
Kyle Horimoto <khorimoto@chromium.org>
2020-05-14 18:14:02 +0000

    [CrOS Settings] Add getters for all sections/subpages/settings
    AllSections() was already used in a unit test, and the other two will be
    used in follow-up CLs.
    
    Bug: 1080892
    Change-Id: Ia0d7f25ad8eb90705491b5460b2de119af248de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199183
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768849}

--- chrome/browser/ui/webui/settings/chromeos/os_settings_manager_unittest.cc
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_manager_unittest.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_manager.h"
 
 #include "base/no_destructor.h"
+#include "chrome/browser/ui/webui/settings/chromeos/constants/constants_util.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_manager_factory.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_sections.h"
@@ -16,30 +17,6 @@
 
 namespace chromeos {
 namespace settings {
-namespace {
-
-const std::vector<mojom::Section>& AllSections() {
-  static const base::NoDestructor<std::vector<mojom::Section>> all_sections([] {
-    int32_t min_section_value = static_cast<int32_t>(mojom::Section::kMinValue);
-    int32_t max_section_value = static_cast<int32_t>(mojom::Section::kMaxValue);
-
-    std::vector<mojom::Section> all_sections;
-    for (int32_t i = min_section_value; i < max_section_value; ++i) {
-      mojom::Section section = static_cast<mojom::Section>(i);
-
-      // Not every value between the min and max values is a section, since some
-      // sections have been deprecated.
-      if (mojom::IsKnownEnumValue(section))
-        all_sections.push_back(section);
-    }
-
-    return all_sections;
-  }());
-
-  return *all_sections;
-}
-
-}  // namespace
 
 // Verifies the OsSettingsManager initialization flow. Behavioral functionality
 // is tested via unit tests on the sub-elements owned by OsSettingsManager.
@@ -66,7 +43,7 @@ class OsSettingsManagerTest : public testing::Test {
 TEST_F(OsSettingsManagerTest, Sections) {
   // For each mojom::Section value, there should be an associated
   // OsSettingsSection class registered.
-  for (const auto& section : AllSections()) {
+  for (const auto& section : constants::AllSections()) {
     EXPECT_TRUE(manager_->sections_->GetSection(section))
         << "No OsSettingsSection instance created for " << section << ".";
   }
