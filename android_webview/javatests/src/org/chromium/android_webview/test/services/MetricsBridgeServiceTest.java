dde46e6144af1a4281fee0f4884f5adb7d58b11d
Nate Fischer <ntfschr@chromium.org>
2020-05-14 20:41:27 +0000

    [AW][Dev-UI] simplify Service tests
    No change to production logic.
    
    This simplifies Service-related tests by adding ServiceConnectionHelper.
    This can be used in a try-with-resources pattern to temporarily bind to
    a Service, and automatically unbind when you're done.
    
    The main benefit is this moves assertions out of the
    onServiceConnected() callback, putting them back on the test thread (see
    http://go/no-asserts-on-main-thread). This also simplifies test logic by
    removing the need for helper methods and coordinating binding/unbinding
    across different helper method calls.
    
    Fixed: 1082017
    Test: run_webview_instrumentation_test_apk -f MetricsBridgeServiceTest.*
    Test: run_webview_instrumentation_test_apk -f VariationsSeedServerTest.*
    Change-Id: I6b87206c17fbbf1da845f3bb5dbec495e15eabf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197201
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768955}

--- android_webview/javatests/src/org/chromium/android_webview/test/services/MetricsBridgeServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/services/MetricsBridgeServiceTest.java
@@ -6,13 +6,9 @@ package org.chromium.android_webview.test.services;
 
 import static org.chromium.android_webview.test.OnlyRunIn.ProcessMode.SINGLE_PROCESS;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.ConditionVariable;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.test.filters.MediumTest;
 
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -45,8 +41,6 @@ import java.util.concurrent.FutureTask;
 @RunWith(AwJUnit4ClassRunner.class)
 @OnlyRunIn(SINGLE_PROCESS)
 public class MetricsBridgeServiceTest {
-    private static final long BINDER_TIMEOUT_MILLIS = 10000;
-
     private File mTempFile;
 
     @Before
@@ -154,7 +148,7 @@ public class MetricsBridgeServiceTest {
     @Test
     @MediumTest
     // Test sending data to the service and retrieving it back.
-    public void testRecordAndRetrieveNonembeddedMetrics() {
+    public void testRecordAndRetrieveNonembeddedMetrics() throws Throwable {
         HistogramRecord recordProto =
                 HistogramRecord.newBuilder()
                         .setRecordType(RecordType.HISTOGRAM_BOOLEAN)
@@ -164,18 +158,25 @@ public class MetricsBridgeServiceTest {
         HistogramRecordList expectedListProto =
                 HistogramRecordList.newBuilder().addRecords(recordProto).build();
 
-        testRecordMetrics(recordProto.toByteArray(), false);
-        byte[] retrievedData = testRetrieveNonembeddedMetrics(true);
+        Intent intent =
+                new Intent(ContextUtils.getApplicationContext(), MetricsBridgeService.class);
+        try (ServiceConnectionHelper helper =
+                        new ServiceConnectionHelper(intent, Context.BIND_AUTO_CREATE)) {
+            IMetricsBridgeService service =
+                    IMetricsBridgeService.Stub.asInterface(helper.getBinder());
+            service.recordMetrics(recordProto.toByteArray());
+            byte[] retrievedData = service.retrieveNonembeddedMetrics();
 
-        Assert.assertNotNull("retrieved byte data from the service is null", retrievedData);
-        Assert.assertArrayEquals("retrieved byte data is different from the expected data",
-                expectedListProto.toByteArray(), retrievedData);
+            Assert.assertNotNull("retrieved byte data from the service is null", retrievedData);
+            Assert.assertArrayEquals("retrieved byte data is different from the expected data",
+                    expectedListProto.toByteArray(), retrievedData);
+        }
     }
 
     @Test
     @MediumTest
     // Test sending data to the service and retrieving it back and make sure it's cleared.
-    public void testClearAfterRetrieveNonembeddedMetrics() {
+    public void testClearAfterRetrieveNonembeddedMetrics() throws Throwable {
         HistogramRecord recordProto =
                 HistogramRecord.newBuilder()
                         .setRecordType(RecordType.HISTOGRAM_BOOLEAN)
@@ -185,18 +186,25 @@ public class MetricsBridgeServiceTest {
         HistogramRecordList expectedListProto =
                 HistogramRecordList.newBuilder().addRecords(recordProto).build();
 
-        testRecordMetrics(recordProto.toByteArray(), false);
-        byte[] retrievedData = testRetrieveNonembeddedMetrics(false);
+        Intent intent =
+                new Intent(ContextUtils.getApplicationContext(), MetricsBridgeService.class);
+        try (ServiceConnectionHelper helper =
+                        new ServiceConnectionHelper(intent, Context.BIND_AUTO_CREATE)) {
+            IMetricsBridgeService service =
+                    IMetricsBridgeService.Stub.asInterface(helper.getBinder());
+            service.recordMetrics(recordProto.toByteArray());
+            byte[] retrievedData = service.retrieveNonembeddedMetrics();
 
-        Assert.assertNotNull("retrieved byte data from the service is null", retrievedData);
-        Assert.assertArrayEquals("retrieved byte data is different from the expected data",
-                expectedListProto.toByteArray(), retrievedData);
+            Assert.assertNotNull("retrieved byte data from the service is null", retrievedData);
+            Assert.assertArrayEquals("retrieved byte data is different from the expected data",
+                    expectedListProto.toByteArray(), retrievedData);
 
-        // Retrieve data a second time to make sure it has been cleared after the first call
-        retrievedData = testRetrieveNonembeddedMetrics(true);
+            // Retrieve data a second time to make sure it has been cleared after the first call
+            retrievedData = service.retrieveNonembeddedMetrics();
 
-        Assert.assertTrue(
-                "metrics kept by the service hasn't been cleared", retrievedData.length == 0);
+            Assert.assertTrue(
+                    "metrics kept by the service hasn't been cleared", retrievedData.length == 0);
+        }
     }
 
     private static void writeProtoToFile(HistogramRecordList recordList, File file)
@@ -221,88 +229,4 @@ public class MetricsBridgeServiceTest {
         return listBuilder.build();
     }
 
-    // Connect to MetricsBridgeService and call recordMetrics with the given data, assert that
-    // the method is called and returned.
-    private void testRecordMetrics(byte[] data, boolean unbind) {
-        final ConditionVariable recordMetricsCalled = new ConditionVariable();
-
-        ServiceConnection connection = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                try {
-                    IMetricsBridgeService.Stub.asInterface(service).recordMetrics(data);
-                } catch (RemoteException e) {
-                    Assert.fail("Faild recording a histogram: " + e.getMessage());
-                } finally {
-                    if (unbind) {
-                        ContextUtils.getApplicationContext().unbindService(this);
-                    }
-                    recordMetricsCalled.open();
-                }
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName name) {}
-        };
-
-        Intent intent =
-                new Intent(ContextUtils.getApplicationContext(), MetricsBridgeService.class);
-        Assert.assertTrue("Failed to bind to MetricsBridgeService",
-                ContextUtils.getApplicationContext().bindService(
-                        intent, connection, Context.BIND_AUTO_CREATE));
-        Assert.assertTrue("Timed out waiting for recordMetrics() to return",
-                recordMetricsCalled.block(BINDER_TIMEOUT_MILLIS));
-    }
-
-    /**
-     * A Service connection class that connect to MetricsBridgeService to retrieve recorded metrics
-     * data and provide a timed blocking call to access it.
-     */
-    private static class TestRetrieveConnection implements ServiceConnection {
-        private byte[] mData;
-        private final ConditionVariable mMethodCalled = new ConditionVariable();
-        private final boolean mUnbind;
-
-        public TestRetrieveConnection(boolean unbind) {
-            mUnbind = unbind;
-        }
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            try {
-                mData = IMetricsBridgeService.Stub.asInterface(service)
-                                .retrieveNonembeddedMetrics();
-                Assert.assertNotNull("retrieved byte data from the service is null", mData);
-            } catch (RemoteException e) {
-                Assert.fail("Faild retrieving metrics: " + e.getMessage());
-            } finally {
-                if (mUnbind) {
-                    ContextUtils.getApplicationContext().unbindService(this);
-                }
-                mMethodCalled.open();
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {}
-
-        // Block until the onServiceConnected is called and data is retrieveNonembeddedMetrics is
-        // called, fail otherwise.
-        public byte[] getData() {
-            Assert.assertTrue("Timed out waiting for retrieveNonembeddedMetrics() to return",
-                    mMethodCalled.block(BINDER_TIMEOUT_MILLIS));
-            return mData;
-        }
-    }
-
-    private byte[] testRetrieveNonembeddedMetrics(boolean unbind) {
-        TestRetrieveConnection connection = new TestRetrieveConnection(unbind);
-        Intent intent =
-                new Intent(ContextUtils.getApplicationContext(), MetricsBridgeService.class);
-        Assert.assertTrue("Failed to bind to MetricsBridgeService",
-                ContextUtils.getApplicationContext().bindService(
-                        intent, connection, Context.BIND_AUTO_CREATE));
-
-        return connection.getData();
-    }
 }
