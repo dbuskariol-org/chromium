35e6243478c524808755f5b5188c87829dfa91b6
Nate Chapin <japhet@chromium.org>
2020-05-15 16:28:48 +0000

    IdleSpellCheckController shouldn't crash when deactivating in an already-destroyed context
    Bug: 1082652
    Change-Id: I95bccb4380db482006e26b5f7f8b878ffa435a66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202160
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769302}

--- third_party/blink/renderer/core/editing/spellcheck/idle_spell_check_controller.cc
+++ third_party/blink/renderer/core/editing/spellcheck/idle_spell_check_controller.cc
@@ -102,6 +102,8 @@ void IdleSpellCheckController::DisposeIdleCallback() {
 }
 
 void IdleSpellCheckController::Deactivate() {
+  if (state_ == State::kInactive)
+    return;
   state_ = State::kInactive;
   if (cold_mode_timer_.IsActive())
     cold_mode_timer_.Cancel();
