19baa0f0ad8b8e03918b9d86de350eb8f378aa41
Glen Robertson <glenrob@chromium.org>
2020-05-15 02:44:05 +0000

    Implement capture of metrics events for web app daily interaction.
    Captures basic static information on all web apps used and more detailed
    sums of foreground/background duration and session count for installed
    web apps that are set to open in a window.
    
    WebAppMetrics now listens on BrowserList events and TabStripModel
    changes in order to determine when web apps move into the foreground or
    background. It keeps a record of the last time each web app was seen so
    that intervening time can be counted as foreground time if switching
    away from the web app (and background time if switching to the web app).
    
    Tbr: achuith@chromium.org
    Bug: 1026148
    Bug: 1081187
    Change-Id: Iae6460c678b59380cf0fad8408e2014918bc2864
    Fixed: 1026148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160638
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769125}

--- chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
@@ -281,7 +281,8 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
 
   EXPECT_EQ(
       result.app_banner_manager->GetInstallableWebAppCheckResultForTesting(),
-      banners::AppBannerManager::InstallableWebAppCheckResult::kNo);
+      banners::AppBannerManager::InstallableWebAppCheckResult::
+          kNoAlreadyInstalled);
   EXPECT_FALSE(pwa_install_view_->GetVisible());
 }
 
@@ -304,7 +305,8 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
   // The nested PWA should now not be installable.
   EXPECT_EQ(
       result.app_banner_manager->GetInstallableWebAppCheckResultForTesting(),
-      banners::AppBannerManager::InstallableWebAppCheckResult::kNo);
+      banners::AppBannerManager::InstallableWebAppCheckResult::
+          kNoAlreadyInstalled);
   EXPECT_FALSE(pwa_install_view_->GetVisible());
 }
 
