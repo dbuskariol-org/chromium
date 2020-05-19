615411c4d33d619317e6560cef245923c50c4cb0
Camille Lamy <clamy@chromium.org>
2020-05-19 09:14:54 +0000

    Revert "Invalidate tabstrip layout when the space available for tabs changes."
    This reverts commit 14dee39372c9a08c92c6c930f0eb1c2ab59bd228.
    
    Reason for revert: Tentative revert because the function introduced in this CL is showing up in uaf stack traces on many tests on the MSAN bots. See https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/23956 for example.
    
    Original change's description:
    > Invalidate tabstrip layout when the space available for tabs changes.
    > 
    > Bug: 1075913
    > Change-Id: I2d65a4dc4b4b112e367f216b4a71788bdf825bbe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2185430
    > Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769803}
    
    TBR=tbergquist@chromium.org,dfried@chromium.org
    
    Change-Id: Ia39ea9e4d912c9d1c74186f22e545b27b79abe27
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1075913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207320
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770048}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -2046,10 +2046,6 @@ SkColor TabStrip::GetPaintedGroupColor(
 // TabStrip, views::AccessiblePaneView overrides:
 
 void TabStrip::Layout() {
-  if (is_doing_layout_)
-    return;
-  base::AutoReset<bool> layout_guard(&is_doing_layout_, true);
-
   if (IsAnimating()) {
     // Hide tabs that have animated at least partially out of the clip region.
     SetTabVisibility();
@@ -2057,7 +2053,7 @@ void TabStrip::Layout() {
   }
 
   // Only do a layout if our size changed.
-  if (last_layout_size_ == size() && last_available_width_ != 0)
+  if (last_layout_size_ == size())
     return;
   if (drag_context_->IsDragSessionActive())
     return;
@@ -3166,22 +3162,14 @@ int TabStrip::UpdateIdealBoundsForPinnedTabs(int* first_non_pinned_index) {
   return layout_helper_->first_non_pinned_tab_x();
 }
 
-int TabStrip::GetAvailableWidthForTabstrip() {
+int TabStrip::CalculateAvailableWidthForTabs() {
   // Layout the parent so that GetAvailableSize is well-defined.
   parent()->Layout();
   base::Optional<int> available_width =
       parent()->GetAvailableSize(this).width();
-  // |available_width| might still be undefined in cases where the tabstrip is
-  // hidden (e.g. presentation mode on MacOS). In these cases we don't care
-  // about the resulting layout, since the tabstrip is not visible, so we can
-  // substitute 0 to ensure that we relayout once the width is defined again.
-  return available_width.value_or(0);
-}
-
-int TabStrip::CalculateAvailableWidthForTabs() {
-  last_available_width_ = GetAvailableWidthForTabstrip();
+  DCHECK(available_width.has_value());
   return override_available_width_for_tabs_.value_or(
-      last_available_width_ - GetRightSideReservedWidth());
+      available_width.value() - GetRightSideReservedWidth());
 }
 
 void TabStrip::StartResizeLayoutAnimation() {
