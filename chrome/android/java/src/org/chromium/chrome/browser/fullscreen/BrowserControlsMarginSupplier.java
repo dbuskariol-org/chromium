6a3ae7d6a61b568203cc2b6a7b90bc89ca39a33d
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-05-21 18:12:44 +0000

    [TabViewManager] Use ChromeFullScreenManager for providing margins
    Using TabBrowserControlsOffsetHelper for providing margins for
    browser controls cause problems for SadTab, since margins are reset
    in TabBrowserControlsOffsetHelper on crash.
    
    This CL uses ChromeFullscreenManager instead for providing the browser
    control margins.
    
    Bug: 1081628
    Change-Id: Ifa9a2687bc3726d851e739552f64d7f9111e6b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208921
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771120}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/BrowserControlsMarginSupplier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/BrowserControlsMarginSupplier.java
@@ -2,23 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.native_page;
+package org.chromium.chrome.browser.fullscreen;
 
 import android.graphics.Rect;
 
 import org.chromium.base.supplier.DestroyableObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
-import org.chromium.chrome.browser.fullscreen.BrowserControlsStateProvider;
 
 /**
  * An implementation of {@link DestroyableObservableSupplier} that monitors changes to browser
- * controls and updates top/bottom margins as needed.
+ * controls and updates a Rect indicating top/bottom margins for Views that should be inset by the
+ * browser control(s) height(s).
  */
-class BrowserControlsMarginSupplier extends ObservableSupplierImpl<Rect>
+public class BrowserControlsMarginSupplier extends ObservableSupplierImpl<Rect>
         implements BrowserControlsStateProvider.Observer, DestroyableObservableSupplier<Rect> {
     private final BrowserControlsStateProvider mBrowserControlsStateProvider;
 
-    BrowserControlsMarginSupplier(BrowserControlsStateProvider browserControlsStateProvider) {
+    public BrowserControlsMarginSupplier(
+            BrowserControlsStateProvider browserControlsStateProvider) {
         mBrowserControlsStateProvider = browserControlsStateProvider;
         mBrowserControlsStateProvider.addObserver(this);
         updateMargins();
