8d6554d3dc636b4b167569392d23f936c058e4f9
Minoru Chikamune <chikamune@chromium.org>
2020-05-21 22:55:11 +0000

    Migrate FileSystemDispatcher to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free
    bugs. This CL is followup CL. FileSystemDispatcher was already
    migrated before, but there were remaining mojo objects.
    
    Bug: 1049056
    Change-Id: Ib9933ee186ecca6d5cc73bf70a6d9a5ee429c010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211564
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771232}

--- third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
+++ third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
@@ -10,7 +10,9 @@
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/mojom/filesystem/file_system.mojom-blink.h"
 #include "third_party/blink/renderer/modules/filesystem/file_system_callbacks.h"
+#include "third_party/blink/renderer/platform/heap/disallow_new_wrapper.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/heap/heap_allocator.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_unique_receiver_set.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -201,7 +203,9 @@ class FileSystemDispatcher : public GarbageCollected<FileSystemDispatcher>,
 
   HeapMojoRemote<mojom::blink::FileSystemManager> file_system_manager_;
   using OperationsMap =
-      HashMap<int, mojo::Remote<mojom::blink::FileSystemCancellableOperation>>;
+      HeapHashMap<int,
+                  Member<DisallowNewWrapper<HeapMojoRemote<
+                      mojom::blink::FileSystemCancellableOperation>>>>;
   OperationsMap cancellable_operations_;
   int next_operation_id_;
   HeapMojoUniqueReceiverSet<mojom::blink::FileSystemOperationListener>
