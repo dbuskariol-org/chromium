142274a8ca777a4e6bdc93848ab21acc6d878386
Esmael El-Moslimany <aee@chromium.org>
2020-05-21 18:07:34 +0000

    WebUI NTP: redirect sign-in to chrome://newtab
    Bug: 1015293
    Change-Id: Ic76e7911403a925fa46a5b5fc58eca95fa292269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209392
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771116}

--- chrome/browser/signin/dice_browsertest.cc
+++ chrome/browser/signin/dice_browsertest.cc
@@ -934,7 +934,7 @@ IN_PROC_BROWSER_TEST_F(DiceBrowserTest, MAYBE_EnableSyncBeforeToken) {
             dice_request_header_);
 
   ui_test_utils::UrlLoadObserver ntp_url_observer(
-      GURL(chrome::kChromeSearchLocalNtpUrl),
+      GURL(chrome::kChromeUINewTabURL),
       content::NotificationService::AllSources());
 
   WaitForSigninSucceeded();
