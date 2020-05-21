32a324caef2279c71dd2809375cc65d4f6bbddbb
Mike Wittman <wittman@chromium.org>
2020-05-21 15:08:43 +0000

    Revert "[Sampling profiler] Provide unwinder in stack_unwinder DFM"
    This reverts commit 01b9624c25a61b61bd162515f7f6cdf4569aeb2a.
    
    Reason for revert: first of two reverts to fix https://ci.chromium.org/p/chromium/builders/try/android_compile_x64_dbg/1504
    
    Original change's description:
    > [Sampling profiler] Provide unwinder in stack_unwinder DFM
    > 
    > Implements the Android native/Java stack unwinder within the stack_unwinder
    > dynamic feature module.
    > 
    > Adds visibility/assert_no_deps constraints to //base/BUILD.gn to avoid
    > leaking module implementation types into general Chrome code, which
    > would defeat the purpose of using the module to avoid Chrome binary size
    > increase.
    > 
    > Adds required stub declarations/implementation for MemoryRegionsMap.
    > 
    > Bug: 1083530, 1004855
    > Change-Id: If9d31c4e1b9d214bb0baabe3afa8475ba7adef4a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209297
    > Commit-Queue: kylechar <kylechar@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#771018}
    
    TBR=wittman@chromium.org,kylechar@chromium.org,etiennep@chromium.org
    
    Change-Id: I992dcf2c36b13ef51d323a44fef840e1e04bdd7b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1083530, 1004855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211927
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771046}

--- chrome/android/features/stack_unwinder/public/memory_regions_map.h
+++ chrome/android/features/stack_unwinder/public/memory_regions_map.h
@@ -11,11 +11,7 @@ namespace stack_unwinder {
 // It must only be subclassed within the module implementation.
 class MemoryRegionsMap {
  public:
-  MemoryRegionsMap();
   virtual ~MemoryRegionsMap() = 0;
-
-  MemoryRegionsMap(const MemoryRegionsMap&) = delete;
-  MemoryRegionsMap& operator=(const MemoryRegionsMap&) = delete;
 };
 
 }  // namespace stack_unwinder
