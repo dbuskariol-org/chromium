ed3b88d964cc5803b40043fe5b28b7f1ec63cca8
Chris Harrelson <chrishtr@chromium.org>
2020-05-21 22:10:00 +0000

    Delete some dead code in PaintLayer
    Change-Id: If8f9a3498573cfd17e21dc8d0819a06c58579dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208099
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771213}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -613,9 +613,6 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
   static void MapPointInPaintInvalidationContainerToBacking(
       const LayoutBoxModelObject& paint_invalidation_container,
       PhysicalOffset&);
-  static void MapQuadInPaintInvalidationContainerToBacking(
-      const LayoutBoxModelObject& paint_invalidation_container,
-      FloatQuad&);
 
   bool PaintsWithTransparency(GlobalPaintFlags global_paint_flags) const {
     return IsTransparent() && !PaintsIntoOwnBacking(global_paint_flags);
