818139b053f21c1829d1ad85fbc6380b80dffe2c
Nate Fischer <ntfschr@chromium.org>
2020-05-22 23:30:26 +0000

    [AW][Dev-UI] log duration for user session
    No change to logic. This adds logs to capture the duration of the user's
    session with each tool. This captures each tool in a different
    histogram, measuring from onResume until onPause (so we intentionally
    don't include time when the app is in the background).
    
    (cherry picked from commit 2cebdc2954bbfceaf51dcd00fdf967697e6dfca3)
    
    Fixed: 1084177
    Test: Add a log statement and verify it fires as expected
    Change-Id: I9933a97c5c99d123440ed4e46b94bc056964ada3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211146
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#771143}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213737
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4147@{#200}
    Cr-Branched-From: 16307825352720ae04d898f37efa5449ad68b606-refs/heads/master@{#768962}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/DevUiBaseFragment.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/DevUiBaseFragment.java
@@ -3,12 +3,18 @@
 // found in the LICENSE file.
 package org.chromium.android_webview.devui;
 
+import android.os.SystemClock;
+
 import androidx.fragment.app.Fragment;
+
+import org.chromium.base.metrics.RecordHistogram;
 
 /**
  * A base class for all fragments in the UI.
  */
 public abstract class DevUiBaseFragment extends Fragment {
+    private long mStartOfSession;
+
     /**
      * Called by {@link MainActivity} to to give the fragment an opportunity to show an error
      * message. It's up to the {@link MainActivity} to call this or not.
@@ -18,5 +24,33 @@ public abstract class DevUiBaseFragment extends Fragment {
      */
     /* package */ void maybeShowErrorView(PersistentErrorView errorView) {
         errorView.hide();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mStartOfSession = SystemClock.elapsedRealtime();
+    }
+
+    private void logSessionDuration() {
+        String suffix = "Unknown";
+        if (this instanceof HomeFragment) {
+            suffix = "HomeFragment";
+        } else if (this instanceof FlagsFragment) {
+            suffix = "FlagsFragment";
+        } else if (this instanceof CrashesListFragment) {
+            suffix = "CrashesListFragment";
+        }
+        // Note: keep this if-else ladder synchronized with the AndroidWebViewFragments
+        // histogram_suffix
+        long endOfSession = SystemClock.elapsedRealtime();
+        RecordHistogram.recordTimesHistogram(
+                "Android.WebView.DevUi.SessionDuration." + suffix, endOfSession - mStartOfSession);
+    }
+
+    @Override
+    public void onPause() {
+        logSessionDuration();
+        super.onPause();
     }
 }
