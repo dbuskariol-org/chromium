d67cf55dbce5a552cbd2b278414d15f633314df3
Jochen Eisinger <jochen@chromium.org>
2020-05-25 16:31:14 +0000

    Enable IntentBlockExternalFormRedirectsNoGesture by default
    Bug: 927509
    Change-Id: I56ec8b5cf524a4e1d14b29427931802f80d54a3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132197
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771584}

--- components/external_intents/android/external_intents_feature_list.cc
+++ components/external_intents/android/external_intents_feature_list.cc
@@ -36,7 +36,7 @@ const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
 // Alphabetical:
 const base::Feature kIntentBlockExternalFormRedirectsNoGesture{
     "IntentBlockExternalFormRedirectsNoGesture",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 static jboolean JNI_ExternalIntentsFeatureList_IsEnabled(
     JNIEnv* env,
