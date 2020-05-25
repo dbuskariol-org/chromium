c70a60a209bf9baee8fea6d9807357034fd6dc5f
Alex Ilin <alexilin@chromium.org>
2020-05-25 13:25:43 +0000

    [signin] Update Reauth dialog frame color for the dark theme
    In the dark mode, the reauth dialog frame color should match the
    contents background color, as it does in the light mode.
    
    Bug: 1045515
    Change-Id: I701e8ece59c9a416f6fd4cf3e1c57e82e418c03e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210502
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771556}

--- chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
+++ chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
@@ -276,9 +276,12 @@ void SigninViewControllerDelegateViews::DisplayModal() {
       NOTREACHED() << "Unsupported dialog modal type " << dialog_modal_type_;
   }
   if (should_show_close_button_) {
+    SkColor border_color = GetNativeTheme()->ShouldUseDarkColors()
+                               ? gfx::kGoogleGrey900
+                               : SK_ColorWHITE;
     GetBubbleFrameView()->SetBubbleBorder(std::make_unique<views::BubbleBorder>(
         views::BubbleBorder::NONE, views::BubbleBorder::BIG_SHADOW,
-        SK_ColorWHITE));
+        border_color));
   }
 
   content_view_->RequestFocus();
