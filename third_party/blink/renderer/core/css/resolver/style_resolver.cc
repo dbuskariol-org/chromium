c3a8fff017b4b78399cce7553ea5a34d7ffb4a9e
Anders Hartvoll Ruud <andruud@chromium.org>
2020-05-29 11:31:21 +0000

    Perform monospace-related adjustment using true parent style
    There is currently some code in FontBuilder that tweaks the specified
    font size if the current generic font family changes to or from
    monospace. This code assumes that the FontDescription stored on the
    ComputedStyle pre-CreateFont is the inherited FontDescription, but
    this is no longer (always) true.
    
    This means that declarations like font-size:1em can be applied (and
    subjected to CreateFont) twice, which means that the second call to
    CheckForGenericFamilyChange won't detect a change in the
    monospace-ness, hence the specified size will fail to adjust.
    
    This CL fixes this by propagating the actual parent style to
    CheckForGenericFamilyChange.
    
    (cherry picked from commit ca7df536bf7bc572825fe4efba2766b34c092698)
    
    Bug: 1086082, 1086680
    Change-Id: Ia61a2327890f27497e4ea5f41dec00a5e0a450d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216511
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#772032}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220031
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4147@{#353}
    Cr-Branched-From: 16307825352720ae04d898f37efa5449ad68b606-refs/heads/master@{#768962}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1081,8 +1081,7 @@ CompositorKeyframeValue* StyleResolver::CreateCompositorKeyframeValueSnapshot(
       cascade.Apply();
     } else {
       StyleBuilder::ApplyProperty(property.GetCSSPropertyName(), state, *value);
-      state.GetFontBuilder().CreateFont(
-          state.StyleRef());
+      state.GetFontBuilder().CreateFont(state.StyleRef(), parent_style);
       CSSVariableResolver(state).ResolveVariableDefinitions();
     }
   }
@@ -1322,7 +1321,7 @@ scoped_refptr<const ComputedStyle> StyleResolver::StyleForText(
 }
 
 void StyleResolver::UpdateFont(StyleResolverState& state) {
-  state.GetFontBuilder().CreateFont(state.StyleRef());
+  state.GetFontBuilder().CreateFont(state.StyleRef(), state.ParentStyle());
   state.SetConversionFontSizes(CSSToLengthConversionData::FontSizes(
       state.Style(), state.RootElementStyle()));
   state.SetConversionZoom(state.Style()->EffectiveZoom());
