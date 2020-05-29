03302fa37ee9e0197bd26b9c6e7f52e02587b385
Minoru Chikamune <chikamune@chromium.org>
2020-05-29 01:03:51 +0000

    Remove an unnecessary prefinalizer from ImageDownloaderImpl.
    HeapMojo wrappers can reset the mojo objects automatically.
    So no need to reset mojo objects in prefinalizer again.
    
    Bug: 1049056
    Change-Id: Ib7276458a9edf9372f4d7cbae13cfa7ceae267ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2217720
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772982}

--- third_party/blink/renderer/modules/image_downloader/image_downloader_impl.h
+++ third_party/blink/renderer/modules/image_downloader/image_downloader_impl.h
@@ -24,7 +24,6 @@ class ImageDownloaderImpl final : public GarbageCollected<ImageDownloaderImpl>,
                                   public Supplement<LocalFrame>,
                                   public ExecutionContextLifecycleObserver,
                                   public mojom::blink::ImageDownloader {
-  USING_PRE_FINALIZER(ImageDownloaderImpl, Dispose);
   USING_GARBAGE_COLLECTED_MIXIN(ImageDownloaderImpl);
 
  public:
@@ -68,8 +67,6 @@ class ImageDownloaderImpl final : public GarbageCollected<ImageDownloaderImpl>,
   void CreateMojoService(
       mojo::PendingReceiver<mojom::blink::ImageDownloader> receiver);
 
-  // USING_PRE_FINALIZER interface.
-  // Called before the object gets garbage collected.
   void Dispose();
 
   // Requests to fetch an image. When done, the image downloader is notified by
