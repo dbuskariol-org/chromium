3cad6490b71789e780d2b3aec473e90ce793424e
Sinan Sahin <sinansahin@google.com>
2020-05-29 01:54:23 +0000

    [Offline indicator v2] Fix text fade in animation in fullscreen
    When the status indicator is shown in fullscreen mode, the text wouldn't
    fade-in. The animation would run, but the frames wouldn't be presented
    to the screen even though they are drawn.
    
    To work around that, this CL adds a #requestLayout() call at the start
    of the animation.
    
    (cherry picked from commit 8202db3f7b860ee46c0e00c93d0a991ba237c143)
    
    Bug: 1083461
    Change-Id: If816b2882b77e41a7b00d8b863548e8629aff248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216281
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#771944}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220696
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/branch-heads/4147@{#344}
    Cr-Branched-From: 16307825352720ae04d898f37efa5449ad68b606-refs/heads/master@{#768962}

--- chrome/android/java/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorMediator.java
@@ -39,6 +39,7 @@ class StatusIndicatorMediator
     private Runnable mOnCompositorShowAnimationEnd;
     private Supplier<Boolean> mCanAnimateNativeBrowserControls;
     private Callback<Runnable> mInvalidateCompositorView;
+    private Runnable mRequestLayout;
 
     private int mIndicatorHeight;
     private int mJavaLayoutHeight;
@@ -57,16 +58,18 @@ class StatusIndicatorMediator
      *                                        where we can't have a reliable cc::BCOM instance, e.g.
      *                                        tab switcher.
      * @param invalidateCompositorView Callback to invalidate the compositor texture.
+     * @param requestLayout Runnable to request layout for the view.
      */
     StatusIndicatorMediator(PropertyModel model, ChromeFullscreenManager fullscreenManager,
             Supplier<Integer> statusBarWithoutIndicatorColorSupplier,
             Supplier<Boolean> canAnimateNativeBrowserControls,
-            Callback<Runnable> invalidateCompositorView) {
+            Callback<Runnable> invalidateCompositorView, Runnable requestLayout) {
         mModel = model;
         mFullscreenManager = fullscreenManager;
         mStatusBarWithoutIndicatorColorSupplier = statusBarWithoutIndicatorColorSupplier;
         mCanAnimateNativeBrowserControls = canAnimateNativeBrowserControls;
         mInvalidateCompositorView = invalidateCompositorView;
+        mRequestLayout = requestLayout;
     }
 
     @Override
@@ -170,6 +173,12 @@ class StatusIndicatorMediator
             final float currentAlpha = (float) anim.getAnimatedValue();
             mModel.set(StatusIndicatorProperties.TEXT_ALPHA, currentAlpha);
         }));
+        animation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mRequestLayout.run();
+            }
+        });
         animation.start();
     }
 
