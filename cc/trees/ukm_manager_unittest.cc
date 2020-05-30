6941aab70b9e78df5f17e4153ba3b86457337672
Mohsen Izadi <mohsen@chromium.org>
2020-05-30 00:22:56 +0000

    Add unit tests for Graphics.Smoothness.Latency UKM metrics
    As a follow-up to r773242 which adds unit tests for
    Graphics.Smoothness.EventLatency UKM metrics, this CL adds similar unit
    tests for Graphics.Smoothness.Latency UKM metrics.
    
    Bug: 1054019
    Change-Id: I69b1a8ce7c100f94806b4875a860ca278949290c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222900
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773437}

--- cc/trees/ukm_manager_unittest.cc
+++ cc/trees/ukm_manager_unittest.cc
@@ -30,22 +30,54 @@ const char kCheckerboardAreaRatio[] = "CheckerboardedContentAreaRatio";
 const char kMissingTiles[] = "NumMissingTiles";
 const char kCheckerboardedImagesCount[] = "CheckerboardedImagesCount";
 
+// Names of compositor/event latency UKM events.
+const char kCompositorLatency[] = "Graphics.Smoothness.Latency";
 const char kEventLatency[] = "Graphics.Smoothness.EventLatency";
-const char kActivation[] = "Activation";
-const char kBeginImplFrameToSendBeginMainFrame[] =
-    "BeginImplFrameToSendBeginMainFrame";
-const char kBrowserToRendererCompositor[] = "BrowserToRendererCompositor";
-const char kCommit[] = "Commit";
-const char kEndActivateToSubmitCompositorFrame[] =
-    "EndActivateToSubmitCompositorFrame";
-const char kEndCommitToActivation[] = "EndCommitToActivation";
+
+// Names of enum metrics used in compositor/event latency UKM metrics.
+const char kMissedFrame[] = "MissedFrame";
 const char kEventType[] = "EventType";
 const char kScrollInputType[] = "ScrollInputType";
+
+// Names of compositor stages and substages used in compositor/event latency UKM
+// metrics.
+const char kBrowserToRendererCompositor[] = "BrowserToRendererCompositor";
+const char kBeginImplFrameToSendBeginMainFrame[] =
+    "BeginImplFrameToSendBeginMainFrame";
 const char kSendBeginMainFrameToCommit[] = "SendBeginMainFrameToCommit";
+const char kCommit[] = "Commit";
+const char kEndCommitToActivation[] = "EndCommitToActivation";
+const char kActivation[] = "Activation";
+const char kEndActivateToSubmitCompositorFrame[] =
+    "EndActivateToSubmitCompositorFrame";
 const char kSubmitCompositorFrameToPresentationCompositorFrame[] =
     "SubmitCompositorFrameToPresentationCompositorFrame";
-const char kTotalLatency[] = "TotalLatency";
+const char kVizBreakdownSubmitToReceiveCompositorFrame[] =
+    "SubmitCompositorFrameToPresentationCompositorFrame."
+    "SubmitToReceiveCompositorFrame";
+const char kVizBreakdownReceivedCompositorFrameToStartDraw[] =
+    "SubmitCompositorFrameToPresentationCompositorFrame."
+    "ReceivedCompositorFrameToStartDraw";
+const char kVizBreakdownStartDrawToSwapStart[] =
+    "SubmitCompositorFrameToPresentationCompositorFrame.StartDrawToSwapStart";
+const char kVizBreakdownSwapStartToSwapEnd[] =
+    "SubmitCompositorFrameToPresentationCompositorFrame.SwapStartToSwapEnd";
+const char kVizBreakdownSwapEndToPresentationCompositorFrame[] =
+    "SubmitCompositorFrameToPresentationCompositorFrame."
+    "SwapEndToPresentationCompositorFrame";
 const char kTotalLatencyToSwapEnd[] = "TotalLatencyToSwapEnd";
+const char kTotalLatency[] = "TotalLatency";
+
+// Names of frame sequence types use in compositor latency UKM metrics (see
+// FrameSequenceTrackerType enum).
+const char kCompositorAnimation[] = "CompositorAnimation";
+const char kMainThreadAnimation[] = "MainThreadAnimation";
+const char kPinchZoom[] = "PinchZoom";
+const char kRAF[] = "RAF";
+const char kTouchScroll[] = "TouchScroll";
+const char kUniversal[] = "Universal";
+const char kVideo[] = "Video";
+const char kWheelScroll[] = "WheelScroll";
 
 class UkmManagerTest : public testing::Test {
  public:
@@ -59,6 +91,8 @@ class UkmManagerTest : public testing::Test {
     test_ukm_recorder_->UpdateSourceURL(kTestSourceId1, GURL(kTestUrl));
     manager_->SetSourceId(kTestSourceId1);
   }
+
+  ~UkmManagerTest() override = default;
 
  protected:
   ukm::TestUkmRecorder* test_ukm_recorder_;
@@ -123,6 +157,193 @@ TEST_F(UkmManagerTest, Basic) {
   }
 }
 
+class UkmManagerCompositorLatencyTest
+    : public UkmManagerTest,
+      public testing::WithParamInterface<
+          CompositorFrameReporter::FrameReportType> {
+ public:
+  UkmManagerCompositorLatencyTest() : report_type_(GetParam()) {}
+  ~UkmManagerCompositorLatencyTest() override = default;
+
+ protected:
+  CompositorFrameReporter::FrameReportType report_type() const {
+    return report_type_;
+  }
+
+ private:
+  CompositorFrameReporter::FrameReportType report_type_;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    UkmManagerCompositorLatencyTest,
+    testing::Values(
+        CompositorFrameReporter::FrameReportType::kNonDroppedFrame,
+        CompositorFrameReporter::FrameReportType::kMissedDeadlineFrame,
+        CompositorFrameReporter::FrameReportType::kDroppedFrame,
+        CompositorFrameReporter::FrameReportType::kCompositorOnlyFrame));
+
+TEST_P(UkmManagerCompositorLatencyTest, CompositorLatency) {
+  base::TimeTicks now = base::TimeTicks::Now();
+
+  const base::TimeTicks begin_impl_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks begin_main_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks begin_commit_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks end_commit_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks begin_activate_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks end_activate_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+  const base::TimeTicks submit_time =
+      (now += base::TimeDelta::FromMicroseconds(10));
+
+  viz::FrameTimingDetails viz_breakdown;
+  viz_breakdown.received_compositor_frame_timestamp =
+      (now += base::TimeDelta::FromMicroseconds(1));
+  viz_breakdown.draw_start_timestamp =
+      (now += base::TimeDelta::FromMicroseconds(2));
+  viz_breakdown.swap_timings.swap_start =
+      (now += base::TimeDelta::FromMicroseconds(3));
+  viz_breakdown.swap_timings.swap_end =
+      (now += base::TimeDelta::FromMicroseconds(4));
+  viz_breakdown.presentation_feedback.timestamp =
+      (now += base::TimeDelta::FromMicroseconds(5));
+
+  std::vector<CompositorFrameReporter::StageData> stage_history = {
+      {
+          CompositorFrameReporter::StageType::
+              kBeginImplFrameToSendBeginMainFrame,
+          begin_impl_time,
+          begin_main_time,
+      },
+      {
+          CompositorFrameReporter::StageType::kSendBeginMainFrameToCommit,
+          begin_main_time,
+          begin_commit_time,
+      },
+      {
+          CompositorFrameReporter::StageType::kCommit,
+          begin_commit_time,
+          end_commit_time,
+      },
+      {
+          CompositorFrameReporter::StageType::kEndCommitToActivation,
+          end_commit_time,
+          begin_activate_time,
+      },
+      {
+          CompositorFrameReporter::StageType::kActivation,
+          begin_activate_time,
+          end_activate_time,
+      },
+      {
+          CompositorFrameReporter::StageType::
+              kEndActivateToSubmitCompositorFrame,
+          end_activate_time,
+          submit_time,
+      },
+      {
+          CompositorFrameReporter::StageType::
+              kSubmitCompositorFrameToPresentationCompositorFrame,
+          submit_time,
+          viz_breakdown.presentation_feedback.timestamp,
+      },
+      {
+          CompositorFrameReporter::StageType::kTotalLatency,
+          begin_impl_time,
+          viz_breakdown.presentation_feedback.timestamp,
+      },
+  };
+
+  CompositorFrameReporter::ActiveTrackers active_trackers;
+  active_trackers.set(
+      static_cast<size_t>(FrameSequenceTrackerType::kTouchScroll));
+  active_trackers.set(
+      static_cast<size_t>(FrameSequenceTrackerType::kCompositorAnimation));
+  active_trackers.set(
+      static_cast<size_t>(FrameSequenceTrackerType::kUniversal));
+
+  manager_->RecordCompositorLatencyUKM(report_type(), stage_history,
+                                       active_trackers, viz_breakdown);
+
+  const auto& entries =
+      test_ukm_recorder_->GetEntriesByName(kCompositorLatency);
+  EXPECT_EQ(1u, entries.size());
+  const auto* entry = entries[0];
+
+  EXPECT_NE(ukm::kInvalidSourceId, entry->source_id);
+  test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, GURL(kTestUrl));
+
+  if (report_type() ==
+      CompositorFrameReporter::FrameReportType::kDroppedFrame) {
+    test_ukm_recorder_->ExpectEntryMetric(entry, kMissedFrame, true);
+  } else {
+    EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kMissedFrame));
+  }
+
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kBeginImplFrameToSendBeginMainFrame,
+      (begin_main_time - begin_impl_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kSendBeginMainFrameToCommit,
+      (begin_commit_time - begin_main_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kCommit, (end_commit_time - begin_commit_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kEndCommitToActivation,
+      (begin_activate_time - end_commit_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kActivation,
+      (end_activate_time - begin_activate_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kEndActivateToSubmitCompositorFrame,
+      (submit_time - end_activate_time).InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kSubmitCompositorFrameToPresentationCompositorFrame,
+      (viz_breakdown.presentation_feedback.timestamp - submit_time)
+          .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kVizBreakdownSubmitToReceiveCompositorFrame,
+      (viz_breakdown.received_compositor_frame_timestamp - submit_time)
+          .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kVizBreakdownReceivedCompositorFrameToStartDraw,
+      (viz_breakdown.draw_start_timestamp -
+       viz_breakdown.received_compositor_frame_timestamp)
+          .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(entry,
+                                        kVizBreakdownStartDrawToSwapStart,
+                                        (viz_breakdown.swap_timings.swap_start -
+                                         viz_breakdown.draw_start_timestamp)
+                                            .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(entry, kVizBreakdownSwapStartToSwapEnd,
+                                        (viz_breakdown.swap_timings.swap_end -
+                                         viz_breakdown.swap_timings.swap_start)
+                                            .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kVizBreakdownSwapEndToPresentationCompositorFrame,
+      (viz_breakdown.presentation_feedback.timestamp -
+       viz_breakdown.swap_timings.swap_end)
+          .InMicroseconds());
+  test_ukm_recorder_->ExpectEntryMetric(
+      entry, kTotalLatency,
+      (viz_breakdown.presentation_feedback.timestamp - begin_impl_time)
+          .InMicroseconds());
+
+  test_ukm_recorder_->ExpectEntryMetric(entry, kCompositorAnimation, true);
+  test_ukm_recorder_->ExpectEntryMetric(entry, kTouchScroll, true);
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kMainThreadAnimation));
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kPinchZoom));
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kRAF));
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kUniversal));
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kVideo));
+  EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kWheelScroll));
+}
+
 TEST_F(UkmManagerTest, EventLatency) {
   base::TimeTicks now = base::TimeTicks::Now();
 
@@ -179,25 +400,23 @@ TEST_F(UkmManagerTest, EventLatency) {
           CompositorFrameReporter::StageType::
               kSubmitCompositorFrameToPresentationCompositorFrame,
           submit_time,
-          viz_breakdown.presentation_feedback.timestamp,
+          present_time,
       },
       {
           CompositorFrameReporter::StageType::kTotalLatency,
           event_time,
-          viz_breakdown.presentation_feedback.timestamp,
+          present_time,
       },
   };
 
   manager_->RecordEventLatencyUKM(events_metrics, stage_history, viz_breakdown);
 
   const auto& entries = test_ukm_recorder_->GetEntriesByName(kEventLatency);
-  ukm::SourceId original_id = ukm::kInvalidSourceId;
   EXPECT_EQ(3u, entries.size());
   for (size_t i = 0; i < entries.size(); i++) {
     const auto* entry = entries[i];
     const auto* event_metrics = event_metrics_ptrs[i].get();
 
-    original_id = entry->source_id;
     EXPECT_NE(ukm::kInvalidSourceId, entry->source_id);
     test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, GURL(kTestUrl));
 
@@ -207,7 +426,6 @@ TEST_F(UkmManagerTest, EventLatency) {
         entry, kScrollInputType,
         static_cast<int64_t>(*event_metrics->scroll_type()));
 
-    EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kActivation));
     test_ukm_recorder_->ExpectEntryMetric(
         entry, kBrowserToRendererCompositor,
         (begin_impl_time - event_time).InMicroseconds());
@@ -219,6 +437,7 @@ TEST_F(UkmManagerTest, EventLatency) {
     EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kCommit));
     EXPECT_FALSE(
         test_ukm_recorder_->EntryHasMetric(entry, kEndCommitToActivation));
+    EXPECT_FALSE(test_ukm_recorder_->EntryHasMetric(entry, kActivation));
     test_ukm_recorder_->ExpectEntryMetric(
         entry, kEndActivateToSubmitCompositorFrame,
         (submit_time - end_activate_time).InMicroseconds());
