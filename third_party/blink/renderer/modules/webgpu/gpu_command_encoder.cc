0fa034b223541d785eeb64df7b1ac3c1fb644279
Kai Ninomiya <kainino@chromium.org>
2020-06-01 19:39:06 +0000

    WebGPU: remove deprecated items, split GPUTextureDataLayout, use USVString
    According to upstream spec changes.
    
    Bug: 1069302
    Change-Id: I191528911b2d20a23e6438548b2c56be37721059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222298
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773779}

--- third_party/blink/renderer/modules/webgpu/gpu_command_encoder.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_command_encoder.cc
@@ -108,10 +108,7 @@ WGPURenderPassDepthStencilAttachmentDescriptor AsDawnType(
   return dawn_desc;
 }
 
-base::Optional<WGPUBufferCopyView> AsDawnType(
-    const GPUBufferCopyView* webgpu_view,
-    GPUDevice* device,
-    ExceptionState& exception_state) {
+WGPUBufferCopyView AsDawnType(const GPUBufferCopyView* webgpu_view) {
   DCHECK(webgpu_view);
   DCHECK(webgpu_view->buffer());
 
@@ -119,32 +116,8 @@ base::Optional<WGPUBufferCopyView> AsDawnType(
   dawn_view.nextInChain = nullptr;
   dawn_view.buffer = webgpu_view->buffer()->GetHandle();
   dawn_view.offset = webgpu_view->offset();
-
-  if (webgpu_view->hasRowPitch()) {
-    device->AddConsoleWarning(
-        "GPUBufferCopyView.rowPitch is deprecated: renamed to bytesPerRow");
-  }
-  if (webgpu_view->hasBytesPerRow()) {
-    dawn_view.bytesPerRow = webgpu_view->bytesPerRow();
-  } else {
-    if (!webgpu_view->hasRowPitch()) {
-      exception_state.ThrowTypeError(
-          "required member bytesPerRow is undefined.");
-      return base::nullopt;
-    }
-    dawn_view.bytesPerRow = webgpu_view->rowPitch();
-  }
-
-  // Note: in this case we check for the deprecated member first, because it is
-  // required, while the new member is optional.
-  if (webgpu_view->hasImageHeight()) {
-    device->AddConsoleWarning(
-        "GPUBufferCopyView.imageHeight is deprecated: renamed to rowsPerImage");
-    dawn_view.rowsPerImage = webgpu_view->imageHeight();
-  } else {
-    dawn_view.rowsPerImage = webgpu_view->rowsPerImage();
-  }
-
+  dawn_view.bytesPerRow = webgpu_view->bytesPerRow();
+  dawn_view.rowsPerImage = webgpu_view->rowsPerImage();
   return dawn_view;
 }
 
@@ -277,8 +250,7 @@ void GPUCommandEncoder::copyBufferToTexture(
     return;
   }
 
-  base::Optional<WGPUBufferCopyView> dawn_source =
-      AsDawnType(source, device_, exception_state);
+  base::Optional<WGPUBufferCopyView> dawn_source = AsDawnType(source);
   if (!dawn_source) {
     return;
   }
@@ -300,8 +272,7 @@ void GPUCommandEncoder::copyTextureToBuffer(
   }
 
   WGPUTextureCopyView dawn_source = AsDawnType(source);
-  base::Optional<WGPUBufferCopyView> dawn_destination =
-      AsDawnType(destination, device_, exception_state);
+  base::Optional<WGPUBufferCopyView> dawn_destination = AsDawnType(destination);
   if (!dawn_destination) {
     return;
   }
