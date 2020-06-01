9156629b5fd1520805b09112768ed688a3353267
Timothy Gu <timothygu@chromium.org>
2020-06-01 01:51:29 +0000

    Rename current to incumbent in Location::SetLocation()
    All call sites actually pass the incumbent window, so call it what it
    is.
    
    Change-Id: I85dd5029f561e1cc055ab350dfbb1555fe1d35fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223984
    Auto-Submit: Timothy Gu <timothygu@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773577}

--- third_party/blink/renderer/core/frame/location.h
+++ third_party/blink/renderer/core/frame/location.h
@@ -106,7 +106,7 @@ class CORE_EXPORT Location final : public ScriptWrappable {
   // ensure we use the correct Javascript world for CSP checks.
   enum class SetLocationPolicy { kNormal, kReplaceThisFrame };
   void SetLocation(const String&,
-                   LocalDOMWindow* current_window,
+                   LocalDOMWindow* incumbent_window,
                    LocalDOMWindow* entered_window,
                    ExceptionState* = nullptr,
                    SetLocationPolicy = SetLocationPolicy::kNormal);
