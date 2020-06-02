0e2010e3d61625d248b9d33fbb676059792e9d19
Alice Wang <aliceywang@chromium.org>
2020-06-02 14:11:31 +0000

    Disable flaky ClipboardAndroidTest.internalClipboardInvalidation
    TBR=twellington@chromium.org
    
    Bug: 1090322
    Change-Id: I5a6ea459dd9c3b5adbbf6edbd008e301206eac29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226731
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774128}

--- ui/android/javatests/src/org/chromium/ui/base/ClipboardAndroidTest.java
+++ ui/android/javatests/src/org/chromium/ui/base/ClipboardAndroidTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.NativeLibraryTestRule;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -48,6 +49,7 @@ public class ClipboardAndroidTest extends DummyUiActivityTestCase {
      */
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1090322")
     public void internalClipboardInvalidation() {
         CriteriaHelper.pollUiThread(() -> getActivity().hasWindowFocus());
 
