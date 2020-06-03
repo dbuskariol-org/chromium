1fe6265db1a5810b1ea21ad87bc2ec5c45bcbaca
Alex Ilin <alexilin@chromium.org>
2020-06-03 20:16:00 +0000

    Reland "[signin] Add reauth browser tests with a fake Gaia page"
    This reverts commit 2d6600719c211afdeeef54b0753e5fbab655c1d8.
    
    Reason for revert: fixed the tests by increasing the dialog
    interaction timeouts
    
    Original change's description:
    > Revert "[signin] Add reauth browser tests with a fake Gaia page"
    > 
    > This reverts commit 62ff616d4bfea4bb8d84a4f3bd4c2195509c8d29.
    > 
    > Reason for revert: 
    > Both of these are failing on MSAN:
    > SigninReauthViewControllerBrowserTest.CancelReauthDialog
    > SigninReauthViewControllerBrowserTest.ConfirmReauthDialog
    > 
    > Original change's description:
    > > [signin] Add reauth browser tests with a fake Gaia page
    > > 
    > > This CL adds a new set of tests for the reauth dialog. These tests
    > > check interactions with a Gaia Reauth endpoint that is imitated through
    > > an EmbeddedTestServer.
    > > 
    > > The main "embedded/xreauth/chrome" endpoint operates in two modes:
    > > - In the default mode, the endpoint redirects to the "challenge" URL
    > > whose response can be controlled within the test by a
    > > ControllableHttpResponse.
    > > - If the "?done" query is present, the endpoint returns the
    > > HTTP_NO_CONTENT response that signifies a successful reauth.
    > > 
    > > The CL also extracts common test code into test suite functions to
    > > reduce the code duplication and make the tests code more readable.
    > > 
    > > Bug: 1045515
    > > Change-Id: I89d4aac57ef25168def1341fb63b1a7498dcfa0f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225396
    > > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > > Reviewed-by: David Roger <droger@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#774205}
    > 
    > TBR=droger@chromium.org,alexilin@chromium.org
    > 
    > Change-Id: I9e322b00bb98f9394b81fda2a0e62bf5db5cd9e9
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1045515, 1090451
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225852
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774265}
    
    TBR=dmazzoni@chromium.org,droger@chromium.org,alexilin@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1045515, 1090451
    Change-Id: I733694fb5790290fe6ae1e661708ddce4a7988c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228540
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774764}

--- chrome/browser/ui/signin_reauth_view_controller_browsertest.cc
+++ chrome/browser/ui/signin_reauth_view_controller_browsertest.cc
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
+#include "base/bind.h"
 #include "base/callback.h"
+#include "base/notreached.h"
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/reauth_result.h"
+#include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/signin_reauth_view_controller.h"
 #include "chrome/browser/ui/signin_view_controller.h"
@@ -19,17 +25,127 @@
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "google_apis/gaia/core_account_id.h"
+#include "google_apis/gaia/gaia_switches.h"
+#include "net/base/escape.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/controllable_http_response.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_response.h"
+#include "net/test/embedded_test_server/request_handler_util.h"
+
+namespace {
+
+const base::TimeDelta kReauthDialogTimeout = base::TimeDelta::FromSeconds(30);
+const char kReauthDonePath[] = "/embedded/xreauth/chrome?done";
+const char kReauthPath[] = "/embedded/xreauth/chrome";
+const char kChallengePath[] = "/challenge";
+
+std::unique_ptr<net::test_server::BasicHttpResponse> CreateRedirectResponse(
+    const GURL& redirect_url) {
+  auto http_response = std::make_unique<net::test_server::BasicHttpResponse>();
+  http_response->set_code(net::HTTP_TEMPORARY_REDIRECT);
+  http_response->AddCustomHeader("Location", redirect_url.spec());
+  http_response->AddCustomHeader("Access-Control-Allow-Origin", "*");
+  return http_response;
+}
+
+std::unique_ptr<net::test_server::HttpResponse> HandleReauthURL(
+    const GURL& base_url,
+    const net::test_server::HttpRequest& request) {
+  if (!net::test_server::ShouldHandle(request, kReauthPath)) {
+    return nullptr;
+  }
+
+  GURL request_url = request.GetURL();
+  std::string parameter =
+      net::UnescapeBinaryURLComponent(request_url.query_piece());
+
+  if (parameter.empty()) {
+    // Parameterless request redirects to the fake challenge page.
+    return CreateRedirectResponse(base_url.Resolve(kChallengePath));
+  }
+
+  if (parameter == "done") {
+    // On success, the reauth returns HTTP_NO_CONTENT response.
+    auto http_response =
+        std::make_unique<net::test_server::BasicHttpResponse>();
+    http_response->set_code(net::HTTP_NO_CONTENT);
+    return http_response;
+  }
+
+  NOTREACHED();
+  return nullptr;
+}
+
+}  // namespace
 
 // Browser tests for SigninReauthViewController.
 class SigninReauthViewControllerBrowserTest : public InProcessBrowserTest {
  public:
+  SigninReauthViewControllerBrowserTest()
+      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
+    scoped_feature_list_.InitAndEnableFeature(kSigninReauthPrompt);
+  }
+
+  void SetUp() override {
+    ASSERT_TRUE(https_server()->InitializeAndListen());
+    InProcessBrowserTest::SetUp();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchASCII(switches::kGaiaUrl, base_url().spec());
+  }
+
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
+
+    https_server()->RegisterRequestHandler(
+        base::BindRepeating(&HandleReauthURL, base_url()));
+    reauth_challenge_response_ =
+        std::make_unique<net::test_server::ControllableHttpResponse>(
+            https_server(), kChallengePath);
+    https_server()->StartAcceptingConnections();
+
     account_id_ = signin::SetUnconsentedPrimaryAccount(identity_manager(),
                                                        "alice@gmail.com")
                       .account_id;
+
+    reauth_result_loop_ = std::make_unique<base::RunLoop>();
   }
+
+  void ShowReauthPrompt() {
+    abort_handle_ = browser()->signin_view_controller()->ShowReauthPrompt(
+        account_id_,
+        base::BindOnce(&SigninReauthViewControllerBrowserTest::OnReauthResult,
+                       base::Unretained(this)));
+  }
+
+  // This method must be called only after the reauth dialog has been opened.
+  void RedirectGaiaChallengeTo(const GURL& redirect_url) {
+    reauth_challenge_response_->WaitForRequest();
+    auto redirect_response = CreateRedirectResponse(redirect_url);
+    reauth_challenge_response_->Send(redirect_response->ToResponseString());
+    reauth_challenge_response_->Done();
+  }
+
+  void OnReauthResult(signin::ReauthResult reauth_result) {
+    reauth_result_ = reauth_result;
+    reauth_result_loop_->Quit();
+  }
+
+  base::Optional<signin::ReauthResult> WaitForReauthResult() {
+    reauth_result_loop_->Run();
+    return reauth_result_;
+  }
+
+  void ResetAbortHandle() { abort_handle_.reset(); }
+
+  net::EmbeddedTestServer* https_server() { return &https_server_; }
+
+  GURL base_url() { return https_server()->base_url(); }
 
   signin::IdentityManager* identity_manager() {
     return IdentityManagerFactory::GetForProfile(browser()->profile());
@@ -43,83 +159,82 @@ class SigninReauthViewControllerBrowserTest : public InProcessBrowserTest {
         signin_view_controller->GetModalDialogDelegateForTesting());
   }
 
-  CoreAccountId account_id() { return account_id_; }
-
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+  net::EmbeddedTestServer https_server_;
+  std::unique_ptr<net::test_server::ControllableHttpResponse>
+      reauth_challenge_response_;
   CoreAccountId account_id_;
+  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle_;
+
+  std::unique_ptr<base::RunLoop> reauth_result_loop_;
+  base::Optional<signin::ReauthResult> reauth_result_;
 };
 
 // Tests that the abort handle cancels an ongoing reauth flow.
 IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
                        AbortReauthDialog_AbortHandle) {
-  base::MockCallback<base::OnceCallback<void(signin::ReauthResult)>>
-      reauth_callback;
-  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
-      browser()->signin_view_controller()->ShowReauthPrompt(
-          account_id(), reauth_callback.Get());
-  EXPECT_CALL(reauth_callback, Run(signin::ReauthResult::kCancelled));
-  abort_handle.reset();
+  ShowReauthPrompt();
+  ResetAbortHandle();
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kCancelled);
 }
 
 // Tests canceling the reauth dialog through CloseModalSignin().
 IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
                        AbortReauthDialog_CloseModalSignin) {
-  base::MockCallback<base::OnceCallback<void(signin::ReauthResult)>>
-      reauth_callback;
-  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
-      browser()->signin_view_controller()->ShowReauthPrompt(
-          account_id(), reauth_callback.Get());
-  EXPECT_CALL(reauth_callback, Run(signin::ReauthResult::kCancelled));
+  ShowReauthPrompt();
   browser()->signin_view_controller()->CloseModalSignin();
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kCancelled);
 }
 
 // Tests closing the reauth dialog through by clicking on the close button (the
 // X).
 IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
                        CloseReauthDialog) {
-  base::MockCallback<base::OnceCallback<void(signin::ReauthResult)>>
-      reauth_callback;
-  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
-      browser()->signin_view_controller()->ShowReauthPrompt(
-          account_id(), reauth_callback.Get());
-  EXPECT_CALL(reauth_callback, Run(signin::ReauthResult::kDismissedByUser));
+  ShowReauthPrompt();
   // The test cannot depend on Views implementation so it simulates clicking on
   // the close button through calling the close event.
   signin_reauth_view_controller()->OnModalSigninClosed();
-}
-
-// Tests clicking on the confirm button in the reauth dialog.
-IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
-                       ConfirmReauthDialog) {
-  base::Optional<signin::ReauthResult> reauth_result;
-  base::RunLoop run_loop;
-  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
-      browser()->signin_view_controller()->ShowReauthPrompt(
-          account_id(),
-          base::BindLambdaForTesting([&](signin::ReauthResult result) {
-            reauth_result = result;
-            run_loop.Quit();
-          }));
-  ASSERT_TRUE(login_ui_test_utils::ConfirmReauthConfirmationDialog(
-      browser(), base::TimeDelta::FromSeconds(30)));
-  run_loop.Run();
-  EXPECT_EQ(reauth_result, signin::ReauthResult::kSuccess);
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kDismissedByUser);
 }
 
 // Tests clicking on the cancel button in the reauth dialog.
 IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
                        CancelReauthDialog) {
-  base::Optional<signin::ReauthResult> reauth_result;
-  base::RunLoop run_loop;
-  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
-      browser()->signin_view_controller()->ShowReauthPrompt(
-          account_id(),
-          base::BindLambdaForTesting([&](signin::ReauthResult result) {
-            reauth_result = result;
-            run_loop.Quit();
-          }));
+  ShowReauthPrompt();
   ASSERT_TRUE(login_ui_test_utils::CancelReauthConfirmationDialog(
-      browser(), base::TimeDelta::FromSeconds(30)));
-  run_loop.Run();
-  EXPECT_EQ(reauth_result, signin::ReauthResult::kDismissedByUser);
+      browser(), kReauthDialogTimeout));
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kDismissedByUser);
+}
+
+// Tests the reauth result in case Gaia page failed to load.
+IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
+                       GaiaChallengeLoadFailed) {
+  ShowReauthPrompt();
+  ASSERT_TRUE(login_ui_test_utils::ConfirmReauthConfirmationDialog(
+      browser(), kReauthDialogTimeout));
+  RedirectGaiaChallengeTo(https_server()->GetURL("/close-socket"));
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kLoadFailed);
+}
+
+// Tests clicking on the confirm button in the reauth dialog. Reauth completes
+// before the confirmation.
+IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
+                       ConfirmReauthDialog_AfterReauthSuccess) {
+  ShowReauthPrompt();
+  RedirectGaiaChallengeTo(https_server()->GetURL(kReauthDonePath));
+  ASSERT_TRUE(login_ui_test_utils::ConfirmReauthConfirmationDialog(
+      browser(), kReauthDialogTimeout));
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kSuccess);
+}
+
+// Tests clicking on the confirm button in the reauth dialog. Reauth completes
+// after the confirmation.
+IN_PROC_BROWSER_TEST_F(SigninReauthViewControllerBrowserTest,
+                       ConfirmReauthDialog_BeforeReauthSuccess) {
+  ShowReauthPrompt();
+  ASSERT_TRUE(login_ui_test_utils::ConfirmReauthConfirmationDialog(
+      browser(), kReauthDialogTimeout));
+  RedirectGaiaChallengeTo(https_server()->GetURL(kReauthDonePath));
+  EXPECT_EQ(WaitForReauthResult(), signin::ReauthResult::kSuccess);
 }
