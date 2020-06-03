26e1243c57d5b879e63c0249a787d0f23b28f1b0
Francois Doray <fdoray@chromium.org>
2020-06-03 21:17:45 +0000

    [sheriff] Disable NaClBrowserTestPnacl.ValidationCacheOfTranslatorNexes on Windows.
    TBR=fdoray@chromium.org
    
    Bug: 1059468
    Change-Id: I050e2d843fc1c303944dd7be4483423d793b0d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229424
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774797}

--- chrome/test/nacl/nacl_browsertest_uma.cc
+++ chrome/test/nacl/nacl_browsertest_uma.cc
@@ -259,6 +259,13 @@ IN_PROC_BROWSER_TEST_F(NaClBrowserTestGLibcVcacheExtension,
 
 // Test that validation for the 2 (or 3) PNaCl translator nexes can be cached.
 // This includes pnacl-llc.nexe, pnacl-ld.nexe, and possibly pnacl-sz.nexe.
+// Flaky on Windows https://crbug.com/1059468#c18
+#if defined(OS_WIN)
+#define MAYBE_ValidationCacheOfTranslatorNexes \
+  DISABLED_ValidationCacheOfTranslatorNexes
+#else
+#define MAYBE_ValidationCacheOfTranslatorNexes ValidationCacheOfTranslatorNexes
+#endif
 IN_PROC_BROWSER_TEST_F(NaClBrowserTestPnacl,
                        ValidationCacheOfTranslatorNexes) {
   const bool uses_subzero_with_o0 = IsSubzeroSupportedForArch();
