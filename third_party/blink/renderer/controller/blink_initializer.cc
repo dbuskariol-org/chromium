dbc0e08729989358e4717900f3889964931cdcb2
Bill Budge <bbudge@chromium.org>
2020-06-03 00:17:59 +0000

    [histograms] Remove Renderer4.ReservedMemory.
    Bug: chromium:1089352
    Change-Id: I4730c417100532410e9355a8fdf5551ee130138f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225591
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774415}

--- third_party/blink/renderer/controller/blink_initializer.cc
+++ third_party/blink/renderer/controller/blink_initializer.cc
@@ -54,7 +54,6 @@
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/disk_data_allocator.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
@@ -112,11 +111,6 @@ void InitializeCommon(Platform* platform, mojo::BinderMap* binders) {
     const size_t kMB = 1024 * 1024;
     for (size_t size = 512 * kMB; size >= 32 * kMB; size -= 16 * kMB) {
       if (base::ReserveAddressSpace(size)) {
-        // Report successful reservation.
-        DEFINE_STATIC_LOCAL(CustomCountHistogram, reservation_size_histogram,
-                            ("Renderer4.ReservedMemory", 32, 512, 32));
-        reservation_size_histogram.Count(size / kMB);
-
         break;
       }
     }
