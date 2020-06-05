d17a43ee698884fdf77954620d233acee57891bc
David Black <dmblack@google.com>
2020-06-05 15:18:48 +0000

    Revise strings for Assistant better onboarding.
    - Added string resources.
    - Contextualized greeting message based on time.
    
    Bug: b:157689497
    Change-Id: Ie074f04c03ea7c473a03a6400cf390630dbd0126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231582
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775575}

--- ash/assistant/ui/main_stage/assistant_onboarding_view.cc
+++ ash/assistant/ui/main_stage/assistant_onboarding_view.cc
@@ -5,17 +5,62 @@
 #include "ash/assistant/ui/main_stage/assistant_onboarding_view.h"
 
 #include <memory>
+#include <string>
 
 #include "ash/assistant/ui/assistant_ui_constants.h"
 #include "ash/assistant/ui/assistant_view_delegate.h"
 #include "ash/assistant/ui/assistant_view_ids.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/strings/stringprintf.h"
+#include "base/time/time.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace ash {
+
+namespace {
+
+// Helpers ---------------------------------------------------------------------
+
+std::string GetGreetingMessage(AssistantViewDelegate* delegate) {
+  base::Time::Exploded now;
+  base::Time::Now().LocalExplode(&now);
+
+  if (now.hour < 5) {
+    return l10n_util::GetStringFUTF8(
+        IDS_ASSISTANT_BETTER_ONBOARDING_GREETING_NIGHT,
+        base::UTF8ToUTF16(delegate->GetPrimaryUserGivenName()));
+  }
+
+  if (now.hour < 12) {
+    return l10n_util::GetStringFUTF8(
+        IDS_ASSISTANT_BETTER_ONBOARDING_GREETING_MORNING,
+        base::UTF8ToUTF16(delegate->GetPrimaryUserGivenName()));
+  }
+
+  if (now.hour < 17) {
+    return l10n_util::GetStringFUTF8(
+        IDS_ASSISTANT_BETTER_ONBOARDING_GREETING_AFTERNOON,
+        base::UTF8ToUTF16(delegate->GetPrimaryUserGivenName()));
+  }
+
+  if (now.hour < 23) {
+    return l10n_util::GetStringFUTF8(
+        IDS_ASSISTANT_BETTER_ONBOARDING_GREETING_EVENING,
+        base::UTF8ToUTF16(delegate->GetPrimaryUserGivenName()));
+  }
+
+  return l10n_util::GetStringFUTF8(
+      IDS_ASSISTANT_BETTER_ONBOARDING_GREETING_NIGHT,
+      base::UTF8ToUTF16(delegate->GetPrimaryUserGivenName()));
+}
+
+}  // namespace
+
+// AssistantOnboardingView -----------------------------------------------------
 
 AssistantOnboardingView::AssistantOnboardingView(
     AssistantViewDelegate* delegate)
@@ -39,7 +84,6 @@ void AssistantOnboardingView::ChildPreferredSizeChanged(views::View* child) {
 }
 
 // TODO(dmblack): Implement suggestions.
-// TODO(dmblack): Finalize strings.
 void AssistantOnboardingView::InitLayout() {
   SetBackground(views::CreateSolidBackground(
       SkColorSetA(gfx::kPlaceholderColor, 0.2 * 0xFF)));
@@ -54,21 +98,19 @@ void AssistantOnboardingView::InitLayout() {
   greeting->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
   greeting->SetEnabledColor(kTextColorPrimary);
   greeting->SetFontList(assistant::ui::GetDefaultFontList());
-  greeting->SetText(base::UTF8ToUTF16(base::StringPrintf(
-      "Good morning %s,", delegate_->GetPrimaryUserGivenName().c_str())));
+  greeting->SetText(base::UTF8ToUTF16(GetGreetingMessage(delegate_)));
   AddChildView(std::move(greeting));
 
-  // Description.
-  auto description = std::make_unique<views::Label>();
-  description->SetAutoColorReadabilityEnabled(false);
-  description->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
-  description->SetEnabledColor(kTextColorPrimary);
-  description->SetFontList(assistant::ui::GetDefaultFontList());
-  description->SetMultiLine(true);
-  description->SetText(base::UTF8ToUTF16(
-      "I'm your Google Assistant, here to help you throughout your day!\nHere "
-      "are some things you can try to get started."));
-  AddChildView(std::move(description));
+  // Intro.
+  auto intro = std::make_unique<views::Label>();
+  intro->SetAutoColorReadabilityEnabled(false);
+  intro->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
+  intro->SetEnabledColor(kTextColorPrimary);
+  intro->SetFontList(assistant::ui::GetDefaultFontList());
+  intro->SetMultiLine(true);
+  intro->SetText(
+      l10n_util::GetStringUTF16(IDS_ASSISTANT_BETTER_ONBOARDING_INTRO));
+  AddChildView(std::move(intro));
 
   // Suggestions.
   auto* suggestions = AddChildView(std::make_unique<views::View>());
--- ash/assistant/ui/main_stage/assistant_onboarding_view_unittest.cc
+++ ash/assistant/ui/main_stage/assistant_onboarding_view_unittest.cc
@@ -7,10 +7,11 @@
 #include <memory>
 #include <string>
 
-#include "ash/assistant/test/assistant_ash_test_base.h"
 #include "ash/assistant/ui/test_support/mock_assistant_view_delegate.h"
+#include "ash/test/ash_test_base.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/icu_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/controls/label.h"
 
@@ -25,20 +26,29 @@ constexpr char kPrimaryUserGivenName[] = "Foo";
 
 // AssistantOnboardingViewTest -------------------------------------------------
 
-class AssistantOnboardingViewTest : public AssistantAshTestBase {
+class AssistantOnboardingViewTest : public AshTestBase {
  public:
-  AssistantOnboardingViewTest() = default;
+  AssistantOnboardingViewTest()
+      : AshTestBase(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   ~AssistantOnboardingViewTest() override = default;
 
-  // AssistantAshTestBase:
+  // AshTestBase:
   void SetUp() override {
-    AssistantAshTestBase::SetUp();
+    AshTestBase::SetUp();
 
     delegate_ = std::make_unique<MockAssistantViewDelegate>();
 
     ON_CALL(*delegate_, GetPrimaryUserGivenName())
         .WillByDefault(Return(kPrimaryUserGivenName));
 
+    RecreateView();
+  }
+
+  void AdvanceClock(base::TimeDelta time_delta) {
+    task_environment()->AdvanceClock(time_delta);
+  }
+
+  void RecreateView() {
     view_ = std::make_unique<AssistantOnboardingView>(delegate_.get());
   }
 
@@ -48,11 +58,12 @@ class AssistantOnboardingViewTest : public AssistantAshTestBase {
     return static_cast<views::Label*>(view_->children().at(0));
   }
 
-  views::Label* description_label() {
+  views::Label* intro_label() {
     return static_cast<views::Label*>(view_->children().at(1));
   }
 
  private:
+  base::test::ScopedRestoreICUDefaultLocale locale{"en_US"};
   std::unique_ptr<MockAssistantViewDelegate> delegate_;
   std::unique_ptr<AssistantOnboardingView> view_;
 };
@@ -62,13 +73,77 @@ class AssistantOnboardingViewTest : public AssistantAshTestBase {
 // Tests -----------------------------------------------------------------------
 
 TEST_F(AssistantOnboardingViewTest, ShouldHaveExpectedGreeting) {
+  // Advance clock to midnight.
+  base::Time::Exploded now;
+  base::Time::Now().LocalExplode(&now);
+  AdvanceClock(base::TimeDelta::FromHours((24 - now.hour) % 24) -
+               base::TimeDelta::FromMinutes(now.minute) -
+               base::TimeDelta::FromSeconds(now.second) -
+               base::TimeDelta::FromMilliseconds(now.millisecond));
+
+  // Verify 4:59 AM.
+  AdvanceClock(base::TimeDelta::FromHours(4) +
+               base::TimeDelta::FromMinutes(59));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(
+                base::StringPrintf("Good night %s,", kPrimaryUserGivenName)));
+
+  // Verify 5:00 AM.
+  AdvanceClock(base::TimeDelta::FromMinutes(1));
+  RecreateView();
   EXPECT_EQ(greeting_label()->GetText(),
             base::UTF8ToUTF16(
                 base::StringPrintf("Good morning %s,", kPrimaryUserGivenName)));
+
+  // Verify 11:59 AM.
+  AdvanceClock(base::TimeDelta::FromHours(6) +
+               base::TimeDelta::FromMinutes(59));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(
+                base::StringPrintf("Good morning %s,", kPrimaryUserGivenName)));
+
+  // Verify 12:00 PM.
+  AdvanceClock(base::TimeDelta::FromMinutes(1));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(base::StringPrintf("Good afternoon %s,",
+                                                 kPrimaryUserGivenName)));
+
+  // Verify 4:59 PM.
+  AdvanceClock(base::TimeDelta::FromHours(4) +
+               base::TimeDelta::FromMinutes(59));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(base::StringPrintf("Good afternoon %s,",
+                                                 kPrimaryUserGivenName)));
+
+  // Verify 5:00 PM.
+  AdvanceClock(base::TimeDelta::FromMinutes(1));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(
+                base::StringPrintf("Good evening %s,", kPrimaryUserGivenName)));
+
+  // Verify 10:59 PM.
+  AdvanceClock(base::TimeDelta::FromHours(5) +
+               base::TimeDelta::FromMinutes(59));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(
+                base::StringPrintf("Good evening %s,", kPrimaryUserGivenName)));
+
+  // Verify 11:00 PM.
+  AdvanceClock(base::TimeDelta::FromMinutes(1));
+  RecreateView();
+  EXPECT_EQ(greeting_label()->GetText(),
+            base::UTF8ToUTF16(
+                base::StringPrintf("Good night %s,", kPrimaryUserGivenName)));
 }
 
-TEST_F(AssistantOnboardingViewTest, ShouldHaveExpectedDescription) {
-  EXPECT_EQ(description_label()->GetText(),
+TEST_F(AssistantOnboardingViewTest, ShouldHaveExpectedIntro) {
+  EXPECT_EQ(intro_label()->GetText(),
             base::UTF8ToUTF16(
                 "I'm your Google Assistant, here to help you throughout your "
                 "day!\nHere are some things you can try to get started."));
