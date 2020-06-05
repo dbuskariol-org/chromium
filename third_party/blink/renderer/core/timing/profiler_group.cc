7d343eb6f0642adb210be8384f4b998352eb591a
Tomasz Moniuszko <tmoniuszko@opera.com>
2020-06-05 13:48:58 +0000

    Remove stopped/canceled Profiler from ProfilerGroup
    This fixes crash in ProfilerGroupTest.
    
    BUG=1088428
    TEST=blink_unittests --gtest_filter=ProfilerGroupTest.*
    
    Change-Id: Ib19164af72402c55c6a09b423f186fefac0dbf8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224857
    Reviewed-by: Andrew Comminos <acomminos@fb.com>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775543}

--- third_party/blink/renderer/core/timing/profiler_group.cc
+++ third_party/blink/renderer/core/timing/profiler_group.cc
@@ -125,11 +125,13 @@ ProfilerGroup::~ProfilerGroup() {
 }
 
 void ProfilerGroup::WillBeDestroyed() {
-  for (auto& profiler : profilers_) {
+  while (!profilers_.IsEmpty()) {
+    Profiler* profiler = profilers_.begin()->Get();
     DCHECK(profiler);
     CancelProfiler(profiler);
     profiler->RemovedFromProfilerGroup();
     DCHECK(profiler->stopped());
+    DCHECK(!profilers_.Contains(profiler));
   }
 
   if (cpu_profiler_)
@@ -179,6 +181,8 @@ void ProfilerGroup::StopProfiler(ScriptState* script_state,
   if (profile)
     profile->Delete();
 
+  profilers_.erase(profiler);
+
   if (--num_active_profilers_ == 0)
     TeardownV8Profiler();
 }
@@ -186,6 +190,7 @@ void ProfilerGroup::StopProfiler(ScriptState* script_state,
 void ProfilerGroup::CancelProfiler(Profiler* profiler) {
   DCHECK(cpu_profiler_);
   DCHECK(!profiler->stopped());
+  profilers_.erase(profiler);
   CancelProfilerImpl(profiler->ProfilerId());
 }
 
@@ -193,6 +198,7 @@ void ProfilerGroup::CancelProfilerAsync(ScriptState* script_state,
                                         Profiler* profiler) {
   DCHECK(cpu_profiler_);
   DCHECK(!profiler->stopped());
+  profilers_.erase(profiler);
   ExecutionContext::From(script_state)
       ->GetTaskRunner(TaskType::kInternalDefault)
       ->PostTask(FROM_HERE,
