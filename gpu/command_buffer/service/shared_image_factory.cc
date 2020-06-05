286a72424203f033012a0349562449b4b1253881
Peng Huang <penghuang@chromium.org>
2020-06-05 14:14:27 +0000

    SharedImageFactory: checking backing before calling OnWriteSucceeded().
    Bug: None
    Change-Id: If79a041d384c313e0fdc2c6c844baf3a54fad195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231686
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775550}

--- gpu/command_buffer/service/shared_image_factory.cc
+++ gpu/command_buffer/service/shared_image_factory.cc
@@ -213,7 +213,8 @@ bool SharedImageFactory::CreateSharedImage(const Mailbox& mailbox,
     return false;
   auto backing = factory->CreateSharedImage(mailbox, format, size, color_space,
                                             usage, data);
-  backing->OnWriteSucceeded();
+  if (backing)
+    backing->OnWriteSucceeded();
   return RegisterBacking(std::move(backing), allow_legacy_mailbox);
 }
 
@@ -236,7 +237,8 @@ bool SharedImageFactory::CreateSharedImage(const Mailbox& mailbox,
   auto backing =
       factory->CreateSharedImage(mailbox, client_id, std::move(handle), format,
                                  surface_handle, size, color_space, usage);
-  backing->OnWriteSucceeded();
+  if (backing)
+    backing->OnWriteSucceeded();
   return RegisterBacking(std::move(backing), allow_legacy_mailbox);
 }
 
