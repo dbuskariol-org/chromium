1c7b7cc0dc31f4f6834f5128fbd9819823ad595c
Maksim Ivanov <emaxx@chromium.org>
2020-06-05 18:43:59 +0000

    policy_fuzzer: Fuzz policy-to-CrosSettings flow
    Extend the policy_fuzzer to also cover the functionality that converts
    from the raw device policy proto (ChromeDeviceSettingsProto) to
    CrosSettings.
    
    Bug: 811690
    Change-Id: I5193b21c0045bb0a88e83aeb46885ed13721d0ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218027
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775677}

--- chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.cc
+++ chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.cc
@@ -2,14 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
+#include "base/path_service.h"
+#include "base/test/task_environment.h"
+#include "chrome/browser/chromeos/dbus/dbus_helper.h"
 #include "chrome/browser/chromeos/policy/device_policy_decoder_chromeos.h"
 #include "chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.pb.h"
+#include "chrome/browser/chromeos/settings/device_settings_provider.h"
+#include "chrome/browser/chromeos/settings/device_settings_service.h"
 #include "chrome/browser/policy/configuration_policy_handler_list_factory.h"
-#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chrome/common/chrome_paths.h"
+#include "chromeos/tpm/install_attributes.h"
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
 #include "components/policy/core/browser/policy_conversions_client.h"
 #include "components/policy/core/browser/policy_error_map.h"
@@ -30,26 +39,58 @@ struct Environment {
   Environment() {
     logging::SetMinLogLevel(logging::LOG_FATAL);
     base::CommandLine::Init(0, nullptr);
-    chromeos::DBusThreadManager::Initialize();
-    policy_handler_list = BuildHandlerList(GetChromeSchema());
+    CHECK(scoped_temp_dir.CreateUniqueTempDir());
+    CHECK(base::PathService::Override(chrome::DIR_USER_DATA,
+                                      scoped_temp_dir.GetPath()));
   }
 
+  base::ScopedTempDir scoped_temp_dir;
+};
+
+struct PerInputEnvironment {
+  PerInputEnvironment() {
+    policy_handler_list = BuildHandlerList(GetChromeSchema());
+    chromeos::InitializeDBus();
+    chromeos::InitializeFeatureListDependentDBus();
+  }
+
+  ~PerInputEnvironment() {
+    chromeos::ShutdownDBus();
+    chromeos::InstallAttributes::Shutdown();
+    chromeos::DeviceSettingsService::Shutdown();
+  }
+
+  base::test::TaskEnvironment task_environment;
   std::unique_ptr<ConfigurationPolicyHandlerList> policy_handler_list;
 };
 
 void CheckPolicyToPrefTranslation(const PolicyMap& policy_map,
-                                  const Environment& environment) {
+                                  const PerInputEnvironment& per_input_env) {
   PrefValueMap prefs;
   PolicyErrorMap errors;
   DeprecatedPoliciesSet deprecated_policies;
-  environment.policy_handler_list->ApplyPolicySettings(
+  per_input_env.policy_handler_list->ApplyPolicySettings(
       policy_map, &prefs, &errors, &deprecated_policies);
+}
+
+void CheckPolicyToCrosSettingsTranslation(
+    const enterprise_management::ChromeDeviceSettingsProto&
+        chrome_device_settings) {
+  PrefValueMap cros_settings_prefs;
+  chromeos::DeviceSettingsProvider::DecodePolicies(chrome_device_settings,
+                                                   &cros_settings_prefs);
+
+  for (const auto& it : cros_settings_prefs) {
+    const std::string& pref_name = it.first;
+    CHECK(chromeos::DeviceSettingsProvider::IsDeviceSetting(pref_name));
+  }
 }
 
 }  // namespace
 
 DEFINE_PROTO_FUZZER(const PolicyFuzzerProto& proto) {
-  static Environment environment;
+  static Environment env;
+  PerInputEnvironment per_input_env;
 
   if (proto.has_chrome_device_settings()) {
     const enterprise_management::ChromeDeviceSettingsProto&
@@ -66,7 +107,8 @@ DEFINE_PROTO_FUZZER(const PolicyFuzzerProto& proto) {
           << "Policy " << policy_name << " has not machine scope";
     }
 
-    CheckPolicyToPrefTranslation(policy_map, environment);
+    CheckPolicyToPrefTranslation(policy_map, per_input_env);
+    CheckPolicyToCrosSettingsTranslation(chrome_device_settings);
   }
 
   if (proto.has_cloud_policy_settings()) {
@@ -86,7 +128,7 @@ DEFINE_PROTO_FUZZER(const PolicyFuzzerProto& proto) {
           << "Policy " << policy_name << " has not user scope";
     }
 
-    CheckPolicyToPrefTranslation(policy_map, environment);
+    CheckPolicyToPrefTranslation(policy_map, per_input_env);
   }
 }
 
