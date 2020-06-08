03f76e68bf7934bf7e80ceb78a133db0945e4381
Akihiro Ota <akihiroota@chromium.org>
2020-06-08 19:19:17 +0000

    Prototype for ChromeVox tutorial.
    This change does the following:
    1. Adds an interactive tutorial polymer component
    2. Adds a lesson polymer component
    3. Adds html files used to define the test areas for interactive
    lessons.
    4. Ports over some content from the existing tutorial into the new
    one.
    5. Connects the interactive tutorial to the ChromeVox panel.
    6. Wait to send commands to the panel until the panel has loaded.
    Connecting the tutorial to the panel was increasing the panel load
    time, which was causing us to fail panel-related tests.
    7. Compiles ChromeVox using ES_NEXT rule to support async panel
    command behavior; errors were fired when trying to compile using ES6.
    
    Bug: 1075752
    AX-Relnotes: N/A
    
    Change-Id: I7589a1d81185cba93242a1e419da4bd466351f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194345
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776153}

--- chrome/browser/resources/chromeos/accessibility/chromevox/tools/jscompilerwrapper.py
+++ chrome/browser/resources/chromeos/accessibility/chromevox/tools/jscompilerwrapper.py
@@ -68,7 +68,7 @@ def RunCompiler(js_files, externs=[]):
   args.extend(['--compilation_level', 'SIMPLE_OPTIMIZATIONS'])
   args.extend(['--jscomp_error=%s' % error for error in _JSCOMP_ERRORS])
   args.extend(['--jscomp_off=%s' % off for off in _JSCOMP_OFF])
-  args.extend(['--language_in', 'ECMASCRIPT6'])
+  args.extend(['--language_in', 'ECMASCRIPT_NEXT'])
   args.extend(['--externs=%s' % extern for extern in externs])
   args.extend(['--js=%s' % js for js in js_files])
   args.extend(['--js_output_file', '/dev/null'])
