168b049b0b1507cdcaaf7d4bd8e58c61100cb0e6
Jinsuk Kim <jinsukkim@chromium.org>
2020-06-08 05:31:28 +0000

    GestureNav: Fix NPE at destruction
    A posted task could run after HistoryNavigation-
    Coordinator is already destroyed, therefore
    caused NPE. This CL removes the posted callback
    upon |destroy| to prevent the exception.
    
    Bug: 1092095
    Change-Id: I510e3382e77f03b8e9a034ca0f2935335af9e2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233425
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775948}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
@@ -30,6 +30,8 @@ import org.chromium.content_public.browser.WebContents;
  */
 public class HistoryNavigationCoordinator implements InsetObserverView.WindowInsetObserver,
                                                      Destroyable, PauseResumeWithNativeObserver {
+    private final Runnable mUpdateNavigationStateRunnable = this::onNavigationStateChanged;
+
     private CompositorViewHolder mCompositorViewHolder;
     private HistoryNavigationLayout mNavigationLayout;
     private InsetObserverView mInsetObserverView;
@@ -224,7 +226,7 @@ public class HistoryNavigationCoordinator implements InsetObserverView.WindowIns
     public void onResumeWithNative() {
         // Check the enabled status again since the system gesture settings might have changed.
         // Post the task to work around wrong gesture insets returned from the framework.
-        mNavigationLayout.post(this::onNavigationStateChanged);
+        mNavigationLayout.post(mUpdateNavigationStateRunnable);
     }
 
     @Override
@@ -245,6 +247,7 @@ public class HistoryNavigationCoordinator implements InsetObserverView.WindowIns
             mCompositorViewHolder = null;
         }
         if (mNavigationLayout != null) {
+            mNavigationLayout.removeCallbacks(mUpdateNavigationStateRunnable);
             mNavigationLayout.destroy();
             mNavigationLayout = null;
         }
