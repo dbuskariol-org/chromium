027aefc6b58096d2aba33def4e882d61b5c70c1f
Kent Tamura <tkent@chromium.org>
2020-06-09 08:53:00 +0000

    OPTION element: Update OPTION label even if it has invalid markup
    https://html.spec.whatwg.org/C/#the-option-element
    > Content model:
    > If the element has no label attribute and is not a child of a datalist
    > element: Text that is not inter-element whitespace.
    
    OPTION elements should not have element children, but they can have if
    web authors add elements by DOM API.
    
    For both of drop-down SELECTs and listbox SELECTs, Chrome missed to
    update OPTION labels. It's a regression since Google Chrome 83 for
    drop-down SELECT, and the issue exists since the WebKit fork for
    listbox SELECTs.
    
    This CL fixes the issue by using MutationObserver only if an OPTION
    element has Element children.
    
    Bug: 1090806
    Change-Id: I69f5aa4646fda0ad2770cc2748016c6b37b3ad06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235227
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776436}

--- third_party/blink/renderer/core/html/forms/html_option_element.h
+++ third_party/blink/renderer/core/html/forms/html_option_element.h
@@ -33,6 +33,7 @@ namespace blink {
 class ExceptionState;
 class HTMLDataListElement;
 class HTMLSelectElement;
+class OptionTextObserver;
 
 class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   DEFINE_WRAPPERTYPEINFO();
@@ -53,6 +54,7 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
                                                    ExceptionState&);
 
   explicit HTMLOptionElement(Document&);
+  void Trace(Visitor* visitor) const override;
 
   // A text to be shown to users.  The difference from |label()| is |label()|
   // returns an empty string if |label| content attribute is empty.
@@ -101,6 +103,9 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   void SetMultiSelectFocusedState(bool);
   bool IsMultiSelectFocused() const;
 
+  // Callback for OptionTextObserver.
+  void DidChangeTextContent();
+
  private:
   ~HTMLOptionElement() override;
 
@@ -116,6 +121,8 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   String CollectOptionInnerText() const;
 
   void UpdateLabel();
+
+  Member<OptionTextObserver> text_observer_;
 
   // Represents 'selectedness'.
   // https://html.spec.whatwg.org/C/#concept-option-selectedness
