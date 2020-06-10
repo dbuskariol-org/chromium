9204709c6ad26c846020e2738126257796b39879
Dominic Farolino <dom@chromium.org>
2020-06-10 23:00:18 +0000

    Implement parser document concept in CreateElementFlags
    This CL implements the HTML Standard's "parser document" concept.
    The design is described [1], which ultimately resolves to keeping
    CreateElementFlags::created_by_parser_ around, and introducing a weak
    reference to a blink::Document to implement
    CreateElementFlags::parser_document_.
    
    [1]: https://docs.google.com/document/d/1PORt2X7AI0-UPepaAX7R50I9Bzumw8KE4iq19uxKI18/edit.
    
    Bug: 721914, 1086507
    Change-Id: Ia09624d84ebdc72db16f8e2416e539db5d2cc798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218347
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777151}

--- third_party/blink/renderer/core/html/parser/html_construction_site.cc
+++ third_party/blink/renderer/core/html/parser/html_construction_site.cc
@@ -750,6 +750,9 @@ void HTMLConstructionSite::InsertFormattingElement(AtomicHTMLToken* token) {
 
 void HTMLConstructionSite::InsertScriptElement(AtomicHTMLToken* token) {
   CreateElementFlags flags;
+  bool should_be_parser_inserted =
+      parser_content_policy_ !=
+      kAllowScriptingContentAndDoNotMarkAlreadyStarted;
   flags
       // http://www.whatwg.org/specs/web-apps/current-work/multipage/scripting-1.html#already-started
       // http://html5.org/specs/dom-parsing.html#dom-range-createcontextualfragment
@@ -757,8 +760,8 @@ void HTMLConstructionSite::InsertScriptElement(AtomicHTMLToken* token) {
       // parser-inserted and already-started and later unmark them. However, we
       // short circuit that logic to avoid the subtree traversal to find script
       // elements since scripts can never see those flags or effects thereof.
-      .SetCreatedByParser(parser_content_policy_ !=
-                          kAllowScriptingContentAndDoNotMarkAlreadyStarted)
+      .SetCreatedByParser(should_be_parser_inserted,
+                          should_be_parser_inserted ? document_ : nullptr)
       .SetAlreadyStarted(is_parsing_fragment_ && flags.IsCreatedByParser());
   HTMLScriptElement* element = nullptr;
   if (const auto* is_attribute = token->GetAttributeItem(html_names::kIsAttr)) {
@@ -866,8 +869,8 @@ void HTMLConstructionSite::TakeAllChildren(
 }
 
 CreateElementFlags HTMLConstructionSite::GetCreateElementFlags() const {
-  return is_parsing_fragment_ ? CreateElementFlags::ByFragmentParser()
-                              : CreateElementFlags::ByParser();
+  return is_parsing_fragment_ ? CreateElementFlags::ByFragmentParser(document_)
+                              : CreateElementFlags::ByParser(document_);
 }
 
 Document& HTMLConstructionSite::OwnerDocumentForCurrentNode() {
@@ -964,7 +967,8 @@ Element* HTMLConstructionSite::CreateElement(
     // only partially construct themselves when created by the parser, but since
     // this is a custom element, we need a fully-constructed element here.
     element = definition->CreateElement(
-        document, tag_name, GetCreateElementFlags().SetCreatedByParser(false));
+        document, tag_name,
+        GetCreateElementFlags().SetCreatedByParser(false, nullptr));
 
     // "8. Append each attribute in the given token to element." We don't use
     // setAttributes here because the custom element constructor may have
