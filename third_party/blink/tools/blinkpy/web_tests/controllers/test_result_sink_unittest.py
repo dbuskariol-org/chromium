15b76c2bdb21838f4c6206462323c9ae3194a425
Scott Lee <ddoman@chromium.org>
2020-06-11 17:57:45 +0000

    [resultdb] implement TestResultSink.sink
    It uploads the test results with the path of the artifacts to upload.
    
    R=nodir@chromium.org,robertma@chromium.org
    CC=​chanli@chromium.org,iannucci@chromium.org
    
    Bug: 1084459
    Change-Id: Id9a78005a6669aca1f66c0e4153ca64d59e31a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2234714
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Scott Lee <ddoman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777398}

--- third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink_unittest.py
@@ -3,14 +3,18 @@
 # found in the LICENSE file.
 
 import contextlib
-import os
 import json
+import mock
+import os
 import sys
 import tempfile
 import unittest
+from urlparse import urlparse
 
 from blinkpy.web_tests.controllers.test_result_sink import CreateTestResultSink
+from blinkpy.web_tests.controllers.test_result_sink import TestResultSink
 from blinkpy.web_tests.models import test_results
+from blinkpy.web_tests.models.typ_types import ResultType
 
 
 @contextlib.contextmanager
@@ -42,9 +46,95 @@ class TestCreateTestResultSink(unittest.TestCase):
         with luci_context(app={'foo': 'bar'}):
             self.assertIsNone(CreateTestResultSink('/tmp/artifacts'))
 
-    def test_with_result_sink_section(self):
-        with luci_context(result_sink={
-                'address': 'localhost:123',
-                'auth_token': 'super secret password'
-        }):
-            self.assertIsNotNone(CreateTestResultSink('/tmp/artifacts'))
+    @mock.patch('urllib2.urlopen')
+    def test_with_result_sink_section(self, urlopen):
+        ctx = {'address': 'localhost:123', 'auth_token': 'secret'}
+        with luci_context(result_sink=ctx):
+            r = CreateTestResultSink('/tmp/artifacts')
+            self.assertIsNotNone(r)
+            r.sink(True, test_results.TestResult('test'))
+
+        urlopen.assert_called_once()
+        req = urlopen.call_args[0][0]
+        self.assertEqual(urlparse(req.get_full_url()).netloc, ctx['address'])
+        self.assertEqual(req.get_header('Authorization'),
+                         'ResultSink ' + ctx['auth_token'])
+
+
+class TestResultSinkMessage(unittest.TestCase):
+    """Tests ResulkSink.sink."""
+
+    def setUp(self):
+        patcher = mock.patch.object(TestResultSink, '_send')
+        self.mock_send = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        ctx = {'address': 'localhost:123', 'auth_token': 'super-secret'}
+        with luci_context(result_sink=ctx):
+            self.r = CreateTestResultSink('/tmp/artifacts')
+
+    def sink(self, expected, test_result):
+        self.r.sink(expected, test_result)
+        self.mock_send.called_once()
+        return self.mock_send.call_args[0][0]['testResults'][0]
+
+    def test_sink(self):
+        tr = test_results.TestResult(test_name='test-name')
+        tr.total_run_time = 123.456
+        tr.type = ResultType.Crash
+        sent_data = self.sink(True, tr)
+
+        self.assertEqual(sent_data['testId'], 'test-name')
+        self.assertEqual(sent_data['expected'], True)
+        self.assertEqual(sent_data['status'], 'CRASH')
+        self.assertEqual(sent_data['duration'], '123.456s')
+
+    def test_device_failure(self):
+        tr = test_results.TestResult(test_name='test-name')
+        tr.type = ResultType.Failure
+        tr.device_failed = True
+        sent_data = self.sink(True, tr)
+
+        # If the device failed, 'expected' and 'status' must be False and 'ABORT'
+        self.assertEqual(sent_data['expected'], False)
+        self.assertEqual(sent_data['status'], 'ABORT')
+
+    def test_timeout(self):
+        tr = test_results.TestResult(test_name='test-name')
+        tr.type = ResultType.Timeout
+        sent_data = self.sink(True, tr)
+
+        # Timeout is considered as 'ABORT'
+        self.assertEqual(sent_data['status'], 'ABORT')
+
+    def test_artifacts(self):
+        tr = test_results.TestResult(test_name='test-name')
+        tr.artifacts.AddArtifact('test-image.png', '/tmp/test-image.png', True)
+        tr.artifacts.AddArtifact('stdout', '/tmp/stdout', True)
+
+        sent_data = self.sink(True, tr)
+        self.assertDictEqual(
+            sent_data['artifacts'], {
+                'test-image.png': {
+                    'filePath': '/tmp/test-image.png'
+                },
+                'stdout': {
+                    'filePath': '/tmp/stdout'
+                }
+            })
+
+    def test_artifacts_with_duplicate_paths(self):
+        tr = test_results.TestResult(test_name='test-name')
+        tr.artifacts.AddArtifact('artifact', '/tmp/foo', False)
+        tr.artifacts.AddArtifact('artifact', '/tmp/bar', False)
+
+        sent_data = self.sink(True, tr)
+        self.assertDictEqual(
+            sent_data['artifacts'], {
+                'artifact': {
+                    'filePath': '/tmp/foo'
+                },
+                'artifact-1': {
+                    'filePath': '/tmp/bar'
+                }
+            })
