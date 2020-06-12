ca14e9a5f1e4c4383200ee44b4a240a731b063c9
sczs <sczs@chromium.org>
2020-06-12 22:45:32 +0000

    [ios] Enables Messages to 100% in Canary, Dev and Beta.
    Finch is not being used since the metrics team doesn't think its an
    appropriate use of Finch (Which is more aimed at A/B testing) and not
    a release tool.
    
    Metrics on NonStable Channels are good since the top crashes are barely
    occurring. We hope that this will help catch any of these elusive crashes
    (Hopefully with breadcrumbs) before the Stable release.
    
    (cherry picked from commit 6126f88a5a9adc52f422183da1a48114c97e8c1b)
    
    Bug: 1065385
    Change-Id: I49707077afd577b612d739ade4c9b3edaa9b5ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238377
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#776771}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244031
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4147@{#619}
    Cr-Branched-From: 16307825352720ae04d898f37efa5449ad68b606-refs/heads/master@{#768962}

--- ios/chrome/browser/ui/infobars/infobar_feature.mm
+++ ios/chrome/browser/ui/infobars/infobar_feature.mm
@@ -5,6 +5,8 @@
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 
 #include "components/infobars/core/infobar_feature.h"
+#include "components/version_info/version_info.h"
+#include "ios/chrome/common/channel_info.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -44,6 +46,17 @@ const base::Feature kInfobarUIRebootOnlyiOS13{"InfobarUIRebootOnlyiOS13",
                                               base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool IsInfobarUIRebootEnabled() {
+  // Enable Messages to 100% in Dev, Canary and Beta.
+  switch (GetChannel()) {
+    case version_info::Channel::BETA:
+    case version_info::Channel::DEV:
+    case version_info::Channel::CANARY:
+      return YES;
+    case version_info::Channel::UNKNOWN:
+    case version_info::Channel::STABLE:
+      break;
+  }
+
   if (base::FeatureList::IsEnabled(kInfobarUIRebootOnlyiOS13)) {
     if (@available(iOS 13, *)) {
       return base::FeatureList::IsEnabled(kIOSInfobarUIReboot);
