d6fe2d81b00635a931e30a2e9f0fef85bfb32cbc
Erik Chen <erikchen@chromium.org>
2020-06-12 19:34:30 +0000

    Avoid a DCHECK during profile initialization.
    Bug: 1065798
    Change-Id: I62517bfae74e61f9ff42d92b4ad865876f59c130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242687
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777914}

--- chrome/browser/task_manager/providers/worker_task_provider.cc
+++ chrome/browser/task_manager/providers/worker_task_provider.cc
@@ -21,6 +21,12 @@ Task* WorkerTaskProvider::GetTaskOfUrlRequest(int child_id, int route_id) {
 void WorkerTaskProvider::OnProfileAdded(Profile* profile) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
+  // It is possible for this method to be called multiple times for the same
+  // profile, if the profile loads an extension during initialization which also
+  // triggers this logic path. https://crbug.com/1065798.
+  if (observed_profiles_.IsObserving(profile))
+    return;
+
   observed_profiles_.Add(profile);
 
   auto per_profile_worker_task_tracker =
