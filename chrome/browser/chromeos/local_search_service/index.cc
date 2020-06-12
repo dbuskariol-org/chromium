7bd3c36fec4170fa20c92607662a62612d20e0da
Jia <jiameng@chromium.org>
2020-06-12 00:46:48 +0000

    [cros search service] Move existing impl to linear map
    Current Index implements linear map search. This cl moves the impl
    to its own class so that Index will remain a client-facing API.
    
    Bug: 1090132
    Change-Id: I36ccaa65113ab1b1e1fc9373eaa6eaca8d9c389b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241391
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777599}

--- chrome/browser/chromeos/local_search_service/index.cc
+++ chrome/browser/chromeos/local_search_service/index.cc
@@ -6,171 +6,48 @@
 
 #include <utility>
 
-#include "base/optional.h"
-#include "base/strings/string_split.h"
-#include "base/strings/string_util.h"
-#include "chrome/common/string_matching/fuzzy_tokenized_string_match.h"
-#include "chrome/common/string_matching/tokenized_string.h"
+#include "chrome/browser/chromeos/local_search_service/linear_map_search.h"
 
 namespace local_search_service {
 
-namespace {
-
-using Positions = std::vector<local_search_service::Position>;
-using TokenizedStringWithId =
-    std::pair<std::string, std::unique_ptr<TokenizedString>>;
-
-void TokenizeSearchTags(const std::vector<Content>& contents,
-                        std::vector<TokenizedStringWithId>* tokenized) {
-  DCHECK(tokenized);
-
-  for (const auto& content : contents) {
-    tokenized->push_back(std::make_pair(
-        content.id, std::make_unique<TokenizedString>(content.content)));
-  }
+Index::Index() {
+  linear_map_search_ = std::make_unique<LinearMapSearch>();
 }
-
-// Returns whether a given item with |search_tags| is relevant to |query| using
-// fuzzy string matching.
-bool IsItemRelevant(const TokenizedString& query,
-                    const std::vector<TokenizedStringWithId>& search_tags,
-                    double relevance_threshold,
-                    bool use_prefix_only,
-                    bool use_edit_distance,
-                    double partial_match_penalty_rate,
-                    double* relevance_score,
-                    Positions* positions) {
-  DCHECK(relevance_score);
-  DCHECK(positions);
-
-  if (search_tags.empty())
-    return false;
-
-  for (const auto& tag : search_tags) {
-    FuzzyTokenizedStringMatch match;
-    if (match.IsRelevant(query, *(tag.second), relevance_threshold,
-                         use_prefix_only, true /* use_weighted_ratio */,
-                         use_edit_distance, partial_match_penalty_rate, 0.1)) {
-      *relevance_score = match.relevance();
-      Position position;
-      position.content_id = tag.first;
-      positions->push_back(position);
-      return true;
-    }
-  }
-  return false;
-}
-
-// Compares Results by |score|.
-bool CompareResults(const local_search_service::Result& r1,
-                    const local_search_service::Result& r2) {
-  return r1.score > r2.score;
-}
-
-}  // namespace
-
-local_search_service::Content::Content(const std::string& id,
-                                       const base::string16& content)
-    : id(id), content(content) {}
-local_search_service::Content::Content() = default;
-local_search_service::Content::Content(const Content& content) = default;
-local_search_service::Content::~Content() = default;
-
-local_search_service::Data::Data(
-    const std::string& id,
-    const std::vector<local_search_service::Content>& contents)
-    : id(id), contents(contents) {}
-local_search_service::Data::Data() = default;
-local_search_service::Data::Data(const Data& data) = default;
-local_search_service::Data::~Data() = default;
-local_search_service::Result::Result() = default;
-local_search_service::Result::Result(const Result& result) = default;
-local_search_service::Result::~Result() = default;
-
-Index::Index() = default;
 
 Index::~Index() = default;
 
 uint64_t Index::GetSize() {
-  return data_.size();
+  DCHECK(linear_map_search_);
+  return linear_map_search_->GetSize();
 }
 
 void Index::AddOrUpdate(const std::vector<local_search_service::Data>& data) {
-  for (const auto& item : data) {
-    const auto& id = item.id;
-    DCHECK(!id.empty());
-
-    // If a key already exists, it will overwrite earlier data.
-    data_[id] = std::vector<TokenizedStringWithId>();
-    TokenizeSearchTags(item.contents, &data_[id]);
-  }
+  DCHECK(linear_map_search_);
+  linear_map_search_->AddOrUpdate(data);
 }
 
 uint32_t Index::Delete(const std::vector<std::string>& ids) {
-  uint32_t num_deleted = 0u;
-  for (const auto& id : ids) {
-    DCHECK(!id.empty());
-
-    const auto& it = data_.find(id);
-    if (it != data_.end()) {
-      // If id doesn't exist, just ignore it.
-      data_.erase(id);
-      ++num_deleted;
-    }
-  }
+  DCHECK(linear_map_search_);
+  const uint32_t num_deleted = linear_map_search_->Delete(ids);
   return num_deleted;
 }
 
 ResponseStatus Index::Find(const base::string16& query,
                            uint32_t max_results,
                            std::vector<Result>* results) {
-  DCHECK(results);
-  results->clear();
-  if (query.empty()) {
-    return ResponseStatus::kEmptyQuery;
-  }
-  if (data_.empty()) {
-    return ResponseStatus::kEmptyIndex;
-  }
-
-  *results = GetSearchResults(query, max_results);
-  return ResponseStatus::kSuccess;
+  DCHECK(linear_map_search_);
+  return linear_map_search_->Find(query, max_results, results);
 }
 
 void Index::SetSearchParams(const SearchParams& search_params) {
-  search_params_ = search_params;
+  DCHECK(linear_map_search_);
+  linear_map_search_->SetSearchParams(search_params);
 }
 
 SearchParams Index::GetSearchParamsForTesting() {
-  return search_params_;
+  DCHECK(linear_map_search_);
+  return linear_map_search_->GetSearchParams();
 }
 
-std::vector<Result> Index::GetSearchResults(const base::string16& query,
-                                            uint32_t max_results) const {
-  std::vector<Result> results;
-  const TokenizedString tokenized_query(query);
-
-  for (const auto& item : data_) {
-    double relevance_score = 0.0;
-    Positions positions;
-    if (IsItemRelevant(
-            tokenized_query, item.second, search_params_.relevance_threshold,
-            search_params_.use_prefix_only, search_params_.use_edit_distance,
-            search_params_.partial_match_penalty_rate, &relevance_score,
-            &positions)) {
-      Result result;
-      result.id = item.first;
-      result.score = relevance_score;
-      result.positions = positions;
-      results.push_back(result);
-    }
-  }
-
-  std::sort(results.begin(), results.end(), CompareResults);
-  if (results.size() > max_results && max_results > 0u) {
-    results.resize(max_results);
-  }
-  return results;
-}
 
 }  // namespace local_search_service
--- chrome/browser/chromeos/local_search_service/index.h
+++ chrome/browser/chromeos/local_search_service/index.h
@@ -13,92 +13,15 @@
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
-
-class TokenizedString;
+#include "chrome/browser/chromeos/local_search_service/shared_structs.h"
 
 namespace local_search_service {
 
-struct Content {
-  // An identifier for the content in Data.
-  std::string id;
-  base::string16 content;
-  Content(const std::string& id, const base::string16& content);
-  Content();
-  Content(const Content& content);
-  ~Content();
-};
-
-struct Data {
-  // Identifier of the data item, should be unique across the registry. Clients
-  // will decide what ids to use, they could be paths, urls or any opaque string
-  // identifiers.
-  // Ideally IDs should persist across sessions, but this is not strictly
-  // required now because data is not persisted across sessions.
-  std::string id;
-
-  // Data item will be matched between its search tags and query term.
-  std::vector<Content> contents;
-
-  Data(const std::string& id, const std::vector<Content>& contents);
-  Data();
-  Data(const Data& data);
-  ~Data();
-};
-
-struct SearchParams {
-  double relevance_threshold = 0.32;
-  double partial_match_penalty_rate = 0.9;
-  bool use_prefix_only = false;
-  bool use_edit_distance = false;
-};
-
-struct Position {
-  std::string content_id;
-  // TODO(jiameng): |start| and |end| will be implemented for inverted index
-  // later.
-  uint32_t start;
-  uint32_t length;
-};
-
-// Result is one item that matches a given query. It contains the id of the item
-// and its matching score.
-struct Result {
-  // Id of the data.
-  std::string id;
-  // Relevance score.
-  // Currently only linear map is implemented with fuzzy matching and score will
-  // always be in [0,1]. In the future, when an inverted index is implemented,
-  // the score will not be in this range any more. Client will be able to select
-  // a search backend to use (linear map vs inverted index) and hence client
-  // will be able to expect the range of the scores.
-  double score;
-  // Position of the matching text.
-  // We currently use linear map, which will return one matching content, hence
-  // the vector has only one element. When we have inverted index, we will have
-  // multiple matching contents.
-  std::vector<Position> positions;
-  Result();
-  Result(const Result& result);
-  ~Result();
-};
-
-// Status of the search attempt.
-// These numbers are used for logging and should not be changed or reused. More
-// will be added later.
-enum class ResponseStatus {
-  kUnknownError = 0,
-  // Search operation is successful. But there could be no matching item and
-  // result list is empty.
-  kSuccess = 1,
-  // Query is empty.
-  kEmptyQuery = 2,
-  // Index is empty (i.e. no data).
-  kEmptyIndex = 3
-};
+class LinearMapSearch;
 
 // A local search service Index.
-// It has a registry of searchable data, which can be updated. It also runs a
-// synchronous search function to find matching items for a given query.
+// It is the client-facing API for search and indexing. It owns different
+// backends that provide actual data storage/indexing/search functions.
 class Index {
  public:
   Index();
@@ -131,21 +54,10 @@ class Index {
   void SetSearchParams(const SearchParams& search_params);
 
   SearchParams GetSearchParamsForTesting();
-
  private:
-  // Returns all search results for a given query.
-  std::vector<Result> GetSearchResults(const base::string16& query,
-                                       uint32_t max_results) const;
-
-  // A map from key to a vector of (tag-id, tokenized tag).
-  std::map<
-      std::string,
-      std::vector<std::pair<std::string, std::unique_ptr<TokenizedString>>>>
-      data_;
-
-  // Search parameters.
-  SearchParams search_params_;
-
+  // TODO(jiameng): Currently linear map is the only backend supported. We will
+  // add inverted index in the next CLs.
+  std::unique_ptr<LinearMapSearch> linear_map_search_;
   base::WeakPtrFactory<Index> weak_ptr_factory_{this};
 };
 
