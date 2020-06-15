603bdc5b98437a8f0c882a9834a22ad31b4b35a1
Koji Ishii <kojii@chromium.org>
2020-06-15 02:48:25 +0000

    [FragmentItem] Fix NGCaretPosition::ToPositionInDOMTreeWithAffinity
    This patch recovers the "!" in the "if" condition that was
    accidentally removed in r707711 <crrev.com/c/1866101>.
    
    Bug: 982194
    Change-Id: Id0c91b737ac4dd3c0459f9c8e6bf0df267c846d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244753
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778173}

--- third_party/blink/renderer/core/layout/ng/inline/ng_caret_position.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_caret_position.cc
@@ -322,17 +322,17 @@ PositionWithAffinity NGCaretPosition::ToPositionInDOMTreeWithAffinity() const {
     return PositionWithAffinity();
   switch (position_type) {
     case NGCaretPositionType::kBeforeBox:
-      if (cursor.Current().GetNode())
-        return PositionWithAffinity();
-      return PositionWithAffinity(
-          Position::BeforeNode(*cursor.Current().GetNode()),
-          TextAffinity::kDownstream);
+      if (const Node* node = cursor.Current().GetNode()) {
+        return PositionWithAffinity(Position::BeforeNode(*node),
+                                    TextAffinity::kDownstream);
+      }
+      return PositionWithAffinity();
     case NGCaretPositionType::kAfterBox:
-      if (cursor.Current().GetNode())
-        return PositionWithAffinity();
-      return PositionWithAffinity(
-          Position::AfterNode(*cursor.Current().GetNode()),
-          TextAffinity::kUpstreamIfPossible);
+      if (const Node* node = cursor.Current().GetNode()) {
+        return PositionWithAffinity(Position::AfterNode(*node),
+                                    TextAffinity::kUpstreamIfPossible);
+      }
+      return PositionWithAffinity();
     case NGCaretPositionType::kAtTextOffset:
       // In case of ::first-letter, |cursor.Current().GetNode()| is null.
       DCHECK(text_offset.has_value());
