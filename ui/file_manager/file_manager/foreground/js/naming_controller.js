436317e3843bbb83444a7497292e484d88537209
Mikel Astiz <mastiz@chromium.org>
2020-06-15 09:52:29 +0000

    Revert "Remove renaming workaround for crbug.com/644140"
    This reverts commit 66bd695d9968ece73b5dc768db6a6e218fc64f02.
    
    Reason for revert: speculative revert for crbug.com/1094826 after
    new errors are reported for graphics.TraceReplay.glxgears_stable.
    
    Bug: 1094826
    
    Original change's description:
    > Remove renaming workaround for crbug.com/644140
    > 
    > Bug: 644140
    > Change-Id: Ie240afaf3718479d07284b44c4357be430b792af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241370
    > Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#778161}
    
    TBR=lucmult@chromium.org,austinct@chromium.org
    
    Change-Id: Ic943871e86cf3bdab857f20f91e569347ec8d18e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 644140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245618
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778234}

--- ui/file_manager/file_manager/foreground/js/naming_controller.js
+++ ui/file_manager/file_manager/foreground/js/naming_controller.js
@@ -209,6 +209,12 @@ class NamingController {
    * @private
    */
   onRenameInputKeyDown_(event) {
+    // Ignore key events if event.keyCode is VK_PROCESSKEY(229).
+    // TODO(fukino): Remove this workaround once crbug.com/644140 is fixed.
+    if (event.keyCode === 229) {
+      return;
+    }
+
     if (!this.isRenamingInProgress()) {
       return;
     }
