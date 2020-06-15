a7bf634cd824db67d6657612fc34bf4850f992f5
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-06-15 16:33:11 +0000

    Reland "[CrOS settings] Add bluetooth_pairing_dialog resource files"
    This reverts commit fb4591bf63275dd689f15c5afa61b01a38808468
    
    Reason for revert:
    Fixed issue relating to flattening html resource file
    Original change's description:
    > Revert "[CrOS settings] Add bluetooth_pairing_dialog resource files"
    >
    > This reverts commit 29ca85f8934dba2ea836fa8fa8ba6b65ed7e9647.
    >
    > Reason for revert:
    > Seems to cause flaky analyzer step failures on https://ci.chromium.org/p/chromium/builders/try/chromeos-amd64-generic-rel
    >
    > Example:
    > https://ci.chromium.org/p/chromium/builders/try/chromeos-amd64-generic-rel/568950
    >
    > Exception: Failed to open ../../out/Release/gen/chrome/browser/resources/chromeos/bluetooth_pairing_dialog/vulcanized.html while trying to flatten ../../out/Release/gen/chrome/browser/resources/chromeos/bluetooth_pairing_dialog/vulcanized.html. (No such file or directory)
    >
    >
    > Original change's description:
    > > [CrOS settings] Add bluetooth_pairing_dialog resource files
    > >
    > > Split bluetooth_pairing_dialog resources into
    > > bluetooth_pairing_dialog_resources.grd
    > > and bluetooth_pairing_dialog_resources_vulcanized.grd
    > >
    > > Bug: 1090882
    > > Change-Id: I0d9855aaaf21db900f27f92b7b38d7ab1daac18a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239147
    > > Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#777891}
    >
    > TBR=sky@chromium.org,khorimoto@chromium.org,tjohnsonkanu@google.com
    >
    > Change-Id: I4af97b356a782dcb6cb88472d8528c54350e9871
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1090882
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243855
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#777985}
    
    Bug: 1090882
    Change-Id: I5f89c6878bd9abe9810e69b123af65dc2b28ee79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244047
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#778328}

--- chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
+++ chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
@@ -8,7 +8,7 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chromeos/bluetooth_dialog_localized_strings_provider.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/grit/browser_resources.h"
+#include "chrome/grit/bluetooth_pairing_dialog_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
