1799b2126e69765a992b226efbc7b2cbdd74f16e
Liquan (Max) Gu <maxlg@chromium.org>
2020-06-16 15:39:06 +0000

    [ExpandablePaymentHandler] Enable by default
    Now that Expandable Payment Handler (aka Bottom Sheet Payment Handler)
    has been approved to launch, this CL changes the flag to enable it by
    default.
    
    Bug: 1037646
    
    Change-Id: Ia294c94d5762515add6122ae3113d345683f00f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241502
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778816}

--- components/payments/content/android/payment_feature_list.cc
+++ components/payments/content/android/payment_feature_list.cc
@@ -52,8 +52,9 @@ const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
 // Android only features.
 const base::Feature kAndroidAppPaymentUpdateEvents{
     "AndroidAppPaymentUpdateEvents", base::FEATURE_DISABLED_BY_DEFAULT};
+// TODO(crbug.com/1094549): clean up after being stable.
 const base::Feature kScrollToExpandPaymentHandler{
-    "ScrollToExpandPaymentHandler", base::FEATURE_DISABLED_BY_DEFAULT};
+    "ScrollToExpandPaymentHandler", base::FEATURE_ENABLED_BY_DEFAULT};
 
 static jboolean JNI_PaymentFeatureList_IsEnabled(
     JNIEnv* env,
