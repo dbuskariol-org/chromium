4d192011709d9b9aa07d95213ab4dad2ec8a77be
shivanigithub <shivanisha@chromium.org>
2020-06-16 11:57:31 +0000

    Enable network isolation key to use scheme://eTLD+1
    Since NIK is based on scheme://etld+1, enable the feature by default.
    
    Change-Id: I1aed1dfbb6ab2581410e3bb66cd75e0777499fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972531
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778741}

--- net/base/features.cc
+++ net/base/features.cc
@@ -158,7 +158,7 @@ const base::Feature kAppendFrameOriginToNetworkIsolationKey{
 
 const base::Feature kUseRegistrableDomainInNetworkIsolationKey{
     "UseRegistrableDomainInNetworkIsolationKey",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kTurnOffStreamingMediaCaching{
     "TurnOffStreamingMediaCaching", base::FEATURE_DISABLED_BY_DEFAULT};
