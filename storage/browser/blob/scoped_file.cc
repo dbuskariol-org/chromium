2261633c4179f1c060540c7ba73e53237faa58cd
Hans Wennborg <hans@chromium.org>
2020-06-17 20:38:17 +0000

    Include-what-you-use related to logging.h (storage)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I2bb6d98fe58955ec4bca2a4a2c73ce786c8d16aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248632
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779445}

--- storage/browser/blob/scoped_file.cc
+++ storage/browser/blob/scoped_file.cc
@@ -8,6 +8,7 @@
 #include "base/callback.h"
 #include "base/files/file_util.h"
 #include "base/location.h"
+#include "base/logging.h"
 #include "base/task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 
