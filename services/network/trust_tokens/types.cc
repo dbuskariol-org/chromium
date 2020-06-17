a48f280b80aec4fcf7b8a9e13358bb0eab751634
Nigel Tao <nigeltao@chromium.org>
2020-06-17 00:21:30 +0000

    Move base/time #include out of util header
    In general, if M files #include a utility header and that header file
    also #include's N files then, very roughly speaking, compilation has to
    process O(M * N) lines of code. This can get increasingly expensive the
    more a utility library becomes a dumping ground of all manner of
    miscellaneous helpers, increasing both M and N. A general counter-
    measure is to move #includes out of the .h file and into the .cc files
    that actually depend on the specific type (e.g. FilePath, TimeDelta,
    UnguessableToken, etc).
    
    This commit does that for the base::Time and base::TimeDelta types.
    
    Bug: 1086733
    Change-Id: Id6009c8377d27eea5c48f1b22c757410e2f13f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247938
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779094}

--- services/network/trust_tokens/types.cc
+++ services/network/trust_tokens/types.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/time/time.h"
 #include "base/util/values/values_util.h"
 
 namespace network {
