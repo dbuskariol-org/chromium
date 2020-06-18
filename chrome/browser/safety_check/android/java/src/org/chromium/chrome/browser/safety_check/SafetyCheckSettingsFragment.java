05dcc07471452433bf096370cf58e74374c34997
Andrey Zaytsev <andzaytsev@google.com>
2020-06-18 16:54:41 +0000

    Safety check on Android: implemented initial UI
    Screenshots:
    Light mode: https://screenshot.googleplex.com/c5hr9M0ypqp.png
    Dark mode: https://screenshot.googleplex.com/iv2wB2Dih0s.png
    
    Bug: 1070620
    Change-Id: Ie256680755395d0de1713af48bb8975f96f7a213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238066
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#779864}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckSettingsFragment.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckSettingsFragment.java
@@ -5,8 +5,15 @@
 package org.chromium.chrome.browser.safety_check;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.preference.PreferenceFragmentCompat;
+
+import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.ui.widget.ButtonCompat;
 
 /**
  * Fragment containing Safety check.
@@ -14,6 +21,23 @@ import androidx.preference.PreferenceFragmentCompat;
 public class SafetyCheckSettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
+        SettingsUtils.addPreferencesFromResource(this, R.xml.safety_check_preferences);
         getActivity().setTitle(R.string.prefs_safety_check);
     }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LinearLayout view =
+                (LinearLayout) super.onCreateView(inflater, container, savedInstanceState);
+
+        // Add a button to the bottom of the preferences view.
+        ButtonCompat checkButton =
+                (ButtonCompat) inflater.inflate(R.layout.safety_check_button, view, false);
+        checkButton.setOnClickListener((View v) -> onSafetyCheckButtonClicked());
+        view.addView(checkButton);
+        return view;
+    }
+
+    private void onSafetyCheckButtonClicked() {}
 }
--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/IconPreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/IconPreference.java
@@ -1,8 +1,8 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.browser_ui.site_settings;
+package org.chromium.components.browser_ui.settings;
 
 import android.content.Context;
 import android.util.AttributeSet;
@@ -11,16 +11,14 @@ import android.widget.ImageView;
 import androidx.core.view.ViewCompat;
 import androidx.preference.PreferenceViewHolder;
 
-import org.chromium.components.browser_ui.settings.ChromeBasePreference;
-
 /**
- * A custom preference for drawing Site Settings entries.
+ * A preference with a horizontally centered icon.
  */
-public class SiteSettingsPreference extends ChromeBasePreference {
+public class IconPreference extends ChromeBasePreference {
     /**
      * Constructor for inflating from XML.
      */
-    public SiteSettingsPreference(Context context, AttributeSet attrs) {
+    public IconPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -28,6 +26,10 @@ public class SiteSettingsPreference extends ChromeBasePreference {
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
 
+        // Horizontally center the preference icon.
+
+        // TODO(crbug.com/1095981): move this logic to ChromeBasePreference and
+        // find a way to center it without a hard-coded padding.
         int padding = getContext().getResources().getDimensionPixelSize(R.dimen.pref_icon_padding);
         ImageView icon = (ImageView) holder.findViewById(android.R.id.icon);
         ViewCompat.setPaddingRelative(
