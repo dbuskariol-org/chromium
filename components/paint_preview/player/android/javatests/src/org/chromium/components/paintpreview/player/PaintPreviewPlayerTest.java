5fec4b13504b22d8442f79243f6b09e0d32054f7
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-06-18 18:00:20 +0000

    [Paint Preview] Re-enable flaky test
    This re-enables PaintPreviewPlayerTest#overscrollRefreshTest.
    The failure cannot be reproduces locally and the bot on which this test
    was flaking has been killed recently [1].
    
    [1]: https://chromium-swarm.appspot.com/bot?id=luci-chromium-ci-xenial-us-central1-b-8-76-mhb1
    
    Bug: 1093083
    Change-Id: Ifacf72e3373fbcfa513d5f8bc87afec03c0c086e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252578
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779899}

--- components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
+++ components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
@@ -20,7 +20,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
@@ -131,7 +130,6 @@ public class PaintPreviewPlayerTest extends DummyUiActivityTestCase {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1093083")
     public void overscrollRefreshTest() throws Exception {
         initPlayerManager();
         UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
