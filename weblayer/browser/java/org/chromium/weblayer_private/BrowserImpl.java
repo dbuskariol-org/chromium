6dd1966c18362d549ab39527b6ca41523f9af2ac
John Abd-El-Malek <jam@chromium.org>
2020-06-19 05:49:34 +0000

    Notify metrics when WebLayer's activity enters foreground and background.
    Bug: 1096390
    Change-Id: I83d9062895cdc29c6f21b815105123b376472085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251054
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780181}

--- weblayer/browser/java/org/chromium/weblayer_private/BrowserImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserImpl.java
@@ -468,10 +468,12 @@ public class BrowserImpl extends IBrowser.Stub {
     public void onFragmentResume() {
         mFragmentResumed = true;
         WebLayerAccessibilityUtil.get().onBrowserResumed();
+        BrowserImplJni.get().onFragmentResume(mNativeBrowser);
     }
 
     public void onFragmentPause() {
         mFragmentResumed = false;
+        BrowserImplJni.get().onFragmentPause(mNativeBrowser);
     }
 
     public boolean isStarted() {
@@ -535,5 +537,7 @@ public class BrowserImpl extends IBrowser.Stub {
                 byte[] persistenceCryptoKey, byte[] minimalPersistenceState);
         void webPreferencesChanged(long nativeBrowserImpl);
         void onFragmentStart(long nativeBrowserImpl);
+        void onFragmentResume(long nativeBrowserImpl);
+        void onFragmentPause(long nativeBrowserImpl);
     }
 }
