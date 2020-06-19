0cae80939e9d76c87e458210967f5e71bb36e235
Xi Han <hanxi@google.com>
2020-06-19 15:17:07 +0000

    Disable InstantStartTest#testFeedPlaceholderVisibility.
    Bug: 1097069
    Change-Id: I5becd0a8850c8eff6dcbb3bc5e55af656dac35fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254060
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780302}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
@@ -48,6 +48,7 @@ import org.chromium.base.StreamUtil;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Matchers;
 import org.chromium.base.test.util.Restriction;
@@ -667,6 +668,7 @@ public class InstantStartTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1097069")
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     @EnableFeatures({ChromeFeatureList.TAB_SWITCHER_ON_RETURN + "<Study,",
             ChromeFeatureList.START_SURFACE_ANDROID + "<Study"})
