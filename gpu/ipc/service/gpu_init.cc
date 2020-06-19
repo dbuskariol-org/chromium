eb1475dcaf40321aa891e387e98bcd0f84ba4491
Hans Wennborg <hans@chromium.org>
2020-06-19 11:11:02 +0000

    Include-what-you-use related to logging.h (gpu)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    TBR=kinuko
    
    Bug: 1031540
    Change-Id: I302009b875c8cdad1465cb72331fdd22dd33adeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249674
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780251}

--- gpu/ipc/service/gpu_init.cc
+++ gpu/ipc/service/gpu_init.cc
@@ -7,6 +7,7 @@
 #include <string>
 
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
