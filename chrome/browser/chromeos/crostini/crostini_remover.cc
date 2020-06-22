7e6417adb3800f52f8b8bb1ae0ec5609ce6253e1
Joel Hockey <joelhockey@chromium.org>
2020-06-22 05:17:17 +0000

    Do not stop concierge when removing crostini
    Bug: 1097596
    Change-Id: Ia792220a5daa29f7d855117162193468c5a5a29e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256107
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780665}

--- chrome/browser/chromeos/crostini/crostini_remover.cc
+++ chrome/browser/chromeos/crostini/crostini_remover.cc
@@ -40,7 +40,7 @@ void CrostiniRemover::RemoveCrostini() {
   } else {
     // Crostini installation didn't install the component. Concierge should not
     // be running, nor should there be any VMs.
-    CrostiniRemover::StopConciergeFinished(true);
+    CrostiniRemover::DestroyDiskImageFinished(true);
   }
 }
 
@@ -84,14 +84,6 @@ void CrostiniRemover::DestroyDiskImageFinished(bool success) {
     std::move(callback_).Run(CrostiniResult::DESTROY_DISK_IMAGE_FAILED);
     return;
   }
-  // Only set kCrostiniEnabled to false once cleanup is completely finished.
-  CrostiniManager::GetForProfile(profile_)->StopConcierge(
-      base::BindOnce(&CrostiniRemover::StopConciergeFinished, this));
-}
-
-void CrostiniRemover::StopConciergeFinished(bool is_successful) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  // The is_successful parameter is never set by debugd.
 
   // UninstallTerminaComponent returns false both if Termina wasn't installed
   // and if the uninstall failed, so we explicitly reset the relevant
