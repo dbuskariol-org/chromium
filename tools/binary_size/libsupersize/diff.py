d20a3819ecd0813bb6d8f5ce6f06335c2950a471
Samuel Huang <huangs@chromium.org>
2020-06-22 15:32:12 +0000

    Reland "[SuperSize] Add multiple Container support using .ssargs files."
    This reverts commit fc8e613a44039a82952e38f889e6dd7480f0037a.
    
    Reason for revert: Fixed the issue.
    
    Original change's description:
    > Revert "[SuperSize] Add multiple Container support using .ssargs files."
    > 
    > This reverts commit 19d7a7806471a0601db6f155a20668acc3dc9edd.
    > 
    > Reason for revert: Breaking CQ (see bug)
    > 
    > Original change's description:
    > > [SuperSize] Add multiple Container support using .ssargs files.
    > > 
    > > This CL adds SuperSize support for multiple containers. Details:
    > > * Introduce .ssargs file, which is a text file to specify multiple
    > >   containers for SuperSize-archive to process into a single .size file.
    > >   * Container file types are auto-detected.
    > >   * Container files are relative to .ssargs file location, unless
    > >     absolute paths are given.
    > > * Introduce Version 1.1 .size file format:
    > >   * Header fields contain Container specs, now absorbing metadata and
    > >     section_sizes.
    > >   * Per-section data become per-container-per-section.
    > > * For compatibility: Running SuperSize-archive on one container still
    > >   generates Version 1.0 .size file format.
    > > * Update SuperSize-console output (including diffs) to accommodate.
    > > 
    > > After this CL, basic Trichrome support is available. A recommended
    > > .ssargs file (3 lines) for the official build is as follows:
    > > 
    > > TrichromeLibraryGoogle.apk --name Library
    > > TrichromeChromeGoogle.minimal.apks --name Chrome --java-only
    > > TrichromeWebViewGoogle.apk --name WebView --java-only
    > > 
    > > Bug: 1040645
    > > Change-Id: I67df00731d07660163410b59c57854bf56ea651e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248860
    > > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#780594}
    > 
    > TBR=huangs@chromium.org,agrieve@chromium.org
    > 
    > Change-Id: Ic3175ea3e8ede41e3183c12b5a00a4a09be55a75
    > No-Try: True
    > Bug: 1040645, 1097572
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256593
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#780639}
    
    TBR=huangs@chromium.org,agrieve@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1040645, 1097572
    Change-Id: I95a20b271d7522a1aa70cc3247ad9a1fab911dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257279
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780788}

--- tools/binary_size/libsupersize/diff.py
+++ tools/binary_size/libsupersize/diff.py
@@ -92,6 +92,8 @@ def _MatchSymbols(before, after, key_func, padding_by_section_name):
 def _DiffSymbolGroups(before, after):
   # For changed symbols, padding is zeroed out. In order to not lose the
   # information entirely, store it in aggregate.
+  # Ignoring Containers, i.e., paddings from sections across Containers are
+  # combined.
   padding_by_section_name = collections.defaultdict(int)
 
   # Usually >90% of symbols are exact matches, so all of the time is spent in
@@ -150,19 +152,25 @@ def _DiffObj(before_obj, after_obj):
 
 
 def _DiffContainerLists(before_containers, after_containers):
-  """Computes diffs between two lists of Containers."""
-  # TODO(huangs): Add support for multiple containers (needs name matching).
-  assert len(before_containers) == 1
-  assert len(after_containers) == 1
+  """Computes diff of Containers lists, matching names."""
+  # Find ordered unique names, preferring order of |container_after|.
+  pairs = collections.OrderedDict()
+  for c in after_containers:
+    pairs[c.name] = [models.Container.Empty(), c]
+  for c in before_containers:
+    if c.name in pairs:
+      pairs[c.name][0] = c
+    else:
+      pairs[c.name] = [c, models.Container.Empty()]
   ret = []
-  for (before_c, after_c) in zip(before_containers, after_containers):
-    name = after_c.name
-    assert before_c.name == name
+  for name, [before_c, after_c] in pairs.items():
     ret.append(
         models.Container(name=name,
                          metadata=_DiffObj(before_c.metadata, after_c.metadata),
                          section_sizes=_DiffObj(before_c.section_sizes,
                                                 after_c.section_sizes)))
+  # This update newly created diff Containers, not existing ones or EMPTY.
+  models.Container.AssignShortNames(ret)
   return ret
 
 
