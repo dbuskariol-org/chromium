5640f6c5db5113316ee6466da120be146f8b8ff2
Adam Langley <agl@chromium.org>
2020-06-23 01:45:21 +0000

    device/fido: add a cache for authenticator ECDH keys.
    While, at the moment, I don't believe we will end up asking for the
    authenticator's ECDH key twice, when we support the PRF extension we
    could. Thus add a cache and skip asking an authenticator for it twice.
    
    Change-Id: I73ecaa38447cbbe04f34c12d83f9c79bca553c90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255208
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#781110}

--- device/fido/fido_device_authenticator.h
+++ device/fido/fido_device_authenticator.h
@@ -127,6 +127,9 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoDeviceAuthenticator
                               base::Optional<pin::KeyAgreementResponse>)>;
   void InitializeAuthenticatorDone(base::OnceClosure callback);
   void GetEphemeralKey(GetEphemeralKeyCallback callback);
+  void OnHaveEphemeralKey(GetEphemeralKeyCallback callback,
+                          CtapDeviceResponseCode status,
+                          base::Optional<pin::KeyAgreementResponse> key);
   void OnHaveEphemeralKeyForGetPINToken(
       std::string pin,
       uint8_t permissions,
@@ -182,6 +185,7 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoDeviceAuthenticator
   base::Optional<AuthenticatorSupportedOptions> options_;
   std::unique_ptr<FidoTask> task_;
   std::unique_ptr<GenericDeviceOperation> operation_;
+  base::Optional<pin::KeyAgreementResponse> cached_ephemeral_key_;
   base::WeakPtrFactory<FidoDeviceAuthenticator> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(FidoDeviceAuthenticator);
