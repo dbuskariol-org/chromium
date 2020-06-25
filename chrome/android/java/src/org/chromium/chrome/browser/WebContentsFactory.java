944a182e78611749020916b852e9eeab8b586587
Ramin Halavati <rhalavati@chromium.org>
2020-06-25 06:37:44 +0000

    Update CreateWebContents to receive the actual profile.
    WebContentsFactory::CreateWebContents received a profile and a boolean
    that said if it is incognito or not. To cover non-primary off-the-record
    profiles, one had to pass the actual profile and an incognito=false
    param.
    
    For better readability and simplifying the API, it is now changed to
    pass the actual profile only and the |incognito| boolean is dropped.
    
    Bug: 1060940
    Change-Id: I3eab77818ee4462179f22f1f59808bea90bd0238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2263072
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782298}

--- chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
@@ -26,18 +26,32 @@ public class WebContentsFactory {
      *                        off-the-record profile or not.
      * @param initiallyHidden Whether or not the {@link WebContents} should be initially hidden.
      * @return                A newly created {@link WebContents} object.
+     *
+     * @deprecated use {@link #createWebContents(Profile, boolean)} instead.
      */
-    // TODO(pshmakov): remove static for unit-testability.
+    @Deprecated
     public static WebContents createWebContents(boolean incognito, boolean initiallyHidden) {
-        return WebContentsFactoryJni.get().createWebContents(
-                Profile.getLastUsedRegularProfile(), incognito, initiallyHidden, false);
+        Profile profile = Profile.getLastUsedRegularProfile();
+        if (incognito) profile = profile.getPrimaryOTRProfile();
+        return createWebContents(profile, initiallyHidden, false);
+    }
+
+    /**
+     * A factory method to build a {@link WebContents} object.
+     * @param profile         The profile with which the {@link WebContents} should be built.
+     * @param initiallyHidden Whether or not the {@link WebContents} should be initially hidden.
+     * @return                A newly created {@link WebContents} object.
+     */
+    // TODO(https://crbug.com/1099138): Remove static for unit-testability.
+    public static WebContents createWebContents(Profile profile, boolean initiallyHidden) {
+        return WebContentsFactoryJni.get().createWebContents(profile, initiallyHidden, false);
     }
 
     // TODO(https://crbug.com/1033955): Remove after check discard error is fixed.
-    private static WebContents createWebContents(Profile profile, boolean incognito,
-            boolean initiallyHidden, boolean initializeRenderer) {
+    private static WebContents createWebContents(
+            Profile profile, boolean initiallyHidden, boolean initializeRenderer) {
         return WebContentsFactoryJni.get().createWebContents(
-                profile, incognito, initiallyHidden, initializeRenderer);
+                profile, initiallyHidden, initializeRenderer);
     }
 
     /**
@@ -49,11 +63,15 @@ public class WebContentsFactory {
      *                        off-the-record profile or not.
      * @param initiallyHidden Whether or not the {@link WebContents} should be initially hidden.
      * @return                A newly created {@link WebContents} object.
+     *
+     * @deprecated use {@link #createWebContentsWithWarmRenderer(Profile, boolean)} instead.
      */
+    @Deprecated
     public WebContents createWebContentsWithWarmRenderer(
             boolean incognito, boolean initiallyHidden) {
-        return createWebContents(
-                Profile.getLastUsedRegularProfile(), incognito, initiallyHidden, true);
+        Profile profile = Profile.getLastUsedRegularProfile();
+        if (incognito) profile = profile.getPrimaryOTRProfile();
+        return createWebContents(profile, initiallyHidden, true);
     }
 
     /**
@@ -66,12 +84,12 @@ public class WebContentsFactory {
      * @return                A newly created {@link WebContents} object.
      */
     public WebContents createWebContentsWithWarmRenderer(Profile profile, boolean initiallyHidden) {
-        return createWebContents(profile, false, initiallyHidden, true);
+        return createWebContents(profile, initiallyHidden, true);
     }
 
     @NativeMethods
     interface Natives {
-        WebContents createWebContents(Profile profile, boolean incognito, boolean initiallyHidden,
-                boolean initializeRenderer);
+        WebContents createWebContents(
+                Profile profile, boolean initiallyHidden, boolean initializeRenderer);
     }
 }
